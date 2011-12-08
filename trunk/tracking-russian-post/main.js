fa02c8c8fe8fee57a1f5a6426d0b0ce66a797b86
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2011-12-08 18:58:47 +0000

    Мелкие правки и приведение в соответствие с доками

--- trunk/tracking-russian-post/main.js
+++ trunk/tracking-russian-post/main.js
@@ -17,6 +17,9 @@ function getRate(result, info, namein, nameout){
 function main(){
 	AnyBalance.trace('Connecting to russianpost...');
 	
+	var prefs = getPreferences();
+	var post_number = prefs.code; //Код отправления, введенный пользователем
+	
 	var dt = new Date();
 	var info = AnyBalance.requestPost('http://www.russianpost.ru/resp_engine.aspx?Path=rp/servise/ru/home/postuslug/trackingpo', {
 		PATHCUR:'rp/servise/ru/home/postuslug/trackingpo',
@@ -25,46 +28,64 @@ function main(){
 		CYEAR:dt.getFullYear(),
 		PATHWEB:'RP/INDEX/RU/Home',
 		PATHPAGE:'RP/INDEX/RU/Home/Search',
-		BarCode:g_preferences.code,
+		BarCode:post_number,
 		searchsign:1
 	});
 	
 	var result = {success: true},
 		matches;
 	
-	if(matches = info.match(/<tr[^>]*><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><td>([^<]*)<\/td><\/tr><\/tbody>/i)){
-		var operation, date, location, attribute;
-		if(AnyBalance.isAvailable('fulltext','operation'))
-			operation = matches[1];
-		if(AnyBalance.isAvailable('fulltext','date'))
-			date = matches[2];
-		if(AnyBalance.isAvailable('index'))
-			result.index = matches[3];
-		if(AnyBalance.isAvailable('fulltext','location'))
-			location = matches[4];
-		if(AnyBalance.isAvailable('fulltext','attribute'))
-			attribute = matches[5];
-		if(AnyBalance.isAvailable('weight'))
-			weight = matches[6];
+	AnyBalance.trace('trying to find table');
+	
+	//Сначала найдём таблицу, содержащую все стадии отправления
+	if(matches = info.match(/<table class="pagetext">.*?<tbody>(.*?)<\/tbody>/)){
+		AnyBalance.trace('found table');
+		var alltable = matches[1];
+		//Потом найдем там последнюю строку
+		var lasttr = alltable.lastIndexOf('<tr');
+		AnyBalance.trace('found last row at ' + lasttr);
 		
-		if(AnyBalance.isAvailable('operation'))
-			result.operation = operation;
-		if(AnyBalance.isAvailable('date'))
-			result.date = date;
-		if(AnyBalance.isAvailable('date'))
-			result.date = date;
-		if(AnyBalance.isAvailable('location'))
-			result.location = location;
-		if(AnyBalance.isAvailable('attribute'))
-			result.attribute = attribute;
+		info = alltable.substring(lasttr);
+		AnyBalance.trace(info);
 		
-		if(AnyBalance.isAvailable('fulltext'))
-			result.fulltext = date + ': ' + operation + '\n' +
-				location + '\n' + 
-				attribute;
-		
-		AnyBalance.setResult(result);
-	}else{
-		throw new AnyBalance.Error("Отправление не найдено.")
+	
+		//Потом найдем отдельные поля
+		if(matches = info.match(/<tr[^>]*><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><td>(.*?)<\/td><\/tr>/i)){
+			AnyBalance.trace('parsed fields');
+			var operation, date, location, attribute;
+			if(AnyBalance.isAvailable('fulltext','operation'))
+				operation = matches[1];
+			if(AnyBalance.isAvailable('fulltext','date'))
+				date = matches[2];
+			if(AnyBalance.isAvailable('index'))
+				result.index = matches[3];
+			if(AnyBalance.isAvailable('fulltext','location'))
+				location = matches[4];
+			if(AnyBalance.isAvailable('fulltext','attribute'))
+				attribute = matches[5];
+			if(AnyBalance.isAvailable('weight'))
+				weight = matches[6];
+			
+			if(AnyBalance.isAvailable('operation'))
+				result.operation = operation;
+			if(AnyBalance.isAvailable('date'))
+				result.date = date;
+			if(AnyBalance.isAvailable('date'))
+				result.date = date;
+			if(AnyBalance.isAvailable('location'))
+				result.location = location;
+			if(AnyBalance.isAvailable('attribute'))
+				result.attribute = attribute;
+			
+			if(AnyBalance.isAvailable('fulltext'))
+				result.fulltext = date + ': ' + operation + '\n' +
+					location + '\n' + 
+					attribute;
+			
+			AnyBalance.setResult(result);
+		}
 	}
+	
+	if(!AnyBalance.isSetResultCalled())
+		throw new AnyBalance.Error("Отправление не найдено.")
 }

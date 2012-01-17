1adf64d7bc13ec8504b104c9e2f5c0c17dae99d4
serg.repin@gmail.com <serg.repin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-01-17 20:23:54 +0000

    Обработка ошибок соединения.

--- trunk/ab-internet-ipls/main.js
+++ trunk/ab-internet-ipls/main.js
@@ -1,4 +1,4 @@
-﻿/**
+ /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
 Домашний Интернет и Телевидение Айпильсин
@@ -7,54 +7,56 @@
 */
 
 function main(){
-    var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://stat.ipls.ru/user/';
-    AnyBalance.setDefaultCharset('utf-8');    
-    // Заходим на главную страницу
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://stat.ipls.ru/user/';
+AnyBalance.setDefaultCharset('utf-8');    
+// Заходим на главную страницу
     var info = AnyBalance.requestPost(baseurl + "", {
-        login: prefs.login,
+    	login: prefs.login,
         password: prefs.password
     });
-    var $parse = $(info);
+var $parse = $(info);
     
-    var error = $('<div>' + info + '</div>').find('Неверно указаны логин или пароль').text();
-    if(error) //Что-то неправильно произошло. Скорее всего, пароль неправильный
-        throw new AnyBalance.Error(error);
+var error;
+    if(error=/style='color:red'>([\W]+)</.exec(info)) //Что-то неправильно произошло. Скорее всего, пароль неправильный
+      throw new AnyBalance.Error(error[1]);
+      if(error=/<div class='mainmenu-act'>Вход в личный кабинет<\/div>/.exec(info))
+      throw new AnyBalance.Error("Не указаны логин или пароль.");
     
     
-    var result = {
-        success: true
-    };
+var result = {success: true};
 
-    var matches;
+var matches;
     //Тарифный план
     if (matches=/Текущий тариф<\/td>\s<td[\s\w='-\/%]*>([\S ]+)</.exec(info)){
-        result.__tariff=matches[1];
+    	result.__tariff=matches[1];
     }
+    
 
-    // Баланс
+
+// Баланс
     if(AnyBalance.isAvailable('balance')){
-        if (matches=/Баланс<\/td>\s<td[\s\w='-\/%]*>([\d\.]+)</.exec(info)){
-            var tmpBalance=matches[1].replace(/ |\xA0/, ""); // Удаляем пробелы
-            tmpBalance=tmpBalance.replace(",", "."); // Заменяем запятую на точку
-            result.balance=parseFloat(tmpBalance);
-        }
+	    if (matches=/Баланс<\/td>\s<td[\s\w='-\/%]*>([\d\.]+)</.exec(info)){
+	        var tmpBalance=matches[1].replace(/ |\xA0/, ""); // Удаляем пробелы
+	        tmpBalance=tmpBalance.replace(",", "."); // Заменяем запятую на точку
+	        result.balance=parseFloat(tmpBalance);
+	    }
     }
     // Лицевой счет
     if(AnyBalance.isAvailable('license')){
-        if (matches=/Основной лицевой счет<\/td>\s<td[\s\w='-\/%]*>([\d\.]+)</.exec(info)){
-            result.license=matches[1];
-        }
+	    if (matches=/Основной лицевой счет<\/td>\s<td[\s\w='-\/%]*>([\d\.]+)</.exec(info)){
+	    	result.license=matches[1];
+	    }
     }
 
-    // ФИО
+   // ФИО
     if(AnyBalance.isAvailable('username')){
-        if (matches=/ФИО<\/td>\s<td[\s\w='-\/%]*>([\S ]+)</i.exec(info)){
-            result.username=matches[1];
-        }
+	    if (matches=/ФИО<\/td>\s<td[\s\w='-\/%]*>([\S ]+)</i.exec(info)){
+	        result.username=matches[1];
+	    }
     }
     
  
-    AnyBalance.setResult(result);
+AnyBalance.setResult(result);
 }
 

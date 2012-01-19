5a59d8c28ce41edea96250160b669919eee018a7
dmitry.kochin <dmitry.kochin@gmail.com>
2012-01-19 21:15:36 +0000

    Банковская ячейка хаддана

---
+++ ab-game-haddan-bankcell/main.js
@@ -0,0 +1,28 @@
+function main(){
+	AnyBalance.trace('Connecting to haddan...');
+	
+	var result = {success: true};
+
+	var prefs = AnyBalance.getPreferences();
+	
+	var info = AnyBalance.requestPost('http://haddan.ru/inner/api_bankcell.php', {
+		cell: prefs.cellid,
+		apikey: prefs.apikey,
+		mode: 'json'
+	});
+
+	var res = new Function("return " + info)();
+	
+	if(typeof(res) != 'object'){
+		throw new AnyBalance.Exception('Wrong answer: ' + res);
+	}
+
+	if(res.error){
+		throw new AnyBalance.Exception(res.error);
+	}
+
+	result.balance = res.mn;
+	
+	AnyBalance.setResult(result);
+		
+}

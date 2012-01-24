5ff050fca9845dfd7acb5451e2bf8d381c10b487
ireset.ag@gmail.com <ireset.ag@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-01-24 06:03:56 +0000

    Добавлен провайдер QIWI Кошелек.

---
+++ trunk/ab-money-qiwi/common_functions.js
@@ -0,0 +1,17 @@
+﻿function getParam (html, result, param, regexp, replaces, parser) {
+	if (param != '__tariff' && !AnyBalance.isAvailable (param))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+		result[param] = value;
+	}
+}

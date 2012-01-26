5d2dc4d62ef3973a5ee2961006cc116399a32a4e
poluphanov.vitaliy@gmail.com <poluphanov.vitaliy@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-01-26 08:39:00 +0000

    Добавлен провайдер LinguaLeo

---
+++ trunk/ab-learning-lingualeo/main.js
@@ -0,0 +1,49 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+LinguaLeo - сервис изучения английского языка
+Сайт сервиса: http://lingualeo.ru/
+**/
+
+function main(){
+	var prefs = AnyBalance.getPreferences();
+	if (!prefs.email || prefs.email == '')
+		throw new AnyBalance.Error ('Введите e-mail');
+	if (!prefs.password || prefs.password == '')
+		throw new AnyBalance.Error ('Введите пароль');
+	AnyBalance.requestPost('http://lingualeo.ru/meatballs', {
+		email: prefs.email,
+		password: prefs.password
+	});
+	var html = AnyBalance.requestGet('http://lingualeo.ru/meatballs');
+	if (html) {
+		var result = {success: true};
+		if (AnyBalance.isAvailable('meatballs')) {
+			var matches = html.match(/<strong class="sub-meat">(\d+?)<i><\/i><\/strong>/i);
+			if (matches) {
+				result.meatballs = matches[1]
+			} else {
+				throw new AnyBalance.Error("Не удалось проверить фрикадельки");
+			}
+		}
+		if (AnyBalance.isAvailable('level')) {
+			var matches = html.match(/<span data-level-number>(\d+?)<\/span>/i);
+			if (matches) {
+				result.level = matches[1]
+			} else {
+				throw new AnyBalance.Error("Не удалось получить текущий уровень");
+			}
+		}
+		if (AnyBalance.isAvailable('xp')) {
+			var matches = html.match(/<div class="progress-bar" data-xp-progressbar style="width: (\d+?)%">/i);
+			if (matches) {
+				result.xp = matches[1]
+			} else {
+				throw new AnyBalance.Error("Не удалось получить текущий опыт");
+			}
+		}
+		AnyBalance.setResult(result);
+	} else {
+		throw new AnyBalance.Error("Не удалось получить данные");
+	}
+}

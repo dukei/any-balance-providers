932de99dea0fa987e4aae5b60f96b5ec4ad3e5d6
ireset.ag@gmail.com <ireset.ag@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-02-01 07:42:21 +0000

    Добавлен провайдер накопительной программы Клуба Л'Этуаль.

---
+++ trunk/ab-bonus-letoile/main.js
@@ -0,0 +1,47 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает информацию о бонусных баллах на клубных картах сети магазинов Л'Этуаль.
+
+Сайт магазина: http://www.letoile.ru/
+Проверка баланса: http://www.letoile.ru/club/cards/account/
+*/
+
+function main () {
+    var prefs = AnyBalance.getPreferences ();
+    var baseurl = 'http://www.letoile.ru/';
+
+    checkEmpty (prefs.number, 'Введите номер карты');
+    checkEmpty (prefs.color, 'Выберите цвет карты');
+
+    AnyBalance.trace ('Trying to enter selfcare at address: ' + baseurl);
+    var html = AnyBalance.requestGet (baseurl +
+        'ajax/check_account.php?card_type=' + 
+        prefs.color + 
+        '&card_number=' +
+        prefs.number);
+
+    // Проверка неправильной пары логин/пароль
+    var regexp=/<div class="g-error">\s*([\s\S]*?)\s*</;
+    var res = regexp.exec (html);
+    if (res)
+        throw new AnyBalance.Error (res[1]);
+
+    var result = {};
+
+    // Баланс
+    getParam (html, result, 'balance', /Баланс\s*карты:[^\d]*(\d+\.?\d*)/i, [], parseFloat);
+
+    if (!result.balance)
+        throw new AnyBalance.Error ('Неопределенная ошибка. Пожалуйста, обратитесь к автору скрипта');
+
+    result.success = true;
+
+
+    var html = AnyBalance.requestGet (baseurl + 'club/cards/account/');
+
+    // Дата актуализации баланса
+    getParam (html, result, 'dateOf', /Данные о балансе[^\d]*(\d{2}.\d{2}.\d{4} \d{2}:\d{2})/i, [/(\d{2}).(\d{2}).(.*)/, '$2/$1/$3'], Date.parse);
+
+    AnyBalance.setResult (result);
+}

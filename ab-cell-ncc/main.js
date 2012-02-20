fc978da49285cde400b13d765d284d474d756839
tujger <tujger@gmail.com>
2012-02-20 17:08:01 +0000

    Добавлена поддержка Нижегородского филиала. В настройках плагина необходимо выбрать регион.

--- ab-cell-ncc/main.js
+++ ab-cell-ncc/main.js
@@ -1,9 +1,9 @@
 /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-Провайдер NCC
+Провайдер NCC (Республика Татарстан, Нижегородский филиал)
 Сайт оператора: http://ncc-volga.ru/
-Личный кабинет (Казань): https://iserve.ncc-volga.ru/
+Личный кабинет: https://iserve.ncc-volga.ru/
 **/
 
 function main(){
@@ -24,44 +24,56 @@ function main(){
 		throw new AnyBalance.Error(error);
 	}
 
-	var matches;
 	if(matches = info.match(/<td align="center" valign="middle" class="red">(.*?)<\/td>/i)){
 		throw new AnyBalance.Error(matches[1]);
 	}
 
-    
 	var result = {success: true};
-//	var balance,contract;
-//    matches;
-
-
 
 	AnyBalance.trace('Parsing...');
-	if(AnyBalance.isAvailable('balance')){
-		if(matches = info.match(/<td class="icons">Баланс:<\/td>\n<td class="icons">(.*?) руб.<\/td>/i)){
-			result.balance = parseFloat(matches[1].replace(',','.'));
-
+	if(prefs.region == "1"){	// Татарстан
+		if(AnyBalance.isAvailable('balance')){
+			if(matches = info.match(/<td class="icons">Баланс:<\/td>\n<td class="icons">(.*?) руб.<\/td>/i)){
+				result.balance = parseFloat(matches[1].replace(',','.'));
+			}
+			else{
+				throw new AnyBalance.Error("Error");
+			}
 		}
-		else{
-			throw new AnyBalance.Error("Error");
+		if(AnyBalance.isAvailable('bonus')){
+			if(matches = info.match(/<td class="icons">Бонус:<\/td>\n<td class="icons">(.*?) руб.<\/td>/i)){
+				result.bonus = parseFloat(matches[1].replace(',','.'));
+			}
 		}
 	}
-	if(AnyBalance.isAvailable('bonus')){
-		if(matches = info.match(/<td class="icons">Бонус:<\/td>\n<td class="icons">(.*?) руб.<\/td>/i)){
-			result.bonus = parseFloat(matches[1].replace(',','.'));
+	else if(prefs.region == "2"){	// Нижегородский
+		if(AnyBalance.isAvailable('balance')){
+			if(matches = info.match(/<td>Баланс:<\/td>\n<td>(.*?) руб.<\/td>\n<\/tr>/i)){
+				result.balance = parseFloat(matches[1].replace(',','.'));
+			}
+			else{
+				throw new AnyBalance.Error("Error");
+			}
 		}
 	}
 
 	AnyBalance.trace("Fetching tariff...");
 	info = AnyBalance.requestGet(baseurl + "?path=tarif");
 
-	if(matches = info.match(/<td>Тариф: (.*?) <\/td>/i)){
-		result.__tariff = matches[1];
+	if(prefs.region == "1"){	// Татарстан
+		if(matches = info.match(/<td>Тариф: (.*?) <\/td>/i)){
+			result.__tariff = matches[1];
+		}
 	}
+	else if(prefs.region == "2"){	// Нижегородский
+		if(matches = info.match(/<td>Тариф: (.*?)<\/td>/i)){
+			result.__tariff = matches[1];
+		}
+	}
+
 	AnyBalance.setResult(result);
 
 
-        
 //	if(!AnyBalance.isSetResultCalled()){
 //		throw new AnyBalance.Error("error")
 //	}

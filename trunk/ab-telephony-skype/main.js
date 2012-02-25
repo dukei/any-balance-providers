a0a9a2772f97e503169f7d204ac4e85417a532b7
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-02-25 21:39:54 +0000

    Скайп

---
+++ trunk/ab-telephony-skype/main.js
@@ -0,0 +1,68 @@
+ /**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Skype IP-телефония
+Сайт оператора: http://www.skype.com/
+Личный кабинет: https://www.skype.com/
+
+Не отлаживается в AnyBalanceDebugger из-за бага в webkit: 
+http://code.google.com/p/chromium/issues/detail?id=96136
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && !AnyBalance.isAvailable (param))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+    else
+      return value
+        }
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');    
+    
+    var info = AnyBalance.requestPost("https://login.skype.com/account/login-form", {
+	username:prefs.login,
+	password:prefs.password
+    });
+
+    var error = getParam(info, null, null, /<div class="messageBody[^>]*>([\s\S]*?)<\/div>/i, [/<.*?>/g, '', /^\s*|\s*$/g, '']);
+    if(error)
+        throw new AnyBalance.Error(error);
+     
+    
+    var result = {
+        success: true
+    };
+
+    var matches;
+
+    getParam(info, result, 'balance', /id="balanceAmount"><span[^>]*>[^<]*<\/span>([^<]*)/i, [/[\s,]+/, ''], parseFloat);
+    getParam(info, result, 'currency', /<span class='ccy'>([^<]*)/i, null, html_entity_decode);
+		
+    AnyBalance.setResult(result);
+}
+

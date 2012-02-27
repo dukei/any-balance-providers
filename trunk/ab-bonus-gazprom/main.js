e22e3befaa3341c05a0eb24e1bf0ee4a3d4a9fd1
MironovJenia@gmail.com <MironovJenia@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-02-27 12:45:46 +0000

    1.0.2 - Добавил баланс в рублях по курсу программы.

--- trunk/ab-bonus-gazprom/main.js
+++ trunk/ab-bonus-gazprom/main.js
@@ -1,12 +1,20 @@
-function main(){
-	
-	var prefs = AnyBalance.getPreferences();
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
+Получает информацию о бонусах на карте Газпромнефть - Нам по пути.
+
+Сайт программы: http://www.gpnbonus.ru
+
+1.0.2 - Добавил баланс в рублях по курсу программы.
+*/
+
+
+function main(){
+	var prefs = AnyBalance.getPreferences();
 	var url='http://www.gpnbonus.ru/bonus_authorize/';
 	AnyBalance.trace('Sending a request for authorization');
         AnyBalance.setDefaultCharset('utf-8');
 	var html = AnyBalance.requestPost(url, prefs);
-
 	if (!/Персональные данные/.exec(html)){
 		AnyBalance.trace(res[1]);
 		throw new AnyBalance.Error ('Authorization error.');
@@ -52,6 +60,15 @@ function main(){
 		if (result.status == 'silver'){result.status='Серебрянный';}
 		if (result.status == 'gold'){result.status='Золотой';}
 		result.__tariff=result.status;
+	}
+
+//Баланс по курсу в рублях
+	if(AnyBalance.isAvailable('balance_money')){
+		html=AnyBalance.requestGet("http://www.gpnbonus.ru/on_the_way/");
+		regexp=/Наш курс: <b>(\d*) бонусов = (\d*) рубль<\/b>/;
+		if (res=regexp.exec(html)){
+			result.balance_money=(result.balance*res[2])/res[1];
+		}
 	}
 	AnyBalance.trace('End parsing...');
 	AnyBalance.setResult(result);

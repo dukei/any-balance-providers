64df1e712d2f40ef1ec35aee8ed9dbea05499cbd
poluphanov.vitaliy@gmail.com <poluphanov.vitaliy@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-03-26 14:18:25 +0000

    Исправлено в соответствии с новой разметкой сайта, добавлены новые типы игр

--- trunk/ab-game-chess-com/main.js
+++ trunk/ab-game-chess-com/main.js
@@ -2,33 +2,48 @@
 Провайдер Chess.com (http://chess.com)
 
 Chess.com — Шахматный интернет-клуб
-Сайт игры: http://chess.сщь/
+Сайт игры: http://chess.com/
 */
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
 	checkEmpty(prefs.nick, 'Отсутствует ник');
 
-	var html = AnyBalance.requestGet('http://www.chess.com/echess/mobile-stats/' + prefs.nick);
+	var html = AnyBalance.requestGet('http://www.chess.com/members/view/' + prefs.nick);
 	
 	if (html) {
-		var result = {success: true};
 		if (html.match(/Members - Chess\.com/i)) {
 			throw new AnyBalance.Error("Такой ник не найден");
 			}
-		if (prefs.type == 'o_standart') {
-			var matches = html.match(/<span style="font-weight: bold; font-size: 1\.6em;">(.+?)<\/span>/i);
-		} else if (prefs.type == 'chess960') {
-			var matches = html.match(/Chess960[\s\S]*?<span style="font-weight: bold; font-size: 1\.6em;">(.+?)<\/span>/i);
-		}
-		if (matches) {
-			if (matches[1] == 'unrated') {
-				throw new AnyBalance.Error('Unrated');
-			} else {
-				result.current = matches[1];
+			
+		var obj = {
+			live_blitz: "Live Chess - Blitz",
+			live_bullet: "Live Chess - Bullet",
+			live_standard: "Live Chess - Standard",
+			online: "Online Chess",
+			chess960: "Chess960",
+			tactics: "Tactics",
+			mentor: "Chess Mentor"
 			}
-		} else {
-			throw new AnyBalance.Error("Не удалось получить текущий рейтинг");
+			
+		var game_type = obj[prefs.type];
+		
+		var result = {success: true};
+		
+		if (AnyBalance.isAvailable('current')) {
+			var tmp_reg = new RegExp('</span>' + game_type + '</h4>[\\s\\S]\*\?<div class="right">(.*?)<span', 'i'); 
+			var matches = tmp_reg.exec(html)
+			if (matches) {
+				if (matches[1] == 'unrated') {
+					throw new AnyBalance.Error("Рейтинг " + game_type + " еще не установлен");
+				} else if (matches[1] == 'N/A') {
+					throw new AnyBalance.Error("Рейтинг " + game_type + " не доступен");
+				} else {
+					result.current = matches[1];
+				}
+			} else {
+				throw new AnyBalance.Error("У вас нет игр в " + game_type);
+			}
 		}
 		AnyBalance.setResult(result);
 	} else {

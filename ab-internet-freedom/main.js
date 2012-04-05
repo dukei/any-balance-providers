099fc5215b168fa30a06c6fd6dacb663dec8dea7
dmitry.kochin <dmitry.kochin@gmail.com>
2012-04-05 16:39:27 +0000

    Воронежский интернет провайдер Фридом

---
+++ ab-internet-freedom/main.js
@@ -0,0 +1,65 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Текущий баланс у воронежского оператора интернет FreeDom.
+
+Сайт оператора: http://freedom-vrn.ru/
+Личный кабинет: https://statserv.freedom-vrn.ru/
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+    else
+      return value
+	}
+}
+
+function getTrafficGb(str){
+  return parseFloat((parseFloat(str)/1024/1024/1024).toFixed(2));
+}
+
+var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    var baseurl = "https://statserv.freedom-vrn.ru/";
+
+    var html = AnyBalance.requestPost(baseurl + 'login.jsp', {
+	login: prefs.login,
+	pwd: prefs.password,
+	'exec': 'Отправить'
+    });
+
+    var error = getParam(html, null, null, /<font color="red">([\s\S]*?)<\/font>/i, replaceTagsAndSpaces);
+    if(error)
+	throw new AnyBalance.Error(error);
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /<span class="cbalance"[^>]*>([\s\S]*?)<\/span>([\s\S]*?)<\/td>/i, replaceFloat, parseFloat);
+    getParam(html, result, 'licschet', /Код лицевого счета: ([\w\d]+)/i, replaceTagsAndSpaces);
+    getParam(html, result, '__tariff', /Текущий расчетный период[\s\S]*?<tr class="fi[rs]{2}t">[\s\S]*?<td[^>]*>[\s\S]*?<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'status', /Текущий расчетный период[\s\S]*?<tr class="fi[rs]{2}t">[\s\S]*?(?:<td[^>]*>[\s\S]*?<\/td>\s*){4}<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+
+    getParam(html, result, 'discount', /Следующий расчетный период[\s\S]*?<tr class="fi[rs]{2}t">[\s\S]*?(?:<td[^>]*>[\s\S]*?<\/td>\s*){2}<td[^>]*>([\s\S]*?)<\/td>/i, replaceFloat, parseFloat);
+    getParam(html, result, 'abonentka', /Следующий расчетный период[\s\S]*?<tr class="fi[rs]{2}t">[\s\S]*?(?:<td[^>]*>[\s\S]*?<\/td>\s*){3}<td[^>]*>([\s\S]*?)<\/td>/i, replaceFloat, parseFloat);
+    
+    AnyBalance.setResult(result);
+}

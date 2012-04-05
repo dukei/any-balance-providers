5246eeeee4017592134adcc42fe066fae2b131ab
dmitry.kochin <dmitry.kochin@gmail.com>
2012-04-05 12:13:33 +0000

    Вернули киевстар на место

--- ab-cell-kyivstar/main.js
+++ ab-cell-kyivstar/main.js
@@ -2,21 +2,21 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
 Киевстар
-Сайт оператора: http://www.beeline.ua/
-Личный кабинет: https://poslugy.beeline.ua/
+Сайт оператора: http://www.kyivstar.ua/
+Личный кабинет: https://my.kyivstar.ua/
 */
 
 function main(){
   var prefs = AnyBalance.getPreferences();
-  var baseurl = "https://poslugy.beeline.ua/";
+  var baseurl = "https://my.kyivstar.ua/";
   AnyBalance.trace('Connecting to ' + baseurl);
 
-  var html = AnyBalance.requestGet(baseurl + 'tbmb/login_beeline/show.do');
+  var html = AnyBalance.requestGet(baseurl + 'tbmb/login/show.do');
   var token = /name="org.apache.struts.taglib.html.TOKEN" value="([\s\S]*?)">/.exec(html);
 
   AnyBalance.trace('Token = ' + token[1]);
 
-  var html = AnyBalance.requestPost(baseurl + "tbmb/login_beeline/perform.do", {
+  var html = AnyBalance.requestPost(baseurl + "tbmb/login/perform.do", {
     isSubmitted: "true",
     "org.apache.struts.taglib.html.TOKEN": token[1],
     user: prefs.login,
@@ -33,20 +33,96 @@ function main(){
   var result = {success: true};
   var str_tmp;
   
-  // Тариф
-  if (matches=/<nobr>Тарифн(?:и|ы)й план:<\/nobr>\s*<\/td>\s*<td.+>(.*?)\s*<\/td>\s*<\/tr>/.exec(html)){
-    str_tmp=/<nobr>Номер д(?:іє|ействует) до:<\/nobr>\s*<\/td>\s*<td>(.*?)<\/td>/.exec(html)
-   result.__tariff=matches[1]+' (до '+str_tmp[1]+')';
+  //Тарифный план
+  if (matches=/Тарифний план:[\s\S]*?<td\s+[^>]*>(.*?)\s*<\/td>/.exec(html)){
+    str_tmp=/Номер діє до:[\s\S]*?<td>(.*?)</.exec(html)
+    result.__tariff=matches[1]+' (до '+str_tmp[1]+')';
   }
   
   // Баланс
   if(AnyBalance.isAvailable('balance')){
-    if (matches=/<nobr>(?:Остаток на счету|Залишок на рахунку):<\/nobr>\s*<\/td>\s*<td>\s*<table.+\s*<colgroup>\s*<col.+\s*<col.+\s*<\/colgroup>\s*<.+\s*<td.+><b>(-?\d[\d\.,\s]*)<\/b>\s*грн.<\/td>/.exec(html)){
+    if (matches=/Залишок на рахунку:[\s\S]*?<b>(.*?)</.exec(html)){
         result.balance=parseFloat(matches[1]);
     }
   }
-
-
+  
+  //Бонусные минуты (1)
+  if(AnyBalance.isAvailable('bonus_mins_1')){
+    if (matches=/Кількість хвилин для дзвінків[\s\S]*?<b>(.*?)</.exec(html)){
+        result.bonus_mins_1=parseInt(matches[1]);
+    }
+  }
+  
+  //Бонусные минуты (2)
+  if(AnyBalance.isAvailable('bonus_mins_2')){
+    if (matches=/Кількість хвилин для дзвінків[\s\S]*?Кількість хвилин для дзвінків[\s\S]*?<b>(.*?)</.exec(html)){
+        result.bonus_mins_2=parseInt(matches[1]);
+    }
+  }
+  
+  //Бонусные MMS
+  if(AnyBalance.isAvailable('bonus_mms')){
+    if (matches=/Бонусні MMS:[\s\S]*?<b>(.*?)</.exec(html)){
+        result.bonus_mms=parseInt(matches[1]);
+    }
+  }
+  
+  //Бонусные SMS
+  if(AnyBalance.isAvailable('bonus_sms')){
+    if (matches=/Бонусні SMS:[\s\S]*?<b>(.*?)</.exec(html)){
+        result.bonus_sms=parseInt(matches[1]);
+    }
+  }
+  
+  //SMS
+  if(AnyBalance.isAvailable('sms')){
+    if (matches=/Бонусні SMS:[\s\S]*?SMS:[\s\S]*?<b>(.*?)</.exec(html)){
+        result.sms=parseInt(matches[1]);
+    }
+  }
+  
+  //Бонусные средства (1)
+  if(AnyBalance.isAvailable('bonus_money_1')){
+    if (matches=/Бонусні кошти:[\s\S]*?<b>(.*?)</.exec(html)){
+        result.bonus_money_1=parseFloat(matches[1]);
+    }
+  }
+  
+  //Бонусные средства (2)
+  if(AnyBalance.isAvailable('bonus_money_2')){
+    if (matches=/Бонуси за умовами тарифного плану "Єдина ціна":[\s\S]*?<b>(.*?)</.exec(html)){
+        result.bonus_money_2=parseFloat(matches[1]);
+    }
+  }
+  
+  //Остаток бонусов
+  if(AnyBalance.isAvailable('bonus_left')){
+    if (matches=/Залишок бонусів:[\s\S]*?<b>(.*?)</.exec(html)){
+        result.bonus_left=parseFloat(matches[1]);
+    }
+  }
+  
+  //Интернет (1)
+  if(AnyBalance.isAvailable('internet_1')){
+    if (matches=/Залишок бонусного об\'єму даних:[\s\S]*?<b>(.*?)</.exec(html)){
+        result.internet_1=Math.round(parseInt(matches[1])/1024/1024*100)/100;
+    }
+  }
+  
+  //Интернет (2)
+  if(AnyBalance.isAvailable('internet_2')){
+    if (matches=/Залишок бонусного об\'єму даних:[\s\S]*?Залишок бонусного об\'єму даних:[\s\S]*?<b>(.*?)</.exec(html)){
+        result.internet_2=Math.round(parseInt(matches[1])/1024/1024*100)/100;
+    }
+  }
+  
+  //Домашний Интернет
+  if(AnyBalance.isAvailable('home_internet')){
+    if (matches=/Від послуги "Домашній Інтернет":[\s\S]*?<b>(.*?)</.exec(html)){
+        result.home_internet=parseFloat(matches[1]);
+    }
+  }
+  
   AnyBalance.setResult(result);
 }
 

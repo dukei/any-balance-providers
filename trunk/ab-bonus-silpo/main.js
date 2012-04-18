cb9d7e46081eeee3d7b527316bbf1d11b68e9ada
igoloviznin@gmail.com <igoloviznin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-04-18 06:01:26 +0000

    Добавление дополнительного баланса. Версия нерабочая. Не работает на телефоне, но работает с Хрома.

--- trunk/ab-bonus-silpo/main.js
+++ trunk/ab-bonus-silpo/main.js
@@ -9,6 +9,7 @@
 function main(){
 	var baseurl = 'https://my.silpo.ua/';
 	var prefs = AnyBalance.getPreferences();
+	AnyBalance.setDefaultCharset('utf-8');
 	var authPwd = prefs.pass;
 	var authBarcode = prefs.login;
 	if (!prefs.login || prefs.login == '')
@@ -25,18 +26,29 @@ function main(){
 		//Вообще, если не получается залогиниться просто передав логин и пароль, то надо смотреть, не устанавливаются ли при запросе какие-нибудь
 		//Дополнительные заголовки или куки
 		{"X-Requested-With":"XMLHttpRequest"}
+		,
+		{"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.142 Safari/535.19"}
 	);
 	if (html == "1"){ //Если авторизация успешна, то возвращается просто один
 		//Переходим на аккаунт
 		html = AnyBalance.requestGet(baseurl + 'account');
 		var result = {success: true};
 		if (AnyBalance.isAvailable('bonus')) {
-			//AnyBalance.trace(html); //С помощью этого на телефоне в окне "показать последний лог" можно увидеть, какой тут html
+			AnyBalance.trace(html); //С помощью этого на телефоне в окне "показать последний лог" можно увидеть, какой тут html
 			var matches = html.match(/<div class="bottom">\s*Загальна кількість\s*<div>(\d+?)<\/div>/i);
 			if (matches) {
 				result.bonus = parseFloat(matches[1]);
 			} else {
 				throw new AnyBalance.Error("Не удалось проверить бонусы");
+			}
+		}
+		if (AnyBalance.isAvailable('baly')) {
+			//AnyBalance.trace(html); //С помощью этого на телефоне в окне "показать последний лог" можно увидеть, какой тут html
+			var matches = html.match(/Мої Спеціальні пропозиції більше ніж +<span>(\d+?)<\/span>/i);
+			if (matches) {
+				result.baly = parseFloat(matches[1]);
+			} else {
+				throw new AnyBalance.Error("Не удалось проверить балы");
 			}
 		}
 		AnyBalance.setResult(result);

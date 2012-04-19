79cc2c473aea75c88d4d0d58b7033cdeded79136
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-04-19 17:21:20 +0000

    Белорусский Velcom

---
+++ trunk/ab-cell-velcom/main.js
@@ -0,0 +1,146 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Текущий баланс миль в программе Aeroflot Bonus.
+
+Сайт оператора: http://aeroflotbonus.ru/
+Личный кабинет: https://www.aeroflot.ru/personal/
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+    else
+      return value
+	}
+}
+
+var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    var baseurl = "https://internet.velcom.by/";
+    AnyBalance.setDefaultCharset('windows-1251');
+
+    var matches;
+    if(!(matches = /^\+375(\d\d)(\d{7})$/.exec(prefs.login)))
+	throw new AnyBalance.Error('Неверный номер телефона. Необходимо ввести номер в международном формате без пробелов и разделителей!');
+
+    var phone = matches[2];
+    var prefix = matches[1];
+
+    var html = AnyBalance.requestGet(baseurl + 'work.html');
+    var sid = getParam(html, null, null, /name="sid3" value="([^"]*)"/i);
+    if(!sid)
+	throw new AnyBalance.Error('Не удалось найти идентификатор сессии!');
+    
+    var html = AnyBalance.requestPost(baseurl + 'work.html',
+'------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="sid3"\r\n\
+\r\n\
+' + sid + '\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="user_input_timestamp"\r\n\
+\r\n\
+' + new Date().getTime() + '\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="user_input_0"\r\n\
+\r\n\
+_next\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="last_id"\r\n\
+\r\n\
+\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="user_input_1"\r\n\
+\r\n\
+' + prefix + '\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="user_input_2"\r\n\
+\r\n\
+' + phone + '\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="user_input_10"\r\n\
+\r\n\
+0\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75\r\n\
+Content-Disposition: form-data; name="user_input_3"\r\n\
+\r\n\
+' + prefs.password + '\r\n\
+------WebKitFormBoundaryM326yJJq1lZxkt75--\r\n\
+', {
+	'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryM326yJJq1lZxkt75',
+	'Host': 'internet.velcom.by',
+	'Origin': 'https://internet.velcom.by',
+	'Referer': 'https://internet.velcom.by/work.html',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.162 Safari/535.19'
+});
+
+    var error = getParam(html, null, null, /<td[^>]+class="INFO_Error"[^>]*>(.*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    if(error)
+        throw new AnyBalance.Error(error);
+
+    var result = {success: true};
+
+    var html = AnyBalance.requestPost(baseurl + 'work.html',
+'------WebKitFormBoundaryrceZMlz5Js39A2A6\r\n\
+Content-Disposition: form-data; name="sid3"\r\n\
+\r\n\
+' + sid + '\r\n\
+------WebKitFormBoundaryrceZMlz5Js39A2A6\r\n\
+Content-Disposition: form-data; name="user_input_timestamp"\r\n\
+\r\n\
+' + new Date().getTime() + '\r\n\
+------WebKitFormBoundaryrceZMlz5Js39A2A6\r\n\
+Content-Disposition: form-data; name="user_input_0"\r\n\
+\r\n\
+_root/USER_INFO\r\n\
+------WebKitFormBoundaryrceZMlz5Js39A2A6\r\n\
+Content-Disposition: form-data; name="last_id"\r\n\
+\r\n\
+\r\n\
+------WebKitFormBoundaryrceZMlz5Js39A2A6\r\n\
+', {
+	'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryrceZMlz5Js39A2A6',
+	'Host': 'internet.velcom.by',
+	'Origin': 'https://internet.velcom.by',
+	'Referer': 'https://internet.velcom.by/',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.162 Safari/535.19'
+});
+
+    getParam(html, result, 'userName', /ФИО:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'userNum', /номер клиента:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Текущий баланс:[\s\S]*?<td[^>]*>(-?\d[\s\d,\.]*)/i, replaceFloat, parseFloat);
+    getParam(html, result, 'status', /Текущий статус абонента:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'min', /Остаток минут, SMS, MMS, GPRS, включенных в абонплату:[\s\S]*?<td[^>]*>[\s\S]*?(-?\d[\s\d,\.]*) мин(?:,|$)/i, replaceFloat, parseFloat);
+    getParam(html, result, 'min_fn', /Остаток минут, SMS, MMS, GPRS, включенных в абонплату:[\s\S]*?<td[^>]*>[\s\S]*?(-?\d[\s\d,\.]*) мин на ЛН/i, replaceFloat, parseFloat);
+    getParam(html, result, 'traffic', /Остаток минут, SMS, MMS, GPRS, включенных в абонплату:[\s\S]*?<td[^>]*>[\s\S]*?(-?\d[\s\d,\.]*) Мб/i, replaceFloat, parseFloat);
+
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

8d22f03dd3a88eb906e33fb0f08663d70f64bd9d
dmitry.kochin <dmitry.kochin@gmail.com>
2012-05-08 19:09:34 +0000

    Мультима (бывший Астрон), Красноярск

---
+++ ab-internet-multima-krs/main.js
@@ -0,0 +1,26 @@
+﻿function main(){
+	var prefs = AnyBalance.getPreferences();
+	var login = prefs.login;
+	var info = AnyBalance.requestPost('http://lk.multima.ru/ext/get_balance_by_client_ext_ident', {
+		ext_ident:login,
+		submit:'Проверить'
+	});
+
+
+	if(info.search('Лицевой счет не найден') != -1){
+		throw new AnyBalance.Error('Лицевой счет не найден');
+	};
+
+
+	var result = {success: true};
+	var regexp = new RegExp('Баланс[^\\-\\d]*(\\-?\\d*\\.?\\d*)');
+        var matches;
+
+	if(matches = info.match(regexp)){
+		if(AnyBalance.isAvailable('balance'))
+			result.balance = parseFloat(matches[1]);
+	};
+
+
+	AnyBalance.setResult(result);
+}

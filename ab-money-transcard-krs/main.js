b300909de2b93b13e38ab12b2da022083e044bb0
LeoHawk67 <LeoHawk67@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-05-10 11:06:05 +0000

    Relise

---
+++ ab-money-transcard-krs/main.js
@@ -0,0 +1,27 @@
+﻿function main(){
+	var prefs = AnyBalance.getPreferences();
+	var CardNumber = prefs.CardNumber;
+	var info = AnyBalance.requestGet('http://www.krasinform.ru/?id=tcard&card_num='+CardNumber);
+
+	if(info.search('Неверный номер транспортной карты') != -1){
+		throw new AnyBalance.Error('Неверный номер транспортной карты');
+	};
+
+	var result = {success: true};
+	var regexp1 = new RegExp('Количество транспортных единиц: (\\d*)');
+	var regexp2 = new RegExp('Приемщик.*?<\\/td><td>([^<]*)<\\/td><td>.*?<\\/td><td>([^<]*)');
+        var matches1, matches2;
+
+	if(matches1 = info.match(regexp1)){
+		if(AnyBalance.isAvailable('KolvoEdinits'))
+			result.KolvoEdinits = parseFloat(matches1[1]);
+		if(matches2 = info.match(regexp2)){
+			if(AnyBalance.isAvailable('LastDate'))
+				result.LastDate = matches2[1].substring(0,10);
+			if(AnyBalance.isAvailable('LastSum'))
+				result.LastSum = parseFloat(matches2[2]);
+		};
+	};
+
+	AnyBalance.setResult(result);
+}

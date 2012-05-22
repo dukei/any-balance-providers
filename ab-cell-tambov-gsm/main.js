8ed796ec3cefe4df933c20236bd64071048956f6
dmitry.kochin <dmitry.kochin@gmail.com>
2012-05-22 05:56:03 +0000

    Тамбов ЖСМ, начальная версия

---
+++ ab-cell-tambov-gsm/main.js
@@ -0,0 +1,24 @@
+function main() {
+var prefs = AnyBalance.getPreferences();
+var html = AnyBalance.requestGet('http://www.tambovgsm.com/PrivateOffice/PrivateOffice_main.php?Login='+prefs.login+'&Password='+prefs.password+'&Auth=Auth');
+
+var r = new RegExp('<TD class="RB">\\s+([0-9.]+)\\s');
+var matches=r.exec(html);
+
+var result = {success: true};
+
+if(matches==null) {
+throw new AnyBalance.Error('Невозможно получить данные. Проверьте логин и пароль.');
+} else {
+result.balance=parseFloat(matches[1]);
+}
+
+r = new RegExp('<TD class="TP">([\\s\\S]+?)</TD>');
+matches=r.exec(html);
+if(matches!=null) {
+matches[1]=matches[1].replace(/^\s+|\s+$/g,'');
+result.__tariff=matches[1];
+}
+
+AnyBalance.setResult(result);
+}

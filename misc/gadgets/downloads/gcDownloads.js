12ad02349cbccefe61a34cecb0fa12a8f9537263
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-06-03 11:40:11 +0000

    Чтобы хром не ругался на безопасность больше

--- misc/gadgets/downloads/gcDownloads.js
+++ misc/gadgets/downloads/gcDownloads.js
@@ -16,7 +16,7 @@
  */
 
 var ROW_TMPL = "<tr><td valign='top' style='white-space: nowrap;'>%DATE%</td><td valign='center'><img width='20' src='%IMAGE%'/></td><td valign='top'><a href='%LINK%' target='_blank'>%FILENAME%</a></td></tr>";
-var MORE_TMPL = "http://code.google.com/p/%PROJECT%/downloads/list";
+var MORE_TMPL = "https://code.google.com/p/%PROJECT%/downloads/list";
 var MONTHS = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 var ITEMS_PER_PAGE = 10;
 
@@ -53,7 +53,7 @@ function handleFeed(response, prefs) {
     var title = entry.Summary.replace(/<pre>\s*([^\n]*)\n[\s\S]*/, '$1');
     var html = ROW_TMPL.replace(/%DATE%/g, formatted_when)
                        .replace(/%LINK%/g, entry.Link)
-		       .replace(/%IMAGE%/g, 'http://any-balance-providers.googlecode.com/svn-history/r%REVISION%/trunk/%PROVNAME%/icon.png')
+		       .replace(/%IMAGE%/g, 'https://any-balance-providers.googlecode.com/svn-history/r%REVISION%/trunk/%PROVNAME%/icon.png')
                        .replace(/%REVISION%/g, provrev)
                        .replace(/%PROVNAME%/g, provname)
                        .replace(/%FILENAME%/g, title);
@@ -90,7 +90,7 @@ function localInit() {
   var lastCount = prefs.getInt("lastCount");
   // There seems to be some disagreement between OpenSocial containers about
   // whether prefs.getString() returns an escaped string or not.  Let's be paranoid.
-  var url = "http://code.google.com/feeds/p/" + escape(projectName) + "/downloads/basic";
+  var url = "https://code.google.com/feeds/p/" + escape(projectName) + "/downloads/basic";
 
   var params = {};
   params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.FEED;

9ac6557e3655d07859df9e757acb8c2ea4e682f3
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-07-20 10:41:44 +0000

    Киевстар - изменения в личном кабинете

--- trunk/ab-cell-kyivstar-corp-ua/main.js
+++ trunk/ab-cell-kyivstar-corp-ua/main.js
@@ -10,20 +10,30 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     
     var baseurl = "https://my.kyivstar.ua/";
+    var headers = {
+      'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+      'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Intel Mac OS X 10.6; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+      Connection: 'keep-alive'
+    };
 
     AnyBalance.trace("Trying to enter selfcare at address: " + baseurl);
     var html = AnyBalance.requestPost(baseurl + "tbmb/login/perform.do", {
         isSubmitted: "true",
         user: prefs.login,
         password: prefs.password
-    });
+    }, headers);
     
     var matches = html.match(/<td class="redError">([\s\S]*?)<\/td>/i);
     if(matches){
         throw new AnyBalance.Error(matches[1]);
     }
+
+//    AnyBalance.trace(html);
     
     var hierarchy = AnyBalance.requestGet(baseurl + "tbmb/flash/hierarchy?action=hier");
+//    AnyBalance.trace(hierarchy);
+
     //<hh><h id='4199266' type='BILLING' name='Billing Hierarchy'><n i='4199267' t='1' v='Billing Hierarchy'><n i='8974631' t='1' virt='0' v='2802451'><n i='17496882' t='3' virt='0' v='4654037' c='' m='+380673401254'/></n></n></h></hh>
     var $hierarchy = $(hierarchy);
     var hierid = $hierarchy.find("h").attr('id');
@@ -34,6 +44,7 @@ function main(){
     
     if(AnyBalance.isAvailable('costs', 'balance')){
         var balance_html = AnyBalance.requestGet(baseurl + "tbmb/flash/hierarchy?action=charges&nodeId=" + nodeid_balance + "&hierId=" + hierid);
+//        AnyBalance.trace(balance_html);
         //<cc i='8974631'><c i='17496882' c='87.65'/></cc>
 	if(AnyBalance.isAvailable('costs')){
             var costs = parseFloat($(balance_html).find('c').attr('c'));
@@ -49,6 +60,7 @@ function main(){
 
     //https://my.kyivstar.ua/tbmb/flash/hierarchy?action=mtninfo&hierId=4199266&nodeId=17496882&time=261
     var info_html = AnyBalance.requestGet(baseurl + "tbmb/flash/hierarchy?action=mtninfo&nodeId=" + nodeid_info + "&hierId=" + hierid);
+//    AnyBalance.trace(info_html);
     var $info = $(info_html);
     
     result.__tariff = $info.find('rp').text();

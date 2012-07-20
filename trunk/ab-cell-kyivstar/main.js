dafd9d66ef172f81159991d23ca7e2a9ec90f9a8
sergey.bashmanov@gmail.com <sergey.bashmanov@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-07-20 13:51:02 +0000

    - Исправлена ошибка с входом в аккаунт.(добавлены headers) - Добавлено описание истории изменений провайдера. (файл history.xml)

--- trunk/ab-cell-kyivstar/main.js
+++ trunk/ab-cell-kyivstar/main.js
@@ -9,9 +9,16 @@
 function main(){
   var prefs = AnyBalance.getPreferences();
   var baseurl = "https://my.kyivstar.ua/";
+  var headers = {
+    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Intel Mac OS X 10.6; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+    Connection: 'keep-alive'
+  };
+
   AnyBalance.trace('Connecting to ' + baseurl);
 
-  var html = AnyBalance.requestGet(baseurl + 'tbmb/login/show.do');
+  var html = AnyBalance.requestGet(baseurl + 'tbmb/login/show.do', headers);
   var token = /name="org.apache.struts.taglib.html.TOKEN" value="([\s\S]*?)">/.exec(html);
 
   AnyBalance.trace('Token = ' + token[1]);
@@ -21,7 +28,7 @@ function main(){
     "org.apache.struts.taglib.html.TOKEN": token[1],
     user: prefs.login,
     password: prefs.password
-  });
+  }, headers);
   
   var matches = html.match(/<td class="redError">([\s\S]*?)<\/td>/i);
   if(matches){

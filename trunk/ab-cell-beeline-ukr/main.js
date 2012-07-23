0d31f84a1b5406de2a26e23daca01250b52b10e5
igoloviznin@gmail.com <igoloviznin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-07-23 13:19:12 +0000

    Исправлена ошибка с входом в аккаунт.(добавлены headers) Добавлено описание истории изменений провайдера. (файл history.xml) Добавлены новые счетчики: "Бонусный баланс", "Бонусные минуты на Beeline и Голден Телеком", "Доплата за входящие звонки".

--- trunk/ab-cell-beeline-ukr/main.js
+++ trunk/ab-cell-beeline-ukr/main.js
@@ -9,9 +9,16 @@
 function main(){
   var prefs = AnyBalance.getPreferences();
   var baseurl = "https://poslugy.beeline.ua/";
+  var headers = {
+    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':'uk-UA,uk;q=0.8,en-US;q=0.6,en;q=0.4',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11',
+    Connection: 'keep-alive'
+  };
+
   AnyBalance.trace('Connecting to ' + baseurl);
 
-  var html = AnyBalance.requestGet(baseurl + 'tbmb/login_beeline/show.do');
+  var html = AnyBalance.requestGet(baseurl + 'tbmb/login_beeline/show.do', headers);
   var token = /name="org.apache.struts.taglib.html.TOKEN" value="([\s\S]*?)">/.exec(html);
 
   AnyBalance.trace('Token = ' + token[1]);
@@ -21,7 +28,7 @@ function main(){
     "org.apache.struts.taglib.html.TOKEN": token[1],
     user: prefs.login,
     password: prefs.password
-  });
+  }, headers);
   
   var matches = html.match(/<td class="redError">\s*([\s\S]*?)\s*<\/td>/i);
   if(matches){
@@ -44,6 +51,27 @@ function main(){
     if (matches=/<nobr>(?:Остаток на счету|Залишок на рахунку):<\/nobr>\s*<\/td>\s*<td>\s*<table.+\s*<colgroup>\s*<col.+\s*<col.+\s*<\/colgroup>\s*<.+\s*<td.+><b>(-?\d[\d\.,\s]*)<\/b>\s*грн.<\/td>/.exec(html)){
         result.balance=parseFloat(matches[1]);
     }
+  }
+
+  // Бонусный баланс
+  if(AnyBalance.isAvailable('bonusbalance')){
+    if (matches=/<td.+>Бонусн(?:ые средства|і кошти):<\/td>\s*<td.+>\s*<nobr><b>(-?\d[\d\.,\s]*)<\/b> грн.\s*<\/nobr>/.exec(html)){
+        result.bonusbalance=parseFloat(matches[1]);
+    }
+  }
+
+  // Бонусные минуты на Beeline и Голден Телеком
+  if(AnyBalance.isAvailable('minutebalance')){
+    if (matches=/<td.+>Бонусн(?:ые минуты|і хвилини) на Beeline (?:и|та) Голден Телеком:<\/td>\s*<td.+>\s*<nobr><b>(\d+?)<\/b> (?:мин|хв). <b>.+<\/b> сек.\s*<\/nobr>/.exec(html)){
+        result.minutebalance=parseFloat(matches[1]);
+    }
+  }
+
+  // Доплата за входящие звонки
+  if(AnyBalance.isAvailable('doplatabalance')){
+    if (matches=/<td.+>Доплата за вх(?:одящие зво|ідні дзві)нки:<\/td>\s*<td.+>\s*<nobr><b>(-?\d[\d\.,\s]*)<\/b> грн.\s*<\/nobr>/.exec(html)){
+        result.doplatabalance=parseFloat(matches[1]);
+    }    
   }
 
 

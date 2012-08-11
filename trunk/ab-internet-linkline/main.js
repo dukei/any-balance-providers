64ed4552ddc6c65ae2b764aa6b32eefd696f6005
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-08-11 17:25:34 +0000

    LinkLine (москва)

---
+++ trunk/ab-internet-linkline/main.js
@@ -0,0 +1,90 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Текущий баланс у московского интернет провайдера одинцовского интернет-провайдера LinkLine
+
+Сайт оператора: http://linkline.ru/
+Личный кабинет: https://stat.linkline.ru
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+    else
+      return value
+	}
+}
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    if(val)
+        val = Math.round(val*100)/100;
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseTrafficGb(str){
+  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+  return parseFloat((val/1024).toFixed(2));
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    AnyBalance.setDefaultCharset('windows-1251');
+
+    var baseurl = "https://stat.linkline.ru";
+
+    var html = AnyBalance.requestPost(baseurl + '/cgi-bin/utm/aaa', {
+        login:prefs.login,
+        password:prefs.password,
+        cmd: 'user_verify'
+    });
+
+    var href = getParam(html, null, null, /['"](\/cgi-bin\/utm\/utm_stat\?cmd=user_report&sid=[^"']*)/i);
+    if(!href){
+        var error = getParam(html, null, null, /<H1[^>]*>([\s\S]*?)<\/H1>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error("Не удалось найти ссылку на пользовательские данные! Изменения на сайте?");
+    }
+
+    html = AnyBalance.requestGet(baseurl + href);
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', />Баланс[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, '__tariff', />Тарифный план[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'status', />Состояние счета[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'trafficLocal', />Локальный[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+    getParam(html, result, 'trafficIn', />Входящий[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+    getParam(html, result, 'trafficOut', />Исходящий[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

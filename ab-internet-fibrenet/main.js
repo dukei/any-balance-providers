bf781c6ccbb6ec807c39f1317aa459a5131afbf4
dmitry.kochin <dmitry.kochin@gmail.com>
2012-08-31 18:15:45 +0000

    fibrenet - Томский интернет провайдер

---
+++ ab-internet-fibrenet/main.js
@@ -0,0 +1,103 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для томского интернет-провайдера FibreNET
+
+Сайт оператора: http://fibrenet.ru
+Личный кабинет: https://billing.fibrenet.ru
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var matches = regexp.exec (html), value;
+	if (matches) {
+		value = matches[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+	}
+   return value
+}
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseTrafficGb(str){
+  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+  return parseFloat((val/1024).toFixed(2));
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "https://billing.fibrenet.ru/";
+
+    var html = AnyBalance.requestPost(baseurl, {
+        login:prefs.login,
+        password:prefs.password,
+        cmd: 'login'
+    });
+
+    //AnyBalance.trace(html);
+    if(!/\?module=zz_logout/.test(html)){
+        var error = getParam(html, null, null, /<p[^>]*style=["']color:\s*red[^>]*>([\s\S]*?)<\/p>/, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /Баланс[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'status', /Состояние интернета[\S\s]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /Основной лицевой счет[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'agreement', />ID<[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    html = AnyBalance.requestGet(baseurl + "?module=40_tariffs");
+
+    getParam(html, result, '__tariff', /<td[^>]*class=['"]utm-cell[^>]*>([\S\s]*?)<\/t[dr]>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    if(AnyBalance.isAvailable('trafficIn', 'trafficOut')){
+        var now = new Date();
+        var m = now.getMonth() + 1;
+        var year = now.getFullYear();
+        var date1 = Date.UTC(year, m-1, 1) / 1000 + now.getTimezoneOffset()*60;
+        var date2 = Date.UTC(year, m, 1) / 1000 + now.getTimezoneOffset()*60;
+        var html = AnyBalance.requestPost(baseurl + '?module=30_traffic_report', {
+             date1:date1,
+             date2:date2,
+             month:0,
+             year:0
+       });
+
+       getParam(html, result, 'trafficIn', /входящий[\s\S]*?<td[^>]*>(-?\d[\d\.,\s]*)/i, replaceFloat, parseTrafficGb);
+       getParam(html, result, 'trafficOut', /исходящий[\s\S]*?<td[^>]*>(-?\d[\d\.,\s]*)/i, replaceFloat, parseTrafficGb);
+    }
+    
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

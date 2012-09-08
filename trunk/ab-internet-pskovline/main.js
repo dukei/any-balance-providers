b330031c75ae15f8e120ecbc53a9f1d76a9691b5
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-09-08 17:39:38 +0000

    ПсковЛайн, исправление

--- trunk/ab-internet-pskovline/main.js
+++ trunk/ab-internet-pskovline/main.js
@@ -1,3 +1,12 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Текущий баланс у оператора интернет и телевидения PskovLine.
+
+Сайт оператора: http://www.pskovline.ru
+Личный кабинет: http://www.pskovline.ru/utm5/
+*/
+
 function main() {
   AnyBalance.setDefaultCharset("utf-8");
   var prefs = AnyBalance.getPreferences();
@@ -13,13 +22,13 @@ function main() {
 
                         var login, ballance, number, credit, work;
 
-                        AnyBalance.setResult({success:true, login:matches[1], ballance:matches[5], number:matches[3], credit:matches[7], work:matches[15]});
+                        AnyBalance.setResult({success:true, login:matches[1], ballance:parseFloat(matches[5]), number:matches[3], credit:matches[7], work:matches[15]});
                 }
 	
         }
         
         if(!AnyBalance.isSetResultCalled())
-                throw new AnyBalance.Error("Error");
+                throw new AnyBalance.Error("Ошибка. Проверьте логин и пароль. Если вы можете с ними войти в личный кабинет, а провайдер не работает, обратитесь к автору провайдера.");
 
 //  AnyBalance.setResult({success: true, login: login, ballance: ballance, number: number, credit: credit, work: work});
 }

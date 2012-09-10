e49f09c30632bbc64343feea72ee60db3ef8b00d
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-09-10 17:18:07 +0000

    Челиндбанк

---
+++ trunk/ab-money-chelindbank/main.js
@@ -0,0 +1,140 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает текущий остаток и другие параметры карт и счетов Челиндбанка
+
+Сайт оператора: http://www.chelindbank.ru
+Личный кабинет: https://www.chelindbank.ru/ib2
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+    else
+      return value
+	}
+}
+
+var replaceTagsAndSpaces = [/\\n/g, ' ', /\[br\]/ig, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, ''];
+
+function parseBalance(text){
+    var _text = text.replace(/\s+/g, '');
+    var val = getParam(_text, null, null, /(-?\d[\d\.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseCurrency(text){
+    var _text = text.replace(/\s+/g, '');
+    var val = getParam(_text, null, null, /-?\d[\d\.,]*\s*(\S*)/);
+    AnyBalance.trace('Parsing currency (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseDate(str){
+    var matches = /(\d+)[^\d](\d+)[^\d](\d+)/.exec(str);
+    if(matches){
+          var date = new Date(+matches[3], matches[2]-1, +matches[1]);
+	  var time = date.getTime();
+          AnyBalance.trace('Parsing date ' + date + ' from value: ' + str);
+          return time;
+    }
+    AnyBalance.trace('Failed to parse date from value: ' + str);
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    var baseurl = "https://www.chelindbank.ru/ib2/";
+    
+    var headers = {
+        'Accept-Language': 'ru, en',
+        Referer: baseurl + 'Logon.aspx',
+        Origin: baseurl + 'Logon.aspx',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
+    }
+
+    var html = AnyBalance.requestPost(baseurl + 'Logon.aspx', {
+        login:prefs.login,
+        password:prefs.password,
+        submit:'Submit'
+    }, headers);
+
+    if(!/AccessOver\.aspx/i.test(html)){
+        var error = getParam(html, null, null, /<div[^>]*id="login_error[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось зайти в интернет-банк. Сайт изменен?');
+    }
+
+    if(prefs.type == 'card')
+        fetchCard(html, headers, baseurl);
+    else if(prefs.type == 'acc')
+        fetchAccount(html, headers, baseurl);
+    else
+        fetchCard(html, headers, baseurl); //По умолчанию карты будем получать
+}
+
+function fetchCard(html, headers, baseurl){
+    var prefs = AnyBalance.getPreferences();
+    if(prefs.cardnum && !/^\d{4}$/.test(prefs.cardnum))
+        throw new AnyBalance.Error("Введите 4 последних цифры номера карты или не вводите ничего, чтобы показать информацию по первой карте");
+
+    var re = new RegExp('(<tr[^>]*class="hand"[^>]*>(?:[\\s\\S](?!<\\/tr>))*\\d{4}XXXXXXXX' + (prefs.cardnum ? prefs.cardnum : '\\d{4}') + '[\\s\\S]*?<\\/tr>)', 'i');
+    var tr = getParam(html, null, null, re);
+
+    if(!tr)
+        throw new AnyBalance.Error('Не удаётся найти ' + (prefs.cardnum ? 'карту с последними цифрами ' + prefs.cardnum : 'ни одной карты'));
+
+    var result = {success: true};
+    getParam(tr, result, 'cardnum', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'balance', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(tr, result, 'accnum', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'currency', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    AnyBalance.setResult(result);
+}
+
+function fetchAccount(html, headers, baseurl){
+    var prefs = AnyBalance.getPreferences();
+    if(prefs.cardnum && !/^\d{20}$/.test(prefs.cardnum))
+        throw new AnyBalance.Error("Введите полный номер счета (20 цифр) или не вводите ничего, чтобы показать информацию по первому счету");
+                                                                                                                                
+    var re = new RegExp('(<tr[^>]*class="hand"[^>]*>(?:[\\s\\S](?!<\\/tr>))*' + (prefs.cardnum ? prefs.cardnum : '\\d{20}') + '[\\s\\S]*?<\\/tr>)', 'i');
+    var tr = getParam(html, null, null, re);
+
+    if(!tr)
+        throw new AnyBalance.Error('Не удаётся найти ' + (prefs.cardnum ? 'счет с первыми цифрами ' + prefs.cardnum : 'ни одного счета'));
+
+    var result = {success: true};
+    getParam(tr, result, 'cardnum', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'balance', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(tr, result, 'accnum', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'currency', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

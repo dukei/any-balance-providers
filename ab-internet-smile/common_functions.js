bb6f5d28aa2fd6977237a378bfdb10682d3b9e7d
dmitry.kochin <dmitry.kochin@gmail.com>
2012-09-19 12:11:43 +0000

    Смайл - изменения в личном кабинете

--- ab-internet-smile/common_functions.js
+++ ab-internet-smile/common_functions.js
@@ -1,10 +1,10 @@
 ﻿function getParam (html, result, param, regexp, replaces, parser) {
-	if (param != '__tariff' && !AnyBalance.isAvailable (param))
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
 		return;
 
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
+	var matches = regexp.exec (html), value;
+	if (matches) {
+		value = matches[1];
 		if (replaces) {
 			for (var i = 0; i < replaces.length; i += 2) {
 				value = value.replace (replaces[i], replaces[i+1]);
@@ -12,6 +12,38 @@
 		}
 		if (parser)
 			value = parser (value);
-		result[param] = value;
+
+    if(param)
+      result[param] = value;
 	}
+   return value
 }
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseDate(str){
+    var matches = /(\d+)[^\d](\d+)[^\d](\d+)(?:[^\d](\d+):(\d+)(?:[^\d](\d+))?)?/.exec(str);
+    var time;
+    if(matches){
+	  time = (new Date(+matches[3], matches[2]-1, +matches[1], matches[4] || 0, matches[5] || 0, matches[6] || 0)).getTime();
+          AnyBalance.trace('Parsing date ' + new Date(time) + ' from value: ' + str);
+          return time;
+    }
+    AnyBalance.trace('Could not parse date from value: ' + str);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

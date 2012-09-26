28de01cfa871ddbf967bd3054e6fa34b36605ac1
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-09-26 07:58:19 +0000

    Твоё ТВ электросталь

--- trunk/ab-internet-tvoetv_elstal/main.js
+++ trunk/ab-internet-tvoetv_elstal/main.js
@@ -1,10 +1,10 @@
 /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-Статус почтового отправления с сайта gdeposylka.ru
+Получает баланс, кредит, номер лицевого счета для Электростальского провайдера интернет, телевидения и телефонии Твоё ТВ.
 
-Сайт оператора: http://gdeposylka.ru
-Личный кабинет: http://gdeposylka.ru
+Сайт оператора: http://eltv.ru
+Личный кабинет: https://stat.eltv.ru/
 */
 
 function getParam (html, result, param, regexp, replaces, parser) {
@@ -29,39 +29,19 @@ function getParam (html, result, param, regexp, replaces, parser) {
 	}
 }
 
-function numSize(num, size){
-  var str = num + '';
-  if(str.length < size){
-    for(var i=str.length; i<size; ++i){
-	str = '0' + str;
-    }
-  }
-  return str;
-}
+var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
 
-function getDateString(dt){
-	if(typeof dt != 'object') dt = new Date(dt);
-	return numSize(dt.getDate(), 2) + '/' + numSize(dt.getMonth()+1, 2) + '/' + dt.getFullYear() + " " + dt.getHours() + ':' + dt.getMinutes();
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
 }
-
-function parseDate(str){
-	AnyBalance.trace('Parsing date: ' + str);
-  var matches = /(\d+)[^\d](\d+)[^\d](\d+)\s+(?:(\d+):(\d+))?/.exec(str);
-  var time = 0;
-  if(matches){
-	time = (new Date(+matches[3], matches[2]-1, +matches[1], +matches[4], +matches[5])).getTime();
-  }
-  return time;
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''  , /Баланс /g,'' , /Основной лицевой счет /g,'', /Кредит /g, ''];
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
-	var v_login = prefs.login;
-	var v_pass = prefs.password; 
 
-	baseurl = "https://stat.eltv.ru/"; 
+	var baseurl = "https://stat.eltv.ru/"; 
 	AnyBalance.trace('connect '+baseurl);
 
 	var html = AnyBalance.requestPost(baseurl , {
@@ -70,31 +50,25 @@ function main(){
         	submit: 'Продолжить'
 		});
 
+	//Проверим, что есть ссылка на выход, это значит, что мы зашли в кабинет
+        if(!/\/logout/i.test(html)){ 
+            //Если ссылки нет, то надо выдать ошибку.
+            //Сначала пытаемся найти текстовое описание
+            var error = getParam(html, null, null, /<p[^>]+style=['"][^'"]*color:red[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces);
+    	    if(error)
+	        throw new AnyBalance.Error(error);
+            throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+        }
+
 	AnyBalance.trace('trying to find data');
 
-	var v_balance = getParam(html, null, null, /Баланс<\/td>\n<td class='utm-cell' align='left' width='50%'>([\s\S]*?)<\/td>/i,replaceTagsAndSpaces);
-	AnyBalance.trace('v_balance='+v_balance);
-
-	var v_ls = getParam(html, null, null, /Основной лицевой счет<\/td>\n<td class='utm-cell' align='left' width='50%'>([\s\S]*?)<\/td>/i,replaceTagsAndSpaces);
-	AnyBalance.trace('v_ls='+v_ls);
-
-	var v_credit = getParam(html, null, null, /Кредит<\/td>\n<td class='utm-cell' align='left' width='50%'>([\s\S]*?)<\/td>/i,replaceTagsAndSpaces);
-	AnyBalance.trace('v_credit='+v_credit);
-
-
-	var error = getParam(html, null, null, /<div class="errorBox"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
-	if(error)
-		throw new AnyBalance.Error(error);
-	error = getParam(html, null, null, /<span class="error"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
-	if(error)
-		throw new AnyBalance.Error(error);
-	
 	var result = {success: true};
 
-	if(AnyBalance.isAvailable('balance'))
-		result.balance = v_balance;
-		result.ls = v_ls;
-		result.credit = v_credit;
+	getParam(html, result, 'balance', /Баланс<\/td>\n<td[^>]*>([\s\S]*?)<\/td>/i,replaceTagsAndSpaces, parseBalance);
+
+	getParam(html, result, 'ls', /Основной лицевой счет<\/td>\n<td[^>]*>([\s\S]*?)<\/td>/i,replaceTagsAndSpaces);
+
+	getParam(html, result, 'credit', /Кредит<\/td>\n<td[^>]*>([\s\S]*?)<\/td>/i,replaceTagsAndSpaces, parseBalance);
 
 	AnyBalance.setResult(result);
 }

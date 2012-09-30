13d652a87fafe8fe68f497f341a88ce5dc0a4097
dmitry.kochin <dmitry.kochin@gmail.com>
2012-09-30 14:30:15 +0000

    QIWI - поддержка нового лк

--- ab-money-qiwi/common_functions.js
+++ ab-money-qiwi/common_functions.js
@@ -1,10 +1,11 @@
 ﻿function getParam (html, result, param, regexp, replaces, parser) {
-	if (param != '__tariff' && !AnyBalance.isAvailable (param))
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
 		return;
 
-	var value = regexp.exec (html);
+	var value = regexp ? regexp.exec (html) : html;
 	if (value) {
-		value = value[1];
+                if(regexp)
+		    value = value[1];
 		if (replaces) {
 			for (var i = 0; i < replaces.length; i += 2) {
 				value = value.replace (replaces[i], replaces[i+1]);
@@ -12,6 +13,21 @@
 		}
 		if (parser)
 			value = parser (value);
-		result[param] = value;
+
+    if(param)
+      result[param] = value;
+    else
+      return value
 	}
 }
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.', /(\d)\-(\d)/g, '$1.$2'];
+
+function parseBalance(text){
+    var _text = text.replace(/\s+/g, '');
+    var val = getParam(_text, null, null, /(-?\d[\d\.,\-]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+

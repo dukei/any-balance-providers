24caabec6f7cd8214adf3fbe367e610221fae360
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-09-30 14:30:15 +0000

    QIWI - поддержка нового лк

--- trunk/ab-money-qiwi/main.js
+++ trunk/ab-money-qiwi/main.js
@@ -11,7 +11,97 @@ function jsonp (obj) {
   return obj;
 }
 
-function main () {
+function main(){
+    var prefs = AnyBalance.getPreferences ();
+    switch(prefs.type){
+    case 'new':
+        mainNew();
+        break;
+    case 'old':
+        mainOld();
+        break;
+    case 'auto':
+    default:
+        try{
+            mainOld();
+        }catch(e){
+            AnyBalance.trace('Ошибка подключения к старому кабинету: ' + e.message + '\nПробуем новый...');
+            mainNew();
+        }
+        break;
+    }
+}
+
+function mainNew () {
+    var prefs = AnyBalance.getPreferences ();
+    var baseurl = 'https://w.qiwi.com/';
+
+    if (!prefs.login)
+        throw new AnyBalance.Error ('Введите номер телефона');
+
+    if (!prefs.password)
+        throw new AnyBalance.Error ('Введите пароль');
+
+    AnyBalance.trace ('Trying to enter NEW account at address: ' + baseurl);
+    var info = AnyBalance.requestGet (baseurl +
+                                      'auth/login.action?source=MENU&login=%2B7' +
+                                      encodeURIComponent(prefs.login) +
+                                      '&password=' +
+                                      encodeURIComponent(prefs.password));
+    AnyBalance.trace ('Login result: ' + info);
+
+    var res = JSON.parse(info);
+
+    // Проверка ошибки входа
+    if (res.code.value != '0'){
+	var errors = {
+		0 : "ОК",
+		1 : "Ошибка",
+		2 : "Ошибка",
+		3 : "Требуется авторизация",
+		4 : "Требуется подтверждение",
+		5 : "Требуется подтверждение",
+		6 : "Срок действия пароля истек",
+		success : "Успех",
+		timeout : "Время соединения истекло. Проверьте подключение к сети",
+		error : "Ошибка соединения. Проверьте подключение к сети",
+		abort : "Запрос отменен",
+		parsererror : "Синтаксическая ошибка"
+	};
+
+        throw new AnyBalance.Error (res.message || (res.messages ? res.messages.join('\n') : 'Не удаётся войти в QIWI кошелек: ' + errors[res.code.value]));
+    }
+
+    var html = AnyBalance.requestGet (baseurl + 'payment/main.action');
+
+    // Проверка на корректный вход
+    if (/\/auth\/logout\.action/i.exec(html)){
+    	AnyBalance.trace ('It looks like we are in selfcare...');
+    }else if(/passwordchangesuccess.action/i.test(html)){
+        throw new AnyBalance.Error('Срок действия пароля истек. Смените пароль, зайдя в свой QIWI-кошелек через браузер.');
+    }else {
+        AnyBalance.trace ('Have not found logout... Unknown error. Please contact author.');
+        throw new AnyBalance.Error ('Неизвестная ошибка. Пожалуйста, свяжитесь с автором провайдера.');
+    }
+
+    var result = {success: true};
+
+    // Баланс
+    getParam (html, result, 'balance', /id="person-accounts-RUB"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+
+    // Сообщения (че-то непонятно, где они в новом личном кабинете)
+    //getParam (html, result, 'messages', /Сообщения:.*?>(\d+)/i, [], parseInt);
+
+    if(AnyBalance.isAvailable('bills')){
+        html = AnyBalance.requestGet(baseurl + 'user/order/main.action?type=1');
+        var count = html.match(/<div[^>]*ordersLine\s+NOT_PAID[^>]*>/ig);
+        result.bills = count ? count.length : 0;
+    }
+
+    AnyBalance.setResult (result);
+}
+
+function mainOld () {
     var prefs = AnyBalance.getPreferences ();
     var baseurl = 'https://w.qiwi.ru/';
 
@@ -21,7 +111,7 @@ function main () {
     if (!prefs.password || prefs.password == '')
         throw new AnyBalance.Error ('Введите пароль');
 
-    AnyBalance.trace ('Trying to enter selfcare at address: ' + baseurl);
+    AnyBalance.trace ('Trying to enter OLD account at address: ' + baseurl);
     var info = AnyBalance.requestGet (baseurl +
                                       'login.action?source=0&phone=7' +
                                       encodeURIComponent(prefs.login) +

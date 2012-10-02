32af48639c4fddf0ee3b322207ffcc9b86f88d21
bazhanov.artem@gmail.com <bazhanov.artem@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-10-02 22:16:00 +0000

    Забыл убрать лишнее

--- trunk/ab-money-google-checkout/main.js
+++ trunk/ab-money-google-checkout/main.js
@@ -87,7 +87,7 @@ function main() {
 
     var html = AnyBalance.requestPost(baseurlLogin + 'ServiceLoginAuth', params, g_headers);
 
-    AnyBalance.trace(html);
+    //AnyBalance.trace(html);
 
 	var r = new RegExp('<b>Входящие&nbsp;\\((\\d+)\\)</b>');
 	var matches=r.exec(html);
@@ -95,47 +95,4 @@ function main() {
 	result.sales=parseInt(matches[1]);
 
 	AnyBalance.setResult(result);
-
-/*
-    var prefs = AnyBalance.getPreferences();
-
-	var html = AnyBalance.requestPost('http://wap.passport.wapstart.ru/index.php?area=login', {
-		login: prefs.login,
-		password: prefs.password,
-		signIn: 'Войти'
-	  });
-
-    var result = {
-        success: true
-    };
-
-    var r = new RegExp('>Изменить пароль</a>');
-	if(r.test(html)) {
-		html = AnyBalance.requestGet('http://wap.plus1.wapstart.ru/?area=mainAccount');
-
-		r = new RegExp('<p>Баланс:\\s+([0-9 ,.]+)\\s+руб.</p>');
-		var matches=r.exec(html);
-		if(matches==null) throw new AnyBalance.Error('Ошибка получения баланса');
-		matches[1]=matches[1].replace(",",".");
-		matches[1]=matches[1].replace(" ","");
-		result.balance=parseFloat(matches[1]);
-
-		r = new RegExp('<table class="statistic">\\s+<tr>[\\s\\S]+?</tr>\\s+<tr>\\s+<td>\\s+([0-9,.]+)\\s+</td>\\s+<td>\\s+([0-9,.]+)\\s+</td>\\s+<td>\\s+([0-9,.]+)\\s+</td>\\s+<td>\\s+([0-9,.]+)\\s+</td>\\s+</tr>\\s+</table>');
-
-		matches=r.exec(html);
-		if(matches==null) throw new AnyBalance.Error('Ошибка заполнения счетчиков');
-		matches[1]=matches[1].replace(",",".");
-		result.yesterday=parseFloat(matches[1]);
-		matches[2]=matches[2].replace(",",".");
-		result.today=parseFloat(matches[2]);
-		matches[3]=matches[3].replace(",",".");
-		result.week=parseFloat(matches[3]);
-		matches[4]=matches[4].replace(",",".");
-		result.month=parseFloat(matches[4]);
-	} else {
-		throw new AnyBalance.Error('Невозможно получить данные. Проверьте логин и пароль.');
-	}
-
-    AnyBalance.setResult(result);
-*/
 }

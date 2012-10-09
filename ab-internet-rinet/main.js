1aad208fcdc4e5a5e0985205be71f6685aee69a0
dmitry.kochin <dmitry.kochin@gmail.com>
2012-10-09 10:49:17 +0000

    Ринет (Москва)

---
+++ ab-internet-rinet/main.js
@@ -0,0 +1,88 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для московского интернет-провайдера Rinet
+
+Сайт оператора: http://rinet.net
+Личный кабинет: http://rinet.net/personal/
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var matches = regexp.exec (html), value;
+	if (matches) {
+		value = matches[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+	}
+   return value
+}
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseTrafficGb(str){
+  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+  return parseFloat((val/1024).toFixed(2));
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "https://ctl.rinet.ru/cgi-bin/uctl.cgi";
+
+    var html = AnyBalance.requestPost(baseurl, {
+        mode:1,
+        cmd:0,
+        user:prefs.login,
+        passwd:prefs.password,
+        x:28,
+        y:14
+    });
+
+    //AnyBalance.trace(html);
+    if(!/\/cgi-bin\/uctl.cgi\?mode=255/.test(html)){
+        var error = getParam(html, null, null, /<div[^>]+class="warning"[^>]*>([\s\S]*?)<\/div>/, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, '__tariff', /Тарифный план:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'balance', /Баланс:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'abon', /Абонентская плата:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'status', /Состояние счета:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(html, result, 'trafficIn', /Трафик за[\s\S]*?Итого(?:[\S\s]*?<td[^>]*>){2}([\S\s]*?)<\/td>/i, replaceFloat, parseTrafficGb);
+    getParam(html, result, 'trafficOut', /Трафик за[\s\S]*?Итого(?:[\S\s]*?<td[^>]*>){1}([\S\s]*?)<\/td>/i, replaceFloat, parseTrafficGb);
+    
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

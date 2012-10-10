3b554da4b953137c7722dd6a132f9c90b0bb09e8
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-10-10 14:52:35 +0000

    Бест Хостер

---
+++ trunk/ab-hosting-besthoster/main.js
@@ -0,0 +1,110 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает остаток дней и информацию о тарифном плане для хостинг провайдера BestHoster
+
+Сайт оператора: http://besthoster.ru/
+Личный кабинет: https://order.best-hoster.ru
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+        var value;
+        if(regexp){
+            var matches = regexp.exec (html);
+            if(matches)
+                value = (matches.length <= 1 ? matches[0] : matches[1]);
+        }else{
+            value = html;
+        }
+
+	if (typeof(value) != 'undefined') {
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+		if(param)
+			result[param] = value;
+		return value
+	}
+}
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseDate(str){
+    var matches = /(\d+)[^\d](\d+)[^\d](\d+)/.exec(str);
+    var time;
+    if(matches){
+	  time = (new Date(+matches[3], matches[2]-1, +matches[1])).getTime();
+          AnyBalance.trace('Parsing date ' + new Date(time) + ' from value: ' + str);
+          return time;
+    }
+    AnyBalance.trace('Could not parse date from value: ' + str);
+}
+
+var g_headers = {
+    Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Cache-Control':'max-age=0',
+    Connection:'keep-alive',
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('windows-1251');
+
+    var baseurl = "https://order.best-hoster.ru/";
+    g_headers.Referer = baseurl;
+
+    var html = AnyBalance.requestPost(baseurl + '?mod=main', {
+        enterlogin:'customer',
+        useraccount:prefs.login,
+        userpassword:prefs.password
+    }, g_headers);
+
+    //AnyBalance.trace(html);
+    if(!/\?mod=quit/i.test(html)){
+        var error = getParam(html, null, null, /<font[^>]*color=["']?#FF0000[^>]*>([\s\S]*?)<\/font>/, replaceTagsAndSpaces);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'daysleft', /Осталось Дней:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'till', /Срок Действия:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+    getParam(html, result, 'accnum', /Номер Аккаунта:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'domains', /Количество Доменов:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'addaccs', /Дополнительные Аккаунты[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'partn', /Заработок в Партнерской Программе[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+
+    html = AnyBalance.requestGet(baseurl + '?mod=account', g_headers);
+    getParam(html, result, '__tariff', /Тарифный План:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

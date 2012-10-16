346962c0b959a3aead37d750bc616d5d8122b717
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-10-16 19:27:10 +0000

    telincom, воля, смашиной

---
+++ trunk/ab-internet-telincom/main.js
@@ -0,0 +1,92 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для московского интернет-провайдера telincom
+
+Сайт оператора: http://www.telincom.ru
+Личный кабинет: http://stat.telincom.ru/site/
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var matches = regexp.exec (html), value;
+	if (matches) {
+		value = matches[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+	}
+   return value
+}
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseTrafficGb(str){
+  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+  return parseFloat((val/1024).toFixed(2));
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "http://stat.telincom.ru/site/stat.php";
+
+    var html = AnyBalance.requestPost(baseurl, {
+        cmd: 'login',
+        login:prefs.login,
+        password:prefs.password
+    });
+
+    //AnyBalance.trace(html);
+    if(!/stat.php\?cmd=logout/.test(html)){
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неверный логин-пароль?');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /Баланс:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'credit', /Кредит:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'status', /Статус Интернета:[\S\s]*?<td[^>]*>([\S\s]*?)(?:<a[^>]*>|<\/td>)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /Л\/С:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(html, result, '__tariff', />Название<[\s\S]*?<td[^>]*class=['"]utm-table[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    if(AnyBalance.isAvailable('trafficIn', 'trafficOut')){
+        var sid = getParam(html, null, null, /SID=([a-f0-9]{32})/i);
+        if(!sid){
+            AnyBalance.trace('Не удаётся получить идентификатор сессии, трафик пропускаем...');
+        }else{
+            html = AnyBalance.requestGet(baseurl + '?cmd=u_traff&SID='+sid);
+            getParam(html, result, 'trafficIn', /ИТОГО(?:[\s\S]*?<td[^>]*>)([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+            getParam(html, result, 'trafficOut', /ИТОГО(?:[\s\S]*?<td[^>]*>){2}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+        }
+    }
+    
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+
--- trunk/ab-internet-volia-broadband/main.js
+++ trunk/ab-internet-volia-broadband/main.js
@@ -6,6 +6,21 @@
 Личный кабинет: https://stat.volia.com:8443/ktvinet/main.do
 */
 
+function findGoodService(services){
+    for(var i=0; services && i<services.length; ++i){
+        if(!services[i].finishDate)
+            return services[i];
+        var matches = /\w+\s+(\w+)\s+(\d+).*(\d{4})/i.exec(services[i].finishDate);
+        if(matches){
+             var dt = Date.parse(matches[1] + ' ' + matches[2] + ', ' + matches[3]);
+             AnyBalance.trace("Parsed date " + new Date(dt) + " from " + services[i].finishDate);
+             if(dt > new Date().getTime())
+                 return services[i];
+        }
+    }
+    return null;
+}
+
 function main(){
 	var prefs = AnyBalance.getPreferences();
 	var pass = prefs.pass;
@@ -13,14 +28,19 @@ function main(){
 	var info = AnyBalance.requestGet('https://stat.volia.com:8443/ktvinet/vapi/jstat.jsp?code=' + login + '&pass=' + pass);
 	if (info){
 		var result = {success: true};
-		var v = $.parseJSON(info);
+		var v = JSON.parse(info);
 		var resst = v.responseStatus.responseValue;
 		if (resst == 'ok'){
 			result.__tariff = v.abonent.fullname;
-			if(AnyBalance.isAvailable('balance')){
+                        if(AnyBalance.isAvailable('fio'))
+                            result.fio = v.abonent.fullname;
+                        var service = findGoodService(v.services);
+                        if(service && service.model)
+                            result.__tariff = (service.name ? service.name + ': ' + service.model : service.model);
+			if(AnyBalance.isAvailable('balance') && v.services[0]){
 				result['balance'] = v.services[0].saldo;
 			}
-			if(AnyBalance.isAvailable('traffic')){
+			if(AnyBalance.isAvailable('traffic') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].underTraffic){
 				result['traffic'] = (v.traffic[0].rows[2].underTraffic / 1000000000).toFixed(1);
 			}
 			AnyBalance.setResult(result);

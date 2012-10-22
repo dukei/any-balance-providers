68f9ed0d466ac494d3c7bf7fd6357c75ebabe7c1
dmitry.kochin <dmitry.kochin@gmail.com>
2012-10-22 16:03:13 +0000

    maxnet ua

---
+++ ab-internet-maxnet/main.js
@@ -0,0 +1,92 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для харьковского интернет-провайдера MaxNet
+
+Сайт оператора: http://MaxNet.ru
+Личный кабинет: https://stat.MaxNet.ru
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var matches = regexp.exec (html), value;
+	if (matches) {
+		value = matches[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+	}
+   return value
+}
+
+var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseTrafficGb(str){
+  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+  return parseFloat((val/1024).toFixed(2));
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "https://stat.maxnet.ua/cgi-bin/stat_user.cgi";
+
+    var html = AnyBalance.requestPost(baseurl, {
+        login:prefs.login,
+        password:prefs.password,
+        action: 'passwd'
+    });
+
+    //AnyBalance.trace(html);
+    if(!/document.stat_form1.action.value=''/.test(html)){
+        var error = getParam(html, null, null, /<div[^>]*style=["'][^'"]*color:\s*black[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /Счет пользователя:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'credit', /Кредит:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'licschet', /Номер лицевого счета:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    //Найдем строчку с IP - это строка для тарифного плана
+    var tr = getParam(html, null, null, /(<tr[^>]*>(?:[\s\S](?!<\/tr>))*>\s*\d+\.\d+\.\d+\.\d+\s*<[\s\S]*?<\/tr>)/i);
+    if(!tr)
+        AnyBalance.trace('Не удалось найти строку с текщим тарифным планом');
+    var pack = getParam(tr || '', null, null, /(?:[\S\s]*?<td[^>]*>){3}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    var tarif = getParam(tr || '', null, null, /(?:[\S\s]*?<td[^>]*>){6}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    if(pack)
+        result.__tariff = pack;
+    if(tarif)
+        result.__tariff = (result.__tariff || '') + ' (' + tarif + ')';
+
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

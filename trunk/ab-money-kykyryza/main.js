0c7dd10ea7241f80194fd6989c2f4184ca726e3e
robix@ulstore.ru <robix@ulstore.ru@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-10-22 15:12:53 +0000

    abab-money-kykyryza Fix auth error for new interface

--- trunk/ab-money-kykyryza/main.js
+++ trunk/ab-money-kykyryza/main.js
@@ -10,28 +10,51 @@ function main(){
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'https://oplata.kykyryza.ru/personal/';
 	
-	var html = AnyBalance.requestGet(baseurl);
+	var html = AnyBalance.requestGet(baseurl + 'pub/Entrance');
     var matches = html.match(/<h1>(ТЕХНИЧЕСКИЕ РАБОТЫ)<\/h1>/i);
     if(matches){
     	throw new AnyBalance.Error(matches[1].replace(/^\s*|\s*$/g, ''));
     }
-	
+//Первый шаг авторизации. Проверка номера карты	
     var html = AnyBalance.requestPost(baseurl + "?wicket:interface=:0:loginForm::IBehaviorListener:0:1", {
-    	id1_hf_0: '',
+    	id7_hf_0: '',
+    	ean: prefs.login,
+    	password: ''
+    });
+    AnyBalance.trace(html);
+    obj = $.parseJSON(html);
+    
+    if (!obj || !obj.validated || obj.cardActive == false){
+    	
+    	if (obj.form && obj.form.errorMessage)
+    		throw new AnyBalance.Error(obj.form.errorMessage);
+    	else if (obj.fields && obj.fields[0] && obj.fields[0].errorMessage)
+    		throw new AnyBalance.Error(obj.fields[0].errorMessage);
+    	else
+    		throw new AnyBalance.Error("Ошибка авторизации. Проверьте правильность ввода номера карты");
+    }
+    
+//Второй шаг авторизации. Проверка пароля	
+    
+    var html = AnyBalance.requestPost(baseurl + "?wicket:interface=:0:loginForm::IBehaviorListener:0:1", {
+    	id7_hf_0: '',
     	ean: prefs.login,
     	password: prefs.password
     });
+    
+    AnyBalance.trace(html);
     obj = $.parseJSON(html);
-    if (!obj || !obj.form ){
-    	throw new AnyBalance.Error("Ошибка авторизации");
-    }
-    if (!obj.form.redirectUrl){
-    	if (!obj.form.errorMessage)
-    		throw new AnyBalance.Error("Неизвестная ошибка авторизации.");
-    	else
-    		throw new AnyBalance.Error(obj.form.errorMessage);
-    }
-    var html = AnyBalance.requestGet(obj.form.redirectUrl);
+    if (obj.form && obj.form.redirectUrl)
+    	url = obj.form.redirectUrl;
+    else
+		throw new AnyBalance.Error("Ошибка авторизации. Проверьте правильность ввода пароля");
+    
+//Получение балансов    
+    AnyBalance.trace(url);	
+    
+    var html = AnyBalance.requestGet(url);
+    
+    //AnyBalance.trace(html);
     var $html = $(html);
 
     var result = {success: true};

82eaa0b82681b6f59608222a3a215b7566d458fa
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-11-02 12:39:48 +0000

    Красное и белое (урал)

---
+++ trunk/ab-bonus-krasnoeibeloe/main.js
@@ -0,0 +1,39 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает накопленную сумму и текущую скидку для карты Красное и белое
+
+Сайт оператора: http://krasnoeibeloe.ru
+*/
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "http://www.krasnoeibeloe.ru/";
+
+    var html = AnyBalance.requestPost(baseurl + '/json/', {
+        _do:'get_card_info',
+        card_id:prefs.login
+    });
+
+    var json;
+    try{
+        json = JSON.parse(html);
+    }catch(e){
+        AnyBalance.trace('Can not parse json: ' + html);
+        throw new AnyBalance.Error('Сервер вернул неверный ответ. Проблемы на сайте или сайт изменен.');
+    }
+
+    if(json.error){
+        throw new AnyBalance.Error('Неверный номер карты.');
+    }
+
+    var result = {success: true};
+
+    result.balance = parseFloat(json.summ);
+    result.discount = parseFloat(json.discount);
+    result.__tariff = json.card_id;
+    
+    AnyBalance.setResult(result);
+}

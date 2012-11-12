67609b2118294843bcbcdd3ceb46ca3d8d7190c1
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-11-12 09:25:42 +0000

    Билайн - исправление счетчика s7 - бонусная программа

---
+++ trunk/ab-bonus-s7/main.js
@@ -0,0 +1,59 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает информацию по бонусной карте s7
+
+Сайт оператора: http://www.s7.ru
+Личный кабинет: https://www.s7.ru/home/priority/ffpAbout.dot
+*/
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "https://www.s7.ru/";
+
+    var html = AnyBalance.requestPost(baseurl + 'cas/login', {
+        renew:true,
+        auto:true,
+        service:baseurl + 'home/priority/ffpAbout.dot',
+        errorPage:baseurl + 'home/priority/ffpLoginError.dot',
+        hiddenText:'',
+        username:prefs.login,
+        password:prefs.password
+    });
+
+    var params = createFormParams(html, function(params, str, name, value){
+        if(/type="(submit|reset)"/i.test(str))
+            return;
+        if(name == 'username')
+            return prefs.login;
+        if(name == 'password')
+            return prefs.password;
+        return value;
+    });
+
+    html = AnyBalance.requestPost(baseurl + 'cas/login', params);
+
+    //AnyBalance.trace(html);
+    if(!/priority\/logout/.test(html)){
+        var error = getParam(html, null, null, /<div[^>]*class=["']error[^>]*>([\s\S]*?)<\/div>/, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /(?:Мили|Miles):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'cardnum', /(?:Номер|Number):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'status', /(?:Статус|Status):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'userName', /<div[^>]+class="ffp_username"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+
+    var cn = getParam(html, null, null, /(?:Номер|Number):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    var status = getParam(html, null, null, /(?:Статус|Status):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+
+    result.__tariff = status + ', №' + cn;
+
+    AnyBalance.setResult(result);
+}

68d81a6f454251bf7920a255faf29bb5ab1a76e8
dmitry.kochin <dmitry.kochin@gmail.com>
2012-11-22 11:51:11 +0000

    ION - отмена сжатия, ибо падает

--- ab-bonus-ion/main.js
+++ ab-bonus-ion/main.js
@@ -39,6 +39,16 @@ function parseBalance(text){
 var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
 var replaceFloat = [/\s+/g, '', /,/g, '.'];
 
+var g_headers = {
+    Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Cache-Control':'max-age=0',
+    'Accept-Encoding':null, //Че-то какой-то битый стрим она получает, ошибка EOFException вываливается. Отменяем сжатие
+    Connection:'keep-alive',
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
+};
+
 function main(){
     var baseurl = "http://i-on.ru/crm/logon";
     var prefs = AnyBalance.getPreferences();
@@ -47,7 +57,7 @@ function main(){
         number:prefs.login,
         password:prefs.password,
         remember:false
-    });
+    }, g_headers);
 
     if(!/\/crm\/logoff/i.test(html)){
         var error = getParam(html, null, null, /<div[^>]+class="validation-summary-errors"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);

f294d28e9fad59a38890cd984347c63980afc67f
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-11-25 08:36:00 +0000

    Триколор ТВ

---
+++ trunk/ab-tv-tricolor/library.js
@@ -0,0 +1,67 @@
+﻿/**
+AnyBalance (http://any-balance-providers.googlecode.com)
+
+Содержит некоторые полезные для извлечения значений с сайтов функции.
+Для конкретного провайдера рекомендуется оставлять в этом файле только те функции, которые используются.
+*/
+
+/**
+ * Получает значение, подходящее под регулярное выражение regexp, производит 
+ * в нем замены replaces, результат передаёт в функцию parser, 
+ * а затем записывает результат в счетчик с именем param в result
+ * Результат в result помещается только если счетчик выбран пользователем 
+ * в настройках аккаунта
+ * 
+ * если result и param равны null, то значение просто возвращается.
+ * eсли parser == null, то возвращается результат сразу после замен
+ * если replaces == null, то замены не делаются
+ * 
+ * replaces - массив, нечетные индексы - регулярные выражения, четные - строки, 
+ * на которые надо заменить куски, подходящие под предыдущее регулярное выражение
+ * см. например replaceTagsAndSpaces
+ */
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var matches = regexp.exec (html), value;
+	if (matches) {
+		value = matches[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+	}
+   return value
+}
+
+//Замена пробелов и тэгов
+var replaceTagsAndSpaces = [/&nbsp;/ig, ' ', /<!--[\s\S]*?-->/g, '', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+//Замена для чисел
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+/**
+ * Извлекает числовое значение из переданного текста
+ */
+function parseBalance(text){
+    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+/**
+ * Заменяет HTML сущности в строке на соответствующие им символы
+ */
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
---
+++ trunk/ab-tv-tricolor/main.js
@@ -0,0 +1,89 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для провайдера спутникового телевидения Tricolor
+
+Сайт оператора: http://tricolor.tv/
+Личный кабинет: https://lk.tricolor.tv/trcustomer/Login.aspx
+*/
+
+var g_headers = {
+  'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+  'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Intel Mac OS X 10.6; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+  Connection: 'keep-alive'
+};
+
+function getViewState(html){
+    return getParam(html, null, null, /name="__VIEWSTATE".*?value="([^"]*)"/);
+}
+
+function getEventValidation(html){
+    return getParam(html, null, null, /name="__EVENTVALIDATION".*?value="([^"]*)"/);
+}
+
+function getPrevPage(html){
+    return getParam(html, null, null, /name="__PREVIOUSPAGE".*?value="([^"]*)"/);
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "https://lk.tricolor.tv/trcustomer/";
+
+    var html = AnyBalance.requestGet(baseurl + 'Login.aspx', g_headers);
+
+    html = AnyBalance.requestPost(baseurl + 'Login.aspx', {
+        'ctl00$ToolkitScriptManager1':'ctl00$ContentPlaceHolder1$UpdatePanel1|ctl00$ContentPlaceHolder1$BLogin',
+        ctl00_ToolkitScriptManager1_HiddenField:'',
+        __EVENTTARGET:'',
+        __EVENTARGUMENT:'',
+        ctl00_ContentPlaceHolder2_NavigationTree1_TreeView1_ExpandState:'ennnennnnnn',
+        ctl00_ContentPlaceHolder2_NavigationTree1_TreeView1_SelectedNode:'',
+        ctl00_ContentPlaceHolder2_NavigationTree1_TreeView1_PopulateLog:'',
+        __VIEWSTATE:getViewState(html),
+        __PREVIOUSPAGE:getPrevPage(html),
+        __EVENTVALIDATION:getEventValidation(html),
+        ctl00$ContentPlaceHolder1$hfPPVRedirect:'',
+        ctl00$ContentPlaceHolder1$hfPPVdate:'',
+        ctl00$ContentPlaceHolder1$TDreid:prefs.login,
+        ctl00$ContentPlaceHolder1$TPassword:prefs.password,
+        __ASYNCPOST:true,
+        'ctl00$ContentPlaceHolder1$BLogin.x':49,
+        'ctl00$ContentPlaceHolder1$BLogin.y':17
+    }, g_headers);
+
+    var redirect = getParam(html, null, null, /\/trCustomer\/(CustPage.aspx[^|]*)/i);
+
+    //AnyBalance.trace(html);
+    if(!redirect){ ///ctl00.logOff/i.test(html)
+        var error = getParam(html, null, null, /<span[^>]*ctl00_ContentPlaceHolder1_ErrDescr[^>]*>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+    }
+
+    html = AnyBalance.requestGet(baseurl + redirect, g_headers);
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /<td[^>]*id="ctl00_ContentPlaceHolder1_pBalanceCurr"[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'agreement', /№ договора абонента:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'device', /№ оборудования:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', />\s*(?:пакет)\s*(<[\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'status', /Статус абонента:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    html = AnyBalance.requestGet(baseurl + 'OServices.aspx', g_headers);
+   
+    var services = [];
+    html.replace(/<tr[^>]*>(?:[\s\S](?!<\/tr))*Активная услуга[\s\S]*?<\/tr>/ig, function(tr){
+        var name = getParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+        var days = getParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        services[services.length] = name + ' (' + days + 'дн)';
+    });
+
+    result.__tariff = services.join(', ');
+
+    AnyBalance.setResult(result);
+}

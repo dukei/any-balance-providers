a7e98873950892fe1212469713d2a243a0ce27f0
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-12-09 13:05:05 +0000

    Котировки металлов с сайта сбербанка

---
+++ trunk/ab-exchange-metal-sbrf/library.js
@@ -0,0 +1,68 @@
+﻿/**
+AnyBalance (http://any-balance-providers.googlecode.com)
+
+Содержит некоторые полезные для извлечения значений с сайтов функции.
+Для конкретного провайдера рекомендуется оставлять в этом файле только те функции, которые используются.
+*/
+
+/**
+ *  Получает JSON из переданного текста, кидает ошибку, если не парсится
+ */
+function getJson(html){
+   try{
+       var json = JSON.parse(html);
+       return json;
+   }catch(e){
+       AnyBalance.trace('Bad json (' + e.message + '): ' + html);
+       throw new AnyBalance.Error('Сервер вернул ошибочные данные: ' + e.message);
+   }
+}
+
+/**
+ *  Проверяет, определено ли значение переменной
+ */
+function isset(v){
+    return typeof(v) != 'undefined';
+}
+
+/**
+ * Date.parse with progressive enhancement for ISO 8601 <https://github.com/csnover/js-iso8601>
+ * В© 2011 Colin Snover <http://zetafleet.com>
+ * Released under MIT license.
+ */
+(function (Date, undefined) {
+    var origParse = Date.parse, numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ];
+    Date.parse = function (date) {
+        var timestamp, struct, minutesOffset = 0;
+
+        // ES5 В§15.9.4.2 states that the string should attempt to be parsed as a Date Time String Format string
+        // before falling back to any implementation-specific date parsing, so thatвЂ™s what we do, even if native
+        // implementations could be faster
+        //              1 YYYY                2 MM       3 DD           4 HH    5 mm       6 ss        7 msec        8 Z 9 В±    10 tzHH    11 tzmm
+        if ((struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:(?:T|\s+)(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(date))) {
+            // avoid NaN timestamps caused by вЂњundefinedвЂќ values being passed to Date.UTC
+            for (var i = 0, k; (k = numericKeys[i]); ++i) {
+                struct[k] = +struct[k] || 0;
+            }
+
+            // allow undefined days and months
+            struct[2] = (+struct[2] || 1) - 1;
+            struct[3] = +struct[3] || 1;
+
+            if (struct[8] !== 'Z' && struct[9] !== undefined) {
+                minutesOffset = struct[10] * 60 + struct[11];
+
+                if (struct[9] === '+') {
+                    minutesOffset = 0 - minutesOffset;
+                }
+            }
+
+            timestamp = new Date(struct[1], struct[2], struct[3], struct[4], struct[5] + minutesOffset, struct[6], struct[7]).getTime();
+        }
+        else {
+            timestamp = origParse ? origParse(date) : NaN;
+        }
+
+        return timestamp;
+    };
+}(Date));
---
+++ trunk/ab-exchange-metal-sbrf/main.js
@@ -0,0 +1,83 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает курсы драг металлов с сайта Сбербанка
+
+Сайт: http://www.sbrf.ru/moscow/ru/quotes/metals/timeline/
+*/
+
+function numSize(num, size){
+  var str = num + '';
+  if(str.length < size){
+    for(var i=str.length; i<size; ++i){
+	str = '0' + str;
+    }
+  }
+  return str;
+}
+
+function getDateString(dt){
+	return numSize(dt.getDate(), 2) + '.' + numSize(dt.getMonth()+1, 2) + '.' + dt.getFullYear();
+}
+
+function main(){
+	AnyBalance.trace('Connecting to sbrf...');
+	var now = new Date();
+	var yesterday = new Date(now.getTime() - 1*86400*1000);
+
+        var metals = {
+            "1": "Au",
+            "6": "Ag",
+            "28": "Pt",
+            "29": "Pd"
+        };
+	
+	var info = AnyBalance.requestPost('http://www.sbrf.ru/common/js/get_quote_values.php', 
+            "version=0&inf_block=223&cbrf=0&group=2&quotes_for=&qid[]=1&qid[]=6&qid[]=28&qid[]=29&_date_afrom114=" + getDateString(yesterday) + "&_date_ato114=" + getDateString(now),
+            {
+                Accept: '*/*',
+                'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+                'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+                'Connection':'keep-alive',
+                'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Referer: 'http://www.sbrf.ru/moscow/ru/quotes/metals/timeline/',
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        );
+        if(!info)
+            throw new AnyBalance.Error('Сбербанк не вернул данные по котировкам, попробуйте ещё раз.', true);
+
+        var json = getJson(info);
+
+	var result = {success: true};
+
+        for(var id in json){
+            if(!isset(metals[id]))
+                continue;
+            var name = metals[id];
+            if(!AnyBalance.isAvailable(name + '_buy', name + '_sell'))
+                continue;
+            if(!json[id].quotes)
+                throw new AnyBalance.Error('Не удается найти котировки для ' + json.meta.TITLE);
+            
+            var dt_max = "";
+            for(var dt in json[id].quotes){
+                if(dt > dt_max)
+                    dt_max = dt;
+            }
+
+            if(!dt_max)
+                continue;
+
+            var quote = json[id].quotes[dt_max];
+            if(AnyBalance.isAvailable(name + '_buy'))
+                result[name + '_buy'] = parseFloat(quote.buy);
+            if(AnyBalance.isAvailable(name + '_sell'))
+                result[name + '_sell'] = parseFloat(quote.sell);
+            if(AnyBalance.isAvailable('date'))
+                result.date = Date.parse(dt_max);
+        }
+	
+	AnyBalance.setResult(result);
+}

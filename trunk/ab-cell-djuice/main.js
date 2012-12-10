9d6a8b2f470e24cedc6d7ad9ec0bbd1ab2b197ac
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-12-10 16:55:25 +0000

    DJuice - добавлены счетчики

--- trunk/ab-cell-djuice/main.js
+++ trunk/ab-cell-djuice/main.js
@@ -94,12 +94,30 @@ function parseTraffic(text){
   return val;
 }
 
+/**
+ * Заменяет HTML сущности в строке на соответствующие им символы
+ */
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+
 function parseMinutes(str){
   AnyBalance.trace('Parsing minutes from value: ' + str);
   return parseFloat(str)*60; //Переводим в секунды
 }
 
 //------------------------------------------------------------------------------
+
+function getToken(html){
+    var token = /name="org.apache.struts.taglib.html.TOKEN"[^>]+value="([\s\S]*?)">/.exec(html);
+    if(!token)
+        throw new AnyBalance.Error("Не удаётся найти код безопасности для входа. Проблемы или изменения на сайте?");
+    return token[1];
+}
 
 function main(){
   var prefs = AnyBalance.getPreferences();
@@ -114,15 +132,11 @@ function main(){
   AnyBalance.trace('Connecting to ' + baseurl);
 
   var html = AnyBalance.requestGet(baseurl + 'tbmb/login_djuice/show.do', headers);
-  var token = /name="org.apache.struts.taglib.html.TOKEN" value="([\s\S]*?)">/.exec(html);
-  if(!token)
-      throw new AnyBalance.Error("Не удаётся найти код безопасности для входа. Проблемы или изменения на сайте?");
-
-  AnyBalance.trace('Token = ' + token[1]);
+  AnyBalance.trace('Token = ' + getToken(html));
 
   var html = AnyBalance.requestPost(baseurl + 'tbmb/login_djuice/perform.do', {
     isSubmitted: "true",
-    "org.apache.struts.taglib.html.TOKEN": token[1],
+    "org.apache.struts.taglib.html.TOKEN": getToken(html),
     user: prefs.login,
     password: prefs.password
   }, headers);
@@ -175,6 +189,93 @@ function main(){
   
   //Срок действия номера
   sumParam(html, result, 'till', /(?:Номер діє до:|Номер действует до:)[\s\S]*?<td>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
-  
+
+  //Получим дату последнего пополнения, а также дату последнего пополнения на 40 гривен и больше + 29 дней (для срока действия пакета интернет)
+  if(AnyBalance.isAvailable('lastpaydate', 'lastpaysum', 'lastpaydesc', 'paydate40end')){
+      html = AnyBalance.requestGet(baseurl + 'tbmb/payment/activity/show.do');
+      var allpayments = [];
+      var now = new Date();
+      var month = new Date(now.getFullYear(), now.getMonth(), 1);
+
+      //Узнаем начальную дату регистрации, чтобы не запрашивать слишком далеко
+      var startDate = sumParam(html, null, null, /(?:предоставляется, начиная с|зв’язку надається, починаючи з)\s*<b[^>]*>([^<]*)<\/b>/i, replaceTagsAndSpaces, parseDate) || 0;
+      getPayments(html, allpayments);
+      var maxTries = 3;
+      while(!findPayments(allpayments, result) && month.getTime() > startDate && maxTries-- > 0){
+          month = new Date(month.getFullYear(), month.getMonth() - 1, 1);
+          
+          html = AnyBalance.requestPost(baseurl + 'tbmb/view/display_view.do', {
+              'org.apache.struts.taglib.html.TOKEN': getToken(html),
+              selectedDate: getDateString(month, '.'),
+              fromDate: getDateString(month, '/'),
+              toDate: getDateString(new Date(month.getFullYear(), month.getMonth()+1, 0), '/')
+          });
+          getPayments(html, allpayments);
+          startDate = startDate || sumParam(html, null, null, /(?:предоставляется, начиная с|зв’язку надається, починаючи з)\s*<b[^>]*>([^<]*)<\/b>/i, replaceTagsAndSpaces, parseDate) || 0;
+      }
+  }
+
   AnyBalance.setResult(result);
+}
+
+function numSize(num, size){
+  var str = num + '';
+  if(str.length < size){
+    for(var i=str.length; i<size; ++i){
+	str = '0' + str;
+    }
+  }
+  return str;
+}
+
+function getDateString(dt, separator){
+        if(!separator) separator = '.';
+	return numSize(dt.getDate(), 2) + separator + numSize(dt.getMonth()+1, 2) + separator + dt.getFullYear();
+}
+
+function getPayments(html, allpayments){
+    var payments = sumParam(html, null, null, /<edx_table[^>]+name="Payments"[^>]*>([\s\S]*?)<\/edx_table>/i);
+    if(payments){
+        payments.replace(/<tr[^>]*>([\s\S]*?)<\/tr>/ig, function(str, tr){
+            var date = sumParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+            var desc = sumParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+            var sum = sumParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+            if(!date || !sum){
+                AnyBalance.trace('Could not obtain date or sum from row: ' + tr);
+            }else{
+                allpayments[allpayments.length] = {date: date, desc: desc, sum: sum};
+            }
+        });
+    }
+}
+
+function findPayments(allpayments, result){
+    var maxIndex = -1, maxIndex40 = -1, maxDate=0, maxDate40=0;
+    for(var i=0; i<allpayments.length; ++i){
+        var p = allpayments[i];
+        if(p.date > maxDate){
+            maxDate = p.date;
+            maxIndex = i;
+        }
+        if(p.date > maxDate40 && p.sum >= 40){ //Если заплачено больше 40 гривен
+            maxDate40 = p.date;
+            maxIndex40 = i;
+        }
+    }
+
+    var ret = true;
+    if(AnyBalance.isAvailable('lastpaydate', 'lastpaysum', 'lastpaydesc')){
+        ret = ret && maxIndex >= 0;
+        if(maxIndex >= 0){
+            result.lastpaydate = allpayments[maxIndex].date;
+            result.lastpaysum = allpayments[maxIndex].sum;
+            result.lastpaydesc = allpayments[maxIndex].desc;
+        }
+    }
+    if(AnyBalance.isAvailable('paydate40end')){
+        ret = ret && maxIndex40 >= 0;
+        if(maxIndex40 >= 0)
+            result.paydate40end = allpayments[maxIndex40].date + 29*86400*1000; //Пакет действует 29 дней после платежа в 40 гривен
+    }
+    return ret;
 }

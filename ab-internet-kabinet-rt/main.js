ccae8614d0e9543f78064f3316d5b229c4e3a719
dmitry.kochin <dmitry.kochin@gmail.com>
2012-12-13 14:26:58 +0000

    Кабинет РТ - несколько счетов Нурбанк (Казахстан)

--- ab-internet-kabinet-rt/main.js
+++ ab-internet-kabinet-rt/main.js
@@ -45,51 +45,110 @@ function main(){
     AnyBalance.setCookie('kabinet.rt.ru', 'sessionHashKey', json.sessionKey);
 
     html = AnyBalance.requestGet(baseurl);
-    json = getParam(html, null, null, /var[\s+]services\s*=\s*(\{[\s\S]*?\})\s*;/, null, getJson);
-    if(!json)
+    var accinfo = getParam(html, null, null, /var[\s+]services\s*=\s*(\{[\s\S]*?\})\s*;/, null, getJson);
+    if(!accinfo)
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     
-    if(json.isError)
-        throw new AnyBalance.Error(json.errorMsg);
+    if(accinfo.isError)
+        throw new AnyBalance.Error(accinfo.errorMsg);
 
-    if(!json.accounts || json.accounts.length == 0)
+    if(!accinfo.accounts || accinfo.accounts.length == 0)
         throw new AnyBalance.Error("В вашем кабинете Ростелеком ещё не подключена ни одна услуга");
 
-    var acc;
-    for(var i=0; i<json.accounts.length; ++i){
-        var _acc = json.accounts[i];
-        if(!prefs.num || (prefs.num && endsWith(_acc.number, prefs.num))){
-            acc = _acc;
-            break;
-        }
-    }
+    AnyBalance.trace('Найдено ' + accinfo.accounts.length + ' л/с');
 
-    if(!acc)
-        throw new AnyBalance.Error("Не найдено лицевого счета, оканчивающегося на " + prefs.num);
+    var nums = prefs.num ? prefs.num.split(/,/g) : ['', '', '', ''];
 
-    html = AnyBalance.requestPost(baseurl + 'serverLogic/getAccountInfo', {account: acc.id});
-    json = getJson(html);
-
+    var not_found = [];
+    var index = 0;
+    var tariffs = [];
+    var totalBalancePlus = 0;
+    var totalBalanceMinus = 0;
     var result = {success: true};
 
-    if(AnyBalance.isAvailable('balance'))
-        result.balance = json.balance/100;
+    for(var i=0; i<nums.length; ++i){
+        var num = nums[i].replace(/\s+/g, '');
 
-    var tariffs = [];
-    var statuses = []; 
-    for(var i=0; json.services && i<json.services.length; ++i){
-        var service = json.services[i];
-        tariffs[tariffs.length] = service.typeString + ': ' + service.tariff.tarName;
-        statuses[statuses.length] = service.typeString + ': ' + g_ServiceStatus[service.status];
+        var acc = null;
+        if(num){
+            //Если num непустой, то ищем аккаунт с этим номером
+            for(var j=0; j<accinfo.accounts.length; ++j){
+                var _acc = accinfo.accounts[j];
+                if(endsWith(_acc.number, num)){
+                    acc = _acc;
+                    break;
+                }
+            }
+        }else{
+            //Если num пустой, то просто берем следующий по счету аккаунт
+            acc = accinfo.accounts[i];
+        }
+        
+        if(!acc){
+            if(num){
+                not_found[not_found.length] = num;
+                AnyBalance.trace('Аккаунт, оканчивающийся на ' + num + ' не найден.');
+            }
+            break;
+        }
+
+        AnyBalance.trace('Получаем данные для л/с: ' + acc.number);
+        html = AnyBalance.requestPost(baseurl + 'serverLogic/getAccountInfo', {account: acc.id});
+        json = getJson(html);
+
+        acc.__detailedInfo = json;
+        
+        if(i < 4){
+            var suffix = i ? i : '';
+            
+            if(AnyBalance.isAvailable('balance'))
+                result['balance' + suffix] = json.balance/100;
+            
+            var statuses = []; 
+            for(var j=0; json.services && j<json.services.length; ++j){
+                var service = json.services[j];
+                tariffs[tariffs.length] = service.typeString + ': ' + service.tariff.tarName;
+                statuses[statuses.length] = service.typeString + ': ' + g_ServiceStatus[service.status];
+            }
+
+            if(AnyBalance.isAvailable('status') && statuses.length > 0)
+                result['status' + suffix] = statuses.join(', ');
+            
+            if(AnyBalance.isAvailable('licschet'))
+                result['licschet' + suffix] = acc.number;
+        }
+
+        if(json.balance > 0)
+            totalBalancePlus += json.balance/100;
+        else
+            totalBalanceMinus += json.balance/100;
     }
+
     if(tariffs.length)
         result.__tariff = tariffs.join(', ');
-    if(AnyBalance.isAvailable('status') && statuses.length > 0)
-        result.status = statuses.join(', ');
 
-    if(AnyBalance.isAvailable('licschet'))
-        result.licschet = acc.number;
+    //Получим баланс для остальных л\с, если юзер запросил
+    if(AnyBalance.isAvailable('totalBalancePlus', 'totalBalanceMinus')){
+        for(var i=0; i<accinfo.accounts.length; ++i){
+            var acc = accinfo.accounts[i];
+            if(!acc.__detailedInfo){
+                AnyBalance.trace('Дополнительно получаем данные для л/с: ' + acc.number);
+                html = AnyBalance.requestPost(baseurl + 'serverLogic/getAccountInfo', {account: acc.id});
+                json = getJson(html);
 
+                if(json.balance > 0)
+                    totalBalancePlus += json.balance/100;
+                else
+                    totalBalanceMinus += json.balance/100;
+            }
+
+            if(AnyBalance.isAvailable('totalBalancePlus'))
+                result.totalBalancePlus = totalBalancePlus;
+
+            if(AnyBalance.isAvailable('totalBalanceMinus'))
+                result.totalBalanceMinus = totalBalanceMinus;
+        }
+    }
 
     AnyBalance.setResult(result);
 }

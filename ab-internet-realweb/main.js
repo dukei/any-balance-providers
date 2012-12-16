7a8b95c55b280e9d691f40691d027cc598da6d7f
dmitry.kochin <dmitry.kochin@gmail.com>
2012-12-16 19:27:39 +0000

    RealWeb

---
+++ ab-internet-realweb/main.js
@@ -0,0 +1,81 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для украинского интернет-провайдера realweb.
+
+Сайт оператора: http://realweb.ua
+Личный кабинет: http://realweb.ua/utm5/
+*/
+
+function getTrafficGb(str){
+  return parseFloat((parseFloat(str)/1024).toFixed(2));
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "http://realweb.ua/utm5/";
+    var html = AnyBalance.requestPost(baseurl, {
+        login: prefs.login,
+        password: prefs.password
+    });
+
+    var error = getParam(html, null, null, /<p[^>]*style=['"]color:red['"][^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+    if(error)
+        throw new AnyBalance.Error(error);
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /Баланс[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'userName', /ФИО[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /Основной лицевой счет[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'credit', /Кредит[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+
+    var html = AnyBalance.requestGet(baseurl + '?module=40_tariffs');
+    getParam(html, result, '__tariff', /<td[^>]*>Текущий ТП[\s\S]*?<td[^>]*class=['"]utm-cell['"][^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    /* не удаётся получить трафик на тестовом аккаунте, наверное, его нет
+    if(AnyBalance.isAvailable('trafficIn')){
+        var now = new Date();
+        var m = now.getMonth() + 1;
+        var year = now.getFullYear();
+        var date1 = Date.UTC(year, m-2, 1) / 1000 + now.getTimezoneOffset()*60;
+        var date2 = Date.UTC(year, m-1, 1) / 1000 + now.getTimezoneOffset()*60;
+        var html = AnyBalance.requestPost(baseurl + '?module=30_traffic_report', {
+             date1:date1,
+             date2:date2,
+             month:0,
+             year:0
+       });
+
+       getParam(html, result, 'trafficIn', /входящий[\s\S]*?<td[^>]*>(-?\d[\d\.,\s]*)/i, replaceFloat, getTrafficGb);
+    }
+    
+    if(AnyBalance.isAvailable('payments')){
+        var now = new Date();
+        var m = now.getMonth() + 1;
+        var year = now.getFullYear();
+        var date1 = Date.UTC(year, m-1, 1) / 1000 + now.getTimezoneOffset()*60;
+        var date2 = Date.UTC(year, m, 1) / 1000 + now.getTimezoneOffset()*60;
+        var html = AnyBalance.requestPost(baseurl + '?module=32_payments_report', {
+             date1:date1,
+             date2:date2,
+             month:0,
+             year:0
+       });
+
+       getParam(html, result, 'payments', /Итого[\s\S]*?<td[^>]*>(-?\d[\d\.,\s]*)/i, replaceFloat, parseFloat);
+    }
+    */
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

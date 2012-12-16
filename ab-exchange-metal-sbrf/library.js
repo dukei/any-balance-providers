0e7f04bad2d4d455538acc91f51d921029f2daca
dmitry.kochin <dmitry.kochin@gmail.com>
2012-12-16 20:18:10 +0000

    Курсы драгоценных металлов сбербанка - настройка региона

--- ab-exchange-metal-sbrf/library.js
+++ ab-exchange-metal-sbrf/library.js
@@ -5,6 +5,9 @@ AnyBalance (http://any-balance-providers.googlecode.com)
 Для конкретного провайдера рекомендуется оставлять в этом файле только те функции, которые используются.
 */
 
+//Замена пробелов и тэгов
+var replaceTagsAndSpaces = [/&nbsp;/ig, ' ', /<!--[\s\S]*?-->/g, '', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+
 /**
  *  Получает JSON из переданного текста, кидает ошибку, если не парсится
  */
@@ -66,3 +69,68 @@ function isset(v){
         return timestamp;
     };
 }(Date));
+
+/**
+ * Получает значение, подходящее под регулярное выражение regexp, производит 
+ * в нем замены replaces, результат передаёт в функцию parser, 
+ * а затем записывает результат в счетчик с именем param в result
+ * Результат в result помещается только если счетчик выбран пользователем 
+ * в настройках аккаунта
+ *
+ * Очень похоже на getParam, но может получать несколько значений (при наличии 
+ * в регулярном выражении флага g). В этом случае суммирует их.
+ * 
+ * если result и param равны null, то значение просто возвращается.
+ * eсли parser == null, то возвращается результат сразу после замен
+ * если replaces == null, то замены не делаются
+ * do_replace - если true, то найденные значения вырезаются из переданного текста
+ * 
+ * replaces - массив, нечетные индексы - регулярные выражения, четные - строки, 
+ * на которые надо заменить куски, подходящие под предыдущее регулярное выражение
+ * см. например replaceTagsAndSpaces
+ */
+function sumParam (html, result, param, regexp, replaces, parser, do_replace, aggregate) {
+    if (param && (param != '__tariff' && !AnyBalance.isAvailable (param))){
+        if(do_replace)
+          return html;
+        else
+            return;
+    }
+
+    if(typeof(do_replace) == 'function'){
+        aggregate = do_replace;
+        do_replace = false;
+    }
+
+    var values = [];
+    if(param && isset(result[param]))
+        values[values.length] = result.param;
+
+    var html_copy = html.replace(regexp, function(str, value){
+	for (var i = 0; replaces && i < replaces.length; i += 2) {
+		value = value.replace (replaces[i], replaces[i+1]);
+	}
+	if (parser)
+		value = parser (value);
+            
+            if(isset(value))
+            	values[values.length] = value;
+            return ''; //Вырезаем то, что заматчили
+    });
+
+    var total_value;
+    if(aggregate)
+        total_value = aggregate(values);
+    else if(!param) //Если не требуется записывать в резалт, и функция агрегации отсутствует, то вернем массив
+        total_value = values;
+
+    if(param){
+      if(isset(total_value)){
+          result[param] = total_value;
+      }
+      if(do_replace)
+          return html_copy;
+    }else{
+      return total_value;
+    }
+}

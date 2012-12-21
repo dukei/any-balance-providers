548c95a0200a712a18aae2f189fdb75f9605a341
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-12-21 06:40:27 +0000

    Билайн украина - добавлены минуты

---
+++ trunk/ab-cell-beeline-ukr/library.js
@@ -0,0 +1,177 @@
+﻿/**
+AnyBalance (http://any-balance-providers.googlecode.com)
+
+Содержит некоторые полезные для извлечения значений с сайтов функции.
+Для конкретного провайдера рекомендуется оставлять в этом файле только те функции, которые используются.
+*/
+
+/**
+ * Получает значение, подходящее под регулярное выражение regexp, производит 
+ * в нем замены replaces, результат передаёт в функцию parser, 
+ * а затем записывает результат в счетчик с именем param в result
+ * Результат в result помещается только если счетчик выбран пользователем 
+ * в настройках аккаунта
+ * 
+ * если result и param равны null, то значение просто возвращается.
+ * eсли parser == null, то возвращается результат сразу после замен
+ * если replaces == null, то замены не делаются
+ * 
+ * replaces - массив, нечетные индексы - регулярные выражения, четные - строки, 
+ * на которые надо заменить куски, подходящие под предыдущее регулярное выражение
+ * см. например replaceTagsAndSpaces
+ */
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var matches = regexp ? regexp.exec (html) : [html, html], value;
+	if (matches) {
+		value = matches[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+	}
+   return value
+}
+
+//Замена пробелов и тэгов
+var replaceTagsAndSpaces = [/&nbsp;/ig, ' ', /<!--[\s\S]*?-->/g, '', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+//Замена для чисел
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+/**
+ * Извлекает числовое значение из переданного текста
+ */
+function parseBalance(text){
+    var val = getParam(html_entity_decode(text).replace(/\s+/g, ''), null, null, /(-?\d[\d.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+/**
+ * Заменяет HTML сущности в строке на соответствующие им символы
+ */
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+
+/**
+ *  Получает дату из строки
+ */
+function parseDate(str){
+    var matches = /(?:(\d+)[^\d])?(\d+)[^\d](\d{2,4})(?:[^\d](\d+):(\d+)(?::(\d+))?)?/.exec(str);
+    if(matches){
+          var year = +matches[3];
+          var date = new Date(year < 1000 ? 2000 + year : year, matches[2]-1, +(matches[1] || 1), matches[4] || 0, matches[5] || 0, matches[6] || 0);
+	  var time = date.getTime();
+          AnyBalance.trace('Parsing date ' + date + ' from value: ' + str);
+          return time;
+    }
+    AnyBalance.trace('Failed to parse date from value: ' + str);
+}
+
+/**
+ *  Проверяет, определено ли значение переменной
+ */
+function isset(v){
+    return typeof(v) != 'undefined';
+}
+
+/**
+ * Получает значение, подходящее под регулярное выражение regexp, производит 
+ * в нем замены replaces, результат передаёт в функцию parser, 
+ * а затем записывает результат в счетчик с именем param в result
+ * Результат в result помещается только если счетчик выбран пользователем 
+ * в настройках аккаунта
+ *
+ * Очень похоже на getParam, но может получать несколько значений (при наличии 
+ * в регулярном выражении флага g). В этом случае суммирует их.
+ * 
+ * если result и param равны null, то значение просто возвращается.
+ * eсли parser == null, то возвращается результат сразу после замен
+ * если replaces == null, то замены не делаются
+ * do_replace - если true, то найденные значения вырезаются из переданного текста
+ * 
+ * replaces - массив, нечетные индексы - регулярные выражения, четные - строки, 
+ * на которые надо заменить куски, подходящие под предыдущее регулярное выражение
+ * см. например replaceTagsAndSpaces
+ */
+function sumParam (html, result, param, regexp, replaces, parser, do_replace, aggregate) {
+    if (param && (param != '__tariff' && !AnyBalance.isAvailable (param))){
+        if(do_replace)
+          return html;
+        else
+            return;
+    }
+
+    if(typeof(do_replace) == 'function'){
+        aggregate = do_replace;
+        do_replace = false;
+    }
+
+    var values = [];
+    if(param && isset(result[param]))
+        values[values.length] = result[param];
+
+    var html_copy = html.replace(regexp, function(str, value){
+	for (var i = 0; replaces && i < replaces.length; i += 2) {
+		value = value.replace (replaces[i], replaces[i+1]);
+	}
+	if (parser)
+		value = parser (value);
+            
+            if(isset(value))
+            	values[values.length] = value;
+            return ''; //Вырезаем то, что заматчили
+    });
+
+    var total_value;
+    if(aggregate)
+        total_value = aggregate(values);
+    else if(!param) //Если не требуется записывать в резалт, и функция агрегации отсутствует, то вернем массив
+        total_value = values;
+
+    if(param){
+      if(isset(total_value)){
+          result[param] = total_value;
+      }
+      if(do_replace)
+          return html_copy;
+    }else{
+      return total_value;
+    }
+}
+
+function aggregate_sum(values){
+    if(values.length == 0)
+        return;
+    var total_value = 0;
+    for(var i=0; i<values.length; ++i){
+        total_value += values[i];
+    }
+    return total_value;
+}
+
+function aggregate_join(values, delimiter){
+    if(values.length == 0)
+        return;
+    if(!isset(delimiter))
+        delimiter = ', ';
+    return values.join(delimiter);
+}
+
+function create_aggregate_join(delimiter){
+    return function(values){ return aggregate_join(values, delimiter); }
+}

d4b595134c770de52ca2613deaee04e5bf71a4d2
dmitry.kochin <dmitry.kochin@gmail.com>
2012-12-28 07:35:05 +0000

    Lufthansa

---
+++ ab-bonus-lufthansa/library.js
@@ -0,0 +1,140 @@
+﻿/**
+AnyBalance (http://any-balance-providers.googlecode.com)
+
+Содержит некоторые полезные для извлечения значений с сайтов функции.
+Для конкретного провайдера рекомендуется оставлять в этом файле только те функции, которые используются.
+*/
+
+/**
+ * Получает значение, подходящее под регулярное выражение regexp, производит 
+ * в нем замены replaces, результат передаёт в функцию parser, 
+ * а затем записывает результат в счетчик с именем param в result
+ * Результат в result помещается только если счетчик выбран пользователем 
+ * в настройках аккаунта
+ * 
+ * если result и param равны null, то значение просто возвращается.
+ * eсли parser == null, то возвращается результат сразу после замен
+ * если replaces == null, то замены не делаются
+ * 
+ * replaces - массив, нечетные индексы - регулярные выражения, четные - строки, 
+ * на которые надо заменить куски, подходящие под предыдущее регулярное выражение
+ * см. например replaceTagsAndSpaces
+ */
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var matches = regexp ? regexp.exec (html) : [html, html], value;
+	if (matches) {
+		value = matches[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+	}
+   return value
+}
+
+//Замена пробелов и тэгов
+var replaceTagsAndSpaces = [/&nbsp;/ig, ' ', /<!--[\s\S]*?-->/g, '', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
+//Замена для чисел
+var replaceFloat = [/\s+/g, '', /,/g, ''];
+
+/**
+ * Извлекает числовое значение из переданного текста
+ */
+function parseBalance(text){
+    var val = getParam(html_entity_decode(text).replace(/\s+/g, ''), null, null, /(-?\d[\d.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+/**
+ * Заменяет HTML сущности в строке на соответствующие им символы
+ */
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+
+/**
+ *  Проверяет, определено ли значение переменной
+ */
+function isset(v){
+    return typeof(v) != 'undefined';
+}
+
+/**
+ * Получает значение, подходящее под регулярное выражение regexp, производит 
+ * в нем замены replaces, результат передаёт в функцию parser, 
+ * а затем записывает результат в счетчик с именем param в result
+ * Результат в result помещается только если счетчик выбран пользователем 
+ * в настройках аккаунта
+ *
+ * Очень похоже на getParam, но может получать несколько значений (при наличии 
+ * в регулярном выражении флага g). В этом случае суммирует их.
+ * 
+ * если result и param равны null, то значение просто возвращается.
+ * eсли parser == null, то возвращается результат сразу после замен
+ * если replaces == null, то замены не делаются
+ * do_replace - если true, то найденные значения вырезаются из переданного текста
+ * 
+ * replaces - массив, нечетные индексы - регулярные выражения, четные - строки, 
+ * на которые надо заменить куски, подходящие под предыдущее регулярное выражение
+ * см. например replaceTagsAndSpaces
+ */
+function sumParam (html, result, param, regexp, replaces, parser, do_replace, aggregate) {
+    if (param && (param != '__tariff' && !AnyBalance.isAvailable (param))){
+        if(do_replace)
+          return html;
+        else
+            return;
+    }
+
+    if(typeof(do_replace) == 'function'){
+        aggregate = do_replace;
+        do_replace = false;
+    }
+
+    var values = [];
+    if(param && isset(result[param]))
+        values[values.length] = result[param];
+
+    var html_copy = html.replace(regexp, function(str, value){
+	for (var i = 0; replaces && i < replaces.length; i += 2) {
+		value = value.replace (replaces[i], replaces[i+1]);
+	}
+	if (parser)
+		value = parser (value);
+            
+            if(isset(value))
+            	values[values.length] = value;
+            return ''; //Вырезаем то, что заматчили
+    });
+
+    var total_value;
+    if(aggregate)
+        total_value = aggregate(values);
+    else if(!param) //Если не требуется записывать в резалт, и функция агрегации отсутствует, то вернем массив
+        total_value = values;
+
+    if(param){
+      if(isset(total_value)){
+          result[param] = total_value;
+      }
+      if(do_replace)
+          return html_copy;
+    }else{
+      return total_value;
+    }
+}
---
+++ ab-bonus-lufthansa/main.js
@@ -0,0 +1,68 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает информацию по бонусной программе Lufthansa Miles and More
+
+Сайт оператора: https://mobile.lufthansa.com/
+Личный кабинет: https://mobile.lufthansa.com/mma/account.do
+*/
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "https://mobile.lufthansa.com";
+    //Need to enter a country
+    html = AnyBalance.requestGet(baseurl + "/hpg/cor.do?l=en_US");
+
+//    return makeCountries(html);
+
+    var country = prefs.country || 'ZZ';
+    var action = getParam(html, null, null, /action="(\/hpg\/cor.do[^"]*)/i, null, html_entity_decode);
+    if(!action)
+        throw new AnyBalance.Error('Can not find country form!');
+
+    var sid = getParam(action, null, null, /jsessionid=([^\?;&]+)/i); 
+
+    html = AnyBalance.requestPost(baseurl + action, {
+        country: country,
+        timezone: jstz.determine_timezone().name()
+    });
+
+    var html = AnyBalance.requestPost(baseurl + "/mma/account.do;jsessionid=" + sid + "?l=en_US", {
+        user:prefs.login,
+        pass:prefs.password,
+        step:'search'
+    });
+
+    if(!/step=logout/.test(html)){
+        var error = getParam(html, null, null, /<span[^>]*class="feedback_neg"[^>]*>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Could not enter miles&more site. Is the site changed?');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /<td[^>]*>Award miles[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'qbalance', /<td[^>]*>Status miles[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, '__tariff', /<td[^>]*>Status[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'cardnum', /<td[^>]*>Customer number[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}
+
+function makeCountries(html){
+    var result = {success: true};
+    
+    var countries = sumParam(html, null, null, /(<option[^>]+value="\w+"[^>]*>[^<]*<\/option>)/ig);
+    var codes = [], names = [];
+    for(var i=0; i<countries.length; ++i){
+        codes[codes.length] = getParam(countries[i], null, null, /value="([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
+        names[names.length] = getParam(countries[i], null, null, /<option[^>]*>([^<]*)<\/option>/i, replaceTagsAndSpaces, html_entity_decode);
+    }
+
+    result.codes = codes.join('|');
+    result.names = names.join('|');
+    AnyBalance.setResult(result);
+}

63725e9a128918fa16ed3dd8c9a9bf5c839d69d0
dmitry.kochin <dmitry.kochin@gmail.com>
2012-12-30 14:49:36 +0000

    Majordomo

--- ab-hosting-majordomo/main.js
+++ ab-hosting-majordomo/main.js
@@ -1,27 +1,27 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-Получает информацию о домене для регистратора registrant
+Получает баланс и информацию о домене для хостинг провайдера majordomo
 
-Сайт оператора: http://registrant.ru
-Личный кабинет: https://control.registrant.ru/
+Сайт оператора: http://majordomo.ru
+Личный кабинет: https://control.majordomo.ru
 */
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    AnyBalance.setDefaultCharset('utf-8');
+    AnyBalance.setDefaultCharset('koi8-r');
 
-    var baseurl = "https://control.registrant.ru/";
+    var baseurl = "https://control.majordomo.ru/";
 
-    var html = AnyBalance.requestPost(baseurl + 'login/', {
+    var html = AnyBalance.requestPost(baseurl, {
+        start:'0',
         login:prefs.login,
-        password:prefs.password,
-        start:'ok'
+        password:prefs.password
     });
 
     //AnyBalance.trace(html);
-    if(!/\/logout\//i.test(html)){
-        var error = getParam(html, null, null, /<p[^>]*class=["']error[^>]*>([\s\S]*?)<\/p>/, replaceTagsAndSpaces, html_entity_decode);
+    if(!/logout.php/i.test(html)){
+        var error = getParam(html, null, null, /<span[^>]*color:\s*red[^>]*>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
@@ -29,18 +29,20 @@ function main(){
 
     var result = {success: true};
 
+    getParam(html, result, 'balance', /Состояние счета[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, '__tariff', /.>\s*Тарифный план[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'status', /Статус[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'domains', /Доменов на аккаунте:[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'userName', /ФИО[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 
     if(prefs.domains){
-        html = AnyBalance.requestGet(baseurl + 'domains/');
+        html = AnyBalance.requestGet(baseurl + 'renew.php');
 
         var notfound = [];
         var found = [];
         var ind = 0;
         
         var domains = prefs.domains.split(/\s*,\s*/g);
-        var domainsHtml = getParam(html, null, null, /<table[^>]+id="domains"[^>]*>([\s\S]*?)<\/table>/i) || '';
+        var domainsHtml = getParam(html, null, null, /<th[^>]*>Истечение срока действия доменных имен([\s\S]*?)<\/table>/i) || '';
         
         for(var i=0; i<domains.length; ++i){
             var domain = domains[i];
@@ -48,7 +50,7 @@ function main(){
             if(_domain == '*')
                 _domain = '\\w+\\.\\w+';
            
-            var tr = getParam(domainsHtml, null, null, new RegExp('(<tr(?:[\\s\\S](?!</tr>))*?<a[^>]+href="/domain/[^>]*>[^<]*' + _domain + '[\\s\\S]*?<\\/tr>)', 'i'));
+            var tr = getParam(domainsHtml, null, null, new RegExp('(<tr(?:[\\s\\S](?!</tr>))*?<input[^>]+name=[\'"]renewname[^>]*value=["\'][^\'"]*' + _domain + '[\\s\\S]*?</tr>)', 'i'));
         
             if(!tr){
                 if(domain != '*')
@@ -57,7 +59,7 @@ function main(){
                 var suffix = ind > 0 ? ind : '';
                 var domain_name = getParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
                 getParam(tr, result, 'domain' + suffix, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-                getParam(tr, result, 'domain_till' + suffix, /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+                getParam(tr, result, 'domain_till' + suffix, /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
                 found[found.length] = domain_name;
             }
         

1a3f6a08bb865ea28519cc58827a5d0bc61f1fdc
tujger@gmail.com <tujger@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2012-12-31 09:23:06 +0000

    Добавлено слежение за объявлениями на Avito.ru

--- trunk/ab-tracking-advertisements/main.js
+++ trunk/ab-tracking-advertisements/main.js
@@ -8,17 +8,22 @@
 function main(){
 	var prefs = AnyBalance.getPreferences();
 	var region = prefs.region;
+	var region_a = prefs.region_a;
 	var pattern = prefs.pattern;
 
 
-	AnyBalance.trace('Selected region: ' + region);
+//Slando
+	AnyBalance.trace('Selected region (Slando): ' + region);
 
 	var pattern1=pattern.replace(" ","+");
 	var baseurl = 'http://'+region+'.slando.ru/list/?q='+pattern1;
+
+//Avito
+	var baseurl_a = 'http://www.avito.ru/'+region_a+'?name='+pattern1;
 	AnyBalance.setDefaultCharset('utf-8');
 
-
-	AnyBalance.trace('Starting search: ' + baseurl);
+//Slando
+	AnyBalance.trace('Starting search (Slando): ' + baseurl);
 	var info = AnyBalance.requestGet(baseurl);
 
 	var error = $('#errHolder', info).text();
@@ -30,8 +35,8 @@ function main(){
 
 	result.__tariff = pattern;
 
-	if(AnyBalance.isAvailable('found') && (matches = info.match(/<li class="fleft">\s*<span class="fleft tab selected">\s*<span class="fbold">Продам<\/span>\s*<span class="color-2 normal">(\d+)<\/span>\s*<\/span>\s*<\/li>/i))){
-		result.found = matches[1];
+	if((AnyBalance.isAvailable('found') || AnyBalance.isAvailable('found_total')) && (matches = info.match(/<li class="fleft">\s*<span class="fleft tab selected">\s*<span class="fbold">Продам<\/span>\s*<span class="color-2 normal">(\d+)<\/span>\s*<\/span>\s*<\/li>/i))){
+		result.found = parseInt(matches[1]);
 	}
 
 	var price=null;
@@ -47,7 +52,7 @@ function main(){
 			price = matches[1];
 		if(price!=null) result.last = result.last + " – " + price;
 		if(matches = info.match(/<h3 class="large lheight20 margintop10">\s*<a href="(.*?)l" class="link linkWithHash clicker {clickerID:'ads_title'}">/i))
-			AnyBalance.trace('See link to the last post: '+matches[1]);
+			AnyBalance.trace('See link to the last post (Slando): '+matches[1]);
 	}
 
 	if(AnyBalance.isAvailable('region') && (matches = info.match(/<span class="link hn"><span>\s*<strong>(.*?)<\/strong>\s*<\/span><\/span>/i))){
@@ -60,7 +65,32 @@ function main(){
 	}
 
 
+//Avito
+	AnyBalance.trace('Starting search (Avito): ' + baseurl_a);
+	var info = AnyBalance.requestGet(baseurl_a);
 
+	price=null;
+	if((AnyBalance.isAvailable('found_a') || AnyBalance.isAvailable('found_total')) && (matches = info.match(/><span class="catalog_breadcrumbs-count">, (.*?)<\/span>/i))){
+		result.found_a = parseInt(matches[1].replace("&nbsp;",""));
+	}
+	result.found_total = result.found + result.found_a;
+
+
+	if(AnyBalance.isAvailable('last_a')){
+		if(matches = info.match(/<div class="t_i_date">\s*(.*?)\s*<span class="t_i_time">(.*?)<\/span>\s*<\/div>/i)){
+			result.date_a = matches[1] + ' ' + matches[2];
+			result.date_a=result.date_a.replace(new RegExp("<.*?>",'g')," ");
+			result.date_a=result.date_a.replace(new RegExp("\s+",'g')," ");
+		}
+		if(matches = info.match(/<div class="t_i_title">\s*<h3 class="t_i_h3">\s*<a name=.*?href="(.*?)".*?>\s*(.*?)<\/a>\s*<\/h3>/i)){
+			result.last_a = matches[2];
+			AnyBalance.trace('See link to the last post (Avito): http://www.avito.ru'+matches[1]);
+		}
+		if(matches = info.match(/<\/h3>\s*<p>\s*<span>(.*?)<\/p>/i)){
+			price = matches[1].replace(new RegExp("<.*?>",'g')," ");
+			result.last_a = result.last_a + " – " + price;
+		}
+	}
 
 	AnyBalance.setResult(result);
 };

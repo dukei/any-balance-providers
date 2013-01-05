189d141bfa69b6a6950fb27c90eef3958e9f4412
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-01-05 09:16:21 +0000

    энергокомфорт амур - добавлена куча счетчиков

--- trunk/ab-municipal-amur/main.js
+++ trunk/ab-municipal-amur/main.js
@@ -41,11 +41,31 @@ function main(){
     replaceTagsAndSpaces.push(/задолж\./ig, '-');
 
     getParam(html, result, 'balance', /<th[^>]*>На конец(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'bill', /<th[^>]*>На конец(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'agreement', /Код плательщика:(?:[\s\S]*?<div[^>]*class="val"[^>]*>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'fio', /Владелец:(?:[\s\S]*?<div[^>]*class="val"[^>]*>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /<th[^>]*>Эл-во(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<(?:br|\/td>)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Адрес:(?:[\s\S]*?<div[^>]*class="val"[^>]*>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'lastdate', /Оплаты(?:(?:[\s\S](?!<\/table>))*?<td[^>]*>){1}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
     getParam(html, result, 'lastsum', /Оплаты(?:(?:[\s\S](?!<\/table>))*?<td[^>]*>){2}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+
+    getParam(html, result, 'lastbill', /Начисления(?:[\s\S](?!<\/table>))*?Начислено:[\s\S]*?<div[^>]+class="val"[^>]*>([\S\s]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'lastbilldate', /Начисления(?:[\s\S](?!<\/table>))*?<td[^>]*>((?:[\s\S](?!<\/td>))*?.)<\/td>\s*<td[^>]*>(?:[\s\S](?!<\/td>))*?Начислено:/i, replaceTagsAndSpaces, parseDate);
+
+    var countersTable = getParam(html, null, null, /(<table[^>]*>(?:[\s\S](?!<\/table>))*?<th[^>]*>Тип<\/th>[\s\S]*?<\/table>)/i) || '';
+    if(!countersTable)
+        AnyBalance.trace('Не удалось найти таблицу счетчиков');
+    var counters = sumParam(countersTable, null, null, /<th[^>]*>([\s\S]*?)<\/th>/ig, replaceTagsAndSpaces, html_entity_decode);
+    for(var i=0; i<counters.length; ++i){
+        var name=counters[i];
+        //Просто верх мастерства - значения счетчиков в таблице по диагонали. Вот тупизм.
+        var re = new RegExp('(?:[\\s\\S]*?<tr[^>]*>){' + (i+1) + '}(?:[\\s\\S]*?<td[^>]*>){' + (i+1) + '}(?:[\\s\\S](?!</td>))*?\\d{4}:([\\S\\s]*?)</td>', 'i');
+        if(/Эл-во/i.test(name))
+            getParam(countersTable, result, 'lastcounter', re, replaceTagsAndSpaces, parseBalance);
+        else if(/ГВС/i.test(name))
+            getParam(countersTable, result, 'lastcounter_gvs', re, replaceTagsAndSpaces, parseBalance);
+        else if(/ХВС/i.test(name))
+            getParam(countersTable, result, 'lastcounter_hvs', re, replaceTagsAndSpaces, parseBalance);
+    }
 
     AnyBalance.setResult(result);
 }

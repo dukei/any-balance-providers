9f9e625cf39b06cf033e21d6f8d3d90cd1af5aa4
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-01-09 17:52:46 +0000

    Callbacker

---
+++ trunk/ab-telephony-callbacker/main.js
@@ -0,0 +1,39 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает текущий остаток и другие параметры у провайдера IP Callbacker
+
+Сайт оператора: http://callbacker.com
+Личный кабинет: https://customer.callbacker.com/cabinet/
+*/
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    var baseurl = "https://customer.callbacker.com/cabinet/";
+    AnyBalance.setDefaultCharset('utf-8');
+
+    AnyBalance.setCookie('customer.callbacker.com', 'ui_language', 'english');
+
+    var html = AnyBalance.requestPost(baseurl + 'userinfo.php', {
+        done:'submit_log',
+        pr_login:prefs.login,
+        pr_password:prefs.password,
+        submit:'Войти'
+    });
+
+    if(!/logout.php\?logout=true/i.test(html)){
+        var error = getParam(html, null, null, /<font[^>]+color="red"[^>]*>([\s\S]*?)<\/font>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неправильный логин-пароль?');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /Balance:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'phone', /Phone:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Account number:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}

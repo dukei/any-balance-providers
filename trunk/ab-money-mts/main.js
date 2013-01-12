a96a4701db93137a5629a3a3319a17b1d8d997ff
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-01-12 14:51:15 +0000

    Банки - исправлен sleep на более энергоэффективный

--- trunk/ab-money-mts/main.js
+++ trunk/ab-money-mts/main.js
@@ -62,11 +62,15 @@ function parseStatus(str){
 }
 
 function sleep(delay) {
-   var startTime = new Date();
-   var endTime = null;
-   do {
-       endTime = new Date();
-   } while (endTime.getTime() - startTime.getTime() < delay);
+   if(AnyBalance.getLevel() < 6){
+      var startTime = new Date();
+      var endTime = null;
+      do {
+          endTime = new Date();
+      } while (endTime.getTime() - startTime.getTime() < delay);
+   }else{
+      AnyBalance.sleep(delay);
+   }
 } 
 
 function encryptPass(pass, map){

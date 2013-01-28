d3058ad353d69d5a9d9485cf213d915e48ddcaa2
dmitry.kochin <dmitry.kochin@gmail.com>
2013-01-28 17:12:26 +0000

    CrazyPark - полностью изменен личный кабинет

--- ab-bonus-crazypark/main.js
+++ ab-bonus-crazypark/main.js
@@ -7,77 +7,52 @@
 Личный кабинет: http://www.crazypark.ru/club/index.php
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
+var g_headers = {
+	'Accept':'application/json, text/javascript, */*; q=0.01',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17',
+	'X-Requested-With':'XMLHttpRequest'
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
 
-    var baseurl = "http://www.crazypark.ru/club/";
+    var baseurl = "http://www.crazypark.ru/client/";
 
-    var html = AnyBalance.requestPost(baseurl + 'login.php', {
-        login:prefs.login,
-        password:prefs.password
-    });
+    var html = AnyBalance.requestGet(baseurl + 'login?id=' + encodeURIComponent(prefs.login) + '&password=' + encodeURIComponent(prefs.password), g_headers);
+
+    var json = getJson(html);
 
     //AnyBalance.trace(html);
-    if(!/login.php\?login=n/.test(html)){
-        var error = getParam(html, null, null, /<div[^>]*class=["']error[^>]*>([\s\S]*?)<\/div>/, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
+    if(!json.success){
+        if(json.error)
+            throw new AnyBalance.Error(json.error);
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
 
     var result = {success: true};
 
-    getParam(html, result, 'balance', /На твоей карте:([\s\S]*?)<br/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'bonuses', /Из них бонусов:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'crazies', /Крэйзики на карте:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'cardnum', /Номер карты:([^<]*)/i, replaceTagsAndSpaces);
-    getParam(html, result, 'level', /<div[^>]+class="status"[^>]*>[\S\s]*?url\((\d)_a\.gif\)/i, replaceTagsAndSpaces, parseInt);
-    getParam(html, result, 'next', /<span[^>]*>([^<]*)<\/span>\s*<br[^>]*>\s*до перехода/i, replaceTagsAndSpaces, parseBalance);
+    html = AnyBalance.requestGet(baseurl + 'index?id=' + encodeURIComponent(prefs.login), g_headers);
+    json = getJson(html);
 
-    var cn = getParam(html, null, null, /Номер карты:([\S\s]*?)<br/i, replaceTagsAndSpaces);
-    var level = getParam(html, null, null, /<div[^>]+class="status"[^>]*>[\S\s]*?url\((\d)_a\.gif\)/i, replaceTagsAndSpaces, parseInt);
+    if(!json.clients[0])
+        throw new AnyBalance.Error('Не найдена информация о клиенте! Сайт изменен?');
 
-    result.__tariff = level + ' уровень, №' + cn;
+    var client = json.clients[0];
+    if(AnyBalance.isAvailable('balance'))
+        result.balance = client.balance.CashBalance;
+    if(AnyBalance.isAvailable('bonuses'))
+        result.bonuses = client.balance.CashBonusBalance;
+    if(AnyBalance.isAvailable('crazies'))
+        result.crazies = client.balance.PointBalance;
+    if(AnyBalance.isAvailable('next'))
+        result.next = client.balance.CashToNextLevel;
+    if(AnyBalance.isAvailable('cardnum'))
+        result.cardnum = client.card.id;
+    if(AnyBalance.isAvailable('level'))
+        result.level = client.card.status;
+
+    result.__tariff = client.card.name + ', ' + client.card.status + ' ур., №' + client.card.id;
 
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

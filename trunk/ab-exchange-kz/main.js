a373cb69f6300ad5bdd4066e2f1b79f07c279459
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-01-28 16:19:38 +0000

    stikhilyas@gmail.com

---
+++ trunk/ab-exchange-kz/main.js
@@ -0,0 +1,22 @@
+﻿function getRate(result, item, code){
+        if($(item).find("title").text() == code && AnyBalance.isAvailable(code)){
+                       result[code] = $(item).find("description").text();
+         }
+
+} 
+	
+function main(){
+        AnyBalance.trace('Connecting to www.nationalbank.kz');        
+        var result = {success: true};          
+        var info = AnyBalance.requestGet('http://www.nationalbank.kz/rss/rates_all.xml');
+        var xmlDoc = $.parseXML(info), $xml = $(xmlDoc);
+        $xml.find("item").each(
+                function(){
+                   getRate(result,$(this),'USD');
+                   getRate(result,$(this),'EUR');
+                   getRate(result,$(this),'RUB');
+                }); 
+         AnyBalance.setResult(result);
+}
+
+

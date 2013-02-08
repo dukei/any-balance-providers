a8e3370dec6212c176d0010f045f4ac37ebce73d
dmitry.kochin <dmitry.kochin@gmail.com>
2013-02-08 12:38:33 +0000

    Profit-partners

---
+++ ab-affiliate-profit/main.js
@@ -0,0 +1,86 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает различные показатели в партнерской программе Profit-Partner.ru (http://profit-partner.ru).
+
+Сайт оператора: http://profit-partner.ru
+Личный кабинет: http://profit-partner.ru/stats/
+*/
+
+var g_baseurl = "http://profit-partner.ru/";
+var g_headers = {
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+        'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+        'Cache-Control':'max-age=0',
+        'Connection':'keep-alive',
+        'Referer':g_baseurl,
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17'
+};
+
+function dateToYMD(date)
+{
+    var d = date.getDate();
+    var m = date.getMonth()+1;
+    var y = date.getFullYear();
+    return '' + y + '-' + (m<=9?'0'+m:m) + '-' + (d<=9?'0'+d:d);
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = g_baseurl;
+
+    var html = AnyBalance.requestPost(baseurl + 'user/auth', {
+        login:prefs.login,
+        pwd:prefs.password,
+        save:0,
+        enter:'Войти'
+    }, g_headers);
+
+    //AnyBalance.trace(html);
+    if(!/\/user\/quit\//i.test(html)){
+        var error = getParam(html, null, null, /<div[^>]+class="error"(?:[^>](?!display:none))*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'today', /<td[^>]*>Сегодня[\s\S]*?<td[^>]*>([\s\S]*?)<\/a>\s*<\/td>/i, [/"[^"]*"/g, '', replaceTagsAndSpaces], parseBalance);
+    getParam(html, result, 'yesterday', /<td[^>]*>Вчера[\s\S]*?<td[^>]*>([\s\S]*?)<\/a>\s*<\/td>/i, [/"[^"]*"/g, '', replaceTagsAndSpaces], parseBalance);
+    getParam(html, result, 'month', /<td[^>]*>За 30 дней[\s\S]*?<td[^>]*>([\s\S]*?)<\/a>\s*<\/td>/i, [/"[^"]*"/g, '', replaceTagsAndSpaces], parseBalance);
+    getParam(html, result, 'estimate', /<td[^>]*>Прогноз дохода[\s\S]*?<td[^>]*>([\s\S]*?)<\/a>\s*<\/td>/i, [/"[^"]*"/g, '', replaceTagsAndSpaces], parseBalance);
+
+    if(AnyBalance.isAvailable('views','clicks','ctr','cpc','cpm','income')){
+        var now = new Date();
+        var startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+        var endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+        html = AnyBalance.requestPost(baseurl + 'stats/common/', {
+            form_reload:1,
+            area:'all',
+            period:dateToYMD(startOfMonth) + '~' + dateToYMD(endOfMonth),
+            from:'',
+            to:'',
+            step:'no',
+            splits:0,
+            split:'',
+            diff:0,
+            ok:'Показать',
+            splitid:'',
+            splitarea:'',
+            splitname:''
+        }, g_headers);
+
+        getParam(html, result, 'views', /Итого:(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'clicks', /Итого:(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'ctr', /Итого:(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'cpc', /Итого:(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'cpm', /Итого:(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'income', /Итого:(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    }
+    
+    AnyBalance.setResult(result);
+}

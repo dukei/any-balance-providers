19e008cc88276af7bdbcbfd896a08ab872d0ecc1
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-02-11 15:26:23 +0000

    Matrix mobile & typical provider

---
+++ trunk/ab-cell-matrixmobile/main.js
@@ -0,0 +1,69 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плате для сотового оператора MatrixMobile 
+
+Operator site: http://matrixmobile.ru
+Личный кабинет: https://matrixopen.ru/login
+*/
+
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    //Получаем в переменную заданные пользователем настройки
+    var prefs = AnyBalance.getPreferences();
+
+    //Лучше базовый url забить где-нибудь вначале в переменную, потом гораздо легче переделывать в другой провайдер
+    var baseurl = "https://matrixopen.ru/";
+
+    //Не забываем устанавливать кодировку по-умолчанию. Её можно узнать из заголовка Content-Type или из тела страницы в теге <meta> 
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    //В данном провайдере в форму передаётся "секретный" параметр, поэтому, прежде чем делать запрос, надо его получить
+    //Для этого сначала загружаем форму входа
+    var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+
+    //Находим секретный параметр
+    var tform = getParam(html, null, null, /<input[^>]+name="t:formdata"[^>]*value="([^"]*)/i, null, html_entity_decode);
+    if(!tform) //Если параметр не найден, то это, скорее всего, свидетельствует об изменении сайта или о проблемах с ним
+        throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+
+    //Теперь, когда секретный параметр есть, можно попытаться войти
+    html = AnyBalance.requestPost(baseurl + 'login.loginform', {
+        't:formdata':tform,      //Передаём все параметры, которые мы отследили в браузере в активности сети
+        baseNumber:prefs.login,
+        password:prefs.password,
+        confirmAgreement:'on'
+        //Данный сайт не проверяет заголовок Referer, но если заголовок требуется передать, то можно сделать это так
+        //В результате в запрос уйдут все заголовки из g_headers и Referer
+    }, addHeaders({Referer: baseurl + 'login'})); 
+
+    //После входа обязательно проверяем маркер успешного входа
+    //Обычно это ссылка на выход, хотя иногда приходится искать что-то ещё
+    if(!/\/Logout/i.test(html)){
+        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
+        var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    //Раз мы здесь, то мы успешно вошли в кабинет
+    //Получаем все счетчики
+    var result = {success: true};
+    getParam(html, result, 'fio', /Имя(?:\s|&nbsp;)*абонента:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'phone', /Номер:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Текущий баланс:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'status', /Статус:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    //Возвращаем результат
+    AnyBalance.setResult(result);
+}
---
+++ trunk/extra/development/timeparse/readme.txt
@@ -0,0 +1,26 @@
+﻿Чтобы распознавать в провайдере дату в почти любом формате, требуется включить файлы
+
+moment.min.js
+moment.lang.ru.min.js
+moment.lang.uk.js
+
+(Ненужные языки можно не включать).
+
+Затем добавить следующую функцию в main.js, при этом можно настроить форматы даты:
+
+function parseDateMoment(str){
+    var mom = moment(str.replace(/i/ig, 'і'), ['DD MMM YYYY', 'HH:mm-D MMM YYYY']);
+    if(!mom.isValid()){
+        AnyBalance.trace('Failed to parse date from ' + str);
+    }else{
+        var val = mom.toDate();
+        AnyBalance.trace('Parsed date ' + val + ' from ' + str);
+        return val.getTime();
+    }
+}
+
+И, наконец, надо выбрать нужный язык где-нибудь в начале main():
+
+moment.lang('uk');
+
+Полная информация по moment.js здесь: http://momentjs.com/

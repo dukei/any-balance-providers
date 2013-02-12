e9b79b53b9565cd1275a9743e0d8bf979713f272
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-02-12 15:51:26 +0000

    Wellcom - добавлен счетчик остатка трафика

--- trunk/ab-cell-wellcom/main.js
+++ trunk/ab-cell-wellcom/main.js
@@ -7,41 +7,6 @@
 Личный кабинет: https://issa-tomsk.bwc.ru
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseTrafficGb(str){
-  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-  return parseFloat((val/1024).toFixed(2));
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
@@ -77,6 +42,13 @@ function main(){
     getParam(html, result, 'licschet', /Лицевой счет\s*-([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'phone', /Номер телефона\s*-([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 
+    var packets = getParam(html, null, null, /<h2[^>]*>\s*Общая продолжительность разговоров[\s\S]*?<table[^>]*>([\s\S]*?)<\/table>/i);
+    if(packets){
+    	sumParam(packets, result, 'traffic_left', /<tr[^>]*>(?:(?:[\s\S](?!<\/tr))*?<td[^>]*>){2}[^<]*\d+ [МГ]Б(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, [/\(мегабайт\)/i, 'мб', /\(гигабайт\)/i, 'гб', replaceTagsAndSpaces], parseTraffic, aggregate_sum);
+    }else{
+        AnyBalance.trace('Не удалось получить таблицу доп. услуг');
+    }
+
     html = AnyBalance.requestGet(baseurl + "?function=is_tarif&inf=3");
 
     getParam(html, result, 'status', /Текущее состояние[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -84,12 +56,3 @@ function main(){
     
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

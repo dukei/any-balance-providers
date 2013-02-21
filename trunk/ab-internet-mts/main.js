963d00f59ff727762cd85adc5221dad7c2df02dc
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-02-21 11:08:09 +0000

    МТС и Пенза-телеком

--- trunk/ab-internet-mts/main.js
+++ trunk/ab-internet-mts/main.js
@@ -22,7 +22,8 @@ var regions = {
    nnov: getNnov,
    sdv: getSdv,
    vlgd: getVologda,
-   izh: getIzhevsk
+   izh: getIzhevsk,
+   pnz: getPnz
 };
 
 function main(){
@@ -362,6 +363,51 @@ function getKrv(){
             continue;
         tariffs[tariffs.length] = tariff;
         getParam(services[i], result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
+    }
+    
+    if(tariffs.length > 0)
+        result.__tariff = tariffs.join(', ');
+
+    getParam(html, result, '__tariff', /Текущий тарифный план:([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'username', /Общая информация\s+\/([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}
+
+function getPnz(){
+    var prefs = AnyBalance.getPreferences();
+
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "http://client.lanbilling.ptcomm.ru/index.php?";
+
+    var html = AnyBalance.requestPost(baseurl + 'r=site/login', {
+        'LoginForm[login]':prefs.login,
+        yt0:'Войти',
+        'LoginForm[password]':prefs.password
+    });
+
+    //AnyBalance.trace(html);
+
+    if(!/\?r=site\/logout/i.test(html)){
+        throw new AnyBalance.Error("Не удалось войти в личный кабинет. Неправильный логин-пароль?");
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'agreement', /Номер договора:([\s\S]*?)<a/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Текущий баланс:([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance2);
+
+    //Тут услуги в таблице, пройдемся по ней и возьмем тарифные планы
+    var services = sumParam(html, null, null, /(<tr[^>]*>\s*<td[^>]+class="first_col"[^>]*>(?:[\s\S](?!\/tr))*<\/tr>)/ig);
+    var tariffs = [];
+    for(var i=0; i<services.length; ++i){
+        var tariff = getParam(services[i], null, null, /(?:[\s\S]*?<td[^>]*>){2}\s*<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(tariff == 'Услуги')
+            continue;
+        tariffs[tariffs.length] = tariff;
+        getParam(services[i], result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
+//        getParam(services[i], result, 'status', /Состояние:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
     }
     
     if(tariffs.length > 0)
--- trunk/ab-internet-ptcomm/main.js
+++ trunk/ab-internet-ptcomm/main.js
@@ -1,56 +1,53 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-Текущий баланс у пензенского интернет-провайдера Пенза Телеком
+Текущий баланс у пензенского интернет-провайдера Пенза Телеком (Ныне МТС уже)
 
 Сайт оператора: http://www.ptcomm.ru/
-Личный кабинет: https://billing.ptcomm.ru
+Личный кабинет: http://client.lanbilling.ptcomm.ru/
 */
 
 function main(){
     var prefs = AnyBalance.getPreferences();
 
-    AnyBalance.setDefaultCharset('koi8-r');
+    AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "https://billing.ptcomm.ru/cgi-bin/";
+    var baseurl = "http://client.lanbilling.ptcomm.ru/index.php?";
 
-    var html = AnyBalance.requestPost(baseurl + 'net/login', {
-        action:'validate',
-        login:prefs.login,
-        password:prefs.password
+    var html = AnyBalance.requestPost(baseurl + 'r=site/login', {
+        'LoginForm[login]':prefs.login,
+        yt0:'Войти',
+        'LoginForm[password]':prefs.password
     });
 
     //AnyBalance.trace(html);
 
-    if(!/logout/i.test(html)){
-        var error = getParam(html, null, null, /<p[^>]+color:\s*red[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        
-        throw new AnyBalance.Error("Не удалось войти в личный кабинет. Личный кабинет изменился или проблемы на сайте.");
+    if(!/\?r=site\/logout/i.test(html)){
+        throw new AnyBalance.Error("Не удалось войти в личный кабинет. Неправильный логин-пароль?");
     }
 
     var result = {success: true};
 
-    getParam(html, result, 'balance', /Сумма на счету[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, '__tariff', /<th[^>]*>\s*Тариф(?:[\s\S]*?<td[^>]*>){4}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'status', /Статус счёта[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'licschet', /Аккаунт[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'agreement', /Номер вашего договора &#0171;([\S\s]*?)&#0187;/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'agreement', /Номер договора:([\s\S]*?)<a/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Текущий баланс:([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance);
 
-    if(AnyBalance.isAvailable('trafficIn','trafficOut','trafficInNight','trafficOutNight')){
-        var servicepart = getParam(html, null, null, /(deal_service\?session_id=[^&]+&service_id=\d+)/i, null, html_entity_decode);
-        if(!servicepart){
-            AnyBalance.trace('Не удалось найти ссылку на услугу интернет');
-        }else{
-            var now = new Date();
-            html = AnyBalance.requestGet(baseurl + "net/" + servicepart + '&start_y=' + now.getFullYear() + '&start_m=' + (now.getMonth()+1) + '&leased=%F0%D2%CF%D3%CD%CF%D4%D2%C5%D4%D8+%D4%D2%C1%C6%C9%CB-%D3%C5%D3%D3%C9%C9');
-            getParam(html, result, 'trafficIn', /Суммарный трафик[\s\S]*?Входящий внешний трафик\s*<(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-            getParam(html, result, 'trafficOut', /Суммарный трафик[\s\S]*?Исходящий внешний трафик\s*<(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-            getParam(html, result, 'trafficInNight', /Суммарный трафик[\s\S]*?Входящий внешний трафик \(ночной\)(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-            getParam(html, result, 'trafficOutNight', /Суммарный трафик[\s\S]*?Исходящий внешний трафик \(ночной\)(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        }
+    //Тут услуги в таблице, пройдемся по ней и возьмем тарифные планы
+    var services = sumParam(html, null, null, /(<tr[^>]*>\s*<td[^>]+class="first_col"[^>]*>(?:[\s\S](?!\/tr))*<\/tr>)/ig);
+    var tariffs = [];
+    for(var i=0; i<services.length; ++i){
+        var tariff = getParam(services[i], null, null, /(?:[\s\S]*?<td[^>]*>){2}\s*<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(tariff == 'Услуги')
+            continue;
+        tariffs[tariffs.length] = tariff;
+        getParam(services[i], result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+        getParam(services[i], result, 'status', /Состояние:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
     }
+    
+    if(tariffs.length > 0)
+        result.__tariff = tariffs.join(', ');
+
+    getParam(html, result, '__tariff', /Текущий тарифный план:([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'username', /Общая информация\s+\/([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 
     AnyBalance.setResult(result);
 }

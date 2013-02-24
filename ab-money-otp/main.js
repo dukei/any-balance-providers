619ca546665fcb952038fc9dcf7c9c51bc5fbf2e
dmitry.kochin <dmitry.kochin@gmail.com>
2013-02-24 12:43:12 +0000

    ОТП банк - изменения в лк

--- ab-money-otp/main.js
+++ ab-money-otp/main.js
@@ -7,56 +7,6 @@
 Личный кабинет: https://direkt.otpbank.ru/
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp ? regexp.exec (html) : html;
-	if (value) {
-                if(regexp)
-		    value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.', /(\d)\-(\d)/g, '$1.$2'];
-
-function parseBalance(text){
-    var _text = text.replace(/\s+/g, '');
-    var val = getParam(_text, null, null, /(-?\d[\d\.,\-]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseCurrency(text){
-    var _text = text.replace(/\s+/g, '');
-    var val = getParam(_text, null, null, /[\d\.,\-]+(\S*)/);
-    AnyBalance.trace('Parsing currency (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseDate(str){
-    var matches = /(\d+)[^\d](\d+)[^\d](\d+)/.exec(str);
-    var time;
-    if(matches){
-	  time = (new Date(+matches[3], matches[2]-1, +matches[1])).getTime();
-    }
-    AnyBalance.trace('Parsing date ' + new Date(time) + ' from value: ' + str);
-    return time;
-}
-
 function sleep(delay) {
    if(AnyBalance.getLevel() < 6){
       var startTime = new Date();
@@ -68,19 +18,6 @@ function sleep(delay) {
       AnyBalance.sleep(delay);
    }
 } 
-
-function createFormParams(html, process){
-    var params = {};
-    html.replace(/<input[^>]+name="([^"]*)"[^>]*>/ig, function(str, name){
-        var value = getParam(str, null, null, /value="([^"]*)"/i, null, html_entity_decode);
-        name = html_entity_decode(name);
-        if(process){
-            value = process(params, str, name, value);
-        }
-        params[name] = value;
-    });
-    return params;
-}
 
 function waitForTransaction(url){
     do{
@@ -107,10 +44,6 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset("utf-8");
     
-    var what = prefs.what || 'card';
-    if(prefs.num && !/\d{4}/.test(prefs.num))
-        throw new AnyBalance.Error("Введите 4 последних цифры номера карты или не вводите ничего, чтобы показать информацию по первой карте");
-
     var html = AnyBalance.requestPost(baseurl + "bejelentkezesJelszoalapu", {
         azonosito:prefs.login,
         jelszo:prefs.password,
@@ -142,12 +75,26 @@ function main(){
 
     var cards = getParam(html, null, null, /<form[^>]+name="bankszamlaMuveletForm"[^>]*>([\s\S]*?)<\/form>/i);
     if(!cards)
-        throw new AnyBalance.Error('Не удалось найти таблицу карт!');
+        throw new AnyBalance.Error('Не удалось найти таблицу карт и счетов!');
+
+    if(prefs.type == 'card')
+        fetchCard(cards);
+    else if(prefs.type == 'acc')
+        fetchAccount(cards);
+    else
+        fetchCard(cards);
+}
+
+function fetchCard(cards){
+    var prefs = AnyBalance.getPreferences();
+
+    if(prefs.num && !/\d{4}/.test(prefs.num))
+        throw new AnyBalance.Error("Введите 4 последних цифры номера карты или не вводите ничего, чтобы показать информацию по первой карте");
 
     var $cards = $('<div>' + cards + '</div>');
     var re = prefs.num ? new RegExp('(\\d{4}\\s*\\*{4}\\s*\\*{4}\\s*' + prefs.num + ')') : /(\d{4}\s*\*{4}\s*\*{4}\s*\d{4})/;
 
-    var $card = $cards.find('table#nagyTabla>tbody>tr').filter(function(index){
+    var $card = $cards.find('table.nagyTabla>tbody>tr').filter(function(index){
         return re.test($(this).text());
     }).first();
 
@@ -156,19 +103,20 @@ function main(){
 
     var result = {success: true};
     
-    getParam($card.text(), result, 'cardnum', re, replaceTagsAndSpaces);
-    getParam($card.find('td:nth-child(2)').first().text(), result, 'cardname', null, replaceTagsAndSpaces);
-    getParam($card.find('td:nth-child(4)').first().text(), result, 'type', null, replaceTagsAndSpaces);
+    getParam($card.text(), result, 'cardnum', re, replaceTagsAndSpaces, html_entity_decode);
+    getParam($card.find('td:nth-child(3)').first().text(), result, 'cardname', null, replaceTagsAndSpaces, html_entity_decode);
+    getParam($card.find('td:nth-child(3)').first().text(), result, '__tariff', null, replaceTagsAndSpaces, html_entity_decode);
+    getParam($card.find('td:nth-child(2)').first().text(), result, 'type', null, replaceTagsAndSpaces, html_entity_decode);
     getParam($card.find('td:nth-child(5)').first().text(), result, 'balance', null, replaceTagsAndSpaces, parseBalance);
-    getParam($card.find('td:nth-child(5)').first().text(), result, 'currency', null, replaceTagsAndSpaces, parseCurrency);
+    getParam($card.find('td:nth-child(5)').first().text(), result, ['currency', 'balance', 'minpay', 'limit', 'gracepay', 'debt', 'own'], null, replaceTagsAndSpaces, parseCurrency);
     
     if(AnyBalance.isAvailable('till','own','debt','minpay','minpaytill','gracepay','gracetill','limit')){
-        var href = $card.find('td:nth-child(2) a').first().attr('href');
+        var href = $card.find('td:nth-child(3) a').first().attr('href');
         href = getParam(href, null, null, /\/homebank\/do\/(.*)/i);
         if(!href)
             throw new AnyBalance.Error('Не удалось найти ссылку на детальную информацию по карте. Обратитесь к автору провайдера');
-        html = AnyBalance.requestGet(baseurl + href, g_headers);
-            
+        var html = AnyBalance.requestGet(baseurl + href, g_headers);
+    
         var form = getParam(html, null, null, /(<form[^>]+name="torzsadatLekerdezesForm"[\s\S]*?<\/form>)/i);
         if(!form)
             throw new AnyBalance.Error("Не удалось найти форму запроса детальной информации по карте. Обратитесь к автору провайдера.");
@@ -185,7 +133,7 @@ function main(){
         getParam(html, result, 'minpay', /Минимальный платеж[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
         getParam(html, result, 'minpaytill', /Дата очередного платежа[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
         getParam(html, result, 'gracepay', /Платеж льготного периода[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'gracetill', /Дата вхождения в льготный период[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+        getParam(html, result, 'gracetill', /(?:Дата вхождения в льготный период|Дата окончания льготного периода)[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
         getParam(html, result, 'limit', /Кредитный лимит[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
 
     }
@@ -193,11 +141,34 @@ function main(){
     AnyBalance.setResult(result);
 }
 
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
+function fetchAccount(accounts){
+    var prefs = AnyBalance.getPreferences();
 
+    if(prefs.num && !/\d{4,20}/.test(prefs.num))
+        throw new AnyBalance.Error("Введите от 4 последних цифр номера счета или не вводите ничего, чтобы показать информацию по первому счету");
+
+    //Сколько цифр осталось, чтобы дополнить до 20
+    var accnum = prefs.cardnum || '';
+    var accprefix = accnum.length;
+    accprefix = 20 - accprefix;
+
+    var $cards = $('<div>' + accounts + '</div>');
+    var re = new RegExp('(' + (accprefix > 0 ? '\\d{' + accprefix + '}' : '') + accnum + ')', 'i');
+
+    var $card = $cards.find('table.nagyTabla>tbody>tr').filter(function(index){
+        return re.test($(this).text());
+    }).first();
+
+    if(!$card.size())
+        throw new AnyBalance.Error('Не удалось найти ' + (prefs.num ? 'счет с последними цифрами ' + prefs.num : 'ни одного счета'));
+
+    var result = {success: true};
+    
+    getParam($card.text(), result, 'cardnum', re, replaceTagsAndSpaces, html_entity_decode);
+    getParam($card.find('td:nth-child(2)').first().text(), result, 'cardname', null, replaceTagsAndSpaces, html_entity_decode);
+    getParam($card.find('td:nth-child(2)').first().text(), result, '__tariff', null, replaceTagsAndSpaces, html_entity_decode);
+    getParam($card.find('td:nth-child(4)').first().text(), result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+    getParam($card.find('td:nth-child(4)').first().text(), result, ['currency', 'balance'], null, replaceTagsAndSpaces, parseCurrency);
+    
+    AnyBalance.setResult(result);
+}

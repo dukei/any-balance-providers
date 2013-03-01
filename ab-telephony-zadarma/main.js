449a09e487ef5533a8d982db8c86c175157d3e9d
dmitry.kochin <dmitry.kochin@gmail.com>
2013-03-01 06:51:48 +0000

    Задарма - отображение прямых номеров

--- ab-telephony-zadarma/main.js
+++ ab-telephony-zadarma/main.js
@@ -12,13 +12,14 @@ function main(){
 	var prefs = AnyBalance.getPreferences();
     
 	AnyBalance.setDefaultCharset('utf-8');
+        var baseurl = "https://ss.zadarma.com/";
 
-	var auth = AnyBalance.requestPost("https://ss.zadarma.com/auth/login/", {
+	var auth = AnyBalance.requestPost(baseurl + "auth/login/", {
 		email: prefs.login,
 		password: prefs.password
 	});
 	
-	var info = AnyBalance.requestGet("https://ss.zadarma.com");
+	var info = AnyBalance.requestGet(baseurl);
 
         if(!/\/auth\/logout\//i.test(info)){
             var error = getParam(auth, null, null, /<p[^>]+class="error"[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -27,53 +28,19 @@ function main(){
             throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
         }
 
-	var balance = info.match(/<span class="balance">.*\$(-?\d+[\.,]\d+).*<\/span>/i);
-	var tariff = info.match(/<p><strong>(.*)<\/strong>( \((?:стоимость|вартість|cost) \d+\.\d+.*\))<\/p>/i);
+	var result = {success: true};
+        getParam(info, result, 'balance', /<span class="balance">.*\$(-?\d+[\.,]\d+).*<\/span>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(info, result, '__tariff', /<p><strong>(.*)<\/strong>( \((?:стоимость|вартість|cost) \d+\.\d+.*\))<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
 
-	if (balance) {
-		var result = {success: true};
-		result.balance = parseFloat(balance[1].replace(/,/g, '.'));
-		if (tariff) {
-        	    result.__tariff = tariff[1] + tariff[2];
-		}
-		AnyBalance.setResult(result);
-	} else {
-		throw new AnyBalance.Error("Не удалось получить текущий баланс");
-	}
+        if(AnyBalance.isAvailable('phone0', 'phone0till', 'phone1', 'phone1till', 'phone2', 'phone2till')){
+		info = AnyBalance.requestGet(baseurl + 'dirnum/');
+                var numbers = sumParam(info, null, null, /<h2[^>]*>(?:Бесплатный|Безкоштовний|Free)(?:[^<](?!c донабором|з донабором|with dtmf))*<\/h2>[\s\S]*?(?:<h2|Номер действителен|Номер діє|The number works)/ig);
+                for(var i=0; i<Math.min(numbers.length, 3); ++i){
+                    getParam(numbers[i], result, 'phone' + i, /(?:Вам подключен прямой номер|Вам надано прямий номер|Your connected number|Вам підключено прямий номер):[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+                    getParam(numbers[i], result, 'phone' + i + 'till', /(?:Действует до|Діє до|Valid till)([^<]*)\./i, replaceTagsAndSpaces, parseDateISO);
+                }
+        }
+
+        AnyBalance.setResult(result);
 	
 }
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp ? regexp.exec (html) : html;
-	if (value) {
-                if(regexp)
-		    value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/ig, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.', /(\d)\-(\d)/g, '$1.$2'];
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

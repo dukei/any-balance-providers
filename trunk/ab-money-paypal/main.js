abece5a021d85550fb3a74426929fccd808ad7b4
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-03-01 04:49:04 +0000

    Пейпал - изменения в лк

--- trunk/ab-money-paypal/main.js
+++ trunk/ab-money-paypal/main.js
@@ -7,37 +7,6 @@
 Личный кабинет: https://www.paypal.com/ru
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
 
@@ -50,13 +19,13 @@ function main(){
 
     var html = AnyBalance.requestGet(baseurl);
 
-    var href = getParam(html, null, null, /<form[^>]*action="([^"]+)"[^>]*name="login_form"/i);
-    var csrfModel = getParam(html, null, null, /<input[^>]*name="csrfModel.returnedCsrf"[^>]*value="([^"]+)"/i);
-    if(!href || !csrfModel)
+    var href = getParam(html, null, null, /<form[^>]*action="([^"]+)"[^>]*name="login_form"/i, null, html_entity_decode);
+//    var csrfModel = getParam(html, null, null, /<input[^>]*name="csrfModel.returnedCsrf"[^>]*value="([^"]+)"/i);
+    if(!href /*|| !csrfModel*/)
         throw new AnyBalance.Error("Can not find login form! Site is changed?");
 
     html = AnyBalance.requestPost(href, {
-      "csrfModel.returnedCsrf": csrfModel,
+//      "csrfModel.returnedCsrf": csrfModel,
       login_email: prefs.login,
       login_password:prefs.password,
       "submit.x": "Log in"

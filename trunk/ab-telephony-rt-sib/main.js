daa4a76aa6a1963065d2c9d0edea0dc8ccba48fb
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-03-03 14:53:57 +0000

    Ростелеком Сибирь

---
+++ trunk/ab-telephony-rt-sib/main.js
@@ -0,0 +1,60 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плате для сибирского филиала ростелеком
+
+Operator site: http://sibirtelecom.ru
+Личный кабинет: https://ispa.sibirtelecom.ru
+*/
+
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "https://ispa.sibirtelecom.ru/";
+    AnyBalance.setDefaultCharset('utf-8'); 
+   
+//    return getRegions();
+
+    var html = AnyBalance.requestPost(baseurl + 'index.jsp', {
+        needauth:'yes',
+        asr:prefs.region,
+        account:prefs.login,
+        pwd:prefs.password
+    }, addHeaders({Referer: baseurl + 'index.jsp'})); 
+
+    if(!/action=logout/i.test(html)){
+        var error = getParam(html, null, null, /<font[^>]+color="red"[^>]*>([\s\S]*?)<\/font>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    var result = {success: true};
+    getParam(html, result, 'licschet', /Лицевой счет №:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'fio', /Лицевой счет №:(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    sumParam(html, result, '__tariff', /<th[^>]*>[^<]*\/([^<]*)\//ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
+    sumParam(html, result, 'balance', /Сумма к оплате на\s*[\d\/]*\s*составляет:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+    sumParam(html, result, 'recom', /Сумма рекомендованного платежа[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+
+    AnyBalance.setResult(result);
+}
+
+function getRegions(result){
+    var html = AnyBalance.requestGet('https://ispa.sibirtelecom.ru/index.jsp');
+    var select = getParam(html, null,null, /<select[^>]+name='asr'[^>]*>([\s\S]*?)<\/select>/i);
+    var result = {success: true};
+    if(select){
+        sumParam(select, result, 'entries', /<option[^>]*>(?:Испа, )?([\s\S]*?)<\/option>/ig, replaceTagsAndSpaces, html_entity_decode, create_aggregate_join('|'));
+        sumParam(select, result, 'entryValues', /<option[^>]*value='([^']*)/ig, replaceTagsAndSpaces, html_entity_decode, create_aggregate_join('|'));
+    }
+    AnyBalance.setResult(result);
+
+}

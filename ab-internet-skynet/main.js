a8f63116d395ad5fb7a20aa9543e1c7724947fb5
dmitry.kochin <dmitry.kochin@gmail.com>
2013-03-05 18:03:50 +0000

    Скайнет (питер)

---
+++ ab-internet-skynet/main.js
@@ -0,0 +1,53 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Текущий баланс у питерского оператора интернет SkyNet.
+
+Сайт оператора: http://www.sknt.ru/
+Личный кабинет: http://bill.sknt.ru
+*/
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('windows-1251');
+
+    var baseurl = "http://bill.sknt.ru/";
+
+    var html = AnyBalance.requestPost(baseurl, {
+        login:prefs.login,
+        password:prefs.password,
+        x:41,
+        y:16,
+        action:'authorize'
+    });
+
+    if(!/action=auth_exit/i.test(html)){
+        var error = getParam(html, null, null, /div[^>]+class="error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode); 
+        if(error){
+            throw new AnyBalance.Error(error);
+        }
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    var result = {success: true};
+
+    html = AnyBalance.requestGet(baseurl + '?cat=bl_user');
+    getParam(html, result, 'fio', /ФИО:\s*<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    var table = getParam(html, null, null, /<div[^>]*class="st_tc"[^>]*>\s*<table[^>]*>([\s\S]*?)<\/table>/i);
+    if(table){
+        sumParam(table, result, '__tariff', /<tr[^>]*>(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
+    }
+     
+    getParam(html, result, 'licschet', /Ваш ID:([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Состояние лицевого счета:([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+
+    if(AnyBalance.isAvailable('trafficIn', 'trafficOut')){
+        html = AnyBalance.requestGet(baseurl + '?cat=bl_user_t');
+
+        getParam(html, result, 'trafficIn', /Итого\s*:(?:[\s\S]*?<th[^>]*>){1}([\s\S]*?)<\/th>/i, replaceTagsAndSpaces, parseTrafficGb);
+        getParam(html, result, 'trafficOut', /Итого\s*:(?:[\s\S]*?<th[^>]*>){2}([\s\S]*?)<\/th>/i, replaceTagsAndSpaces, parseTrafficGb);
+    }
+
+    AnyBalance.setResult(result);
+}

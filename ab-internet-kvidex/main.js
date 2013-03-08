c26792f056c382cd70205c52a19fac42ca724b41
dmitry.kochin <dmitry.kochin@gmail.com>
2013-03-08 06:33:46 +0000

    Квидекс - исправление получения трафика

--- ab-internet-kvidex/main.js
+++ ab-internet-kvidex/main.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
 Текущий баланс у московского интернет-провайдера Kvidex.
@@ -90,8 +90,8 @@ function main(){
     getParam(html, result, 'status', /<strong>Интернет<\/strong>[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces);
     getParam(html, result, '__tariff', /<strong>Тарифный план<\/strong>[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces);
     getParam(html, result, 'abon', /<strong>Стоимость<\/strong>[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'trafficIn', /<strong>Входящий<\/strong>[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
-    getParam(html, result, 'trafficOut', /<strong>Исходящий<\/strong>[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
+    getParam(html, result, 'trafficIn', /<strong>Входящий трафик<\/strong>[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
+    getParam(html, result, 'trafficOut', /<strong>Исходящий трафик<\/strong>[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
 
     AnyBalance.setResult(result);
 }

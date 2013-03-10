3cec3b930950c028aa6a714de16726b1aab455cf
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-03-10 17:43:03 +0000

    Innovative Securities

---
+++ trunk/ab-affiliate-innovative-securities/main.js
@@ -0,0 +1,43 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Текущий баланс у Innovative Securities
+
+Сайт оператора: https://www.innovativesecurities.com
+Личный кабинет: https://www.innovativesecurities.com
+*/
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = "https://www.innovativesecurities.com/";
+
+    var html = AnyBalance.requestPost(baseurl + 'loginclient.php', {
+	username:prefs.login,
+	password:prefs.password,
+	submit:''
+    });
+
+    if(!/logout.php/i.test(html)){
+        var error = getParam(html, null, null, /<div[^>]+class=["']errorbox["'][^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode); 
+        if(error){
+            throw new AnyBalance.Error(error);
+        }
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    html = AnyBalance.requestGet(baseurl + 'details.php');
+    
+    var result = {success: true};
+    getParam(html, result, 'fio', /(?:Владелец счёта|Account holder|Kontobesitzer):[\s\S]*?<font[^>]*>([\s\S]*?)<\/font>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /(?:Продукт|Product|Produkt):([\s\S]*?)<img/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'acc', /(?:Номер счёта|Account number|Kontonummer):([\s\S]*?)<img/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(html, result, 'balance', /(?:Баланс инвестиции|Investment Balance|Investitionssaldo):[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'rs', /(?:Расчётный счёт|Charge Balance|Verrechnungskonto):[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'credit', /(?:Кредитный баланс|Credit Balance|Kreditsaldo):[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'avail', /(?:Доступный баланс для транзакции|Available Balance for Transaction|Verfügbarer Saldo für Transaktion):[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+
+    AnyBalance.setResult(result);
+}

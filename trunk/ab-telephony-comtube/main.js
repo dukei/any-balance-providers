df066d867774ba7708f03307a3c45fb3c2d2f1c7
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-03-22 14:00:28 +0000

    Комтьюб - изменения в личном кабинете

--- trunk/ab-telephony-comtube/main.js
+++ trunk/ab-telephony-comtube/main.js
@@ -6,45 +6,6 @@ ComTube IP-телефония
 Личный кабинет: https://www.comtube.com/
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/i, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');    
@@ -66,8 +27,10 @@ function main(){
         success: true
     };
 
-    getParam(info, result, 'balance', /login_balance_label[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(info, result, 'currency', /login_balance_label[^>]*>[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    var pset = getParam(info, null, null, /_pset\s*=\s*(\{[\s\S]*?\});/, null, getJson);
+
+    getParam(pset.curr_balance, result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+    getParam(pset.currency, result, ['currency', 'balance'], null, replaceTagsAndSpaces);
     getParam(info, result, 'number', /(?:Ваш номер внутри|You number in) Comtube:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
 		
     AnyBalance.setResult(result);

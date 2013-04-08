b79412999f10d96755908b166cd6bbf92413a9bd
dmitry.kochin <dmitry.kochin@gmail.com>
2013-04-08 16:29:13 +0000

    Ивантеевские телекоммуникации

---
+++ ab-internet-itkm/main.js
@@ -0,0 +1,58 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плате для сотового оператора xxxxxx 
+
+Operator site: http://xxxxxx.ru
+Личный кабинет: https://kabinet.xxxxxx.ru/login
+*/
+
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "https://stat.itkm.su/";
+
+    AnyBalance.setDefaultCharset('koi8-r');
+
+    var dt = new Date();
+
+    var html = AnyBalance.requestPost(baseurl, {
+	user:prefs.login,
+	passwd:prefs.password,
+	BeginDay:dt.getDate(),
+	BeginMonth:dt.getMonth()+1,
+	BeginYear:dt.getFullYear(),
+	EndDay:dt.getDate(),
+	EndMonth:dt.getMonth()+1,
+	EndYear:dt.getFullYear()
+    }, addHeaders({Referer: baseurl})); 
+
+    if(!/\?LogOut=1/i.test(html)){
+        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
+        var error = getParam(html, null, null, /<h3[^>]*>([\s\S]*?)<\/h3>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    //Раз мы здесь, то мы успешно вошли в кабинет
+    //Получаем все счетчики
+    var result = {success: true};
+    getParam(html, result, 'login', /Пользователь[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Тарифный план[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'trafficTotal', /Предоплаченный трафик[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+    getParam(html, result, 'balance', /Осталось на счету[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'trafficIn', /Входящий трафик[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+    getParam(html, result, 'trafficOut', /Исходящий трафик[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+
+    //Возвращаем результат
+    AnyBalance.setResult(result);
+}

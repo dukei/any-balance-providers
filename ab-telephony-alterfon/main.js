01fa16191fb9b12934ce2279c9935dfee163ceb3
dmitry.kochin <dmitry.kochin@gmail.com>
2013-04-08 09:20:27 +0000

    Альтерфон

---
+++ ab-telephony-alterfon/main.js
@@ -0,0 +1,47 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плате для провайдера IP-телефонии АЛЬТЕРФОН
+
+Operator site: http://www.alterfon.ru/
+Личный кабинет: https://my.alterfon.ru
+*/
+
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "https://my.alterfon.ru/";
+
+    AnyBalance.setDefaultCharset('windows-1251'); 
+
+    var html = AnyBalance.requestPost(baseurl + 'main.php', {
+	UserName:prefs.login,
+	PWDD:prefs.password
+    }, addHeaders({Referer: baseurl})); 
+
+    if(!/\?parm=exit/i.test(html) || /<input[^>]+name="UserName"/i.test(html)){
+        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
+        var error = getParam(html, null, null, /<font[^>]+color="?red"?[^>]*>([\s\S]*?)<\/font>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    //Раз мы здесь, то мы успешно вошли в кабинет
+    //Получаем все счетчики
+    var result = {success: true};
+    getParam(html, result, 'balance', /Баланс:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, '__tariff', /Аккаунт:([^<]*?)(?:Баланс:|<)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /Аккаунт:([^<]*?)(?:Баланс:|<)/i, replaceTagsAndSpaces, html_entity_decode);
+
+    //Возвращаем результат
+    AnyBalance.setResult(result);
+}

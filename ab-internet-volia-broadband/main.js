c3a43b82195c0906dd42ac3f195b454bca61a2e8
igoloviznin <igoloviznin@gmail.com>
2013-04-17 14:00:35 +0000

    Воля. Иправлены/добавлены счетчики по трафику. Улучшено отображение тарифного плана.

--- ab-internet-volia-broadband/main.js
+++ ab-internet-volia-broadband/main.js
@@ -30,20 +30,52 @@ function main(){
 		var result = {success: true};
 		var v = JSON.parse(info);
 		var resst = v.responseStatus.responseValue;
+		var tariff_type = prefs.tariff_type || '3';
 		if (resst == 'ok'){
 			result.__tariff = v.abonent.fullname;
                         if(AnyBalance.isAvailable('fio'))
                             result.fio = v.abonent.fullname;
                         var service = findGoodService(v.services);
-                        if(service && service.model)
-                            result.__tariff = ((service.name && service.code != 'inet') ? service.name + ': ' + service.model : service.model);
+                        //if(service && service.model) //Старое отображение тарифа
+                        //    result.__tariff = ((service.name && service.code != 'inet') ? service.name + ': ' + service.model : service.model);
+			if(v.services[tariff_type])
+                            result.__tariff = v.services[tariff_type].name + ': ' + v.services[tariff_type].model;
+			//Баланс
 			if(AnyBalance.isAvailable('balance') && v.services[0]){
                                 AnyBalance.trace('balance is ' + v.services[0].saldo + ', ' + typeof(v.services[0].saldo));
 				result['balance'] = v.services[0].saldo;
 			}
-			if(AnyBalance.isAvailable('traffic') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].underTraffic){
-				result['traffic'] = (v.traffic[0].rows[2].underTraffic / 1000000000).toFixed(1);
+			//Общий трафик
+			if(AnyBalance.isAvailable('traffic') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].traffic){
+				result['traffic'] = (v.traffic[0].rows[2].traffic / 1000000000).toFixed(1);
 			}
+			if(AnyBalance.isAvailable('traffic_in_lim') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].underTraffic){
+				result['traffic_in_lim'] = (v.traffic[0].rows[2].underTraffic / 1000000000).toFixed(1);
+			}
+			if(AnyBalance.isAvailable('traffic_over_lim') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].overTraffic){
+				result['traffic_over_lim'] = (v.traffic[0].rows[2].overTraffic / 1000000000).toFixed(1);
+			}
+			//Трафик внешний зарубежный
+			if(AnyBalance.isAvailable('traffic_SatEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[1] && v.traffic[0].rows[1].traffic){
+				result['traffic_SatEx'] = (v.traffic[0].rows[1].traffic / 1000000000).toFixed(1);
+			}
+			if(AnyBalance.isAvailable('traffic_in_lim_SatEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[1] && v.traffic[0].rows[1].underTraffic){
+				result['traffic_in_lim_SatEx'] = (v.traffic[0].rows[1].underTraffic / 1000000000).toFixed(1);
+			}
+			if(AnyBalance.isAvailable('traffic_over_lim_SatEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[1] && v.traffic[0].rows[1].overTraffic){
+				result['traffic_over_lim_SatEx'] = (v.traffic[0].rows[1].overTraffic / 1000000000).toFixed(1);
+			}
+			//Трафик внутренний украинский
+			if(AnyBalance.isAvailable('traffic_UkrEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[0] && v.traffic[0].rows[0].traffic){
+				result['traffic_UkrEx'] = (v.traffic[0].rows[0].traffic / 1000000000).toFixed(1);
+			}
+			if(AnyBalance.isAvailable('traffic_in_lim_UkrEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[0] && v.traffic[0].rows[0].underTraffic){
+				result['traffic_in_lim_UkrEx'] = (v.traffic[0].rows[0].underTraffic / 1000000000).toFixed(1);
+			}
+			if(AnyBalance.isAvailable('traffic_over_lim_UkrEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[0] && v.traffic[0].rows[0].overTraffic){
+				result['traffic_over_lim_UkrEx'] = (v.traffic[0].rows[0].overTraffic / 1000000000).toFixed(1);
+			}
+			
 			AnyBalance.setResult(result);
 		} else if (resst == 'bad login'){
 			throw new AnyBalance.Error('Неправильный логин или пароль');

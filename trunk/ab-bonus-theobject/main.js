49400279f010a74ec1b2859b5367d430b7aaa157
v.vostrikov@softworks.ru <v.vostrikov@softworks.ru@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-04-25 09:50:52 +0000

    TheObject - в список счетчиков добавлен настрел.

--- trunk/ab-bonus-theobject/main.js
+++ trunk/ab-bonus-theobject/main.js
@@ -1,13 +1,20 @@
 function main(){
 var prefs = AnyBalance.getPreferences();
+var url = 'http://theobject.ru/my/';
 
-var str = AnyBalance.requestPost('http://theobject.ru/my/user/auth', {
+var str = AnyBalance.requestPost(url+'user/auth', {
     email: prefs.login,
     password: prefs.password
   });
 var obj = jQuery.parseJSON(str);  
-if(obj.success)
- AnyBalance.setResult({success: true, bonus: obj.bonus, balance: obj.balance, "__tariff": obj.level}); 
+if(obj.success) {
+ var result = {success: true, bonus: obj.bonus, balance: obj.balance, "__tariff": obj.level};
+ if(AnyBalance.isAvailable('shoots')) {
+  var html = AnyBalance.requestGet(url);   
+  getParam (html, result, 'shoots', /Общий настрел:[\s\S]*?<td[^>]*><strong>([\s\S]*?)<\/strong><\/td>/i, replaceTagsAndSpaces, parseBalance);
+ }
+ AnyBalance.setResult(result); 
+} 
 else
  AnyBalance.setResult({error: true, message: "Не удалось получить данные с сайта"}); 
 }

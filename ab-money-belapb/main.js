909e3dfd6a303b33fdf9eea4853a63692cf395eb
dmitry.kochin <dmitry.kochin@gmail.com>
2013-05-02 16:38:56 +0000

    Беларусагропромбанк

---
+++ ab-money-belapb/main.js
@@ -0,0 +1,254 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает текущий остаток и другие параметры карт и счетов Белагропромбанка
+
+Сайт оператора: http://www.belapb.by
+Личный кабинет: https://www.ibank.belapb.byz
+*/
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+    else
+      return value
+	}
+}
+
+var replaceTagsAndSpaces = [/\\n/g, ' ', /\[br\]/ig, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var _text = text.replace(/\s+/g, '');
+    var val = getParam(_text, null, null, /(-?\d[\d\.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseCurrency(text){
+    var _text = text.replace(/\s+/g, '');
+    var val = getParam(_text, null, null, /-?\d[\d\.,]*\s*(\S*)/);
+    AnyBalance.trace('Parsing currency (' + val + ') from: ' + text);
+    return val;
+}
+
+function parseDate(str){
+    var matches = /(\d+)[^\d](\d+)[^\d](\d+)/.exec(str);
+    if(matches){
+          var date = new Date(+matches[3], matches[2]-1, +matches[1]);
+	  var time = date.getTime();
+          AnyBalance.trace('Parsing date ' + date + ' from value: ' + str);
+          return time;
+    }
+    AnyBalance.trace('Failed to parse date from value: ' + str);
+}
+
+function parseStatus(str){
+    return getParam(str, null, null, /^([^\(]*)/, replaceTagsAndSpaces);
+}
+
+function sleep(delay) {
+   if(AnyBalance.getLevel() < 6){
+      var startTime = new Date();
+      var endTime = null;
+      do {
+          endTime = new Date();
+      } while (endTime.getTime() - startTime.getTime() < delay);
+   }else{
+      AnyBalance.sleep(delay);
+   }
+} 
+
+function encryptPass(pass, map){
+	if(map){
+		var ch='',i=0,k=0,TempPass='',PassTemplate=map.split(','), Pass='';
+		TempPass=pass;
+		while(TempPass!=''){
+			ch=TempPass.substr(0, 1);
+			k = ch.charCodeAt(0);
+			if(k>0xFF) k-=0x350;
+			if(k==7622) k=185;
+			TempPass=TempPass.length>1?TempPass.substr(1, TempPass.length):'';
+			if(Pass!='')Pass=Pass+';';
+			Pass=Pass+PassTemplate[k];
+		}
+                return Pass;
+	}else{
+		return pass;
+	}
+
+}
+
+var g_headers = {
+    'Accept-Language': 'ru, en',
+    BSSHTTPRequest:1,
+//    Referer: 'https://www.open24.kz/v1/cgi/bsi.dll?T=RT_2Auth.BF&L=RUSSIAN&color=blue',
+//    Origin: 'https://www.open24.kz',,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    var baseurl = "https://www.ibank.belapb.by/v1/cgi/bsi.dll?";
+    
+    var html = AnyBalance.requestGet(baseurl + 'T=RT_2Auth.BF');
+    var mapId = getParam(html, null, null, /<input[^>]*name="MapID"[^>]*value="([^"]*)"/i);
+    var map = getParam(html, null, null, /var\s+PassTemplate\s*=\s*new\s+Array\s*\(([^\)]*)/i);
+    var pass = encryptPass(prefs.password, map);
+
+    html = AnyBalance.requestPost(baseurl, {
+        tic: 0,
+        T:'RT_2Auth.CL',
+        A:prefs.login,
+        B:pass,
+        L:'russian',
+        C:'',
+        IdCaptcha:'',
+        IMode:'',
+        sTypeInterface:'default',
+        MapID:mapId || ''
+    }, g_headers);
+
+    var error = getParam(html, null, null, /<BSS_ERROR>\d*\|?([\s\S]*?)<\/BSS_ERROR>/i, replaceTagsAndSpaces, html_entity_decode);
+    if(error)
+        throw new AnyBalance.Error(error);
+
+    var jsonInfo = getParam(html, null, null, /ClientInfo=(\{[\s\S]*?\})\s*(?:<\/div>|$)/i);
+    if(!jsonInfo)
+        throw new AnyBalance.Error("Не удалось найти информацию о сессии в ответе банка.");
+
+    jsonInfo = JSON.parse(jsonInfo);
+
+    var html = AnyBalance.requestPost(baseurl, {
+        SID:jsonInfo.SID,
+        tic:1,
+        T:'rt_0clientupdaterest.doheavyupd'
+    }, g_headers);
+
+    var i=0;
+    do{
+        AnyBalance.trace('Ожидание обновления данных: ' + (i+1));
+        html = AnyBalance.requestPost(baseurl, {
+            SID:jsonInfo.SID,
+            tic:1,
+            T:'rt_0clientupdaterest.CheckForAcyncProcess'
+        }, g_headers);
+        var opres = getParam(html, null, null, /^\s*(?:<BSS_ERROR>([\s\S]*?)<\/BSS_ERROR>)?\d+\s*$/i, replaceTagsAndSpaces, html_entity_decode);
+        if(opres){
+            AnyBalance.trace('Обновление данных закончено. ' + opres);
+            break; //Всё готово, надо получать баланс
+        }
+        opres = getParam(html, null, null, /^\s*(?:<BSS_ERROR>([\s\S]*?)<\/BSS_ERROR>)\s*$/i, replaceTagsAndSpaces, html_entity_decode);
+        if(opres){
+            AnyBalance.trace('Обновление данных закончено с ошибкой: ' + opres);
+            break; //Всё готово, надо получать баланс
+        }
+        if(++i > 10){  //На всякий случай не делаем больше 10 попыток
+            AnyBalance.trace('Не удалось за 10 попыток обновить баланс, получаем старое значение...');
+            break;
+        }
+        sleep(3000);
+    }while(true);
+
+/*
+    var html = AnyBalance.requestPost(baseurl, {
+        SID:jsonInfo.SID,
+        tic:1,
+        T:'RT_2IC.form',
+        nvgt:1,
+        SCHEMENAME:'COMMPAGE',
+        XACTION:''
+    }, g_headers);
+*/
+    if(prefs.type == 'card')
+        fetchCard(jsonInfo, baseurl);
+    else if(prefs.type == 'acc')
+        fetchAccount(jsonInfo, baseurl);
+    else
+        fetchAccount(jsonInfo, baseurl); //По умолчанию счета будем получать
+}
+
+function fetchCard(jsonInfo, baseurl){
+    var prefs = AnyBalance.getPreferences();
+    if(prefs.cardnum && !/^\d{4}$/.test(prefs.cardnum))
+        throw new AnyBalance.Error("Введите 4 последних цифры номера карты или не вводите ничего, чтобы показать информацию по первой карте");
+
+    var html = AnyBalance.requestPost(baseurl, {
+        SID:jsonInfo.SID,
+        tic:1,
+        T:'RT_2IC.ShowMultiSchemePage',
+        nvgt:1,
+        SCHEMENAME:'CARDS'
+    }, g_headers);
+
+    var cardnum = prefs.cardnum ? prefs.cardnum : '\\d{4}';
+    var re = new RegExp('(<tr[^>]*>(?:[\\s\\S](?!<tr))*\\d+\\s+\\d+\\*+\\s+\\*+\\s+' + cardnum + '[\\s\\S]*?</tr>)', 'i');
+    var tr = getParam(html, null, null, re);
+    if(!tr)
+        throw new AnyBalance.Error('Не удаётся найти ' + (prefs.cardnum ? 'карту с последними цифрами ' + prefs.cardnum : 'ни одной карты'));
+
+    var result = {success: true};
+    getParam(tr, result, 'cardnum', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'balance', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(tr, result, 'accnum', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'currency', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'type', /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'fio', /(?:[\s\S]*?<td[^>]*>){7}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'status', /(?:[\s\S]*?<td[^>]*>){8}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    AnyBalance.setResult(result);
+}
+
+function fetchAccount(jsonInfo, baseurl){
+    var prefs = AnyBalance.getPreferences();
+    if(prefs.cardnum && !/^\d{4,}$/.test(prefs.cardnum))
+        throw new AnyBalance.Error("Введите от четырех последних цифр номера счета или не вводите ничего, чтобы показать информацию по первому счету");
+                                                                                                                                
+    var html = AnyBalance.requestPost(baseurl, {
+        SID:jsonInfo.SID,
+        tic:1,
+        T:'RT_2IC.form',
+        nvgt:1,
+        SCHEMENAME:'STM',
+        XACTION: 'NEW'
+    }, g_headers);
+
+    var cardnum = prefs.cardnum ? prefs.cardnum : '\\d{4,}';
+    var re = new RegExp('(<tr[^>]*>(?:[\\s\\S](?!<tr))*<div[^>]+class="card"[^>]*>[^<]*' + cardnum + '<[\\s\\S]*?</tr>)', 'i');
+    var tr = getParam(html, null, null, re);
+    if(!tr)
+        throw new AnyBalance.Error('Не удаётся найти ' + (prefs.cardnum ? 'счет с последними цифрами ' + prefs.cardnum : 'ни одного счета'));
+
+    var result = {success: true};
+    getParam(tr, result, 'accnum', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'balance', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(tr, result, 'currency', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, 'type', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(tr, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(jsonInfo.CNS, result, 'fio', /(.*)/i, replaceTagsAndSpaces);
+    AnyBalance.setResult(result);
+}
+
+function html_entity_decode(str)
+{
+    //jd-tech.net
+    var tarea=document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}
+

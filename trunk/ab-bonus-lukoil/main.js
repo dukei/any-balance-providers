8f412cf51d684abeaa4b7117b2b54afb670f49aa
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-05-04 06:22:56 +0000

    Исправлен Лукойл бонус

--- trunk/ab-bonus-lukoil/main.js
+++ trunk/ab-bonus-lukoil/main.js
@@ -1,105 +1,62 @@
-﻿/**
-Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+﻿/**             
+ Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+ 
+ Получает текущий баланс бонусных баллов, статус карты, регион участия программы.
+ 
+ Сайт оператора: http://club-lukoil.ru/
+ Личный кабинет: http://club-lukoil.ru/cabinet/
+ Личный кабинет (информация о владельце): http://club-lukoil.ru/cabinet/personal
+ */
 
-Получает текущий баланс бонусных баллов, статус карты, регион участия программы.
-
-Сайт оператора: http://club-lukoil.ru/
-Личный кабинет: http://club-lukoil.ru/cabinet/
-Личный кабинет (информация о владельце): http://club-lukoil.ru/cabinet/personal
-*/
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
 
 function main(){
-        var prefs = AnyBalance.getPreferences();
-        var PASS = prefs.password;
-		var LOGIN = prefs.login;
-		var url='http://club-lukoil.ru/cabinet/';
-		var url1='http://club-lukoil.ru/';
-		var url2='http://club-lukoil.ru/cabinet/personal';
-		var html1 = AnyBalance.requestPost(url1, prefs);
-		AnyBalance.trace('Получение sessid');
-		regexp=/bxSession.*\n*, '(.*)', /m;
-        if (str=regexp.exec(html1)){
-              sessid=str[1];
-        }
-		AnyBalance.trace('sessid = ' + sessid);
-		
-		AnyBalance.trace('Отправка запроса авторизации');
-        var html = AnyBalance.requestPost(url, {
-			LOGIN:prefs.login,
-			PASS:prefs.password,
-			sessid:sessid
-		});
-		var html2 = AnyBalance.requestPost(url2, {
-			LOGIN:prefs.login,
-			PASS:prefs.password,
-			sessid:sessid
-		});
-		regexp=/Контактные данные/m;
-		regexp2=/Неправильная пара логин-пароль/m;
-		if (str=regexp.exec(html)){	
-			AnyBalance.trace('Доступ получен');
-			}
-		else if (str=regexp2.exec(html)){	
-			AnyBalance.trace('Неправильная пара логин-пароль!');
-			throw new AnyBalance.Error ('Неправильный логин-пароль.');
-			}
-		else	
-			{
-			AnyBalance.trace('Нет доступа');
-           throw new AnyBalance.Error ('Доступ не получен.');
-		};
-		
-		var result = {success: true};
-		AnyBalance.trace('Разбор полученных данных:');
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "http://club-lukoil.ru/";
+    AnyBalance.setDefaultCharset('utf-8'); 
 
-		
-//Баланс
-        regexp=/Баланс Ваших Баллов.*\n*>(.*) б/m;
-        if (str=regexp.exec(html)){
-              result.balance=parseFloat(str[1]);
-        }
-		AnyBalance.trace('Баланс = ' + str[1]);
+    var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+    var sid = getParam(html, null, null, /bitrix_sessid':'([^']*)/i, replaceSlashes);
+    if(!sid) //Если параметр не найден, то это, скорее всего, свидетельствует об изменении сайта или о проблемах с ним
+        throw new AnyBalance.Error('Не удалось найти идентификатор сессии. Сайт изменен?');
 
-//Статус карты
-		regexp=/Ваша Карта.*\n*>(.*)</m;
-        if (str=regexp.exec(html)){
-              result.status=str[1];
-        }
-		AnyBalance.trace('Статус карты = ' + str[1]);
+    html = AnyBalance.requestPost(baseurl + 'login/', {
+        sessid:sid,
+        LOGIN:prefs.login,
+        PASS:prefs.password
+    }, addHeaders({Referer: baseurl})); 
 
-//Регион программы
-		regexp=/Регион Программы.*\n*>(.*)</m;
-        if (str=regexp.exec(html)){
-              result.region=str[1];
-        }
-		AnyBalance.trace('Регион = ' + str[1]);		
-		
-//Статус в программе
-        regexp=/статус в Программе.*\n*>(.*)</m;
-        if (str=regexp.exec(html)){
-              result.statusprog=str[1];
-        }
-		AnyBalance.trace('Статус в программе = ' + str[1]);
+    //После входа обязательно проверяем маркер успешного входа
+    //Обычно это ссылка на выход, хотя иногда приходится искать что-то ещё
+    if(!/\?ACTION=logout/i.test(html)){
+        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
+        var error = getParam(html, null, null, /<p[^>]+class="err"[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
 
-//ФИО
-        regexp=/ФИО.*\n*>(.*)</m;
-        if (str=regexp.exec(html2)){
-              result.username=str[1];
-        }
-		AnyBalance.trace('Владелец = ' + str[1]);
+    //Раз мы здесь, то мы успешно вошли в кабинет
+    //Получаем все счетчики
+    var result = {success: true};
+    getParam(html, result, '__tariff', /Номер Вашей Карты:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'cardnum', /Номер Вашей Карты:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'region', /Регион Программы:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Баланс Ваших Баллов:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, parseBalance);
 
-//Телефон
-        regexp=/Мобильный телефон.*\n*>(.*)</m;
-        if (str=regexp.exec(html2)){
-              result.phonenumber=str[1];
-        }
-		AnyBalance.trace('Мобильный телефон = ' + str[1]);
+    if(AnyBalance.isAvailable('username', 'phonenumber')){
+        html = AnyBalance.requestGet(baseurl + 'cabinet/personal/');
+        getParam(html, result, 'username', /ФИО:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+        getParam(html, result, 'phonenumber', /Мобильный телефон:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+    }
 
-		AnyBalance.trace('Разбор завершен.');
-        AnyBalance.setResult(result);
+    //Возвращаем результат
+    AnyBalance.setResult(result);
 }
-
-
-
-
-

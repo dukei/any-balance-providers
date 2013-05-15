4ed55c6777c3f1611c63c4449d2d7dd592234958
alexandrashalamova@gmail.com <alexandrashalamova@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-05-15 14:26:03 +0000

    add ab-bonus-british-airways

---
+++ trunk/ab-bonus-british-airways/main.js
@@ -0,0 +1,54 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и другую информацию для British Airways 
+
+Operator site: https://www.britishairways.com/travel/viewaccount/execclub/_gf/ru_ru
+
+*/
+
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+
+    var baseurl = "https://www.britishairways.com/";
+
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html = AnyBalance.requestGet(baseurl + 'travel/loginr/public/ru_ru');
+
+    var eId = getParam(html, null, null, /<input[^>]+name="eId"[^>]+value=\"([\s\S]*?)\"[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
+alert(eId);
+    html = AnyBalance.requestPost(baseurl + 'travel/loginr/public/ru_ru?eId=' + eId, {
+        Directional_Login:'/travel/echome/execclub/_gf/ru_ru',
+        membershipNumber:prefs.login,
+        password:prefs.password
+    }, addHeaders({Referer: baseurl + 'travel/echome/execclub/_gf/ru_ru'})); 
+
+    if(!/id="logout"/i.test(html)){
+        var error = getParam(html, null, null, /<span[^>]+class="errorTitle"[^>]*>Error<\/span>[\s\S\]*<div[^>]+class="podBody"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    var result = {success: true};
+    getParam(html, result, 'fio', /id="welcomeLabel"[^>]*>[\s\S]*?,\s*([\s\S]*?)<\/h1>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /<h2[^>]+class="lineSpace aviosPoints"[^>]*>([\s\S]*?)<\/h2>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balanceFamaly', /Баллы Avios на Семейном Счете:(?:[\s\S]*?<h3[^>]*>){1}([\s\S]*?)<\/h3>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balanceStatus', /<h2[^>]+class="headingDisp"[^>]*>([\s\S]*?)<\/h2>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'cartType', /<h4[^>]+class="tierH4Style headingDisp"[^>]*>([\s\S]*?)<\/h4>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'yearEnd', /<table[^>]+summary="Мои Статусные Баллы"[^>]*>(?:[\s\S]*?<td[^>]*>){4}\s*<strong>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'cartEnd', /<table[^>]+summary="Мои Статусные Баллы"[^>]*>(?:[\s\S]*?<td[^>]*>){6}\s*<strong>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+
+
+    //Возвращаем результат
+    AnyBalance.setResult(result);
+}

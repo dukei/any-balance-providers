f06a4e0eae3499f4d592e97d9d2c7074a39e9e0c
sinitsynviktor <sinitsynviktor@gmail.com>
2013-05-17 21:22:27 +0000

    ab-internet-stats -> добавлены счетчики "Объем использованного трафика" и "Объем использованного трафика Wimax"

--- ab-internet-stats/main.js
+++ ab-internet-stats/main.js
@@ -49,6 +49,7 @@ function main(){
         getParam(html, result, 'ipStatus', /Состояние IP адресов:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
         getParam(html, result, 'period', /Текущий период:\s*([\s\S]*?)<\/h2>/i, replaceTagsAndSpaces, html_entity_decode);  
         getParam(html, result, 'rekplat', /Необходимая для внесения [^<]*?услуг:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance); 
+        getParam(html, result, 'trafik', /Объем использованного трафика:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance); 
     }
 
     if (/wrapper\.php\?v_s=27/i.test(html)) {            
@@ -68,7 +69,8 @@ function main(){
         getParam(html, result, 'promiseWimax', /Сумма обещанных платежей:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
         getParam(html, result, 'statusWimax', /Состояние IP адресов:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
         getParam(html, result, 'periodWimax', /Текущий период:\s*([\s\S]*?)<\/h2>/i, replaceTagsAndSpaces, html_entity_decode);  
-        getParam(html, result, 'rekplatWimax', /Необходимая для внесения [^<]*?услуг:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance); 
+        getParam(html, result, 'rekplatWimax', /Необходимая для внесения [^<]*?услуг:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'trafikWimax', /Объем использованного трафика:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);  
     }
 
     AnyBalance.setResult(result);

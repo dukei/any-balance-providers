52b536cb015db5cd0244cb6ab301e726e4635ebe
alexandrashalamova <alexandrashalamova@gmail.com>
2013-05-21 14:51:26 +0000

    в ab-transport-nrg-tk Города переделаны списком, вывод информации сделан одним счетчиком.

--- ab-transport-nrg-tk/main.js
+++ ab-transport-nrg-tk/main.js
@@ -21,16 +21,8 @@ function main() {
 
     AnyBalance.setDefaultCharset('utf-8'); 
 
-    var html = AnyBalance.requestGet(baseurl + 'poisk-nakladnoj.html', g_headers);
-
-    var preg = new RegExp('<select[^>]+id="city"[^>]*>[\\s\\S]*<option[^>]+value="([\\s\\S]*?)">' + prefs.city + '<\/option>', "i");
-
-    var tform = getParam(html, null, null, preg, null, parseBalance);
-    if(!tform)
-        throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
-
-    html = AnyBalance.requestPost(baseurl + 'tracking.php', {
-        city:tform,    
+    var html = AnyBalance.requestPost(baseurl + 'tracking.php', {
+        city:prefs.city,    
         invoice:prefs.number,
     }, addHeaders({Referer: baseurl + 'tracking.php'})); 
 
@@ -42,6 +34,7 @@ function main() {
     }
 
     var result = {success: true};
+
     getParam(html, result, 'now', /\nТекущее местоположение:\s*([\s\S]*?)$/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'tocity', /\nКуда:\s*([\s\S]*?)\n/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'from', /\nОткуда:\s*([\s\S]*?)\n/i, replaceTagsAndSpaces, html_entity_decode);
@@ -49,5 +42,32 @@ function main() {
     getParam(html, result, 'weight', /\nВес:\s*([\s\S]*?)\n/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'volume', /\nОбъём:\s*([\s\S]*?)\n/i, replaceTagsAndSpaces, parseBalance);
 
+    getParam(html, result, 'results', null, [/:([\s\S]*?)(?:\n|$)/ig, ": <strong>$1</strong><br/>", /\n/g, "<br/>"], null);
+
+// buildRegions(html);
+
     AnyBalance.setResult(result);
+}
+
+function buildRegions(result){
+    var html = AnyBalance.requestGet('http://nrg-tk.ru/poisk-nakladnoj.html'),
+        select = getParam(html, null, null, /(<select[^>]+id="city"[^>]*>([\s\S]*?)<\/select>)/i, null, null),
+        regions = sumParam(select, null, null, /(<option[^>]*>([\s\S]*?)<\/option>?)/ig);
+
+    // console.log(regions);
+
+    var names = [], values = [], value = '';
+    for(var i=0; i<regions.length; ++i){
+        value = getParam(regions[i], null, null, /value=['"]([^'"]*)/i, replaceTagsAndSpaces);
+        if (value !== "") {
+            values.push(value);
+            names.push(getParam(regions[i], null, null, /<option[^>]*>([\s\S]*?)<\/option>/i, replaceTagsAndSpaces));
+        }
+
+    }
+    if (values.length === 0) AnyBalance.trace('Города не найдены.');
+
+    console.log(names.join('|'));
+    console.log(values.join('|'));
+
 }

e42994ba4904194b7578d47796ef7502c08771a0
dmitry.kochin <dmitry.kochin@gmail.com>
2013-05-22 05:03:16 +0000

    Спортмастер - поддержка изменений на сайте

--- ab-bonus-sportmaster/main.js
+++ ab-bonus-sportmaster/main.js
@@ -7,38 +7,6 @@
 Личный кабинет: http://www.sportmaster.ru/personal/bonus.php
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-function parseBalance(text){
-    var _text = text.replace(/\s+/, '');
-    var val = getParam(_text, null, null, /(-?\d[\d\.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
 function main(){
     var prefs = AnyBalance.getPreferences();
 
@@ -67,17 +35,33 @@ function main(){
         AnyBalance.trace('Пароль не введен - получаем данные по номеру карты');
 
         var baseurl = "http://www.sportmaster.ru/club-program/";
-        var html = AnyBalance.requestPost(baseurl, {
-            card_id:prefs.login,
-            check_bonus:''
-        });
-        
-        if(!/Доступно средств:([^']*)/i.test(html))
-            throw new AnyBalance.Error("Не удалось получить баланс. Неверный номер карты?");
+        var types = {
+            '300': 'Синяя карта',
+            '301': 'Серебряная карта',
+            '302': 'Золотая карта',
+        };
+        var status;
+
+        for(var type in types){
+            var html = AnyBalance.requestPost(baseurl, {
+                card_type:type,
+                card_id:prefs.login,
+                check_bonus:''
+            });
+            status = getParam(html, null, null, /<div[^>]+class="[^"]*newbg3[\s\S]*?alert\s*\(\s*'([^']*)/i, replaceSlashes);
+            if(/Доступно средств:/i.test(status))
+                break;
+        }
+
+        if(!/Доступно средств:([^']*)/i.test(html)){
+            if(status)
+                throw new AnyBalance.Error(status);
+            throw new AnyBalance.Error("Не удалось получить баланс. Неверный номер карты или сайт изменен?");
+        }
 
         var result = {success: true};
         
-        result.__tariff = prefs.login;
+        result.__tariff = types[type];
         if(AnyBalance.isAvailable('cardnum'))
             result.cardnum = prefs.login;
         getParam(html, result, 'balance', /Доступно средств:([^']*)/i, replaceTagsAndSpaces, parseBalance);
@@ -85,12 +69,3 @@ function main(){
 
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

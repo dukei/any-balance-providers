482c91a12427e4a01a04f1560d3f2123f5f87060
alexandrashalamova@gmail.com <alexandrashalamova@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-06-04 12:17:53 +0000

    добавлен ab-internet-tiera

---
+++ trunk/ab-internet-tiera/main.js
@@ -0,0 +1,65 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получается баланс для интернет провайдера Tiera 
+
+Operator site: https://my.tiera.ru/
+*/
+
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    moment.lang('ru');
+    var prefs = AnyBalance.getPreferences();
+
+    var baseurl = "https://my.tiera.ru/";
+
+    AnyBalance.setDefaultCharset('cp1251'); 
+
+    var html = AnyBalance.requestPost(baseurl, {
+        action: 'logon',      
+        login:prefs.login,
+        password:prefs.password,
+        x:'64',        
+        y:'17'
+    }, addHeaders({Referer: baseurl})); 
+
+    if(!/\/Logout/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Возможно не правильно введен логин или пароль. Либо сайт изменен.');
+    }
+
+    var result = {success: true};
+    getParam(html, result, 'fio', /<td[^>]*>ФИО<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'prognoz', /<td[^>]*>Прогноз отключения<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDateMoment);
+    getParam(html, result, 'number', /<td[^>]*>Лицевой счет<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /<td[^>]*>Баланс<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalanceRK);
+    getParam(html, result, 'status', /<td[^>]*>Статус<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}
+
+function parseBalanceRK(_text){
+    var text = _text.replace(/\s+/g, '');
+    var rub = getParam(text, null, null, /(-?\d[\d\.,]*)руб/i, replaceFloat, parseFloat) || 0;
+    var kop = getParam(text, null, null, /(-?\d[\d\.,]*)коп/i, replaceFloat, parseFloat) || 0;
+    var val = rub+kop/100;
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + _text);
+    return val;
+}
+
+function parseDateMoment(str){
+    var mom = moment(str.replace(/i/ig, 'і'), ['DD MMM YYYY', 'HH:mm-D MMM YYYY']);
+    if(!mom.isValid()){
+        AnyBalance.trace('Failed to parse date from ' + str);
+    }else{
+        var val = mom.toDate();
+        AnyBalance.trace('Parsed date ' + val + ' from ' + str);
+        return val.getTime();
+    }
+}

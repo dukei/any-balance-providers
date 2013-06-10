dd5a0e1ce85685b12d81ff55653a6c899f7e8cb1
dmitry.kochin <dmitry.kochin@gmail.com>
2013-06-10 16:46:36 +0000

    Райффайзен

---
+++ ab-money-raif/main.js
@@ -0,0 +1,171 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс на картах и счетах Райффайзен-банка
+
+Operator site: http://www.raiffeisen.ru
+Личный кабинет: https://connect.raiffeisen.ru
+*/
+
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для сотового оператора xxxxxx 
+
+Operator site: http://xxxxxx.ru
+Личный кабинет: https://kabinet.xxxxxx.ru/login
+*/
+
+var g_headers = {
+'Accept':'text/xml',
+'Content-Type':'text/xml',
+'Connection':'keep-alive',
+'User-Agent':'Dalvik/1.6.0 (Linux; U; Android 4.1.2; GT-I9300 Build/JZO54K) Android/2.1.1(121)'
+};
+
+var g_xml_login = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?><soapenv:Envelope xmlns:xsd="http://entry.rconnect/xsd" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.rconnect" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header /><soapenv:Body><ser:login><ser:login>%LOGIN%</ser:login><ser:password>%PASSWORD%</ser:password></ser:login></soapenv:Body></soapenv:Envelope>',
+    g_xml_accounts = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?><soapenv:Envelope xmlns:xsd="http://entry.rconnect/xsd" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.rconnect" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header /><soapenv:Body><ser:GetAccounts /></soapenv:Body></soapenv:Envelope>',
+    g_xml_cards = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?><soapenv:Envelope xmlns:xsd="http://entry.rconnect/xsd" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.rconnect" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Header /><soapenv:Body><ser:GetCards /></soapenv:Body></soapenv:Envelope>';
+
+
+function translateError(error){
+    var errors = {
+        'logins.password.incorrect': 'Неправильный логин или пароль',
+    };
+
+    if(errors[error])
+        return errors[error];
+
+    AnyBalance.trace('Неизвестная ошибка: ' + error);
+    return error;
+}
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "https://connect.raiffeisen.ru/mobile/services/";
+
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html = AnyBalance.requestPost(baseurl + 'RCAuthorizationService', g_xml_login.replace(/%LOGIN%/g, prefs.login).replace(/%PASSWORD%/g, prefs.password), addHeaders({SOAPAction: 'urn:login'})); 
+
+    if(!/<ax21:name>/i.test(html)){
+        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
+        var error = getParam(html, null, null, /<faultstring>([\s\S]*?)<\/faultstring>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(translateError(error));
+        AnyBalance.trace(html);
+        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
+        throw new AnyBalance.Error('Не удалось зайти в интернет банк. Обратитесь к автору провайдера.');
+    }
+
+    var result = {success: true};
+    getParam(html, result, 'fio', /<ax\d+:name>([\s\S]*?)<\/ax\d+:name>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    if(prefs.type == 'card')
+        fetchCard(baseurl, html, result);
+    else if(prefs.type == 'acc')
+        fetchAccount(baseurl, html, result);
+    else if(prefs.type == 'dep')
+        fetchDeposit(baseurl, html, result);
+    else
+        fetchAccount(baseurl, html, result);
+
+    //Возвращаем результат
+    AnyBalance.setResult(result);
+}
+
+//    
+
+function fetchCard(baseurl, html, result){
+    var prefs = AnyBalance.getPreferences();
+    throw new AnyBalance.Error('Карты пока не поддерживаются. Обращайтесь к автору провайдера по е-мейл dco@mail.ru');
+
+    if(prefs.num && !/^\d+$/.test(prefs.num))
+        throw new AnyBalance.Error('Введите последние цифры интересующего вас счета или не вводите ничего, чтобы получить информацию по первому счету');
+
+    html = AnyBalance.requestPost(baseurl + 'RCCardService', g_xml_cards, addHeaders({SOAPAction: 'urn:GetCards'})); 
+   
+    var re = new RegExp('<ns:return[^>]*>((?:[\\s\\S](?!</ns:return>))*?<ax\\d+:number>\\d+' + (prefs.num ? prefs.num : '') + '</ac\\d+:number>[\\s\\S]*?)</ns:return>', 'i');
+
+    getParam(html, result, '__tariff', /<ax\d+:number>([\s\S]*?)<\/ax\d+:number>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(html, result, 'accnum', /<ax\d+:number>([\s\S]*?)<\/ax\d+:number>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /<ax\d+:balance>([\s\S]*?)<\/ax\d+:balance>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'currency', /<ax\d+:currency>([\s\S]*?)<\/ax\d+:currency>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(html, result, 'minpaytill', /<ax\d+:nextCreditPaymentDate>([\s\S]*?)<\/ax\d+:nextCreditPaymentDate>/i, replaceTagsAndSpaces, parseDateISO);
+    getParam(html, result, 'limit', /<ax\d+:creditLimit>([\s\S]*?)<\/ax\d+:creditLimit>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'till', /<ax\d+:closeDate>([\s\S]*?)<\/ax\d+:closeDate>/i, replaceTagsAndSpaces, parseDateISO);
+}
+
+function fetchAccount(baseurl, html, result){
+    var prefs = AnyBalance.getPreferences();
+
+    if(prefs.num && !/^\d+$/.test(prefs.num))
+        throw new AnyBalance.Error('Введите последние цифры интересующего вас счета или не вводите ничего, чтобы получить информацию по первому счету');
+    html = AnyBalance.requestPost(baseurl + 'RCAccountService', g_xml_accounts, addHeaders({SOAPAction: 'urn:GetAccounts'})); 
+   
+    var re = new RegExp('<ns:return[^>]*>((?:[\\s\\S](?!</ns:return>))*?<ax\\d+:number>\\d*' + (prefs.num ? prefs.num : '') + '</ax\\d+:number>[\\s\\S]*?)</ns:return>', 'i');
+    var info = getParam(html, null, null, re);
+    if(!info)
+        throw new AnyBalance.Error(prefs.num ? 'Не удалось найти счета с последними цифрами ' + prefs.num : 'Не найдено ни одного счета');
+
+    getParam(info, result, '__tariff', /<ax\d+:number>([\s\S]*?)<\/ax\d+:number>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(info, result, 'accnum', /<ax\d+:number>([\s\S]*?)<\/ax\d+:number>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(info, result, 'balance', /<ax\d+:balance>([\s\S]*?)<\/ax\d+:balance>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(info, result, 'currency', /<ax\d+:currency>([\s\S]*?)<\/ax\d+:currency>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(info, result, 'minpaytill', /<ax\d+:nextCreditPaymentDate>([\s\S]*?)<\/ax\d+:nextCreditPaymentDate>/i, replaceTagsAndSpaces, parseDateISO);
+    getParam(info, result, 'limit', /<ax\d+:creditLimit>([\s\S]*?)<\/ax\d+:creditLimit>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(info, result, 'till', /<ax\d+:closeDate>([\s\S]*?)<\/ax\d+:closeDate>/i, replaceTagsAndSpaces, parseDateISO);
+
+    if(AnyBalance.isAvailable('all')){
+        var all = sumParam(html, null, null, /<ns:return[^>]*>([\s\S]*?)<\/ns:return>/ig);
+        var out = [];
+        for(var i=0; i<all.length; ++i){
+            var info = all[i];
+            var accnum = getParam(info, null, null, /<ax\d+:number>([\s\S]*?)<\/ax\d+:number>/i, replaceTagsAndSpaces, html_entity_decode);
+            var balance = getParam(info, null, null, /<ax\d+:balance>([\s\S]*?)<\/ax\d+:balance>/i, replaceTagsAndSpaces, parseBalance);
+            var currency = getParam(info, null, null, /<ax\d+:currency>([\s\S]*?)<\/ax\d+:currency>/i, replaceTagsAndSpaces, html_entity_decode);
+            out.push(accnum + ': ' + balance + ' ' + currency);
+        }
+        result.all = out.join('\n');
+    }
+}
+
+function fetchDeposit(baseurl, html, result){
+    var prefs = AnyBalance.getPreferences();
+
+    if(prefs.num && !/^\d+$/.test(prefs.num))
+        throw new AnyBalance.Error('Введите последние цифры интересующего вас счета или не вводите ничего, чтобы получить информацию по первому счету');
+    html = AnyBalance.requestPost(baseurl + 'RCDepositService', g_xml_accounts, addHeaders({SOAPAction: 'urn:GetDeposits'})); 
+   
+    var re = new RegExp('<ns:return[^>]*>((?:[\\s\\S](?!</ns:return>))*?<ax\\d+:accountNumber>\\d+' + (prefs.num ? prefs.num : '') + '</ax\\d+:accountNumber>[\\s\\S]*?)</ns:return>', 'i');
+    var info = getParam(html, null, null, re);
+    if(!info)
+        throw new AnyBalance.Error(prefs.num ? 'Не удалось найти счета с последними цифрами ' + prefs.num : 'Не найдено ни одного счета');
+
+    getParam(info, result, 'accnum', /<ax\d+:accountNumber>([\s\S]*?)<\/ax\d+:accountNumber>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(info, result, 'balance', /<ax\d+:initialAmount>([\s\S]*?)<\/ax\d+:initialAmount>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(info, result, 'currency', /<ax\d+:currency>[^<]*?([^<\.]*?)<\/ax\d+:currency>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    getParam(info, result, '__tariff', /<ax\d+:names>([\s\S]*?)<\/ax\d+:names>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(info, result, 'rate', /<ax\d+:interestRate>([\s\S]*?)<\/ax\d+:interestRate>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(info, result, 'pcts', /<ax\d+:totalInterest>([\s\S]*?)<\/ax\d+:totalInterest>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(info, result, 'till', /<ax\d+:closeDate>([\s\S]*?)<\/ax\d+:closeDate>/i, replaceTagsAndSpaces, parseDateISO);
+
+    if(AnyBalance.isAvailable('all')){
+        var all = sumParam(html, null, null, /<ns:return[^>]*>([\s\S]*?)<\/ns:return>/ig);
+        var out = [];
+        for(var i=0; i<all.length; ++i){
+            var info = all[i];
+            var accnum = getParam(info, null, null, /<ax\d+:accountNumber>([\s\S]*?)<\/ax\d+:accountNumber>/i, replaceTagsAndSpaces, html_entity_decode);
+            var balance = getParam(info, null, null, /<ax\d+:initialAmount>([\s\S]*?)<\/ax\d+:initialAmount>/i, replaceTagsAndSpaces, parseBalance);
+            var currency = getParam(info, null, null, /<ax\d+:currency>[^<]*?([^<\.]*?)<\/ax\d+:currency>/i, replaceTagsAndSpaces, html_entity_decode);
+            out.push(accnum + ': ' + balance + ' ' + currency);
+        }
+        result.all = out.join('\n');
+    }
+
+}

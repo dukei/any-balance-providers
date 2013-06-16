dfb0c8c0b23fa5d5ef1af24de425ab3ff496e49a
tujger <tujger@gmail.com>
2013-06-16 18:50:26 +0000

    - Security warning added

--- ab-money-perfectmoney/main.js
+++ ab-money-perfectmoney/main.js
@@ -24,7 +24,7 @@ function main(){
 	}
 
 	if(matches = info.match(/API is disabled for this IP/i)){
-		throw new AnyBalance.Error("API is disabled. Try to enable API in security section of your account on www.perfectmoney.is and set IP mask *.*.*.*.");}
+		throw new AnyBalance.Error("API is disabled. Try to enable API in security section of your account on www.perfectmoney.is and set IP mask *.*.*.*. PLEASE NOTE THAT ENABLING API IS SERIOUS SECURITY RISK FOR YOUR ACCOUNT.");}
 
 	if(matches = info.match(/<input name='ERROR' type='hidden' value='(.*?)'>/i)){
 		throw new AnyBalance.Error(matches[1]);}

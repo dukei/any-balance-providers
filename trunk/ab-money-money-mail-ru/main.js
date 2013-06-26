c3cdf91d9e933d76181c7736b1d51b4e727beb54
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-06-26 09:28:36 +0000

    money-mail-ru

---
+++ trunk/ab-money-money-mail-ru/main.js
@@ -0,0 +1,39 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    if(!prefs.login)
+         throw new AnyBalance.Error('Введите е-мейл для входа в личный кабинет money.mail.ru');
+
+    var parts = prefs.login.match(/^([\s\S]*?)@((?:mail|inbox|list|bk)\.ru)$/i);
+    if(!parts)
+         throw new AnyBalance.Error('Вы ввели неправильный е-мейл для входа на money.mail.ru.');
+
+    var baseurlLogin = "https://auth.mail.ru/cgi-bin/auth";
+    var baseurl = "https://money.mail.ru/";
+
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var html = AnyBalance.requestPost(baseurlLogin, {
+		FailPage:'https://money.mail.ru/nologin',
+        Page:baseurl,
+        Login:parts[1],
+        Domain:parts[2].toLowerCase(),
+        Password:prefs.password,
+    });
+    if(!/url=https:\/\/money.mail.ru/i.test(html))
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неправильный логин-пароль?');
+
+    html = AnyBalance.requestGet(baseurl); 
+
+    if(!/\/cgi-bin\/logout/i.test(html))
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неправильный логин-пароль?');
+
+	var result = {success: true};
+    getParam(prefs.login, result, 'login');
+    getParam(html, result, 'balance', /AccountBalanceInfo">[\s\S]*?([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+    
+    AnyBalance.setResult(result);
+}

aaafd334345df6c8a3a3a9b7c868de36195d2701
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-07-02 15:55:01 +0000

    added ab-internet-network-lviv

---
+++ trunk/ab-internet-network-lviv/main.js
@@ -0,0 +1,47 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://my.network.lviv.ua/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html = AnyBalance.requestPost(baseurl + 'ajax/index/authfl', {
+        login:prefs.login,
+        password:prefs.password,
+    }, g_headers); 
+
+	var found = /<us_uid>([\s\S]*?)<\/us_uid>/i.exec(html);
+    if(!found)
+        throw new AnyBalance.Error('Не удалось получить ID сесии. Сайт изменен?');
+
+	// Запускает на сервере обновление балансов
+	html = AnyBalance.requestPost(baseurl + 'ajax/users/stattrafflex', {
+		uid:found[1],
+    }, g_headers); 
+	
+	if(!/<time_on>/.test(html))
+		throw new AnyBalance.Error('Не удалось получить ID сесии. Сайт изменен?');
+
+	html = AnyBalance.requestGet(baseurl + 'ajax/users/getuserdatafl', addHeaders({Referer: baseurl+'ajax/users/getuserdatafl'})); 
+		
+		
+	var result = {success: true};
+	
+	getParam(html, result, 'fio', /<fio>([\s\S]*?)<\/fio>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /<deposit>([\s\S]*?)<\/deposit>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'credit', /<credit>([\s\S]*?)<\/credit>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /<tarif>([\s\S]*?)<\/tarif>/i, replaceTagsAndSpaces, html_entity_decode);
+	//<date_itog>20-08-2013</date_itog>
+
+    AnyBalance.setResult(result);
+}

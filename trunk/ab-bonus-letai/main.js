4ca6300f2ee559d4bb914cffa1dd785b64758816
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-07-05 13:07:15 +0000

    added: bonus-letai bonus-ostin internet-bttk

---
+++ trunk/ab-bonus-letai/main.js
@@ -0,0 +1,43 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "http://bonus.letai.ru/";
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html = AnyBalance.requestGet(baseurl + 'portal/site/letaibonus/template.LOGIN/', g_headers);
+
+    var VGN_NONCE = getParam(html, null, null, /VGN_NONCE"\s*value="([\s\S]*?)"/i, null, html_entity_decode);
+    if(!VGN_NONCE)
+        throw new AnyBalance.Error('Не удалось токен авторизации. Сайт изменен?');
+
+    html = AnyBalance.requestPost(baseurl + 'portal/site/letaibonus/template.LOGIN/action.process/', {
+        'entrance':'login', 
+        'VGN_NONCE':VGN_NONCE, 
+        login:prefs.login,
+        password:prefs.password,
+        Submit223:'Войти'
+    }, g_headers); 
+
+    if(!/LOGOUT/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+	html = AnyBalance.requestGet(baseurl + 'portal/site/letaibonus/profile/', g_headers);
+
+    var result = {success: true};
+    getParam(html, result, 'fio', /ФИО:\s*<\/dt>\s*<dd>([\s\S]*?)<\/dd>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'acc', /Номер участия в программе Летай Бонус:\s*<\/dt>\s*<dd>([\s\S]*?)<\/dd>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /<strong>([\s\S]{1,20})бонусных баллов<\/strong>/i, replaceTagsAndSpaces, parseBalance);
+
+    AnyBalance.setResult(result);
+}
---
+++ trunk/ab-internet-bttk/main.js
@@ -0,0 +1,37 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://188.244.184.44/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html = AnyBalance.requestPost(baseurl + 'login', {
+		'action.remote_login.1f71s9.x':'20',
+		'action.remote_login.1f71s9.y':'5',
+		'ej71s9':'0',
+		'fj71s9':'true',
+		'redirect':'',
+		login_remote3xpht9:prefs.login,
+        password_remote4xpht9:prefs.password,
+    }, addHeaders({Referer:'https://188.244.184.44/'})); 
+
+	if(!/Выход/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+    var result = {success: true};
+    getParam(html, result, 'fio', /Наименование клиента[\s\S]{1,150}valign="top">([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Итого на[\s\S]*?<b>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'overpay', /Переплата([\s\S]*?)руб/i, replaceTagsAndSpaces, parseBalance);
+
+    AnyBalance.setResult(result);
+}

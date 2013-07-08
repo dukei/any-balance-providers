cfa352eb50ff519361461c87cde687d3752e77d6
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-07-08 15:07:11 +0000

    skype updated to v 2.13

--- trunk/ab-telephony-skype/main.js
+++ trunk/ab-telephony-skype/main.js
@@ -8,103 +8,6 @@ Skype IP-телефония
 Не отлаживается в AnyBalanceDebugger из-за бага в webkit: 
 http://code.google.com/p/chromium/issues/detail?id=96136
 */
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && !AnyBalance.isAvailable (param))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-        }
-}
-
-function sumParam (html, result, param, regexp, replaces, parser, do_replace) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param))){
-            if(do_replace)
-  	        return html;
-            else
-                return;
-	}
-
-        var total_value;
-	var html_copy = html.replace(regexp, function(str, value){
-		for (var i = 0; replaces && i < replaces.length; i += 2) {
-			value = value.replace (replaces[i], replaces[i+1]);
-		}
-		if (parser)
-			value = parser (value);
-                if(typeof(total_value) == 'undefined')
-                	total_value = value;
-                else
-                	total_value += value;
-                return ''; //Вырезаем то, что заматчили
-        });
-
-    if(param){
-      if(typeof(total_value) != 'undefined'){
-          if(typeof(result[param]) == 'undefined')
-      	      result[param] = total_value;
-          else 
-      	      result[param] += total_value;
-      }
-      if(do_replace)
-          return html_copy;
-    }else{
-      return total_value;
-    }
-}
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var _text = html_entity_decode(text.replace(/\s+/, ''));
-    var val = getParam(_text, null, null, /(-?\d[\d\.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseCurrency(text){
-    var val = html_entity_decode(text.replace(/\s+/, '')).replace(/[\-\d\.,]+/,'');
-    AnyBalance.trace('Parsing currency (' + val + ') from: ' + text);
-    return val;
-}
-
-function createFormParams(html, process){
-    var params = {};
-    html.replace(/<input[^>]+name="([^"]*)"[^>]*>/ig, function(str, name){
-        var value = getParam(str, null, null, /value="([^"]*)"/i, null, html_entity_decode);
-        name = html_entity_decode(name);
-        if(process){
-            value = process(params, str, name, value);
-        }
-        params[name] = value;
-    });
-    return params;
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');    
@@ -142,8 +45,8 @@ function main(){
     var matches;
 
     //getParam(info, result, 'balance', /<a[^>]*class="credit(?:ATU)? icon[\s\S]*?<a[^>]*class="first[^>]*>([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'balance', /class="credit(?:ATU)? icon[\s\S]*?class="first[^>]*>([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(info, result, 'currency', /class="credit(?:ATU)? icon[\s\S]*?class="first[^>]*>([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseCurrency);
+	getParam(info, result, 'balance', /class="credit[\s\S]{1,50}icon[\s\S]*?class="first">([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+    getParam(info, result, 'currency', /class="credit[\s\S]{1,50}icon[\s\S]*?class="first">([\s\S]*?)<\//i, replaceTagsAndSpaces, parseCurrency);
     
     if(AnyBalance.isAvailable('subscriptions')){
     	//getParam(info, result, 'subscriptions', /<li[^>]+class="subs"[^>]*>([\s\S]*?)<(?:ul|li)[^>]*>/i, replaceTagsAndSpaces, parseBalance);

141e17dc3602ccc931ad2a834a37731223fe5750
stark027 <stark027@gmail.com>
2013-07-08 14:23:05 +0000

    added inti-net telos-bratsk globalsim

---
+++ ab-internet-teleos-bratsk/main.js
@@ -0,0 +1,39 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://stat.teleos.ru/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+	
+	var html = AnyBalance.requestPost(baseurl + 'login', {
+        'LoginForm[username]':prefs.login,
+        'LoginForm[password]':prefs.password,
+        'LoginForm[rememberMe]':'0',
+		'yt0':'Войти'
+    }, addHeaders({Referer: baseurl + 'login'})); 
+
+    if(!/logout/i.test(html)){
+        var error = getParam(html, null, null, /class="errorMessage">([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+    var result = {success: true};
+    getParam(html, result, 'acc_num', /Лицевой счет[\s\S]{1,100}value">([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'abon', /Абонентская плата[\s\S]*?([\s\S]*?)Р./i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance', /Баланс[\s\S]*?([\s\S]*?)Р./i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /service-item-tariff">([\s\S]*?)<\/span/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'status', /service-item-params-caption service-item-params-cel[\s\S]{1,50}(Интернет[\s\S]*?)\s*<\//i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}
---
+++ ab-telephony-globalsim/main.js
@@ -0,0 +1,48 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'http://simglobalsim.ru/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html = AnyBalance.requestGet(baseurl + 'emptyfrontpage?destination=emptyfrontpage', g_headers);
+
+    var form_build_id = getParam(html, null, null, /name="form_build_id"\s*id="([\s\S]*?)"/i, null, html_entity_decode);
+    if(!form_build_id)
+        throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+
+	html = AnyBalance.requestPost(baseurl + 'emptyfrontpage?destination=emptyfrontpage', {
+        'form_build_id':form_build_id, 
+        name:prefs.login,
+        pass:prefs.password,
+		'form_id':'user_login_block',
+		'nachricht':'',
+		'op':'Войти',
+    }, g_headers); 
+	
+    if(!/Logout/i.test(html)){
+        var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+	
+	html = AnyBalance.requestGet(baseurl + 'user', g_headers);
+
+    var result = {success: true};
+    getParam(html, result, 'fio', /Название[\s\S]*?<dd>([\s\S]*?)<\/dd>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'phone', /Телефон GLOBALSIM[\s\S]*?<dd>([\s\S]*?)<\/dd>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Баланс[\s\S]{1,50}<dd>([\s\S]*?)<\/dd>/i, replaceTagsAndSpaces, parseBalance);
+
+    AnyBalance.setResult(result);
+}

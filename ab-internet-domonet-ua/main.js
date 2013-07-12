cf49d2d5fd3a6d6a51d66ef9e1e11683bbd2bdd0
stark027 <stark027@gmail.com>
2013-07-12 06:51:09 +0000

    domonet updated to v2

--- ab-internet-domonet-ua/main.js
+++ ab-internet-domonet-ua/main.js
@@ -3,17 +3,19 @@
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = "https://my.domonet.ua/";
     AnyBalance.setDefaultCharset('utf-8'); 
+	
+	AnyBalance.requestGet(baseurl, g_headers);
 
 	var html = AnyBalance.requestPost(baseurl, {
         UserName:prefs.login,

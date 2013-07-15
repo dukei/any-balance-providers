ff0389e09f81d4bc1e6c3af77a317c128f78562c
stark027 <stark027@gmail.com>
2013-07-15 09:54:04 +0000

    pildyk updated to v4

--- ab-cell-pildyk/main.js
+++ ab-cell-pildyk/main.js
@@ -6,9 +6,7 @@
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = "https://mano.pildyk.lt/Prisijungti.aspx";
-    var result = {success: true},matches; 
-    
+    var baseurl = 'https://mano.pildyk.lt/';
     AnyBalance.setDefaultCharset('utf-8'); 
     
     if(!prefs.login)
@@ -20,7 +18,7 @@ function main(){
 	
     if (!/<a id="ctl00_ctl00_main_ctl00_userInfo_logoff" class="right logout" href="[\s\S]*?">Atsijungti<\/a>/i.test(html)) {
 	
-		html = AnyBalance.requestPost(baseurl, {
+		html = AnyBalance.requestPost(baseurl + 'Prisijungti.aspx', {
 			'ctl00$ctl00$main$main$login$tbUserName':prefs.login
 			,'ctl00$ctl00$main$main$login$tbPassword':prefs.password
 			,'__EVENTTARGET':'ctl00$ctl00$main$main$login$btnLogin'
@@ -29,15 +27,17 @@ function main(){
 			,'__EVENTVALIDATION':getParam(html, null, null, /<input\s*type="hidden"\s*name="__EVENTVALIDATION"\s*id="__EVENTVALIDATION"\s*value="([^\"]*)"\s*\/>/i, null, null)
 			,'ctl00$ctl00$main$main$login$btnLogin':'Prisijungti'
 		});
-	}
+	}	
 	
 	var error = getParam(html, null, null, /<div[\s\S]*?class="msg msg-error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
     if(error)
         throw new AnyBalance.Error(error);
         
-    getParam(html, result, 'balance', /<p\s*id="userinfo"\s*[^>]*>[\s\S]*?Sąskaitos\s*likutis:\s*<strong>([^>]*?)\s*LT<\/strong>[\s\S]*?<\/p>/i, replaceTagsAndSpaces, parseBalance);
+	var result = {success: true}; 
+
+	getParam(html, result, 'balance', /<p\s*id="userinfo"\s*[^>]*>[\s\S]*?Sąskaitos\s*likutis:\s*<strong>([^>]*?)\s*LT<\/strong>[\s\S]*?<\/p>/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'name', /<p\s*id="userinfo"\s*[^>]*>[\s\S]*?Prisijungęs:\s*<strong>([^>]*?)\s*<\/strong>[\s\S]*?<\/p>/i, replaceTagsAndSpaces, null);
-    getParam(html, result, 'balanceExpire', /<th>Sąskaitos\s*likutis:[\s\S]*?<small\sclass="expiration_date">\s*galioja\s*iki\s*([^>]*?)\s*<\/small>\s*<\/th>/i, replaceTagsAndSpaces, null);
+    getParam(html, result, 'balanceExpire', /<th>Sąskaitos\s*likutis:[\s\S]*?<small\sclass="expiration_date">\s*galioja\s*iki\s*([^>]*?)\s*<\/small>\s*<\/th>/i, replaceTagsAndSpaces, parseDateISO);
     
     getParam('+370 ' + prefs.login, result, 'telnum', /(.*)/i, replaceTagsAndSpaces, null);    
            

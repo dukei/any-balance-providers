2817c7b245132520af8666228e5ae6064d300684
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-07-22 07:17:39 +0000

    Квидекс - исправление получения трафика в терабайтах и осовременивание

--- trunk/ab-internet-kvidex/main.js
+++ trunk/ab-internet-kvidex/main.js
@@ -7,65 +7,6 @@
 Личный кабинет: https://stat.kvidex.ru/
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseTraffic(text){
-    var _text = text.replace(/\s+/, '');
-    var val = getParam(_text, null, null, /(-?\d[\d\.,]*)/, replaceFloat, parseFloat);
-    var units = getParam(_text, null, null, /(kb|mb|gb|кб|мб|гб|байт|bytes)/i);
-    switch(units.toLowerCase()){
-      case 'bytes':
-      case 'байт':
-        val = Math.round(val/1024/1024/1024*100)/100;
-        break;
-      case 'kb':
-      case 'кб':
-        val = Math.round(val/1024/1024*100)/100;
-        break;
-      case 'gb':
-      case 'гб':
-        val = Math.round(val*100)/100;
-        break;
-    }
-    var textval = ''+val;
-    if(textval.length > 6)
-      val = Math.round(val);
-    else if(textval.length > 5)
-      val = Math.round(val*10)/10;
-
-    AnyBalance.trace('Parsing traffic (' + val + ') from: ' + text);
-    return val;
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
 
@@ -95,12 +36,3 @@ function main(){
 
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

a31488aec139e6eb6e2e64d4a3e5d16acf1dc7e8
anisimov26@gmail.com <anisimov26@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-07-22 11:55:35 +0000

    narodmon v.1.0.3

--- trunk/ab-weather-narodmon/main.js
+++ trunk/ab-weather-narodmon/main.js
@@ -1,66 +1,18 @@
-/**
-Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Текущий баланс у калининградского оператора интернет Диалог.
-
-Сайт оператора: http://tis-dialog.ru/
-Личный кабинет: https://stats.tis-dialog.ru
-*/
-
-function getParam (html, result, param, regexp, replaces, parser) {
-        if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-                return;
-
-        var value = regexp.exec (html);
-        if (value) {
-                value = value[1];
-                if (replaces) {
-                        for (var i = 0; i < replaces.length; i += 2) {
-                                value = value.replace (replaces[i], replaces[i+1]);
-                        }
-                }
-                if (parser)
-                        value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-        }
-}
-
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
 function main(){
-        AnyBalance.setDefaultCharset('windows-1251');
+    AnyBalance.setDefaultCharset('utf-8');
     var prefs = AnyBalance.getPreferences();
-    var number = prefs.number || prefs.numberList;
+    var number = prefs.number;
 
-    var baseurl = "http://moe26.ru/projects/narodmon/?id="+number;
+    var baseurl = "http://narodmon.ru/client.php";
 
-    var html = AnyBalance.requestGet(baseurl);
-
-    var error = getParam(html, null, null, /<\/h1>-->([\n\r\s ]+?)<\/div>/i);
-    if(error)
+    var html = AnyBalance.requestPost(baseurl,'{"cmd":"sensorDev","id":"'+number+'","api_key":"90FeOzSOINuLI","uuid":"5d41402abc4b2a76b9719d911017c592"}');
+	var info=$.parseJSON(html);
+    if(info.sensors==null)
         throw new AnyBalance.Error("Датчик с номером " + number + " отсутствует. Проверьте номер!");
-
+		
     var result = {success: true};
-
- 
-    getParam(html, result, 'temperature', /&nbsp;<b>([\d.+-]*?)&deg;<\/b>/i, replaceFloat, parseBalance);
-    getParam(html, result, 'humidity', /&nbsp;<b>([\d]*?)%<\/b>/i, replaceFloat, parseBalance);
-	getParam(html, result, 'pressure', /&nbsp;<b>([\d]*?)mmHg<\/b>/i, replaceFloat, parseBalance);
-
-
-
-
+	if (info.sensors.length>=1) result.temperature=info.sensors[0].value;
+	if (info.sensors.length>=2) result.humidity=info.sensors[1].value;
+	if (info.sensors.length>=3) result.pressure=info.sensors[2].value;
     AnyBalance.setResult(result);
 }
-

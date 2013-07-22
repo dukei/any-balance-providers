3433da55f65cf3826b4452370f5683189acd2900
stark027 <stark027@gmail.com>
2013-07-22 07:25:11 +0000

    added

---
+++ ab-service-alpha-sms-ua/main.js
@@ -0,0 +1,30 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'http://www.alphasms.ua/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+	var html = AnyBalance.requestPost(baseurl + 'account/login/', {
+        login:prefs.login,
+        password:prefs.password,
+    }, addHeaders({Referer: baseurl + 'account/login/'})); 
+
+    if(!/\/logout/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    var result = {success: true};
+    getParam(html, result, 'balance', /Баланс:([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    AnyBalance.setResult(result);
+}

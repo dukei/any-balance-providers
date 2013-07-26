846b020d101185166167a9a96e44bdc256be6446
dmitry.kochin <dmitry.kochin@gmail.com>
2013-07-26 17:16:02 +0000

    sgku - только на новой версии anybalance

---
+++ ab-municipal-sgku/main.js
@@ -0,0 +1,58 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для оператора  https://sgku.ru
+
+Operator site: https://sgku.ru
+Личный кабинет: https://sgku.ru
+*/
+
+var g_headers = {
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+'Connection':'keep-alive',
+'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "https://sgku.ru/";
+
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html;
+    if(prefs.locality == 'Сургут'){
+        html = AnyBalance.requestGet(baseurl + 'accounts?locality=' + encodeURIComponent(prefs.locality) + '&street=' + encodeURIComponent(prefs.street) + '&house=' + prefs.house + '&flat=' + prefs.flat, addHeaders({Referer: baseurl})); 
+    }else{
+        html = AnyBalance.requestGet(baseurl + 'accounts?locality=' + encodeURIComponent(prefs.locality) + '&account=' + encodeURIComponent(prefs.account), addHeaders({Referer: baseurl})); 
+    }
+
+    //После входа обязательно проверяем маркер успешного входа
+    //Обычно это ссылка на выход, хотя иногда приходится искать что-то ещё
+    if(!/Лицевой счёт:/i.test(html)){
+        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
+        var error = getParam(html, null, null, /<div class="wrap">\s*<p[^>]+class="error"[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
+        throw new AnyBalance.Error('Не удалось получить информацию. Сайт изменен?');
+    }
+
+    //Раз мы здесь, то мы успешно вошли в кабинет
+    //Получаем все счетчики
+    var result = {success: true};
+    getParam(html, result, 'address', /<div[^>]+class="row content"[^>]*>\s*<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /<div[^>]+class="row content"[^>]*>\s*<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /Лицевой счёт:\s*<strong[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    var kvpl = getParam(html, null, null, /Квартплата([\s\S]*?)<\/div><br/i);
+    if(kvpl){
+        getParam(html, result, 'balance', /(?:Переплата|Долг|Задолженность):\s*<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'peni', /Пени:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'date', /На дату:([^<]*)/i, replaceTagsAndSpaces, parseDate);
+    }
+
+    //Возвращаем результат
+    AnyBalance.setResult(result);
+}
---
+++ ab-municipal-sgku/preferences.js
@@ -0,0 +1,25 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифном плане для оператора  https://sgku.ru
+
+Operator site: https://sgku.ru
+Личный кабинет: https://sgku.ru
+*/
+
+function onChangeLocality(){
+    var locality = AnyBalance.getPreference('locality');
+    var surgut = (locality.get('value') == 'Сургут');
+
+    AnyBalance.getPreference('street').set('visible', surgut);
+    AnyBalance.getPreference('house').set('visible', surgut);
+    AnyBalance.getPreference('flat').set('visible', surgut);
+    AnyBalance.getPreference('account').set('visible', !surgut);
+}
+
+function main(){
+    var baseurl = "https://sgku.ru/";
+    AnyBalance.trace('sdfas');
+    AnyBalance.addCallback('change#locality', onChangeLocality);
+    onChangeLocality();
+}

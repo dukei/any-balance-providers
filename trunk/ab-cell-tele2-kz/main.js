69253284c4573134509842893370e159fd0180b7
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-07-29 07:23:54 +0000

    v4

--- trunk/ab-cell-tele2-kz/main.js
+++ trunk/ab-cell-tele2-kz/main.js
@@ -15,10 +15,10 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-
+	
     var baseurl = "https://iself.tele2.kz/";
     AnyBalance.setDefaultCharset('utf-8'); 
-
+	
 	// Значала зачем-то проверяем номер, какую-то куку ставит еще...
 	var json = {msisdn: prefs.login};
     var html = AnyBalance.requestPost(baseurl + 'auth/checkMsisdn.json', JSON.stringify(json), g_headers);
@@ -45,31 +45,27 @@ function main(){
 
     var result = {success: true};
     getParam(html, result, 'fio', /"profile">[\s\S]*?([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /class="header">([\s\S]*?)\s*тенге/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balance', /class="profile-balance">([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 	
 	try{
 		// Сайт возвращает JSON c доп. балансами, они -то нам и нужны
 		html = AnyBalance.requestPost(baseurl + 'balanceres', '', addHeaders({Referer: baseurl + 'profile'}));
 		json = getJson(html);
-                var v;
-                if(isset(json.returns[0])){ //Ежемесячные
+		var v;
+        if(isset(json.returns[0])){ //Ежемесячные
 		    v = Math.round(json.returns[0].volume);
 		    getParam('' + v, result, 'internet_trafic', null, null, parseBalance);
-                }
-
-                if(isset(json.returns[1])){ //Еженедельные
-		    v = Math.round(json.returns[1].volume);
+        }
+		if(isset(json.returns[1])){ //Еженедельные
+			v = Math.round(json.returns[1].volume);
+			getParam('' + v, result, 'internet_trafic', null, null, parseBalance);
+        }
+		if(isset(json.returns[2])){ //Ежедневные
+			v = Math.round(json.returns[2].volume);
 		    getParam('' + v, result, 'internet_trafic', null, null, parseBalance);
-                }
-		
-                if(isset(json.returns[2])){ //Ежедневные
-		    v = Math.round(json.returns[2].volume);
-		    getParam('' + v, result, 'internet_trafic', null, null, parseBalance);
-                }
-		
+		}
 	} catch(e){
-		AnyBalance.trace('Попытались получить баланс Интернет-пакеты, из-за ошибки ' + e);
+		AnyBalance.trace('Ошибка при получении Интернет-пакетов: ' + e);
 	}
-
     AnyBalance.setResult(result);
 }

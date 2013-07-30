bc360d73d1b48fa2defd2a698108aa583881d451
dmitry.kochin <dmitry.kochin@gmail.com>
2013-07-30 05:14:34 +0000

    Paypal - получение валюты счета

--- ab-money-paypal/main.js
+++ ab-money-paypal/main.js
@@ -19,31 +19,36 @@ function main(){
 
     var html = AnyBalance.requestGet(baseurl);
 
-    var href = getParam(html, null, null, /<form[^>]*action="([^"]+)"[^>]*name="login_form"/i, null, html_entity_decode);
-//    var csrfModel = getParam(html, null, null, /<input[^>]*name="csrfModel.returnedCsrf"[^>]*value="([^"]+)"/i);
-    if(!href /*|| !csrfModel*/)
-        throw new AnyBalance.Error("Can not find login form! Site is changed?");
+    if(!prefs.__dbg){
+        var href = getParam(html, null, null, /<form[^>]*action="([^"]+)"[^>]*name="login_form"/i, null, html_entity_decode);
+//  //    var csrfModel = getParam(html, null, null, /<input[^>]*name="csrfModel.returnedCsrf"[^>]*value="([^"]+)"/i);
+        if(!href /*|| !csrfModel*/)
+            throw new AnyBalance.Error("Can not find login form! Site is changed?");
 	
-    html = AnyBalance.requestPost(href, {
-//      "csrfModel.returnedCsrf": csrfModel,
-      login_email: prefs.login,
-      login_password:prefs.password,
-      "submit.x": "Log in"
-    });
+        html = AnyBalance.requestPost(href, {
+//  //      "csrfModel.returnedCsrf": csrfModel,
+          login_email: prefs.login,
+          login_password:prefs.password,
+          "submit.x": "Log in"
+        });
 
-    var error = getParam(html, null, null, /<div[^"]*class="[^"]*error[^"]*">\s*<h2[^>]*>[^<]*<\/h2>\s*<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces);
-    if(error)
-        throw new AnyBalance.Error(error);
+        var error = getParam(html, null, null, /<div[^"]*class="[^"]*error[^"]*">\s*<h2[^>]*>[^<]*<\/h2>\s*<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces);
+        if(error)
+            throw new AnyBalance.Error(error);
+        
+        href = getParam(html, null, null, /"([^"]*(?:%5f|_)login(?:%2d|-)done[^"]*)/i, null, html_entity_decode);
+        if(!href)
+            throw new AnyBalance.Error("Can not log in. Wrong login, password or site is changed.");
+        
+        html = AnyBalance.requestGet(href);
+    }else{
+        html = AnyBalance.requestGet('https://www.paypal.com/webapps/hub/');
+    }
 
-    href = getParam(html, null, null, /"([^"]*(?:%5f|_)login(?:%2d|-)done[^"]*)/i, null, html_entity_decode);
-    if(!href)
-        throw new AnyBalance.Error("Can not log in. Wrong login, password or site is changed.");
-
-    html = AnyBalance.requestGet(href);
 
     var result={success: true};
 
-	getParam(html, result, 'currency', /<span[^>]*class="balance[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseCurrency);
+	getParam(html, result, ['currency','balance'], /<span[^>]*class="balance[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseCurrency);
     getParam(html, result, 'balance', /<span[^>]*class="balance[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
 
 	

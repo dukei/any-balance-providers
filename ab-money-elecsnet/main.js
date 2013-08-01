0a41fa435f40d37a3ba9e072a1e3bab5e12091eb
stark027 <stark027@gmail.com>
2013-08-01 14:00:43 +0000

    v2

--- ab-money-elecsnet/main.js
+++ ab-money-elecsnet/main.js
@@ -1,83 +1,38 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс на кошельке Элекснет
-
-Сайт оператора: http://elecsnet.ru
-Личный кабинет: https://services.elecsnet.ru
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/<span[^>]+display:\s*none[^>]*>[\s\S]*?<\/span>/gi, ' ', /&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function getViewState(html){
-    return getParam(html, null, null, /name="__VIEWSTATE".*?value="([^"]*)"/);
-}
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36',
+	'Origin':'https://1.elecsnet.ru'
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    AnyBalance.setDefaultCharset('windows-1251');
+    var baseurl = 'https://1.elecsnet.ru/';
+    AnyBalance.setDefaultCharset('utf-8'); 
 
-    var baseurl = "https://services.elecsnet.ru/notebook/login.aspx";
+    var html = AnyBalance.requestGet(baseurl + 'NotebookFront/Default.aspx', g_headers);
 
-    var html = AnyBalance.requestGet(baseurl);
-
-    html = AnyBalance.requestPost(baseurl, {
-        __EVENTTARGET:'',
-        __EVENTARGUMENT:'',
-        __VIEWSTATE:getViewState(html),
-        MainLogin$UserName:prefs.login,
-        MainLogin$Password:prefs.password,
-        MainLogin$LoginButton:'Войти'
+	var params = createFormParams(html, function(params, str, name, value){
+		if(name == 'ctl00$MainLogin$UserName')
+			return prefs.login;
+		else if(name == 'ctl00$MainLogin$Password')
+			return prefs.password;
+        return value;
     });
+	html = AnyBalance.requestPost(baseurl + 'NotebookFront/Default.aspx', params, addHeaders({Referer: baseurl+'NotebookFront/Default.aspx'})); 
 
-    if(!/\WOut.aspx/i.test(html)){
-        var error = getParam(html, null, null, /<p[^>]*>((?:[\s\S](?!<p))*.)<\/p>\s*<\/td>\s*<\/tr>\s*<\/table>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    if(!/ExitButton/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 
     var result = {success: true};
-
-    getParam(html, result, 'balance', /<span[^>]+id="ctl00_LabelBalance"[^>]*>([\S\s]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-    result.__tariff = prefs.login;
-    
+    getParam(html, result, 'balance', /Остаток на кошельке:[\s\S]*?>([\s\S]*?)руб/i, replaceTagsAndSpaces, parseBalance);
+	getParam(prefs.login, result, 'phone', null, null, null);
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

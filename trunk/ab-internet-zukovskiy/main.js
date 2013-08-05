822bde1492a07f7bf53e7d65ddc55263bb577817
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-08-05 14:11:01 +0000

    added

---
+++ trunk/ab-internet-zukovskiy/main.js
@@ -0,0 +1,33 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://stat.zhukovsky.net/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+	var html = AnyBalance.requestPost(baseurl + 'index.php', {
+        login:prefs.login,
+        password:prefs.password,
+    }, addHeaders({Referer: baseurl + 'login'})); 
+
+    if(!/Выход/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    var result = {success: true};
+    getParam(html, result, 'fio', /Вы:[\s\S]*?<td>([\s\S]*?)<\/td/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Баланс[\s\S]*?style="color: red">([\s\S]*?)</i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, '__tariff', /Изменить тариф[\s\S]*?>([\s\S]*?)</i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}

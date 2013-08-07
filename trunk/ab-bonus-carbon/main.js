db05e18ac98445f58b1273de978066f55f1da3de
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-08-07 10:48:39 +0000

    v3

--- trunk/ab-bonus-carbon/main.js
+++ trunk/ab-bonus-carbon/main.js
@@ -7,19 +7,46 @@
 Личный кабинет: https://pay.carbon-card.ru/personal/pub/Login
 */
 
+function parseBalanceRK(_text){
+    var text = _text.replace(/\s+/g, '');
+    var rub = getParam(text, null, null, /(-?\d[\d\.,]*)(?:&thinsp;)/i, replaceFloat, parseFloat) || 0;
+    var kop = getParam(text, null, null, /руб[\s\S]*?(-?\d[\d\.,]*)/i, replaceFloat, parseFloat) || 0;
+    var val = rub+kop/100;
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + _text);
+    return val;
+}
+
 function main() {
 	var result = {success: true};
     var prefs = AnyBalance.getPreferences();
-    
+	AnyBalance.setDefaultCharset('utf-8');
+	
     var html = AnyBalance.requestPost('https://pay.carbon-card.ru/personal/pub/Login?wicket:interface=:1:frmLogin::IFormSubmitListener::', {
         'login':prefs.login,
         'password':prefs.password
     });
 
-	var regexp;
+    if(!/Выйти/i.test(html)){
+        var error = getParam(html, null, null, /inputBadPwdOrCard" class="error">([\s\S]+?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    var result = {success: true};
+    getParam(html, result, 'balance', /"balance-value"[\s\S]*?">([\s\S]*?availableBalanceKopecks[\s\S]*?коп)/i, replaceTagsAndSpaces, parseBalanceRK);
+	getParam(html, result, 'active', /"bonuspoints">[\s\S]*?<\/span>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'deferred', /deferred[\s\S]*?<span class="bonuspoints">([\s\S]*?)</i, replaceTagsAndSpaces, parseBalance);
+	
+    AnyBalance.setResult(result);
+	
+	
+	
+	
+	/*var regexp;
 	if(html.indexOf('<a href="#" class="exit"')!=-1) {
 		//Целая часть баланса
-		regexp=new RegExp('<span class="balance-value" testid="availableBalance" id="[^"]+">([0-9 ]+)</span>');
+		regexp = new RegExp('<span class="balance-value" testid="availableBalance" id="[^"]+">([0-9 ]+)</span>');
 		matches=regexp.exec(html);
 		if(matches==null) throw new AnyBalance.Error("Ошибка получения первой части баланса.");
 		result.balance=parseFloat(matches[1].replace(" ",""));
@@ -29,7 +56,7 @@ function main() {
 		matches=regexp.exec(html);
 		if(matches==null) throw new AnyBalance.Error("Ошибка получения второй части баланса.");
 		result.balance=result.balance+parseFloat(matches[1])/Math.pow(10,matches[1].length);
-
+	
 		//Активные бонусы
 		regexp=new RegExp('<div class="active">[\\s\\S]+?<span class="bonuspoints">([0-9. ]+)</span>');
 		matches=regexp.exec(html);
@@ -54,6 +81,6 @@ function main() {
 		throw new AnyBalance.Error("Авторизация не удалась. Попробуйте повторить попытку позднее.");
 	}
 
-    AnyBalance.setResult(result);
+    AnyBalance.setResult(result);*/
 }
 

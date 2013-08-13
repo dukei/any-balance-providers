1360a7899f21f0a76bf9b9badb3fe9952d405e86
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-08-13 16:57:54 +0000

    rp5 - исправлено получение давления ну и избавились от jquery

--- trunk/ab-weather-rp5/main.js
+++ trunk/ab-weather-rp5/main.js
@@ -5,18 +5,19 @@
 XML данные с http://rp5.ru/docs/xml/ru
 */
 
-function parseDate(str){
-  //Рассчитывает на библиотеку date.js
-  var dt = Date.parse(str);
-  if(!dt){
-      AnyBalance.trace('Can not parse date from ' + str);
-      return;
-  }
-
-  dt = new Date(dt);
-  
-  AnyBalance.trace('Parsed date ' + dt.toString() + ' from ' + str);
-  return dt.getTime(); 
+function parseDateMoment(str){
+    var mom = moment(str.replace(/i/ig, 'і'), ['D MMM, HH:mm', 'DD MMM']);
+    if(!mom.isValid()){
+        AnyBalance.trace('Failed to parse date from ' + str);
+    }else{
+        var now = moment();
+        mom.add('years', now.year());
+        if(mom.isBefore(now))
+            mom.add('years', 1);
+        var val = mom.toDate();
+        AnyBalance.trace('Parsed date ' + val + ' from ' + str);
+        return val.getTime();
+    }
 }
 
 function main(){
@@ -30,6 +31,7 @@ function main(){
 
         var retry = false;
         try{
+//            var x = y;
             var xml = AnyBalance.requestGet("http://rp5.ru/xml/"+prefs.city+"/00000/ru");
             retry = /404 Not Found/i.test(xml);
         }catch(e){
@@ -49,19 +51,21 @@ function parseHtml(html){
         if(!/\/wap\/style.css/i.test(html))
             throw new AnyBalance.Error('Не удаётся получить данные по выбранному городу. Неверный код города?');
 
+        moment.lang('ru');
+
 	var result = {success: true};
         getParam(html, result, '__tariff', /<h1[^>]*>([\s\S]*?)<\/h1>/i, replaceTagsAndSpaces, html_entity_decode);
 
         var wasToday = false;
         html.replace(/<tr><td><b>(?:пн|вт|ср|чт|пт|сб|вс)(?:[\s\S]*?<\/tr>){5}/ig, function(tr){
-            var time = getParam(tr, null, null, /<tr><td><b>(?:пн|вт|ср|чт|пт|сб|вс),([^<]*)/i, replaceTagsAndSpaces, parseDate);
+            var time = getParam(tr, null, null, /<tr><td><b>(?:пн|вт|ср|чт|пт|сб|вс),([^<]*)/i, replaceTagsAndSpaces, parseDateMoment);
             var hour = new Date(time).getHours();
             
             if(8 < hour && hour <= 20){ //Это день
                 var suffix = wasToday ? '2' : '1';
                 wasToday = true;
 
-                getParam(tr, result, 'date'+suffix, /<tr><td><b>(?:пн|вт|ср|чт|пт|сб|вс),([^<]*)/i, replaceTagsAndSpaces, parseDate);
+                getParam(tr, result, 'date'+suffix, /<tr><td><b>(?:пн|вт|ср|чт|пт|сб|вс),([^<]*)/i, replaceTagsAndSpaces, parseDateMoment);
                 getParam(tr, result, 'cloud'+suffix, /облачность([^<]*)/i, replaceTagsAndSpaces, parseBalance);
                 getParam(tr, result, 'temp'+suffix, /(?:[\s\S]*?<tr[^>]*>){4}([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, parseBalance);
                 getParam(tr, result, 'humidity'+suffix, /влажность([^<]*)/i, replaceTagsAndSpaces, parseBalance);
@@ -69,6 +73,8 @@ function parseHtml(html){
                 getParam(tr, result, 'wind_vel'+suffix, />ветер(.*?)м\/сек/i, replaceTagsAndSpaces, parseBalance);
                 getParam(tr, result, 'falls'+suffix, /(?:[\s\S]*?<tr[^>]*>){4}.*?,([^\(,]*)/i, replaceTagsAndSpaces, html_entity_decode);
                 getParam(tr, result, 'precip'+suffix, /(?:[\s\S]*?<tr[^>]*>){4}.*?,(.*?)мм/i, replaceTagsAndSpaces, parseBalance);
+                getParam(tr, result, 'pressure'+suffix, /давление\s*([\d.,]+)/i, replaceTagsAndSpaces, parseBalance);
+
                 
             }
         });
@@ -76,47 +82,45 @@ function parseHtml(html){
 	AnyBalance.setResult(result);
 }
 
-function parseXml(xml){
-	var xmlDoc = $.parseXML(xml),
-          $xml = $(xmlDoc);
+function getXMLParam(xml, result, name, tag, repl, decode){
+    var regExp = new RegExp('<' + tag + '>([\\s\\S]*?)</' + tag + '>', 'i');
+    return getParam(xml, result, name, regExp, repl, decode);
+}
 
-        if(!$xml.find('point>point_name').text() && !$xml.find('point>country_id').text())
+function parseXml(xml){
+        if(!getXMLParam(xml, null, null, 'point_name') && !getXMLParam(xml, null, null, 'country_id'))
           throw new AnyBalance.Error("Похоже, код города " + prefs.city + " неверный.");
      
 	var result = {success: true};
-        if(AnyBalance.isAvailable('point_name'))
-            result.point_name = $xml.find('point>point_name').text();
-        result.__tariff = $xml.find('point>point_name').text();
-        
+
+        getXMLParam(xml, result, 'point_name', 'point_name', replaceTagsAndSpaces, html_entity_decode);
+        getXMLParam(xml, result, '__tariff', 'point_name', replaceTagsAndSpaces, html_entity_decode);
+
+        var timesteps = sumParam(xml, null, null, /<timestep>([\s\S]*?)<\/timestep>/ig);
         var wasToday = false;
-        $timesteps = $xml.find('timestep').each(function(index){
-            var $timestep = $(this);
-            var hour = parseInt($timestep.find('G').text());
+        for(var i=0; i<timesteps.length; ++i){
+            var timestep = timesteps[i];
+            var hour = getXMLParam(timestep, null, null, 'G', null, parseBalance);
            
             if(8 < hour && hour <= 20){ //Это день
                 var suffix = wasToday ? '2' : '1';
                 wasToday = true;
                 
-        	if(AnyBalance.isAvailable('date'+suffix))
-                    result['date'+suffix] = parseDate($timestep.find('datetime').text());
-        	if(AnyBalance.isAvailable('cloud'+suffix))
-                    result['cloud'+suffix] = parseFloat($timestep.find('cloud_cover').text());
-        	if(AnyBalance.isAvailable('temp'+suffix))
-                    result['temp'+suffix] = parseFloat($timestep.find('temperature').text());
-        	if(AnyBalance.isAvailable('humidity'+suffix))
-                    result['humidity'+suffix] = parseFloat($timestep.find('humidity').text());
-        	if(AnyBalance.isAvailable('wind_dir'+suffix))
-                    result['wind_dir'+suffix] = $timestep.find('wind_direction').text();
-        	if(AnyBalance.isAvailable('wind_vel'+suffix))
-                    result['wind_vel'+suffix] = parseFloat($timestep.find('wind_velocity').text());
-        	if(AnyBalance.isAvailable('falls'+suffix)){
-                    var falls = ['без осадков', 'дождь', 'дождь со снегом', 'снег'];
-                    result['falls'+suffix] = falls[parseInt($timestep.find('falls').text())];
-                }
-        	if(AnyBalance.isAvailable('precip'+suffix))
-                    result['precip'+suffix] = parseFloat($timestep.find('precipitation').text());
+                getXMLParam(timestep, result, 'date'+suffix, 'datetime', replaceTagsAndSpaces, parseDateISO);
+                getXMLParam(timestep, result, 'cloud'+suffix, 'cloud_cover', replaceTagsAndSpaces, parseBalance);
+                getXMLParam(timestep, result, 'temp'+suffix, 'temperature', replaceTagsAndSpaces, parseBalance);
+                getXMLParam(timestep, result, 'humidity'+suffix, 'humidity', replaceTagsAndSpaces, parseBalance);
+                getXMLParam(timestep, result, 'pressure'+suffix, 'pressure', replaceTagsAndSpaces, parseBalance);
+                getXMLParam(timestep, result, 'wind_dir'+suffix, 'wind_direction', replaceTagsAndSpaces, html_entity_decode);
+                getXMLParam(timestep, result, 'wind_vel'+suffix, 'wind_velocity', replaceTagsAndSpaces, parseBalance);
+                getXMLParam(timestep, result, 'precip'+suffix, 'precipitation', replaceTagsAndSpaces, parseBalance);
+
+                var falls = ['без осадков', 'дождь', 'дождь со снегом', 'снег'];
+                getXMLParam(timestep, result, 'wind_dir'+suffix, 'wind_direction', replaceTagsAndSpaces, function(str){
+                    return falls[parseInt(str)];
+                });
             }
-        });
+        };
 
 	AnyBalance.setResult(result);
 }

9ea0e7d11449fa8e0cb820738590a3ea97d37c34
stark027 <stark027@gmail.com>
2013-08-20 10:11:13 +0000

    IPNET (Киев) Мелкие улучшения

--- ab-internet-ipnet/main.js
+++ ab-internet-ipnet/main.js
@@ -10,37 +10,36 @@
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
-
+	
     if(AnyBalance.getLevel() < 5)
         throw new AnyBalance.Error('Для этого провайдера необходима AnyBalance v.2.9.600+.');
-
+	
     var baseurl = "https://stat.ipnet.ua/";
-
+	
     var html = AnyBalance.requestGet(baseurl);
     var url = AnyBalance.getLastUrl();
-
+	
     var html = AnyBalance.requestPost(url, {
         login:prefs.login,
         password:prefs.password,
         'auth.x': 63,
         'auth.y': 12
     });
-
-    //AnyBalance.trace(html);
+	
     if(!/logout/.test(html)){
         var error = getParam(html, null, null, /<font[^>]*color=["']?red[^>]*>([\s\S]*?)<\/font>/, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
-
+	
     var result = {success: true};
-
+	
     getParam(html, result, 'balance', /Текущий остаток[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'till', /Прогнозируемая дата ухода в минус[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
     getParam(html, result, 'status', /Текущее состояние[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'agreement', /Номер договора[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /Тарифный пакет[\S\s]*?<td[^>]*>([\S\s]*?)<\/t[dr]>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Тарифный пакет[\S\s]*?<td[^>]*center">([\S\s]*?)<\/t[dr]>/i, replaceTagsAndSpaces, html_entity_decode);
     
     AnyBalance.setResult(result);
 }

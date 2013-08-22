c60f1e0ec3cf7f68666c9e0ccaca5c0d83bb3f74
dmitry.kochin <dmitry.kochin@gmail.com>
2013-08-22 18:38:25 +0000

    Газпромбонус с капчей

--- ab-bonus-gazprom/main.js
+++ ab-bonus-gazprom/main.js
@@ -12,28 +12,40 @@
 1.0.2 - Добавил баланс в рублях по курсу программы.
 */
 
+function img2status(str){
+    var statuses = {
+        silver: 'Серебряный',
+        gold: 'Золотой',
+        platinum: 'Платиновый'
+    };
+
+    return statuses[str] || str;
+}
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
-	var baseurl='https://m.gpnbonus.ru/';
+	var baseurl='https://www.gpnbonus.ru/';
 	AnyBalance.trace('Sending a request for authorization');
         AnyBalance.setDefaultCharset('utf-8');
 
-        var html = AnyBalance.requestGet(baseurl + 'to_private_office/');
+        var html = AnyBalance.requestGet(baseurl + 'login/');
 
-        var form = getParam(html, null, null, /<form[^>]+class="loginForm"[^>]*>([\s\S]*?)<\/form>/i);
+        var form = getParam(html, null, null, /<form[^>]+id="login_form"[^>]*>([\s\S]*?)<\/form>/i);
+        if(!form)
+                throw new AnyBalance.Error('Не найдена форма входа. Сайт изменен?');
+         
         var params = createFormParams(form);
 
         if(params.captcha_code){
             if(AnyBalance.getLevel() >= 7){
                 AnyBalance.trace('Пытаемся ввести капчу');
-                var captcha = AnyBalance.requestGet('http://m.gpnbonus.ru/bitrix/tools/captcha.php?captcha_code=' + params.captcha_code);
-                params.captcha_word_2002 = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
-                AnyBalance.trace('Капча получена: ' + params.captcha_word_2002);
+                var captcha = AnyBalance.requestGet(baseurl + 'bitrix/tools/captcha.php?captcha_code=' + params.captcha_code);
+                params.captcha_word = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+                AnyBalance.trace('Капча получена: ' + params.captcha_word);
             }else{
                 throw new AnyBalance.Error('К сожалению, сайт http://www.gpnbonus.ru ввел капчу (ввод циферок с картинки) для входа в личный кабинет. Пожалуйста, обратитесь в справочную службу ГазПромНефть по телефону 8 800 700 5151 и попросите отменить капчу или сделать интерфейс для автоматических программ.');
             }
-        }
+        }                                                                	
 
         AnyBalance.trace('Отправляем данные: ' + JSON.stringify(params));
 
@@ -45,67 +57,41 @@ function main(){
 	if(/newpass/i.test(AnyBalance.getLastUrl())){
             throw new AnyBalance.Error('Газпромбонус просит сменить пароль. Пожалуйста, зайдите в личный кабинет через браузер и смените пароль.');
         }
+	if(/pass=false/i.test(AnyBalance.getLastUrl())){
+            throw new AnyBalance.Error('Неверный пароль. Пожалуйста, убедитесь, что вы правильно ввели пароль, и попробуйте ещё раз.');
+        }
  
 	if (!/Персональные данные/.exec(html)){
-                var error = getParam(html, null, null, /<div[^>]+errors_panel[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+                var error = getParam(html, null, null, /<ul[^>]+class="form-errors"[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
                 if(error)
                      throw new AnyBalance.Error(error);
 		AnyBalance.trace('Что-то не то, возможно проблемы с сайтом');
 		throw new AnyBalance.Error ('Не удаётся зайти в личный кабинет. Возможно, неправильный логин, пароль или сайт изменен.');
 	};
 	AnyBalance.trace('Authorization was successful');
-	var result = {success: true};
 	AnyBalance.trace('Start parsing...');
 
-//Баланс
-	regexp=/Бонусов доступно.*\n.*\n.*>(.*)</m;
-	if (res=regexp.exec(html)){
-		result.balance=res[1];
-	}
-//Сумма покупок за месяц
-   	regexp=/за текущий месяц.*\n.*\n.*>(.*) руб.</m;
-	if (res=regexp.exec(html)){
-		result.month=res[1];
-	}
-//Сумма для подтверждения статуса
-	regexp=/Для подтверждения статуса необходимо совершить покупки на сумму.*\n.*\n.*>(.*) руб.</m;
-	if (res=regexp.exec(html)){
-		result.month_need=res[1];
-	}
-//Владелец
-   	regexp=/.*href=\"\/profile\/main\/\">\n(.*)</m;
-	if (res=regexp.exec(html)){
-		result.customer=res[1];
-		res=/\t(\S+).*?(\S+)\t/.exec(result.customer);
-		result.customer=res[1]+' '+res[2];
-	}
-//Текущий статус
-	regexp=/.*Текущий статус карты.*\n.*\n.*>(.*?)</m;
-	if (res=regexp.exec(html)){
-		result.status=res[1];
-		result.__tariff=result.status;
-	}
-//Сумма для повышения статуса
-	if(AnyBalance.isAvailable('month_need_up')){
-		var url='https://www.gpnbonus.ru/profile/login/';
-		var html = AnyBalance.requestPost(url, prefs);
-		regexp=/Для повышения статуса необходимо совершить покупки на сумму.*\n.*DinPro fs24 .*>(.*) руб.</m;
-		if (res=regexp.exec(html)){
-			result.month_need_up=res[1];
-		}
-	}
+	var result = {success: true};
+        var balance = getParam(html, null, null, /Бонусов доступно[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'balance', /Бонусов доступно[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+
+        getParam(html, result, 'status', /Текущий статус карты[\s\S]*?<img[^>]+src="[^"]*images\/([^\/"]*)\.png"[^>]*>/i, replaceTagsAndSpaces, img2status);
+        getParam(html, result, '__tariff', /Текущий статус карты[\s\S]*?<img[^>]+src="[^"]*images\/([^\/"]*)\.png"[^>]*>/i, replaceTagsAndSpaces, img2status);
+        getParam(html, result, 'month_need', /Для подтверждения статуса необходимо совершить покупки на сумму[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'month', /Сумма покупок за текущий месяц. От нее зависит статус участника программы на следующий месяц.[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'customer', /<div[^>]+class="[^"]*PersonalName"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        getParam(html, result, 'month_need_up', /Для повышения статуса необходимо совершить покупки на сумму[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+
 //Баланс по курсу в рублях
-	if(AnyBalance.isAvailable('balance_money')){
-		html=AnyBalance.requestGet("https://www.gpnbonus.ru/on_the_way/");
-		regexp=/Наш курс: <b>(\d*) бонусов = (\d*) рубль<\/b>/;
+	if(AnyBalance.isAvailable('balance_money', 'kurs')){
+		html=AnyBalance.requestGet("http://www.gpnbonus.ru/on_the_way/");
+		var regexp=/Наш курс: <b>(\d*) бонусов = (\d*) рубль<\/b>/;
 		if (res=regexp.exec(html)){
 			result.balance_money=Math.floor(((result.balance*res[2])/res[1])*100)/100;
 		}
-		regexp=/Наш курс: <b>(\d* бонусов = \d* рубль)<\/b>/;
-		if (res=regexp.exec(html)){
-			result.kurs=res[1];
-		}
+        	getParam(html, result, 'kurs', /Наш курс: <b>(\d* бонусов = \d* рубль)<\/b>/i, replaceTagsAndSpaces, parseBalance);
 	}
+
 	AnyBalance.trace('End parsing...');
 	AnyBalance.setResult(result);
 }

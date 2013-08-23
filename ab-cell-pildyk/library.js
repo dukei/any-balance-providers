764abb5067060b6c532c732e6e5e3d4739d3f3f3
stark027 <stark027@gmail.com>
2013-08-23 12:30:34 +0000

    Pildyk - Проще написать что не менялось, переписан практически с нуля. (Stark)

--- ab-cell-pildyk/library.js
+++ ab-cell-pildyk/library.js
@@ -120,208 +120,126 @@ function parseCurrency(text){
 /**
  * Заменяет HTML сущности в строке на соответствующие им символы
  */
- function html_entity_decode (string) {
-  // http://kevin.vanzonneveld.net
-  // +   original by: john (http://www.jd-tech.net)
-  // +      input by: ger
-  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   bugfixed by: Onno Marsman
-  // +   improved by: marc andreu
-  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +      input by: Ratheous
-  // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // +      input by: Nick Kolosov (http://sammy.ru)
-  // +   bugfixed by: Fox
-  // -    depends on: get_html_translation_table
-  // *     example 1: html_entity_decode('Kevin &amp; van Zonneveld');
-  // *     returns 1: 'Kevin & van Zonneveld'
-  // *     example 2: html_entity_decode('&amp;lt;');
-  // *     returns 2: '&lt;'
-  var hash_map = {},
-    symbol = '',
-    tmp_str = '',
-    entity = '';
-  tmp_str = string.toString();
-  var quote_style = '';
-  if (false === (hash_map = get_html_translation_table('HTML_ENTITIES', quote_style))) {
-    return false;
-  }
-
-  // fix &amp; problem
-  // http://phpjs.org/functions/get_html_translation_table:416#comment_97660
-  delete(hash_map['&']);
-  hash_map['&'] = '&amp;';
-
-  for (symbol in hash_map) {
-    entity = hash_map[symbol];
-    tmp_str = tmp_str.split(entity).join(symbol);
-  }
-  tmp_str = tmp_str.split('&#039;').join("'");
-
-  return tmp_str;
+function html_entity_decode (string) {
+    var entities = get_html_translation_table();
+    var replaced = string.replace(/&(#?)(\w+);/g, function(str, sharp, m){
+        if(!sharp){
+            var ml = m.toLowerCase(m);
+            if(entities.hasOwnProperty(ml))
+                return String.fromCharCode(entities[ml]);
+        }else{
+            if(/^\d+$/.test(m))
+                return String.fromCharCode(parseInt(m));
+        }
+    });
+    return replaced;
 }
-function get_html_translation_table (table, quote_style) {
-  // http://kevin.vanzonneveld.net
-  // +   original by: Philip Peterson
-  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   bugfixed by: noname
-  // +   bugfixed by: Alex
-  // +   bugfixed by: Marco
-  // +   bugfixed by: madipta
-  // +   improved by: KELAN
-  // +   improved by: Brett Zamir (http://brett-zamir.me)
-  // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // +      input by: Frank Forte
-  // +   bugfixed by: T.Wild
-  // +      input by: Ratheous
-  // %          note: It has been decided that we're not going to add global
-  // %          note: dependencies to php.js, meaning the constants are not
-  // %          note: real constants, but strings instead. Integers are also supported if someone
-  // %          note: chooses to create the constants themselves.
-  // *     example 1: get_html_translation_table('HTML_SPECIALCHARS');
-  // *     returns 1: {'"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;'}
-  var entities = {},
-    hash_map = {},
-    decimal;
-  var constMappingTable = {},
-    constMappingQuoteStyle = {};
-  var useTable = {},
-    useQuoteStyle = {};
 
-  // Translate arguments
-  constMappingTable[0] = 'HTML_SPECIALCHARS';
-  constMappingTable[1] = 'HTML_ENTITIES';
-  constMappingQuoteStyle[0] = 'ENT_NOQUOTES';
-  constMappingQuoteStyle[2] = 'ENT_COMPAT';
-  constMappingQuoteStyle[3] = 'ENT_QUOTES';
+function get_html_translation_table () {
+  var entities = {
+    amp:	38,
+    nbsp:	160,
+    iexcl:	161,
+    cent:	162,
+    pound:	163,
+    curren:	164,
+    yen:	165,
+    brvbar:	166,
+    sect:	167,
+    uml:	168,
+    copy:	169,
+    ordf:	170,
+    laquo:	171,
+    not:	172,
+    shy:	173,
+    reg:	174,
+    macr:	175,
+    deg:	176,
+    plusmn:	177,
+    sup2:	178,
+    sup3:	179,
+    acute:	180,
+    micro:	181,
+    para:	182,
+    middot:	183,
+    cedil:	184,
+    sup1:	185,
+    ordm:	186,
+    raquo:	187,
+    frac14:	188,
+    frac12:	189,
+    frac34:	190,
+    iquest:	191,
+    agrave:	192,
+    aacute:	193,
+    acirc:	194,
+    atilde:	195,
+    auml:	196,
+    aring:	197,
+    aelig:	198,
+    ccedil:	199,
+    egrave:	200,
+    eacute:	201,
+    ecirc:	202,
+    euml:	203,
+    igrave:	204,
+    iacute:	205,
+    icirc:	206,
+    iuml:	207,
+    eth:	208,
+    ntilde:	209,
+    ograve:	210,
+    oacute:	211,
+    ocirc:	212,
+    otilde:	213,
+    ouml:	214,
+    times:	215,
+    oslash:	216,
+    ugrave:	217,
+    uacute:	218,
+    ucirc:	219,
+    uuml:	220,
+    yacute:	221,
+    thorn:	222,
+    szlig:	223,
+    agrave:	224,
+    aacute:	225,
+    acirc:	226,
+    atilde:	227,
+    auml:	228,
+    aring:	229,
+    aelig:	230,
+    ccedil:	231,
+    egrave:	232,
+    eacute:	233,
+    ecirc:	234,
+    euml:	235,
+    igrave:	236,
+    iacute:	237,
+    icirc:	238,
+    iuml:	239,
+    eth:	240,
+    ntilde:	241,
+    ograve:	242,
+    oacute:	243,
+    ocirc:	244,
+    otilde:	245,
+    ouml:	246,
+    divide:	247,
+    oslash:	248,
+    ugrave:	249,
+    uacute:	250,
+    ucirc:	251,
+    uuml:	252,
+    yacute:	253,
+    thorn:	254,
+    yuml:	255,
+    quot:	34,
+    lt:		60,
+    gt:		62
+  };
 
-  useTable = !isNaN(table) ? constMappingTable[table] : table ? table.toUpperCase() : 'HTML_SPECIALCHARS';
-  useQuoteStyle = !isNaN(quote_style) ? constMappingQuoteStyle[quote_style] : quote_style ? quote_style.toUpperCase() : 'ENT_COMPAT';
-
-  if (useTable !== 'HTML_SPECIALCHARS' && useTable !== 'HTML_ENTITIES') {
-    throw new Error("Table: " + useTable + ' not supported');
-    // return false;
-  }
-
-  entities['38'] = '&amp;';
-  if (useTable === 'HTML_ENTITIES') {
-    entities['160'] = '&nbsp;';
-    entities['161'] = '&iexcl;';
-    entities['162'] = '&cent;';
-    entities['163'] = '&pound;';
-    entities['164'] = '&curren;';
-    entities['165'] = '&yen;';
-    entities['166'] = '&brvbar;';
-    entities['167'] = '&sect;';
-    entities['168'] = '&uml;';
-    entities['169'] = '&copy;';
-    entities['170'] = '&ordf;';
-    entities['171'] = '&laquo;';
-    entities['172'] = '&not;';
-    entities['173'] = '&shy;';
-    entities['174'] = '&reg;';
-    entities['175'] = '&macr;';
-    entities['176'] = '&deg;';
-    entities['177'] = '&plusmn;';
-    entities['178'] = '&sup2;';
-    entities['179'] = '&sup3;';
-    entities['180'] = '&acute;';
-    entities['181'] = '&micro;';
-    entities['182'] = '&para;';
-    entities['183'] = '&middot;';
-    entities['184'] = '&cedil;';
-    entities['185'] = '&sup1;';
-    entities['186'] = '&ordm;';
-    entities['187'] = '&raquo;';
-    entities['188'] = '&frac14;';
-    entities['189'] = '&frac12;';
-    entities['190'] = '&frac34;';
-    entities['191'] = '&iquest;';
-    entities['192'] = '&Agrave;';
-    entities['193'] = '&Aacute;';
-    entities['194'] = '&Acirc;';
-    entities['195'] = '&Atilde;';
-    entities['196'] = '&Auml;';
-    entities['197'] = '&Aring;';
-    entities['198'] = '&AElig;';
-    entities['199'] = '&Ccedil;';
-    entities['200'] = '&Egrave;';
-    entities['201'] = '&Eacute;';
-    entities['202'] = '&Ecirc;';
-    entities['203'] = '&Euml;';
-    entities['204'] = '&Igrave;';
-    entities['205'] = '&Iacute;';
-    entities['206'] = '&Icirc;';
-    entities['207'] = '&Iuml;';
-    entities['208'] = '&ETH;';
-    entities['209'] = '&Ntilde;';
-    entities['210'] = '&Ograve;';
-    entities['211'] = '&Oacute;';
-    entities['212'] = '&Ocirc;';
-    entities['213'] = '&Otilde;';
-    entities['214'] = '&Ouml;';
-    entities['215'] = '&times;';
-    entities['216'] = '&Oslash;';
-    entities['217'] = '&Ugrave;';
-    entities['218'] = '&Uacute;';
-    entities['219'] = '&Ucirc;';
-    entities['220'] = '&Uuml;';
-    entities['221'] = '&Yacute;';
-    entities['222'] = '&THORN;';
-    entities['223'] = '&szlig;';
-    entities['224'] = '&agrave;';
-    entities['225'] = '&aacute;';
-    entities['226'] = '&acirc;';
-    entities['227'] = '&atilde;';
-    entities['228'] = '&auml;';
-    entities['229'] = '&aring;';
-    entities['230'] = '&aelig;';
-    entities['231'] = '&ccedil;';
-    entities['232'] = '&egrave;';
-    entities['233'] = '&eacute;';
-    entities['234'] = '&ecirc;';
-    entities['235'] = '&euml;';
-    entities['236'] = '&igrave;';
-    entities['237'] = '&iacute;';
-    entities['238'] = '&icirc;';
-    entities['239'] = '&iuml;';
-    entities['240'] = '&eth;';
-    entities['241'] = '&ntilde;';
-    entities['242'] = '&ograve;';
-    entities['243'] = '&oacute;';
-    entities['244'] = '&ocirc;';
-    entities['245'] = '&otilde;';
-    entities['246'] = '&ouml;';
-    entities['247'] = '&divide;';
-    entities['248'] = '&oslash;';
-    entities['249'] = '&ugrave;';
-    entities['250'] = '&uacute;';
-    entities['251'] = '&ucirc;';
-    entities['252'] = '&uuml;';
-    entities['253'] = '&yacute;';
-    entities['254'] = '&thorn;';
-    entities['255'] = '&yuml;';
-  }
-
-  if (useQuoteStyle !== 'ENT_NOQUOTES') {
-    entities['34'] = '&quot;';
-  }
-  if (useQuoteStyle === 'ENT_QUOTES') {
-    entities['39'] = '&#39;';
-  }
-  entities['60'] = '&lt;';
-  entities['62'] = '&gt;';
-
-
-  // ascii decimals to real symbols
-  for (decimal in entities) {
-    if (entities.hasOwnProperty(decimal)) {
-      hash_map[String.fromCharCode(decimal)] = entities[decimal];
-    }
-  }
-
-  return hash_map;
+  return entities;
 }
 /*function html_entity_decode(str)
 {
@@ -351,21 +269,21 @@ function get_html_translation_table (table, quote_style) {
  */
 function createFormParams(html, process, array){
     var params = array ? [] : {};
-    html.replace(/<input[^>]+name="([^"]*)"[^>]*>|<select[^>]+name="([^"]*)"[^>]*>[\s\S]*?<\/select>/ig, function(str, nameInp, nameSel){
+    html.replace(/<input[^>]+name=['"]([^'"]*)['"][^>]*>|<select[^>]+name=['"]([^'"]*)['"][^>]*>[\s\S]*?<\/select>/ig, function(str, nameInp, nameSel){
         var value = '';
         if(nameInp){
-            if(/type="button"/i.test(str))
+            if(/type=['"]button['"]/i.test(str))
                 value=undefined;
             else
-                value = getParam(str, null, null, /value="([^"]*)"/i, null, html_entity_decode) || '';
+                value = getParam(str, null, null, /value=['"]([^'"]*)['"]/i, null, html_entity_decode) || '';
             name = nameInp;
         }else if(nameSel){
-            value = getParam(str, null, null, /^<[^>]*value="([^"]*)"/i, null, html_entity_decode);
+            value = getParam(str, null, null, /^<[^>]*value=['"]([^'"]*)['"]/i, null, html_entity_decode);
             if(typeof(value) == 'undefined'){
                 var optSel = getParam(str, null, null, /(<option[^>]+selected[^>]*>)/i);
                 if(!optSel)
                     optSel = getParam(str, null, null, /(<option[^>]*>)/i);
-                value = getParam(optSel, null, null, /value="([^"]*)"/i, null, html_entity_decode);
+                value = getParam(optSel, null, null, /value=['"]([^'"]*)["']/i, null, html_entity_decode);
             }
             name = nameSel;
         }
@@ -704,7 +622,7 @@ function parseTrafficEx(text, thousand, order, defaultUnits){
         AnyBalance.trace("Could not parse traffic value from " + text);
         return;
     }
-    var units = getParam(_text, null, null, /([kmgкмг][бb]?|[бb](?![\wа-я])|байт|bytes)/i);
+    var units = getParam(_text, null, null, /([kmgtкмгт][бb]?|[бb](?![\wа-я])|байт|bytes)/i);
     if(!units && !defaultUnits){
         AnyBalance.trace("Could not parse traffic units from " + text);
         return;
@@ -727,13 +645,17 @@ function parseTrafficEx(text, thousand, order, defaultUnits){
       case 'г':
         val = Math.round(val/Math.pow(thousand, order-3)*100)/100;
         break;
+      case 't':
+      case 'т':
+        val = Math.round(val/Math.pow(thousand, order-4)*100)/100;
+        break;
     }
     var textval = ''+val;
     if(textval.length > 6)
       val = Math.round(val);
     else if(textval.length > 5)
       val = Math.round(val*10)/10;
-    var dbg_units = {0: 'b', 1: 'kb', 2: 'mb', 3: 'gb'};
+    var dbg_units = {0: 'b', 1: 'kb', 2: 'mb', 3: 'gb', 4: 'tb'};
     AnyBalance.trace('Parsing traffic (' + val + dbg_units[order] + ') from: ' + text);
     return val;
 }

3049b66656be39b1ce8c12a965d3189ecb8b5a48
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-08-23 11:32:29 +0000

    ИОН: Поддержка нового кабинета https://pay.i-on.ru/personal/pub/Entrance

--- trunk/ab-bonus-ion/main.js
+++ trunk/ab-bonus-ion/main.js
@@ -6,39 +6,6 @@
 Сайт: http://i-on.ru/
 Личный кабинет: http://i-on.ru/crm/logon
 */
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-function parseBalance(text){
-    var _text = html_entity_decode(text);
-    var val = getParam(_text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
 var g_headers = {
     Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
@@ -46,14 +13,64 @@ var g_headers = {
     'Cache-Control':'max-age=0',
     'Accept-Encoding':null, //Че-то какой-то битый стрим она получает, ошибка EOFException вываливается. Отменяем сжатие
     Connection:'keep-alive',
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
+	'X-Requested-With':'XMLHttpRequest',
+	Referer:'https://pay.i-on.ru/personal/pub/Entrance'
 };
 
 function main(){
-    var baseurl = "http://i-on.ru/crm/logon";
     var prefs = AnyBalance.getPreferences();
-    
-    var html = AnyBalance.requestPost(baseurl, {
+	if(prefs.cabinet == 'new')
+		doNewCabinet(prefs);
+	else /*if(prefs.cabinet == 'old')*/
+		doOldCabinet(prefs);
+}
+
+function doNewCabinet(prefs){
+	var baseurl = 'https://pay.i-on.ru/';
+	var html = AnyBalance.requestGet(baseurl + 'personal/pub/Entrance', g_headers);
+	// Это форма где нужно ввести только номер карты, затем ставится кука и можно уже вводить логин и пароль
+	var loginForm = getParam(html, null, null, /<form[^>]*>(?!<form[^>]*>)[\s\S]*?b-form-login[\s\S]*?<\/form>/i);
+	if(!loginForm)
+		throw new AnyBalance.Error("Не удалось найти форму входа, сайт изменен?");
+		
+	var loginFormSubmit = getParam(loginForm, null, null, /<form[^>]*data-validator-ajax-url="\.\.([\s\S]*?)"/i);
+
+	var params = createFormParams(loginForm, function(params, str, name, value){
+		if(name == 'ean')
+			return prefs.login;
+		else if(name == 'password')
+			return prefs.password;
+		return value;
+	});
+	// в итоге должны запрашивать нечто https://pay.i-on.ru/personal/;jsessionid=51927EFF6F83933EE13B1AD5B3B9330B?wicket:interface=:0:loginForm::IBehaviorListener:0:1
+    html = AnyBalance.requestPost(baseurl + 'personal' + loginFormSubmit, params, g_headers);
+	// Какой ?#*$%#**%%! делал этот кабинет?! Два Post запроса подряд с одинаковой date, чтобы убедится что мы не роботы? :)
+	html = AnyBalance.requestPost(baseurl + 'personal' + loginFormSubmit, params, g_headers);
+	
+	var json = getJson(html);
+	
+    if(!json.validated){
+        var error = getParam(html, null, null, /<div[^>]+class="validation-summary-errors"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error("Не удалось зайти в личный кабинет. Проверьте номер карты и пароль.");
+    }
+
+	html = AnyBalance.requestGet(json.form.redirectUrl, g_headers);
+	
+    var result = {success: true};
+    getParam(html, result, 'balance', /Баланс[\s\S]*?b-user-info__balance[^>]*>([\s\S]*?)</i, null, parseBalance);
+	getParam(html, result, 'bonuses', /bonus-statement[^>]*>\s*<span[^>]*b-user-info__balance[^>]*>([\s\S]*?)</i, null, parseBalance);
+	result.__tariff = prefs.login;
+
+    AnyBalance.setResult(result);
+}
+
+function doOldCabinet(prefs)
+{
+	var baseurl = 'http://i-on.ru/';
+    var html = AnyBalance.requestPost(baseurl + 'crm/logon', {
         number:prefs.login,
         password:prefs.password,
         remember:false
@@ -75,12 +92,3 @@ function main(){
 
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

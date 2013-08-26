117c54582b0df8c99df27361d133224d4e378177
dmitry.kochin <dmitry.kochin@gmail.com>
2013-08-26 12:01:29 +0000

    sgku - слегка исправлено

--- ab-municipal-sgku/preferences.js
+++ ab-municipal-sgku/preferences.js
@@ -6,7 +6,7 @@
 Operator site: https://sgku.ru
 Личный кабинет: https://sgku.ru
 */
-
+/*
 function onChangeLocality(){
     var locality = AnyBalance.getPreference('locality');
     var surgut = (locality.get('value') == 'Сургут');
@@ -16,10 +16,46 @@ function onChangeLocality(){
     AnyBalance.getPreference('flat').set('visible', surgut);
     AnyBalance.getPreference('account').set('visible', !surgut);
 }
+*/
+
+/**
+ *  Проверяет, является ли объект массивом
+    Эта функция требуется в API но по случайности там не определена. На всякий случай определяем здесь.
+ */
+function isArray(arr){
+	return Object.prototype.toString.call( arr ) === '[object Array]';
+}
+
+function onChangeLocality(){
+    var props = AnyBalance.getPreferenceProperties({
+        locality: {
+            value: ''
+        }
+    });
+
+    var surgut = (props.locality.value == 'Сургут');
+
+    AnyBalance.setPreferenceProperties({
+        street: {visible: surgut},
+        house: {visible: surgut},
+        flat: {visible: surgut},
+        account: {visible: !surgut},
+    });
+
+//    AnyBalance.requestGet('http://bing.com/', null, {callback: onGetURL});
+}
+
+function onGetURL(info){
+    delete info.content;
+    AnyBalance.trace(AnyBalance.getLastUrl() + ":" + JSON.stringify(info));
+}
 
 function main(){
     var baseurl = "https://sgku.ru/";
-    AnyBalance.trace('sdfas');
     AnyBalance.addCallback('change#locality', onChangeLocality);
+
+//    AnyBalance.requestPost('http://ya.ru/', {test: 1}, null, {callback: onGetURL});
+//    AnyBalance.requestGet('https://google.ru/', null, {callback: onGetURL});
+    
     onChangeLocality();
 }

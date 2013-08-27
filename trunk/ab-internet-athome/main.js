db70b2608da2fa73f037ecf2133c4e3577abd72d
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-08-27 09:25:50 +0000

    Эт Хоум: Поддержка изменений в личном кабинете

--- trunk/ab-internet-athome/main.js
+++ trunk/ab-internet-athome/main.js
@@ -7,46 +7,20 @@
 Личный кабинет: https://billing.at-home.ru/
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseTrafficGb(str){
-  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-  return parseFloat((val/1024).toFixed(2));
-}
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    AnyBalance.setDefaultCharset('utf-8');
+    //AnyBalance.setDefaultCharset('utf-8');
+	AnyBalance.setOptions({FORCE_CHARSET: 'utf-8'});
 
-    var baseurl = "https://billing.at-home.ru/";
+    var baseurl = 'https://billing.at-home.ru/';
 
     var html = AnyBalance.requestPost(baseurl + 'lk/user/authorize/', {
         redirect: 1,
@@ -55,7 +29,7 @@ function main(){
         pwd:prefs.password,
         x: 34,
         y: 2
-    });
+    }, g_headers);
 
     //AnyBalance.trace(html);
     if(!/\/lk\/user\/logout/.test(html)){
@@ -76,18 +50,14 @@ function main(){
         var dt = new Date();
         var html = AnyBalance.requestGet(baseurl + 'lk/report/trafic/?start_date=' + dt.getFullYear() + '-' + (dt.getMonth()+1) + '-' + '01&end_date=' + dt.getFullYear() + '-' + (dt.getMonth()+1) + '-' + dt.getDate() + '&x=27&y=10');
 
-       getParam(html, result, 'trafficIn', /Входящий трафик(?:[\s\S]*?<td[^>]*>){2}([\S\s]*?)<\/td>/i, replaceFloat, parseTrafficGb);
-       getParam(html, result, 'trafficOut', /Исходящий трафик(?:[\s\S]*?<td[^>]*>){3}([\S\s]*?)<\/td>/i, replaceFloat, parseTrafficGb);
+       getParam(html, result, 'trafficIn', /Входящий трафик(?:[\s\S]*?<td[^>]*>){2}([\S\s]*?)<\/td>/i, replaceFloat, parseTrafficGbMy);
+       getParam(html, result, 'trafficOut', /Исходящий трафик(?:[\s\S]*?<td[^>]*>){3}([\S\s]*?)<\/td>/i, replaceFloat, parseTrafficGbMy);
     }
     
     AnyBalance.setResult(result);
 }
 
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
+function parseTrafficGbMy(str){
+  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+  return parseFloat((val/1024).toFixed(2));
 }
-

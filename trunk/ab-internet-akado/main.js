cf99fcd1c76bfdba00738586f481b51e9a3cede1
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-08-28 12:13:14 +0000

    Акадо Москва: - Избавление от Jquery

--- trunk/ab-internet-akado/main.js
+++ trunk/ab-internet-akado/main.js
@@ -15,7 +15,7 @@ function main(){
     var pass = hex_md5(prefs.password).toUpperCase(); //Пароль в md5
     
     // Заходим на главную страницу
-    var info = AnyBalance.requestPost(baseurl + "login.xml/login", {
+    var info = AnyBalance.requestPost(baseurl + 'login.xml/login', {
         login: prefs.login,
         password: pass
     }/*, {
@@ -24,75 +24,42 @@ function main(){
 	'Referer': baseurl + 'login.xml',
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.152 Safari/535.19'
     }*/);
+	
+	if(!/splash-loading/i.test(info)){
+        var error = getParam(info, null, null, /<response[^>]*><message>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
 
-    
-    var error = $('<div>' + info + '</div>').find('response>message').text();
-    if(error) //Что-то неправильно произошло. Скорее всего, пароль неправильный
-      throw new AnyBalance.Error(error);
-    
     var result = {success: true};
-    
-    if(AnyBalance.isAvailable('balance', 'agreement', 'username')){
-      var status = AnyBalance.requestGet(baseurl + "status.xml");
-      var $parse = $('<div>' + status + '</div>');
-      
-      if(AnyBalance.isAvailable('balance')){
-        var balance = $parse.find('#account-balance-value').text();
-        if(balance)
-          result.balance = parseFloat(balance);
-      }
-      
-      if(AnyBalance.isAvailable('username')){
-        var value = $parse.find('#account-fullname').text();
-        if(value)
-          result.username = value;
-      }
-      
-      if(AnyBalance.isAvailable('agreement')){
-        var value = $parse.find('#account-ID').text();
-        if(value)
-          result.agreement = value;
-      }
-    }
-    
+
+	if(AnyBalance.isAvailable('balance', 'agreement', 'username'))
+	{
+		var status = AnyBalance.requestGet(baseurl + 'status.xml');
+		getParam(status, result, 'balance', /account-balance-value[^>]*>([^<]*)/i, null, parseBalance);
+		getParam(status, result, 'username', /account-fullname[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(status, result, 'agreement', /account-ID[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    }	
+
     if(AnyBalance.isAvailable('payhint')){
-      var account = AnyBalance.requestGet(baseurl + "account.xml");
-      var $parse = $(account);
-      
-      var value = $parse.find("span.description:contains('Рекомендуемая сумма оплаты услуг в следующем месяце')").parent().find("span.amount").text();
-      if(value){
-        var res = /([\d+\.,]+)/.exec(value);
-        if(res){
-          result.payhint = parseFloat(res[1]);
-        }
-      }
-    }
-    
-    var services = AnyBalance.requestGet(baseurl + "services.xml");
-    var $parse = $($.parseXML(services));
-    var servid = $parse.find("tr:contains('УСЛУГИ ИНТЕРНЕТ')").first().attr('data-id');
-    
-    if(servid){ //Может, услуг интернета-то и нет
-        services = AnyBalance.requestGet(baseurl + "services.xml?ID="+servid+"&depth=1");
-        $parse = $($.parseXML(services));
-        var tariff = $parse.find('th a').first().text();
-        if(tariff){
-          var res = /\(\s*(.*?)\s*[,\)]/.exec(tariff);
-          if(res)
-            tariff = res[1];
-          result.__tariff = tariff;
-        }
-    }
+		var account = AnyBalance.requestGet(baseurl + 'account.xml');
+		getParam(account, result, 'payhint', /<[^>]*"amount[^>]*>([\s\S]{1,200})<\/span>\s*<span class="description">\s*Рекомендуемая сумма оплаты услуг в следующем месяце/i, null, parseBalance);
+	}
 
-    var value = $parse.find("span.description:contains('Рекомендуемая сумма оплаты услуг в следующем месяце')").parent().find("span.amount").text();
-    if(value){
-      var res = /([\d+\.,]+)/.exec(value);
-      if(res){
-        result.payhint = parseFloat(res[1]);
-      }
-    }
-    
+    var services = AnyBalance.requestGet(baseurl + 'services.xml');
 
+	if(/УСЛУГИ ИНТЕРНЕТ/i.test(services)){
+		var servid = getParam(services, null, null, /<a href="\?ID=([\s\S]*?)">УСЛУГИ ИНТЕРНЕТ<\/a>/i, null, html_entity_decode);
+		services = AnyBalance.requestGet(baseurl + 'services.xml?ID='+servid+'&depth=1');
+		var tariff = getParam(services, null, null, /<th>\s*<a href[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		if(tariff){
+			var res = /\(([^,]*)/.exec(tariff);
+			if(res)
+				tariff = res[1];
+			result.__tariff = tariff;
+		}
+	}
     AnyBalance.setResult(result);
 }
 

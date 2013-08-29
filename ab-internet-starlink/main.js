229513e9f684504d44bd0e98fd029fbdf459e6fb
stark027 <stark027@gmail.com>
2013-08-29 15:30:28 +0000

    Starlink (Москва): - Поддержка изменений в личном кабинете, удалены отсутствующие счетчики

--- ab-internet-starlink/main.js
+++ ab-internet-starlink/main.js
@@ -7,44 +7,30 @@
 Личный кабинет: https://stat.starlink.ru/
 */
 
-function parseTrafficGb(str){
-  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d.,]*)/, replaceFloat, parseBalance);
-  if(isset(val)){
-      var ret = parseFloat((val/1024).toFixed(2));
-      AnyBalance.trace('Parsed traffic ' + ret + 'Gb from ' + str);
-      return ret;
-  }
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
-
-    AnyBalance.setDefaultCharset('windows-1251');
-
-    var baseurl = "https://stat.starlink.ru/";
-
+    AnyBalance.setDefaultCharset('UTF-8');
+    var baseurl = 'https://stat.starlink.ru/';
+	
     var html = AnyBalance.requestPost(baseurl + 'form.statistics.php', {
-        login:prefs.login,
-        pass:prefs.password,
-        check:1
+        userLogin:prefs.login,
+        userPassword:prefs.password,
+        loginNow:true
     });
-
-    if(!/\?s=exit/i.test(html)){
-        var error = getParam(html, null, null, /<font[^>]+color="?red[^>]*>([\s\S]*?)<\/font>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        
+	
+    if(!/Выход из кабинета/i.test(html)){
         throw new AnyBalance.Error("Не удалось войти в личный кабинет. Личный кабинет изменился или проблемы на сайте.");
     }
-
     var result = {success: true};
-
-    getParam(html, result, 'balance', /Баланс лицевого счета[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'available', /Доступно для оплаты услуг[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'daysleft', /До конца (?:рас|от)четного периода осталось дней[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, '__tariff', /<td[^>]*>\s*Тарифный план[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'traffic', /Трафик за период(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
-    getParam(html, result, 'trafficTurbo', /Осталось турбо трафика(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
-
+    getParam(html, result, 'balance', /Баланс\s*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+	if(isAvailable('daysleft')){
+		html = AnyBalance.requestGet(baseurl);
+		getParam(html, result, 'daysleft', /Осталось([\s\S]*?)дней/i, replaceTagsAndSpaces, parseBalance);
+	}
+	if(isAvailable('traffic')){
+		html = AnyBalance.requestGet(baseurl+'statistics/');
+		getParam(html, result, 'traffic', /Входящий трафик(?:[\s\S]*?<td[^>]*>){2}\s*([\s\S]*?)\s*<\/td>/i, replaceTagsAndSpaces, parseTraffic);
+	}
     AnyBalance.setResult(result);
 }

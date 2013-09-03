921c5a4d9f018945e53adc96a221cdbc753e87b4
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-03 10:38:27 +0000

    Лукойл, добавлены потерянные поля из предыдущих версий провайдера

--- trunk/ab-bonus-lukoil/main.js
+++ trunk/ab-bonus-lukoil/main.js
@@ -18,8 +18,24 @@ function main(){
     AnyBalance.setDefaultCharset('utf-8'); 
 	var result = {success: true};
 	
-	if(prefs.type == 'club')
+	if(prefs.type == 'likard')
 	{
+		var html = AnyBalance.requestPost(baseurl + 'login', {
+			submit:'',
+			login:prefs.login,
+			pass:prefs.password,
+		}, g_headers); 
+		//получим id пользователя
+		var usedId = /\/([\s\S]{1,15})\/client/i.exec(html);
+		if(!usedId)
+			throw new AnyBalance.Error('Не удалось найти пользователя, проверьте логин и пароль');
+		
+		getParam(prefs.login, result, 'cardnum');
+		getParam(html, result, 'balance', /Баланс[\s\S]*?>[\s\S]*?>([\s\S]*?)<\/b/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'last_payment', /Последний платёж[\s\S]*?payments">([\s\S]*?)<\/a/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'name', /class="value user-name">\s*<b>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, null);
+	// для совместимости оставим так, потом когда все обновятся можно раскоментировать
+	}else /*if(prefs.type == 'club')*/{
 		var html = AnyBalance.requestGet(baseurlFizik, g_headers);
 		
 		var sessid = getParam(html, null, null, /id="sessid" value="([\s\S]*?)"/i, null, null);
@@ -32,30 +48,29 @@ function main(){
 			'sessid':sessid
 		}, g_headers); 
 		
-		if(!/сменить свой пароль/i.test(html))
-			throw new AnyBalance.Error('Не удалось войти в личный кабинет, проверьте логин и пароль');
-		
-		getParam(html, result, 'balance', /Баланс[\s\S]*?([\s\S]*?)баллов/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'acc_num', /Номер карты[\s\S]*?ph">([\s\S]*?)<\/div/i, replaceTagsAndSpaces, null);
+                //После входа обязательно проверяем маркер успешного входа
+                //Обычно это ссылка на выход, хотя иногда приходится искать что-то ещё
+                if(!/\?ACTION=logout/i.test(html)){
+                    //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
+                    var error = getParam(html, null, null, /<p[^>]+class="err"[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+                    if(error)
+                        throw new AnyBalance.Error(error);
+                    //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
+                    throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+                }
+
+		getParam(html, result, 'balance', /Баланс[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'cardnum', /Номер карты[\s\S]*?ph">([\s\S]*?)<\/div/i, replaceTagsAndSpaces, null);
 		getParam(html, result, '__tariff', /<li><span>Ваш статус в Программе:<\/span>([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, null);
-	}
-	// для совместимости оставим так, потом когда все обновятся можно раскоментировать
-	else /*if(prefs.type == 'likard')*/
-	{
-		var html = AnyBalance.requestPost(baseurl + 'login', {
-			submit:'',
-			login:prefs.login,
-			pass:prefs.password,
-		}, g_headers); 
-		//получим id пользователя
-		var usedId = /\/([\s\S]{1,15})\/client/i.exec(html);
-		if(!usedId)
-			throw new AnyBalance.Error('Не удалось найти пользователя, проверьте логин и пароль');
-		
-		getParam(html, result, 'balance', /Баланс[\s\S]*?>[\s\S]*?>([\s\S]*?)<\/b/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'last_payment', /Последний платёж[\s\S]*?payments">([\s\S]*?)<\/a/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'name', /class="value user-name">\s*<b>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, null);
-	}
+    		
+		getParam(html, result, 'region', /Регион Программы:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    		if(AnyBalance.isAvailable('name', 'phonenumber')){
+        		html = AnyBalance.requestGet(baseurlFizik + 'personal/');
+        		getParam(html, result, 'name', /ФИО:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+        		getParam(html, result, 'phonenumber', /Мобильный телефон:([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+ 		}
+    }
     //Возвращаем результат
     AnyBalance.setResult(result);
 }

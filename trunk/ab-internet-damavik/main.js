f57b9b819ca8df599cdf45ec26d6b6a010c9d131
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-06 09:40:57 +0000

    Новый провайдер: Шпарки Дамовик

---
+++ trunk/ab-internet-damavik/main.js
@@ -0,0 +1,38 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
+	'Origin':'https://issa.damavik.by',
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://issa.damavik.by/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+	
+    var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	html = AnyBalance.requestPost(baseurl, {
+		form_action_true:'https://issa.damavik.by/about',
+        login__n18:prefs.login,
+        password__n18:prefs.password,
+        action__n18:'login'
+    }, addHeaders({Referer: baseurl})); 
+	
+    if(!/Информация о лицевом счете/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+	
+    var result = {success: true};
+    getParam(html, result, 'balance', /Состояние счета[^>]*value="([^"]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /Добро пожаловать,\s*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'acc', /Номер лицевого счета[^>]*value="([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+    AnyBalance.setResult(result);
+}

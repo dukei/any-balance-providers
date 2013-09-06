9287421d213af5b31a073e403c4a391c1381d8c5
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-06 11:07:31 +0000

    Сумма Телеком: - Поддержка изменений на сайте. - Новый счетчик: Лицевой счет (Stark)

--- trunk/ab-internet-sumtel/main.js
+++ trunk/ab-internet-sumtel/main.js
@@ -7,55 +7,21 @@
 Сайт оператора: http://www.sumtel.ru
 Личный кабинет: http://my.sumtel.ru/
 */
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function getViewState(html){
-    return getParam(html, null, null, /name="__VIEWSTATE".*?value="([^"]*)"/);
-}
-
-function getEventValidation(html){
-    return getParam(html, null, null, /name="__EVENTVALIDATION".*?value="([^"]*)"/);
-}
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
+var g_headers = {
+	'Accept':'*/*',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
+	'Origin':'http://my.sumtel.ru/'
+};
 function main(){
-    //По санкт-петербургу пока старый кабинет. Новый стал с капчей
     var prefs = AnyBalance.getPreferences();
 
-    var baseurl = "https://stat.sumtel.ru/";
+    var baseurl = 'http://my.sumtel.ru/';
     AnyBalance.setDefaultCharset('utf-8');
 
-    var html = AnyBalance.requestGet(baseurl);
+    /*var html = AnyBalance.requestGet(baseurl);
     var eventvalidation = getEventValidation(html);
     var viewstate = getViewState(html);
 
@@ -84,7 +50,59 @@ function main(){
     getParam(html, result, 'status', /lblStatus[^>]*>([^<]*)/i, replaceTagsAndSpaces);
     getParam(html, result, '__tariff', /lblTarifName[^>]*>([^<]*)/i, replaceTagsAndSpaces);
     getParam(html, result, 'abon', /lblTarifCost[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'fio', /lblClientName[^>]*>([^<]*)/i, replaceTagsAndSpaces);
+    getParam(html, result, 'fio', /lblClientName[^>]*>([^<]*)/i, replaceTagsAndSpaces);*/
+	var html = AnyBalance.requestGet(baseurl + 'ps/scc/login.php?SECONDARY_LOGIN=1');
+	var session = getParam(html, null, null, /name="PHPSESSID"[^>]*value="([^"]*)/i);
+
+	var captchaa;
+	if(AnyBalance.getLevel() >= 7){
+		AnyBalance.trace('Пытаемся ввести капчу');
+		var captcha = AnyBalance.requestGet(baseurl+ '/ps/scc/php/cryptographp.php');
+		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaa);
+	}else{
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}
+    
+	html = AnyBalance.requestPost(baseurl + 'ps/scc/php/check.php', {
+        PHPSESSID:session,
+        LOGIN:prefs.login,
+        PASSWORD:prefs.password,
+        CODE:captchaa
+    }, addHeaders({Referer: baseurl})); 
+	
+	session = getParam(html, null, null, /<SESSION_ID>([\s\S]*?)<\/SESSION_ID>/i);
+	
+    if(!session){
+		var error = getParam(html, null, null, /<ERROR_ID>([\s\S]*?)<\/ERROR_ID>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error == -3)
+            throw new AnyBalance.Error('Введите цифры с картинки!');
+        else if(error == -6)
+            throw new AnyBalance.Error('Вы ввели не верные символы с картинки! Попробуйте еще раз');
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+	
+	html = AnyBalance.requestPost(baseurl + 'SCC/SC_BASE_LOGIN', {
+        SESSION_ID:session,
+        LOGIN:prefs.login,
+        PASSWD:prefs.password,
+        CHANNEL:'WWW'
+    }, addHeaders({Referer: baseurl + 'ps/scc/login.php?SECONDARY_LOGIN=1'})); 
+	
+	html = AnyBalance.requestPost(baseurl + 'SCWWW/ACCOUNT_INFO', {
+        SESSION_ID:session,
+		CHANNEL:'WWW',
+        P_USER_LANG_ID:'1',
+        find:''
+    }, addHeaders({Referer: baseurl + 'SCC/SC_BASE_LOGIN'})); 
+
+    var result = {success: true};
+    getParam(html, result, 'fio', /class="group-client"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'acc_num', /&#1051;&#1080;&#1094;&#1077;&#1074;&#1086;&#1081; &#1089;&#1095;&#1077;&#1090;:(?:[\s\S]*?<div[^>]*>){2}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /&#1041;&#1072;&#1083;&#1072;&#1085;&#1089;(?:[\s\S]*?<div[^>]*>){2}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /&#1058;&#1077;&#1082;&#1091;&#1097;&#1080;&#1081; &#1090;&#1072;&#1088;&#1080;&#1092;&#1085;&#1099;&#1081; &#1087;&#1083;&#1072;&#1085;:(?:[\s\S]*?<div[^>]*>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'status', /&#1057;&#1090;&#1072;&#1090;&#1091;&#1089; &#1072;&#1073;&#1086;&#1085;&#1077;&#1085;&#1090;&#1072;:(?:[\s\S]*?<div[^>]*>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'abon', /&#1040;&#1073;&#1086;&#1085;&#1077;&#1085;&#1090;&#1089;&#1082;&#1072;&#1103; &#1087;&#1083;&#1072;&#1090;&#1072;(?:[\s\S]*?<div[^>]*>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
 
     AnyBalance.setResult(result);
 }

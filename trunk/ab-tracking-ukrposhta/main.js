2de96548570a5349984d926992e1b49db43952a5
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-09 14:46:14 +0000

    Новый провайдер: Укрпошта

---
+++ trunk/ab-tracking-ukrposhta/main.js
@@ -0,0 +1,35 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'http://services.ukrposhta.com/';
+    AnyBalance.setDefaultCharset('utf-8'); 
+
+    var html = AnyBalance.requestGet(baseurl+'barcodesingle/default.aspx?culture=uk', g_headers);
+
+	var params = createFormParams(html, function(params, str, name, value){
+		if(name == 'ctl00$centerContent$txtBarcode')
+			return prefs.login;
+		return value;
+	});
+	html = AnyBalance.requestPost(baseurl+'barcodesingle/default.aspx?culture=uk', params, addHeaders({Referer: baseurl + 'vidslidkuvati-forma-poshuku'})); 
+	
+	if(!/Відправлення за номером/i.test(html)){
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+	
+    var result = {success: true};
+    getParam(html, result, 'all', /<div id="ctl00_centerContent_divInfo">\s*([\s\S]*?)\s*<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+    AnyBalance.setResult(result);
+}

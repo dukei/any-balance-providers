f33825732f1f7e0aa253c78213425f77042a8bb8
a.listratenkov@gmail.com <a.listratenkov@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-09 18:56:18 +0000

    Увеличил версию, исправил URL запроса.

--- trunk/ab-weather-montemp/main.js
+++ trunk/ab-weather-montemp/main.js
@@ -2,7 +2,7 @@
 
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-Получает текущую погоду с сайта http://flags8192.ru/temps/. Для измерения температуры используется температурный 
+Получает текущую погоду с сайта http://www.flags8192.ru/temps/. Для измерения температуры используется температурный 
 датчик собственного производства под управлением UsbTenkiMux (тоже собственного производства), расположенный в 
 п. Монастырщина Смоленской области и защищенный от воздействия внешних факторов, что позволяет довольно точно 
 определять температуру воздуха. 
@@ -13,8 +13,8 @@ function main(){
 	AnyBalance.setDefaultCharset('UTF-8');
 	
 	var result = {success: true};
-	AnyBalance.trace("Loading http://flags8192.ru/temps/");
-	var html = AnyBalance.requestGet('http://flags8192.ru/temps/temp2');
+	AnyBalance.trace("Loading http://www.flags8192.ru/temps/");
+	var html = AnyBalance.requestGet('http://www.flags8192.ru/temps/temp2');
 	
 	AnyBalance.trace("Parsing current temperature");
 	var regexp = /\S+/;

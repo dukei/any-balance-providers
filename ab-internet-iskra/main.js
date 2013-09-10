36efcf37c41de6f51ce2e9074392eceacba01ee6
stark027 <stark027@gmail.com>
2013-09-10 10:47:19 +0000

    Seven Sky (ИСКРАТЕЛЕКОМ) (Москва): - Переименован провайдер в Seven Sky и переведен на новый адрес

--- ab-internet-iskra/main.js
+++ ab-internet-iskra/main.js
@@ -7,41 +7,11 @@
 Личный кабинет: https://my.istel.ru
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "https://my.istel.ru/cgi-bin/clients/";
+    var baseurl = 'https://stat.7-sky.info/cgi-bin/clients/';
 
     var html = AnyBalance.requestPost(baseurl + 'login', {
         action:'validate',
@@ -51,7 +21,6 @@ function main(){
         submit:'Войти'
     });
 
-    //AnyBalance.trace(html);
     if(!/action=logout/.test(html)){
         var error = getParam(html, null, null, /<span[^>]*style=["']color:\s*#101010[^>]*>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
         if(error)
@@ -62,8 +31,8 @@ function main(){
     var result = {success: true};
 
     getParam(html, result, 'balance', /Ваш баланс:[\S\s]*?<span[^>]*>([\S\s]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'licschet', /Счет &#x2116;([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-    var sessid = getParam(html, null, null, /<input[^>]*name="session_id"[^>]*value="([^"]*)/i);
+    getParam(html, result, 'licschet', /Счет N([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    /*var sessid = getParam(html, null, null, /<input[^>]*name="session_id"[^>]*value="([^"]*)/i);
 
     html = AnyBalance.requestGet(baseurl + "deal_account?session_id=" + sessid + "&action=stat_user_show");
 
@@ -79,15 +48,6 @@ function main(){
             result.__tariff = strs.join(', ');
         }
     }
-
+	*/
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

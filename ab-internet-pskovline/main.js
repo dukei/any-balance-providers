d982e9f98270f04be626d1d280d24c04feecd2e4
ivandor666 <ivandor666@gmail.com>
2013-09-11 20:16:47 +0000

    Очередная смена кабинета

--- ab-internet-pskovline/main.js
+++ ab-internet-pskovline/main.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
 Текущий баланс у оператора интернет и телевидения PskovLine.
@@ -11,38 +11,36 @@ function main() {
   AnyBalance.setDefaultCharset("utf-8");
   var prefs = AnyBalance.getPreferences();
   var url="http://stat.pskovline.ru/";
-
-//  var info = AnyBalance.requestPost(url, {user_login: prefs.login, password: prefs.password});
-  var info = AnyBalance.requestPost(url+'login', {'user[login]': prefs.login, 'user[password]': prefs.password});
+  var info = AnyBalance.requestPost(url, {login: prefs.login, password: prefs.password});
 
   var result = {success: true}, matches;
 
-        if (info.match("Неверный пользователь или пароль")) {
+        if (info.match("Неверно введен логин или пароль")) {
 		throw new AnyBalance.Error("Повторите ввод логина и пароля");
 	}
-	else if (info.match("input type=\"hidden\" id=\"profile_content\"")) { null; }
-	else if (info.match("<html id=\"nojs\">"))
-		 info = AnyBalance.requestPost(url+'/login', {'user[login]': prefs.login, 'user[password]': prefs.password});
+	else if(info.match(/rightblock.*?>/i)){
+                if(matches = info.match(/<td>(.*?)<\/td>/igm)){
 
-	if (info.match(/\"tab_user_main\".*?>/i)) {
-
-info=info.replace(/&thinsp;/ig,"").replace(/&minus;/ig,"-");
-
-                       var login, ballance, number, credit, work;
+		var i=0; while(x=matches[i]) {
+			 matches[i]=x.match('td>(.*?)<\/td','i')[1];
+		i++;
+		}
+                        var login, ballance, number;
 
 			result['success']=true;
-			result['ballance']=parseFloat(info.match(/(-?\d+,\d+)/ig)[1].replace(",","."));
-			if (AnyBalance.isAvailable('login'))
-				result['login']=info.match(/ (\w+)<\/title/i)[1];
-			if (AnyBalance.isAvailable('number'))
-				result['number']=info.match(/>Лицевой счет.*?(\d+).*?\(/i)[1];
+			result['ballance']=parseFloat(matches[3].match(/>([\d\-\.]+)</)[1]);
 
-//AnyBalance.trace("Traffic: "+parseFloat(info.match(/(\d+,\d+)/ig)[6].replace(",",".")));
+			if (AnyBalance.isAvailable('login'))
+				result['login'] =matches[5];
+
+			if (AnyBalance.isAvailable('number'))
+				result['number']=matches[1];
 
                         AnyBalance.setResult(result);
-
+                }
+	
         }
-
+        
         if(!AnyBalance.isSetResultCalled())
                 throw new AnyBalance.Error("Ошибка. Проверьте работу личного кабинета. Если Вы можете войти в личный кабинет, а провайдер не работает, обратитесь к автору провайдера.");
 }

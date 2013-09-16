38d1397fc18abaef0c021c838107d889c52545f8
stark027 <stark027@gmail.com>
2013-09-16 09:43:56 +0000

    Ulmart: - Новые счетчики: Кол-во участников, Сводка по участникам

--- ab-bonus-ulmart/main.js
+++ ab-bonus-ulmart/main.js
@@ -6,6 +6,15 @@
 Сайт оператора: http://ulmart.ru
 Личный кабинет: http://www.ulmart.ru/cabinet/
 */
+var g_headers = {
+	'Accept':'text/html, */*; q=0.01',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'X-Requested-With':'XMLHttpRequest',
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
@@ -17,7 +26,7 @@ function main(){
         j_username:prefs.login,
         j_password:prefs.password,
         _spring_security_remember_me:''
-    });
+    }, g_headers);
 
     if(!/\/logout/.test(html)){
         var error = getParam(html, null, null, /<div[^>]+id="loginErrorDiv"[^>]*>([\s\S]*?)(?:Проверьте состояние|<\/div>)/i, replaceTagsAndSpaces, html_entity_decode);
@@ -33,5 +42,19 @@ function main(){
     getParam(html, result, 'price', /цена(?:&nbsp;|\s)+(\d+)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, '__tariff', /<div[^>]+class="b-dropdown-popup__info"[^>]*>[\s\S]*?<\/div>([\s\S]*?)<ul/i, replaceTagsAndSpaces, html_entity_decode);
 
+	if(isAvailable('subaccountall', 'subaccounts')){
+		html = AnyBalance.requestGet(baseurl + 'cabinet/xxlBonus', addHeaders({Referer:'http://www.ulmart.ru/cabinet?v=bonus'}));
+		getParam(html, result, 'subaccounts', /вашей сети[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+		
+		var list = getParam(html, null, null, /(<ul>[\s\S]*?<\/ul>)/i, replaceTagsAndSpaces, html_entity_decode);
+		if(list){
+			result.subaccountall = '';
+			var li = sumParam(html, null, null, /<li[^>]*class="cl"[^>]*>[\s\S]*?<\/li>/ig);
+			for(i=0; i< li.length; i++){
+				result.subaccountall += getParam(li[i], null, null, null, replaceTagsAndSpaces, html_entity_decode) +'\n';
+			}
+		}
+	}
+	
     AnyBalance.setResult(result);
 }

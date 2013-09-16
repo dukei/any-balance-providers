f293633da53bbdbf5ed25f2e77a25e7496f00f97
stark027 <stark027@gmail.com>
2013-09-16 08:51:25 +0000

    BalticMiles: - Minor fixes

--- ab-bonus-balticmiles/main.js
+++ ab-bonus-balticmiles/main.js
@@ -15,41 +15,25 @@ function main(){
     	redirect_url:baseurl,
         identity:prefs.login,
         password:prefs.password,
-        login:"Login"
+        login:'Login'
     }); 
-
-    //После входа обязательно проверяем маркер успешного входа
-    //Обычно это ссылка на выход, хотя иногда приходится искать что-то ещё
     if (!/\/Logout/i.test(html)){
-        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
         var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
-        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
         throw new AnyBalance.Error("Can't enter to the account. Possible that website is changed.");
     }
-
-    //Раз мы здесь, то мы успешно вошли в кабинет
-    //Получаем все счетчики
     var result = {success: true};
-    getParam(html, result, 'name', /class="hello"[\s\S]*?>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /class="userclass"[\s\S]*?>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /My Points:[\s\S]*?<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'name', /class="hello"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /class="userclass"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /My Points:[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
 
-    if (isAvailable('card') ||
-        isAvailable('statusbalance') ||
-        isAvailable('explastupdated') ||
-        isAvailable('expbalance1') ||
-        isAvailable('expbalance2') ||
-        isAvailable('expmonth1') ||
-        isAvailable('expmonth2') ||
-        isAvailable('expiration'))
-    {
+    if (isAvailable('card', 'statusbalance', 'explastupdated', 'expbalance1', 'expbalance2', 'expmonth1', 'expmonth2', 'expiration')){
     	AnyBalance.trace('GET: ' + baseurl + 'my-account/account-statement');
         html = AnyBalance.requestGet(baseurl + 'my-account/account-statement');
-        getParam(html, result, 'statusbalance', /<span>([^<>]*?)<\/span>[\s]*?Status Points/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'card', /Your BalticMiles card number:[\s\S]*?<span>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-        getParam(html, result, 'explastupdated', /Point expiry information last updated:[\s\S]*?<span>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseDateISO, 'expiration');
+        getParam(html, result, 'statusbalance', /<span>([^<]*)<\/span>\s*Status Points/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'card', /Your BalticMiles card number:(?:[\s\S]*?<[^>]*>){3}([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+        getParam(html, result, 'explastupdated', /Point expiry information last updated:(?:[\s\S]*?<[^>]*>){1}([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseDateISO, 'expiration');
         expired = html.match(/Will expire[\s\S]*?<span>\s*<span>\s*([^<>]*?)<\/span>\s*in\s([^<>]*?)\s*<\/span>\s*<span>\s*<span>\s*([^<>]*?)<\/span>\s*in\s([^<>]*?)\s*<\/span>/i);
         if (expired != null && expired.length >= 5)
         {
@@ -57,16 +41,13 @@ function main(){
         	getParam(expired[2], result, 'expmonth1', null, replaceTagsAndSpaces, html_entity_decode, 'expiration');
         	getParam(expired[3], result, 'expbalance2', null, replaceTagsAndSpaces, parseBalance, 'expiration');
         	getParam(expired[4], result, 'expmonth2', null, replaceTagsAndSpaces, html_entity_decode, 'expiration');
-        	if (isAvailable('expiration'))
-        	{
+			if (isAvailable('expiration')) {
             	var date = new Date(result['explastupdated']);
-            	result['expiration'] = result['expbalance1'] + ' in ' + result['expmonth1'] + ', ' +
-                	result['expbalance2'] + ' in ' + result['expmonth2'] +
+            	result['expiration'] = result['expbalance1'] + ' in ' + result['expmonth1'] + ', ' + result['expbalance2'] + ' in ' + result['expmonth2'] +
                 	', updated at: ' + date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
         	}
         }
     }
 	
-    //Возвращаем результат
     AnyBalance.setResult(result);
 }

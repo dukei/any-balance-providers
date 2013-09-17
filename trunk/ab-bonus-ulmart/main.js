31cf1d40eedfb2c3b4483d229e4126ceb0bf7ffa
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-17 14:16:04 +0000

    Ullmart: - Мелкие улучшения

--- trunk/ab-bonus-ulmart/main.js
+++ trunk/ab-bonus-ulmart/main.js
@@ -35,26 +35,25 @@ function main(){
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
 
-    var result = {success: true};
-
+    var result = {success: true, subaccountall:''};
+	
     getParam(html, result, 'balance', /class="title"[^>]*>XXL-Бонус[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'fio', /<div[^>]+class="name"[^>]*>([\s\S]*?)(?:▼|<\/div>)/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'price', /цена(?:&nbsp;|\s)+(\d+)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, '__tariff', /<div[^>]+class="b-dropdown-popup__info"[^>]*>[\s\S]*?<\/div>([\s\S]*?)<ul/i, replaceTagsAndSpaces, html_entity_decode);
-
-	if(isAvailable('subaccountall', 'subaccounts')){
+	
+	if(isAvailable(['subaccountall', 'subaccounts'])){
 		html = AnyBalance.requestGet(baseurl + 'cabinet/xxlBonus', addHeaders({Referer:'http://www.ulmart.ru/cabinet?v=bonus'}));
+		
 		getParam(html, result, 'subaccounts', /вашей сети[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 		
 		var list = getParam(html, null, null, /(<ul>[\s\S]*?<\/ul>)/i, replaceTagsAndSpaces, html_entity_decode);
 		if(list){
-			result.subaccountall = '';
 			var li = sumParam(html, null, null, /<li[^>]*class="cl"[^>]*>[\s\S]*?<\/li>/ig);
 			for(i=0; i< li.length; i++){
 				result.subaccountall += getParam(li[i], null, null, null, replaceTagsAndSpaces, html_entity_decode) +'\n';
 			}
 		}
-	}
-	
+	}	
     AnyBalance.setResult(result);
 }

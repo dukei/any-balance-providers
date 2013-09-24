4e6e396781fe2555b8c2384973643e6e510aa774
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-24 09:15:48 +0000

    Киномакс: - Поддержка изменений в личном кабинете (Stark)

--- trunk/ab-bonus-kinomax/main.js
+++ trunk/ab-bonus-kinomax/main.js
@@ -7,55 +7,51 @@
 Сайт сети: http://kinomax.ru/
 Личный кабинет: http://kinomax.ru/users/lk/dnk.htm
 */
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main() {
-    var prefs = AnyBalance.getPreferences();
-    var html = AnyBalance.requestPost('http://kinomax.ru/users/login/', {
-        'userEmail':prefs.login,
-        'userPassword':prefs.password,
-        'login_submit':''
-    });
-
-    var result = {
-        success: true
-    };
-
-	html = AnyBalance.requestGet('http://kinomax.ru/users/lk/dnk.htm');
-    var r = new RegExp('<a[^>]+href="/users/logout/">');
-	if(r.test(html)) {
-		AnyBalance.setDefaultCharset('utf-8');
-
-		html = AnyBalance.requestGet('http://kinomax.ru/schedule/lk.php');
-		//AnyBalance.trace(html);
-
-		r=new RegExp('карта № <b>(\\d+)</b>');
-		matches=r.exec(html);
-		if(matches==null) throw new AnyBalance.Error("Ошибка получения номера карты.");
-		result.card=matches[1];
-
-		r=new RegExp('ФИО: <b>([^<]+)</b>');
-		matches=r.exec(html);
-		if(matches==null) throw new AnyBalance.Error("Ошибка получения ФИО.");
-		result.name=matches[1];
-
-		r=new RegExp('бонусов: <b>Мультикарта (\\d+)%</b></p>');
-		matches=r.exec(html);
-		if(matches==null) throw new AnyBalance.Error("Ошибка получения уровня накопления бонусов.");
-		result.level=parseInt(matches[1]);
-
-		r=new RegExp('<p>Накопительный баланс: <b>(\\d+)руб (\\d+)коп</b></p>');
-		matches=r.exec(html);
-		if(matches==null) throw new AnyBalance.Error("Ошибка получения накопительного баланса.");
-		result.accum=parseInt(matches[1])+parseInt(matches[2])/100;
-
-		r=new RegExp('<p>Активный баланс \\(накопленные бонусы\\): <b>(\\d+)руб (\\d+)коп</b></p>');
-		matches=r.exec(html);
-		if(matches==null) throw new AnyBalance.Error("Ошибка получения активного баланса.");
-		result.active=parseInt(matches[1])+parseInt(matches[2])/100;
-
-	} else {
-		throw new AnyBalance.Error('Не удается авторизоваться. Проверьте логин и пароль.');
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://kinomax.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	if(!prefs.login)
+		throw new AnyBalance.Error('Введите логин!');
+	if(!prefs.password)
+		throw new AnyBalance.Error('Введите пароль!');
+		
+	var html = AnyBalance.requestPost(baseurl + 'index2.php?r=lk/login', {
+        'LoginForm[username]':prefs.login,
+        'LoginForm[password]':prefs.password,
+        'login_submit.x':38,
+		'login_submit.y':7,
+	}, addHeaders({Referer: baseurl + 'index2.php?r=lk/login'}));
+	
+	if(!/editprofile/i.test(html)) {
+		var error = getParam(html, null, null, /class="error-flash"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+		if(error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	if(isAvailable(['card', 'name'])) {
+		html = AnyBalance.requestGet(baseurl + 'index2.php?r=lk/multicard', g_headers);
+		getParam(html, result, 'card', /<strong>Карта (\d+)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'name', /<strong>Карта \d+<\/strong>[^\(]*\(([^\)]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	}
 
+	if(isAvailable(['level', 'accum', 'active'])) {
+		html = AnyBalance.requestGet(baseurl + 'index2.php?r=lk/cardinfo&_=1380012900366', g_headers);
+		getParam(html, result, 'level', /Мультикарта (\d+)%/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'accum', /Накопительный баланс:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'active', /Активный баланс:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	}
+	
     AnyBalance.setResult(result);
 }

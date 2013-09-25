42ae97a2485b908bfd59bb07a337ba0c5290fd07
dmitry.kochin <dmitry.kochin@gmail.com>
2013-09-25 05:29:51 +0000

    велком - поддержка изменений в процедуре входа

--- ab-cell-velcom/library.js
+++ ab-cell-velcom/library.js
@@ -681,7 +681,7 @@ function requestPostMultipart(url, data, headers){
 		'',
 		data[name]);
 	}
-	parts.push(boundary, '--');
+	parts.push(boundary + '--');
         if(!headers) headers = {};
 	headers['Content-Type'] = 'multipart/form-data; boundary=' + boundary.substr(2);
 	return AnyBalance.requestPost(url, parts.join('\r\n'), headers);

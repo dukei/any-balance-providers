177029af950b00476e68b0005f6262a5f4f0c5b4
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-25 11:50:13 +0000

    Новый провайдер: АйМаниБанк (iclick)

--- trunk/ab-money-imoneybank-iclick/main.js
+++ trunk/ab-money-imoneybank-iclick/main.js
@@ -10,10 +10,10 @@ var g_headers = {
 	'Origin':'https://iclick.imoneybank.ru',
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'
 };
+var baseurl = 'https://iclick.imoneybank.ru/';
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://iclick.imoneybank.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
 
     var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
@@ -46,33 +46,48 @@ function main(){
     }
 	var result = {success: true};
 	
-	var cardsTable = getParam(html, null, null, /<span>Карты<\/span>[^>]*>\s*(<table[\s\S]*?<\/table>)/i);
+	if(!prefs.cardnum) {
+		fetch(html, result);
+	} else {
+		/*var cardnum = prefs.cardnum ? prefs.cardnum : '\\d{4}';
+		var re = new RegExp('class="code"[^>]*>([^<]*)(?:[^>]*>){2,3}[^<]*' + cardnum + '\\s*<', 'i');
+		var code = getParam(html, null, null, re);
+		if(!code)
+			throw new AnyBalance.Error('Не удаётся получить данные, свяжитесь с разработчиком');
+			
+		AnyBalance.trace('Нашли код нужной карты '+code);
+		
+		html = AnyBalance.requestPost(baseurl + 'frontend/frontend', {
+			RQ_TYPE:'WORK',
+			Step_ID:3,
+			SCREEN_ID:'MAIN',
+			SID:sid,
+			MENU_ID:'CONTRACT_LIST',
+			ITEM_ID:'SELECT',
+			CONTRACT_TO:code,
+		}, addHeaders({Referer: baseurl + 'frontend/frontend'}));
+
+		fetch(html, result);*/
+	}
 	
-	
-	(<tr\s*>[\s\S]*?(?!<th>)</tr>)
-	
-	
-	//(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)</
-	
-	
-	
-	
-	/*// карты
-	html = AnyBalance.requestGet(baseurl + 'accounts/imoney-cards', g_headers);
-	var json = getJson(html);
-	
-	
-	// Читаем в цикле
-	for(var i = 0; i < json.accounts.length; i++)
-	{
-		var current = json.accounts[i];
-		result.balance = parseBalance(current.balance+'');
-		result.type = current.accountType;
-		result.deadline = parseDate(current.expiry);
-		result.acc_num = current.accountNumber
-		result.card_num = result.__tariff = current.maskedNumber
-		// Убрать, пока нет смысла читать цикл, возьмем первую карту
-		break;
-	}*/
     AnyBalance.setResult(result);
+}
+
+function fetch(html, result) {
+	var table = getParam(html, null, null, /<span>Карты<\/span>[^>]*>\s*(<table[\s\S]*?<\/table>)/i);
+	if(!table)
+		throw new AnyBalance.Error('Не удаётся найти ни одной карты или счета');
+		
+	getParam(table, result, 'balance', /Остаток[^>]*>(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(table, result, ['currency', 'balance'], /Остаток[^>]*>(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseCurrency);
+	getParam(table, result, 'card_num', /Номер[^>]*>(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces);
+	getParam(table, result, 'type', /Продукт[^>]*>(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces);
+	result.__tariff = result.type;
+	if(isAvailable(['deadline', 'status', 'acc_num'])) {
+		table = AnyBalance.requestGet(baseurl + getParam(table, null, null, /Остаток[^>]*>(?:[\s\S]*?<td[^>]*>){1}[\s\S]*?href="\/([^"]*)/i), g_headers);
+		
+		getParam(table, result, 'deadline', /Действительна по(?:[\s\S]*?<dd[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseDate);
+		getParam(table, result, 'status', /Статус карты(?:[\s\S]*?<dd[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces);
+		getParam(table, result, 'acc_num', /Счет карты(?:[\s\S]*?<dd[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces);
+	}
 }

150e2822222042dbda85300818207b1c0daa3ba2
stark027 <stark027@gmail.com>
2013-09-25 08:12:13 +0000

    Ufanet: - Поддержка корпоративных тарифов, у которых в логине есть кирилица - Автоматическое определение кабинета, поддерживаются все тарифы (Stark)

--- ab-internet-ufanet/main.js
+++ ab-internet-ufanet/main.js
@@ -5,41 +5,30 @@
 
 Сайт оператора: http://ufanet.ru/
 Личный кабинет: https://my.ufanet.ru/
+
+Отлаживается отладчиком параметры:
+__dbg:true, - без этого будет падать
+__dbgCab:'old' - определяем кабинет
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
+function encodeURIComponent1251 (str) {
+	var transAnsiAjaxSys = [];
+	for (var i = 0x410; i <= 0x44F; i++)
+		transAnsiAjaxSys[i] = i - 0x350; // А-Яа-я
+		
+	transAnsiAjaxSys[0x401] = 0xA8;    // Ё
+	transAnsiAjaxSys[0x451] = 0xB8;    // ё
 
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
+	var ret = [];
+	// Составляем массив кодов символов, попутно переводим кириллицу
+	for (var i = 0; i < str.length; i++) {
+		var n = str.charCodeAt(i);
+		if (typeof transAnsiAjaxSys[n] != 'undefined')
+			n = transAnsiAjaxSys[n];
+		if (n <= 0xFF)
+			ret.push(n);
 	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseTrafficGb(str){
-  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-  return parseFloat((val/1024).toFixed(2));
+	return escape(String.fromCharCode.apply(null, ret));
 }
 
 function main(){
@@ -49,54 +38,66 @@ function main(){
     if(prefs.licschet && !/^\d{5,}$/.test(prefs.licschet))
         throw new AnyBalance.Error('Укажите цифры номера лицевого счета, по которому вы хотите получить информацию, или не указывайте ничего, чтобы получить информацию по первому лицевому счету.');
 
-    var baseurl = "https://my.ufanet.ru/";
+    var baseurl = 'https://my.ufanet.ru/';
+	var html = '';
+	
+	if(!prefs.__dbg) {
+		html = AnyBalance.requestPost(baseurl + 'login', {
+			city: (prefs.city ? prefs.city : 'ufa'),
+			contract:encodeURIComponent1251(prefs.login),
+			password:prefs.password
+		});
+	} else {
+		if(prefs.__dbgCab == 'old')
+			html = AnyBalance.requestGet('http://stat1.ufanet.ru/bgbilling/webexecuter');
+		else
+			html = AnyBalance.requestGet(baseurl);
+	}
+	var result = {success: true};
+	
+	if(AnyBalance.getLastUrl().indexOf('stat1.ufanet.ru') > 0) {
+		AnyBalance.setDefaultCharset('windows-1251');
+		AnyBalance.trace('Попали в старый кабинет, обратите внимание, в старом кабинете игнорируется настройка № договора, для исправления свяжитесь с автором по e-mail');
+		AnyBalance.trace(html);
+		if(!/action=Exit/i.test(html)){
+			var error = getParam(html, null, null, /<div[^>]*class=["']error[^>]*>([\s\S]*?)<\/div>/, replaceTagsAndSpaces, html_entity_decode);
+			if(error)
+				throw new AnyBalance.Error(error);
+			throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+		}
+		// http://stat1.ufanet.ru/bgbilling/webexecuter?action=ShowBalance&mid=contract&contractId=488711
+		html = AnyBalance.requestGet('http://stat1.ufanet.ru/bgbilling/webexecuter' + getParam(html, null, null, /href="([^"]*)[^>]*>Просмотр баланса/));
+		
+		getParam(html, result, 'balance', /Исходящий остаток на конец месяца(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'agreement', /Договор №([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	} else {
+		AnyBalance.trace('Попали в новый кабинет');
+		if(!/\/logout/i.test(html)){
+			var error = getParam(html, null, null, /<div[^>]*class=["']error[^>]*>([\s\S]*?)<\/div>/, replaceTagsAndSpaces, html_entity_decode);
+			if(error)
+				throw new AnyBalance.Error(error);
+			throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+		}
+		if(prefs.licschet){
+			var ls = getParam(html, null, null, /Лицевой счет<\/p>\s*<p[^>]+class="important"[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+			if(!ls || ls.indexOf(prefs.licschet) < 0){
+				var re = new RegExp('<a[^>]+href="/(contract/change-contract/[^"]*)"[^>]*>(?:[\\s\\S](?!</a>))*?' + prefs.login, 'i');
+				var href = getParam(html, null, null, re, null, html_entity_decode);
+				AnyBalance.trace("Переходим в другой лиц. счет: " + href);
+				html = AnyBalance.requestGet('https://my.ufanet.ru/' + href);
+			}
+		}
+		getParam(html, result, 'balance', /Остаток на счете:[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'pay', /Рекомендуемая сумма к оплате:[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'daysleft', /До конца учетного периода:[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'licschet', /Лицевой счет<\/p>\s*<p[^>]+class="important"[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'agreement', /№ договора[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
 
-    var html = AnyBalance.requestPost(baseurl + 'login', {
-        city:prefs.city,
-        contract:prefs.login,
-        password:prefs.password
-    });
+		html = AnyBalance.requestGet(baseurl + "contract/information");
 
-    //AnyBalance.trace(html);
-    if(!/\/logout/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]*class=["']error[^>]*>([\s\S]*?)<\/div>/, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
-    }
-
-    var result = {success: true};
-
-    if(prefs.licschet){
-        var ls = getParam(html, null, null, /Лицевой счет<\/p>\s*<p[^>]+class="important"[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(!ls || ls.indexOf(prefs.licschet) < 0){
-            var re = new RegExp('<a[^>]+href="/(contract/change-contract/[^"]*)"[^>]*>(?:[\\s\\S](?!</a>))*?' + prefs.login, 'i');
-            var href = getParam(html, null, null, re, null, html_entity_decode);
-            AnyBalance.trace("Переходим в другой лиц. счет: " + href);
-            html = AnyBalance.requestGet('https://my.ufanet.ru/' + href);
-        }
-        
-    }
-
-    getParam(html, result, 'balance', /Остаток на счете:[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'pay', /Рекомендуемая сумма к оплате:[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'daysleft', /До конца учетного периода:[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'licschet', /Лицевой счет<\/p>\s*<p[^>]+class="important"[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'agreement', /№ договора[\S\s]*?<p[^>]*>([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    html = AnyBalance.requestGet(baseurl + "contract/information");
-
-    getParam(html, result, '__tariff', /Тарифный план([\S\s]*?)(?:сменить|<\/p>|\(<a)/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'abon', /Абонентская плата([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, '__tariff', /Тарифный план([\S\s]*?)(?:сменить|<\/p>|\(<a)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'abon', /Абонентская плата([\S\s]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+	}
 
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

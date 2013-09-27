52705a9ef2d3cbcbb57cfd729fda5b58a895b6e0
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-09-27 13:56:04 +0000

    Интеркон (Воронеж): - Новый счетчик: Бонусы будущих периодов - сумма

--- trunk/ab-internet-intercon/main.js
+++ trunk/ab-internet-intercon/main.js
@@ -7,7 +7,6 @@ var g_headers = {
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
 };
 
@@ -40,14 +39,14 @@ function main(){
 	
 	var table = getParam(html, null, null, /Бонусы будущих периодов[^>]*>\s*(<table[\s\S]*?<\/table>)/i);
 	if(table) {
+		sumParam(table, result, 'nextbonuses_summ', /(?:[\s\S]*?<td[^>]*>){4}([^<]*)/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+	
 		var tr = sumParam(table, null, null, /(<tr>[\s\S]*?<\/tr>)/ig, replaceTagsAndSpaces, html_entity_decode);
 		for(i = 0; i < tr.length; i++) {
 			result.nextbonuses += tr[i] + '\n';
 		}
+		result.nextbonuses = result.nextbonuses.replace(/^\s+|\s+$/g, '');
 	}
-	
-	
-	
-	
+
     AnyBalance.setResult(result);
 }

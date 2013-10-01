dbd9e2ab52acd6b90fd530e8a97c391136dcf21b
stark027 <stark027@gmail.com>
2013-10-01 10:55:37 +0000

    Домашний Интернет и Телевидение МТС: - Добавлен Саратов (http://stat.saratov.comstar.ru/)

--- ab-internet-mts/main.js
+++ ab-internet-mts/main.js
@@ -34,6 +34,7 @@ var regions = {
    kurgan: getKurgan,
    barnaul: getBarnaul,
    belgorod: getBelgorod,
+   saratov: getSaratov,
 };
 
 function main(){
@@ -1070,6 +1071,51 @@ function getBelgorod(){
     getParam(html, result, 'agreement', /Номер договора:[^<]*<[^>]*>([^<]*)/i, replaceTagsAndSpaces);
     getParam(html, result, 'balance', /Текущий баланс:[^<]*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
     getParam(html, result, 'username', /Мои аккаунты[\s\S]{1,150}<strong>([\s\S]*?)<\/strong>/i, null);
+
+    AnyBalance.setResult(result);
+}
+
+function getSaratov(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
+    var baseurl = 'http://stat.saratov.comstar.ru/';
+
+    if(!prefs.__dbg){
+        var html = AnyBalance.requestPost(baseurl + '?r=site/login&0=site%2Flogin', {
+            'LoginForm[login]':prefs.login,
+            yt0: 'Войти',
+            'LoginForm[password]':prefs.password
+        });
+    }else{
+        var html = AnyBalance.requestGet(baseurl + '?r=account/index');
+    }
+
+    if(!/\?r=site\/logout/i.test(html)){
+        throw new AnyBalance.Error("Не удалось войти в личный кабинет. Неправильный логин-пароль?");
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'agreement', /Номер договора:([\s\S]*?)<a/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Текущий баланс:([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance2);
+
+    //Тут услуги в таблице, пройдемся по ней и возьмем тарифные планы
+    var services = sumParam(html, null, null, /(<tr[^>]*>\s*<td[^>]+class="first_col"[^>]*>(?:[\s\S](?!\/tr))*<\/tr>)/ig);
+    var tariffs = [];
+    for(var i=0; i<services.length; ++i){
+        var tariff = getParam(services[i], null, null, /(?:[\s\S]*?<td[^>]*>){2}\s*<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(tariff == 'Услуги')
+            continue;
+        tariffs[tariffs.length] = tariff;
+        getParam(services[i], result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
+    }
+    
+    if(tariffs.length > 0)
+        result.__tariff = tariffs.join(', ');
+
+    getParam(html, result, '__tariff', /Текущий тарифный план:([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'username', /Общая информация\s+\/([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 
     AnyBalance.setResult(result);
 }

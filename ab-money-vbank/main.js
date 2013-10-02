e8edf70a53d73b7acb06188d252163920470377b
stark027 <stark027@gmail.com>
2013-10-02 12:34:07 +0000

    Банк Возрождение: - Поддержка изменений в личном кабинете

--- ab-money-vbank/main.js
+++ ab-money-vbank/main.js
@@ -6,89 +6,40 @@
 Сайт оператора: http://www.vbank.ru
 Личный кабинет: https://online.bankcard.ru
 */
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp ? regexp.exec (html) : html;
-	if (value) {
-                if(regexp)
-		    value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-
-	}
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.', /(\d)\-(\d)/g, '$1.$2'];
-
-function parseBalance(text){
-    var _text = text.replace(/\s+/g, '');
-    var val = getParam(_text, null, null, /(-?\d[\d\.,\-]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseCurrency(text){
-    var _text = text.replace(/\s+/g, '');
-    var val = getParam(_text, null, null, /[\d\.,\-]+(\S*)/);
-    AnyBalance.trace('Parsing currency (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseDate(str){
-    var matches = /(\d+)[^\d](\d+)[^\d](\d+)/.exec(str);
-    var time = 0;
-    if(matches){
-	  time = (new Date(+matches[3], matches[2]-1, +matches[1])).getTime();
-    }
-    AnyBalance.trace('Parsing date ' + new Date(time) + 'from value: ' +  str);
-    return time;
-}
-
-function getViewState(html){
-    return getParam(html, null, null, /name="__VIEWSTATE".*?value="([^"]*)"/);
-}
-
-function getEventValidation(html){
-    return getParam(html, null, null, /name="__EVENTVALIDATION".*?value="([^"]*)"/);
-}
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = "https://online.bankcard.ru/client/";
-    AnyBalance.setDefaultCharset("utf-8");
+    var baseurl = 'https://online.bankcard.ru/';
+    AnyBalance.setDefaultCharset('utf-8');
     
     if(prefs.num && !/^\d{4}$/.test(prefs.num))
         throw new AnyBalance.Error("Введите последние 4 цифры карты или не вводите ничего, чтобы показать информацию по первой карте.");
 
-    var html = AnyBalance.requestGet(baseurl + 'login.aspx');
+    var html = AnyBalance.requestGet(baseurl + 'client/logon?ReturnUrl=%2fclient%2f', g_headers);
 
-    html = AnyBalance.requestPost(baseurl + 'login.aspx', {
-        __VIEWSTATE:getViewState(html),
-        __EVENTVALIDATION:getEventValidation(html),
-        m$body$mode:'password',
-        m$body$login:prefs.login,
-        m$body$password:prefs.password,
-        m$body$card:'',
-        m$body$token:'',
-        m$body$logon:'Вход в систему'
-    });
+	var params = createFormParams(html, function(params, str, name, value){
+		if(name == 'Mode')
+			return 'password';
+		else if(name == 'UserName')
+			return prefs.login;
+		else if(name == 'Password')
+			return prefs.password;			
+		return value;
+	});
 
-    if(!/\/client\/logout\.aspx/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]class="error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+    html = AnyBalance.requestPost(baseurl + 'client/logon?ReturnUrl=%2fclient%2f', params, addHeaders({Referer: baseurl + 'client/logon?ReturnUrl=%2fclient%2f'}));
+    if(!/client\/logoff/i.test(html)){
+        var error = getParam(html, null, null, [/<div[^>]class="error"[^>]*>([\s\S]*?)<\/div>/i, /class="[^>]*error[^>]*>([^<]*)/i], replaceTagsAndSpaces);
         if(error)
             throw new AnyBalance.Error(error);
         if(/changepassword.aspx/i.test(html)){
@@ -97,11 +48,9 @@ function main(){
         }
         throw new AnyBalance.Error("Не удалось зайти в интернет-банк. Сайт изменился?");
     }
-
     try{
-        html = AnyBalance.requestGet(baseurl + "contracts.aspx");
-        
-        var reCard = new RegExp('<a[^>]+href=[\'"](card\\.aspx\\?[^\'"]*)[\'"][^>]*>\\s*\\d{4}\\s*XXXX\\s*XXXX\\s*' + (prefs.num ? prefs.num : '\\d{4}'), 'i');
+        html = AnyBalance.requestGet(baseurl + 'client/contracts');
+        var reCard = new RegExp('<a[^>]+href=[\'"](/client/?[^\'"]*)[\'"][^>]*>\\s*\\d{4}\\s*XXXX\\s*XXXX\\s*' + (prefs.num ? prefs.num : '\\d{4}'), 'i');
         var href = getParam(html, null, null, reCard, null, html_entity_decode);
         
         if(!href){
@@ -111,19 +60,19 @@ function main(){
                 throw new AnyBalance.Error('Не удалось найти ни одной карты!');
         }
         
-        html = AnyBalance.requestGet(baseurl + href + '&operation=bal');
+        html = AnyBalance.requestGet(baseurl + href);
         
         var result = {success: true};
         
-        getParam(html, result, 'cardnum', /номер:\s*<\/label>([^<]*)/i, replaceTagsAndSpaces);
-        getParam(html, result, 'currency', /Валюта:\s*<\/label>([^<]*)/i, replaceTagsAndSpaces);
-        getParam(html, result, 'cardname', /Наименование:\s*<\/label>([^<]*)/i, replaceTagsAndSpaces);
-        getParam(html, result, '__tariff', /Наименование:\s*<\/label>([^<]*)/i, replaceTagsAndSpaces);
-        getParam(html, result, 'till', /Срок действия:\s*<\/label>([^<]*)/i, replaceTagsAndSpaces, parseDate);
-        getParam(html, result, 'status', /Статус:\s*<\/label>([^<]*)/i, replaceTagsAndSpaces);
-        getParam(html, result, 'balance', /Доступные средства:[\s\S]*?<span[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'cardnum', /Номер:(?:[\s\S]*?<[^>]*td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+        getParam(html, result, ['currency', 'balance'], /Валюта:(?:[\s\S]*?<[^>]*td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+        getParam(html, result, 'cardname', /Наименование:(?:[\s\S]*?<[^>]*td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+        getParam(html, result, '__tariff', /Наименование:(?:[\s\S]*?<[^>]*td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+        getParam(html, result, 'till', /Срок действия:(?:[\s\S]*?<[^>]*td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseDate);
+        getParam(html, result, 'status', /Статус:\s*(?:[\s\S]*?<[^>]*td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+        getParam(html, result, 'balance', /Доступные средства:(?:[\s\S]*?<[^>]*td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
     }finally{
-        AnyBalance.requestGet(baseurl + 'logout.aspx');
+        AnyBalance.requestGet(baseurl + 'client/logoff');
         AnyBalance.trace("Вышли из системы");
     }
     

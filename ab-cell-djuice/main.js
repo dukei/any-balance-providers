3a776c0d94146521920dfc8612b79895c27be6c5
sergey.bashmanov <sergey.bashmanov@gmail.com>
2013-10-02 17:38:11 +0000

    ab-cell-djuice - улучшено обработка ошибок входа

--- ab-cell-djuice/main.js
+++ ab-cell-djuice/main.js
@@ -46,7 +46,7 @@ function main(){
 	var html = AnyBalance.requestPost(baseurl + "tbmb/login_djuice/perform.do", params, headers);
 	
 	if(!/\/tbmb\/logout\/perform/i.test(html)){
-		var matches = html.match(/<td class="redError"[\s\S]*?>([\s\S]*?)<\/td>/i);
+		var matches = html.match(/<td class="redError"[^>]*>([\s\S]*?)<\/td>/i);
 		if(matches){
 			throw new AnyBalance.Error(matches[1]);
 		}

3d2ddc71820fc90e7d4d6b568539525529bb6393
stark027 <stark027@gmail.com>
2013-10-09 10:02:25 +0000

    Новый провайдер: Расчетный центр Онлайн

---
+++ ab-municipal-rc-online/main.js
@@ -0,0 +1,35 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://cabinet.rc-online.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+		
+	var html = AnyBalance.requestPost(baseurl + 'go/', {
+        e_login:prefs.login,
+        e_password:prefs.password,
+		e_cookie:'on',
+        form_logon_submitted:'yes'
+    }, addHeaders({Referer: baseurl + 'login'}));
+	
+	if(!/logout/i.test(html)) {
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+    var result = {success: true};
+	getParam(html, result, 'balance', /"header-site_object"[^>]*>[^>]*>[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /Абонент:[^>]*>[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+    AnyBalance.setResult(result);
+}

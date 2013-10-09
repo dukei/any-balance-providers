adacbfb7e761ff86635609dc8e710d23dff8563d
stark027 <stark027@gmail.com>
2013-10-09 07:58:56 +0000

    Новый провайдер: Белинвестбанк (Беларусь)

---
+++ ab-money-belinvestbank/main.js
@@ -0,0 +1,83 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'utf-8, iso-8859-1, utf-16, *;q=0.7',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Linux; U; Android 4.0.4; ru-ru; Android SDK built for x86 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+};
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://ibank.belinvestbank.by/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, "Введите логин!");
+	checkEmpty(prefs.password, "Введите пароль!");
+	checkEmpty(prefs.keyword, "Введите ключевое слово!");
+		
+	var html = AnyBalance.requestGet(baseurl + 'signin', g_headers);
+
+	html = AnyBalance.requestPost(baseurl + 'signin', {
+        login:prefs.login,
+        keyword:prefs.keyword,
+    }, addHeaders({Referer: baseurl + 'signin'}));
+	
+	var encryptArrayVar = getParam(html, null, null, /var\s*keyLang\s*=\s*\[([^\]]*)/i);
+	checkEmpty(encryptArrayVar, "Не нашли ключ шифрования, свяжитесь с разработчиком", true);
+	var encryptArray = sumParam(encryptArrayVar, null, null, /\d+/ig);
+	
+	html = AnyBalance.requestPost(baseurl + 'signin2', {
+        password:cod(prefs.password, encryptArray),
+    }, addHeaders({Referer: baseurl + 'signin2'}));
+	
+	if(!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /class="attention"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		if(error && /Пароль введен неверно/i.test(error))
+			throw new AnyBalance.Error(error, null, true);
+		if(error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	// сейчас ничего кроме карт там нет, да и карта только одна, поэтому пока сделаем обработку только первой карты
+	html = AnyBalance.requestGet(baseurl + 'cards', addHeaders({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',}));
+
+	var table = getParam(html, null, null, /(<table[^>]*class="datatable"[\s\S]*?<\/table>)/i);
+	checkEmpty(table, "Не нашли ни одной карты.", true);
+	
+    var result = {success: true};
+	getParam(table, result, '__tariff', /(\*\*\*\*\s*\*\*\*\*\s*\*\*\*\*\s*\d{4})/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(table, result, 'balance', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(table, result, 'currency', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(table, result, 'validto', /Срок действия:([^<]*)/i, replaceTagsAndSpaces, parseDate);
+	getParam(table, result, 'status', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	//<div>([\s\S]*?)<nobr>\s*<b>\*\*\*\*\s*\*\*\*\*\s*\*\*\*\*\s*\d{4}
+    AnyBalance.setResult(result);
+}
+
+function cod(str, keyLang) {
+	var lang = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0','_','.','-'];
+	// Этот аррей динамический, надо его передавать сюда
+	//var keyLang = ['48','117','121','119','66','88','89','78','57','45','71','99','80','65','87','75','52','113','84','101','85','102','95','103','76','46','53','98','100','116','56','105','86','77','109','81','106','110','54','120','82','90','118','97','111','50','114','73','67','49','51','79','68','72','115','74','70','122','108','107','69','55','112','104','83'];
+	var result = '';
+	var pass = str.split('');
+	for (n = 0; n < pass.length; n++) {
+		var isLegal = false;
+		if (pass[n]) {
+			for (i = 0; i < lang.length; i++) {
+				if (pass[n] == lang[i]) {
+					result += String.fromCharCode(keyLang[i]);
+					isLegal = true;
+				}
+			}
+			if (!isLegal) {
+				result += pass[n];
+			}
+		} else {
+			break;
+		}
+	}
+	return result;
+}

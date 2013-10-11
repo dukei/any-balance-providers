d4de9a483242104ebc7c89d8c3a4d76f19d095f1
stark027 <stark027@gmail.com>
2013-10-11 08:25:23 +0000

    Airarabia.com: - Bugfixes

--- ab-bonus-airarabia/main.js
+++ ab-bonus-airarabia/main.js
@@ -15,37 +15,47 @@ var g_headers = {
 function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = 'https://reservations.airarabia.com/';
-    AnyBalance.setDefaultCharset('utf-8'); 
-
-    var html = AnyBalance.requestGet(baseurl + 'ibe/public/showReservation.action?hdnParamData=RU'+encodeURIComponent('^')+'SI&hdnCarrier=', g_headers);
-
-	var params = createFormParams(html, function(params, str, name, value){
+    AnyBalance.setDefaultCharset('utf-8');
+	// Ставит куку какую-то
+	var html = AnyBalance.requestGet('http://www.airarabia.com/home', g_headers);
+	// Только после этого доступен переход сюда
+    html = AnyBalance.requestGet(baseurl + 'ibe/public/showReservation.action?hdnParamData=RU'+encodeURIComponent('^')+'SI&hdnCarrier=', addHeaders({Referer: baseurl + 'http://www.airarabia.com/home'}));
+	// Создаем параметры
+	var params = createFormParams(html, function(params, str, name, value) {
 		if(name == 'emailId')
 			return prefs.login;
 		else if(name == 'password')
-			return prefs.password;
+			return prefs.password.substring(0,12);
 		return value;
 	});
-	
+	// Пробуем войти
 	html = AnyBalance.requestPost(baseurl + 'ibe/public/customerLogin.action', params, addHeaders({Referer: baseurl + 'ibe/public/showReservation.action?hdnParamData=RU^SI&hdnCarrier='})); 
+	try {
+		var json = getJson(html);
+	} catch(e) {
+		throw new AnyBalance.Error('Error in JSON, please, contact to developers.');
+	}
 	
-	var json = getJson(html);
-	
-	if(json.messageTxt != '')
-	{
+	if(json.messageTxt) {
         var error = json.messageTxt;
         if(error)
             throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+        throw new AnyBalance.Error('Can`t login to selfcare. Site changed?');
     }
-	html = AnyBalance.requestPost(baseurl + 'ibe/public/showCustomerLoadPage!loadCustomerHomePage.action', params, addHeaders({Referer: baseurl + 'ibe/public/showReservation.action?hdnParamData=RU^SI&hdnCarrier='}));
+	//html = AnyBalance.requestPost(baseurl + 'ibe/public/showCustomerLoadPage!loadCustomerHomePage.action', params, addHeaders({Referer: baseurl + 'ibe/public/showReservation.action?hdnParamData=RU^SI&hdnCarrier='}));
 	html = AnyBalance.requestGet(baseurl + 'ibe/public/showCustomerHomeDetail.action', g_headers);
-
-	json = getJson(html);
+	try {
+		json = getJson(html);
+	} catch(e) {
+		throw new AnyBalance.Error('Error in JSON2, please, contact to developers.');
+	}
 
     var result = {success: true};
-	result.fio = json.customer.firstName+' ' + json.customer.lastName;
-	getParam(json.totalCustomerCredit, result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.customer.firstName+' ' + json.customer.lastName, result, 'fio', null, replaceTagsAndSpaces);
+	if(result.fio)
+		result.__tariff = result.fio;
+
+	getParam(json.totalCustomerCredit+'', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
 
     AnyBalance.setResult(result);
 }

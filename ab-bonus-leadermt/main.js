c0c8ec3482db5cfaf1fa962ada381d2461baf2c3
stark027 <stark027@gmail.com>
2013-10-14 15:41:25 +0000

    Leader Bonus: - Translated to english

--- ab-bonus-leadermt/main.js
+++ ab-bonus-leadermt/main.js
@@ -1,43 +1,36 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плане для сотового оператора xxxxxx 
-
-Operator site: http://xxxxxx.ru
-Личный кабинет: https://kabinet.xxxxxx.ru/login
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
-function main(){
-    var prefs = AnyBalance.getPreferences();
-
-    var baseurl = "http://bonus.leadermt.ru/";
-
-    AnyBalance.setDefaultCharset('utf-8'); 
-
-    var html = AnyBalance.requestPost(baseurl + 'Account/LogOn', {   
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = "http://bonus.leadermt.ru/";
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	var html = AnyBalance.requestPost(baseurl + 'Account/LogOn', {   
         UserName: prefs.login,
         Password: prefs.password
     }, addHeaders({Referer: baseurl})); 
-
-    if(!/logout/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]+class="validation-summary-errors"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	
+	if(!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="validation-summary-errors"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		if(error)
+			throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Can`t login. Site changed?');
     }
-
+	
     var result = {success: true};
-    getParam(html, result, 'fio', /Здравствуйте, ([\s\S]*?)\!<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /<div[^>]*class="bonuses"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'cart', /Корзина\s*\(([\s\S]*?)\)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'fio', /Здравствуйте,([^<]*)\!/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /<div[^>]*class="bonuses"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'cart', /Корзина\s*\((\d+)\)/i, replaceTagsAndSpaces, parseBalance);
 
     AnyBalance.setResult(result);
 }

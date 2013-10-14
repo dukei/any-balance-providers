b0bd03a324a5cf349dd54d0c129bab2765da62e3
stark027 <stark027@gmail.com>
2013-10-14 13:44:42 +0000

    Новый провайдер: Obltelecom

---
+++ ab-internet-obltelecom/main.js
@@ -0,0 +1,39 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://billing.obltelecom.ru/';
+	AnyBalance.setDefaultCharset('windows-1251');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+		
+	var html = AnyBalance.requestGet(baseurl + 'ds/', g_headers);
+	
+	var action = getParam(html, null, null, /name='LoginForm'[^>]*action='([^']*)/i);
+	
+	html = AnyBalance.requestPost(baseurl + 'ds/'+action, {
+        CardNum:prefs.login,
+        Pwd:prefs.password,
+        login_submit:'OK'
+    }, addHeaders({Referer: baseurl + 'ds/'}));
+	
+	if(!/Изменить пароль/i.test(html)) {
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+    var result = {success: true};
+	getParam(html, result, 'fio', /Клиент:(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'dogovor', /Договор:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Баланс:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+    AnyBalance.setResult(result);
+}

c05e8533cd1576739ae0917bae9478c07b430094
stark027 <stark027@gmail.com>
2013-10-15 15:00:05 +0000

    SIPNET: Мелочи

--- ab-telephony-sipnet/main.js
+++ ab-telephony-sipnet/main.js
@@ -12,15 +12,19 @@ function main(){
 	var info = AnyBalance.requestGet('https://customer.voipexchange.ru/cgi-bin/Exchange.dll/MTK?oper=3&uid=' + encodeURIComponent(prefs.login));
 
 	if(!/Информация о клиенте/i.test(info))
-		throw new AnyBalance.Error('Can`t login. Check login and password');
+		throw new AnyBalance.Error('Can`t login. Check login and password!');
 
 	var result = {success: true};
 	getParam(info, result, '__tariff', /Тарифный план(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(info, result, 'sipid', /SIP ID(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(info, result, 'login', /Логин(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(info, result, 'balance_precise', /Остаток(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	if(AnyBalance.isAvailable('balance'))
-		result['balance'] = result['balance_precise'].toFixed(2);
+	
+	var balance = getParam(info, null, null, /Остаток(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	if(!balance)
+		throw new AnyBalance.Error('Can`t find balance! Site changed?');
+	
+	getParam(balance, result, 'balance_precise');
+	getParam(balance.toFixed(2), result, 'balance');
 
 	AnyBalance.setResult(result);
 }

5ae7f2df03777c347cc82268d1c671d868e8ae89
poluphanov.vitaliy@gmail.com <poluphanov.vitaliy@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-10-15 13:33:47 +0000

    SipNet - добавлен новый счетчик (точный баланс), баланс округляется до центов (автор - Erelen)

--- trunk/ab-telephony-sipnet/main.js
+++ trunk/ab-telephony-sipnet/main.js
@@ -1,4 +1,4 @@
- /** 
+/*
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
@@ -6,19 +6,21 @@ function main(){
 	var prefs = AnyBalance.getPreferences();
 	checkEmpty(prefs.login, 'Enter login or SIP ID');
 	checkEmpty(prefs.password, 'Enter password');
-    
+
 	AnyBalance.setAuthentication(prefs.login, prefs.password);
-	
+
 	var info = AnyBalance.requestGet('https://customer.voipexchange.ru/cgi-bin/Exchange.dll/MTK?oper=3&uid=' + encodeURIComponent(prefs.login));
-	
+
 	if(!/Информация о клиенте/i.test(info))
 		throw new AnyBalance.Error('Can`t login. Check login and password');
-	
+
 	var result = {success: true};
 	getParam(info, result, '__tariff', /Тарифный план(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(info, result, 'sipid', /SIP ID(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(info, result, 'login', /Логин(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(info, result, 'balance', /Остаток(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	
-    AnyBalance.setResult(result);
+	getParam(info, result, 'balance_precise', /Остаток(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	if(AnyBalance.isAvailable('balance'))
+		result['balance'] = result['balance_precise'].toFixed(2);
+
+	AnyBalance.setResult(result);
 }

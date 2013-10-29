7e101ba9a15a179c4e142ce26143781a4ca2afe7
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-10-29 13:33:29 +0000

    МГТС

--- trunk/ab-telephony-mgts/main.js
+++ trunk/ab-telephony-mgts/main.js
@@ -1,76 +1,39 @@
- /*
- 
+/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Московская Городская Телефонная Сеть
-Сайт оператора: http://www.mgts.ru/
-Личный кабинет: https://lk.mgts.ru
-
 */
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function getViewState(html){
-    return getParam(html, null, null, /name="__VIEWSTATE".*?value="([^"]*)"/);
-}
-
-function main(){
-    AnyBalance.setDefaultCharset("utf-8");
-    
-    var baseurl = "https://ihelper.mgts.ru/CustomerSelfCare2/";
+function main() {
+	AnyBalance.setDefaultCharset("utf-8");
+	var baseurl = "https://login.mgts.ru/amserver/UI/Login";
 	var prefs = AnyBalance.getPreferences();
-    
-    var html = AnyBalance.requestGet(baseurl);
-    var viewstate = getViewState(html);
-
-    var pin = prefs.password; //.substr(0, 8); //Слишком длинные пины тупо не воспринимаются
-    
-    html = AnyBalance.requestPost(baseurl + "logon.aspx", {
-        	__VIEWSTATE: viewstate,
-		ctl00$MainContent$tbPhoneNumber: prefs.login,
-		ctl00$MainContent$tbPassword: pin,
-                ctl00$MainContent$btnEnter: "Вход"
-    });
-    
-    $html = $(html);
-    var error = $.trim($html.find(".b_error").text());
-    if(error)
-        throw new AnyBalance.Error(error);
-    
-    var result = {success: true};
-    getParam(html, result, 'fio', /<h3[^>]*>([^<]*)/i, replaceTagsAndSpaces);
-    getParam(html, result, 'licschet', /Лицевой счет:\s*<strong[^>]*>([^<]*)/i, replaceTagsAndSpaces);
-    getParam(html, result, '__tariff', /Тарифный план:\s*<strong[^>]*>([^<]*?)\./i, replaceTagsAndSpaces);
-    getParam(html, result, 'balance', /"balance-value"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    
-    AnyBalance.setResult(result);
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	var pin = prefs.password; //.substr(0, 8); //Слишком длинные пины тупо не воспринимаются
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'IDToken1') return prefs.login;
+		if (name == 'IDToken2') return pin;
+		return value;
+	});
+	html = AnyBalance.requestPost(baseurl, params, addHeaders({
+		Referer: 'https://login.mgts.ru/amserver/UI/Login'
+	}));
+	if (!/logout/i.test(html)) {
+		var errors = sumParam(html, null, null, /"auth-error-text"[^>]*>([^<]*)/ig);
+		if (errors) throw new AnyBalance.Error(errors.join(' '));
+		throw new AnyBalance.Error('Login failed, is site changed?');
+	}
+	var result = {
+		success: true
+	};
+	getParam(html, result, 'fio', /"cabinet-aside"[^>]*>[^>]*<h3>((?:[^>]*>){6})/i, replaceTagsAndSpaces);
+	getParam(html, result, 'balance', /Баланс:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'licschet', /Лицевой счет:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+	//getParam(html, result, '__tariff', /Тарифный план:\s*<strong[^>]*>([^<]*?)\./i, replaceTagsAndSpaces);
+	AnyBalance.setResult(result);
 }

d9575ca43638c3fbc4d0bbb8b5aacdd1999ca395
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-10-29 09:34:05 +0000

    Новый провайдер: EcoLines

---
+++ trunk/ab-bonus-ecolines/main.js
@@ -0,0 +1,39 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://legacy.ecolines.net/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Enter login!');
+	checkEmpty(prefs.password, 'Enter password!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'index.php?s=791', g_headers);
+	
+	html = AnyBalance.requestPost(baseurl + 'index.php?s=791', {
+        email:prefs.login,
+        pass:prefs.password,
+        login:'OK'
+    }, addHeaders({Referer: baseurl + 'login'}));
+	
+	if(!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<td\s*align="left"\s*colspan="\d+">\s*<br>\s*<b>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+		if(error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Login failed, is site changed?');
+	}
+    var result = {success: true};
+	getParam(html, result, 'balance', /На Вашем счете накоплено[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	
+    AnyBalance.setResult(result);
+}

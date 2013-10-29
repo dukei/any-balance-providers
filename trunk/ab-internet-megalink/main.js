70fc08042478e9d42bf8dc173c906edb6d40b490
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-10-29 09:46:50 +0000

    Новый провайдер: Мегалинк (Украина)

---
+++ trunk/ab-internet-megalink/main.js
@@ -0,0 +1,40 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.megalink.com.ua/';
+	AnyBalance.setDefaultCharset('koi8-r');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'index.php', g_headers);
+	
+	html = AnyBalance.requestPost(baseurl, {
+        un:prefs.login,
+        pw:prefs.password,
+        func:'userLogin'
+    }, addHeaders({Referer: baseurl}));
+	
+	if(!/logout/i.test(html)) {
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	html = AnyBalance.requestGet(baseurl + 'index.php?page=pa_info', g_headers);
+	
+    var result = {success: true};
+	getParam(html, result, 'accnum', /Лицевой счет(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Текущая сумма на счету(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+    AnyBalance.setResult(result);
+}

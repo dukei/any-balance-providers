ac6cc184856bcd66d2e88ab72ff6eb85d10d7b42
poluphanov.vitaliy <poluphanov.vitaliy@gmail.com>
2013-10-31 11:27:18 +0000

    World of Tanks - восстановление провайдера

---
+++ ab-game-wot/common_functions.js
@@ -0,0 +1,36 @@
+﻿function checkEmpty(param, error) {
+    if (!param || param == '')
+        throw new AnyBalance.Error(error);
+}
+
+function getData(url) {
+	var data = AnyBalance.requestGet(url);
+	if (data){
+		var code = data.match(/<title>(.+?)<\/title>/i);
+		if (code){
+			throw new AnyBalance.Error(code[1]);
+		} else {
+			var js = $.parseJSON(data);
+			var st =  js.status
+			if (st == 'ok'){
+				return js;
+			} else if (st == 'error'){
+				var err = js.error
+				throw new AnyBalance.Error('Ошибка: ' + err);
+			} else {
+				throw new AnyBalance.Error('Неизвестный ответ сервера: ' + st);
+			}
+		}
+	} else {
+		throw new AnyBalance.Error('Неизвестная ошибка');
+	}
+}
+
+function getID(nick, API_ADDR, APP_ID) {
+	var v = getData(API_ADDR + '/2.0/account/list/?application_id=' + APP_ID + '&search=' + nick);
+	if (v.data[0]) {
+		return v.data[0].id;
+	} else {
+		throw new AnyBalance.Error('ID not found! Check your settings.');
+	}
+}

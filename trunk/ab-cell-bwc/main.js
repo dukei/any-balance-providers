d61bc13866a982852f48bed66f2b9d3cb27081cb
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-05 11:51:54 +0000

    БайкалВестКом (Ростелеком) v4

--- trunk/ab-cell-bwc/main.js
+++ trunk/ab-cell-bwc/main.js
@@ -32,52 +32,18 @@ function main() {
 			AnyBalance.trace('Нашли опцию ' + name);
 			// Интернет 
 			if (/3G|GPRS|\d+\s+Мб/i.test(name)) {
-				sumParam(option, result, 'traffic_total', /(?:[^>]*>){6}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-				sumParam(option, result, 'traffic_used', /(?:[^>]*>){8}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-				getParam((result.traffic_total - result.traffic_used) + '', result, 'traffic_left');
+				sumParam(option, result, 'traffic_used', /(?:[^>]*>){6}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+				sumParam(option, result, 'traffic_left', /(?:[^>]*>){8}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+				getParam(result.traffic_left + result.traffic_used, result, 'traffic_total');
 			} // Пакеты минут
 			else if (/\d+\s+мин/i.test(name)) {
-				sumParam(option, result, 'min_total', /(?:[^>]*>){6}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-				sumParam(option, result, 'min_used', /(?:[^>]*>){8}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-				getParam((result.min_total - result.min_used) + '', result, 'min_left');
+				sumParam(option, result, 'min_used', /(?:[^>]*>){6}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+				sumParam(option, result, 'min_left', /(?:[^>]*>){8}([^<]*)/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+				getParam(result.min_used + result.min_left, result, 'min_total');
 			} else {
 				AnyBalance.trace('Неизвестная опция ' + name + ' свяжитесь с разработчиком! ' + option);
 			}
 		}
 	}
-	/*var $tableCounters = $html.find('h2:contains("Общая продолжительность разговоров")').next();
-    $tableCounters.find('tr').each(function(index){
-        var str = $('td:nth-child(2)', this).text();
-        if(!str)
-            return;
-        
-        //Пакет минут
-        if(matches = str.match(/(\d+) мин/)){
-            result.min_total = parseInt(matches[1]);
-            str = $('td:nth-child(3)', this).text();
-            if(matches = str.match(/(\d+):\d+/)){
-                result.min_used = parseInt(matches[1]);
-                result.min_left = result.min_total - result.min_used;
-            }
-        }else if(matches = str.match(/([\d\.]+) Мб/)){
-            result.traffic_total = parseFloat(matches[1]);
-            str = $('td:nth-child(3)', this).text();
-            if(matches = str.match(/([\d\.]+)/)){
-                result.traffic_used = parseFloat(matches[1]);
-                result.traffic_left = result.traffic_total - result.traffic_used;
-            }
-        }else if(str.match(/GPRS/i)){
-            str = $('td:nth-child(3)', this).text();
-            if(matches = str.match(/([\d\.]+)/)){
-                result.traffic_used = parseFloat(matches[1]);
-            }
-        }
-    });
-    
-    html = AnyBalance.requestGet(baseurl + 'function=is_tarif&inf=3');
-    var val = $(html).find('h2:contains("Тарифный план:")').text();
-    if(val && (matches = val.match(/:\s*(.*)/))){
-        result.__tariff = matches[1];
-    }*/
 	AnyBalance.setResult(result);
 }

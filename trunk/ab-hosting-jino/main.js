1f8ed75f8bbf545772f068c66c7c55b004eea803
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-06 09:15:47 +0000

    Jino v5

--- trunk/ab-hosting-jino/main.js
+++ trunk/ab-hosting-jino/main.js
@@ -1,114 +1,93 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
+
 var g_headers = {
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0',
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-	'Accept-Encoding': 'gzip, deflate',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
-function main()
-{
+function main() {
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'http://cp.jino.ru/cpanel';
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+    var baseurl = 'https://auth.jino.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
 
-    var html = AnyBalance.requestPost(baseurl, {
-		auth:'true',
-		login:prefs.login,
-		next:'/',
-		password:prefs.password,
-    }, g_headers); 
+	if(!prefs.dbg) {
+		var html = AnyBalance.requestGet(baseurl + 'login/cpanel/', g_headers);
+		
+		var params = createFormParams(html, function(params, str, name, value) {
+			if (name == 'login') 
+				return prefs.login;
+			else if (name == 'password')
+				return prefs.password;
 
-    if(!/logout=true/i.test(html)){
-        var error = getParam(html, null, null, /msgs"[^>]*>[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
+			return value;
+		});
+		
+		html = AnyBalance.requestPost(baseurl + 'login/cpanel/', params, addHeaders({Referer: baseurl + 'login/cpanel/'})); 		
+		
+		if(!/logout=true/i.test(html)){
+			var error = getParam(html, null, null, /msgs"[^>]*>[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+			if(error)
+				throw new AnyBalance.Error(error);
+			throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+		}
+	} else {
+		var html = AnyBalance.requestGet('https://cp.jino.ru/', g_headers);
+	}
+	
     var result = {success: true};
+	
     getParam(html, result, 'balance', /Баланс[^>]*>:\s*[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'deadline', /Истекает[^>]*>:\s*[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseDateMoment);
+	getParam(html, result, 'deadline', /Истекает[^>]*>:\s*[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseDateWord);
 	getParam(html, result, 'state', /Статус[^>]*>:\s*[^>]*>\s*([^<]*?)\s*</i, replaceTagsAndSpaces, html_entity_decode);
 	// Дисковое простанство
-	if(isAvailable('storage_percent','storage_used','monthly_fee','daily_fee', 'storage_percent_left', 'storage_total', 'storage_left')){
-		html = AnyBalance.requestGet(baseurl + '?area=services_srv&srv=disk');
-		getParam(html, result, 'monthly_fee', /Оплата в месяц(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, null, parseBalance);
-		getParam(html, result, 'daily_fee', /Оплата в сутки(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, null, parseBalance);
+	if(isAvailable(['storage_percent','storage_used','monthly_fee','daily_fee', 'storage_percent_left', 'storage_total', 'storage_left'])) {
+		html = AnyBalance.requestGet('https://cp.jino.ru/?area=services_srv&srv=disk');
 		
-		var ptc = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}[\s\S]*?(\d[\d.,]*)%/i, null, parseBalance);
+		getParam(html, result, 'monthly_fee', /Оплата в месяц(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'daily_fee', /Оплата в сутки(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+		
+		var ptc = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}[\s\S]*?(\d[\d.,]*)%/i, replaceTagsAndSpaces, parseBalance);
 		if(ptc){
-			result.storage_percent = ptc;
-			result.storage_percent_left = 100-ptc;
+			getParam(ptc, result, 'storage_percent');
+			getParam(100-ptc, result, 'storage_percent_left');
 		}
-		var storageTotal = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>)[^<]*Мб{1}([^<]*)Мб/i, null, parseBalance);
-		var storageUsed = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}([^<]*)Мб/i, null, parseBalance);
-		result.storage_total = storageTotal;
-		result.storage_used = storageUsed;
+		var storageTotal = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>)[^<]*Мб{1}([^<]*)Мб/i, replaceTagsAndSpaces, parseBalance);
+		var storageUsed = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}([^<]*)Мб/i, replaceTagsAndSpaces, parseBalance);
+		
+		getParam(storageTotal, result, 'storage_total');
+		getParam(storageUsed, result, 'storage_used');
+
 		if(storageTotal && storageUsed){
-			result.storage_left = storageTotal-storageUsed;
+			getParam(storageTotal-storageUsed, result, 'storage_left');
 		}
 	}
 	// Почтовый сервис
-	if(isAvailable('mail_used','mail_total','mail_left','mail_percent', 'mail_percent_left')){
-		html = AnyBalance.requestGet(baseurl + '?area=services_srv&srv=mail');
+	if(isAvailable(['mail_used','mail_total','mail_left','mail_percent', 'mail_percent_left'])){
+		html = AnyBalance.requestGet('https://cp.jino.ru/?area=services_srv&srv=mail');
 		
-		var ptc = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}[\s\S]*?(\d[\d.,]*)%/i, null, parseBalance);
+		var ptc = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}[\s\S]*?(\d[\d.,]*)%/i, replaceTagsAndSpaces, parseBalance);
 		if(ptc){
-			result.mail_percent = ptc;
-			result.mail_percent_left = 100-ptc;
+			getParam(ptc, result, 'mail_percent');
+			getParam(100-ptc, result, 'mail_percent_left');
 		}
-		var storageTotal = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>)[^<]*Мб{1}([^<]*)Мб/i, null, parseBalance);
-		var storageUsed = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}([^<]*)Мб/i, null, parseBalance);
-		result.mail_total = storageTotal;
-		result.mail_used = storageUsed;
-		if(storageTotal && storageUsed){
-			result.mail_left = storageTotal-storageUsed;
+		var mail_total = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>)[^<]*Мб{1}([^<]*)Мб/i, null, parseBalance);
+		var mail_used = getParam(html, null, null, /Использование услуги:(?:[\s\S]*?<p[^>]*>){1}([^<]*)Мб/i, null, parseBalance);
+		
+		getParam(mail_total, result, 'mail_total');
+		getParam(mail_used, result, 'mail_used');
+		
+		if(mail_total && mail_used){
+			getParam(mail_total-mail_used, result, 'mail_left');
 		}
 	}
 	
     AnyBalance.setResult(result);
 }
-// Парсит дату из такого вида в мс 27 июля 2013
-function parseDateMoment(str){
-	var found = /(\d{1,2})\s*([\s\S]*?)\s*(\d{1,4})/i.exec(str);
-	if(found)
-	{
-		var day = found[1];
-		var month = found[2];
-		var year = found[3];
-
-		if(month == 'января')
-			month = '01';
-		else if(month == 'февраля')
-			month = '02';
-		else if(month == 'марта')
-			month = '03';
-		else if(month == 'апреля')
-			month = '04';
-		else if(month == 'мая')
-			month = '05';
-		else if(month == 'июня')
-			month = '06';
-		else if(month == 'июля')
-			month = '07';
-		else if(month == 'августа')
-			month = '08';
-		else if(month == 'сентября')
-			month = '09';
-		else if(month == 'октября')
-			month = '10';
-		else if(month == 'ноября')
-			month = '11';
-		else if(month == 'декабря')
-			month = '12';
-
-		return getParam(day+'.'+month+'.'+ year, null, null, null, replaceTagsAndSpaces, parseDate);
-	}
-	else
-		AnyBalance.trace('Failed to parse date from ' + str);
-}
-

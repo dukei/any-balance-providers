4348abdec441a0895ac6883f8b37cb48c33ebd48
stark027 <stark027@gmail.com>
2013-11-07 07:32:17 +0000

    Курсы валют ЦБ - добавлен <api flags="no_browser"/>

--- ab-exchange-cbr/main.js
+++ ab-exchange-cbr/main.js
@@ -1,24 +1,25 @@
-function getRateText(result, info, namein, nameout, force){
-	var matches, regexp = new RegExp('"'+namein+'"[^"]*"([^"]*)"', 'i');
-	if(matches = info.match(regexp)){
-		if(force || AnyBalance.isAvailable(nameout))
-			result[nameout] = matches[1];
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+function getRateText(result, info, namein, nameout, force) {
+	var matches, regexp = new RegExp('"' + namein + '"[^"]*"([^"]*)"', 'i');
+	if (matches = info.match(regexp)) {
+		if (force || AnyBalance.isAvailable(nameout)) result[nameout] = matches[1];
 	}
 }
 
-function getRate(result, info, namein, nameout){
-	var matches, regexp = new RegExp('"'+namein+'"[^"]*"([^"]*)"', 'i');
-	if(matches = info.match(regexp)){
-		if(AnyBalance.isAvailable(nameout))
-			result[nameout] = parseFloat(matches[1].replace(',','.'));
+function getRate(result, info, namein, nameout) {
+	var matches, regexp = new RegExp('"' + namein + '"[^"]*"([^"]*)"', 'i');
+	if (matches = info.match(regexp)) {
+		if (AnyBalance.isAvailable(nameout)) result[nameout] = parseFloat(matches[1].replace(',', '.'));
 	}
 }
-	
-function main(){
+
+function main() {
 	AnyBalance.trace('Connecting to forex...');
-	
+
 	var info = AnyBalance.requestGet('http://www.forexpf.ru/_informer_/cbrf.php?id=012345678');
-	
 	var result = {success: true};
 
 	getRate(result, info, 'usrutm', 'USD');
@@ -33,6 +34,6 @@ function main(){
 
 	getRateText(result, info, 'pfdt2', 'date');
 	getRateText(result, info, 'pfdt2', '__tariff', true);
-	
+
 	AnyBalance.setResult(result);
 }

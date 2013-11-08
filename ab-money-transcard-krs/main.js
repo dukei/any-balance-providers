f40a6ca2a5716834a68fc26b1c7d201ce2555ffd
stark027 <stark027@gmail.com>
2013-11-08 11:35:31 +0000

    Транспортная карта, Красноярск: - Поддержка изменений на сайте

--- ab-money-transcard-krs/main.js
+++ ab-money-transcard-krs/main.js
@@ -1,27 +1,44 @@
-﻿function main(){
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
 	var prefs = AnyBalance.getPreferences();
-	var CardNumber = prefs.CardNumber;
-	var info = AnyBalance.requestGet('http://www.krasinform.ru/?id=tcard&card_num='+CardNumber);
+	var baseurl = 'http://www.krasinform.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
 
-	if(info.search('Неверный номер транспортной карты') != -1){
-		throw new AnyBalance.Error('Неверный номер транспортной карты');
-	};
+	checkEmpty(prefs.CardNumber, 'Введите номер карты!');
 
+	var html = AnyBalance.requestGet(baseurl + 'card/transport/', g_headers);
+
+	var token = getParam(html, null, null, /name="csrf_token"[^>]*value="([^"]*)/i, null, html_entity_decode);
+	if (!token)
+		throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+
+	html = AnyBalance.requestPost(baseurl + 'card/transport/', {
+		'csrf_token': token,
+		card_num: prefs.CardNumber,
+	}, addHeaders({Referer: baseurl + 'card/transport/'}));
+
+	if (!/<h2>\s*Информация по транспортной карте\s*<\/h2>/i.test(html)) {
+		var error = getParam(html, null, null, /"alert alert-error"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось получить информацию по карте № '+prefs.CardNumber);
+	}
 	var result = {success: true};
-	var regexp1 = new RegExp('Количество транспортных единиц: (\\d*\\,?\\d*)');
-	var regexp2 = new RegExp('Приемщик.*?<\\/td><td>([^<]*)<\\/td><td>.*?<\\/td><td>([^<]*)');
-        var matches1, matches2;
-
-	if(matches1 = info.match(regexp1)){
-		if(AnyBalance.isAvailable('KolvoEdinits'))
-			result.KolvoEdinits = parseFloat(matches1[1].replace(',','.'));
-		if(matches2 = info.match(regexp2)){
-			if(AnyBalance.isAvailable('LastDate'))
-				result.LastDate = matches2[1].substring(0,10);
-			if(AnyBalance.isAvailable('LastSum'))
-				result.LastSum = parseFloat(matches2[2]);
-		};
-	};
-
+	
+	getParam(html, result, 'KolvoEdinits', /Количество транспортных единиц:[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'LastDate', /<h3>\s*Платежи(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'LastSum', /<h3>\s*Платежи(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	
 	AnyBalance.setResult(result);
 }

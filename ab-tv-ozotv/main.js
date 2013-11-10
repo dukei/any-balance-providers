608781736864f804dad2b3d207f8a2698f496a67
ValeraVi <ValeraVi@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-10 20:43:07 +0000

    ozotv: Improved error handling

--- ab-tv-ozotv/main.js
+++ ab-tv-ozotv/main.js
@@ -19,61 +19,55 @@ function main() {
 	checkEmpty(prefs.login, 'Enter the login!');
 	checkEmpty(prefs.password, 'Enter the password!');
 
-    page='tools/ajax.php'
-    AnyBalance.trace('POST: ' + baseurl + page);
+	page='tools/ajax.php'
+	AnyBalance.trace('POST: ' + baseurl + page);
 	html = AnyBalance.requestPost(baseurl + page, {
-        fmt:'json',
-        action:'cmdLogin',
-        lng:'ru',
-        login:prefs.login,
-        pass:prefs.password,
-        lang:'ru',
-        referer:''
-    }, addHeaders({
-    	Referer: baseurl + '?part=login&lang=ru&wmode=transparent',
-    	Origin: 'http://my.ozo.tv',
-    	'X-Requested-With': 'XMLHttpRequest'
-    }));
-    AnyBalance.trace('POST: ' + baseurl + page + ' ... OK');
-    
-	if(/"message":\{"type":"error"/i.test(html)) {
-		var error = getParam(html, null, null, /"message":\{"type":"error","text":"([\s\S]*?)","container":"message"}/i, replaceTagsAndSpaces, html_entity_decode);
+		fmt:'json',
+		action:'cmdLogin',
+		lng:'ru',
+		login:prefs.login,
+		pass:prefs.password,
+		lang:'ru',
+		referer:''
+	}, addHeaders({
+		"Accept": "application/json, text/javascript, */*; q=0.01",
+		"Referer": baseurl + "?part=login&lang=ru&wmode=transparent",
+		"Origin": "http://my.ozo.tv",
+		"X-Requested-With": "XMLHttpRequest"
+	}));
+	AnyBalance.trace('POST: ' + baseurl + page + ' ... OK');
+	
+	var json = getJson(html);
+	if(json == null || json.redirect == null || json.redirect == ""	|| json.error == null || json.error != ""
+			|| (json.message != null && json.message.type == "error" )) {
+		if (json != null && json.message != null) {
+			error = json.message.text;
+		}
 		if(error)
-			throw new AnyBalance.Error(unescape(getJson('"'+error+'"')));
-		throw new AnyBalance.Error('Cant login. Web site was changed?');
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error("Can't login. Web site may changed or temporarily down.");
 	}
 
-    page='?part=subscriptions&lang=ru'
-    AnyBalance.trace('GET: ' + baseurl + page);	
+	page='?part=subscriptions&lang=ru'
+	AnyBalance.trace('GET: ' + baseurl + page);	
 	html = AnyBalance.requestGet(baseurl + page, g_headers);
-    AnyBalance.trace('GET: ' + baseurl + page + ' ... OK');	
+	AnyBalance.trace('GET: ' + baseurl + page + ' ... OK');	
 	
-    var result = {success: true};
-    subs = html.match(/<tr>\s*<td>1<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td><[\s\S]*?><[\s\S]*?>([\s\S]*?)<\/span><\/span><\/td>\s*<\/tr>/i);
+	var result = {success: true};
+	subs = html.match(/<tr>\s*<td>1<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td>([\s\S]*?)<\/td>\s*<td><[\s\S]*?><[\s\S]*?>([\s\S]*?)<\/span><\/span><\/td>\s*<\/tr>/i);
+	if (subs == null || subs.length < 8 ) {
+		AnyBalance.trace("Error: Can't parse: " + html);	
+		throw new AnyBalance.Error("Can't parse web site response. Web site may changed or temporarily down.");
+	}
 	getParam(subs[1], result, '__tariff', null, replaceTagsAndSpaces, html_entity_decode);
 	getParam(subs[2], result, 'duration', null, replaceTagsAndSpaces, html_entity_decode);
 	getParam(subs[3], result, 'cost', null, replaceTagsAndSpaces, parseBalance);
-	getParam(subs[4], result, 'created', null, replaceTagsAndSpaces, parseDateMy);
-	getParam(subs[5], result, 'activated', null, replaceTagsAndSpaces, parseDateMy);
+	getParam(subs[4], result, 'created', null, replaceTagsAndSpaces, parseDateISO);
+	//subs[5] can be "автоматически" if subscription is just purchased and not used at least once. So 'activated' will be unavailable.
+	getParam(subs[5], result, 'activated', null, replaceTagsAndSpaces, parseDateISO);
 	getParam(subs[6], result, 'balance', null, replaceTagsAndSpaces, parseBalance);
 	getParam(subs[7], result, 'status', null, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, ['currency','cost'], /STR_AMOUNT-23[\s\S]*?\(([\s\S]*?)\)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	if (result['activated']==null) {
-		//subs[5] can be "автоматически" if subscription is just purchased and not used at least once.
-		result['activated']=result['created'];
-	}
-    AnyBalance.setResult(result);
-}
-
-/** Получает дату из строки 2013-10-30 16:45:59 */
-function parseDateMy(str){
-    var matches = /(?:(\d+)[^\d])?(\d+)[^\d](\d{2,4})(?:[^\d](\d+):(\d+)(?::(\d+))?)?/.exec(str);
-    if(matches){
-          var year = +matches[1];
-          var date = new Date(year < 1000 ? 2000 + year : year, matches[2]-1, +(matches[3] || 1), matches[4] || 0, matches[5] || 0, matches[6] || 0);
-          var time = date.getTime();
-          AnyBalance.trace('Parsing date ' + date + ' from value: ' + str);
-          return time;
-    }
-    AnyBalance.trace('Failed to parse date from value: ' + str);
+	
+	AnyBalance.setResult(result);
 }

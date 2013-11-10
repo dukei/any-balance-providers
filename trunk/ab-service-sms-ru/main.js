4917f5b70c5dbc5c7a9f458fbc6ecce9800d6bc2
xandrosko@gmail.com <xandrosko@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-10 15:11:57 +0000

    Разобрался с svn, можно выкатывать обновления обоих провайдеров. В sms.ru всяческие причесывания

--- trunk/ab-service-sms-ru/main.js
+++ trunk/ab-service-sms-ru/main.js
@@ -8,50 +8,49 @@
 */
 
 function main(){
-		var prefs = AnyBalance.getPreferences();
+	var prefs = AnyBalance.getPreferences();
 
-		var baseurl = 'http://sms.ru/';
+	var baseurl = 'http://sms.ru/';
 
-		var strToken = AnyBalance.requestGet(baseurl + 'auth/get_token');
-		
-		var strBalance = AnyBalance.requestPost(baseurl + 'my/balance', {
-        		login:prefs.login,
-        		password:prefs.password,
-				token:strToken,
-    	});
-		
-		var status = strBalance.substr(0,3);
+	var strToken = AnyBalance.requestGet(baseurl + 'auth/get_token');
 
-		AnyBalance.trace(status);
-	
-		if( status > 100){
-				throw new AnyBalance.Error('Ошибка. Что-то пошло не так');
-    	}
+	var strBalance = AnyBalance.requestPost(baseurl + 'my/balance', {
+		login:prefs.login,
+		password:prefs.password,
+		token:strToken,
+	});
 
-		var strLimit = AnyBalance.requestPost(baseurl + 'my/limit', {
-        		login:prefs.login,
-        		password:prefs.password,
-				token:strToken,
-    	});
+	var status = strBalance.substr(0,3);
 
-		if( status > 100){
-				throw new AnyBalance.Error('Ошибка. Что-то пошло не так');
-    	}
-	
-		var result = {success: true};
+	if( status > 100){
+		throw new AnyBalance.Error('Ошибка. Что-то пошло не так');
+	}
 
-    	strBalance = strBalance.split("\n");
-        result['balance'] = strBalance[strBalance.length-1]
+	var strLimit = AnyBalance.requestPost(baseurl + 'my/limit', {
+		login:prefs.login,
+		password:prefs.password,
+		token:strToken,
+	});
 
-		var strOctatok = strLimit.split("\n");
-        result['octatok'] = strOctatok[strOctatok.length-2]-strOctatok[strOctatok.length-1]
+	var status = strLimit.substr(0,3);
 
-		var strSent = strLimit.split("\n");
-        result['sent'] = strLimit[strLimit.length-1]
+	if( status > 100){
+		throw new AnyBalance.Error('Ошибка. Что-то пошло не так');
+	}
 
-		strLimit = strLimit.split("\n");
-        result['limit'] = strLimit[strLimit.length-2]
+	var result = {success: true};
 
-		AnyBalance.setResult(result);
+	strBalance = strBalance.split("\n");
+	result['balance'] = strBalance[strBalance.length-1]
+
+	var strOctatok = strLimit.split("\n");
+	result['octatok'] = strOctatok[strOctatok.length-2]-strOctatok[strOctatok.length-1]
+
+	var strSent = strLimit.split("\n");
+	result['sent'] = strLimit[strLimit.length-1]
+
+	strLimit = strLimit.split("\n");
+	result['limit'] = strLimit[strLimit.length-2]
+
+	AnyBalance.setResult(result);
 }
-

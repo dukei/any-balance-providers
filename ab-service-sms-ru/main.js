d8a8948b2f490ab09b8f9de392e2755bdd9802b2
dmitry.kochin <dmitry.kochin@gmail.com>
2013-11-10 12:30:33 +0000

    SMS.ru (xandrosko)

---
+++ ab-service-sms-ru/main.js
@@ -0,0 +1,45 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получение информации о состоянии баланса и использовании дневного лимита SMS с сайта http://sms.ru
+
+Сайт оператора: http://sms.ru/
+Личный кабинет: https://sms.ru/
+*/
+
+function getValue(result, namein, nameout){
+		var matches, regexp = new RegExp('(?=(\n.*){1}$)', 'i');
+		if(matches = namein.match(regexp)){
+				if(AnyBalance.isAvailable(nameout))
+						result[nameout] = parseFloat(matches[1].replace(',','.'));
+		}
+}
+
+function main(){
+		var prefs = AnyBalance.getPreferences();
+
+		var baseurl = 'http://sms.ru/';
+
+		var strToken = AnyBalance.requestGet(baseurl + 'auth/get_token');
+		
+		var strBalance = AnyBalance.requestPost(baseurl + 'my/balance', {
+        		login:prefs.login,
+        		password:prefs.password,
+				token:strToken,
+    	});
+
+		var strLimit = AnyBalance.requestPost(baseurl + 'my/limit', {
+        		login:prefs.login,
+        		password:prefs.password,
+				token:strToken,
+    	});
+	
+		var result = {success: true};
+
+		getValue(result, strBalance, 'balance');
+		
+		getValue(result, strLimit, 'sent');
+    
+		AnyBalance.setResult(result);
+}
+

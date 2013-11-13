2693bbd31fafa884d7fa820a60e6aad37b89b989
stark027 <stark027@gmail.com>
2013-11-13 13:01:09 +0000

    Новый провайдер: Газпромнефть Бонус (Корпоративный)

---
+++ ab-bonus-gpncard/main.js
@@ -0,0 +1,45 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://www.gpncard.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestGet(baseurl + 'auth/', g_headers);
+
+	html = AnyBalance.requestPost(baseurl + 'auth/?login=yes', {
+		'backurl':'/auth/',
+		'AUTH_FORM':'Y',
+		'TYPE':'AUTH',
+		USER_LOGIN:prefs.login,
+		USER_PASSWORD:prefs.password,
+		'Login':'Y'
+	}, addHeaders({Referer: baseurl + 'auth'}));
+
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<h4[^>]*color:red[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error && /Неверный логин или пароль/i.test(error))
+			throw new AnyBalance.Error(error, null, true);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	var result = {success: true};
+
+	getParam(html, result, 'balance', /Рубли[^>]*>[^>]*>[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+
+	AnyBalance.setResult(result);
+}

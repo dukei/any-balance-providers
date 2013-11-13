064831376ad8eb95342a9ab9fc177a7b67633efa
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-13 11:52:59 +0000

    Квартплата 24 v2

--- trunk/ab-municipal-kvartplata24/main.js
+++ trunk/ab-municipal-kvartplata24/main.js
@@ -10,6 +10,8 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
+var monthsArray = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'http://lk.kvp24.ru/';
@@ -31,25 +33,56 @@ function main() {
 		if (error) throw new AnyBalance.Error(error);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
-	
-	var year = new Date().getFullYear();
-	var p = {
-		year: "2013"
-	};
-	
-	html = AnyBalance.requestPost(baseurl + 'lk/payment/getDataByYear', JSON.stringify(p), addHeaders( {
-		Referer: baseurl + 'lk/payment',
-		Accept:'*/*',
-		'X-Requested-With':'XMLHttpRequest',		
-	}));
-	
-	var paymentsJson = getJson(html);
-	var lastPayment = paymentsJson[paymentsJson.length-1];
-	
+	var dt = new Date();
+	var year = dt.getFullYear();
+	var p = {'year': year+''};
 	var result = {success: true};
 	
-	getParam(lastPayment.totalAmount+'', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
-	getParam(lastPayment.date+'', result, 'date', null, replaceTagsAndSpaces, parseBalance);
+	if(isAvailable(['balance', 'date'])) {
+		html = AnyBalance.requestPost(baseurl + 'lk/payment/getDataByYear', JSON.stringify(p), addHeaders( {
+			Referer: baseurl + 'lk/payment',
+			Accept:'*/*',
+			'X-Requested-With':'XMLHttpRequest',		
+		}));
+		
+		var paymentsJson = getJson(html);
+		var lastPayment = paymentsJson[paymentsJson.length-1];
+		
+		getParam(lastPayment.totalAmount+'', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+		getParam(lastPayment.date+'', result, 'date', null, replaceTagsAndSpaces, parseBalance);
+	}
+
+	if(isAvailable(['balance', 'date'])) {
+		html = AnyBalance.requestPost(baseurl + 'lk/history/getDataByYear', JSON.stringify(p), addHeaders( {
+			Referer: baseurl + 'lk/payment',
+			Accept:'*/*',
+			'X-Requested-With':'XMLHttpRequest',		
+		}));		
+		
+		var historyJson = getJson(html);
+		
+		var m = dt.getMonth();
+		
+		for(i = 0; i < historyJson.length; i++) {
+			var curr = historyJson[i];
+			
+			if(prefs.usluga) {
+				if(prefs.usluga == curr.facilityGroup) {
+					parseElectricity(curr, result, m);
+					break;
+				}
+			} else {
+				parseElectricity(curr, result, m);
+				break;
+			}
+		}
+	}	
 	
 	AnyBalance.setResult(result);
+}
+
+function parseElectricity(curr, result, m) {
+	getParam(curr.facilityGroup+'', result, 'facilityGroup', null, replaceTagsAndSpaces);
+	getParam(curr.serialNumber+'', result, 'serialNumber', null, replaceTagsAndSpaces);
+	getParam(curr[monthsArray[m-1]+'Readings']+'', result, 'usluga_balance', null, replaceTagsAndSpaces, parseBalance);
 }

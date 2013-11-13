ba4cd7793051162e6cddd0d9f4899b834c03f1ad
stark027 <stark027@gmail.com>
2013-11-13 12:46:04 +0000

    Новый провайдер: SMS-7

---
+++ ab-service-sms7/main.js
@@ -0,0 +1,44 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://my.sms-7.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestPost(baseurl + 'login', {
+		login_data: prefs.login,
+		password: prefs.password,
+		login_btn: 'Войти'
+	}, addHeaders({Referer: baseurl + 'login'}));
+
+	if (!/login\?quit/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Баланс[^>]*>([^<]*)/, replaceTagsAndSpaces, parseBalance);
+	
+	if(isAvailable(['sms'])) {
+		html = AnyBalance.requestGet(baseurl + 'check_balance?rnd=' + Math.random());
+		
+		getParam(html, result, 'sms', /"sms":(\d+)/i, replaceTagsAndSpaces, parseBalance);
+	}
+	
+	AnyBalance.setResult(result);
+}

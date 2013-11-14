6c6f4e9ae2e5a33d89d285c1beab0650461f8749
stark027 <stark027@gmail.com>
2013-11-14 13:24:26 +0000

    Новый провайдер: Мобиллак (Украина)

---
+++ ab-bonus-mobilluck/main.js
@@ -0,0 +1,45 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.mobilluck.com.ua/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestGet(baseurl + 'input.php', g_headers);
+	
+	html = AnyBalance.requestPost('http://www.mobilluck.com.ua/cart.php', {
+		mode:'postuser',
+		action:'orderlogin',
+		clientlog: prefs.login,
+		clientpass: prefs.password,
+		loginbut: 'войти'
+	}, addHeaders({Referer: baseurl + 'input.php'}));
+
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /class="error"(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	var result = {success: true};
+	
+	getParam(html, result, '__tariff', /Дисконтная карта:(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'card_num', /Дисконтная карта:(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Бонусные баллы:(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'money', /дают право на скидку([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

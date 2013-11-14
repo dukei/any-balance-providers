e2b6eb3172e291c0e5f38b8f09d8c773eaa25c05
stark027 <stark027@gmail.com>
2013-11-14 09:25:56 +0000

    Userator v2

--- ab-seo-userator/main.js
+++ ab-seo-userator/main.js
@@ -7,7 +7,6 @@ var g_headers = {
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
 };
 
@@ -16,12 +15,12 @@ function main(){
     var baseurl = 'http://userator.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
 
-    var html = AnyBalance.requestGet(baseurl + '/cgi-bin/login.cgi', g_headers);
+    var html = AnyBalance.requestGet(baseurl + 'cgi-bin/plogin.cgi', g_headers);
 
-	html = AnyBalance.requestPost(baseurl + '/cgi-bin/login.cgi', {
+	html = AnyBalance.requestPost(baseurl + 'cgi-bin/plogin.cgi', {
         Login:prefs.login,
         Password:prefs.password,
-    }, addHeaders({Referer: baseurl + '/cgi-bin/login.cgi'})); 
+    }, addHeaders({Referer: baseurl + 'cgi-bin/plogin.cgi'})); 
 
     if(!/\/Logout/i.test(html)){
         var error = getParam(html, null, null, /<p\s*style="color:red;">([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -31,7 +30,13 @@ function main(){
     }
 
     var result = {success: true};
-    getParam(html, result, 'balance', /Баланс:[^>]*>[^>]*>[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	if(isAvailable('balance')) {
+		getParam(html, result, 'balance', /Баланс:(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	}
+	if(isAvailable('rating')) {
+		html = AnyBalance.requestGet(baseurl + 'cgi-bin/moder_view.cgi', g_headers);
+		getParam(html, result, 'rating', /<h1>Рейтинг пользователя\s*:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	}
 
     AnyBalance.setResult(result);
 }

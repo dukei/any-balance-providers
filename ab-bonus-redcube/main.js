e7f1a3ba7da6952925f59bc3200e99a716256e06
stark027 <stark027@gmail.com>
2013-11-15 08:47:17 +0000

    Красный куб v3.0.5

--- ab-bonus-redcube/main.js
+++ ab-bonus-redcube/main.js
@@ -7,21 +7,15 @@
 Личный кабинет: http://www.redcube.ru/clients/club/cabinet/
 */
 
-function checkEmpty (param, error) {
-    if (!param || param == '')
-        throw new AnyBalance.Error (error);
+function enter(url, data) {
+	var html = AnyBalance.requestPost(url, data);
+
+	var error = getParam(html, null, null, /<div[^>]+id="dangerMessage"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	if (error)
+		throw new AnyBalance.Error(error);
+
+	return html;
 }
-
-function enter (url, data) {
-    var html = AnyBalance.requestPost (url, data);
-
-    var error=getParam(html, null, null, /<div[^>]+id="dangerMessage"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    if (error)
-        throw new AnyBalance.Error (error);
-
-    return html;
-}
-
 
 function main () {
     AnyBalance.setDefaultCharset ('utf-8');
@@ -29,60 +23,48 @@ function main () {
     var prefs = AnyBalance.getPreferences ();
     var baseurl = 'http://club.redcube.ru/';
 
-    checkEmpty (prefs.number, 'Введите номер карты');
-    checkEmpty (prefs.date, 'Введите дату рождения');
+    checkEmpty (prefs.number, 'Введите номер карты!');
+    var res = /(\d{2})\.(\d{2})\.(\d{4})/.exec(prefs.db_date);
+	if (!res)
+        throw new AnyBalance.Error ('Введите дату рождения в формате ДД.ММ.ГГГГ');
 
-    var regexp=/(\d{2})\.(\d{2})\.(\d{2})/;
-    var res = regexp.exec (prefs.date);
-    var date = Date.parse (res[2] + '-' + res[1] + '-' + res[3]);
-    if (!res || !date)
-        throw new AnyBalance.Error ('Дата рождения введена неправильно');
+    AnyBalance.trace('Trying to enter selfcare at address: ' + baseurl);
 
-    date = new Date(date);
-
-    AnyBalance.trace ('Trying to enter selfcare at address: ' + baseurl);
-
-    enter (baseurl, {
-	_method:'POST',
-	'data[Auth][card]':prefs.number,
-	serverurl:'http://www.redcube.ru',
-	text:'Найти'
+    enter(baseurl, {
+    	_method: 'POST',
+    	'data[Auth][card]': prefs.number,
+    	serverurl: 'http://www.redcube.ru',
+    	text: 'Найти'
     });
 
-    var html = enter (baseurl, {
-	_method:'POST',
-	'data[Auth][date][day]':date.getDate() < 10 ? '0' + date.getDate() : date.getDate(),
-	'data[Auth][date][month]':(date.getMonth() < 9 ? '0' : '') + (date.getMonth()+1),
-	'data[Auth][date][year]':date.getFullYear(),
-	serverurl:'http://www.redcube.ru',
-	text:'Найти'
+    var html = enter(baseurl, {
+    	_method: 'POST',
+    	'data[Auth][date][day]': res[1],
+    	'data[Auth][date][month]': res[2],
+    	'data[Auth][date][year]': res[3],
+    	serverurl: 'http://www.redcube.ru',
+    	text: 'Найти'
     });
 
     // Проверка на корректный вход
-    regexp = /">Выйти</;
-    if (regexp.exec(html))
-    	AnyBalance.trace ('It looks like we are in selfcare...');
-    else {
-        AnyBalance.trace ('Have not found logOff... Unknown error. Please contact author.');
-        throw new AnyBalance.Error ('Неизвестная ошибка. Пожалуйста, свяжитесь с автором провайдера.');
+    if (/">Выйти</.exec(html)) {
+		AnyBalance.trace('It looks like we are in selfcare...');
+    } else {
+    	//AnyBalance.trace('Have not found logOff... Unknown error. Please contact author.');
+    	throw new AnyBalance.Error('Неизвестная ошибка. Пожалуйста, свяжитесь с автором провайдера.');
     }
-
+	
     var result = {success: true};
-
     // ФИО
-    getParam (html, result, 'customer', /Уважаем(?:ый|ая)\s*([^!]*)!/i, replaceTagsAndSpaces, html_entity_decode);
-
+    getParam(html, result, 'customer', /Уважаем(?:ый|ая)\s*([^!]*)!/i, replaceTagsAndSpaces, html_entity_decode);
     // Номер карты
-    getParam (html, result, 'cardNumber', /Номер\s*Вашей\s*карты[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-
+    getParam(html, result, 'cardNumber', /Номер\s*Вашей\s*карты[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
     // Остаток бонусов
-    getParam (html, result, 'bonus', /Остаток\s*бонусов[^\d]*(\d+[.,]?\d*)/i, replaceTagsAndSpaces, parseBalance);
-
+    getParam(html, result, 'bonus', /Остаток\s*бонусов[^\d]*(\d+[.,]?\d*)/i, replaceTagsAndSpaces, parseBalance);
     // Сумма всех покупок
-    getParam (html, result, 'costPurchase', /Сумма\s*всех\s*покупок[^\d]*(\d+[.,]?\d*)/i, replaceTagsAndSpaces, parseBalance);
-
+    getParam(html, result, 'costPurchase', /Сумма\s*всех\s*покупок[^\d]*(\d+[.,]?\d*)/i, replaceTagsAndSpaces, parseBalance);
     // Дата последней операции
-    getParam (html, result, 'dateLastOperation', /Дата\s*последней\s*операции[^\d]*(\d{2}.\d{2}.\d{4})/i);
+    getParam(html, result, 'dateLastOperation', /Дата\s*последней\s*операции[^\d]*(\d{2}.\d{2}.\d{4})/i);
 
-    AnyBalance.setResult (result);
+    AnyBalance.setResult(result);
 }

e48502c81181cf1469e694614f4dd2f9cd73d61f
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-19 15:14:31 +0000

    Новый провайдер: TELECOMTECHNOLOGY LIMITED

---
+++ trunk/ab-telephony-t-pbx/main.js
@@ -0,0 +1,29 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://balance.t-pbx.net/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestGet(baseurl + 'balance.php?login='+encodeURIComponent(prefs.login)+'&password='+encodeURIComponent(prefs.password), g_headers);
+
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /<body>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['currency', 'balance'], /<body>([^<]*)/i, replaceTagsAndSpaces, parseCurrency);
+
+	AnyBalance.setResult(result);
+}

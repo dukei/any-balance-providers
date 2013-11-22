83f865480a7157fcd8b2b37103fa85278cb5ac8d
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-22 13:26:00 +0000

    Новый провайдер: НПФ "РГС"

---
+++ trunk/ab-money-npfrgs/main.js
@@ -0,0 +1,47 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://mynpf.rgs.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	var loginRegexp = /(\d{3})(\d{3})(\d{3})(\d{2})/i.exec(prefs.login);
+	if(!loginRegexp) {
+		throw new AnyBalance.Error('Введите логин (СНИЛС) без разделителей, 11 цифр подряд.', null, true);
+	}
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+
+	html = AnyBalance.requestPost(baseurl + 'index.wbp', {
+		Login: prefs.login,
+		Password: prefs.password,
+	}, addHeaders({Referer: baseurl}));
+
+	if (!/Вы успешно вошли в систему/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error && /Неверный логин или пароль/i.test(error))
+			throw new AnyBalance.Error(error, null, true);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	html = AnyBalance.requestGet(baseurl+'private/ops.wbp', g_headers);
+	
+	var result = {success: true};
+	getParam(html, result, 'balance', /Общая сумма средств пенсионных накоплений(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'outpay', /Сумма выплат накопительной части трудовой пенсии(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /СНИЛС:(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

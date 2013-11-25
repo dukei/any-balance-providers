ce4d6dd5b9e07024461f35c9519bb7a488ed966e
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-25 07:59:40 +0000

    Новый провайдер: Инсис Екатеринбург

---
+++ trunk/ab-internet-profintel/main.js
@@ -0,0 +1,45 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://stat.profintel.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+
+	html = AnyBalance.requestPost(baseurl + 'login', {
+		login: prefs.login,
+		password: prefs.password,
+		submit: 'submit'
+	}, addHeaders({Referer: baseurl + 'login'}));
+	
+	// Сайт не дает информации об ошибке
+	if (!/logout/i.test(html)) {
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	// Иногда нам навязывают какие-то услуги по подписке, пропускаем их
+	if(/У вас не активирован сервис(?:[^>]*>){1}([^<]*)SMS-рассылки/i.test(html))
+		html = AnyBalance.requestGet(baseurl + 'welcome/index/1/', g_headers);
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Текущий баланс(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance_expense', /Расход за текущий день(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'id_pay', /ID для оплаты через банкоматы(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'fio', /ФИО(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

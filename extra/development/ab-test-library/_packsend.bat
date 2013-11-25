043de14112950c0eb5994987064e273eeff0e6b7
stark027 <stark027@gmail.com>
2013-11-25 08:50:33 +0000

    Обновлена библиотека и тест под нее

---
+++ extra/development/ab-test-library/_packsend.bat
@@ -0,0 +1,4 @@
+del provider.zip
+adb.exe -e shell md /sdcard/AnyBalance
+"C:\Program Files\7-zip\7z.exe" a provider.zip anybalance-manifest.xml icon.png main.js preferences.xml history.xml library.js
+adb.exe -e push provider.zip /sdcard/AnyBalance/provider.zip

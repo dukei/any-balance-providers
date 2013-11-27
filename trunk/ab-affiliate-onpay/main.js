a8f7267061d91354cf4f026a0b7e91a41fa96964
bazhanov.artem@gmail.com <bazhanov.artem@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-27 11:45:25 +0000

    Изменения в кабинете

--- trunk/ab-affiliate-onpay/main.js
+++ trunk/ab-affiliate-onpay/main.js
@@ -31,11 +31,15 @@ function main() {
 	if(html.indexOf('<a href="/logout" class="brand">')==-1) throw new AnyBalance.Error('Ошибка авторизации. Проверьте логин и пароль');
 	
 	html=AnyBalance.requestGet('https://secure.onpay.ru/accounts');	
-	r=new RegExp("<tr id='row-[\\s\\S]+?<td align='right' id='delayed.+?'>([0-9.]+)</td>\\s+<td align='right' id='balance.+?'>([0-9.]+)</td>\\s+<td>([A-Z]{3})</td>[\\s\\S]+?</tr>",'g');
+	r=new RegExp("<tr id='row-[\\s\\S]+?<td align='right' id='delayed.+?'>([0-9.]+)</td>\\s+<td align='right' id='balance.+?'>([0-9.]+) / [0-9.]+</td>\\s+<td>([A-Z]{3})</td>[\\s\\S]+?</tr>",'g');
+	var found=false;
 	while((matches=r.exec(html))!=null) {
+		if(!found) found=true;
 		if(AnyBalance.isAvailable(matches[3]+'_delayed')) result[matches[3]+'_delayed']=matches[1];
 		if(AnyBalance.isAvailable(matches[3]+'_balance')) result[matches[3]+'_balance']=matches[2];
 	}
+	
+	if(!found) throw new AnyBalance.Error('Ошибка разбора данных');
 
     AnyBalance.setResult(result);
 }

4a06ac670491323c3c95ec993c6ee5351b7bd06b
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-11-27 16:13:59 +0000

    TD-net (Комсомольск-на-Амуре) v2

--- trunk/ab-internet-tdnet/main.js
+++ trunk/ab-internet-tdnet/main.js
@@ -14,22 +14,22 @@ function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'http://report.td-net.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
-
+	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
-
+	
 	var html = AnyBalance.requestGet(baseurl, g_headers);
-
+	
 	var action = getParam(html, null, null, /name=["']LoginForm["'][^>]*action=["']([^"']*)/i, null, html_entity_decode);
 	if (!action)
 		throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
-
+	
 	html = AnyBalance.requestPost(baseurl + action, {
 		CardNum: prefs.login,
 		Pwd: prefs.password,
 		login_submit: 'OK'
 	}, addHeaders({Referer: baseurl}));
-
+	
 	if (!/>Другой\s*логин</i.test(html)) {
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
@@ -37,7 +37,18 @@ function main() {
 	
 	getParam(html, result, 'fio', /Клиент:(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'balance', /Баланс:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'dogovor1', /Договор:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-
+	
+	var href = getParam(html, null, null, /<a[^>]*href="\/([^"]*)[^>]*>Балансовые счета/i);
+	if(!href)
+		throw new AnyBalance.Error('Не удалось найти ссылку на счета, сайт изменен?');
+	
+	html = AnyBalance.requestGet(baseurl + href, g_headers);
+	
+	getParam(html, result, 'dogovor1', /<H1>Балансовые счета<\/H1>(?:[\s\S]*?<td[^>]*>)([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance1', /<H1>Балансовые счета<\/H1>(?:[\s\S]*?<td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+	getParam(html, result, 'dogovor2', /<H1>Балансовые счета<\/H1>(?:[\s\S]*?<td[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance2', /<H1>Балансовые счета<\/H1>(?:[\s\S]*?<td[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	
 	AnyBalance.setResult(result);
 }

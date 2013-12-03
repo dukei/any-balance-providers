5725dadce70f972d6e5fea354c3f068ad7fb402d
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-12-03 15:38:56 +0000

    Turkcell v8

--- trunk/ab-cell-turkcell/main.js
+++ trunk/ab-cell-turkcell/main.js
@@ -54,6 +54,17 @@ function main() {
 	getParam(html, result, 'fio', /"media__title text-satura"(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'phone', /"media__number"(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	
+	// Для постоплаты, дата платежа и сумма счета
+	if(isAvailable(['fatura', 'fatura_date'])) {
+		html = AnyBalance.requestGet(baseurl + 'hesabim/faturalar/fatura-ozet', addHeaders({Referer: baseurl + 'giris'}));
+		
+		getParam(html, result, 'fatura', /Toplam Tutar[^>]*>\s*Son ödeme tarihi(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+		if(isset(result.fatura))
+			result.balance = undefined;
+		
+		getParam(html, result, 'deadline', /Son ödeme tarihi([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	}
+	
 	// Пакеты
 	if(isAvailable(['sms_days', 'sms_left', 'sms_total', 'sms_used', 
 		'minutes_local_days', 'minutes_local_left', 'minutes_local_total', 'minutes_local_used', 
@@ -90,108 +101,6 @@ function main() {
 			if(isset(result.sms_left) && isset(result.sms_used))
 				getParam(result.sms_left+result.sms_used, result, 'sms_total');
 		}		
-	}
-	
-	
-	// Предоплата
-	if(true) {
-		
-	// Постоплата
-	} else {
-		if(isAvailable(['balance', 'fio', 'phone', 'deadline'])) {
-			html = AnyBalance.requestGet('http://m.turkcell.com.tr/faturam.aspx', addHeaders({'X-Requested-With':'XMLHttpRequest', Referer: 'http://m.turkcell.com.tr/InvoiceLogin.aspx?ReturnUrl=/faturam.aspx'}));
-			
-			getParam(html, result, 'balance', /Güncel Fatura Tutarınız(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-			
-			getParam(html, result, 'fio', /"clsCustName"(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-			getParam(html, result, 'phone', /"clsCustTel"(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-			getParam(html, result, 'deadline', /Son Ödeme Tarihi(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseDate);
-		}	
-		// Пакеты минут
-		if(isAvailable(['sms_days', 'sms_left', 'sms_total', 'sms_used', 'minutes_local_days', 'minutes_local_left', 'minutes_local_total', 'minutes_local_used', 'minutes_days', 'minutes_left', 'minutes_total', 'minutes_used', 'data_days', 'data_left', 'data_total', 'data_used'])) {
-			html = requestBonuses();
-
-			var json = getJson(html);
-			// Не бросаем исключение! Может есть только баланс?
-			if(json && json.success == 'true') {
-				// Остаток СМС
-				var sms = json.sms;
-				if(sms) {
-					//AnyBalance.trace('Found sms ' + sms);
-					// Это смс кому угодно, могут быть еще и внутри сети
-					var total = sumParam(sms, null, null, /class="info"[^>]*>\s*\d+\s*\/\s(\d*)\s*ADET(?:[^>]*>){4}SMSHeryone/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-					var used = sumParam(sms, null, null, /class="info"[^>]*>\s*(\d+)\s*\/\s\d*\s*ADET(?:[^>]*>){4}SMSHeryone/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-					sumParam(sms, result, 'sms_days', /Kalan Gün[^\d]*(\d+)/ig, replaceTagsAndSpaces, parseBalance, aggregate_min);
-					
-					if(total >= 0 && used >= 0) {
-						getParam(total-used, result, 'sms_left');
-						getParam(total, result, 'sms_total');
-						getParam(used, result, 'sms_used');
-					} else {
-						AnyBalance.trace('Can`t parse sms, contact the developer, please!');
-					}
-				}
-				// Остаток минут
-				var minutes = json.voice;
-				if(minutes) {
-					//AnyBalance.trace('Found minutes ' + minutes);
-					// Это минуты внутри сети
-					if(isAvailable(['minutes_local_days', 'minutes_local_left', 'minutes_local_total', 'minutes_local_used'])) {
-						var total = sumParam(minutes, null, null, /class="info"[^>]*>\s*\d+\s*\/\s([\d\.]*)\s*DK(?:[^>]*>){4}Turkcelllilerle/ig, [replaceTagsAndSpaces, /\./, ''], parseBalance, aggregate_sum);
-						var used = sumParam(minutes, null, null, /class="info"[^>]*>\s*([\d\.]*)\s*\/\s[\d\.]*\s*DK(?:[^>]*>){4}Turkcelllilerle/ig, [replaceTagsAndSpaces, /\./, ''], parseBalance, aggregate_sum);
-						var mins = sumParam(minutes, null, null, /Kalan Gün[^\d]*(\d+)(?:[^>]*>){6,8}Turkcelllilerle/ig, replaceTagsAndSpaces, parseBalance, aggregate_min);
-						result.minutes_local_days = mins;
-						// Нет даты у этого пакета, это совместный пакет
-						if(!result.minutes_local_days) {
-							getParam(result.minutes_days, result, 'minutes_local_days');
-						}					
-						if(total >= 0 && used >= 0) {
-							getParam(total-used, result, 'minutes_local_left');
-							getParam(total, result, 'minutes_local_total');
-							getParam(used, result, 'minutes_local_used');
-						} else {
-							AnyBalance.trace('Can`t parse Turkcelllilerle minutes, contact the developer, please!');
-						}				
-					}
-					// Это минуты кому угодно
-					if(isAvailable(['minutes_days', 'minutes_left', 'minutes_total', 'minutes_used'])) {
-						var total = sumParam(minutes, null, null, /class="info"[^>]*>\s*\d+\s*\/\s([\d\.]*)\s*DK(?:[^>]*>){4}Heryone/ig, [replaceTagsAndSpaces, /\./, ''], parseBalance, aggregate_sum);
-						var used = sumParam(minutes, null, null, /class="info"[^>]*>\s*([\d\.]*)\s*\/\s[\d\.]*\s*DK(?:[^>]*>){4}Heryone/ig, [replaceTagsAndSpaces, /\./, ''], parseBalance, aggregate_sum);
-						var mins = sumParam(minutes, null, null, /Kalan Gün[^\d]*(\d+)(?:[^>]*>){6,8}Turkcelllilerle/ig, replaceTagsAndSpaces, parseBalance, aggregate_min);
-						result.minutes_days = mins;					
-						// Нет даты у этого пакета, это совместный пакет
-						if(!result.minutes_days) {
-							getParam(result.minutes_local_days, result, 'minutes_days');
-						}
-						if(total >= 0 && used >= 0) {
-							getParam(total-used, result, 'minutes_left');
-							getParam(total, result, 'minutes_total');
-							getParam(used, result, 'minutes_used');
-						} else {
-							AnyBalance.trace('Can`t parse Heryone minutes, contact the developer, please!');
-						}				
-					}
-				}
-				// Остаток интернета
-				var data = json.data;
-				if(data) {
-					//AnyBalance.trace('Found data ' + data);
-					var total = sumParam(data, null, null, /class="info"[^>]*>\s*\d+\s*\/\s(\d*)\s*MB/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-					var used = sumParam(data, null, null, /class="info"[^>]*>\s*(\d+)\s*\/\s\d*\s*MB/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-					sumParam(data, result, 'data_days', /Kalan Gün[^\d]*(\d+)/ig, replaceTagsAndSpaces, parseBalance, aggregate_min);
-					
-					if(total >= 0 && used >= 0) {
-						getParam(total-used, result, 'data_left');
-						getParam(total, result, 'data_total');
-						getParam(used, result, 'data_used');
-					} else {
-						AnyBalance.trace('Can`t parse data, contact the developer, please!');
-					}
-				}
-			} else {
-				AnyBalance.trace('Can`t find bonuses!');
-			}
-		}
 	}
 	
     AnyBalance.setResult(result);

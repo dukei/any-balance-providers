bfdad2a3a56871b4bb49f0e20773539469577641
stark027 <stark027@gmail.com>
2013-12-05 11:56:50 +0000

    NetByNet v13

--- ab-internet-netbynet/main.js
+++ ab-internet-netbynet/main.js
@@ -1,10 +1,5 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс, остаток дней и статус у интернет-провайдера NetByNet.
-
-Сайт провайдера: http://www.netbynet.ru/
-Личный кабинет: http://stat.netbynet.ru/
 */
 
 var g_regions = {
@@ -45,55 +40,41 @@ function mainCenter(){
         }
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
-
-
+	
     AnyBalance.trace ("It looks like we are in selfcare...");
-
+	
     var result = {success: true};
-
+	
     AnyBalance.trace("Parsing data...");
-
-    // Тарифный план
     // Тариф выцепить сложно, пришлось ориентироваться на запись после остатка дней
     value = html.match (/Осталось[\s\S]*?<td>(.*?)<\/td>/i);
     if (value && value[1].indexOf ('нет') == -1)
-      result.__tariff = value[1];
+		result.__tariff = value[1];
 
-    // Баланс
     getParam (html, result, 'balance', /<span[^>]+class="balance"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-
-    // Абонент
     getParam (html, result, 'subscriber', /Абонент[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Номер договора
     getParam (html, result, 'contract', /Договор([^<(]*)/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Расчетный период - остаток
     getParam (html, result, 'day_left', /Осталось[^\d]*([\d]+)/i, replaceTagsAndSpaces, parseBalance);
 
-    //Таблица с тарифными планами
     var table = getParam(html, null, null, /<table[^>]*>(?:[\s\S](?!<\/table>))*?<th[^>]*>Договор(?:[\s\S](?!<\/table>))*?<th[^>]*>Текущий тариф[\s\S]*?<\/table>/i, [/<!--[\S\s]*?-->/g, '']);
     
-    if(table){
-        if(/<th[^>]*>\s*Остаток трафика/i.test(table)){
+    if(table) {
+        if(/<th[^>]*>\s*Остаток трафика/i.test(table)) {
             AnyBalance.trace('Найден остаток трафика');
             getParam(table, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
             getParam(table, result, 'status', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
             getParam(table, result, 'trafficLeft', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
-        }else{
+        } else {
             getParam(table, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
             getParam(table, result, 'status', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
         }
-    }else{
-        AnyBalance.trace('Не удалось найти таблицу с тарифным планом. Сайт изменен?');
-    }
-
-    if (AnyBalance.isAvailable ('promised_payment')) {
-
+    } else {
+		AnyBalance.trace('Не удалось найти таблицу с тарифным планом. Сайт изменен?');
+	}
+	
+	if (AnyBalance.isAvailable ('promised_payment')) {
         AnyBalance.trace ("Fetching stats...");
-
         html = AnyBalance.requestGet(baseurl + "stats");
-
         AnyBalance.trace("Parsing stats...");
 
         // Обещанные платежи
@@ -161,8 +142,8 @@ function mainVoronezh(){
 function mainBelgorod(region){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
-    var baseurl = 'https://selfcare.puzzle.su/'+region+'/';
-
+    var baseurl = 'https://selfcare.netbynet.ru/'+region+'/';
+	
     AnyBalance.trace ("Trying to enter selfcare at address: " + baseurl);
     var html = requestPostMultipart (baseurl + "?", {
     	'LOGIN': prefs.login,
@@ -171,7 +152,7 @@ function mainBelgorod(region){
     	'subm.x': 31,
     	'subm.y': 4
     });
-
+	
     if(!/\?exit=1/i.test(html)){
         var error = getParam (html, null, null, /<font[^>]+color=['"]red['"][^>]*>([\s\S]*?)<\/font>/i, replaceTagsAndSpaces);
         if (error){
@@ -179,34 +160,23 @@ function mainBelgorod(region){
         }
         throw new AnyBalance.Error ("Не удаётся войти в личный кабинет. Сайт изменен?");
     }
-
+	
     AnyBalance.trace ("It looks like we are in selfcare...");
-
+	
     var result = {success: true};
-
+	
     html = AnyBalance.requestGet(baseurl + '?pr%5Bcontrol%5D%5Bkernel%5D%5Brecord%5D=23&pr%5Bcontrol%5D%5Bkernel%5D%5Bparent%5D=19&menu=19');
-
+	
     AnyBalance.trace("Parsing data...");
-
-    // Баланс
+	
     getParam (html, result, 'balance', /<b[^>]*>Лицевой счет:[\s\S]*?баланс:([\s\S]*?)<br/i, replaceTagsAndSpaces, parseBalance);
-
-    // Абонент
     getParam (html, result, 'subscriber', /Приветствуем Вас,([^<]*)/i, replaceTagsAndSpaces);
-
-    // Номер договора
     getParam (html, result, 'contract', /<b[^>]*>Лицевой счет:([\s\S]*?),/i, replaceTagsAndSpaces);
-
-    // Расчетный период - остаток
     getParam (html, result, 'day_left', /(?:Количество дней до ухода в финансовую блокировку|До списания абонентской платы осталось):([\s\S]*?)<br/i, replaceTagsAndSpaces, parseBalance);
-
-    // Бонусный баланс 
     getParam (html, result, 'bonus_balance', /Баланс:([\s\S]*?)\s*балл/i, replaceTagsAndSpaces, parseBalance);
-
     sumParam(html, result, '__tariff', /Тарифный план:([\s\S]*?)(?:<\/span>|<a)/i, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
-    // Бонусный статус
     sumParam (html, result, '__tariff', /(<strong[^>]*>\s*Бонусный счет[\s\S]*?)Баланс/i, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
-
+	
     // Статус договора 
     getParam (html, result, 'status', /Текущий статус договора:([\s\S]*?)<br/i, replaceTagsAndSpaces, html_entity_decode);
     getParam (html, result, 'abonday', /Суточная абонентская плата:([\s\S]*?)<br/i, replaceTagsAndSpaces, parseBalance);
@@ -214,4 +184,3 @@ function mainBelgorod(region){
 
     AnyBalance.setResult(result);
 }
-

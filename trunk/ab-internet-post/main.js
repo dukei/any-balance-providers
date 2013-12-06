c5ec45aec80996ffb5fc731038edcffd29db429b
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-12-06 13:14:17 +0000

    Post ltd. (Пятигорск) v2

--- trunk/ab-internet-post/main.js
+++ trunk/ab-internet-post/main.js
@@ -3,34 +3,38 @@
 */
 
 var g_headers = {
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36;',
 };
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = 'https://lk.kmv.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
-
+	
     var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
-
-	html = AnyBalance.requestPost(baseurl + 'login', {
-        'user[login]':prefs.login,
-        'user[password]':prefs.password,
-    }, addHeaders({Referer: baseurl + 'login'})); 
-
-    if(!/logout_link/i.test(html)){
+	
+	var params = createFormParams(html, function(params, str, name, value){
+		if(name == 'user[login]')
+			return prefs.login;
+		else if(name == 'user[password]')
+			return prefs.password;			
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + 'login', params, addHeaders({Referer: baseurl + 'login'})); 
+	
+    if(!/new HupoApp({/i.test(html)){
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
-
+	
     var result = {success: true};
-    getParam(html, result, 'balance', /Баланс[^>]*>\s*<[^>]*Остаток средств на вашем лицевом счете[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'acc_num', /Лицевой счет №&thinsp;([^<]*?)(?:\(|<)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, '__tariff', />Тариф([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /"n_sum_bal":"([^"]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'acc_num', /"vc_account":"([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	//getParam(html, result, '__tariff', />Тариф([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	
     AnyBalance.setResult(result);
 }

bd2d1c2a3af1113fc9706bed5fb8e55e5fbc2851
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-12-18 11:45:58 +0000

    Новый провайдер: IVIDEON

---
+++ trunk/ab-service-ivideon/main.js
@@ -0,0 +1,45 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://ru.ivideon.com/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'my/service/login', g_headers);
+	
+	html = AnyBalance.requestPost(baseurl + 'my/service/login', {
+		'LoginForm[username]': prefs.login,
+		'LoginForm[password]': prefs.password,
+		yt0: ''
+	}, addHeaders({Referer: baseurl + 'my/service/login'}));
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error && /Неверный логин или пароль/i.test(error))
+			throw new AnyBalance.Error(error, null, true);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	html = AnyBalance.requestGet(baseurl + 'my/billing/balance_async', g_headers);
+	var json = getJson(html);
+	
+	var result = {success: true};
+	getParam(json.currency+'', result, ['currency', 'balance'], null, replaceTagsAndSpaces);
+	getParam(json.balance_formatted+'', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+
+	AnyBalance.setResult(result);
+}

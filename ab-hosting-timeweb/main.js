165983470db384c7ef7dca1bc02f8fbdc8e7e85e
stark027 <stark027@gmail.com>
2013-12-19 10:16:25 +0000

    Time WEB v2

--- ab-hosting-timeweb/main.js
+++ ab-hosting-timeweb/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://cp.timeweb.ru/';
+    var baseurl = 'https://cp2.timeweb.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
 	
 	var html = AnyBalance.requestPost(baseurl, {
@@ -31,12 +31,13 @@ function main(){
     }
 
     var result = {success: true};
-    getParam(html, result, '__tariff', /customer_plan_value[\s\S]*?<td>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'balance', /Текущий баланс[\s\S]*?<td>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'deadline', /Дата блокирования[\s\S]*?block_date_value">([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
 	
-	html = AnyBalance.requestGet(baseurl + 'info/');
-	getParam(html, result, 'fio', /ФИО[\s\S]{1,10}'([\s\S]*?)'/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', [/Текущий баланс[\s\S]*?<td>([\s\S]*?)<\/td>/i, /"user-balance"(?:[^>]*>){1}([^<]*)/i], replaceTagsAndSpaces, parseBalance);
+	// getParam(html, result, '__tariff', /customer_plan_value[\s\S]*?<td>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 	
+	var days = getParam(html, null, null, /Дней осталось:(?:[^>]*>){3}(\d+)/i, replaceTagsAndSpaces, parseBalance);
+	if(isset(days)) {
+		getParam(new Date().getTime() + (days*86400*1000), result, 'deadline');
+	}
     AnyBalance.setResult(result);
 }

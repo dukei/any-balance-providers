0e3c6c0ee6a29ef7fdc2b0349f00f9b6c628a2e5
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-12-23 09:36:59 +0000

    Новый провайдер: Температура в Омске

---
+++ trunk/ab-weather-omsk-temperature/main.js
@@ -0,0 +1,18 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+function main() {
+	var text = AnyBalance.requestGet("http://myxa.opsb.ru/files/weather.js");
+	var result = {success: true};
+	
+	var temp = getParam(text, null, null, /Therm\s*=\s*"([^"]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+	if(!isset(temp)) {
+		AnyBalance.trace(text);
+		throw new AnyBalance.Error('Не получить температуру, проблемы на сайте или сайт изменен?');
+	}
+	
+	getParam(temp, result, 'temperature');
+	AnyBalance.setResult(result);
+}

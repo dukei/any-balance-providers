c435d513391468cb938428af55befde3dcc32842
stark027 <stark027@gmail.com>
2013-12-23 09:06:59 +0000

    Ростелеком Сибирь v4

--- ab-telephony-rt-sib/main.js
+++ ab-telephony-rt-sib/main.js
@@ -1,25 +1,20 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плате для сибирского филиала ростелеком
-
-Operator site: http://sibirtelecom.ru
-Личный кабинет: https://ispa.sibirtelecom.ru
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = "https://ispa.sibirtelecom.ru/";
     AnyBalance.setDefaultCharset('utf-8'); 
-   
+	
 //    return getRegions();
 
     var html = AnyBalance.requestPost(baseurl + 'index.jsp', {
@@ -64,5 +59,4 @@ function getRegions(result){
         sumParam(select, result, 'entryValues', /<option[^>]*value='([^']*)/ig, replaceTagsAndSpaces, html_entity_decode, create_aggregate_join('|'));
     }
     AnyBalance.setResult(result);
-
 }

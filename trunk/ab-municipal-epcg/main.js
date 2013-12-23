55cf8f2a9c0abe08f859a37b1b7eafed30d14b9b
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-12-23 07:50:54 +0000

    Новый провайдер: Elektroprivreda Crne Gore AD Niksic

---
+++ trunk/ab-municipal-epcg/main.js
@@ -0,0 +1,49 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.epcg.co.me/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Enter login!');
+	checkEmpty(prefs.meterno, 'Enter meter number!');
+
+	var html = AnyBalance.requestGet(baseurl + 'en02_12.html', g_headers);
+
+	html = AnyBalance.requestPost(baseurl + 'uvidustanje1.asp', {
+		tipdv:prefs.typeofuser || 'dom',
+		opstina:prefs.municipality || 'Budva',
+		sifra:prefs.login,
+		brbra:prefs.meterno, 
+		B3:'Potvrdi',
+	}, addHeaders({Referer: baseurl + 'en02_12.html'}));
+
+	if (!/Povratak/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	var result = {success: true};
+
+	getParam(html, result, 'fio', /Naziv:(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'adress', /Adresa:(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
+	getParam(html, result, 'meter_num', /Broj brojila:(?:[\s\S]*?<td[^>]*>){10}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Saldo:(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	
+	getParam(html, result, 'topay', /Iznos zadnje\s+fakture:(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'topay_date', /Datum zadnje\s+fakture:(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+
+	AnyBalance.setResult(result);
+}

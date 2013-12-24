75fd1818c872227bc48b677532084c71422ec98f
stark027 <stark027@gmail.com>
2013-12-24 07:15:29 +0000

    NETIS (Ярославль) v2

--- ab-internet-netis/main.js
+++ ab-internet-netis/main.js
@@ -1,91 +1,10 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плане для ярославского интернет-провайдера Netis
-
-Сайт оператора: http://netis.ru
-Личный кабинет: http://www.netis.ru/clients/stat.php
 */
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
 
 function parseTrafficGb(str){
   var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
   return parseFloat((val/1024).toFixed(2));
-}
-
-function requestPostMultipart(url, data, headers){
-	var parts = [];
-	var boundary = '------WebKitFormBoundaryrceZMlz5Js39A2A6';
-	for(var name in data){
-		parts.push(boundary, 
-		'Content-Disposition: form-data; name="' + name + '"',
-		'',
-		data[name]);
-	}
-	parts.push(boundary);
-        if(!headers)
-            headers = {};
-	headers['Content-Type'] = 'multipart/form-data; boundary=' + boundary.substr(2);
-	return AnyBalance.requestPost(url, parts.join('\r\n'), headers);
-}
-
-function createFormParams(html, process){
-    var params = {};
-    html.replace(/<input[^>]+name="([^"]*)"[^>]*>|<select[^>]+name="([^"]*)"[^>]*>[\s\S]*?<\/select>/ig, function(str, nameInp, nameSel){
-        var value = '';
-        if(nameInp){
-            value = getParam(str, null, null, /value="([^"]*)"/i, null, html_entity_decode);
-            name = nameInp;
-        }else if(nameSel){
-            value = getParam(str, null, null, /^<[^>]*value="([^"]*)"/i, null, html_entity_decode);
-            if(typeof(value) == 'undefined'){
-                var optSel = getParam(str, null, null, /(<option[^>]+selected[^>]*>)/i);
-                if(!optSel)
-                    optSel = getParam(str, null, null, /(<option[^>]*>)/i);
-                value = getParam(optSel, null, null, /value="([^"]*)"/i, null, html_entity_decode);
-            }
-            name = nameSel;
-        }
-
-        name = html_entity_decode(name);
-        if(process){
-            value = process(params, str, name, value);
-        }
-        if(typeof(value) != 'undefined')
-            params[name] = value;
-    });
-
-    AnyBalance.trace('Form params are: ' + JSON.stringify(params));
-    return params;
 }
 
 function createParamsArray(params){
@@ -145,12 +64,3 @@ function main(){
     
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

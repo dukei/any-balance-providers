38735d9052204b0f11cdd475ad76c42dac7ac76d
dmitry.kochin <dmitry.kochin@gmail.com>
2013-12-25 13:05:49 +0000

    Петербургская сбытовая компания - изменения в лк

--- ab-municipal-pitersbyt/main.js
+++ ab-municipal-pitersbyt/main.js
@@ -29,7 +29,7 @@ function gwtGetStrongName(js){
 function gwtGetJSON(str){
     if(/^\/\/EX/i.test(str)){
         var error = getParam(str, null, null, /Exception.*?","([^"]*)/);
-        throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
     }
 
     var json = getParam(str, null, null, /\/\/OK(.*)/);
@@ -41,20 +41,25 @@ function gwtGetJSON(str){
 var g_lks = {
     pesc: {
        url: 'http://ikus.pesc.ru:8080/IKUSUser/',
-       uid: '82CA86D7466828F87336B012396E3F69'
+       uid: '82CA86D7466828F87336B012396E3F69',
+       auth_uid: '82CA86D7466828F87336B012396E3F69'
     },
     pes: {
-       url: 'https://ikus.pes.spb.ru/IKUSUser/',
-       uid: 'BA2D39EE78C67FD4DE935035CA67FEC0'
-//       uid: '82133025EC649C66FFD6B00CF20FDAA9'
+	url: 'https://ikus.pes.spb.ru/IKUSUser/',
+	uid: '82CA86D7466828F87336B012396E3F69',
+	auth_uid: 'BA2D39EE78C67FD4DE935035CA67FEC0'
     }
 }
 
 function main(){
     var prefs = AnyBalance.getPreferences();
 
+    checkEmpty(prefs.login, 'Введите логин!');
+    checkEmpty(prefs.password, 'Введите пароль!');
+
     var baseurl = !prefs.type || !g_lks[prefs.type] ? g_lks.pesc.url : g_lks[prefs.type].url;
     var uid = !prefs.type || !g_lks[prefs.type] ? g_lks.pesc.uid : g_lks[prefs.type].uid;
+    var auth_uid = !prefs.type || !g_lks[prefs.type] ? g_lks.pesc.auth_uid : g_lks[prefs.type].auth_uid;
     AnyBalance.trace('Используем личный кабинет ' + baseurl);
 
     AnyBalance.setDefaultCharset('utf-8');    
@@ -88,10 +93,12 @@ function main(){
     html = AnyBalance.requestGet(baseurl + 'gwt/personalAccount/personalAccount.nocache.js');
     var permut = gwtGetStrongName(html);
 
+    /this\.\w=\$moduleBase;[^;]*;this.\w+=(\w+)/
+
     //Получаем баланс
     ////OK[8.65,36,55,'BuQ',54,53,52,10,51,50,10,12,40,10,9,2,3,1,2,49,10,12,48,10,9,2,3,1,2,47,10,12,46,10,9,2,3,1,2,45,10,12,44,10,9,2,3,1,2,43,10,12,33,10,9,2,3,1,2,42,10,12,40,10,9,2,3,1,2,41,10,12,40,10,9,2,3,1,2,7,1,39,38,10,37,0.0,36,35,34,10,12,33,10,9,2,3,1,2,32,10,12,0,9,2,3,1,2,31,10,12,30,10,9,2,3,1,2,29,10,12,28,10,9,2,3,1,2,27,10,12,26,10,9,2,3,1,2,25,10,12,24,10,9,2,3,1,2,23,10,12,22,10,9,2,3,1,2,21,10,12,20,10,9,2,3,1,2,19,10,12,18,10,9,2,3,1,2,17,10,12,16,10,9,2,3,1,2,15,10,12,14,10,9,2,3,1,2,13,10,12,11,10,9,2,3,1,2,12,1,8,5,5,7,0,6,1,5,4,10,3,1,2,1,1,["java.util.ArrayList/3821976829","com.sigma.gxt.client.BeanModel/1927856426","com.extjs.gxt.ui.client.data.RpcMap/3441186752","meter_plan","java.lang.Integer/3438268394","id","meter_capacity","meter","value","java.lang.String/2004016611","21.04.2009","key","Дата установки","062591708","Номер","В ДОМЕ","Место установки","Сбытовая","Владелец","Исправный","Состояние","Однотарифный","План прибора учета","СОЛО 1S-В","Тип","220","Вольтаж","5-60","Ток","5","Разрядность","Класс точности","0","Межповерочный интервал","peni","java.lang.Double/858496421","address","ПОДБОРЬЕ ДЕР. (ИЛЬИНСКАЯ) ПОДБОРЬЕ ДЕР., д.30 , кв.0","flat","1","Комнат","Потребителей","Льготников","Сельское население","Тариф","Газ.","Плита","","Площадь кв.м.","Прописанных","account","021\\00066029","payment_id","java.lang.Long/4227064769","energy"],0,6]
     html = AnyBalance.requestPost(baseurl + 'com.sigma.personal.client.ClientService.gwt',
-        "6|0|4|" + baseurl + "gwt/personalAccount/|"+uid+"|com.sigma.personal.client.ClientService|getAbonsList|1|2|3|4|0|",
+        "6|0|4|" + baseurl + "gwt/personalAccount/|"+auth_uid+"|com.sigma.personal.client.ClientService|getAbonsList|1|2|3|4|0|",
         { 
           'Content-Type': 'text/x-gwt-rpc; charset=UTF-8', 
           'X-GWT-Module-Base':baseurl + 'gwt/personalAccount/',

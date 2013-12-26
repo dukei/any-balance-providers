b3690ac35eb096ebd419f7516c3b34e59e970359
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-12-26 11:50:17 +0000

    Life:) Беларусь

--- trunk/ab-cell-life-by/main.js
+++ trunk/ab-cell-life-by/main.js
@@ -9,26 +9,35 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
+function getStateParams(html, param) {
+	return getParam(html, null, null, new RegExp(param + "[^>]*value=['\"]([^'\"]+)", 'i'));
+}
+
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://issa2.life.com.by/';
+    var baseurl = 'https://issa.life.com.by/';
+	var baseurlOld = 'https://issa2.life.com.by/';
 	
-	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.login, 'Введите номер телефона!');
 	checkEmpty(prefs.password, 'Введите пароль!');
-
-	var html = AnyBalance.requestGet(baseurl + '', g_headers);
+	
+	var main = 'ru/login/?next=/ru/';
+	var html = AnyBalance.requestGet(baseurl + main, g_headers);
 	
     var matches = prefs.login.match(/^(\d{2})(\d{7})$/);
     if(!matches)
         throw new AnyBalance.Error('Пожалуйста, введите 9 последних цифр номера телефона (без префикса +375) без пробелов и разделителей.');
         
-    html = AnyBalance.requestPost(baseurl, {
-        Code: matches[1],
-        Phone: matches[2],
-        password: prefs.password
-    }, g_headers);
+    html = AnyBalance.requestPost(baseurl + main, {
+		csrfmiddlewaretoken:getStateParams(html, 'csrfmiddlewaretoken'),
+		msisdn_code:matches[1],
+		msisdn:matches[2],
+		super_password:prefs.password,
+		form:true,
+		next:'/ru/'
+	}, addHeaders({Referer: baseurl + main}));
     
-    if(!/\/Account.aspx\/Logoff/i.test(html)){
+    if(!/log-out/i.test(html)){
         var error = getParam(html, null, null, /<div class="validation-summary-errors errorMessage">([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
@@ -37,23 +46,33 @@ function main(){
 
     var result = {success: true};
 	
-    getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /Текущий основной баланс:[\s\S]*?<div[^>]*>\s*(-?\d[\d\., \s]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'balance_bonus', /Текущий бонусный баланс:[\s\S]*?<div[^>]*>\s*(-?\d[\d\., \s]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /Тарифный план([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'fio', /ФИО(?:[^>]+>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'phone', /Номер life(?:[^>]+>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 
-    if (AnyBalance.isAvailable('traffic_left')) {
-    	html = AnyBalance.requestGet(baseurl + 'User.aspx/Index');
-    	var table = getParam(html, null, null, /Остаток пакетов:[\s\S]*<table[^>]+class="longinfo"[^>]*>([\s\S]*?)<\/table>/i);
-    	if (table) {
-    		sumParam(html, result, 'traffic_left', /<tr[^>]*>\s*<td[^>]*>(?:[\s\S](?!<tr|НОЧНОЙ))*?<td[^>]*>((?:[\s\S](?!<tr))*?(?:[мmkкгg][бb]|байт|byte)[^<]*)<\/td>\s*<td[^>]*>/ig, replaceTagsAndSpaces, parseTraffic, aggregate_sum);
-    		sumParam(html, result, 'min_left_other', /<tr[^>]*>\s*<td[^>]*>(?:[\s\S](?!<tr))*?другие сети(?:[\s\S](?!<tr))*?<td[^>]*>((?:[\s\S](?!<tr))*?(?:мин|min)[^<]*)<\/td>\s*<td[^>]*>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-    		sumParam(html, result, 'min_left', /<tr[^>]*>\s*<td[^>]*>(?:[\s\S](?!<tr|другие сети))*?<td[^>]*>((?:[\s\S](?!<tr))*?(?:мин|min)[^<]*)<\/td>\s*<td[^>]*>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-    		sumParam(html, result, 'traffic_night_left', /<tr[^>]*>\s*<td[^>]*>(?:[\s\S](?!<tr))*?НОЧНОЙ(?:[\s\S](?!<tr))*?<td[^>]*>((?:[\s\S](?!<tr))*?(?:[мmkкгg][бb]|байт|byte)[^<]*)<\/td>\s*<td[^>]*>/ig, replaceTagsAndSpaces, parseTraffic, aggregate_sum);
-    		sumParam(html, result, 'sms_left', /<td[^>]*>(?:[\s\S](?!<tr))*?<td[^>]*>((?:[\s\S](?!<tr))*?(?:СМС|sms)[^<]*)<\/td>\s*<td[^>]*>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-    		sumParam(html, result, 'mms_left', /<td[^>]*>(?:[\s\S](?!<tr))*?<td[^>]*>((?:[\s\S](?!<tr))*?(?:ММС|mms)[^<]*)<\/td>\s*<td[^>]*>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-    	} else {
-    		AnyBalance.trace('Информация по пакетам не найдена.');
-    	}
-    }
+	sumParam(html, result, 'sms_left', /SMS внутри сети(?:[^>]+>){2}([^<]+)/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+	sumParam(html, result, 'mms_left', /MMS внутри сети(?:[^>]+>){2}([^<]+)/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+	sumParam(html, result, 'min_left_other', /Звонки на другие сети(?:[^>]+>){2}([^<]+)/ig, replaceTagsAndSpaces, parseMinutes, aggregate_sum);
+    sumParam(html, result, 'min_left', /Звонки внутри сети(?:[^>]+>){2}([^<]+)/ig, replaceTagsAndSpaces, parseMinutes, aggregate_sum);
+	sumParam(html, result, 'traffic_left', /Интернет(?:[^>]+>){2}([^<]+)/ig, replaceTagsAndSpaces, parseTraffic, aggregate_sum);
+	//sumParam(html, result, 'traffic_night_left', /<tr[^>]*>\s*<td[^>]*>(?:[\s\S](?!<tr))*?НОЧНОЙ(?:[\s\S](?!<tr))*?<td[^>]*>((?:[\s\S](?!<tr))*?(?:[мmkкгg][бb]|байт|byte)[^<]*)<\/td>\s*<td[^>]*>/ig, replaceTagsAndSpaces, parseTraffic, aggregate_sum);	
+	// В новом кабинете нет баланса, очень круто :)
+	if (AnyBalance.isAvailable('balance', 'balance_bonus')) {
+	    html = AnyBalance.requestPost(baseurlOld, {
+			Code: matches[1],
+			Phone: matches[2],
+			password: prefs.password
+		}, g_headers);
+		
+		if(!/\/Account.aspx\/Logoff/i.test(html)){
+			var error = getParam(html, null, null, /<div class="validation-summary-errors errorMessage">([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+			if(error)
+				throw new AnyBalance.Error(error);
+			throw new AnyBalance.Error("Не удалось зайти в личный кабинет. Сайт изменен?");
+		}
+		getParam(html, result, 'balance', /Текущий основной баланс:[\s\S]*?<div[^>]*>\s*(-?\d[\d\., \s]*)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'balance_bonus', /Текущий бонусный баланс:[\s\S]*?<div[^>]*>\s*(-?\d[\d\., \s]*)/i, replaceTagsAndSpaces, parseBalance);
+	}
+
     AnyBalance.setResult(result);
 }

c409e884d457dc9ecdfe2ed6f0d77c076fd57b47
stark027 <stark027@gmail.com>
2013-12-27 07:23:43 +0000

    Курсы валют НБУ v3

--- ab-exchange-nbu/main.js
+++ ab-exchange-nbu/main.js
@@ -1,64 +1,64 @@
-function getRateDate(result, info){
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+function getRateDate(result, info) {
 	var matches, regexp = /<td[^>]+class="date"[^>]*>([\s\S]*?)<\/td>/i;
-	if(matches = info.match(regexp)){
-		if(AnyBalance.isAvailable('date')){
+	if (matches = info.match(regexp)) {
+		if (AnyBalance.isAvailable('date')) {
 			result.date = matches[1];
 			result.__tariff = matches[1];
-                }
+		}
 	}
 }
 
-function getRate(result, info, namein){
-	if(AnyBalance.isAvailable(namein)){
-                var prefs = AnyBalance.getPreferences();
-		var matches, regexpValue = new RegExp('<tr[^>]*>\\s*<td[^>]*>[^<]*</td>\\s*<td[^>]*>'+namein+'(?:[\\s\\S]*?<td[^>]*>){3}([^<]*)', 'i'),
-                             regexpMul = new RegExp('<tr[^>]*>\\s*<td[^>]*>[^<]*</td>\\s*<td[^>]*>'+namein+'(?:[\\s\\S]*?<td[^>]*>){1}([^<]*)', 'i');
-		if(matches = info.match(regexpValue)){
-	     	        var val = parseFloat(matches[1].replace(',','.'));
-                        if(prefs.normalize){
-                            matches = info.match(regexpMul);
-                            var mul = parseInt(matches[1]) || 1;
-                            val /= mul;
-                            if(val >= 1){ 
-                                //Если курс больше единицы, то так и оставляем, он комфортен
-                                result['suf' + namein] = ' ' + shorts.UAH + '/' + (shorts[namein] || namein);
-                            }else{
-                                //Если курс меньше единицы, то лучше инвертировать его
-                                val = 1/val;
-                                result['suf' + namein] = ' ' + (shorts[namein] || namein) + '/' + shorts.UAH;
-                            }
-                            val = Math.round(val*100)/100;
-                        }else{
-                            result['pre' + namein] = (shorts[namein] || namein) + " ";
-                        }
-                        result[namein] = val;
+function getRate(result, info, namein) {
+	if (AnyBalance.isAvailable(namein)) {
+		var prefs = AnyBalance.getPreferences();
+		var matches, regexpValue = new RegExp('<tr[^>]*>\\s*<td[^>]*>[^<]*</td>\\s*<td[^>]*>' + namein + '(?:[\\s\\S]*?<td[^>]*>){3}([^<]*)', 'i'),
+			regexpMul = new RegExp('<tr[^>]*>\\s*<td[^>]*>[^<]*</td>\\s*<td[^>]*>' + namein + '(?:[\\s\\S]*?<td[^>]*>){1}([^<]*)', 'i');
+		if (matches = info.match(regexpValue)) {
+			var val = parseFloat(matches[1].replace(',', '.'));
+			if (prefs.normalize) {
+				matches = info.match(regexpMul);
+				var mul = parseInt(matches[1]) || 1;
+				val /= mul;
+				if (val >= 1) {
+					//Если курс больше единицы, то так и оставляем, он комфортен
+					result['suf' + namein] = ' ' + shorts.UAH + '/' + (shorts[namein] || namein);
+				} else {
+					//Если курс меньше единицы, то лучше инвертировать его
+					val = 1 / val;
+					result['suf' + namein] = ' ' + (shorts[namein] || namein) + '/' + shorts.UAH;
+				}
+				val = Math.round(val * 100) / 100;
+			} else {
+				result['pre' + namein] = (shorts[namein] || namein) + " ";
+			}
+			result[namein] = val;
 		}
-        }
+	}
 }
-
 var shorts = {
-UAH: "₴",
-USD: "$",
-EUR: "€", 
-GBP: "£",
-BYR: "Br", 
-KZT: "〒",
-CHF: "₣",
-CNY: "Ұ",
-JPY: "¥",
-RUB: "р"
+	UAH: "₴",
+	USD: "$",
+	EUR: "€",
+	GBP: "£",
+	BYR: "Br",
+	KZT: "〒",
+	CHF: "₣",
+	CNY: "Ұ",
+	JPY: "¥",
+	RUB: "р"
 };
-	
-function main(){
-        if(AnyBalance.getLevel() < 5)
-            return "Для этого провайдера необходимо AnyBalance API v.5+";
 
+function main() {
+	if (AnyBalance.getLevel() < 5) return "Для этого провайдера необходимо AnyBalance API v.5+";
 	AnyBalance.trace('Connecting to nbu...');
-	
 	var info = AnyBalance.requestGet('http://bank.gov.ua/control/uk/curmetal/detail/currency?period=daily');
 	
 	var result = {success: true};
-
+	
 	getRate(result, info, 'AUD');
 	getRate(result, info, 'AZM');
 	getRate(result, info, 'GBP');
@@ -86,9 +86,7 @@ function main(){
 	getRate(result, info, 'CHF');
 	getRate(result, info, 'CNY');
 	getRate(result, info, 'JPY');
-	
 	getRateDate(result, info);
 	
 	AnyBalance.setResult(result);
 }
-

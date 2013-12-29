12539776efc779d4d478c78e6d69beb137059387
ValeraVi <ValeraVi@1f0edefa-e828-6bf1-598a-efb20914ff10>
2013-12-29 02:52:13 +0000

    Supported new website design at www.shop.skanetrafiken.se

--- ab-transport-jojo/main.js
+++ ab-transport-jojo/main.js
@@ -2,17 +2,10 @@
 AnyBalance Provider (http://any-balance-providers.googlecode.com)
 
 Retrieves the balance of the Jojo Skanetrafiken transport card.
-Some algorithms, urls and regexps are taken from:
-https://github.com/liato/android-bankdroid/blob/master/src/com/liato/bankdroid/banking/banks/Jojo.java
 
 Skanetrafiken website: http://www.skanetrafiken.se
-My pages: https://www.skanetrafiken.se/templates/MSRootPage.aspx?id=2935&epslanguage=SV
+My pages: http://www.shop.skanetrafiken.se/
 */
-
-var reViewState = "__VIEWSTATE\"\\s+value=\"([^\"]+)\"";
-var ctLoginFail = "ctl00_fullRegion_menuRegion_Login_msNotLoggedInDiv"
-var ctWrongLoginPass = "Inloggningen misslyckades";
-var reAccounts = "1_mRepeaterMyCards_ctl(\\d{2,3})_LinkButton\\d{1,3}\"[^>]+>([^<]+)</a>\\s*</td>\\s*<td[^>]+>\\s*<a\\s*id=\"ctl00_fullRegion_mainRegion_CardInformation1_mRepeaterMyCards_ctl\\d{2,3}_LinkButton\\d{1,3}\"[^>]+>([^<]+)</a>";
 
 var headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64)"};
@@ -29,86 +22,39 @@ if (typeof String.prototype.trim != 'function') { // detect native implementatio
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
-	if(prefs.card)
-		if(!/^\d{4}$/.test(prefs.card))
-			throw new AnyBalance.Error("Enter only 4 last digits of the card or leave this field empty.");
+	var result = {success: true};
+	if(prefs.cardnum)
+		if(!/^\d{10}$/.test(prefs.cardnum))
+			throw new AnyBalance.Error("Enter only first 10 digits of the card.");
 
-	var baseurl = "https://www.skanetrafiken.se/templates/";
-	var result = {success: true}, matches;
+	var baseurl = "http://www.shop.skanetrafiken.se/";
 	AnyBalance.setDefaultCharset("ISO-8859-1");
 	
-	var url = baseurl + "MSRootPage.aspx?id=2935&epslanguage=SV";
-	AnyBalance.trace("GET: " + url);
-	var html = AnyBalance.requestGet(url, headers);
-	AnyBalance.trace("GET: " + url + "... OK");
-
-	if (!(matches = html.match(reViewState)))
-		throw new AnyBalance.Error("login token not found.");
-	var token = matches[1];
-	//AnyBalance.trace(url + " token: " + token);
-		
-	url = baseurl + "MSRootPage.aspx?id=2935&epslanguage=SV";
+	var url = baseurl + "kollasaldo.html";
 	AnyBalance.trace("POST: " + url);
 	html = AnyBalance.requestPost(url, {
-		'__EVENTTARGET':'',
-		'__EVENTARGUMENT':'',
-		'__VIEWSTATE':token,
-		'ctl00$fullRegion$menuRegion$Login$UsernameTextBox':prefs.login,
-		'ctl00$fullRegion$menuRegion$Login$PasswordTextBox':prefs.password,
-		'ctl00$fullRegion$menuRegion$Login$LoginButton':'Logga in',
+		'cardno':prefs.cardnum,
+		'backno':prefs.cardcvc,
+		'ST_CHECK_SALDO':'Se saldo',
 	}, headers);
 	AnyBalance.trace("POST: " + url + "... OK");
-	
-	if (html.indexOf(ctWrongLoginPass) >= 0)
-		throw new AnyBalance.Error("Invalid login and/or password");
-	if (html.indexOf(ctLoginFail) >= 0)
-		throw new AnyBalance.Error("Login failed. Website design is changed?");
-	
-	url = baseurl + "CardInformation.aspx?id=26957&epslanguage=SV";
-	AnyBalance.trace("GET: " + url);
-	html = AnyBalance.requestGet(url, headers);
-	AnyBalance.trace("GET: " + url + "... OK");
 
-	if (!(matches = html.match(reViewState)))
-		throw new AnyBalance.Error("accounts token not found.");
-	token = matches[1];
-	//AnyBalance.trace(url + " accounts token: " + token);
-
-	var pattern = new RegExp(reAccounts, 'gi');
-	var accID, accName, accNumLast4;
-	while(matches = pattern.exec(html))
-	{
-		accID = matches[1];
-		accName = matches[2].trim();
-		accNumLast4 = matches[3].trim().slice(-4);
-		AnyBalance.trace("Account found: id=" + accID + " numLast4=" + accNumLast4 + " name=" + accName);
-		if (!prefs.card || prefs.card == accNumLast4)
-			break;
+	if(/class="error"/i.test(html)){
+		var error = getParam(html, null, null, /<div[^>]+class="error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		if(error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Card balance get error. Website design is changed?');
 	}
-	if (!matches)
-		throw new AnyBalance.Error("Card number, specified in settings, was not found. Correct it or set it to blank and try again.");
-	AnyBalance.trace("Account selected: id=" + accID + " numLast4=" + accNumLast4 + " name=" + accName);
-		
-	url = baseurl + "CardInformation.aspx?id=26957&epslanguage=SV";
-	var obj = {};
-	obj['__EVENTTARGET']='';
-	obj['__EVENTARGUMENT']='';
-	obj['__VIEWSTATE']=token;
-	obj['ctl00$fullRegion$mainRegion$CardInformation1$mRepeaterMyCards$ctl' + accID + '$Button']='Kortinfo';
-	AnyBalance.trace("POST: " + url);
-	html = AnyBalance.requestPost(url, obj, headers);
-	AnyBalance.trace("POST: " + url + "... OK");
-
-	result.__tariff = accName;
-	getParam(html, result, 'balance', /labelsaldoinfo"[\s\S]*?>([^<]+)</i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, ['currency','balance'], /labelsaldoinfo"[\s\S]*?>([^<]+)</i, replaceTagsAndSpaces, parseCurrency);
-	getParam(html, result, 'validfrom', /labelvalidperiodinfo"[\s\S]*?>([^<]+) - \d/i, replaceTagsAndSpaces, parseDateISO);
-	getParam(html, result, 'validtill', /labelvalidperiodinfo"[\s\S]*?>[\s\S]*? - ([^<]+)</i, replaceTagsAndSpaces, parseDateISO);
-	getParam(html, result, 'zoneslist', /labelzoneareainfozones"[\s\S]*?>([^<]+)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'zonearea', /labelzoneareainfo"[\s\S]*?>([^<]+)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'status', /labelstatusinfo"[\s\S]*?>([^<]+)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'info', /labelobsinfo"[\s\S]*?>([^<]+)</i, replaceTagsAndSpaces);
-	cardnum = getParam(html, null, null, /labelcardnumberinfo"[\s\S]*?>([^<]+)</i, replaceTagsAndSpaces);
+	
+	result.__tariff = prefs.cardnum;
+	cardnum = prefs.cardnum;
+	getParam(html, result, 'balance', /Nytt saldo:<\/h3>[\s\S]*?<h3>([\s\S]*?)<\/h3>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balancenow', /Tillg.ngligt nu:<\/h4>[\s\S]*?<p>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balancedownload', /Att h.mta:[\s\S]*?<p>.([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['currency','balance','balancenow','balancedownload'], /Nytt saldo:<\/h3>[\s\S]*?<h3>([\s\S]*?)<\/h3>/i, replaceTagsAndSpaces, parseCurrency);
+	getParam(html, result, 'validfrom', /Periodkort fr:<\/h4>[\s\S]*?<p>([\s\S]*?) till/i, replaceTagsAndSpaces, parseDateISO);
+	getParam(html, result, 'validtill', /Periodkort fr:<\/h4>[\s\S]*? till ([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, parseDateISO);
+	getParam(html, result, 'zonesnum', /Antal zoner:<\/h4>[\s\S]*?<p>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
 	if (isAvailable('cardnum'))
 	{
 		result['cardnum'] = cardnum;

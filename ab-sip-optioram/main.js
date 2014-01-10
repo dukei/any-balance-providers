34e74cc80354c02de0e9fe0c78dd44ea8463ef50
stark027 <stark027@gmail.com>
2014-01-10 15:58:35 +0000

    Optiroam

---
+++ ab-sip-optioram/main.js
@@ -0,0 +1,42 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://customer.optiroam.com/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestPost(baseurl + 'index.php?m=general&a=login', {
+		done:'submit_log',
+		remember_me:'',
+		pr_login:prefs.login,
+		pr_password:prefs.password
+	}, addHeaders({Referer: baseurl + 'index.php'}));
+
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /(AUTHENTICATION REFUSED[^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Текущий баланс:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['currency', 'balance'], /Текущий баланс:([^<]+)/i, replaceTagsAndSpaces, parseCurrency);
+	getParam(html, result, 'phone', /Роум Номер:([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'phone2', /Ваш телефонный номер:([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

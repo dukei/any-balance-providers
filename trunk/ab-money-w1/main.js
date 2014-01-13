aa25d1ebcb661bb67c938df65cbfd188bb3373cd
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-01-13 13:41:24 +0000

    W1 Единый Кошелек v4

--- trunk/ab-money-w1/main.js
+++ trunk/ab-money-w1/main.js
@@ -2,9 +2,12 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 var g_headers = {
-	'Connection': 'keep-alive',
-	'Content-type': 'text/xml; charset=UTF-8',
-	'User-Agent': 'Apache-HttpClient/UNAVAILABLE (java 1.4)'
+	'Origin':'https://www.walletone.com',
+	'JS-Framework':'Basis',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
+	'Accept': '*/*',
+	'Content-type': 'text/xml;charset=utf-8',
+	'Referer':'https://www.walletone.com/client/?attempt=1',
 };
 
 function encryptPass(pass) {
@@ -30,14 +33,17 @@ function main() {
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	var loginXml = '<?xml version="1.0" encoding="UTF-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><GetSessionTicket xmlns="Wallet.Security.WebService"><Login>' + prefs.login + '</Login><LoginType>Auto</LoginType><Password>' + encryptPass(prefs.password) + '</Password><ClientId>W1_android</ClientId></GetSessionTicket></soap:Body></soap:Envelope>';
+	var html = AnyBalance.requestGet('https://www.walletone.com/client/?attempt=1', g_headers);
+	
+	var loginXml = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><soap:Header><ParamsHeader xmlns="Wallet.Security.WebService"><Params><Param Name="CultureId" Value="ru-RU"/></Params></ParamsHeader></soap:Header><soap:Body><GetSessionTicket xmlns="Wallet.Security.WebService"><Login>'+ prefs.login +'</Login><Password>' + encryptPass(prefs.password) +'</Password><LoginType>Auto</LoginType><ClientId>w1_web</ClientId><Params><Param Name="UserAgent" Value="Chrome 31.0.1650.63"/><Param Name="ClientResolutionX" Value="1920"/><Param Name="ClientResolutionY" Value="1080"/><Param Name="AppVersion" Value="201312260718-test"/></Params></GetSessionTicket></soap:Body></soap:Envelope>';
 	// Получаем SessionKey
-	var html = AnyBalance.requestPost('http://services.w1.ru/10/SecurityService.asmx', loginXml, addHeaders({
+	html = AnyBalance.requestPost('https://www.walletone.com/w1service/SecurityService.asmx', loginXml, addHeaders({
 		'SOAPAction': 'Wallet.Security.WebService/GetSessionTicket'
 	}));
 	var SessionUserId = getParam(html, null, null, /<SessionUserId>([\s\S]*?)<\/SessionUserId>/i);
 	var SessionKey = getParam(html, null, null, /<SessionKey>([\s\S]*?)<\/SessionKey>/i);
-	if (!SessionKey || !SessionUserId) throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	if (!SessionKey || !SessionUserId) 
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	var balanceXml = '<?xml version="1.0" encoding="UTF-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Header><SecurityHeader xmlns="Wallet.Processing.WebService"><SessionKey>' + SessionKey + '</SessionKey></SecurityHeader><ParamsHeader><Params><Param Name="CultureId" Value="ru-RU" /></Params></ParamsHeader></soap:Header><soap:Body><GetUserBalance xmlns="Wallet.Processing.WebService" /></soap:Body></soap:Envelope>';
 	html = AnyBalance.requestPost('http://services.w1.ru/11/ProcessingService.asmx', balanceXml, addHeaders({
 		'SOAPAction': 'Wallet.Processing.WebService/GetUserBalance'

b6b6c12163f75d2b20250070602baa2b71d5a07c
stark027 <stark027@gmail.com>
2014-01-13 09:15:37 +0000

    Новый провайдер: Газфонд (НПФ)

---
+++ ab-money-gazfondpf/main.js
@@ -0,0 +1,49 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://client.gazfond.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+	
+	var captchaa;
+	if(AnyBalance.getLevel() >= 7){
+		AnyBalance.trace('Пытаемся ввести капчу');
+		var captcha = AnyBalance.requestGet(baseurl + 'captcha/main.png?p=' + new Date().getTime());
+		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaa);
+	}else{
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + 'main?sysname=adm_logon_post', {
+		login: prefs.login,
+		password: prefs.password,
+		captcha_edit: captchaa
+	}, addHeaders({Referer: baseurl + 'login'}));
+
+	if (!/logout/i.test(html)) {
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Договор об обязательном пенсионном страховании(?:[^>]*>){18}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /Договор об обязательном пенсионном страховании(?:[^>]*>){13}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(result.__tariff, result, 'dogovor');
+	
+	AnyBalance.setResult(result);
+}

832badd92d12b92dafb966dc735e8bfc45b774c4
stark027 <stark027@gmail.com>
2014-01-13 12:01:49 +0000

    Dx.com

--- ab-service-dxcom/main.js
+++ ab-service-dxcom/main.js
@@ -7,29 +7,33 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
+	Origin:'http://m.dealextreme.com',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://passport.dx.com/';
+	var baseurl = 'http://m.dealextreme.com/';
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Enter login!');
 	checkEmpty(prefs.password, 'Enter password!');
 
-	var html = AnyBalance.requestGet(baseurl + 'accounts/default.dx?redirect=http%3A%2F%2Fdx.com%2F', g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'accounts/login.dx', addHeaders({Referer: baseurl + ''}));
 
 	var params = createFormParams(html, function(params, str, name, value) {
-		if (name == 'ctl00$content$txtLoginEmail') 
+		if (name == 'ctl00$content$txtLoginEmail' || name == 'ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$txtEmail') 
 			return prefs.login;
-		else if (name == 'ctl00$content$txtPassword')
+		else if (name == 'ctl00$content$txtPassword' || name == 'ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$txtPassword')
 			return prefs.password;
 
 		return value;
 	});
+	
+	params['ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$btnLogin.x'] = 73;
+	params['ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$btnLogin.y'] = 10;
 
-	html = AnyBalance.requestPost(baseurl + 'accounts/default.dx?redirect=http%3A%2F%2Fdx.com%2F', params, addHeaders({Referer: baseurl + 'accounts/default.dx?redirect=http%3A%2F%2Fdx.com%2F'}));
+	html = AnyBalance.requestPost(baseurl + 'accounts/login.dx', params, addHeaders({Referer: baseurl + 'accounts/login.dx'}));
 
 	if (!/logout|выход/i.test(html)) {
 		var error = sumParam(html, null, null, /<div>\s*<\s*span[^>]*color:Red[^>]*>([^<]+)/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
@@ -41,20 +45,27 @@ function main() {
 		throw new AnyBalance.Error('Can`t login, is the site changed?');
 	}
 	
-	html = AnyBalance.requestGet('https://my.dx.com/', g_headers);
+	html = AnyBalance.requestGet(baseurl + 'accounts/myorders.dx', addHeaders({Referer: baseurl + 'accounts'}));
 
 	var result = {success: true};
 	
-	getParam(html, result, 'points', /DX Points:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	// getParam(html, result, 'points', /DX Points:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	
-	var firstTr = getParam(html, null, null, /<tr class="(?:on|)">(?:[^>]+>){10,15}\s*<\/tr>/i);
+	var firstTr = getParam(html, null, null, /<tr>(?:[^>]+>){8,10}\s*<\/tr>/i);
 	if(firstTr) {
-		getParam(html, result, 'order_num', /<tr\s*class="(?:on|)">(?:[^>]+>){2}\s*(\d{10,})/i, replaceTagsAndSpaces, html_entity_decode);
-		getParam(html, result, 'order_date', /<tr\s*class="(?:on|)">(?:[^>]+>){5}([\s\S]*?)<\/td/i, replaceTagsAndSpaces, parseDate);
-		getParam(html, result, 'order_sum', /<tr\s*class="(?:on|)">(?:[^>]+>){7}([\s\S]*?)<\/td/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'order_status', /<tr\s*class="(?:on|)">(?:[^>]+>){9}([\s\S]*?)<\/td/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(firstTr, result, 'order_num', /(?:[^>]+>){2}\s*(\d{10,})/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(firstTr, result, 'order_date', /(?:[^>]+>){5}([^<]+)/i, replaceTagsAndSpaces, parseDate);
+		getParam(firstTr, result, 'order_status', /(?:[^>]+>){7}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		
+		if(isAvailable('order_sum')) {
+			var href = getParam(firstTr, null, null, /href="\/([^"]+)/i);
+			if(href) {
+				html = AnyBalance.requestGet(baseurl + href, addHeaders({Referer: baseurl + 'accounts'}));
+				getParam(html, result, 'order_sum', /Grand Total:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+			}
+		}
 	} else {
-		AnyBalance.trace('Can`t find any orded.');
+		AnyBalance.trace('Can`t find any order.');
 	}
 	
 	AnyBalance.setResult(result);

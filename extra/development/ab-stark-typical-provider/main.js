53457e803ce52af275f00641f488422a7adb3e8e
stark027 <stark027@gmail.com>
2014-01-15 08:56:24 +0000

    Новая версия ab-stark-typical-provider

--- extra/development/ab-stark-typical-provider/main.js
+++ extra/development/ab-stark-typical-provider/main.js
@@ -17,31 +17,35 @@ function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'https://kabinet.xxxxxx.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
-
+	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
-
+	
 	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'login') 
+			return prefs.login;
+		else if (name == 'password')
+			return prefs.password;
 
-	var tform = getParam(html, null, null, /<input[^>]+name="t:formdata"[^>]*value="([^"]*)/i, null, html_entity_decode);
-	if (!tform)
-		throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
-
-	html = AnyBalance.requestPost(baseurl + 'login.loginform', {
-		't:formdata': tform,
-		baseNumber: prefs.login,
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + 'login', {
+		login: prefs.login,
 		password: prefs.password,
-		confirmAgreement: 'on'
+		'Remember': 'false'
 	}, addHeaders({Referer: baseurl + 'login'}));
-
+	
 	if (!/logout/i.test(html)) {
 		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error && /Неверный логин или пароль/i.test(error))
-			throw new AnyBalance.Error(error, null, true);
 		if (error)
-			throw new AnyBalance.Error(error);
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(html));
+		
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
+	
 	var result = {success: true};
 	
 	getParam(html, result, 'fio', /Имя абонента:(?:[\s\S]*?<b[^>]*>){1}([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -51,6 +55,6 @@ function main() {
 	getParam(html, result, 'deadline', /Действителен до:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseDate);
 	getParam(html, result, ['currency', 'balance'], /Текущий баланс:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseCurrency);
 	getParam(html, result, 'status', /Статус:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
-
+	
 	AnyBalance.setResult(result);
 }

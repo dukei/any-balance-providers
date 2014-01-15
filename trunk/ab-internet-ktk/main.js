8eba1cdf7ac5390e4a98d2fb83f95321e67e5695
belchikovs@gmail.com <belchikovs@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-01-15 18:17:43 +0000

    Updated KTK

--- trunk/ab-internet-ktk/main.js
+++ trunk/ab-internet-ktk/main.js
@@ -1,53 +1,55 @@
 /**
-РџСЂРѕРІР°Р№РґРµСЂ AnyBalance (http://any-balance-providers.googlecode.com)
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-"РљСѓСЂСЃРєР°СЏ С‚РµР»РµС„РѕРЅРЅР°СЏ РєРѕРјРїР°РЅРёСЏ" - РРЅС‚РµСЂРЅРµС‚ (kurskonline)
-РЎР°Р№С‚ РѕРїРµСЂР°С‚РѕСЂР°: http://www.r46.ru/
-Р›РёС‡РЅС‹Р№ РєР°Р±РёРЅРµС‚: http://test.kabinet.r46.ru/login?next=/
+"Курская телефонная компания" - Интернет (kurskonline)
+Сайт оператора: http://www.r46.ru/
+Личный кабинет: https://kabinet.r46.ru/
 */
 
+function checkEmpty(param, error, notfatal) {
+  if (!param)
+        throw new AnyBalance.Error(error, null, !notfatal);
+}
+
 function main(){
-    var prefs = AnyBalance.getPreferences();
-    var baseurl = 'http://test.kabinet.r46.ru/';
-	var result = {success: true};
-    
-    AnyBalance.setDefaultCharset('utf-8');
+  var prefs = AnyBalance.getPreferences();
+  var baseurl = 'https://kabinet.r46.ru/';
+  var result = {success: true};
 
-	if(AnyBalance.isAvailable('balance', 'agreement')){
-    
-		// Р—Р°С…РѕРґРёРј РЅР° РіР»Р°РІРЅСѓСЋ СЃС‚СЂР°РЅРёС†Сѓ
-		var info = AnyBalance.requestPost(baseurl + "login?next=/", {
-			login: prefs.login,
-			pass: prefs.password
-		});
-		var $parse = $('<div>' + info + '</div>');
-		var sidebar = $parse.find("#sidebar");
-		if(!sidebar){
-			throw new AnyBalance.Error("РРјСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ РёР»Рё РїР°СЂРѕР»СЊ РЅРµ РІРµСЂРЅС‹");
-		}
-		var ths = sidebar.find("th");
-		for(var i=0; i< ths.length; i++)
-		{
-			var th = $(ths[i]);
-			var text = th.text();
-			if(text  == "в„– РґРѕРіРѕРІРѕСЂР°: ")
-			{
-				result.agreement = parseFloat(th.next().text());
-			}
-			else if(text == "Р‘Р°Р»Р°РЅСЃ: ")
-			{
-				var b = th.next().text();
-				b = b.substr(0, b.length - 3).trim();
-				result.balance = parseFloat(b);
-			}
-		}
-		
-	}
+  AnyBalance.setDefaultCharset('utf-8');
 
-    
-    
-    
+  if(AnyBalance.isAvailable('balance', 'agreement')){
 
-    AnyBalance.setResult(result);
+    checkEmpty(prefs.login, "Введите логин");
+    checkEmpty(prefs.password, "Введите пароль");
+    var info = AnyBalance.requestPost(baseurl + "login?next=/", {
+      login: prefs.login,
+      pass: prefs.password
+    });
+    var $parse = $('<div>' + info + '</div>');
+    var rows = $parse.find("#sidebar table:first tr");
+    if(rows.length == 0){
+      throw new AnyBalance.Error("Имя пользователя или пароль не верны");
+    }
+    var row = $(rows[0]);
+    result.agreement = row.find("td").text().trim();
+    row = $(rows[1]);
+    result.balance = Number(row.find("td").text().replace("р.","").trim());
+    row = $(rows[2]);
+    cells = row.find("td");
+    if(cells.length == 3){
+     result.details = [$(cells[0]).text().trim(), $(cells[1]).text().replace(/[ \r\n]+/g, " ").trim(), $(cells[2]).text().trim()].join(" ");
+    }
+    else if(cells.length == 2){
+     result.details = [$(cells[0]).text().trim(), $(cells[1]).text().trim()].join(" ");
+    }
+    else if(cells.length == 1){
+     result.details = $(cells[0]).text().trim();
+    }
+    else{
+     result.details = "";
+    }
+  }
+  AnyBalance.setResult(result);
 }
 

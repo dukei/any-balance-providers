2d21546684efaa9b480eb6b2d66433af693384da
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-01-15 12:43:09 +0000

    Банк УРАЛСИБ

--- trunk/ab-money-uralsib/main.js
+++ trunk/ab-money-uralsib/main.js
@@ -31,6 +31,14 @@ function getP_instance(html) {
 	return getParam(html, null, null, /p_instance[^>]*value=['"]([^'"]+)/i);
 }
 
+function getFlowID(html) {
+	return getParam(html, null, null, /value="([^"]+)[^<]*pFlowId/i);
+}
+
+function getFlowStepID(html) {
+	return getParam(html, null, null, /p_flow_step_id[^>]*value="([^"]+)/i);
+}
+
 function nvl(a, v, vl, vr) {
 	return a != undefined && a != null ? (vl != undefined && vl != null ? vl : '') + a + (vr != undefined && vr != null ? vr : '') : (v != undefined && v != null ? v : '')
 }
@@ -75,8 +83,8 @@ function doNewCabinet(prefs) {
 	
 	html = AnyBalance.requestPost(baseurl + 'wwv_flow.show', {
 		p_request:'APPLICATION_PROCESS=AUTHENTICATE',
-		p_flow_id:'10',
-		p_flow_step_id:'1000',
+		p_flow_id:getFlowID(html),
+		p_flow_step_id:getFlowStepID(html),
 		p_instance:p_instance,
 		x01:'AUTH#PASSWORD',
 		x02:prefs.login,
@@ -99,10 +107,11 @@ function doNewCabinet(prefs) {
 	if(!loginVar)
 		throw new AnyBalance.Error('Не удалось найти ссылку на страницу с данными, сайт изменен?');
 	
-	html = AnyBalance.requestGet(baseurl + 'f?p=10:MAIN:' + loginVar, g_headers);
+	var url = baseurl + 'f?p=10:MAIN:' + loginVar;
+	html = AnyBalance.requestGet(url, g_headers);
 	// Все, теперь можно разбирать данные
     if(prefs.type == 'acc')
-        fetchAcc(html, baseurl, prefs);
+        fetchAcc(html, baseurl, prefs, url);
     else
         fetchCard(html, baseurl, prefs);
 }
@@ -145,29 +154,48 @@ function fetchCard(html, baseurl, prefs) {
 	AnyBalance.setResult(result);
 }
 
-function fetchAcc(html, baseurl, prefs) {
-	html = AnyBalance.requestGet(baseurl + getParam(html, null, null, /href="([^"]+ACCOUNTS[^"]+)/i), g_headers);
+function fetchAcc(html, baseurl, prefs, url) {
+	var P_instance = getP_instance(html);
+	var FlowID = getFlowID(html);
+	var FlowStepID = getFlowStepID(html);
+	
+	var requests = ['APPLICATION_PROCESS=AFTER_AUTH', 'APPLICATION_PROCESS=GET_MAIL_COUNT', ''/*, '', 'APPLICATION_PROCESS=GET_CRM'*/];
+	// Запросы посылаются в цикле :)
+	for(var i = 0; i < requests.length; i++) {
+		var currentRequest = requests[i];
+		
+		html = AnyBalance.requestPost(baseurl + 'wwv_flow.show', {
+			'p_request':currentRequest,
+			'p_instance':getP_instance(html) || P_instance,
+			p_flow_id:getFlowID(html) || FlowID,
+			p_flow_step_id:getFlowStepID(html) || FlowStepID,		
+		}, addHeaders({
+			Referer: url,
+			'X-Requested-With': 'XMLHttpRequest'
+		}));		
+	}
 	
 	var lastdigits = prefs.lastdigits ? prefs.lastdigits : '\\d{4}';
-	
-	// <li[^>]*class(?:[^>]*>){25}\d{4}[-x]{8,}5821(?:[^>]*>){5}\s*</li>
-	var reCard = new RegExp('<li[^>]*class(?:[^>]*>){25}\\d{4}[-x]{8,}' + lastdigits + '(?:[^>]*>){5}\\s*</li>', 'i');
+	// <div[^>]*desc(?:[^>]*>){22}\s*\d{14,}6688(?:[^>]*>){1}\s*</div>
+	var reCard = new RegExp('<div[^>]*desc(?:[^>]*>){22}\\s*\\d{14,}' + lastdigits + '(?:[^>]*>){1}\\s*</div>', 'i');
 	
 	var tr = getParam(html, null, null, reCard);
 	if(!tr)
-		throw new AnyBalance.Error('Не удалось найти ' + (prefs.lastdigits ? 'карту с последними цифрами '+prefs.lastdigits : 'ни одной карты!'));
+		throw new AnyBalance.Error('Не удалось найти ' + (prefs.lastdigits ? 'счет с последними цифрами '+prefs.lastdigits : 'ни одного счета!'));
 	
 	var result = {success: true};
 	
-	getParam(tr, result, '__tariff', /(\d{4}[-x]{8,}\d{4})/i, replaceTagsAndSpaces);
-	getParam(result.__tariff, result, 'cardNumber');
-	getParam(tr, result, 'userName', /"profile-name"(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces);
-	getParam(tr, result, 'balance', /"sum"(?:[^>]*>){1}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(tr, result, ['currency', 'balance'], /"sum"(?:[^>]*>){1}([^<]+)/i, replaceTagsAndSpaces, parseCurrency);
-	getParam(tr, result, 'till', /\d{4}[-x]{8,}\d{4}[^<]*?(\d{1,2}\/\d{1,2})/i, [replaceTagsAndSpaces, /(.*)/i, '01/$1'], parseDate);
+	getParam(tr, result, '__tariff', /\d{20}/i, replaceTagsAndSpaces);
+	getParam(result.__tariff, result, 'accnum');
+	getParam(tr, result, 'acctype', /ITEM_ID[^>]*>([^<]+)/i, replaceTagsAndSpaces);
+	getParam(tr, result, 'balance', /ITEM_ID(?:[^>]*>){8}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(tr, result, ['currency', 'balance', 'debt'], /ITEM_ID(?:[^>]*>){10}([^<]+)/i, replaceTagsAndSpaces);
 	
-	// Дополнительная инфа по картам.
-	if (isAvailable(['status', 'accnum', 'acctype'])) {
+	if(isset(result.currency)) {
+		getParam(tr, result, 'debt', new RegExp('([-.,\\d]+)\\s*' + result.currency, 'i'), replaceTagsAndSpaces, parseBalance);
+	}
+	// Дополнительная инфа.
+	/*if (isAvailable(['status', 'accnum', 'acctype'])) {
 		var href = getParam(tr, null, null, /<a\s*href="([^"]*)/i);
 		if(href) {
 			html = AnyBalance.requestGet(baseurl + href, g_headers);
@@ -179,7 +207,7 @@ function fetchAcc(html, baseurl, prefs) {
 		} else {
 			AnyBalance.trace('Не нашли ссылку на дополнительную информацию по картам, возможно, сайт изменился?');
 		}
-	}
+	}*/
 	AnyBalance.setResult(result);
 }
 

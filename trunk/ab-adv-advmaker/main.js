de851b8e0e08d221160d0b80b90a488cd19d77a1
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-01-24 08:42:48 +0000

    AdvMaker

--- trunk/ab-adv-advmaker/main.js
+++ trunk/ab-adv-advmaker/main.js
@@ -20,22 +20,22 @@ function main(){
         'password_adv':prefs.password,
     }, addHeaders({Referer: baseurl + 'webmaster/'})); 
 
-    if(!/\/exit\//i.test(html)){
+    if(!/exit\//i.test(html)){
         var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 	
-	var table = getParam(html, null, null, /(<table[\s\S]*?>[\s\S]*?<\/table>)/i, null, null);
+	var table = getParam(html, null, null, /(<table[\s\S]*?>[\s\S]*?<\/table>)/i);
 	if(!table)
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+		throw new AnyBalance.Error('Не удалось найти таблицу с данными. Сайт изменен?');
 	
 	var result = {success: true};
-	getParam(html, result, 'balance', /Заработано на[\s\S]{15}([\s\S]*?)<\/strong>/i, null, parseBalance);
-	getParam(table, result, 'click_under', /Всего:(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, null, parseBalance);
-	getParam(table, result, 'slide_banner', /Всего:(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, null, parseBalance);
-	getParam(table, result, 'banners', /Всего:(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, null, parseBalance);
+	getParam(html, result, 'balance', [/Ваш баланс(?:[^>]*>){2}([^<]+)/i, /Заработано на[\s\S]{15}([\s\S]*?)<\/strong>/i], replaceTagsAndSpaces, parseBalance);
+	getParam(table, result, 'click_under', /Всего:(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(table, result, 'slide_banner', /Всего:(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(table, result, 'banners', /Всего:(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
 
     AnyBalance.setResult(result);
 }

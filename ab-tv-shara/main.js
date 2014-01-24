95af687732d5a0388ef4ec10e6edb4d188c7176e
stark027 <stark027@gmail.com>
2014-01-24 12:02:57 +0000

    Новый провайдер: Shara.tv

---
+++ ab-tv-shara/main.js
@@ -0,0 +1,88 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.shara.tv/';
+	AnyBalance.setDefaultCharset('windows-1251');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'login') 
+			return prefs.login;
+		else if (name == 'pass')
+			return prefs.password;
+
+		return value;
+	});
+	
+	var captchaa;
+	if(AnyBalance.getLevel() >= 7) {
+		AnyBalance.trace('Пытаемся ввести капчу');
+		var captcha = AnyBalance.requestGet(baseurl+ getParam(html, null, null, /(images\/kcaptcha[^"]+)/i));
+		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaa);
+	} else {
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}
+	
+	params.keystring = captchaa;
+	
+	html = AnyBalance.requestPost(baseurl, params, addHeaders({Referer: baseurl}));
+	
+	if (!/Перенаправляемся на/i.test(html)) {
+		var error = getParam(html, null, null, /"red"[^>]*>(ОШИБКА[^<.]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	html = AnyBalance.requestGet(baseurl + 'index.php', g_headers);
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Денег на счету([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	
+	var packageTable = getParam(html, null, null, /<table[^>]*class="stat_tb"[\s\S]*?<\/table>/i);
+	if(packageTable) {
+		var array = sumParam(html, null, null, /<tr(?:[^>]*>){6}\d{4}(?:[^>]*>){5,}\s*<\/tr>/ig);
+		for(var i = 0; i < array.length; i++) {
+			AnyBalance.trace(array[i]);
+			sumParam(array[i], result, 'deadline', /(?:[^>]*>){6}([^<]+)/i, replaceTagsAndSpaces, parseDateGibdd, aggregate_min);
+		}
+	} else {
+		AnyBalance.trace('Нет ни одного подключенного пакета.');
+	}
+	
+	
+	AnyBalance.setResult(result);
+}
+
+/** Получает дату из строки, почему-то parseDateISO на устройстве не может распарсить вот такую дату 2013-11-23 21:16:00 */
+function parseDateGibdd(str){
+	//new Date(year, month, date[, hours, minutes, seconds, ms] )
+	//2013-11-23 21:16:00
+
+    var matches = /(\d{4})\D(\d{2})\D(\d{2})\D(\d{1,2}):(\d{1,2})/.exec(str);
+    if(matches){
+          var date = new Date(matches[1], matches[2]-1, +matches[3], matches[4] || 0, matches[5] || 0, matches[6] || 0);
+	  var time = date.getTime();
+          AnyBalance.trace('Parsing date ' + date + ' from value: ' + str);
+          return time;
+    }
+    AnyBalance.trace('Failed to parse date from value: ' + str);
+}

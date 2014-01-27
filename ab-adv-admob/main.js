1d8ead5c60bfbcb28969c024fab749ab41666468
bazhanov.artem <bazhanov.artem@gmail.com>
2014-01-27 17:06:44 +0000

    Попробовал реализовать информацию из AdMob... Получилось не очень прямо, но все же работает. Если есть возможность и видите необходимость - подправьте, пожалуйста.

---
+++ ab-adv-admob/main.js
@@ -0,0 +1,355 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Данные из кабинета AdMob.
+Провайдер получает эти данные из личного Кабинета. Для работы требуется указать в настройках e-mail и пароль.
+*/
+
+var g_headers = {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Connection':'keep-alive',
+    //Мобильный браузер хотим
+    'User-Agent':'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+}
+
+function getParam (html, result, param, regexp, replaces, parser) {
+	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
+		return;
+
+	var value = regexp.exec (html);
+	if (value) {
+		value = value[1];
+		if (replaces) {
+			for (var i = 0; i < replaces.length; i += 2) {
+				value = value.replace (replaces[i], replaces[i+1]);
+			}
+		}
+		if (parser)
+			value = parser (value);
+
+    if(param)
+      result[param] = value;
+    else
+      return value
+	}
+}
+
+var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
+var replaceFloat = [/\s+/g, '', /,/g, '.'];
+
+function parseBalance(text){
+    var val = getParam(html_entity_decode(text).replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
+    return val;
+}
+
+/**
+ * Заменяет HTML сущности в строке на соответствующие им символы
+ */
+ function html_entity_decode (string) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: john (http://www.jd-tech.net)
+  // +      input by: ger
+  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   bugfixed by: Onno Marsman
+  // +   improved by: marc andreu
+  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +      input by: Ratheous
+  // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: Nick Kolosov (http://sammy.ru)
+  // +   bugfixed by: Fox
+  // -    depends on: get_html_translation_table
+  // *     example 1: html_entity_decode('Kevin &amp; van Zonneveld');
+  // *     returns 1: 'Kevin & van Zonneveld'
+  // *     example 2: html_entity_decode('&amp;lt;');
+  // *     returns 2: '&lt;'
+  var hash_map = {},
+    symbol = '',
+    tmp_str = '',
+    entity = '';
+  tmp_str = string.toString();
+  var quote_style = '';
+  if (false === (hash_map = get_html_translation_table('HTML_ENTITIES', quote_style))) {
+    return false;
+  }
+
+  // fix &amp; problem
+  // http://phpjs.org/functions/get_html_translation_table:416#comment_97660
+  delete(hash_map['&']);
+  hash_map['&'] = '&amp;';
+
+  for (symbol in hash_map) {
+    entity = hash_map[symbol];
+    tmp_str = tmp_str.split(entity).join(symbol);
+  }
+  tmp_str = tmp_str.split('&#039;').join("'");
+
+  return tmp_str;
+}
+function get_html_translation_table (table, quote_style) {
+  // http://kevin.vanzonneveld.net
+  // +   original by: Philip Peterson
+  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // +   bugfixed by: noname
+  // +   bugfixed by: Alex
+  // +   bugfixed by: Marco
+  // +   bugfixed by: madipta
+  // +   improved by: KELAN
+  // +   improved by: Brett Zamir (http://brett-zamir.me)
+  // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+  // +      input by: Frank Forte
+  // +   bugfixed by: T.Wild
+  // +      input by: Ratheous
+  // %          note: It has been decided that we're not going to add global
+  // %          note: dependencies to php.js, meaning the constants are not
+  // %          note: real constants, but strings instead. Integers are also supported if someone
+  // %          note: chooses to create the constants themselves.
+  // *     example 1: get_html_translation_table('HTML_SPECIALCHARS');
+  // *     returns 1: {'"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;'}
+  var entities = {},
+    hash_map = {},
+    decimal;
+  var constMappingTable = {},
+    constMappingQuoteStyle = {};
+  var useTable = {},
+    useQuoteStyle = {};
+
+  // Translate arguments
+  constMappingTable[0] = 'HTML_SPECIALCHARS';
+  constMappingTable[1] = 'HTML_ENTITIES';
+  constMappingQuoteStyle[0] = 'ENT_NOQUOTES';
+  constMappingQuoteStyle[2] = 'ENT_COMPAT';
+  constMappingQuoteStyle[3] = 'ENT_QUOTES';
+
+  useTable = !isNaN(table) ? constMappingTable[table] : table ? table.toUpperCase() : 'HTML_SPECIALCHARS';
+  useQuoteStyle = !isNaN(quote_style) ? constMappingQuoteStyle[quote_style] : quote_style ? quote_style.toUpperCase() : 'ENT_COMPAT';
+
+  if (useTable !== 'HTML_SPECIALCHARS' && useTable !== 'HTML_ENTITIES') {
+    throw new Error("Table: " + useTable + ' not supported');
+    // return false;
+  }
+
+  entities['38'] = '&amp;';
+  if (useTable === 'HTML_ENTITIES') {
+    entities['160'] = '&nbsp;';
+    entities['161'] = '&iexcl;';
+    entities['162'] = '&cent;';
+    entities['163'] = '&pound;';
+    entities['164'] = '&curren;';
+    entities['165'] = '&yen;';
+    entities['166'] = '&brvbar;';
+    entities['167'] = '&sect;';
+    entities['168'] = '&uml;';
+    entities['169'] = '&copy;';
+    entities['170'] = '&ordf;';
+    entities['171'] = '&laquo;';
+    entities['172'] = '&not;';
+    entities['173'] = '&shy;';
+    entities['174'] = '&reg;';
+    entities['175'] = '&macr;';
+    entities['176'] = '&deg;';
+    entities['177'] = '&plusmn;';
+    entities['178'] = '&sup2;';
+    entities['179'] = '&sup3;';
+    entities['180'] = '&acute;';
+    entities['181'] = '&micro;';
+    entities['182'] = '&para;';
+    entities['183'] = '&middot;';
+    entities['184'] = '&cedil;';
+    entities['185'] = '&sup1;';
+    entities['186'] = '&ordm;';
+    entities['187'] = '&raquo;';
+    entities['188'] = '&frac14;';
+    entities['189'] = '&frac12;';
+    entities['190'] = '&frac34;';
+    entities['191'] = '&iquest;';
+    entities['192'] = '&Agrave;';
+    entities['193'] = '&Aacute;';
+    entities['194'] = '&Acirc;';
+    entities['195'] = '&Atilde;';
+    entities['196'] = '&Auml;';
+    entities['197'] = '&Aring;';
+    entities['198'] = '&AElig;';
+    entities['199'] = '&Ccedil;';
+    entities['200'] = '&Egrave;';
+    entities['201'] = '&Eacute;';
+    entities['202'] = '&Ecirc;';
+    entities['203'] = '&Euml;';
+    entities['204'] = '&Igrave;';
+    entities['205'] = '&Iacute;';
+    entities['206'] = '&Icirc;';
+    entities['207'] = '&Iuml;';
+    entities['208'] = '&ETH;';
+    entities['209'] = '&Ntilde;';
+    entities['210'] = '&Ograve;';
+    entities['211'] = '&Oacute;';
+    entities['212'] = '&Ocirc;';
+    entities['213'] = '&Otilde;';
+    entities['214'] = '&Ouml;';
+    entities['215'] = '&times;';
+    entities['216'] = '&Oslash;';
+    entities['217'] = '&Ugrave;';
+    entities['218'] = '&Uacute;';
+    entities['219'] = '&Ucirc;';
+    entities['220'] = '&Uuml;';
+    entities['221'] = '&Yacute;';
+    entities['222'] = '&THORN;';
+    entities['223'] = '&szlig;';
+    entities['224'] = '&agrave;';
+    entities['225'] = '&aacute;';
+    entities['226'] = '&acirc;';
+    entities['227'] = '&atilde;';
+    entities['228'] = '&auml;';
+    entities['229'] = '&aring;';
+    entities['230'] = '&aelig;';
+    entities['231'] = '&ccedil;';
+    entities['232'] = '&egrave;';
+    entities['233'] = '&eacute;';
+    entities['234'] = '&ecirc;';
+    entities['235'] = '&euml;';
+    entities['236'] = '&igrave;';
+    entities['237'] = '&iacute;';
+    entities['238'] = '&icirc;';
+    entities['239'] = '&iuml;';
+    entities['240'] = '&eth;';
+    entities['241'] = '&ntilde;';
+    entities['242'] = '&ograve;';
+    entities['243'] = '&oacute;';
+    entities['244'] = '&ocirc;';
+    entities['245'] = '&otilde;';
+    entities['246'] = '&ouml;';
+    entities['247'] = '&divide;';
+    entities['248'] = '&oslash;';
+    entities['249'] = '&ugrave;';
+    entities['250'] = '&uacute;';
+    entities['251'] = '&ucirc;';
+    entities['252'] = '&uuml;';
+    entities['253'] = '&yacute;';
+    entities['254'] = '&thorn;';
+    entities['255'] = '&yuml;';
+  }
+
+  if (useQuoteStyle !== 'ENT_NOQUOTES') {
+    entities['34'] = '&quot;';
+  }
+  if (useQuoteStyle === 'ENT_QUOTES') {
+    entities['39'] = '&#39;';
+  }
+  entities['60'] = '&lt;';
+  entities['62'] = '&gt;';
+
+
+  // ascii decimals to real symbols
+  for (decimal in entities) {
+    if (entities.hasOwnProperty(decimal)) {
+      hash_map[String.fromCharCode(decimal)] = entities[decimal];
+    }
+  }
+
+  return hash_map;
+}
+/*function html_entity_decode(str)
+{
+	//return str;
+    //jd-tech.net
+    var tarea = document.createElement('textarea');
+    tarea.innerHTML = str;
+    return tarea.value;
+}*/
+
+function createFormParams(html, process){
+    var params = {};
+    html.replace(/<input[^>]+name="([^"]*)"[^>]*>/ig, function(str, name){
+        var value = getParam(str, null, null, /value="([^"]*)"/i, null, html_entity_decode);
+        name = html_entity_decode(name);
+        if(process){
+            value = process(params, str, name, value);
+        }
+        params[name] = value;
+    });
+    return params;
+}
+
+function addZero(i) {
+	return (i<10)?"0"+i:i;
+}
+
+function main() {
+
+	var result = {
+        success: true
+    };
+
+    var prefs = AnyBalance.getPreferences();
+    
+    AnyBalance.setDefaultCharset('utf-8');
+
+	var baseurl = "https://accounts.google.com/ServiceLogin?service=admob&rm=hide&nui=15&alwf=true&ltmpl=default&passive=false&continue=https%3A%2F%2Fapps.admob.com%2Fadmob%2Fsignup%3Fsubid%3Dww-ww-et-admob2%26_adc%3Dww-ww-et-admob2%26hl%3Den&followup=https%3A%2F%2Fapps.admob.com%2Fadmob%2Fsignup%3Fsubid%3Dww-ww-et-admob2%26_adc%3Dww-ww-et-admob2%26hl%3Den&migrate=true&skipvpage=false&hl=en_US";
+	var baseurlLogin = "https://accounts.google.com/";
+    
+    var html = AnyBalance.requestGet(baseurl, g_headers);
+    var params = createFormParams(html, function(params, input, name, value){
+        var undef;
+        if(name == 'Email')
+            value = prefs.login;
+        else if(name == 'Passwd')
+            value = prefs.password;
+        else if(name == 'PersistentCookie')
+            value = undef; //Снимаем галочку
+       
+        return value;
+    });
+    
+    //AnyBalance.trace(JSON.stringify(params));
+
+    var html = AnyBalance.requestPost(baseurlLogin + 'ServiceLoginAuth', params, g_headers);
+
+	/*
+    if(!/authenticatedUserName/i.test(html)){
+        var error = getParam(html, null, null, /<span[^>]+class="errormsg[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+        if(error)
+            throw new AnyBalance.Error(error);
+        error = getParam(html, null, null, /(<form[^>]+name="verifyForm")/i);
+        if(error)
+            throw new AnyBalance.Error("This account requires 2-step authorization. Turn off 2-step authorization to use this provider.");
+        throw new AnyBalance.Error('Can not log in google account.');
+    }*/
+
+	html = AnyBalance.requestGet('https://apps.admob.com/', g_headers);
+	//AnyBalance.trace(html);
+    if(!/,"\/logout",/i.test(html)){
+        throw new AnyBalance.Error('Авторизация не удалась.');
+    }
+    
+    var date = new Date();
+    
+    html = AnyBalance.requestPost('https://apps.admob.com/monetize-reports', {
+			t:'ADMOB_NETWORK',
+			rs:'{"1":{"1":1},"2":[{"1":5,"2":'+(date.getFullYear()+addZero(date.getMonth()+1)+"01")+',"3":'+(date.getFullYear()+addZero(date.getMonth()+1)+addZero(date.getDate()))+'}],"3":[1]}',
+			cc:'USD'
+		}, g_headers);
+		
+	AnyBalance.trace(html);
+
+	var lines = html.split("\n");
+	if(lines.length>1) {
+	AnyBalance.trace(lines);
+		var balance=0;
+		for(i=1;i<lines.length;i++) {
+			var parts=lines[i].split("\t");
+			if(parts && parts.length==9) {
+				AnyBalance.trace(parts);
+				var tmp=parts[8];
+				tmp=tmp.replace(',','.');
+				tmp=tmp.substring(1,tmp.length-1);
+				balance+=parseFloat(tmp);
+			}
+		}
+		result.month=balance;
+	}
+
+    AnyBalance.setResult(result);
+}

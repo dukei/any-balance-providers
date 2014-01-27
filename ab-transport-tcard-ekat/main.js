8b3b46fb1b514c1eb3edf5934f80113a9b363ba0
stark027 <stark027@gmail.com>
2014-01-27 14:30:12 +0000

    Новый провайдер: Транспортная карта (Екатеринбург)

---
+++ ab-transport-tcard-ekat/main.js
@@ -0,0 +1,54 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://albank.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите номер карты!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'ru/transp_card/', g_headers);
+	
+	var captchaa;
+	var captcha_code = getParam(html, null, null, /captcha_code=([^"]*)/i);
+	if(AnyBalance.getLevel() >= 7) {
+		AnyBalance.trace('Пытаемся ввести капчу');
+		var captcha = AnyBalance.requestGet(baseurl + 'bitrix/tools/captcha.php?captcha_code=' + captcha_code);
+		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaa);
+	} else {
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}	
+	
+	html = AnyBalance.requestPost(baseurl + 'ru/transp_card/', {
+		'cardType':32,
+		'cardNumber':prefs.login,
+		'captcha_code':captcha_code,
+		captcha_word:captchaa,
+	}, addHeaders({Referer: baseurl + 'ru/transp_card/'}));
+	
+	if (!/alert-success/i.test(html)) {
+		var error = getParam(html, null, null, /alert-error">([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, '__tariff', /Баланс карты[\s']+(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Баланс карты[^<]*составляет([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

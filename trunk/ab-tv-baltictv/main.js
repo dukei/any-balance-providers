2e6f343f070498fe26082ef351d13158cac628e7
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-01-30 16:04:59 +0000

    Новый провайдер: Baltic TV

---
+++ trunk/ab-tv-baltictv/main.js
@@ -0,0 +1,52 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://core.baltictv.net/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'ca_login.php', g_headers);
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'login') 
+			return prefs.login;
+		else if (name == 'password')
+			return prefs.password;
+
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + 'ca_login.php', {
+		login: prefs.login,
+		pass: prefs.password,
+	}, addHeaders({Referer: baseurl + 'ca_login.php'}));
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /В кошельке(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['currency', 'balance'], /В кошельке(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseCurrency);
+	getParam(html, result, 'status', /Статус(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

a417b0b4df14ac9083e75ccfd47adf2ee9a029ba
stark027 <stark027@gmail.com>
2014-02-06 17:02:19 +0000

    Домашний Интернет и Телевидение МТС

--- ab-internet-mts/main.js
+++ ab-internet-mts/main.js
@@ -745,10 +745,13 @@ function getBarnaul(){
 	});
 	json = getJson(html);
 	html = JSON.stringify(json);
+	
+	AnyBalance.trace(html);
+	
     var result = {success: true};
     //Вначале попытаемся найти активный тариф
     getParam(html, result, '__tariff', /Name[\s\S]{1,20}Тариф\s*'([\s\S]*?)'/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'balance', /ClientId":"","Name":"[\s\S]*?(-?\d[\d\s]*[.,]?\d*[.,]?\d*)\s*руб/i, replaceTagsAndSpaces, parseBalance2);
+	getParam(html, result, 'balance', /\(([\d.\-,]+)[^(]*руб/i, replaceTagsAndSpaces, parseBalance2);
 
 /*	getParam(html, result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
     getParam(html, result, 'internet_cur', /Израсходовано:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);

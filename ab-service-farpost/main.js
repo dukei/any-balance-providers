845a1d4d8d5fb4ea86d08656e2d833774bd581cd
stark027 <stark027@gmail.com>
2014-02-06 16:29:07 +0000

    Farpost (Реклама)

--- ab-service-farpost/main.js
+++ ab-service-farpost/main.js
@@ -7,7 +7,6 @@ var g_headers = {
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36',
 };
 function parseBalanceRK(_text){
@@ -21,9 +20,12 @@ function parseBalanceRK(_text){
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'http://moskva.farpost.ru/';
+    var baseurl = 'https://moskva.farpost.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
-
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     var html = AnyBalance.requestGet(baseurl + 'sign?return=%2F', g_headers);
 
 	html = AnyBalance.requestPost(baseurl + 'sign?return=%2F', {

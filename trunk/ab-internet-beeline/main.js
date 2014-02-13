31e156b8e87639d0e6d6805fbef4f68d106dbb1d
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-02-13 09:36:01 +0000

    Beeline интернет

--- trunk/ab-internet-beeline/main.js
+++ trunk/ab-internet-beeline/main.js
@@ -1,18 +1,80 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
-function parseTrafficGb(str) {
-	var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-	return parseFloat((val / 1000).toFixed(2));
-}
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	AnyBalance.setDefaultCharset('utf-8');
-	var baseurl = "https://lk.beeline.ru/";
 	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	if(prefs.type == 'cab') {
+		proceedCab(prefs);
+	} else {
+		proceedLk(prefs);
+	}
+}
+
+function requestJson(url, data, headers) {
+	var json = getJson(AnyBalance.requestPost(url, data, headers));
+	
+	if(!json.success) {
+		AnyBalance.trace(JSON.stringify(json));
+		throw new AnyBalance.Error('Возникла ошибка при выполнении запроса: ' + json.error);
+	}
+	
+	return json;
+}
+
+function proceedCab(prefs) {
+	var baseurl = 'https://cabinet.beeline.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	var html = AnyBalance.requestGet(baseurl + 'lk', g_headers);
+	
+	var json = requestJson(baseurl + 'lk/ajax.php', {
+		module:'bee_lk.auth',
+		action:'login',
+		v_login: prefs.login,
+		v_password: prefs.password,
+	}, addHeaders({Referer: baseurl + 'lk'}));
+	
+	var result = {success: true};
+	
+	// Интернет
+	var current = {empty:true};
+	for(var i = 0; i < json.client.net.nums.length; i++) {
+		current = json.client.net.nums[i];
+		
+		var state = current['v_nmstatus'];
+		if(/Активeн/i.test(state)) {
+			AnyBalance.trace('Нашли активный счет: ' + current['v_nmbillgroup']);
+			break;
+		} else {
+			AnyBalance.trace('Cчет: ' + current['v_nmbillgroup'] + ' не активен');
+		}
+	}
+	if(current.empty)
+		throw new AnyBalance.Error('Не удалось найти ни одного активного счета, сайт изменен?');
+	
+	getParam(current['v_saldo'], result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+	getParam(current['v_nmbplan'], result, '__tariff', null, replaceTagsAndSpaces, html_entity_decode);
+	getParam(current['v_nmbillgroup'], result, 'bill', null, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}
+
+function proceedLk(prefs) {
+	AnyBalance.setDefaultCharset('utf-8');
+	var baseurl = "https://lk.beeline.ru/";
 	
 	if (!prefs.__dbg) {
 		var html = AnyBalance.requestGet(baseurl); //Чтобы кука установилась
@@ -56,4 +118,9 @@ function main() {
 	getParam(html, result, 'traffic', /Предоплаченный трафик[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
 	
 	AnyBalance.setResult(result);
+}
+
+function parseTrafficGb(str) {
+	var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
+	return parseFloat((val / 1000).toFixed(2));
 }

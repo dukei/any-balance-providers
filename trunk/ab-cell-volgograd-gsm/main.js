aba8288a00865f5a49e513b7328e02f33a476c32
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-02-19 13:34:30 +0000

    Волгоград-GSM (Ростелеком)

--- trunk/ab-cell-volgograd-gsm/main.js
+++ trunk/ab-cell-volgograd-gsm/main.js
@@ -1,17 +1,12 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Текущий баланс у сотового оператора НТК.
-
-сайт оператора: http://www.vntc.ru
-ИССА оператора: https://issa.vntc.ru
 */
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     
-//    var baseurl = "https://issa.volgogsm.ru/cgi-bin/cgi.exe?";
-    var baseurl = "https://80.69.146.12/issa/cgi-bin/cgi.exe?";
+    var baseurl = "https://issa.volgogsm.ru/cgi-bin/cgi.exe?";
+//    var baseurl = "https://80.69.146.12/issa/cgi-bin/cgi.exe?";
 
     AnyBalance.trace("Trying to enter issa at address: " + baseurl + "function=is_login");
     var html = AnyBalance.requestPost(baseurl + "function=is_login", {
@@ -19,28 +14,38 @@ function main(){
         Password: prefs.password
     });
     
-    var error = getParam(html, null, null, /<table[^>]+class=['"]?centorize-td[^>]*>([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, html_entity_decode);
-    if(error){
-        throw new AnyBalance.Error(error);
-    }
-
+	if (/Войдите в систему ещё раз|Ошибка аутентификации/i.test(html)) {
+		var error = getParam(html, null, null, [/<table[^>]+class=['"]?centorize-td[^>]*>([\s\S]*?)<\/tr>/i, /class=error[^>]*>([^<]+)/i], replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /неверный пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	html = AnyBalance.requestGet(baseurl + "function=is_account");
+	
     var result = {success: true};
-    
-    html = AnyBalance.requestGet(baseurl + "function=is_account");
-    if(!/\?function=is_exit/i.test(html)){
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-
-    getParam(html, result, 'ActualBalance', /Актуальный баланс:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	
+	getParam(html, result, 'ActualBalance', /Актуальный баланс:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'DayRashod', /Средняя скорость расходования средств по лицевому счету в день:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'BonusBalance', /Количество бонусных баллов на счету:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'MinBalabce', /Минимальный баланс для подключения:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'DateOff', /Предположительная дата отключения без поступления средств менее, чем через:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-
-    sumParam(html, result, 'sms', /остаток Бонус (?:&quot;|")\d+ SMS(?:&quot;|") составляет (\d+) SMS/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-    sumParam(html, result, 'sms', /у Вас остаток (\d+) для услуги Бонус (?:&quot;|")\d+ SMS(?:&quot;|")/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-    sumParam(html, result, 'min', /накоплено исходящий трафик составляет (\d+) мин/i, replaceTagsAndSpaces, parseBalance, aggregate_sum);
-
+	
+    sumParam(html, result, 'sms', [/остаток Бонус (?:&quot;|")\d+ SMS(?:&quot;|") составляет (\d+) SMS/i,/у Вас остаток (\d+) для услуги Бонус (?:&quot;|")\d+ SMS(?:&quot;|")/i,/остаток\s+лимита\s+составляет([\s\d]+)SMS/i], replaceTagsAndSpaces, parseBalance, aggregate_sum);
+    sumParam(html, result, 'min', [/накоплено исходящий трафик составляет (\d+) мин/i,/остаток\s+лимита\s+составляет([\s\d]+)мин/i], replaceTagsAndSpaces, parseBalance, aggregate_sum);
+	
+	var traficStr = getParam(html, null, null, /лимита\s+GPRS\s+Остаток([^<]+Б)/i);
+	if(traficStr) {
+		// мы получили строку вида: 1 ГБ 9 MБ 208 KБ 0 Б 
+		var traf = sumParam(traficStr, null, null, /([\s\d]+(?:М|M|K|К|Г|G)?(?:B|Б))/ig);
+		for(var i = 0; i < traf.length; i++) {
+			var current = traf[i];
+			sumParam(current, result, 'trafic', null, replaceTagsAndSpaces, parseTraffic, aggregate_sum);
+		}
+	}
+	
     getParam(html, result, '__tariff', /issa\/tariff\.gif[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode); 
     
     AnyBalance.setResult(result);

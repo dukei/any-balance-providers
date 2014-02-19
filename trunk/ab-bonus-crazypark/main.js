6b5f748a26d6215b0a0bf0bf6838b6dd472171d1
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-02-19 14:10:15 +0000

    Crazy Park

--- trunk/ab-bonus-crazypark/main.js
+++ trunk/ab-bonus-crazypark/main.js
@@ -1,10 +1,5 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает информацию по бонусной карте CrazyPark
-
-Сайт оператора: http://www.crazypark.ru
-Личный кабинет: http://www.crazypark.ru/club/index.php
 */
 
 var g_headers = {
@@ -16,43 +11,39 @@ var g_headers = {
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
-
+	
     var baseurl = "http://www.crazypark.ru/client/";
-
+	
     var html = AnyBalance.requestGet(baseurl + 'login?id=' + encodeURIComponent(prefs.login) + '&password=' + encodeURIComponent(prefs.password), g_headers);
-
+	
     var json = getJson(html);
-
-    //AnyBalance.trace(html);
+	
     if(!json.success){
         if(json.error)
             throw new AnyBalance.Error(json.error);
+		
+		AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
-
+	
     var result = {success: true};
-
+	
     html = AnyBalance.requestGet(baseurl + 'index?id=' + encodeURIComponent(prefs.login), g_headers);
     json = getJson(html);
-
+	
     if(!json.clients[0])
         throw new AnyBalance.Error('Не найдена информация о клиенте! Сайт изменен?');
-
+	
     var client = json.clients[0];
-    if(AnyBalance.isAvailable('balance'))
-        result.balance = client.balance.CashBalance;
-    if(AnyBalance.isAvailable('bonuses'))
-        result.bonuses = client.balance.CashBonusBalance;
-    if(AnyBalance.isAvailable('crazies'))
-        result.crazies = client.balance.PointBalance;
-    if(AnyBalance.isAvailable('next'))
-        result.next = client.balance.CashToNextLevel;
-    if(AnyBalance.isAvailable('cardnum'))
-        result.cardnum = client.card.id;
-    if(AnyBalance.isAvailable('level'))
-        result.level = client.card.status;
-
+	
+	getParam(client.balance.CashBalance, result, 'balance');
+	getParam(client.balance.CashBonusBalance, result, 'bonuses');
+	getParam(client.balance.PointBalance, result, 'crazies');
+	getParam(client.balance.CashToNextLevel, result, 'next');
+	getParam(client.card.id, result, 'cardnum');
+	getParam(client.card.status, result, 'level');
+	
     result.__tariff = client.card.name + ', ' + client.card.status + ' ур., №' + client.card.id;
-
+	
     AnyBalance.setResult(result);
 }

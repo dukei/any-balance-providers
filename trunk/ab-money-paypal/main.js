1cd190e9581d37b9f15bac9726fa46ee49a05beb
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-02-25 11:12:21 +0000

    PayPal

--- trunk/ab-money-paypal/main.js
+++ trunk/ab-money-paypal/main.js
@@ -1,11 +1,7 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает текущий остаток в PayPal
-
-Сайт оператора: https://www.paypal.com/ru
-Личный кабинет: https://www.paypal.com/ru
 */
+
 var g_headers = {
 	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
@@ -18,52 +14,52 @@ var g_headers = {
 function followLink(html, signature){
 	var re = new RegExp('<a[^>]+href="([^"]*' + signature + ')"[^<]*>', 'i');
 	var href = getParam(html, null, null, re, null, html_entity_decode);
-	if(!href)
+	if(!href) {
+		AnyBalance.trace(html_entity_decode(html));
 		throw new AnyBalance.Error('Can not find reference ' + signature + '. Is the site changed?');
+	}
 	return AnyBalance.requestGet(href, g_headers);
 }
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-
-    if(!prefs.login)
-        throw new AnyBalance.Error("Enter e-mail!");
-    if(!prefs.password)
-        throw new AnyBalance.Error("Enter password!");
-
-    var baseurl = 'https://mobile.paypal.com/cgi-bin/wapapp';
-    var html = AnyBalance.requestGet(baseurl, g_headers);
-
-    html = followLink(html, 'view_balance.x=');
-
-    var form = getParam(html, null, null, /<form[^>]+name="Login"[\s\S]*?<\/form>/i);
-    if(!form)
-        throw new AnyBalance.Error("Can not find login form! Site is changed?");
-
+	
+	checkEmpty(prefs.login, 'Enter e-mail!');
+	checkEmpty(prefs.password, 'Enter password!');
+    
+	var baseurl = 'https://mobile.paypal.com/cgi-bin/wapapp';
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	html = followLink(html, 'view_balance.x=');
+	
+	var form = getParam(html, null, null, /<form[^>]+name="Login"[\s\S]*?<\/form>/i);
+	if(!form)
+        throw new AnyBalance.Error('Can not find login form! Site is changed?');
+	
     var params = createFormParams(form);
     params.login_email = prefs.login;
     params.login_password = prefs.password;
     
     var action = getParam(form, null, null, /<form[^>]+action="([^"]*)/i, null, html_entity_decode);
     html = AnyBalance.requestPost(action, params, g_headers);
-
-    if(!/cmd=_wapapp-logout/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]+id="crit"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Can not login to PayPal. Is the site changed?');
-    }
-    if(!/<\/h4>([^<]*)<hr/i.test(html)){
+	
+	if (!/cmd=_wapapp-logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+id="crit"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html_entity_decode(html));
+		throw new AnyBalance.Error('Can not login to PayPal. Is the site changed?');
+	}
+	if(!/<\/h4>([^<]*)<hr/i.test(html)){
+		AnyBalance.trace(html);
         throw new AnyBalance.Error('Can not find PayPal balance. Is the site changed?');
     }
-
-    var result={success: true};
-
-    getParam(html, result, ['currency','balance'], /<\/h4>([^<]*)<hr/i, replaceTagsAndSpaces, parseCurrency);
+	
+	var result = {success: true};
+	
+	getParam(html, result, ['currency','balance'], /<\/h4>([^<]*)<hr/i, [replaceTagsAndSpaces, /in/i, ''], parseCurrency);
     getParam(html, result, 'balance', /<\/h4>([^<]*)<hr/i, replaceTagsAndSpaces, parseBalance);
-
-	
-	
 	
     AnyBalance.setResult(result);
 }

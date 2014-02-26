059eaaa8334afed639725fb96c4ee1ff9bf06059
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-02-26 09:04:45 +0000

    Казгорсеть

--- trunk/ab-internet-kazgorset/main.js
+++ trunk/ab-internet-kazgorset/main.js
@@ -1,72 +1,69 @@
 /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Провайдер Казгорсеть
-Сайт оператора: http://kazgorset.ru/
 */
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
-
-	var baseurl = 'https://bill.kazgorset.ru/client/stat1/bgbilling/webexecuter';
+	checkEmpty(prefs.user, 'Введите логин!');
+	checkEmpty(prefs.pswd, 'Введите пароль!');
+	
 	AnyBalance.setDefaultCharset('utf-8');
-
-
-
-//разлогиниться из кабинета 
-//	var outinfo = AnyBalance.requestGet(baseurl+'?action=Exit&mid=contract');
-
-
-// Авторизуемся
-	AnyBalance.trace('Authorizing by ' + baseurl);
-	var info = AnyBalance.requestPost(baseurl, {
+	var baseurl = 'https://bill.kazgorset.ru/client/stat1/bgbilling/webexecuter';
+	//разлогиниться из кабинета
+	if(prefs.dbg) {
+		AnyBalance.trace('Дебаг: выходдим из кабинета...');
+		var outinfo = AnyBalance.requestGet(baseurl + '?action=Exit&mid=contract');
+	}
+	// Авторизуемся
+	AnyBalance.trace('Authorizing: ' + baseurl);
+	var html = AnyBalance.requestPost(baseurl, {
 		"user": prefs.user,
 		"pswd": prefs.pswd
 	});
-    
-	var error = $('#errHolder', info).text();
-	if(error){
-		throw new AnyBalance.Error(error);
+	
+	if (!/action=Exit/i.test(html)) {
+		var error = getParam(html, null, null, [/<h2>(ОШИБКА[^<]+)/i, /<error>\n(.*?)\n<\/error>/i], replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неправильный пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
-    
 	var result = {success: true};
-	if(matches = info.match(/<error>\n(.*?)\n<\/error>/i)){
-		throw new AnyBalance.Error(matches[1]);
-	}
-
-	result.__tariff=prefs.user;
-
-//запрос баланса
-	if(AnyBalance.isAvailable('balance')){
+	
+	result.__tariff = prefs.user;
+	//запрос баланса
+	if(AnyBalance.isAvailable('balance')) {
 		AnyBalance.trace('Getting balance by ' + baseurl + '?action=ShowBalance&mid=contract');
-		info = AnyBalance.requestGet(baseurl + '?action=ShowBalance&mid=contract');
-
-		if(matches = info.match(/Исходящий остаток на конец месяца<\/th><td>(.*?)<\/td>/i)){
-			result.balance = matches[1];
-		}
+		html = AnyBalance.requestGet(baseurl + '?action=ShowBalance&mid=contract');
+		
+		getParam(html, result, 'balance', /Исходящий остаток на конец месяца([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
 	}
-
-//module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month=6&year=2012&day_from=1&day_to=31
-
-	if(AnyBalance.isAvailable('traffic_outer') || AnyBalance.isAvailable('uptime') || AnyBalance.isAvailable('price')){
-		var d = new Date();var n=d.getMonth()+1;var y=d.getYear()+1900;
-
-		AnyBalance.trace('Getting uptime and traffic by ' +n+'/'+y+' (' + baseurl + '?module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month='+n+'&year='+y+'&day_from=1&day_to=31)');
-
-		info = AnyBalance.requestGet(baseurl + '?module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month='+n+'&year='+y+'&day_from=1&day_to=31');
-		if(matches = info.match(/<td>Итого:<\/td><td>.*?<\/td><td>(.*?)\s+\[.*?<\/td><td>(.*?)<\/td><td>(.*?)<\/td>/i)){
-			result.uptime= matches[1];
-			value=matches[2].replace(',','.');value=value.replace(' ','');
-			result.price= parseFloat(value);
-			value=matches[3].replace(',','.');value=value.replace(' ','');
-			result.traffic_outer= parseFloat(value);
-			AnyBalance.trace('WARNING! Statistics for current connection hasn\'t been showed.');
-		}
-		else{
-			throw new AnyBalance.Error("Error getting statistics");
-		}
-	}
-
+	//module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month=6&year=2012&day_from=1&day_to=31
+    if (AnyBalance.isAvailable('traffic_outer') || AnyBalance.isAvailable('uptime') || AnyBalance.isAvailable('price')) {
+    	var d = new Date();
+    	var n = d.getMonth() + 1;
+    	var y = d.getYear() + 1900;
+		
+    	AnyBalance.trace('Getting uptime and traffic by ' + n + '/' + y + ' (' + baseurl + '?module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month=' + n + '&year=' + y + '&day_from=1&day_to=31)');
+    	
+		html = AnyBalance.requestGet(baseurl + '?module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month=' + n + '&year=' + y + '&day_from=1&day_to=31');
+    	
+		if (matches = html.match(/<td>Итого:<\/td><td>.*?<\/td><td>(.*?)\s+\[.*?<\/td><td>(.*?)<\/td><td>(.*?)<\/td>/i)) {
+    		result.uptime = matches[1];
+    		value = matches[2].replace(',', '.');
+    		value = value.replace(' ', '');
+    		result.price = parseFloat(value);
+    		value = matches[3].replace(',', '.');
+    		value = value.replace(' ', '');
+    		result.traffic_outer = parseFloat(value);
+    		AnyBalance.trace('WARNING! Statistics for current connection hasn\'t been showed.');
+    	} else {
+			AnyBalance.trace('Error getting statistics');
+    		//throw new AnyBalance.Error("Error getting statistics");
+    	}
+    }
+	
 	if(AnyBalance.isAvailable('traffic_outer_last') || AnyBalance.isAvailable('uptime_last') || AnyBalance.isAvailable('price_last')){
 		var d = new Date();var n=d.getMonth();var y=d.getYear()+1900;
 		if(n==0){
@@ -76,8 +73,8 @@ function main(){
 
 		AnyBalance.trace('Getting uptime and traffic by ' +n+'/'+y+' (' + baseurl + '?module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month='+n+'&year='+y+'&day_from=1&day_to=31)');
 
-		info = AnyBalance.requestGet(baseurl + '?module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month='+n+'&year='+y+'&day_from=1&day_to=31');
-		if(matches = info.match(/<td>Итого:<\/td><td>.*?<\/td><td>(.*?)\s+\[.*?<\/td><td>(.*?)<\/td><td>(.*?)<\/td>/i)){
+		html = AnyBalance.requestGet(baseurl + '?module=dialup&mid=1&action=ShowLoginsBalance&pageIndex=1&unit=1048576&month='+n+'&year='+y+'&day_from=1&day_to=31');
+		if(matches = html.match(/<td>Итого:<\/td><td>.*?<\/td><td>(.*?)\s+\[.*?<\/td><td>(.*?)<\/td><td>(.*?)<\/td>/i)){
 			result.uptime_last= matches[1];
 			value=matches[2].replace(',','.');value=value.replace(' ','');
 			result.price_last= parseFloat(value);
@@ -85,25 +82,18 @@ function main(){
 			result.traffic_outer_last= parseFloat(value);
 		}
 		else{
-			throw new AnyBalance.Error("Error getting statistics");
+			AnyBalance.trace('Error getting statistics');
+			//throw new AnyBalance.Error("Error getting statistics");
 		}
 	}
-
+	
 	if(AnyBalance.isAvailable('delimiter')){
-
 		AnyBalance.trace('Getting check period ' + baseurl + '?action=ShowPeriods&mid=1&module=dialup');
-
-		info = AnyBalance.requestGet(baseurl + '?action=ShowPeriods&mid=1&module=dialup');
-		if(matches = info.match(/<td>.*?<\/td><td>(.*?)<\/td>\s+<\/tr>\s+<\/tbody>/i)){
-			result.delimiter = matches[1];
-		}
+		
+		html = AnyBalance.requestGet(baseurl + '?action=ShowPeriods&mid=1&module=dialup');
+		
+		getParam(html, result, 'delimiter', /<td>.*?<\/td><td>(.*?)<\/td>\s+<\/tr>\s+<\/tbody>/i, replaceTagsAndSpaces, html_entity_decode);
 	}
-
+	
 	AnyBalance.setResult(result);
 };
-
-
-
-
-
-

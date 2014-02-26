a560c1a13aeb67db3103dfd58b6b2851542d6a0f
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-02-26 11:54:46 +0000

    Планета (Екатеринбург)

--- trunk/ab-internet-planeta/main.js
+++ trunk/ab-internet-planeta/main.js
@@ -10,14 +10,14 @@ function parseTrafficGb(str){
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
-
+	
     var baseurl = "https://weburg.me/planeta";
-
+	
     var html = AnyBalance.requestPost(baseurl + '/login', {
         'planeta[login]':prefs.login,
         'planeta[password]':prefs.password
     });
-
+	
     if(!/pl-logout/i.test(html)){
         var error = getParam(html, null, null, /<p[^>]*error_message[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
@@ -29,14 +29,14 @@ function main(){
 
     getParam(html, result, '__tariff', /<div[^>]*pl-curent-tarif-title[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
     getParam(html, result, 'licschet', /Договор([^<]*)от/i, replaceTagsAndSpaces);
-    getParam(html, result, 'balance', /Остаток на счете[\s\S]*?<div[^>]*class="\s*pl-tab-value\s*"[^>]*>([\S\s]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balance', [/Остаток на счете[\s\S]*?<div[^>]*class="\s*pl-tab-value\s*"[^>]*>([\S\s]*?)<\/div>/i, />Остаток на счете([^>]*>){8}/i], replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'chatles', /<div[^>]*pl-tab-value_chatles[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-
+	
     if(AnyBalance.isAvailable('trafficIn', 'trafficOut')){
         html = AnyBalance.requestGet(baseurl + '/statistic/internet');
         getParam(html, result, 'trafficIn', /pl-stat-td_total_border(?:[\s\S]*?<td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseTrafficGb);
         getParam(html, result, 'trafficOut', /pl-stat-td_total_border(?:[\s\S]*?<td[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces, parseTrafficGb);
     }
-
+	
     AnyBalance.setResult(result);
 }

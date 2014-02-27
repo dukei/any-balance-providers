5ff7047b4dc0a8f8798478fd5386aabb26d97bcd
stark027 <stark027@gmail.com>
2014-02-27 15:45:29 +0000

    Новый провайдер: Избёнка (Бонусы)

---
+++ ab-bonus-izbenka/main.js
@@ -0,0 +1,51 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://it-stuff.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'Client/Account/LogOn.aspx', g_headers);
+	
+	var form = getParam(html, null, null, /<form name="aspnetForm" method="post" action="LogOn.aspx"[\s\S]*?<\/form>/i);
+	checkEmpty(form, 'Не удалось найти форму входа, сайт изменен?');
+	
+	var params = createFormParams(form, function(params, str, name, value) {
+		if (name == 'ctl00$MainContent$UserName') 
+			return prefs.login;
+		else if (name == 'ctl00$MainContent$Password')
+			return prefs.password;
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + 'Client/Account/LogOn.aspx', params, addHeaders({Referer: baseurl + 'Client/Account/LogOn.aspx'}));
+	
+	if (!/Account\/LogOff\.aspx/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Сумма баллов:(?:[^>]*>){2}\s*<input[^>]*value=['"]?([^"'\s]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'summ', /Сумма покупок с начала месяца:(?:[^>]*>){2}\s*<input[^>]*value=['"]?([^"'\s]+)/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

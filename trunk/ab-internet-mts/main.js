1bf91a4aa9ad3cfe39685c4297ccf60debcc3c50
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-03 09:30:02 +0000

    Домашний Интернет и Телевидение МТС

--- trunk/ab-internet-mts/main.js
+++ trunk/ab-internet-mts/main.js
@@ -417,93 +417,13 @@ function getKrv(){
 	var urlAjax = 'https://lk.kirovnet.net/index.php?r=account/vgroups&agrmid=';
 	
 	newTypicalLanBillingInetTv(urlIndex, urlAjax);
-    /*var prefs = AnyBalance.getPreferences();
-    AnyBalance.setDefaultCharset('utf-8');
-
-    var baseurl = 'https://lk.kirovnet.net/';
-
-    if(!prefs.__dbg){
-        var html = AnyBalance.requestPost(baseurl + '?r=site/login&0=site%2Flogin', {
-            'LoginForm[login]':prefs.login,
-            yt0: 'Войти',
-            'LoginForm[password]':prefs.password
-        });
-    }else{
-        var html = AnyBalance.requestGet(baseurl + '?r=account/index');
-    }
-
-    if(!/\?r=site\/logout/i.test(html)){
-        throw new AnyBalance.Error("Не удалось войти в личный кабинет. Неправильный логин-пароль?");
-    }
-
-    var result = {success: true};
-
-    getParam(html, result, 'agreement', /Номер договора:([\s\S]*?)<a/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /Текущий баланс:([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance2);
-
-    //Тут услуги в таблице, пройдемся по ней и возьмем тарифные планы
-    var services = sumParam(html, null, null, /(<tr[^>]*>\s*<td[^>]+class="first_col"[^>]*>(?:[\s\S](?!\/tr))*<\/tr>)/ig);
-    var tariffs = [];
-    for(var i=0; i<services.length; ++i){
-        var tariff = getParam(services[i], null, null, /(?:[\s\S]*?<td[^>]*>){2}\s*<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(tariff == 'Услуги')
-            continue;
-        tariffs[tariffs.length] = tariff;
-        getParam(services[i], result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
-    }
-    
-    if(tariffs.length > 0)
-        result.__tariff = tariffs.join(', ');
-
-    getParam(html, result, '__tariff', /Текущий тарифный план:([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'username', /Общая информация\s+\/([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-
-    AnyBalance.setResult(result);*/
 }
 
 function getPnz(){
-    var prefs = AnyBalance.getPreferences();
-
-    AnyBalance.setDefaultCharset('utf-8');
-
-    var baseurl = "https://client.lanbilling.ptcomm.ru/index.php?";
-
-    var html = AnyBalance.requestPost(baseurl + 'r=site/login', {
-        'LoginForm[login]':prefs.login,
-        yt0:'Войти',
-        'LoginForm[password]':prefs.password
-    });
-
-    //AnyBalance.trace(html);
-
-    if(!/\?r=site\/logout/i.test(html)){
-        throw new AnyBalance.Error("Не удалось войти в личный кабинет. Неправильный логин-пароль?");
-    }
-
-    var result = {success: true};
-
-    getParam(html, result, 'agreement', /Номер договора:([\s\S]*?)<a/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /Текущий баланс:([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance2);
-
-    //Тут услуги в таблице, пройдемся по ней и возьмем тарифные планы
-    var services = sumParam(html, null, null, /(<tr[^>]*>\s*<td[^>]+class="first_col"[^>]*>(?:[\s\S](?!\/tr))*<\/tr>)/ig);
-    var tariffs = [];
-    for(var i=0; i<services.length; ++i){
-        var tariff = getParam(services[i], null, null, /(?:[\s\S]*?<td[^>]*>){2}\s*<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(tariff == 'Услуги')
-            continue;
-        tariffs[tariffs.length] = tariff;
-        getParam(services[i], result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
-//        getParam(services[i], result, 'status', /Состояние:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-    }
-    
-    if(tariffs.length > 0)
-        result.__tariff = tariffs.join(', ');
-
-    getParam(html, result, '__tariff', /Текущий тарифный план:([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'username', /Общая информация\s+\/([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-
-    AnyBalance.setResult(result);
+	var urlIndex = 'https://client.lanbilling.ptcomm.ru/index.php?r=site/login';
+	var urlAjax = 'https://client.lanbilling.ptcomm.ru/index.php?r=account/vgroups&agrmid=';
+	
+	newTypicalLanBillingInetTv(urlIndex, urlAjax);
 }
 
 function getNnov(){

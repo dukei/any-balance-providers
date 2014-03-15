963d41286cca33af3ff27cbd51584872133befe2
arbu3iki <arbu3iki@gmail.com>
2014-03-15 11:48:00 +0000

    Адаптировано под новый биллинг

--- ab-internet-magnus/main.js
+++ ab-internet-magnus/main.js
@@ -1,88 +1,40 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
+	'Content-Type':'application/x-www-form-urlencoded',
+};
+
 function main(){
-		
-		AnyBalance.setDefaultCharset('utf-8');
-		
-		AnyBalance.trace('Connecting...');
-		
-		var result = {success: true};
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://bill.magnus.net.ua/';
+    AnyBalance.setDefaultCharset('windows-1251'); 
+	
+	var html = AnyBalance.requestPost(baseurl + 'user/index.php', {
+        'type':'userlogin',
+		us_abon_login:prefs.login,
+        us_abon_pass:prefs.password,
+    }, addHeaders({Referer: baseurl + 'user/index.php'})); 
+	
+    if(!/Выход/i.test(html)){
+        var error = getParam(html, null, null, /<div[^>]*id="[^"]*login_page_alert_inside[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
 
+    var result = {success: true};
+    getParam(html, result, 'fio', /class="lk_lk_userdata"><b>([\s\S]*?)<\/b><br>/i, replaceTagsAndSpaces);
+	getParam(html, result, '__tariff', /Тариф: <b>([\s\S]*?).\(/i, replaceTagsAndSpaces);
+	getParam(html, result, 'balance', /Баланс.*?label_h2\">([\s\S]*?)<\/span><br>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'abonplata', /Тариф.*\(([\s\S]*?)ежедневно\)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'state', /Состояние:([\s\S]*?)<\/b><br>/i, replaceTagsAndSpaces);
+	getParam(html, result, 'ip', /IP:([\s\S]*?)<\/b><br>/i, replaceTagsAndSpaces);
+	getParam(html, result, 'currency', /Баланс.*?label_h2\">([\s\S]*?).<\/span><br>/i, replaceTagsAndSpaces, parseCurrency);
 
-		var prefs = AnyBalance.getPreferences();
-		var usr_log = prefs.usr_log; // Логин
-		var usr_pass = prefs.usr_pass; // Пароль
-		
-		var info = AnyBalance.requestPost('http://copypaster.org/magnus/ab_api.php', {
-				username:usr_log,
-				password:usr_pass,
-		});
-		
-		var xmlDoc = $.parseXML(info),
-		  $xml = $(xmlDoc);
-		
-		AnyBalance.trace('Checking error...');
-		
-		//Проверим, нет ли ошибки
-		var $error = $xml.find('info>error').each(function(){ 
-				throw new AnyBalance.Error($(this).text());
-		});
-		
-		
-		AnyBalance.trace('Looking for information...');
-        
-        var $info = $xml.find('info>user');
-        if(!$info.size())
-        throw new AnyBalance.Error("Error: no info");
-		
-		
-		AnyBalance.trace('Getting counters...');
-		
-		if(AnyBalance.isAvailable('mlog')){
-				result.mlog = $info.find('mlog').text(); // Логин
-		}
-
-		if(AnyBalance.isAvailable('mid')){
-				result.mid = $info.find('mid').text(); // ID
-		}
-
-		if(AnyBalance.isAvailable('mbal')){
-				result.mbal = $info.find('mbal').text(); // Баланс
-		}
-
-		if(AnyBalance.isAvailable('mtar')){
-				result.mtar = $info.find('mtar').text(); // Тарифный план
-		}
-
-		if(AnyBalance.isAvailable('mfio')){
-				result.mfio = $info.find('mfio').text(); // ФИО
-		}
-
-		if(AnyBalance.isAvailable('madr')){
-				result.madr = $info.find('madr').text(); // Адрес
-		}
-
-		if(AnyBalance.isAvailable('mip')){
-				result.mip = $info.find('mip').text(); // Real-IP
-		}
-
-		if(AnyBalance.isAvailable('mmac')){
-				result.mmac = $info.find('mmac').text(); // MAC-адрес
-		}
-
-		if(AnyBalance.isAvailable('mtel')){
-				result.mtel = $info.find('mtel').text(); // Домашний телефон
-		}
-
-		if(AnyBalance.isAvailable('mmob')){
-				result.mmob = $info.find('mmob').text(); // Мобильный телефон
-		}
-
-		if(AnyBalance.isAvailable('mstat')){
-				result.mstat = $info.find('mstat').text(); // Статус
-		}
-
-		if(AnyBalance.isAvailable('mreg')){
-				result.mreg = $info.find('mreg').text(); // Дата регистрации
-		}
-
-		AnyBalance.setResult(result);
+    AnyBalance.setResult(result);
 }

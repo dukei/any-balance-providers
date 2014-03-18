495de1fb1f272b56baf2d830787ac799fd8701af
poluphanov.vitaliy@gmail.com <poluphanov.vitaliy@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-18 11:19:07 +0000

    WoT Balance - новый провайдер

---
+++ trunk/ab-game-wotbalance/common_functions.js
@@ -0,0 +1,66 @@
+﻿function checkEmpty(param, error) {
+    if (!param || param == '')
+        throw new AnyBalance.Error(error);
+}
+
+function getData(url) {
+	var data = AnyBalance.requestGet(url);
+	if (data){
+		var code = data.match(/<title>(.+?)<\/title>/i);
+		if (code){
+			throw new AnyBalance.Error(code[1]);
+		} else {
+			var js = $.parseJSON(data);
+			var st =  js.status
+			if (st == 'ok'){
+				return js;
+			} else if (st == 'error'){
+				var err = js.error.message
+				throw new AnyBalance.Error('Error: ' + err);
+			} else {
+				throw new AnyBalance.Error('Unknown server response: ' + st);
+			}
+		}
+	} else {
+		throw new AnyBalance.Error('Unknown error');
+	}
+}
+
+function getID(nick, API_ADDR) {
+	var v = getData(API_ADDR + '/2.0/account/list/?application_id=demo&search=' + nick);
+	if (v.data[0]) {
+		return v.data[0].id;
+	} else {
+		throw new AnyBalance.Error('ID not found! Check your settings.');
+	}
+}
+
+function getDateTimeToString(d, strFormat){
+// Author: Iljin Oleg
+// http://iljin-oleg.blogspot.com/2012/09/javascript.html
+var resultDateTime = strFormat;
+
+var daysLong = ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"];
+var daysShort = ["Пн.","Вт.","Ср.","Чт.","Пт.","Сб.","Вс."];
+var yearRegExp = d.getFullYear();
+var monthRegExp = (String(d.getMonth()+1).length ==1) ? ("0"+(d.getMonth()+1)) : (d.getMonth()+1);
+var dayRegExp = (d.getDate().toString().length ==1) ? ("0"+d.getDate()) : d.getDate();
+var dayNameRegExp = d.getDay();
+var hoursRegExp = (d.getHours().toString().length ==1) ? ("0"+d.getHours()) : d.getHours();
+var minuteRegExp = (d.getMinutes().toString().length ==1) ? ("0"+d.getMinutes()) : d.getMinutes();
+var secondsRegExp = (d.getSeconds().toString().length ==1) ? ("0"+d.getSeconds()) : d.getSeconds();
+var milisecondsRegExp = (d.getMilliseconds().toString().length ==1) ? ("00"+d.getMilliseconds()) : ((d.getMilliseconds().toString().length ==2) ? ("0"+d.getMilliseconds()) : d.getMilliseconds());
+
+resultDateTime = resultDateTime.replace(new RegExp('yyyy', 'g'), yearRegExp);
+resultDateTime = resultDateTime.replace(new RegExp('yy', 'g'), String(yearRegExp).slice(-2));
+resultDateTime = resultDateTime.replace(new RegExp('MM', 'g'), monthRegExp);
+resultDateTime = resultDateTime.replace(new RegExp('dddd', 'g'), daysLong[dayNameRegExp-1]);
+resultDateTime = resultDateTime.replace(new RegExp('ddd', 'g'), daysShort[dayNameRegExp-1]);
+resultDateTime = resultDateTime.replace(new RegExp('dd', 'g'), dayRegExp);
+resultDateTime = resultDateTime.replace(new RegExp('hh', 'g'), hoursRegExp);
+resultDateTime = resultDateTime.replace(new RegExp('mm', 'g'), minuteRegExp);
+resultDateTime = resultDateTime.replace(new RegExp('ss', 'g'), secondsRegExp);
+resultDateTime = resultDateTime.replace(new RegExp('zz', 'g'), milisecondsRegExp);
+
+return resultDateTime+"";
+};
---
+++ trunk/ab-game-wotbalance/main.js
@@ -0,0 +1,59 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+World of Tanks — бесплатная онлайн игра
+Сайт игры: http://worldoftanks.ru/
+*/
+
+function main(){
+	var prefs = AnyBalance.getPreferences();
+	checkEmpty(prefs.nick, 'Enter nick or ID!');
+	
+	// Проверяем правильность id
+	if (prefs.method == 'id'){
+		var regexp = /\d+$/;
+		if (!(regexp.test(prefs.nick)))
+			throw new AnyBalance.Error('ID must contain only digits!');
+	}
+	
+	if (!prefs.region || prefs.region == 'RU'){
+		var API_ADDR = 'http://api.worldoftanks.ru';
+	} else if (prefs.region == 'EU'){
+		var API_ADDR = 'http://api.worldoftanks.eu';
+	} else if (prefs.region == 'NA'){
+		var API_ADDR = 'http://api.worldoftanks.com';
+	} else if (prefs.region == 'ASIA'){
+		var API_ADDR = 'http://api.worldoftanks.asia';
+	} else if (prefs.region == 'KR'){
+		var API_ADDR = 'http://api.worldoftanks.kr';
+	}
+	
+	// Получаем ID игрока
+	var id = (prefs.method == 'id') ? prefs.nick : getID (prefs.nick, API_ADDR);
+
+	// Получаем информацию игрока
+	var info = getData(API_ADDR + '/wot/account/info/?application_id=demo&account_id=' + id + '&fields=private,nickname&access_token=' + prefs.token);
+	
+	var result = {success: true};
+	
+	result.__tariff = info.data[id].nickname;
+	
+	if(AnyBalance.isAvailable('gold'))
+		result['gold'] = info.data[id]['private'].gold;
+		
+	if(AnyBalance.isAvailable('credits'))
+		result['credits'] = info.data[id]['private'].credits;
+		
+	if(AnyBalance.isAvailable('free_xp'))
+		result['free_xp'] = info.data[id]['private'].free_xp;
+	
+	if(AnyBalance.isAvailable('premium'))
+		if(info.data[id]['private'].is_premium) {
+			var t = new Date(info.data[id]['private'].premium_expires_at * 1000);
+			result['premium'] = getDateTimeToString(t, "dddd, dd.MM.yyyy (hh:mm)");; 
+		} else {
+			result['premium'] = "Премиум-аккаунт отсутствует";
+		}
+		
+	AnyBalance.setResult(result);
+}

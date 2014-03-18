689f45567e99cbcc4fe26193f7cb1e5a7b947114
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-18 12:29:05 +0000

    УВО МВД Кемерово

--- trunk/ab-guard-uvokuzbass/main.js
+++ trunk/ab-guard-uvokuzbass/main.js
@@ -8,11 +8,11 @@ Operator site: http://www.uvokuzbass.ru/
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36'
 };
 
 function main(){
@@ -20,31 +20,38 @@ function main(){
     var baseurl = "http://www.uvokuzbass.ru/";
     AnyBalance.setDefaultCharset('utf-8'); 
 
-//    AnyBalance.requestGet(baseurl + 'dolg.html', g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'zadolg/', g_headers);
 
-    var html = AnyBalance.requestPost(baseurl + 'check_balance.php', {
-        dataset:1, //По лицевому счету
-	cities:prefs.city,
-	dogovor_number:prefs.login,
-	dog_number:'',
-	last_name:''
-    }, addHeaders({Referer: baseurl + 'dolg.html', 'X-Requested-With':'XMLHttpRequest'})); 
-
-    if(!/По состоянию на/i.test(html)){
-        //Если в кабинет войти не получилось, то в первую очередь надо поискать в ответе сервера объяснение ошибки
-        var error = getParam(html, null, null, /<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        //Если объяснения ошибки не найдено, при том, что на сайт войти не удалось, то, вероятно, произошли изменения на сайте
-        throw new AnyBalance.Error('Не удалось получить баланс счета. Сайт изменен?');
-    }
-
-    //Раз мы здесь, то мы успешно вошли в кабинет
-    //Получаем все счетчики
+    html = AnyBalance.requestPost(baseurl + 'zadolg/', {
+		sposob:1, //По лицевому счету
+		NOMOVO:'А8238',
+		LSCHET:prefs.login,
+		DOG:'',
+		FIO:'',
+		sendinfo:'Узнать задолженность'
+		//cities:prefs.city,
+    }, addHeaders({Referer: baseurl + 'zadolg/'})); 
+	
+	if(!/На дату \d{2}.\d{2}.\d{2,4}/i.test(html)){
+		var error = getParam(html, null, null, /<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти информацию. Сайт изменен?');
+	}
+	
     var result = {success: true};
-    getParam(html, result, 'licschet', /(?:Переплата|Задолженность) по счету\s*<strong[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /(?:Переплата|Задолженность) по счету[\s\S]*?(<span[^>]*>[\s\S]*?)<\/span>/i, [/<span[^>]+color:\s*red[^>]*>/g, '-', replaceTagsAndSpaces], parseBalance);
+	
+	getParam(html, result, 'balance', /(?:Переплата|Задолженность) по штрафам(?:[^>]*>){7}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'licschet', /На дату[^<]*(?:задолженность|переплата)\s+по\s+договору([^<]*)составляет/i, replaceTagsAndSpaces, html_entity_decode);
+    //getParam(html, result, 'balance', /(?:Переплата|Задолженность) по счету[\s\S]*?(<span[^>]*>[\s\S]*?)<\/span>/i, [/<span[^>]+color:\s*red[^>]*>/g, '-', replaceTagsAndSpaces], parseBalance);
+	
 
+	
+	
+	
+	
 //    makeCities(result);
 
     //Возвращаем результат

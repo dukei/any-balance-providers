6d5b12c076c7c2e57a13b811136da4571fc9720d
stark027 <stark027@gmail.com>
2014-03-19 10:23:09 +0000

    Triolan (Украина)

--- ab-internet-triolan/main.js
+++ ab-internet-triolan/main.js
@@ -1,36 +1,17 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает текущий остаток и другие параметры у украинского интернет-провайдера Триолан
-
-Сайт оператора: http://triolan.com/
-Личный кабинет: https://triolan.name/
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function getViewState(html){
     return getParam(html, null, null, /name="__VIEWSTATE".*?value="([^"]*)"/);
@@ -40,64 +21,50 @@ function getEventValidation(html){
     return getParam(html, null, null, /name="__EVENTVALIDATION".*?value="([^"]*)"/);
 }
 
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
 
-function parseDate(str){
-    AnyBalance.trace('Parsing date from value: ' + str);
-    var matches = /(\d+)[^\d](\d+)[^\d](\d+)/.exec(str);
-    var time;
-    if(matches){
-	  time = (new Date(+matches[1], matches[2]-1, +matches[3])).getTime();
-    }
-    return time;
-}
-
-function main(){
-    var prefs = AnyBalance.getPreferences();
-
-    var baseurl = "https://triolan.name/";
-    AnyBalance.setDefaultCharset('utf-8');
-
-    var html = AnyBalance.requestGet(baseurl);
-    var eventvalidation = getEventValidation(html);
-    var viewstate = getViewState(html);
-
-    html = AnyBalance.requestPost(baseurl, {
-      __EVENTTARGET:'',
-      __EVENTARGUMENT:'',
-      __VIEWSTATE:viewstate,
-      __EVENTVALIDATION:eventvalidation,
-      tb_login:prefs.login,
-      tb_pass:prefs.password,
-      'ib_login.x':37,
-      'ib_login.y':14
-    });
-
-    if(!/ctl00\$m_main/.test(html)){
-        var error = getParam(html, null, null, /id="l_error"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-
-    var result = {success: true};
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://triolan.name/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	try {
+		html = AnyBalance.requestPost(baseurl, {
+			'__EVENTTARGET':'',
+			'__EVENTARGUMENT':'',
+			'__VIEWSTATE':getViewState(html),
+			'__VIEWSTATEENCRYPTED':'',
+			'__EVENTVALIDATION':getEventValidation(html),
+			'login1$tbAgreement':prefs.login,
+			'login1$tbPassword':prefs.password,
+			'login1$btnLoginByAgr':'Войти',
+			'login1$tbPhone':'',
+			'login1$tbEmail':'',
+			'login1$hfType':'1',
+		}, addHeaders({Referer: baseurl}));
+	} catch(e) {}
+	
+	html = AnyBalance.requestGet(baseurl + 'RegRovno.aspx', g_headers);
+	
+	if (!/>Выход</i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
     getParam(html, result, 'license', /id="l_stat"[\s\S]*?Л\/с[\s\S]*?>([^<]*)</i, replaceTagsAndSpaces);
     getParam(html, result, '__tariff', /<select name="ctl00\$cph_main\$ddl_activations"[\s\S]*?value=[\s\S]*?>"?([^<"]*)"?</i, replaceTagsAndSpaces);
     getParam(html, result, 'balance', /id="l_stat"[\s\S]*?Баланс:[\s\S]*?>"?([^<]*)"?</i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'till', /id="l_stat"[\s\S]*?оплачено по:[\s\S]*?>([^<]*)</i, replaceTagsAndSpaces, parseDate);
-
-    AnyBalance.setResult(result);
+	
+	AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

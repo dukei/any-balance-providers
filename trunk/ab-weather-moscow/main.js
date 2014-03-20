468feb7f1992f19cbedf923bc20233976f94d279
erelen@melda.ru <erelen@melda.ru@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-20 02:08:12 +0000

    ab-weather-moscow v3

--- trunk/ab-weather-moscow/main.js
+++ trunk/ab-weather-moscow/main.js
@@ -1,30 +1,44 @@
-﻿/**
+﻿/*
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Температура в Москве. Источник - http://gradus.melda.ru/
 */
 
 function main() {
     var prefs = AnyBalance.getPreferences();
-	
-	prefs.critical_data_age = 30 * 60; // seconds
-	
-	json = AnyBalance.requestGet("http://gradus.melda.ru/data.json?anybalance.v2.0.7");
-	
-	var data = getJson(json);
-	
-	var delay = Math.round((new Date().getTime()/1000 - data.ts.data));
-    if (delay > prefs.critical_data_age) {
-		throw new AnyBalance.Error("Проблемы на сервере: данные устарели более чем на " + parseInt(delay / 60) + " мин.");
+
+    prefs.version = 'v3';
+    prefs.critical_data_age = 30 * 60; // seconds
+
+    json = AnyBalance.requestGet("https://gradus.melda.ru/data.json?anybalance." + prefs.version);
+
+    try {
+        var data = JSON.parse(json);
+    } catch (e) {
+        throw new AnyBalance.Error("Ошибка парсинга JSON-данных.");
     }
-	var result = {success: true};
 
-	getParam('<font color="' + data.temperature_color + '">' + data.temperature + '°</font>', result, 'temperature_color');
-	getParam(data.temperature, result, 'temperature');
+    if (data.version.major != 1) {
+        throw new AnyBalance.Error("Версия провайдера фатально устарела. Пожалуйста, обновитесь: http://anybalance.ru/catalog.php?id=ab-weather-moscow");
+    }
 
-	var dt = new Date(parseInt(data.ts.data)*1000);
-	if(dt)
-		getParam((dt.getDate() < 10 ? '0'+dt.getDate() : dt.getDate())+'/'+(dt.getMonth()+1 < 10 ? '0'+dt.getMonth()+1 : dt.getMonth()+1)+'/'+dt.getFullYear() + ' ' + dt.getHours()+':' + (dt.getMinutes() < 10 ? '0'+dt.getMinutes() : dt.getMinutes()), result, '__tariff');
-		
-		
-	//result['__tariff'] = "http://gradus.melda.ru/";
-	AnyBalance.setResult(result);
+    var delay = Math.round((new Date().getTime()/1000 - data.ts.data));
+    if (delay > prefs.critical_data_age) {
+        throw new AnyBalance.Error("Проблемы на сервере: данные устарели более чем на " + parseInt(delay / 60) + " мин.");
+    }
+
+    var result = {success: true};
+
+    result['temperature_color'] = '<font color="' + data.temperature_color + '">' + data.temperature + '°</font>';
+    result['temperature'] = data.temperature;
+    result['temperature_int'] = parseFloat(data.temperature);
+
+    var ts = new Date(data.ts.data * 1000);
+    result['ts'] = ts.getDate() + '.' + (ts.getMonth()+1 < 10 ? '0' : '') + (ts.getMonth()+1) + '.' + ts.getFullYear() + ' ' + ts.getHours() + ':' + (ts.getMinutes() < 10 ? '0' : '') + ts.getMinutes();
+
+    if (prefs.show_link) {
+        result['__tariff'] = "График: https://gradus.melda.ru/";
+    }
+
+    AnyBalance.setResult(result);
 }

f713e59dc90c6b21fbcf1507d72796813ca53894
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-20 15:10:06 +0000

    Etisalat

--- trunk/ab-cell-etisalat/main.js
+++ trunk/ab-cell-etisalat/main.js
@@ -1,19 +1,13 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Retrieves balance, status and renewal date from Etisalat personal account on specified prepaid mobile number.
-
-
-Operator site: http://www.etisalat.ae
-Personal account: https://www.e4me.ae/
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 var g_errors = {
@@ -31,12 +25,14 @@ var g_errors = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-
+	checkEmpty(prefs.login, 'Enter login, please!');
+	checkEmpty(prefs.password, 'Enter password, please!');
+	
     var baseurl = "https://www.e4me.ae/e4me/etisalat/";
     AnyBalance.setDefaultCharset('utf-8'); 
-
+	
     var result = {success: true};
-
+	
     try{
         var html = AnyBalance.requestPost(baseurl + 'login', {
             UserName:prefs.login,
@@ -62,12 +58,17 @@ function main(){
         getParam(tr, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
         getParam(tr, result, 'fio', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
         getParam(tr, result, 'status', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-        getParam(tr, result, 'till', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+        getParam(tr, result, 'till', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
         
         if(AnyBalance.isAvailable('balance')){
             var id = getParam(tr, null, null, /getAccountDetails\s*\(\s*'([^']*)/i);
-            html = AnyBalance.requestGet(baseurl + 'accounts?SID=10&NationalNo=' + id);
-            getParam(html, result, 'balance', /Available balance[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, [replaceTagsAndSpaces, /^\./, '0.'], parseBalance);
+			
+            html = AnyBalance.requestPost(baseurl + 'accounts?SID=15&MobileNo=' + id, {
+				'SID':'1',
+				SelectedAccount:''
+			}, addHeaders({'Referer':baseurl + 'accounts?SID=9'}));
+			
+            getParam(html, result, 'balance', />balance(?:[\s\S]*?<td[^>]*>){5}([\d,.\s]+)/i, replaceTagsAndSpaces, parseBalance);
         
         }
     }finally{

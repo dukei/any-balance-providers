4d5d3c26cafbfc58cefecf111e45a2c0b0c1d5b5
dmitry.kochin <dmitry.kochin@gmail.com>
2014-03-22 15:35:48 +0000

    Велком - изменения в лк

--- ab-cell-velcom/main.js
+++ ab-cell-velcom/main.js
@@ -12,7 +12,7 @@ function main(){
     checkEmpty(prefs.password, 'Введите пароль к ИССА!');
 
     var matches;
-    if(!(matches = /^\+375(\d\d)(\d{7})$/.exec(prefs.login)))
+    if(!(matches = /^\+(375\d\d)(\d{7})$/.exec(prefs.login)))
 		throw new AnyBalance.Error('Неверный номер телефона. Необходимо ввести номер в международном формате без пробелов и разделителей!');
 
     var phone = matches[2];
@@ -32,7 +32,7 @@ function main(){
 	if(id){
 		if(/PRE/i.test(id)){ //Это префикс
 			value = prefix;
-		}else if(/NUMBER/i.test(id)){ //Это номер
+		}else if(/NUMBER|MSISDN/i.test(id)){ //Это номер
 			value = phone;
 		}else if(/PWD/i.test(id)){ //Это пароль
 			value = prefs.password.substr(0, 8);  //Велкам принимает только первые 8 символов всё равно.
@@ -52,6 +52,7 @@ function main(){
 		value = '0';
 	return value || '';
     });
+    params.user_submit='';
 
     var required_headers = {
 		'Origin': 'https://internet.velcom.by',
@@ -62,14 +63,16 @@ function main(){
     var html = requestPostMultipart(baseurl + 'work.html', params, required_headers);
 
     var kabinetType, personalInfo;
-    if(/_root\/(USER|FIN)_INFO/i.test(html)){
-        personalInfo = '_root/USER_INFO';
+    if(/_root\/PERSONAL_INFO/i.test(html)){
+        personalInfo = '_root/PERSONAL_INFO';
         kabinetType = 2;
     }else if(/_root\/MENU0/i.test(html)){
         personalInfo = '_root/MENU1/USER_INFO';
         kabinetType = 1;
     }
-	
+
+    AnyBalance.trace('Cabinet type: ' + kabinetType);
+
     if(!kabinetType){
         var error = sumParam(html, null, null, /<td[^>]+class="INFO(?:_Error|_caption)?"[^>]*>(.*?)<\/td>/ig, replaceTagsAndSpaces, html_entity_decode, create_aggregate_join(' '));
         if(error)
@@ -94,10 +97,9 @@ function main(){
 		throw new AnyBalance.Error('ИССА Velcom временно недоступна. Пожалуйста, попробуйте позже.');
 	}	
 	
-    AnyBalance.trace('Cabinet type: ' + kabinetType);
-
     var result = {success: true};
 
+    //Персональная информация
     var html = requestPostMultipart(baseurl + 'work.html', {
         sid3: sid,
         user_input_timestamp: new Date().getTime(),
@@ -105,20 +107,23 @@ function main(){
         last_id: ''
     }, required_headers);
          
-    getParam(html, result, 'userName', /ФИО(?: \(название абонента\)):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'userNum', /(?:номер клиента|Номер телефона):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /(?:Текущий баланс|Баланс|Баланс основного счета|Начисления абонента\*?):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'userName', /ФИО(?: \(название абонента\))?:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'userNum', /(?:Номер):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /(?:Баланс лицевого счета):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     sumParam(html, result, 'balanceBonus', /(?:Баланс бонусного счета \d):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
     getParam(html, result, 'status', /(?:Текущий статус абонента|Статус абонента):[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'min', /Остаток минут, SMS, MMS, GPRS, включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*) мин(?:ут)?(?:\s*во все сети)?(?:,|\s*<)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'min_fn', /Остаток минут, SMS, MMS, GPRS, включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*) мин(?:ут)? на ЛН/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'min_velcom', /Остаток минут, SMS, MMS, GPRS, включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*) мин(?:ут)? на velcom/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'min', /Остаток минут, SMS, MMS, (?:МБ|GPRS), включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*) мин(?:ут)?(?:\s*во все сети)?(?:,|\s*<)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'min_fn', /Остаток минут, SMS, MMS, (?:МБ|GPRS), включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*) мин(?:ут)? на ЛН/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'min_velcom', /Остаток минут, SMS, MMS, (?:МБ|GPRS), включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*) мин(?:ут)? на velcom/i, replaceTagsAndSpaces, parseBalance);
 
-    getParam(html, result, 'traffic', /Остаток минут, SMS, MMS, GPRS, включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*)\s*Мб/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'traffic', /Остаток минут, SMS, MMS, (?:МБ|GPRS), включенных в абонплату:[\s\S]*?<td[^>]*>(?:[\s\S](?!<\/td>))*?(-?\d[\d,\.]*)\s*Мб/i, replaceTagsAndSpaces, parseBalance);
 	
+    //Кабинет изменился, рассрочку надо исправлять!
+    //Пока не получаем её
 	if(AnyBalance.isAvailable('loan_balance', 'loan_left', 'loan_end')) {
-		var html = requestPostMultipart(baseurl + 'work.html', {
+                AnyBalance.trace('Для исправления получения информации о рассрочке обратитесь к автору провайдера.');
+/*		var html = requestPostMultipart(baseurl + 'work.html', {
 			sid3: sid,
 			user_input_timestamp: new Date().getTime(),
 			user_input_0: '_root/MENU1/FIN_INFO1/LOAN',
@@ -128,7 +133,7 @@ function main(){
 		
 		getParam(html, result, 'loan_balance', /Размер ежемесячного платежа:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 		getParam(html, result, 'loan_left', /Оставшаяся к выплате сумма по рассрочке:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'loan_end', /Дата погашения рассрочки:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseDate);
+		getParam(html, result, 'loan_end', /Дата погашения рассрочки:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseDate);*/
 	}
 	
 	/*

d2338b1495335e48a892a312bcbe40c096fc0e72
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-25 19:03:36 +0000

    Новый провайдер НЕТОРН

---
+++ trunk/ab-internet-netorn/main.js
@@ -0,0 +1,54 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://userstat.netorn.ru/';
+	AnyBalance.setDefaultCharset('koi8-r');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'noaccess.php?ERROR_PWD9', g_headers);
+	
+	html = AnyBalance.requestPost(baseurl + 'noaccess.php?ERROR_PWD9', {
+		Login: prefs.login,
+		Password: prefs.password,
+		'Submit': 'Войти'
+	}, addHeaders({Referer: baseurl + 'noaccess.php?ERROR_PWD9'}));
+	
+	if (!/index.php\?/i.test(html)) {
+		var error = getParam(html, null, null, /<font color='red' face='verdana' size='1'[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверное имя пользователя или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var href = getParam(html, null, null, /index.php\?[^"]+/i);
+	
+	checkEmpty(href, 'Не удалось найти идентификатор сессии, сайт изменен?', true);
+	
+	html = AnyBalance.requestGet(baseurl + href, g_headers);
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Баланс:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'status', /Статус:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

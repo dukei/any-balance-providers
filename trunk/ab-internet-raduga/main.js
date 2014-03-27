f1142da9e5b1e6ad9202d65fb6db8769a9f1c808
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-27 11:44:29 +0000

    Радуга Интернет

--- trunk/ab-internet-raduga/main.js
+++ trunk/ab-internet-raduga/main.js
@@ -36,10 +36,7 @@ function main(){
 	getParam(html, result, 'agreement', /<p[^>]*>\s*Договор\s*<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'fio', /<div[^>]+id="balance-block"[^>]*>[\s\S]*?<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, '__tariff', [/Текущий тариф:([^>]*>){4}/i, /<th[^>]*>Тариф:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i], replaceTagsAndSpaces, html_entity_decode);
-	
-	
-	
-	// Проверено на одном типе кабинета
+	getParam(html, result, 'till', /<th[^>]*>Списание аб.платы за следующий период[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDateWord);
 	getParam(html, result, 'add_traffic', /<th[^>]*>Использовать доп. трафик после окончания включенного[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'abon', /<th[^>]*>Абонентская плата:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'add_traffic_cost', /<th[^>]*>Стоимость доп. трафика:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
@@ -49,22 +46,16 @@ function main(){
     getParam(html, result, 'traffic_total', [/Трафик[^>]*>\(оставшийся\/включенный\)\s*:(?:[^>]*>){2}[\s\d]+Мб\s*\/([\s\d]+Мб)/i,
 	/<th[^>]*>Трафик, включенный в абонентскую плату:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i], replaceTagsAndSpaces, parseTraffic);
 	
+    getParam(html, result, ['traffic_left_night', 'traffic_used_night'], /Ночной трафик[^>]*>\(оставшийся\/включенный\)\s*:(?:[^>]*>){2}([\s\d]+Мб)/i, replaceTagsAndSpaces, parseTraffic);	
+    getParam(html, result, ['traffic_total_night', 'traffic_used_night'], /Ночной трафик[^>]*>\(оставшийся\/включенный\)\s*:(?:[^>]*>){2}[\s\d]+Мб\s*\/([\s\d]+Мб)/i, replaceTagsAndSpaces, parseTraffic);	
 	
-	
-	
-	getParam(html, result, 'till', /<th[^>]*>Списание аб.платы за следующий период[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDateWord);
-	
-	
+	if(isset(result.traffic_total_night) && isset(result.traffic_left_night)) {
+		getParam(result.traffic_total_night - result.traffic_left_night, result, 'traffic_used_night');
+	}
 	
     //getParam(html, result, 'limit_traffic', /<h2[^>]*>Ограничение трафика[\s\S]*<th[^>]*>Статус:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    
     //getParam(html, result, 'autoactive', /<th[^>]*>Автоактивация:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     //getParam(html, result, 'till', /<th[^>]*>Списание аб.платы за следующий период[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDateMomentMSK);
-    
-
-		
-    
-    
 	
     if(AnyBalance.isAvailable('traffic')){
 		var href = getParam(html, null, null, /<a href="\/([^"]+)" >\s*за месяц/i);

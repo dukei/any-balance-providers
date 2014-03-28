a1079e030670ef990c7c93d6666806b83ede022c
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-03-28 15:01:32 +0000

    Новый провайдер: Гродненские информационные сети (Grin)

---
+++ trunk/ab-internet-grin/main.js
@@ -0,0 +1,47 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31',
+	'Host':'stat.vintem.ru'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = "https://utm.grin.by/";
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	AnyBalance.setDefaultCharset('UTF-8');
+	
+    var html = AnyBalance.requestPost(baseurl, {
+        login:prefs.login,
+        password:prefs.password,
+    }, addHeaders({Referer: baseurl}));
+	
+    if(!/Выйти/i.test(html)){
+        var error = getParam(html, null, null, /(?:[\s\S]*?<BR[^>]*>){2}([\s\S]*?)<BR>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error);
+		
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+	
+    var result = {success: true};
+	
+    getParam(html, result, 'balance', /<td[^>]*>Баланс<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balanceCredit', /<td[^>]*>Кредит<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	
+	getParam(html, result, 'fio', /<td[^>]*>ФИО<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'account', /<td[^>]*>Лицевой счёт для оплаты в банках<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+    html = AnyBalance.requestGet(baseurl + '?module=40_tariffs', g_headers);
+	
+    getParam(html, result, '__tariff', /<TD[^>]*>Текущий ТП<\/TD>(?:[\s\S]*?<td[^>]*>){7}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);    
+	
+    AnyBalance.setResult(result);
+}

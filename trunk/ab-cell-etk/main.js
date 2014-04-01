c4a1fafb0107b9e70df037181dd339ecc9f9f117
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-04-01 19:09:01 +0000

    Енисейтелеком (Ростелеком - ЕТК)

--- trunk/ab-cell-etk/main.js
+++ trunk/ab-cell-etk/main.js
@@ -1,39 +1,39 @@
-﻿function main(){
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+function main(){
 	var prefs = AnyBalance.getPreferences();
-	var login = prefs.login;
-	var password = prefs.password;
-	var Postinfo = AnyBalance.requestPost('https://issa.etk.ru/cgi-bin/cgi.exe?function=is_login', {
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestPost('https://issa.etk.ru/cgi-bin/cgi.exe?function=is_login', {
 		Lang:2,
-		mobnum:login,
-		Password:password,
+		mobnum:prefs.login,
+		Password:prefs.password,
 		x:35,
 		y:6
 	});
-
-	var info = AnyBalance.requestGet('https://issa.etk.ru/cgi-bin/cgi.exe?function=is_account');
-
-	if(Postinfo.search('Ошибка аутентификации') != -1){
-		throw new AnyBalance.Error('Введен неверный номер или пароль, либо этот номер заблокирован');
-	};
-
+	
+	if (!/Осуществляется вход в систему/i.test(html)) {
+		var error = getParam(html, null, null, /class=error([^>]*>){2}/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /неверный пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}	
+	
+	html = AnyBalance.requestGet('https://issa.etk.ru/cgi-bin/cgi.exe?function=is_account');	
+	
 	var result = {success: true};
-
-	var regexp1 = new RegExp('Актуальный баланс:[^&]*&nbsp;(\\-?\\d*\\.?\\d*)');
-        var matches1;
-
-	var regexp2 = new RegExp('Средняя скорость расходования средств по лицевому счету в день:[^&]*&nbsp;(\\-?\\d*\\.?\\d*)');
-        var matches2;
-
-	if(matches1 = info.match(regexp1)){
-		if(AnyBalance.isAvailable('ActualBalance'))
-			result.ActualBalance = parseFloat(matches1[1]);
-	};
-
-	if(matches2 = info.match(regexp2)){
-		if(AnyBalance.isAvailable('DayRashod'))
-			result.DayRashod = parseFloat(matches2[1]);
-	};
-
-
+	
+	getParam(html, result, 'ActualBalance', /Актуальный баланс:([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'DayRashod', /Средняя скорость расходования средств по лицевому счету в день:([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /"Пользователь"([^>]*>){5}/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'phone', /"Номер телефона"([^>]*>){5}/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'licschet', /"Лицевой счет"([^>]*>){5}/i, replaceTagsAndSpaces, html_entity_decode);
+	
 	AnyBalance.setResult(result);
 }

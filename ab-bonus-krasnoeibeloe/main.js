b62b84bb412b9e0372110582d6c998e19ef31386
stark027 <stark027@gmail.com>
2014-04-04 11:28:01 +0000

    Красное&Белое (Урал)

--- ab-bonus-krasnoeibeloe/main.js
+++ ab-bonus-krasnoeibeloe/main.js
@@ -1,57 +1,47 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает накопленную сумму и текущую скидку для карты Красное и белое
-
-Сайт оператора: http://krasnoeibeloe.ru
 */
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "http://www.krasnoeibeloe.ru/";
-
-    var html = AnyBalance.requestPost(baseurl + '/json/', {
-        _do:'get_card_info',
-        card_id:prefs.login
-    });
-
-    var json;
-    try{
-        json = JSON.parse(html);
-    }catch(e){
-        AnyBalance.trace('Can not parse json: ' + html);
-        throw new AnyBalance.Error('Сервер вернул неверный ответ. Проблемы на сайте или сайт изменен.');
-    }
-
-    if(json.error){
-        throw new AnyBalance.Error('Неверный номер карты.');
-    }
+    var baseurl = "http://krasnoeibeloe.ru/";
+	
+	var html = AnyBalance.requestGet(baseurl + 'discount/', g_headers);
+	
+	var form = getParam(html, null, null, /<form action="[^"]*" method="POST"(?:[^>]*>){10,12}\s*<\/form>/i);
+	
+	var params = createFormParams(form, function(params, str, name, value) {
+		if (name == 'card_number') 
+			return prefs.login;
+		return value;
+	});
+	
+    html = AnyBalance.requestPost(baseurl + 'discount/', params);
+	
+	if (!/Скидка по вашей карте(?:[^>]*>)[1-9][0-9]*%/i.test(html)) {
+		var error = getParam(html, null, null, /"white-ramka"([^>]*>){2}/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}	
 
     var result = {success: true};
+	
+	getParam(html, result, 'discount', /Скидка по вашей карте(?:[^>]*>)([0-9]+%)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'sumleft', /Вам необходимо накопить еще([\s\d,.]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'nextdis', /До скидки([\s\d,.%]+)Вам необходимо накопить еще/i, replaceTagsAndSpaces, parseBalance);
 
-    result.balance = parseFloat(json.summ);
-    result.discount = parseFloat(json.discount);
-    result.__tariff = json.card_id;
-
-    var levels = [
-        [500, 1],
-        [3000, 2],
-        [8000, 4],
-        [18000, 6],
-        [36000, 8],
-        [60000, 10],
-    ];
-
-    for(var i=0; i<levels.length; ++i){
-        if(levels[i][1] > json.discount){
-            result.sumleft = levels[i][0] - json.summ;
-            result.nextsum = levels[i][0];
-            result.nextdis = levels[i][1];
-            break;
-        }
-    }
-    
     AnyBalance.setResult(result);
 }

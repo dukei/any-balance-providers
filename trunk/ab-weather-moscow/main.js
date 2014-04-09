90cc347a10d2f6c1435df62b585a04f1da0d8b1d
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-04-09 08:07:16 +0000

    Температура в Москве

--- trunk/ab-weather-moscow/main.js
+++ trunk/ab-weather-moscow/main.js
@@ -1,4 +1,4 @@
-﻿/*
+﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
 Температура в Москве. Источник - http://gradus.melda.ru/
@@ -6,33 +6,35 @@
 
 function main() {
     var prefs = AnyBalance.getPreferences();
-
+	
     prefs.version = 'v3';
     prefs.critical_data_age = 30 * 60; // seconds
-
+	
     json = AnyBalance.requestGet("https://gradus.melda.ru/data.json?anybalance." + prefs.version);
     var data = getJson(json);
-
+	
     if (data.version.major != 1) {
         throw new AnyBalance.Error("Версия провайдера фатально устарела. Пожалуйста, обновитесь: http://anybalance.ru/catalog.php?id=ab-weather-moscow");
     }
-
+	
     var delay = Math.round((new Date().getTime()/1000 - data.ts.data));
     if (delay > prefs.critical_data_age) {
         throw new AnyBalance.Error("Проблемы на сервере: данные устарели более чем на " + parseInt(delay / 60) + " мин.");
     }
-
+	
     var result = {success: true};
-
+	
     getParam('<font color="' + data.temperature_color + '">' + data.temperature + '°</font>', result, 'temperature_color');
     getParam(data.temperature, result, 'temperature');
-    getParam(data.temperature, result, 'temperature_int', null, null, parseFloat);
+    getParam(data.temperature, result, 'temperature_int', null, null, parseBalance);
     var ts = new Date(data.ts.data * 1000);
-    getParam(ts.getDate() + '.' + (ts.getMonth()+1 < 10 ? '0' : '') + (ts.getMonth()+1) + '.' + ts.getFullYear() + ' ' + ts.getHours() + ':' + (ts.getMinutes() < 10 ? '0' : '') + ts.getMinutes(), result, 'ts');
-
+    getParam(ts.getDate() + '.' + (ts.getMonth()+1 < 10 ? '0' : '') + (ts.getMonth()+1) + '.' + ts.getFullYear() + ' ' + ts.getHours() + ':' + (ts.getMinutes() < 10 ? '0' : '') + ts.getMinutes(), result, ['ts', '__tariff']);
+	
     if (prefs.show_link) {
         result['__tariff'] = "График: https://gradus.melda.ru/";
-    }
+    } else {
+		result['__tariff'] = result.ts;
+	}
 
     AnyBalance.setResult(result);
 }

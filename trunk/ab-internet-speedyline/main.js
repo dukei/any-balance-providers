952e5baa934cfc544b1ebc4633d70c82b00cfd6c
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-04-16 13:23:21 +0000

    Спиди-Лайн

--- trunk/ab-internet-speedyline/main.js
+++ trunk/ab-internet-speedyline/main.js
@@ -1,76 +1,49 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает информацию о состоянии счета интернет-провайдера Спиди-Лайн.
-Для корректной работы провайдера необходимо в личном кабинете
-в разделе Настройки включить опцию "Разрешить вход в личный кабинет
-с чужих ip адресов".
-
-Сайт оператора: http://www.speedyline.ru/
-Личный кабинет: http://web.speedyline.ru/
 */
 
 function main () {
     var prefs = AnyBalance.getPreferences ();
-    var baseurl = 'https://web.speedyline.ru/';
+    var baseurl = 'http://web.speedyline.ru/';
 
     checkEmpty (prefs.login, 'Введите логин');
     checkEmpty (prefs.password, 'Введите пароль');
 
-    AnyBalance.trace ('Trying to enter selfcare at address: ' + baseurl);
-    var html = AnyBalance.requestPost (baseurl, {
-        login: prefs.login,
-        password: prefs.password,
-        submit: 'Войти'
-        });
-
+    AnyBalance.trace('Trying to enter selfcare at address: ' + baseurl);
+    var html = AnyBalance.requestPost(baseurl, {
+    	login: prefs.login,
+    	password: prefs.password,
+    	submit: 'Войти'
+    });
     // Проверка на корректный вход
-    if(!/logout.html/i.test(html)){
-        // Проверка неправильной пары логин/пароль
-        var error = getParam(html, null, null, /class='no-border red'>[\s*]*([^<]+?)\s*</i, replaceTagsAndSpaces, html_entity_decode);
-        if (error) {
-            if (error.indexOf ('Доступ разрешен только с ip адресов') >= 0)
-                throw new AnyBalance.Error (error + ' Для корректной работы провайдера необходимо в личном кабинете в разделе Настройки включить опцию "Разрешить вход в личный кабинет с чужих ip адресов".');
-	    else if(error.indexOf('Логин или пароль указаны неверно') >= 0)
-                throw new AnyBalance.Error (error, null, true);
-            throw new AnyBalance.Error (error);
-	}
-    	
-        AnyBalance.trace ('Have not found logout... Unknown error. Please contact author.');
-        throw new AnyBalance.Error ('Не удалось войти в личный кабинет. Сайт изменен?');
+    if (!/logout.html/i.test(html)) {
+    	// Проверка неправильной пары логин/пароль
+    	var error = getParam(html, null, null, /class='no-border red'>[\s*]*([^<]+?)\s*</i, replaceTagsAndSpaces, html_entity_decode);
+    	if (error) {
+    		if (error.indexOf('Доступ разрешен только с ip адресов') >= 0)
+				throw new AnyBalance.Error(error + ' Для корректной работы провайдера необходимо в личном кабинете в разделе Настройки включить опцию "Разрешить вход в личный кабинет с чужих ip адресов".');
+    		else if (error.indexOf('Логин или пароль указаны неверно') >= 0) 
+				throw new AnyBalance.Error(error, null, true);
+			
+    		throw new AnyBalance.Error(error);
+    	}
+    	AnyBalance.trace(html);
+    	throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
     }
 
     AnyBalance.trace ('It looks like we are in selfcare...');
-    AnyBalance.trace ('Parsing data...');
 
     var result = {success: true};
-
-    // ID пользователя
-    getParam (html, result, 'id', /<th[^>]*>ID(?:\s|&nbsp;)*пользователя[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Клиент
-    getParam (html, result, 'client', /<th[^>]*>Клиент[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Скорость
-    getParam (html, result, 'speed', /<th[^>]*>Скорость[\s\S]*?<td[^>]*>([\s\S]*?)(?:<a|<\/td>)/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Скачано
-    getParam (html, result, 'download', /<th[^>]*>Скачано[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, function(str){return parseTraffic(str, 'мб')});
-
-    // Статус
-    getParam (html, result, 'state', /<th[^>]*>Состояние[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Интернет
-    getParam (html, result, 'stateinternet', /<th[^>]*>Интернет[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Текущий баланс
-    getParam (html, result, 'balance', /<th[^>]*>Текущий баланс[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-
-    // Кредит
-    getParam (html, result, 'credit', /<th[^>]*>Кредит[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-
-    // Тарифный план
-    getParam (html, result, '__tariff', /<th[^>]*>Тарифный план[\s\S]*?<tr[^>]*?>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    AnyBalance.setResult (result);
+	
+	getParam(html, result, 'id', /<th[^>]*>ID(?:\s|&nbsp;)*пользователя[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'client', /<th[^>]*>Клиент[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'speed', /<th[^>]*>Скорость[\s\S]*?<td[^>]*>([\s\S]*?)(?:<a|<\/td>)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'download', /<th[^>]*>Скачано[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, function(str) {return parseTraffic(str, 'мб')});
+	getParam(html, result, 'state', /<th[^>]*>Состояние[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'stateinternet', /<th[^>]*>Интернет[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /<th[^>]*>Текущий баланс[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'credit', /<th[^>]*>Кредит[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /<th[^>]*>Тарифный план[\s\S]*?<tr[^>]*?>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
 }

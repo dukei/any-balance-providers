9e29d34a0c886cbf0fce738a838b321a15a7335a
stark027 <stark027@gmail.com>
2014-04-16 12:36:15 +0000

    I-ghost.net

--- ab-service-ighost/main.js
+++ ab-service-ighost/main.js
@@ -7,25 +7,32 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36',
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://i-ghost.net/';
+	var baseurl = 'https://www.i-ghost.net/';
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 
-	var html = AnyBalance.requestGet(baseurl + 'auth/login', g_headers);
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+
+	var cookie = getParam(html, null, null, /document\.cookie='_ddn_intercept_2_=([^';]+)/i);
+	
+	checkEmpty(cookie, 'Не удалось обойти защиту от роботов, сайт изменен?', true);
+	AnyBalance.setCookie('www.i-ghost.net', '_ddn_intercept_2_', cookie);	
 	
 	html = requestPostMultipart(baseurl + 'auth/login', {
-		username: prefs.login,
-		password: prefs.password,
-		submit_login: ''
+		'username': prefs.login,
+		'password': prefs.password,
+		'submit_login': ''
 	}, addHeaders({Referer: baseurl + 'auth/login'}));
-
+	
+	//html = AnyBalance.requestGet(baseurl, g_headers);
+	
 	if (!/logout/i.test(html)) {
 		var error = sumParam(html, null, null, /color="red"(?:[^>]*>){1}([^<]+)/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
 		if (error && /Неверный логин или пароль/i.test(error))

fb42fe36a3c9523d54945cac75c1b07464f1a840
stark027 <stark027@gmail.com>
2014-05-06 08:26:51 +0000

    Домашний Интернет и Телевидение АКАДО

--- ab-internet-akado/main.js
+++ ab-internet-akado/main.js
@@ -6,59 +6,63 @@
 Личный кабинет: https://office.akado.ru/login.xml
 */
 
+var g_headers = {
+	'Accept': '*/*',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru,en;q=0.8',
+	'Connection': 'keep-alive',
+	'Origin': 'https://office.akado.ru',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36',
+};
+
+function getId(html) {
+	return getParam(html, null, null, /akado-request\s*([\dA-F\-]+)/i);
+}
+
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://office.akado.ru/';
-    
     AnyBalance.setDefaultCharset('utf-8');
-
-    var pass = hex_md5(prefs.password).toUpperCase(); //Пароль в md5
-    
-    // Заходим на главную страницу
-    var info = AnyBalance.requestPost(baseurl + 'login.xml/login', {
-        login: prefs.login,
-        password: pass
-    }/*, {
-        'X-Requested-With':'XMLHttpRequest',
-        'Origin':'https://office.akado.ru',
-	'Referer': baseurl + 'login.xml',
-	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.152 Safari/535.19'
-    }*/);
 	
-	if(!/splash-loading/i.test(info)){
-        var error = getParam(info, null, null, /<response[^>]*><message>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var baseurl = 'https://office.akado.ru/';
+	
+	var html = AnyBalance.requestGet(baseurl + 'user/login.xml?NeedLoggedOn', g_headers);
+	
+	html = requestPostMultipart(baseurl + 'user/login.xml', {
+		login: prefs.login,
+		password: prefs.password,
+	}, addHeaders({
+		Referer: baseurl + 'user/login.xml?NeedLoggedOn',
+		'X-Request': 'xml',
+		'X-Request-ID':getId(html)
+	}));
+	
+	if(!/Вы успешно вошли в Личный кабинет/i.test(html)){
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
 
     var result = {success: true};
 
-	if(AnyBalance.isAvailable('balance', 'agreement', 'username'))
-	{
-		var status = AnyBalance.requestGet(baseurl + 'status.xml');
-		getParam(status, result, 'balance', /account-balance-value[^>]*>([^<]*)/i, null, parseBalance);
-		getParam(status, result, 'username', /account-fullname[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-		getParam(status, result, 'agreement', /account-ID[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-    }	
-
+	getParam(html, result, 'balance', /balance="([^"]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'username', /<account([\s\S]*?)\/>/i, [replaceTagsAndSpaces, /surname="([^"]+)"\s*name="([^"]+)"\s*patronymic="([^"]+)[\s\S]*/i, '$1 $2 $3'], html_entity_decode);
+	getParam(html, result, 'agreement', /crc="([^"]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
     if(AnyBalance.isAvailable('payhint')){
-		var account = AnyBalance.requestGet(baseurl + 'account.xml');
-		getParam(account, result, 'payhint', /<[^>]*"amount[^>]*>([\s\S]{1,200})<\/span>\s*<span class="description">\s*Рекомендуемая сумма оплаты услуг в следующем месяце/i, null, parseBalance);
+		html = AnyBalance.requestGet(baseurl + 'finance/prepay.xml');
+		
+		getParam(html, result, 'payhint', /prepay amount="([^"]+)/i, replaceTagsAndSpaces, parseBalance);
 	}
 
-    var services = AnyBalance.requestGet(baseurl + 'services.xml');
-
-	if(/УСЛУГИ ИНТЕРНЕТ/i.test(services)){
-		var servid = getParam(services, null, null, /<a href="\?ID=([^"&]*)">УСЛУГИ ИНТЕРНЕТ<\/a>/i, null, html_entity_decode);
-		services = AnyBalance.requestGet(baseurl + 'services.xml?ID='+servid+'&depth=1');
-		var tariff = getParam(services, null, null, /<th>\s*<a href[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-		if(tariff){
-			var res = /\(([^,]*)/.exec(tariff);
-			if(res)
-				tariff = res[1];
-			result.__tariff = tariff;
-		}
-	}
+    html = AnyBalance.requestGet(baseurl + 'services/display.xml');
+	
+	sumParam(html, result, '__tariff', /<service[^>]*name="([^"]+)[^>]*">/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
+	
     AnyBalance.setResult(result);
 }

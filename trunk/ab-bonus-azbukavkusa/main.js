0faf21d7c6a90f9ebac9bf4557d4e4814595b356
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-05-12 09:56:07 +0000

    Азбука Вкуса (Бонусы)

--- trunk/ab-bonus-azbukavkusa/main.js
+++ trunk/ab-bonus-azbukavkusa/main.js
@@ -10,50 +10,46 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
+var g_cards = ['АВ-OLD', 'АВ', 'АВ9', 'ВК', 'СТ', 'ЭБВГ', 'АВЭ', 'M2M', 'ДС', 'КВ', 'ЛО'];
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://www.av.ru/';
+	var baseurl = 'http://av.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
-
+	
 	checkEmpty(prefs.login, 'Введите номер карты!');
-
-	var html = AnyBalance.requestGet(baseurl + 'index.aspx?sPage=42', g_headers);
-
-	var captchaa;
-	if(AnyBalance.getLevel() >= 7){
-		AnyBalance.trace('Пытаемся ввести капчу');
-		var captcha = AnyBalance.requestGet(baseurl+ 'img.aspx?21108');
-		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
-		AnyBalance.trace('Капча получена: ' + captchaa);
-	}else{
-		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'personal/discount_cards/', g_headers);
+	
+	var found = false;
+	for(var i = 0; i < g_cards.length; i++) {
+		html = AnyBalance.requestPost(baseurl + 'udata/avemarket/request_value_proc/', {
+			'type_card': g_cards[i],
+			'number_card': prefs.login,
+		}, addHeaders({
+			Referer: baseurl + 'personal/discount_cards/',
+			'X-Requested-With': 'XMLHttpRequest'
+		}));
+		
+		if(found = !/Карта не найдена/i.test(html))
+			break;
 	}
-
-	html = AnyBalance.requestPost(baseurl + 'index.aspx?sPage=42', {
-		't': 1,
-		n: prefs.login,
-		vv:captchaa,
-		send: 'Проверить информацию'
-	}, addHeaders({Referer: baseurl + 'index.aspx?sPage=42'}));
-
-	if (!/<p>\s*Размер\s+скидки\s+по\s+карте/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-	}
+	
+	if (!found)
+		throw new AnyBalance.Error('Не удалось получить данные по карте, сайт изменен?');
+	
 	var result = {success: true};
 	
-	getParam(html, result, '__tariff', /Размер скидки по карте[^>]*>\s*(№\d+)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, ['discount', 'next_discount'], /составляет\s*(\d+)\s*%/i, replaceTagsAndSpaces, parseBalance);
+	getParam(prefs.login, result, '__tariff');
+	getParam(html, result, 'discount', /<udata[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	
-	if(isset(result.discount)) {
-		var next = getParam(html, null, null, /для увеличения скидки на\s*(\d+)\s*%/i, replaceTagsAndSpaces, parseBalance);
-		getParam(next + result.discount, result, 'next_discount');
-	}
+	// if(isset(result.discount)) {
+		// var next = getParam(html, null, null, /для увеличения скидки на\s*(\d+)\s*%/i, replaceTagsAndSpaces, parseBalance);
+		// getParam(next + result.discount, result, 'next_discount');
+	// }
 	
-	getParam(html, result, 'to_reach_next_discount', /необходимо совершить покупок([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'last_purch_date', /Дата последней покупки:([^<]*)/i, replaceTagsAndSpaces, parseDate);
+	// getParam(html, result, 'to_reach_next_discount', /необходимо совершить покупок([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	// getParam(html, result, 'last_purch_date', /Дата последней покупки:([^<]*)/i, replaceTagsAndSpaces, parseDate);
 	
 	AnyBalance.setResult(result);
 }

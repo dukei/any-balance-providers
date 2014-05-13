7ec58a88ac9284a065010a0100123e6086099a5f
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-05-13 13:18:07 +0000

    Новый провайдер: Ace Stream

---
+++ trunk/ab-service-acestream/main.js
@@ -0,0 +1,55 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://accounts.acestream.net/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'login_check?pa=1', g_headers);
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == '_username') 
+			return prefs.login;
+		else if (name == '_password')
+			return prefs.password;
+
+		return value;
+	});
+	
+	try {
+		html = AnyBalance.requestPost(baseurl + 'login_check?pa=1', params, addHeaders({Referer: baseurl + 'login_check?pa=1'}));
+	} catch(e) {
+		html = AnyBalance.requestGet('https://user317797.acestream.net/', g_headers);
+	}
+	
+	if (!/\/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Баланс\s*:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['currency', 'balance'], /Баланс\s*:([^<]+)/i, replaceTagsAndSpaces, parseCurrency);
+	getParam(html, result, 'days', /No ADs\s*<\/h5>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['days_cur', 'days'], /No ADs\s*<\/h5>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseCurrency);	
+	
+	AnyBalance.setResult(result);
+}

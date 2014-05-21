88c5ad956ab06cfcc8872586086f19a36da0849e
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-05-21 09:13:56 +0000

    Autodoc.ru

--- trunk/ab-shop-autodoc/main.js
+++ trunk/ab-shop-autodoc/main.js
@@ -43,7 +43,28 @@ function main() {
 	if(isAvailable('balance')) {
 		html = AnyBalance.requestGet(baseurl + 'Web/Pages/BalanceForm.aspx?type=1', g_headers);
 		
-		getParam(html, result, 'balance', /"Баланс с учётом получения всех заказанных деталей"(?:[^>]*>){6}([\s\d,.]*)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'balance', /"Баланс с учётом получения всех заказанных деталей"(?:[^>]*>){6}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	}
+	
+	if(isAvailable('orders_details') && prefs.order_num) {
+		var dt = new Date();
+		
+		html = AnyBalance.requestGet(baseurl + 'Web/Pages/ordersSubClients.aspx?m=Dvigenie&OrderNumber='+prefs.order_num+'&Article=&From=14.05.2012&To='+dt.getDate()+'.'+(dt.getMonth()+1)+'.'+dt.getFullYear()+'&StateID=0&p=p1week&m=Dvigenie', g_headers);
+		
+		var rows = sumParam(html, null, null, /<tr class="gridItemStyleOp" style="background-color:#(?:CCFFCD|b2e59f)">(?:[^>]*>){20,30}\s*<\/tr>/ig);
+		var all = '<b>Заказ №' + prefs.order_num + '</b><br/>';
+		if(rows) {
+			for(var i = 0; i < rows.length; i++) {
+				var name = getParam(rows[i], null, null, /"detal"[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+				//var sum = getParam(rows[i], null, null, /"order_num"[^>]*>([\d\s.,]*)<\/td>\s*<td class="comment/i, replaceTagsAndSpaces, parseBalance);
+				var title = getParam(rows[i], null, null, /"comment tipp"[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+				//var comment = getParam(rows[i], null, null, /"comment"[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+				var awaiting = getParam(rows[i], null, null, /<td style="width:100px">([\s\S]*?)<\//i, replaceTagsAndSpaces) || 'Неизвестно';				
+				
+				all += name + ': ' + title + ' - Ожидается: ' + awaiting + '<br/>';
+			}
+			getParam(all, result, 'orders_details', null, [/<br\/>$/, '']);
+		}
 	}
 	
 	AnyBalance.setResult(result);

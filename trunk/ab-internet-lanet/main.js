6783bc4cd1adef0603a8c7e95d7da87918785fbe
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-05-26 11:09:11 +0000

    Ланет Нетворк (Киев)

--- trunk/ab-internet-lanet/main.js
+++ trunk/ab-internet-lanet/main.js
@@ -1,51 +1,6 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плане для киевского интернет-провайдера lanet
-
-Сайт оператора: http://lanet.ua
-Личный кабинет: https://my.lanet.ua
 */
-// Парсит дату из такого вида в мс 27 июля 2013
-function parseDateMoment(str){
-	AnyBalance.trace('Original input: ' + str);
-	var found = /(\d{1,2})\s*([\s\S]*?)\s*(\d{1,4})/i.exec(str);
-	if(found)
-	{
-		var date = new Date();
-		var day = found[1];
-		var month = found[2];
-
-		if(month == 'января')
-			month = '01';
-		else if(month == 'Фев')
-			month = '02';
-		else if(month == 'Мар')
-			month = '03';
-		else if(month == 'Апр')
-			month = '04';
-		else if(month == 'Мая')
-			month = '05';
-		else if(month == 'Июня')
-			month = '06';
-		else if(month == 'Июля')
-			month = '07';
-		else if(month == 'Авг')
-			month = '08';
-		else if(month == 'Сен')
-			month = '09';
-		else if(month == 'Окт')
-			month = '10';
-		else if(month == 'Ноя')
-			month = '11';
-		else if(month == 'Дек')
-			month = '12';
-
-		return getParam(day+'.'+month+'.'+ date.getFullYear(), null, null, null, replaceTagsAndSpaces, parseDate);
-	}
-	else
-		AnyBalance.trace('Failed to parse date from ' + str);
-}
 
 var g_headers = {
 	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -70,10 +25,12 @@ function main(){
 	});
     html = AnyBalance.requestPost(baseurl + 'login.php#login', params, g_headers);
 
-    if(!/client_info.php\?my_menu_selected=ex#my_menu/.test(html)){
+    if(!/"logoff.php"/.test(html)){
         var error = getParam(html, null, null, /<td[^>]+class="form_error"[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
+			
+		AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
 
@@ -84,9 +41,8 @@ function main(){
     getParam(html, result, 'pay', /нехватка[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'agreement', /&#8470; договора[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, '__tariff', /тип подключения[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'paytill', /конец периода[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseDateMoment);
+    getParam(html, result, 'paytill', /конец периода[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, [replaceTagsAndSpaces, /\d+:\d+/i, ''+new Date().getFullYear()], parseDateWord);
     getParam(html, result, 'userName', /<td[^>]+class="cname"[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
-
-
+	
     AnyBalance.setResult(result);
 }

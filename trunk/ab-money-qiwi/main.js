734b3b249b102f5feac392611bd6e06347bb29a4
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-05-29 10:08:54 +0000

    QIWI Кошелек

--- trunk/ab-money-qiwi/main.js
+++ trunk/ab-money-qiwi/main.js
@@ -64,29 +64,26 @@ function parseCurrencyMy(text){
 function mainNew () {
     var prefs = AnyBalance.getPreferences ();
     var baseurl = 'https://w.qiwi.com/';
-
-    if (!prefs.login)
-        throw getFatalError ('Введите номер телефона');
-
-    if (!prefs.password)
-        throw getFatalError ('Введите пароль');
-
+	
+	checkEmpty(prefs.login, 'Введите номер телефона!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     AnyBalance.requestGet(baseurl + 'payment/main.action'); //Надо сессию поставить
-
+	
     AnyBalance.trace ('Trying to enter NEW account at address: ' + baseurl);
-
+	
     var login = /^\s*\+/.test(prefs.login) ? prefs.login : '+7' + prefs.login;
     var params = {
         source: 'MENU',
         login: login,
         password: prefs.password
     };
-
+	
     var info = AnyBalance.requestPost (baseurl + 'auth/login.action', params, addHeaders({Accept: 'application/json, text/javascript', 'X-Requested-With':'XMLHttpRequest'}));
     AnyBalance.trace ('Login result: ' + info);
-
+	
     var res = getJson(info);
-
+	
     if(res.code.value == '7'){
         AnyBalance.trace('Требуется ввести токен авторизации');
         params.loginToken = res.data.token;
@@ -95,32 +92,35 @@ function mainNew () {
 
         res = getJson(info);
     }
-
+	
     // Проверка ошибки входа
-    if (res.code.value != '0'){
-	var errors = {
-		0 : "ОК",
-		1 : "Ошибка",
-		2 : "Ошибка",
-		3 : "Требуется авторизация",
-		4 : "Требуется подтверждение",
-		5 : "Требуется подтверждение",
-		6 : "Срок действия пароля истек",
-                7 : "Требуется ввод токена авторизации",
-		success : "Успех",
-		timeout : "Время соединения истекло. Проверьте подключение к сети",
-		error : "Ошибка соединения. Проверьте подключение к сети",
-		abort : "Запрос отменен",
-		parsererror : "Синтаксическая ошибка"
-	};
-
-        throw new AnyBalance.Error (res.message || (res.messages ? res.messages.join('\n') : 'Не удаётся войти в QIWI кошелек: ' + (errors[res.code.value] || res.code._NAME)));
+    if (res.code.value != '0') {
+    	var errors = {
+    		0: "ОК",
+    		1: "Ошибка",
+    		2: "Ошибка",
+    		3: "Требуется авторизация",
+    		4: "Требуется подтверждение",
+    		5: "Требуется подтверждение",
+    		6: "Срок действия пароля истек",
+    		7: "Требуется ввод токена авторизации",
+    		success: "Успех",
+    		timeout: "Время соединения истекло. Проверьте подключение к сети",
+    		error: "Ошибка соединения. Проверьте подключение к сети",
+    		abort: "Запрос отменен",
+    		parsererror: "Синтаксическая ошибка"
+    	};
+		
+		var errors = res.message || (res.messages ? res.messages.join('\n') : 'Не удаётся войти в QIWI кошелек: ' + (errors[res.code.value] || res.code._NAME));
+    	throw new AnyBalance.Error(errors, null, /Неверный логин или пароль/i.test(errors));
     }
-
+	
     var html = AnyBalance.requestGet (baseurl + 'payment/main.action');
-
+	
+	AnyBalance.trace ('It looks like we are in selfcare...');
+	
     // Проверка на корректный вход
-    if (/id="person-accounts-/i.exec(html)){
+    /*if (/id="person-accounts-/i.exec(html)){
     	AnyBalance.trace ('It looks like we are in selfcare...');
         if(!getParam(html, null, null, /(profileBalance)/i)){
             //Похоже, проблема с паролем, устарел, наверное.
@@ -134,33 +134,27 @@ function mainNew () {
     }else {
         AnyBalance.trace ('Have not found logout... Unknown error. Please contact author.');
         throw new AnyBalance.Error ('Неизвестная ошибка. Пожалуйста, свяжитесь с автором провайдера.');
-    }
-
+    }*/
+	
     var result = {success: true};
-
-    // Баланс
+	
     getParam (html, result, 'balance', /<option[^>]+id="person-accounts-\w+"[^>]*selected[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    // Это не всегда работает на девайсе, хотя в ебугере все отлично
-	//getParam (html, result, ['currency', 'balance'], /<option[^>]+id="person-accounts-\w+"[^>]*selected[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseCurrencyMy);
 	getParam (html, result, ['currency', 'balance'], /<option[^>]+id="person-accounts-\w+"[^>]*selected[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseCurrencyMy);
-
-    // Сообщения (че-то непонятно, где они в новом личном кабинете)
-    //getParam (html, result, 'messages', /Сообщения:.*?>(\d+)/i, [], parseInt);
 	
     if(AnyBalance.isAvailable('bills')){
         html = AnyBalance.requestGet(baseurl + 'user/order/main.action?type=1');
+		
         var count = html.match(/<div[^>]*ordersLine\s+NOT_PAID[^>]*>/ig);
         result.bills = count ? count.length : 0;
     }
-
+	
     if(AnyBalance.isAvailable('messages')){
         html = AnyBalance.requestGet(baseurl + 'user/message/content/loadlist.action');
         var count = html.match(/<li[^>]+data-container-name="item"[^>]*class="unread"/ig);
         result.messages = count ? count.length : 0;
     }
 	// QVC
-	if(AnyBalance.isAvailable('qvc_card'))
-	{
+	if(AnyBalance.isAvailable('qvc_card')) {
         html = AnyBalance.requestGet(baseurl + 'qvc/main.action');
 		
 		var card = getParam (html, result, 'qvc_card', /Номер карты:[\s\S]{1,70}value">\s*([\s\S]*?)\s*<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -175,8 +169,7 @@ function mainNew () {
 		html = AnyBalance.requestGet(baseurl + 'qvc/reports.action?number='+card+'&daterange=true&start=24.06.2013&finish='+day+'.'+month+'.'+yr);
 		
 		var element = getParam (html, null, null, /(<div class="reportsLine">[\s\S]*?<div class="clearBoth">)/i, null, html_entity_decode);
-		if(element)
-		{
+		if(element) {
 			var seller = getParam (html, null, null, /<div class="comment">([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
 			var ammount = getParam (html, null, null, /<div class="cash">(-?\d[\d\s]*[.,]?\d*[.,]?\d*)/i, null, null);
 			
@@ -187,7 +180,7 @@ function mainNew () {
 			getParam (all, result, 'qvc_last', null, null, null);
 		}
 		else
-			AnyBalance.trace('Не нашли ни одной транзакции');
+			AnyBalance.trace('Не нашли ни одной транзакции!');
     }
     AnyBalance.setResult (result);
 }

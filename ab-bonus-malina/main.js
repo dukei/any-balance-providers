af6f864c69d4d7e63f5a4c0e1135047b3e22e25c
stark027 <stark027@gmail.com>
2014-05-30 07:50:34 +0000

    Малина

--- ab-bonus-malina/main.js
+++ ab-bonus-malina/main.js
@@ -1,87 +1,77 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает информацию по баллам на карте накопительной программы Малина.
-
-Сайт бонусной программы: http://www.malina.ru
-Персональная страница: http://catalog.malina.ru/login.php
 */
 
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main () {
     var prefs = AnyBalance.getPreferences ();
-    var baseurl = 'https://www.malina.ru/';
+    var baseurl = 'http://malina.ru/';
 
-    if (!prefs.login || prefs.login == '')
-        throw new AnyBalance.Error ('Введите логин');
-
-    if (!prefs.password || prefs.password == '')
-        throw new AnyBalance.Error ('Введите пароль');
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');	
 
     AnyBalance.trace ('Trying to enter selfcare at address: ' + baseurl);
-    AnyBalance.requestGet (baseurl + 'login.php');  // Запрос необходим для формирования cookie с регионом MSK
-    var html = AnyBalance.requestPost (baseurl + 'login.php', {
-        login: prefs.login,
-        password: prefs.password,
-        backurl: ''
-    });
-
-    // Проверка на корректный вход
-    if(!/\/logout.php/i.test(html)){
-        // Проверка неправильной пары логин/пароль
-        var error = getParam(html, null, null, /<div[^>]+id="alert"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти на персональную страницу. Сайт изменен?');
-    }
-
+	
+	try {
+		var html = AnyBalance.requestGet (baseurl + 'msk/', g_headers);  // Запрос необходим для формирования cookie с регионом MSK
+		
+		html = AnyBalance.requestPost(baseurl + 'msk/pp/login/', {
+			csrfmiddlewaretoken:getParam(html, null, null, /csrfmiddlewaretoken[^>]*value=["']([^"']+)["']/i),
+			contact: prefs.login,
+			password: prefs.password,
+			'next': ''
+		}, addHeaders({'X-Requested-With':'XMLHttpRequest'}));
+		
+		// Проверка на корректный вход
+		if(!/"status"\s*:\s*"ok"/i.test(html)){
+			// Проверка неправильной пары логин/пароль
+			var error = getParam(html, null, null, /<div[^>]+id="alert"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+			if(error)
+				throw new AnyBalance.Error(error);
+			throw new AnyBalance.Error('Не удалось зайти на персональную страницу. Сайт изменен?');
+		}	
+	} catch(e) {
+		
+	}
+	
+	html = AnyBalance.requestGet (baseurl + 'pp/', g_headers);
+	
     AnyBalance.trace ('It looks like we are in selfcare...');
-
-    AnyBalance.trace ('Parsing data...');
-    
-    matches = /class="points"/.exec (html);
-    if (!/class="points"/i.test(html))
-        throw new AnyBalance.Error ('Невозможно найти информацию об аккаунте, свяжитесь с автором');
-
+	
+    if (!/class="acc-points"/i.test(html))
+        throw new AnyBalance.Error ('Невозможно найти информацию об аккаунте, свяжитесь с разработчиками!');
+	
+	AnyBalance.trace ('Parsing data...');
+	
     var result = {success: true};
-  
-    // Номер счета
-    getParam (html, result, 'accountNumber', /<th[^>]*>Номер счета[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Владелец счета
-    getParam (html, result, 'customer', /<th[^>]*>Владелец счета[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    // Статус счета
-    getParam (html, result, 'status', /<th[^>]*>Статус счета[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
+	
+	getParam (html, result, 'balance', /"acc-points"(?:[^>]*>){2}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+	getParam (html, result, 'accountNumber', /Номер счета(?:[^>]*>){2}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam (html, result, 'customer', /Владелец счета(?:[^>]*>){2}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam (html, result, 'status', /Статус счета(?:[^>]*>){2}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam (html, result, 'availableForPay', /Доступно к обмену на товары и услуги(?:[^>]*>){2}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+	
     // Накоплено основных баллов
     //getParam (html, result, 'mainPoints', /<th[^>]*>Владелец счета[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
     // Накоплено EXPRESS-баллов
     //getParam (html, result, 'expressPoints', $table, 'tr:contains("EXPRESS") td', parseBalance);
-
     // Израсходовано баллов
     //getParam (html, result, 'gonePoints', $table, 'tr:contains("Израсходовано баллов") td', parseBalance);
-
     // Сгорело баллов
     //getParamFind (result, 'burnPoints', $table, 'tr:contains("Сгорело баллов") td', parseBalance);
-
-    // Баланс баллов
-    getParam (html, result, 'balance', /<th[^>]*>Баланс баллов[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-
-    // Доступно к оплате у партнеров
-    getParam (html, result, 'availableForPay', /<th[^>]*>Доступно к обмену на товары и услуги[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-
+	
     if (AnyBalance.isAvailable ('burnInThisMonth')) {
+        html = AnyBalance.requestGet (baseurl + 'pp/forecast/');
 
-        AnyBalance.trace ('Fetching balance structure...');
-
-        html = AnyBalance.requestGet (baseurl + 'siebel/personal/forecast/');
-
-        AnyBalance.trace ('Parsing balance structure...');
-
-        getParam(html, result, 'burnInThisMonth', /Баллов, которые сгорят в ближайшие месяцы —\s*(\d+)/i, null, parseBalance);
+        getParam(html, result, 'burnInThisMonth', /Баллов, которые сгорят в ближайшие месяцы([^<]+)/i, null, parseBalance);
     }
-
+	
     AnyBalance.setResult (result);
 }

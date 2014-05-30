fa387328070d74d4a8fc9729c3fa4e31950702bf
stark027 <stark027@gmail.com>
2014-05-30 08:51:39 +0000

    Новый - Телеком-МК (Нижний Новгород)

---
+++ ab-internet-telecom-mk/main.js
@@ -0,0 +1,48 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://abonent.telecom-mk.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+	
+	html = AnyBalance.requestPost(baseurl + 'login', {
+		'lqp2sw':0,
+		'mqp2sw':true,
+		'redirect':'',
+		'login_remote3ftqtw':prefs.login,
+		'password_remote4ftqtw':prefs.password,
+		'action.remote_login.8mp2sw.x':7,
+		'action.remote_login.8mp2sw.y':4,
+	}, addHeaders({Referer: baseurl + 'login'}));
+	
+	if (!/images\/btn_exit.gif/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Итого на \d+\.\d+\.\d{4}(?:[^>]*>){3}([\s\S]*?)<\//i, [replaceTagsAndSpaces, /долг|задолже/, '-'], parseBalance);
+	getParam(html, result, 'account', /Договор([\s\d]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

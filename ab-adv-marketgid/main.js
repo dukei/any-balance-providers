e543878c96e353cd5a280caa218cf5b9b1d69cde
stark027 <stark027@gmail.com>
2014-06-04 11:54:42 +0000

    МаркетГид

--- ab-adv-marketgid/main.js
+++ ab-adv-marketgid/main.js
@@ -12,19 +12,14 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://my.marketgid.com/';
+    var baseurl = 'https://dashboard.marketgid.com/';
     AnyBalance.setDefaultCharset('utf-8'); 
 
     var html = AnyBalance.requestGet(baseurl + 'user/signin', g_headers);
-/*
-    var tform = getParam(html, null, null, /<input[^>]+name="t:formdata"[^>]*value="([^"]*)/i, null, html_entity_decode);
-    if(!tform) //Если параметр не найден, то это, скорее всего, свидетельствует об изменении сайта или о проблемах с ним
-        throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
-*/
+	
 	html = AnyBalance.requestPost(baseurl + 'user/signin', {
-        //'t:formdata':tform,      //Передаём все параметры, которые мы отследили в браузере в активности сети
-        signinLogin:prefs.login,
-        signinPassword:prefs.password,
+        login:prefs.login,
+        password:prefs.password,
         signin:'Войти'
     }, addHeaders({Referer: baseurl + 'user/signin'})); 
 	
@@ -34,8 +29,15 @@ function main(){
             throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
-    var result = {success: true};
-    getParam(html, result, 'balance', /Заработок[^<]*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+    
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', />\s*Нераспределенный заработок:(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance_total', />\s*Общий заработок:(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance30', />\s*Заработок за 30 дней:(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	
+	
+	/*
 	getParam(html, result, 'wallet', /МГ-кошелёк[^<]*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, ['currency','balance'], /Заработок[^<]*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseCurrency);
 	// Клики
@@ -50,6 +52,6 @@ function main(){
 	getParam(html, result, 'forclick', /Итого по выбранным информерам(?:[\s\S]*?<td[^>]*>){6}\s*([\s\S]*?)\s*<\/td>/i, replaceTagsAndSpaces, parseBalance);
 	//Заработок
 	getParam(html, result, 'earnings', /Итого по выбранным информерам(?:[\s\S]*?<td[^>]*>){7}\s*([\s\S]*?)\s*<\/td>/i, replaceTagsAndSpaces, parseBalance);
-
+*/
     AnyBalance.setResult(result);
 }

7b650f4365b479620501e897dc7a8d4397599552
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-06-05 12:02:14 +0000

    Новиком банк

--- trunk/ab-money-novikom/main.js
+++ trunk/ab-money-novikom/main.js
@@ -3,48 +3,66 @@
 */
 
 function sleep(delay) {
-   AnyBalance.trace('Sleeping ' + delay + ' ms');
-   if(AnyBalance.getLevel() < 6){
-      var startTime = new Date();
-      var endTime = null;
-      do {
-          endTime = new Date();
-      } while (endTime.getTime() - startTime.getTime() < delay);
-   }else{
-      AnyBalance.trace('Calling hw sleep');
-      AnyBalance.sleep(delay);
-   }
-} 
+	AnyBalance.trace('Sleeping ' + delay + ' ms');
+	if (AnyBalance.getLevel() < 6) {
+		var startTime = new Date();
+		var endTime = null;
+		do {
+			endTime = new Date();
+		} while (endTime.getTime() - startTime.getTime() < delay);
+	} else {
+		AnyBalance.trace('Calling hw sleep');
+		AnyBalance.sleep(delay);
+	}
+}
 
-function encryptPass(pass, map){
-	if(map){
-		var ch='',i=0,k=0,TempPass='',PassTemplate=map.split(','), Pass='';
-		TempPass=pass;
-		while(TempPass!=''){
-			ch=TempPass.substr(0, 1);
+function encryptPass(pass, map) {
+	if (map) {
+		var ch = '',
+			i = 0,
+			k = 0,
+			TempPass = '',
+			PassTemplate = map.split(','),
+			Pass = '';
+		TempPass = pass;
+		while (TempPass != '') {
+			ch = TempPass.substr(0, 1);
 			k = ch.charCodeAt(0);
-			if(k>0xFF) k-=0x350;
-			if(k==7622) k=185;
-			TempPass=TempPass.length>1?TempPass.substr(1, TempPass.length):'';
-			if(Pass!='')Pass=Pass+';';
-			Pass=Pass+PassTemplate[k];
+			if (k > 0xFF) k -= 0x350;
+			if (k == 7622) k = 185;
+			TempPass = TempPass.length > 1 ? TempPass.substr(1, TempPass.length) : '';
+			if (Pass != '') Pass = Pass + ';';
+			Pass = Pass + PassTemplate[k];
 		}
-                return Pass;
-	}else{
+		return Pass;
+	} else {
 		return pass;
 	}
 }
 
 var g_headers = {
-    'Accept-Language': 'ru, en',
-    BSSHTTPRequest:1,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
+	'Accept-Language': 'ru, en',
+	BSSHTTPRequest: 1,
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
 }
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://retail.novikom.ru/v2/cgi/bsi.dll?';
+    var baseurl = 'https://retail.novikom.ru/'; //   v2/cgi/bsi.dll?
     
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	// Откроем первую страницу, чтобы получить ссылку
+	var html = AnyBalance.requestGet(baseurl);
+	
+	var href = getParam(html, null, null, /(v\d+\/cgi\/bsi\.dll\?)/i);
+	if(!href) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти ссылку на вход, сайт изменен?');
+	}
+	baseurl += href;
+	
     var html = AnyBalance.requestGet(baseurl + 'T=RT_2Auth.BF');
     var mapId = getParam(html, null, null, /<input[^>]*name="MapID"[^>]*value="([^"]*)"/i);
     var map = getParam(html, null, null, /var\s+PassTemplate\s*=\s*new\s+Array\s*\(([^\)]*)/i);
@@ -78,26 +96,27 @@ function main(){
         T:'rt_0clientupdaterest.doheavyupd'
     }, g_headers);
 	
-    /*var i=0;
-    do{
-        AnyBalance.trace('Ожидание обновления данных: ' + (i+1));
-        html = AnyBalance.requestPost(baseurl, {
-            SID:jsonInfo.SID,
-            tic:1,
-            T:'rt_0clientupdaterest.CheckForAcyncProcess'
-        }, addHeaders({Referer: baseurl+'T=RT_2Auth.BF'}));
-
-        var opres = getParam(html, null, null, /^\s*(?:<BSS_ERROR>([\s\S]*?)<\/BSS_ERROR>)?([\s\S]*>)?\d+\s*$/i, replaceTagsAndSpaces, html_entity_decode);
-        if(opres){
-            AnyBalance.trace('Обновление данных закончено. ' + opres);
-            break; //Всё готово, надо получать баланс
-        }
-        if(++i > 10){  //На всякий случай не делаем больше 10 попыток
-            AnyBalance.trace('Не удалось за 10 попыток обновить баланс, получаем старое значение...');
-            break;
-        }
-        sleep(3000);
-    }while(true);*/
+    var i = 0;
+    do {
+    	AnyBalance.trace('Ожидание обновления данных: ' + (i + 1));
+    	html = AnyBalance.requestPost(baseurl, {
+    		SID: jsonInfo.SID,
+    		tic: 1,
+    		T: 'rt_0clientupdaterest.CheckForAcyncProcess'
+    	}, addHeaders({
+    		Referer: baseurl + 'T=RT_2Auth.BF'
+    	}));
+    	var opres = getParam(html, null, null, /^\s*(?:<BSS_ERROR>([\s\S]*?)<\/BSS_ERROR>)?([\s\S]*>)?\d+\s*$/i, replaceTagsAndSpaces, html_entity_decode);
+    	if (opres) {
+    		AnyBalance.trace('Обновление данных закончено. ' + opres);
+    		break; //Всё готово, надо получать баланс
+    	}
+    	if (++i > 10) { //На всякий случай не делаем больше 10 попыток
+    		AnyBalance.trace('Не удалось за 10 попыток обновить баланс, получаем старое значение...');
+    		break;
+    	}
+    	sleep(3000);
+    } while (true);
 	
     if(prefs.type == 'acc')
         fetchAccount(jsonInfo, baseurl);

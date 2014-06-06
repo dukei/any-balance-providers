6ada94313b5aa594d806fa12bb961fd1bc5ba342
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-06-06 13:25:10 +0000

    Зеленая точка

--- trunk/ab-internet-zelenayatochka/main.js
+++ trunk/ab-internet-zelenayatochka/main.js
@@ -10,14 +10,57 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
+var g_regions = {
+	'stavr': getStavr,
+	'nal': getNal,
+};
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://personal.ooonet.ru/';
+	
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	// По умолчанию ставрополь
+	if(!prefs.region)
+		prefs.region = 'stavr';
+	
+	AnyBalance.trace('Регион: ' + prefs.region);
+	g_regions[prefs.region](prefs);
+}
 
+function getNal(prefs) {
+	var baseurl = 'http://abonent.naltel.ru/';
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+
+	html = AnyBalance.requestPost(baseurl, {
+		login: prefs.login,
+		password: prefs.password,
+	}, addHeaders({Referer: baseurl}));
+
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<p style='color:red'>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+		if (error && /Неверно указаны логин или пароль/i.test(error))
+			throw new AnyBalance.Error(error, null, true);
+		if (error)
+			throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	var result = {success: true};
+	
+	getParam(html, result, 'fio', /ФИО(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Баланс(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'cred_balance', /Кредит(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}
+
+function getStavr(baseurl, prefs) {
+	var baseurl = 'https://personal.ooonet.ru/';
+	
 	var html = AnyBalance.requestGet(baseurl, g_headers);
 
 	html = AnyBalance.requestPost(baseurl, {

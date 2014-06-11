561ae85588f4cd1742d4c08331503fb07d4e0683
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-06-11 13:23:00 +0000

    Московский паркинг

--- trunk/ab-municipal-parking/main.js
+++ trunk/ab-municipal-parking/main.js
@@ -12,39 +12,64 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     var baseurl = 'https://parkingcab.mos.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
 	
 	var html = AnyBalance.requestGet(baseurl, g_headers);
 	
-    html = AnyBalance.requestPost(baseurl + 'local/MPGU.php', {
-		module:'',
-		login:'Войти в личный кабинет',
-		_reqNo:0		
-	}, g_headers);
+	try {
+		html = AnyBalance.requestPost(baseurl + 'local/MPGU.php', {
+			module:'',
+			login:'Войти в личный кабинет',
+			_reqNo:0		
+		}, g_headers);
+		
+		//AnyBalance.trace(html);
+		
+		html = AnyBalance.requestPost('https://login.mos.ru/eaidit/eaiditweb/outerlogin.do', {
+			username:prefs.login,
+			password:prefs.password,
+		}, addHeaders({Referer: 'https://login.mos.ru/eaidit/eaiditweb/openouterlogin.do'})); 
+	} catch (e) {}
 	
-	AnyBalance.trace(html);
-	html = AnyBalance.requestPost('https://login.mos.ru/eaidit/eaiditweb/outerlogin.do', {
-        username:prefs.login,
-        password:prefs.password,
-    }, addHeaders({Referer: 'https://login.mos.ru/eaidit/eaiditweb/openouterlogin.do'})); 
-	
-	AnyBalance.trace(html);
+	//AnyBalance.trace(html);
     if(!/logout/i.test(html)){
         /*var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);*/
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
-    var result = {success: true};
+    
+	var result = {success: true};
+	
     getParam(html, result, 'balance', /Текущий баланс[^>]*>[^>]*>[^>]*>[^>]*>[^>]*balance">([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	
+	html = AnyBalance.requestGet(baseurl + '?page=accountData', g_headers);
+
+	var trs = sumParam(html, null, null, /<tr>\s*<td[^>]+toggle-details(?:[^>]+>){18}\s+<\/tr>/ig);
+		
+	var text = '';
+	for(var i = 0; i < trs.length; i++) {
+		var name = getParam(trs[i], null, null, /([^>]+>){7}/i, replaceTagsAndSpaces);
+		var region = getParam(trs[i], null, null, /([^>]+>){9}/i, replaceTagsAndSpaces);
+		var regNumAndPermNum = getParam(trs[i], null, null, /([^>]+>){13}/i, replaceTagsAndSpaces);
+		var till = getParam(trs[i], null, null, /([^>]+>){15}/i, replaceTagsAndSpaces, html_entity_decode);
+		
+		getParam(regNumAndPermNum, result, 'car' + (i+1), /([^<(\s]+)/i);
+		getParam(regNumAndPermNum, result, 'permission' + (i+1), /\(([\-\d]+)/i);
+		getParam(till, result, 'permission_till' + (i+1), /\d{2}.\d{2}.\d{4}$/i, null, parseDate);
+		
+		getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'phone', /Номер:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+		// Сводка
+		text += name + ' ' + region + ': ' + regNumAndPermNum + ' ' + till + '<br/><br/>';
+	}
 	
-	getParam(html, result, 'fio', /Имя(?:\s|&nbsp;)*абонента:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'phone', /Номер:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
-    
-    getParam(html, result, 'status', /Статус:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(text, result, 'all', null, [/<br\/><br\/>$/i, '']);
 	
     AnyBalance.setResult(result);
 }

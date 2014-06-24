062c1b52e0b3397b71c96c7a4b9753c55d624fad
stark027 <stark027@gmail.com>
2014-06-24 15:02:42 +0000

    Life:) Беларусь

--- ab-cell-life-by/main.js
+++ ab-cell-life-by/main.js
@@ -77,6 +77,8 @@ function main(){
 	getParam(html, result, 'balance_bonus', /Бонусный баланс:([^<]+)\s*<\/li>/i, replaceTagsAndSpaces, parseBalance);
 	// Оплаченные обязательства	
 	getParam(html, result, 'balance_corent', /Оплаченные обязательства(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	// Карманы
+	getParam(html, result, 'carmani_min_left', /(?:&#34;|")карманы(?:&#34;|")(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 	
 	html = AnyBalance.requestGet(baseurl + 'ru/upravleniye-kontraktom/smena-tarifnogo-plana/', g_headers);
 	getParam(html, result, '__tariff', /href="[^"]*">([^<]+)(?:[^>]*>){11}Активен/i, replaceTagsAndSpaces, html_entity_decode);

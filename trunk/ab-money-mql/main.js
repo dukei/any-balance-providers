1910714de548a5bfcf83f13b60fba3a0896deb41
tujger@gmail.com <tujger@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-07-02 10:18:13 +0000

    Добавлен счетчик "Всего сделок", добавлен параметр для масштабирования центовых счетов

--- trunk/ab-money-mql/main.js
+++ trunk/ab-money-mql/main.js
@@ -9,10 +9,10 @@ var g_headers = {
 	'Connection': 'keep-alive',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
-
+var prefs;
 function main() {
 	var html;
-	var prefs = AnyBalance.getPreferences();
+	prefs = AnyBalance.getPreferences();
 	var baseurl = 'http://www.mql5.com/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
@@ -25,7 +25,6 @@ function main() {
 			html = AnyBalance.requestPost('https://login.mql5.com/en/auth_login', {
 				'Login':prefs.Login,
 				'Password':prefs.Password,
-//				'RedirectAfterLoginUrl':baseurl+'en',//+'en/signals/'+prefs.signal,
 				'RememberMe':'false',
 			}, addHeaders({Referer: 'https://login.mql5.com/en/auth_login'}));		
 		} catch(e) {
@@ -42,23 +41,30 @@ function main() {
 	}
 
 	result.__tariff = getParam(html, null, null , /<div class="info signals">[\s\S]*?<meta itemprop="name" content="(.*?)" \/>/, replaceTagsAndSpaces, html_entity_decode);
-	result.initial_deposit = getParam(html, null, null, />Initial Deposit:\s+<span>(.*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-	result.deposits = getParam(html, null, null , />Deposits:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
+	result.initial_deposit = getParam(html, null, null, />Initial Deposit:\s+<span>(.*?)<\/span>/i, replaceTagsAndSpaces, parseBalanceMulti);
+	result.deposits = getParam(html, null, null , />Deposits:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalanceMulti);
 	result.total_deposit = Math.round((result.initial_deposit+result.deposits)*100)/100;
 	result.currency = getParam(html, null, null , />Initial Deposit:\s+<span>.*? ([\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'withdrawals' , />Withdrawals:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
-	result.balance = getParam(html, null, null , />Balance:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
-	result.equity = getParam(html, null, null , />Equity:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'withdrawals' , />Withdrawals:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalanceMulti);
+	result.balance = getParam(html, null, null , />Balance:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalanceMulti);
+	result.equity = getParam(html, null, null , />Equity:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalanceMulti);
 	result.current_profit = Math.round((result.equity-result.balance)*100)/100;
-	getParam(html, result, 'profit' , />Profit:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'profit' , />Profit:\s+<span>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalanceMulti);
 	getParam(html, result, 'subscribers' , />Subscribers:\s+<span>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'subscribers_funds' , />Subscribers' funds:\s+<span>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'maximum_drawdown' , />Maximum drawdown:\s+<span.*?>(.*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'weeks' , />Weeks:\s+<span>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'latest_trade' , />Latest trade:\s+<span.*?>([\s\S]*?)</, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'trades_per_week' , />Trades per week:\s+<span>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'trades_total' , />Trades:\s+<span>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'avg_holding_time' , />Avg holding time:\s+<span>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
 
 	
 	AnyBalance.setResult(result);
 }
+function parseBalanceMulti(text) {
+	var val=parseBalance(text);
+	var div=prefs.multiplier || 1;
+	return Math.round(val/div*100)/100;
+}
+

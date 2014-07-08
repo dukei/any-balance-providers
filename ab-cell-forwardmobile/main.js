b47193ca2d8be351b907e270d84c31a302408f39
stark027 <stark027@gmail.com>
2014-07-08 08:57:02 +0000

    Форвард-мобайл - new

---
+++ ab-cell-forwardmobile/main.js
@@ -0,0 +1,54 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://mn.forwardmobile.ru/';
+	AnyBalance.setDefaultCharset('windows-1251');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	try {
+		var html = AnyBalance.requestGet(baseurl, g_headers);
+	} catch(e){}
+	
+	if(AnyBalance.getLastStatusCode() > 400 || !html) {
+		throw new AnyBalance.Error('Ошибка! Сервер не отвечает! Попробуйте обновить баланс позже.');
+	}
+	
+	html = AnyBalance.requestPost(baseurl, {
+		'act':'monitoring.do_login',
+		'redirect':'/',
+		login: prefs.login,
+		password: prefs.password,
+		'save': 'Войти'
+	}, addHeaders({Referer: baseurl}));
+	
+	if (!/Аккаунт проверен\. Подождите/i.test(html)) {
+		var error = getParam(html, null, null, /<b>Ошибка<\/b>(?:[^>]*>){2}([\s\S]*?)</i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /не найден или неверный пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'fio', /Субдилер:(?:[^>]*>){1}([\s\S]*?)</i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Баланс:(?:[^>]*>){1}([\s\S]*?)</i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

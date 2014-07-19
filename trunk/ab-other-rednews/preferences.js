ee86a70b2238846fe23aa59b0feeb46940a96fa5
tujger@gmail.com <tujger@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-07-19 12:39:36 +0000

    Добавлен альтернативный источник новостей Конфигурация подстраивается в зависимости от источника

---
+++ trunk/ab-other-rednews/preferences.js
@@ -0,0 +1,34 @@
+﻿/*
+Provider of AnyBalance (http://any-balance-providers.googlecode.com)
+
+Provider for Rednews
+mailto:wtiger@mail.ru
+*/
+function onChangeType(){
+
+	var props = AnyBalance.getPreferenceProperties({
+		source: {value: ''},
+		currency: {visible: ''},
+		in_language: {visible: ''},
+		in_currency: {visible: ''}
+	});
+
+	if(props.source.value == 2){
+		AnyBalance.setPreferenceProperties({
+			currency: {visible: false},
+			in_language: {visible: true},
+			in_currency: {visible: true}
+		});
+	}else{
+		AnyBalance.setPreferenceProperties({
+			currency: {visible: true},
+			in_language: {visible: false},
+			in_currency: {visible: false}
+		});
+	};
+}
+
+function main(){
+	AnyBalance.addCallback('change#source', onChangeType);
+	onChangeType();
+}

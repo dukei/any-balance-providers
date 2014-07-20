0ae7d58ecfb2b1cca5bf2ad1f89a44b93392d41e
BorisLip <borislip@gmail.com>
2014-07-20 21:34:48 +0000

    ab-other-daka90 - same as "ab-other-hulyo" but for www.daka90.co.il (less reliable, though - Huilo got JSON API, this one does not)

---
+++ ab-other-daka90/main.js
@@ -0,0 +1,83 @@
+// http request headers
+var g_headers = 
+{
+	'Accept': 'application/json, text/javascript, */*; q=0.01',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'en-US',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
+};
+
+
+// days of week
+var g_weekdays = ["א'","ב'","ג'","ד'","ה'","ו'","ש'"];
+
+
+// get first bracket group from a regex match
+function firstMatch(regex,str)
+{
+	var result = regex.exec(str);
+	if (!result)
+		throw new AnyBalance.Error("מידע לא צפוי מאתר דקה 90");
+	return(result[1]);
+}
+
+
+// Main
+function main() 
+{
+	var prefs = AnyBalance.getPreferences();
+	var url = 'http://www.daka90.co.il/48hours';
+	var result = {success: true};
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	// access daka90 page, unlike Hulyo, there is no JSON API here
+	var html = AnyBalance.requestGet(url, g_headers); 
+	
+	// iterate through the info blocks
+	var info=""; var cnt = 0;
+	var infoRegex = /SearchResultsWithImageBoxContainer"\s*>([\s\S]*?)<div\s*class="SearchResultWithImageBoxInnerBoxFooter"\s*>/ig;
+	var infoBlock;
+	while (infoBlock=infoRegex.exec(html))
+	{
+		infoBlock = infoBlock[1];
+		
+		// get the dates
+		var dateRegex = /(\d\d\/\d\d\/\d\d\d\d)[\s\S]*?(\d\d\:\d\d)/g;
+		var there = dateRegex.exec(infoBlock); dateRegex.exec(infoBlock); // the skipped one is the landing date
+		var back = dateRegex.exec(infoBlock); 
+		if (!dateRegex.exec(infoBlock))
+			throw new AnyBalance.Error("מידע לא צפוי מאתר דקה 90");
+		
+		// convet the dates, calculate the travel days 
+		there = new Date(parseDate(there[1] + " " + there[2]));
+		back = new Date(parseDate(back[1] + " " + back[2]));
+		var days = (back-there)/(1000*60*60*24);
+		
+		// don't show flights that exceed max days limit
+		if (days>prefs.maxdays)
+			continue;
+
+		// dont show flights starting or returning at disallowed days of week
+		if (!eval("prefs.day"+there.getDay()))
+			continue;
+		if (!eval("prefs.rday"+back.getDay()))
+			continue;
+
+		// parse the rest of the data (city, 
+		var destination = replaceAll(firstMatch(/class="DepartmentTitleCampaignGroupBy">([\s\S]*?)<\/div>\s*<\/div>/i,infoBlock),replaceTagsAndSpaces);
+		var price = replaceAll(firstMatch(/<div class="FloatLeft">([\s\S]*?)<\/div>\s*<\/div>\s*<\/div>/i,infoBlock),replaceTagsAndSpaces).replace(/\s*/g,'');
+		
+		// add remaining flights
+		info += info.length ? "\n" : "";
+		info += price + ", " + g_weekdays[there.getDay()] + there.toTimeString().substring(0,5) + 
+				" עד " + g_weekdays[back.getDay()] + back.toTimeString().substring(0,5) + ", " + destination;
+	}
+
+	// put some message if no flights were found
+	info += info.length ? "" : "אין טיסות";
+	
+	// update the info and get the hell out
+	getParam(info, result, "info", null, null, html_entity_decode)
+	AnyBalance.setResult(result);
+}

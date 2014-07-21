e78c0a5867164b836e6ad3f2dcc882959799ccad
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-07-21 08:16:08 +0000

    Либрусек

--- trunk/ab-service-librusec/main.js
+++ trunk/ab-service-librusec/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://lib.rus.ec/';
+	var baseurl = 'https://lib.rus.ec/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');

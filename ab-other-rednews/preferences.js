df3e8eed10994093f1a4f9d1973b6ebaf2345016
tujger <tujger@gmail.com>
2014-07-24 13:25:33 +0000

    Добавлен источник новостей Сделан перебор источников Улучшено распознавание времени

--- ab-other-rednews/preferences.js
+++ ab-other-rednews/preferences.js
@@ -5,30 +5,98 @@ Provider for Rednews
 mailto:wtiger@mail.ru
 */
 function onChangeType(){
-
 	var props = AnyBalance.getPreferenceProperties({
 		source: {value: ''},
 		currency: {visible: ''},
 		in_language: {visible: ''},
-		in_currency: {visible: ''}
+		in_currency: {visible: ''},
+		dx_currency: {visible: ''},
+		select_timezone_ff: {value:'',visible: ''},
+		timezone_ff: {visible: ''},
+		select_timezone_in: {value:'',visible: ''},
+		timezone_in: {visible: ''},
+		select_timezone_dx: {value:'',visible: ''},
+		timezone_dx: {visible: ''},
 	});
 
-	if(props.source.value == 2){
+	if(props.source.value==null || props.source.value==""){//All
 		AnyBalance.setPreferenceProperties({
-			currency: {visible: false},
+			currency: {visible: true},
 			in_language: {visible: true},
-			in_currency: {visible: true}
+			in_currency: {visible: true},
+			dx_currency: {visible: true},
+			select_timezone_ff: {visible: true},
+			select_timezone_in: {visible: true},
+			select_timezone_dx: {visible: true},
 		});
-	}else{
+		if(props.select_timezone_ff.value==1 || props.select_timezone_ff.value==null)
+			AnyBalance.setPreferenceProperties({timezone_ff: {visible: false}});
+		else
+			AnyBalance.setPreferenceProperties({timezone_ff: {visible: true}});
+
+		if(props.select_timezone_in.value==1 || props.select_timezone_in.value==null)
+			AnyBalance.setPreferenceProperties({timezone_in: {visible: false}});
+		else
+			AnyBalance.setPreferenceProperties({timezone_in: {visible: true}});
+
+		if(props.select_timezone_dx.value==1 || props.select_timezone_dx.value==null)
+			AnyBalance.setPreferenceProperties({timezone_dx: {visible: false}});
+		else
+			AnyBalance.setPreferenceProperties({timezone_dx: {visible: true}});
+
+	}else if(props.source.value==1){//ForexFactory
 		AnyBalance.setPreferenceProperties({
 			currency: {visible: true},
 			in_language: {visible: false},
-			in_currency: {visible: false}
+			in_currency: {visible: false},
+			select_timezone_ff: {visible: true},
+			select_timezone_in: {visible: false},
+			select_timezone_dx: {visible: false},
+			timezone_in: {visible: false},
+			timezone_dx: {visible: false},
 		});
+		if(props.select_timezone_ff.value==1 || props.select_timezone_ff.value==null)
+			AnyBalance.setPreferenceProperties({timezone_ff: {visible: false}});
+		else
+			AnyBalance.setPreferenceProperties({timezone_ff: {visible: true}});
+
+	}else if(props.source.value == 2){
+		AnyBalance.setPreferenceProperties({
+			currency: {visible: false},
+			in_language: {visible: true},
+			in_currency: {visible: true},
+			select_timezone_ff: {visible: false},
+			select_timezone_in: {visible: true},
+			select_timezone_dx: {visible: false},
+			timezone_ff: {visible: false},
+			timezone_dx: {visible: false},
+		});
+		if(props.select_timezone_in.value==1 || props.select_timezone_in.value==null)
+			AnyBalance.setPreferenceProperties({timezone_in: {visible: false}});
+		else
+			AnyBalance.setPreferenceProperties({timezone_in: {visible: true}});
+	}else if(props.source.value == 3){
+		AnyBalance.setPreferenceProperties({
+			currency: {visible: false},
+			in_language: {visible: false},
+			in_currency: {visible: false},
+			select_timezone_ff: {visible: false},
+			select_timezone_in: {visible: false},
+			select_timezone_dx: {visible: true},
+			timezone_ff: {visible: false},
+			timezone_in: {visible: false},
+		});
+		if(props.select_timezone_dx.value==1 || props.select_timezone_dx.value==null)
+			AnyBalance.setPreferenceProperties({timezone_dx: {visible: false}});
+		else
+			AnyBalance.setPreferenceProperties({timezone_dx: {visible: true}});
 	};
 }
 
 function main(){
 	AnyBalance.addCallback('change#source', onChangeType);
+	AnyBalance.addCallback('change#select_timezone_ff', onChangeType);
+	AnyBalance.addCallback('change#select_timezone_in', onChangeType);
+	AnyBalance.addCallback('change#select_timezone_dx', onChangeType);
 	onChangeType();
 }

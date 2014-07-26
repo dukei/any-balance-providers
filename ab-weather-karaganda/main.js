658af427cfad3e7bd7315e48e235015186bf6c81
stikhilyas <stikhilyas@gmail.com>
2014-07-26 08:20:58 +0000

    [ab-weather-karaganda] исправлена загрузка данных с meteoclub.kz - добавлен заголовок к запросу

--- ab-weather-karaganda/main.js
+++ ab-weather-karaganda/main.js
@@ -12,7 +12,9 @@ function main(){
       
         var result = {success: true};
 
-        var info = AnyBalance.requestGet('http://meteoclub.kz/meteoXML.php');
+        var info = AnyBalance.requestGet('http://meteoclub.kz/meteoXML.php', {
+          "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryIu4PwUJhm5qcpsdI",
+          "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64)"});
         var xmlDoc = $.parseXML(info), $xml = $(xmlDoc);
         getParam(result,$xml,'temperature');
         getParam(result,$xml,'pressure');

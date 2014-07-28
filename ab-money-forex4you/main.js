cfa2e7a94e419457d0fa62d0d4a26aeb091f704f
stark027 <stark027@gmail.com>
2014-07-28 09:49:56 +0000

    Forex4you

--- ab-money-forex4you/main.js
+++ ab-money-forex4you/main.js
@@ -7,9 +7,6 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
@@ -36,7 +33,7 @@ function main() {
 	}, addHeaders({Referer: baseurl + 'login'}));
 	
 	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		var error = getParam(html, null, null, /class="alert([^>]+>){3}/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
 			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
 		
@@ -49,8 +46,8 @@ function main() {
 	getParam(html, result, 'balance', /Баланс:([^>]+>){3}/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'cred', /Кредитные Бонусы([^>]+>){3}/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, ['currency', 'balance', 'cred'], /Валюта:([^>]+>){3}/i, [replaceTagsAndSpaces, /\s*центов\s*/i, ''], html_entity_decode);
-	getParam(html, result, '__tariff', /Торговый счет([^>]+>){3}/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'account', /Торговый счет([^>]+>){3}/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, '__tariff', />Торговый счет([^>]+>){3}/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'account', />Торговый счет([^>]+>){3}/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'arm', /Плечо:([^>]+>){3}/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'fio', /Имя:([^>]+>){3}/i, replaceTagsAndSpaces, html_entity_decode);
 	

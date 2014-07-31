55bb9b2803245746c4ac9f672d3cb29bd308a1ea
igoloviznin@gmail.com <igoloviznin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-07-31 08:33:37 +0000

    Нова пошта. Исправления связанные с вводом нового личного кабинета.

--- trunk/ab-bonus-nova-poshta/main.js
+++ trunk/ab-bonus-nova-poshta/main.js
@@ -3,39 +3,52 @@
 
 Оператор экспресс-доставки «Нова пошта»
 Сайт оператора: http://novaposhta.ua
-Личный кабинет: http://novaposhta.ua/loyalty/dashboard
 */
 
 function main(){
     var prefs = AnyBalance.getPreferences();
 
-    var baseurl = 'http://orders.novaposhta.ua/account/login/';
+    var baseurl = 'https://my.novaposhta.ua/';
 
-    AnyBalance.trace("Trying to enter selfcare at address: " + baseurl);
-    var html = AnyBalance.requestPost(baseurl, {
-    	a: prefs.login,
-        n: prefs.passw
+    AnyBalance.trace("Trying to enter selfcare at address: " + baseurl + '?r=auth/index');
+    var html = AnyBalance.requestPost(baseurl + '?r=auth/index', {
+    	"LoginForm[username]": prefs.email,
+        "LoginForm[password]": prefs.passw
     });
 
     var result = {success: true};
 
-    if(!/\/logout/i.test(html)){
+    if(!/Налаштування/i.test(html)){
 
-    var error = getParam(html, null, null, /<div id=\"error_list\">\s*([^<]*)<br \/>/i, replaceTagsAndSpaces, html_entity_decode);
+    var error = getParam(html, null, null, /<div class="errorSummary">\s*<ul>\s*<li>([\s\S]*)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
     if(error)
         throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 
+    html = AnyBalance.requestPost(baseurl + '?r=loyaltyUser/index');
+
+    // ФИО
+    getParam(html, result, '__tariff', /<th>Власник:<\/th>\s*<td>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+
     // Номер карты
-    sumParam(html, result, '__tariff', /Всі документи по картці (\d+)<br>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+    getParam (html, result, 'ncard', /<th>Номер картки лояльності:<\/th>\s*<td>(\d+)<\/td>/i, replaceTagsAndSpaces, parseBalance);
 
-    // Баланс
-    sumParam (html, result, 'balance', /Кількість балів:\s*(\d+)\s*<\/div>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+    html = AnyBalance.requestPost(baseurl + '?r=loyaltyUser/turnover');
 
-    // Количество отправок
-    sumParam (html, result, 'send', /Кількість відправок:\s*(\d+)\s*<\/div>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+    // Балы
+    sumParam (html, result, 'balance', /<th>Залишок балів:<\/th>\s*<td>(\d+)<\/td>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+    sumParam (html, result, 'balance_all', /<th>Всього балів:<\/th>\s*<td>(\d+)<\/td>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
 
+    // Скидка
+    sumParam (html, result, 'discount', /<th>Доступна знижка, грн.:<\/th>\s*<td>(\d+.\d+)<\/td>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+
+    // Кол-во отправок
+    sumParam (html, result, 'send', /<th>Кількість ТТН:<\/th>\s*<td>(\d+)<\/td>/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+
+    // Статус
+    getParam (html, result, 'status', /<th>Статус Учасника:<\/th>\s*<td>([^<]*)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam (html, result, 'status_next', /<th>Наступний статус:<\/th>\s*<td>([^<]*)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 
     AnyBalance.setResult(result);
 

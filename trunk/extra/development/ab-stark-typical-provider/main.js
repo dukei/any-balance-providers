f7541d1141403bf1a3ddb0aefc32ac9a6bb3676d
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-08-01 09:09:54 +0000

    ab-stark-typical-provider

--- trunk/extra/development/ab-stark-typical-provider/main.js
+++ trunk/extra/development/ab-stark-typical-provider/main.js
@@ -23,7 +23,7 @@ function main() {
 	
 	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
 	
-	if(AnyBalance.getLastStatusCode() > 400 || !html) {
+	if(AnyBalance.getLastStatusCode() > 400 || !html)
 		throw new AnyBalance.Error('Ошибка! Сервер не отвечает! Попробуйте обновить баланс позже.');
 	
 	var params = createFormParams(html, function(params, str, name, value) {

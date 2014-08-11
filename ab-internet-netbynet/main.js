35bd6723d336357d913cbb2b06141974aea4ecc7
stark027 <stark027@gmail.com>
2014-08-11 13:31:56 +0000

    NetByNet

--- ab-internet-netbynet/main.js
+++ ab-internet-netbynet/main.js
@@ -9,7 +9,8 @@ var g_regions = {
     orel: mainBelgorod,
     oskol: mainBelgorod,
     lipetsk: mainBelgorod,
-	tver: mainUniversal
+	tver: mainUniversal,
+	lobnya: mainLobnya,
 };
 
 function main(){
@@ -216,6 +217,40 @@ function mainBelgorod(region){
     getParam (html, result, 'status', /Текущий статус договора:([\s\S]*?)<br/i, replaceTagsAndSpaces, html_entity_decode);
     getParam (html, result, 'abonday', /Суточная абонентская плата:([\s\S]*?)<br/i, replaceTagsAndSpaces, parseBalance);
     getParam (html, result, 'abon', /Ежемесячная абонентская плата:([\s\S]*?)<br/i, replaceTagsAndSpaces, parseBalance);
+
+    AnyBalance.setResult(result);
+}
+
+function mainLobnya(region){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://selfcare.netbynet.ru/'+region+'/';
+
+    AnyBalance.trace ("Trying to enter selfcare at address: " + baseurl);
+    var html = requestPostMultipart (baseurl + "?", {
+    	'pr[form][auto][form_save_to_link]': 0,
+    	'pr[form][auto][login]': prefs.login,
+    	'pr[form][auto][password]': prefs.password,
+    	'pr[form][auto][form_event]': 'Войти'
+    }, {'Accept-Charset': 'windows-1251'});
+
+    if(!/'\?exit=1'/i.test(html)){
+        var error = sumParam (html, null, null, /<font[^>]+color=['"]red['"][^>]*>([\s\S]*?)<\/font>/ig, replaceTagsAndSpaces, null, aggregate_join);
+        if (error){
+            throw new AnyBalance.Error (error);
+        }
+        throw new AnyBalance.Error ("Не удаётся войти в личный кабинет. Сайт изменен?");
+    }
+	//html = AnyBalance.requestGet(baseurl + '?pr%5Bcontrol%5D%5Bkernel%5D%5Brecord%5D=23&pr%5Bcontrol%5D%5Bkernel%5D%5Bparent%5D=19&menu=19');
+    
+	var result = {success: true};
+	
+    getParam (html, result, 'balance', /баланс:([\s\d.,-]+)руб/i, replaceTagsAndSpaces, parseBalance);
+    getParam (html, result, 'subscriber', /Приветствуем Вас,([^<]*)/i, replaceTagsAndSpaces);
+    getParam (html, result, 'contract', />\s*Лицевой счет:([^<,]*)/i, replaceTagsAndSpaces);
+    getParam (html, result, 'day_left', /дней до ухода в финансовую блокировку:(?:[^>]*>){2}\s*(\d+)/i, replaceTagsAndSpaces, parseBalance);
+	//sumParam(html, result, '__tariff', /Тарифный план:([\s\S]*?)(?:<\/span>|<a)/i, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
+    //getParam (html, result, 'bonus_balance', /Баланс:([^<]*)балл/i, replaceTagsAndSpaces, parseBalance);
+    //sumParam (html, result, '__tariff', /(<strong[^>]*>\s*Бонусный счет[\s\S]*?)Баланс/i, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
 
     AnyBalance.setResult(result);
 }

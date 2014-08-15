62333aff4f7679f8abaa0020f530dc3c392f3dfb
stark027 <stark027@gmail.com>
2014-08-15 10:10:49 +0000

    Velcom (Беларусь)

--- ab-cell-velcom/main.js
+++ ab-cell-velcom/main.js
@@ -28,7 +28,7 @@ function main(){
     
     var html = AnyBalance.requestGet(baseurl, g_headers);
 	
-	if(/Your browser must support JavaScript!/.test(html)) {
+	if(/JavaScript/.test(html)) {
 		// Функчи из скрипта
 		function toNumbers(d){var e=[];d.replace(/(..)/g,function(d){e.push(parseInt(d,16))});return e};
 		function toHex(){for(var d=[],d=1==arguments.length&&arguments[0].constructor==Array?arguments[0]:arguments,e="",f=0;f<d.length;f++)e+=(16>d[f]?"0":"")+d[f].toString(16);return e.toLowerCase()};

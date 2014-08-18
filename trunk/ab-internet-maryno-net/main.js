16d337e45360ddf2ad4b4ba9e9123a4a673ea4db
MironovJenia@gmail.com <MironovJenia@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-08-18 10:47:29 +0000

    Марьино.NET - 1.0.12 - Обновления на сайте. Можно публиковать проверенно.

--- trunk/ab-internet-maryno-net/main.js
+++ trunk/ab-internet-maryno-net/main.js
@@ -4,6 +4,7 @@
 Провайдер Марьино.NET 
 Сайт оператора: http://maryno.net/
 Личный кабинет: https://my.maryno.net/
+1.0.12- Полностью изменен личный кабинет
 1.0.11- Обновления на сайте.
 1.0.10- Обновления на сайте.
 1.0.9 - Округление трафика до мегабайтов.
@@ -17,126 +18,94 @@ function main(){
 
 	AnyBalance.setAuthentication(data.login,data.password);
 	
-	var url='https://my.maryno.net/ls.php?ls=' + data.login;
-	
-	var html = AnyBalance.requestGet(url);
+	var url='https://lk.maryno.net/login';
 
-	regexp = /Личный кабинет/;
-	if (regexp.exec(html)){
+	var html = AnyBalance.requestPost('https://lk.maryno.net/login',{
+		username:data.login,
+		password:data.password
+	});
+	html=AnyBalance.requestGet('https://lk.maryno.net/api/user/contract');
+	if (res=/contract_id\":(.*?),/.exec(html)){var contract_id=res[1];}
+        html = AnyBalance.requestPost('https://lk.maryno.net/api/user/contract/'+contract_id);
+        html = AnyBalance.requestGet('https://lk.maryno.net/api/user/subscriber');
+	if (res=/subscriber_id\":(.*?),/.exec(html)){var subscriber_id=res[1];}
+        html = AnyBalance.requestPost('https://lk.maryno.net/api/user/subscriber/'+subscriber_id);
+        html = AnyBalance.requestGet('https://lk.maryno.net/api/user/product');
+	if (res=/product_id\":(.*?),/.exec(html)){var product_id=res[1];}
+        html = AnyBalance.requestPost('https://lk.maryno.net/api/user/product/'+product_id);
+	url='https://lk.maryno.net/api/user/all';
+	html=AnyBalance.requestGet(url);
+	regexp = /Unauthorized/;
+	if (!regexp.exec(html)){
       		AnyBalance.trace ('Authorization Ok!');
 	} else {
       		AnyBalance.trace ('Authorization Required.');
 		throw new AnyBalance.Error ('Ошибка авторизации.');
 	}
-	var result = {success: true};
+
 	AnyBalance.trace ('Start parsing...');
+	var result = {success: true};
+	var res='';
 
-	var res=/<\/script>(<table.*<\/table>)<br><table/.exec(html);
-	var tmp=res[1];
-        res=tmp.replace(/<.*?>/g,'-');
-	res=res.replace(/--+/g,'=');
-	var tmp=res;
 
-	if(AnyBalance.isAvailable('number')){
-    regexp=/=Номер договорной записи=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.number=res[1];}
+	if (res=/number\":\"(.*?)\"/.exec(html)){result.number=res[1];}
+	if (res=/contract_num\":\"(.*?)\"/.exec(html)){result.dogovor=res[1];}
+	if (res=/fio\":\"(.*?)\"/.exec(html)){result.FIO=res[1];}
+	if (res=/address\":\"(.*?)\"/.exec(html)){result.address=res[1];}
+	if (res=/date_begin\":\"(.*?)\"/.exec(html)){result.start_day=res[1];}
+	if (res=/balance\":(.*?),/.exec(html)){result.balance=Math.floor(res[1]*100)/100;}
+	if (res=/bonusBalance\":(.*?),/.exec(html)){result.bonus_balance=res[1];}
+	if (res=/plan\":\"(.*?)\"/.exec(html)){result.__tariff=res[1];result.tariff=res[1];}
+	if (res=/turnbackBalance\":(.*?),/.exec(html)){result.credit=res[1];}
+	if (res=/isBlocked\":(.*?)}/.exec(html)){
+		if (res[1] == 0){result.status="Не блокирован";}
+		if (res[1] != 0){result.status=res[1]}
 	}
-	if(AnyBalance.isAvailable('dogovor')){
-    regexp=/=Договор=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.dogovor=res[1];}
-	}
-	if(AnyBalance.isAvailable('FIO')){
-    regexp=/=Фамилия И.О.=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.FIO=res[1];}
-	}
-	if(AnyBalance.isAvailable('adress')){
-    regexp=/=Адрес=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.adress=res[1];}
-	}
-	if(AnyBalance.isAvailable('Home_Phone')){
-    regexp=/=Телефон домашний=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.Home_Phone=res[1]}
-	}
-	if(AnyBalance.isAvailable('Phone')){
-    regexp=/=Телефон мобильный=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.Phone=res[1];}
-	}
-	if(AnyBalance.isAvailable('email')){
-    regexp=/=E-Mail=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.email=res[1];}
-	}
-	if(AnyBalance.isAvailable('start_day')){
-    regexp=/=Дата открытия=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.start_day=res[1];}
-	}
-    regexp=/=Актуальный баланс, руб.=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.balance=Math.floor(res[1]*100)/100;}
-	if(AnyBalance.isAvailable('bonus_balance')){
-    regexp=/=Бонусный баланс, руб.=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.bonus_balance=res[1];}
-	}
+	url='https://lk.maryno.net/api/user/email';
+	html=AnyBalance.requestGet(url);
+	if (res=/value\":\"(.*?)\"/.exec(html)){result.email=res[1];}
 
-	if(AnyBalance.isAvailable('unconfirmed')){
-    regexp=/=Неподтвержденные платежи, руб.>(.*?)=/;
-		if (res=regexp.exec(tmp)){result.unconfirmed=res[1];}
-	}
-	if(AnyBalance.isAvailable('credit')){
-    regexp=/=Кредитование, руб.=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.credit=res[1];}
-	}
-	if(AnyBalance.isAvailable('status')){
-    regexp=/=Статус счета=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.status=res[1];}
-	}
 
-  regexp=/=Тарифный план=(.*?)=/;
-  if (res=regexp.exec(tmp)){
-    result.__tariff=res[1];
-    if(AnyBalance.isAvailable('tariff')){result.tariff=res[1];}
-	}        
-	if(AnyBalance.isAvailable('included')){
-    regexp=/=Остаток оплаченного включенного трафика, Мб=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.included=res[1];}
-	}
-	if(AnyBalance.isAvailable('unpaid')){
-    regexp=/=Остаток неоплаченного включенного трафика, Мб=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.unpaid=res[1];}
-	}
-	if(AnyBalance.isAvailable('transfer')){
-    regexp=/=Перенесенный трафик, Мб=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.transfer=res[1];}
-	}
-	if(AnyBalance.isAvailable('bonus')){
-    regexp=/=Бонусный трафик, Мб=(.*?)=/;
-		if (res=regexp.exec(tmp)){result.bonus=res[1];}
-	}
-	if(AnyBalance.isAvailable('traffic_month_in')||
-	   AnyBalance.isAvailable('traffic_month_out')){
-		url="https://my.maryno.net/stat.php?act=det_mon&ls="+data.login;
-		html = AnyBalance.requestGet(url);
-		res =/<td>Итого за месяц<.*?><.*?>(.*?) .*?<.*?><.*?>(.*?) .*?<.*?>/.exec(html);
-		result.traffic_month_in=Math.floor(res[1]);
-		result.traffic_month_out=Math.floor(res[2]);
-	}
-	if(AnyBalance.isAvailable('traffic_last_month_in')||
-	   AnyBalance.isAvailable('traffic_last_month_out')){
+	url='https://lk.maryno.net/api/accounts';
+	html=AnyBalance.requestGet(url);
+	if (res=/ip_address\":\"(.*?)\"/.exec(html)){ip_address=res[1];
         	var date = new Date ();
 		var year=date.getFullYear();
 		var month=date.getMonth();
+		url='https://lk.maryno.net/api/accounts/details/month/'+ip_address+'/'+year+'/'+(month+1);
+		t=AnyBalance.requestGet(url);
+		t_m=t.split("}");
+		var tr_in=0;
+		var tr_out=0;
+		for (i=0;i<=t_m.length-2;i++){
+			res =/incoming\":(.*?),\"outgoing\":(.*)/.exec(t_m[i]);
+        		tr_in=tr_in+Math.floor((res[1])/1024/1024);
+			tr_out=tr_out+Math.floor((res[2])/1024/1024);
+		}
+		result.traffic_month_in=tr_in;
+		result.traffic_month_out=tr_out;
+
+
 		if (month == 0 ){
 			year=year-1;
 			month=12;
 		}
-    url="https://my.maryno.net/stat.php?act=det_mon&ls="+data.login+"&ip=&month="+month+"&year="+year;
-		html = AnyBalance.requestGet(url);
-		res =/<td>Итого за месяц<.*?><.*?>(.*?) .*?<.*?><.*?>(.*?) .*?<.*?>/.exec(html);
-		result.traffic_last_month_in=Math.floor(res[1]);
-		result.traffic_last_month_out=Math.floor(res[2]);
+		url='https://lk.maryno.net/api/accounts/details/month/'+ip_address+'/'+year+'/'+month;
+		t_pr=AnyBalance.requestGet(url);
+		t_m_pr=t_pr.split("}");
+		var tr_lm_in=0;
+		var tr_lm_out=0;
+		for (i=0;i<=t_m_pr.length-2;i++){
+			res =/incoming\":(.*?),\"outgoing\":(.*)/.exec(t_m_pr[i]);
+        		tr_lm_in=tr_lm_in+Math.floor((res[1])/1024/1024);
+			tr_lm_out=tr_lm_out+Math.floor((res[2])/1024/1024);
+		}
+		result.traffic_last_month_in=tr_lm_in;
+		result.traffic_last_month_out=tr_lm_out;
 	}
-
-
 
 	AnyBalance.trace ('End parsing...');	
 	AnyBalance.setResult(result);
+        
 
 }

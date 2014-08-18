ace4ca3cc5bb3510bf640048d6123f05aa60243b
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-08-18 09:17:17 +0000

    Деловая сеть (Беларусь)

--- trunk/ab-internet-bn-by/main.js
+++ trunk/ab-internet-bn-by/main.js
@@ -1,22 +1,35 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Текущий баланс у белорусского интернет провайдера Деловая сеть
-
-Сайт оператора: http://bn.by
-Личный кабинет: http://ui.bn.by
 */
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
-
     var baseurl = "http://ui.bn.by/index.php?";
-
-    var html = AnyBalance.requestPost(baseurl + 'locale=ru', {
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка! Сервер не отвечает! Попробуйте обновить баланс позже.');
+	
+    html = AnyBalance.requestPost(baseurl + 'locale=ru', {
         login:prefs.login,
         passwd:prefs.password
-    });
+    }, addHeaders({Referer: baseurl}));
 
     if(!/mode=logout/i.test(html)){
         var error = getParam(html, null, null, /<div[^>]*class=['"]alarma[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);

44ea704780cb6d97e97a5b230343b1510d699859
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-08-29 06:48:08 +0000

    Рос-Интра (Урал-ТранТелеКом)

--- trunk/ab-internet-rosintra/main.js
+++ trunk/ab-internet-rosintra/main.js
@@ -7,9 +7,6 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
@@ -21,7 +18,7 @@ function main() {
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	AnyBalance.setAuthentication(prefs.login,prefs.password);
+	AnyBalance.setAuthentication(prefs.login, prefs.password);
 	
 	var html = AnyBalance.requestGet(baseurl + 'client/', g_headers);
 	
@@ -39,7 +36,7 @@ function main() {
 	
 	var result = {success: true};
 	
-	getParam(html, result, 'balance', /Общий баланс:(?:[^>]*>){3}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance', /Общий баланс:(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'fio', /Пользователь:(?:[^>]*>){3}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 
 	AnyBalance.setResult(result);

78a5a502f566552cd34db2e7be7a77f1a5fcc71b
stark027 <stark027@gmail.com>
2014-09-11 10:23:27 +0000

    Новый - PAYBACK

---
+++ ab-bonus-payback/main.js
@@ -0,0 +1,38 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://www2.payback.pl/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Enter PayBack number!');
+	
+	var html = AnyBalance.requestGet('https://occssl.payback.pl/balance?member-id=' + prefs.login + '&remember-me=n', g_headers);
+	
+	if (!/availablePointsAmount/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Can`t find balance information, is the site has changed?');
+	}
+	
+	var json = getJson(html);
+	
+	var result = {success: true};
+	
+	getParam(json.availablePointsAmount + '', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

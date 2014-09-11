e8468be8180e113e8ec9b4fa11be2e769dc8a522
stark027 <stark027@gmail.com>
2014-09-11 09:36:04 +0000

    Новый - ARTEM-CATV

---
+++ ab-internet-artem-catv/main.js
@@ -0,0 +1,54 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://bill.artem-catv.net/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	html = AnyBalance.requestPost(baseurl, {
+		user: prefs.login,
+		pswd: prefs.password,
+	}, addHeaders({Referer: baseurl}));
+	
+	if(!/\?action=Exit/i.test(html)){
+        var error = getParam(html, null, null, /<b>\s*ОШИБКА:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error)
+            throw new AnyBalance.Error(error, null, /не найден|Неправильный пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');		
+    }
+
+    var result = {success: true};
+    
+    if(AnyBalance.isAvailable('balance')){
+        html = AnyBalance.requestGet(baseurl + '?action=ShowBalance&mid=contract');
+        getParam(html, result, 'balance', /Баланс(?:[^>]*>){3}([^<]+)<\//i, replaceTagsAndSpaces, parseBalance);
+    }
+
+    html = AnyBalance.requestGet(baseurl + '?action=ChangeTariff&mid=contract');
+    var plans = getParam(html, null, null, /<td>\s*Тарифный план[\s\S]*?<tbody[^>]*>([\S\s]*?)<\/tbody>/i);
+    if(plans){
+        sumParam(plans, result, '__tariff', /<td>\s*<font>([\S\s]*?)<\/font>/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
+    }
+	
+	AnyBalance.setResult(result);
+}

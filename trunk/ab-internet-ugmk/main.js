a74a70047072707329dcb7c4aab7008c418630ff
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-09-11 11:15:27 +0000

    УГМК-Телеком

--- trunk/ab-internet-ugmk/main.js
+++ trunk/ab-internet-ugmk/main.js
@@ -1,10 +1,5 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плане для УГМК телеком - крупного оператора современных услуг связи, работающий на рынке телекоммуникаций, более чем в 20 населенных пунктах Урала, Сибири, Башкирии и Центральной России.
-
-Сайт оператора: http://ugmk-telecom.ru/
-Личный кабинет: https://stats.ugmk-telecom.ru
 */
 
 function parseTrafficTotalGb(str){
@@ -25,43 +20,28 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "https://stats.ugmk-telecom.ru/bgbilling/webexecuter";
+    var baseurl = "https://lk.ugmk-telecom.ru/";
 
-    var html = AnyBalance.requestPost(baseurl, {
-        midAuth:0,
-        user:prefs.login,
-        pswd:prefs.password
+    var html = AnyBalance.requestPost(baseurl + '?login=yes', {
+		backurl:'/',
+		Login:'Войти',
+        USER_LOGIN:prefs.login,
+        USER_PASSWORD:prefs.password
     });
 
-    //AnyBalance.trace(html);
-    if(!/\?action=Exit/i.test(html)){
-        var error = getParam(html, null, null, /<h2[^>]*>ОШИБКА:([\s\S]*?)(?:<\/ul>|<\/div>)/, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error, null, /Лицевой счет не найден|Неправильный пароль/i.test(error));
-        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
-    }
-
+    if (!/logout=yes/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
     var result = {success: true};
     
-    if(AnyBalance.isAvailable('balance')){
-        html = AnyBalance.requestGet(baseurl + '?action=ShowBalance&mid=contract');
-        getParam(html, result, 'balance', /Исходящий остаток[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'promise', /<th[^>]*>\s*Обещанный платеж[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    }
-
-    html = AnyBalance.requestGet(baseurl + '?action=ChangeTariff&mid=contract');
-    var plans = getParam(html, null, null, /<td>Тарифный план[\s\S]*?<tbody[^>]*>([\S\s]*?)<\/tbody>/i);
-    if(plans){
-        sumParam(plans, result, '__tariff', /<td><font>([\S\s]*?)<\/font>/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
-    }
-/*
-    if(AnyBalance.isAvailable('traffic_time', 'traffic_cost', 'traffic_total')){
-        html = AnyBalance.requestGet(baseurl + '?action=ShowLoginsBalance&mid=6&module=dialup');
-        
-        getParam(html, result, 'traffic_time', /Итого:(?:[\S\s]*?<td[^>]*>){2}[\S\s]*?\[(\d+)\]/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'traffic_cost', /Итого:(?:[\S\s]*?<td[^>]*>){3}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'traffic_total', /Итого:(?:[\S\s]*?<td[^>]*>){4}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficTotalGb);
-    }
-*/    
+	getParam(html, result, 'balance', /На счете:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /Текущий тариф:(?:[^>]*>){3}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
     AnyBalance.setResult(result);
 }

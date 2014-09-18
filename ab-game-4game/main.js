0888ad213644657c07cb8604ed4ecdb9b1376d24
stark027 <stark027@gmail.com>
2014-09-18 14:10:50 +0000

    Фогейм

--- ab-game-4game/main.js
+++ ab-game-4game/main.js
@@ -1,11 +1,7 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и бонусы из личного кабинета фогейма
-
-Сайт оператора: https://ru.4game.com
-Личный кабинет: https://ru.4game.com
 */
+
 var g_headers = {
     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
@@ -21,23 +17,34 @@ function main(){
 	
 	var baseurl = "https://ru.4game.com/";
 	
-    var html = AnyBalance.requestGet(baseurl + 'widgetjson/signin?&loginField=' + encodeURIComponent(prefs.login) + '&passwordField=' + encodeURIComponent(prefs.password) + '&serviceId=0&jsonp&callback=');
-    var json = getJson(html);
-    if(!json.success){
-        var message = json.message;
-        throw new AnyBalance.Error(message);
-    }
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + '?popupWidget=AuthPopupWidget&block=login', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'login') 
+			return prefs.login;
+		else if (name == 'password')
+			return prefs.password;
 
-    html = AnyBalance.requestGet(baseurl + 'settings/personal/', addHeaders({
-		'Referer':baseurl + 'subscription/index.html',
-		'X-Requested-With':'XMLHttpRequest'
-    }));
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + '?popupWidget=AuthPopupWidget&block=login', {
+		'AuthForm[loginOrEmail]': prefs.login,
+		'AuthForm[password]': prefs.password,
+		'AuthForm[bruteforceDetected]': ''
+	}, addHeaders({Referer: baseurl + '?popupWidget=AuthPopupWidget&block=login'}));
+
     var result = {success: true};
 
     result.__tariff = prefs.login;
-    getParam(html, result, 'balance', /"jsUserBarBalance__eMoneyValue">([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-    // бонусы не фиксены
-	getParam(html, result, 'bonus', /и\s*([^<]*)(?:<[^>]*>\s*)*бонусов/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balance', /balance[^:]*:([\s\d,.]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'bonus', /bonus[^:]*:([\s\d,.]+)/i, replaceTagsAndSpaces, parseBalance);
 
     AnyBalance.setResult(result); 
 }

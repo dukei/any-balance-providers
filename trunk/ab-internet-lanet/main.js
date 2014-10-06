194d372587591fc016394a35ed436c5e3ccbaa81
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-06 14:39:13 +0000

    Ланет Нетворк (Киев)

--- trunk/ab-internet-lanet/main.js
+++ trunk/ab-internet-lanet/main.js
@@ -13,19 +13,22 @@ var g_headers = {
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
-
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     var baseurl = 'https://my.lanet.ua/';
-	var html = AnyBalance.requestGet(baseurl, g_headers);
-	var params = createFormParams(html, function(params, str, name, value){
-		if(name == 'login')
-			return prefs.login;
-		else if(name == 'pass')
-			return prefs.password;
-		return value;
-	});
-    html = AnyBalance.requestPost(baseurl + 'login.php#login', params, g_headers);
-
-    if(!/"logoff.php"/.test(html)){
+	var html = AnyBalance.requestGet(baseurl + 'login.php', g_headers);
+	
+    html = AnyBalance.requestPost(baseurl + 'login.php?api', {
+		'method':'login',
+		'parameters':'["'+prefs.login+'","'+prefs.password+'"]',
+		'id':'3'
+	}, addHeaders({'Referer': baseurl + 'login.php'}));
+	
+	var json = getJson(html);
+	
+    if(!json.status) {
         var error = getParam(html, null, null, /<td[^>]+class="form_error"[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
@@ -33,16 +36,49 @@ function main(){
 		AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
-
-    var result = {success: true};
-
-    getParam(html, result, 'balance', /личный счет[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'bonus', /Дополнительные услуги[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'pay', /нехватка[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'agreement', /&#8470; договора[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /тип подключения[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'paytill', /конец периода[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, [replaceTagsAndSpaces, /\d+:\d+/i, ''+new Date().getFullYear()], parseDateWord);
-    getParam(html, result, 'userName', /<td[^>]+class="cname"[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+	
+	html = AnyBalance.requestGet(json.result[1], g_headers);
+	
+	var token = getParam(html, null, null, /"\?api"\s*,\s*"([^"]+)/i);
+	checkEmpty(token, 'Не удалось найти токен авторизации, сайт изменен?', true);
+	
+    html = AnyBalance.requestPost(baseurl + 'login.php?api', {
+		'method':'login',
+		'parameters':'["'+prefs.login+'","'+prefs.password+'"]',
+		'id':'3'
+	}, addHeaders({'Referer': baseurl + 'login.php'}));
+	
+    html = AnyBalance.requestPost(baseurl + 'client_info.php?api', {
+		'batch':'[["getContract"],["getDeposit"],["getBonus"],["getName"], ["getPeriodInfo"], ["getMiss"]]',
+		'id':'1',
+		'token':token
+	}, addHeaders({'Referer': baseurl + 'client_info.php'}));
+	
+	json = getJson(html);
+	
+	var result = {success: true};
+	
+	for(var i = 0; i < json.result.length; i++) {
+		var current = json.result[i];
+		if(current.name == 'getContract') {
+			getParam(current.result + '', result, 'agreement');
+		} else if(current.name == 'getDeposit') {
+			getParam(current.result + '', result, 'balance', null, replaceTagsAndSpaces, parseBalanceMy);
+		} else if(current.name == 'getMiss') {
+			getParam(current.result + '', result, 'pay', null, replaceTagsAndSpaces, parseBalanceMy);
+		} else if(current.name == 'getBonus') {
+			getParam(current.result + '', result, 'bonus', null, replaceTagsAndSpaces, parseBalanceMy);
+		} else if(current.name == 'getName') {
+			getParam(current.result.last_name + ' ' + current.result.first_name, result, 'userName', null, replaceTagsAndSpaces);
+		} else if(current.name == 'getPeriodInfo') {
+			getParam(current.result.period_end, result, 'paytill', null, replaceTagsAndSpaces, parseFloat);
+			getParam(current.result.connection_type.name, result, '__tariff', null, replaceTagsAndSpaces, html_entity_decode);
+		}
+	}
+	
+	function parseBalanceMy(str) {
+		return parseBalance(str)/100;
+	}
 	
     AnyBalance.setResult(result);
 }

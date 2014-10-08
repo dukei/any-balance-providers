7e71769f09fc4f53e95f26a880d7746c465424db
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-08 10:02:56 +0000

    Dx.com

--- trunk/ab-service-dxcom/main.js
+++ trunk/ab-service-dxcom/main.js
@@ -13,27 +13,21 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://m.dealextreme.com/';
+	var baseurl = 'https://m.dx.com/';
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Enter login!');
 	checkEmpty(prefs.password, 'Enter password!');
 
-	var html = AnyBalance.requestGet(baseurl + 'accounts/login.dx', addHeaders({Referer: baseurl + ''}));
+	var html = AnyBalance.requestGet(baseurl + 'passport', addHeaders({Referer: baseurl}));
 
-	var params = createFormParams(html, function(params, str, name, value) {
-		if (name == 'ctl00$content$txtLoginEmail' || name == 'ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$txtEmail') 
-			return prefs.login;
-		else if (name == 'ctl00$content$txtPassword' || name == 'ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$txtPassword')
-			return prefs.password;
-
-		return value;
-	});
+	https://m.dx.com/passport/?r=https%3A%2F%2Fm.dx.com%2Fmy
 	
-	params['ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$btnLogin.x'] = 73;
-	params['ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$btnLogin.y'] = 10;
-
-	html = AnyBalance.requestPost(baseurl + 'accounts/login.dx', params, addHeaders({Referer: baseurl + 'accounts/login.dx'}));
+	html = AnyBalance.requestPost(baseurl + 'passport/?r=https%3A%2F%2Fm.dx.com%2Fmy', {
+		'ReturnUrl':'https://m.dx.com/my',
+		'username':prefs.login,
+		'password':prefs.password,
+	}, addHeaders({Referer: baseurl + 'accounts/login.dx'}));
 
 	if (!/logout|выход/i.test(html)) {
 		var error = sumParam(html, null, null, /<div>\s*<\s*span[^>]*color:Red[^>]*>([^<]+)/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
@@ -45,23 +39,23 @@ function main() {
 		throw new AnyBalance.Error('Can`t login, is the site changed?');
 	}
 	
-	html = AnyBalance.requestGet(baseurl + 'accounts/myorders.dx', addHeaders({Referer: baseurl + 'accounts'}));
+	html = AnyBalance.requestGet(baseurl + 'my/OrderList', addHeaders({Referer: baseurl + 'passport'}));
 
 	var result = {success: true};
 	
 	// getParam(html, result, 'points', /DX Points:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	
-	var firstTr = getParam(html, null, null, /<tr>(?:[^>]+>){8,10}\s*<\/tr>/i);
+	var firstTr = getParam(html, null, null, /<a href="\/my\/OrderDetail(?:[^>]+>){8,30}\s*<\/a>/i);
 	if(firstTr) {
-		getParam(firstTr, result, 'order_num', /(?:[^>]+>){2}\s*(\d{10,})/i, replaceTagsAndSpaces, html_entity_decode);
-		getParam(firstTr, result, 'order_date', /(?:[^>]+>){5}([^<]+)/i, replaceTagsAndSpaces, parseDate);
-		getParam(firstTr, result, 'order_status', /(?:[^>]+>){7}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(firstTr, result, 'order_num', /OrderDetail\/(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(firstTr, result, 'order_date', /<em>([^<]+)<\/em>/i, replaceTagsAndSpaces, parseDate);
+		getParam(firstTr, result, 'order_status', /<span>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 		
 		if(isAvailable('order_sum')) {
 			var href = getParam(firstTr, null, null, /href="\/([^"]+)/i);
 			if(href) {
-				html = AnyBalance.requestGet(baseurl + href, addHeaders({Referer: baseurl + 'accounts'}));
-				getParam(html, result, 'order_sum', /Grand Total:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+				html = AnyBalance.requestGet(baseurl + href, addHeaders({Referer: baseurl + 'passport'}));
+				getParam(html, result, 'order_sum', /Grand Total(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 			}
 		}
 	} else {

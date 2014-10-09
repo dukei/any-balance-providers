b7fc990b96b9cacde069dfadd542f1cf5a367643
stark027 <stark027@gmail.com>
2014-10-09 09:07:08 +0000

    Google Wallet (Checkout)

--- ab-money-google-checkout/main.js
+++ ab-money-google-checkout/main.js
@@ -20,7 +20,7 @@ function main() {
 	var baseurl = 'https://wallet.google.com/';
 	var baseurlLogin = 'https://accounts.google.com/';
 
-	if(!prefs.dbg) {
+	try {
 		var html = AnyBalance.requestGet(baseurlLogin + 'ServiceLoginAuth', g_headers);
 
 		var GALX = getParam(html, null, null, /GALX[^>]*value="([^"]*)/i);
@@ -33,48 +33,55 @@ function main() {
 			'GALX': GALX,
 			'PersistentCookie': 'yes',
 		}, g_headers);
-		
-		if (!/logout/i.test(html)) {
-			var error = getParam(html, null, null, /<span[^>]+class="errormsg[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-			if (error)
-				throw new AnyBalance.Error(error);
-			error = getParam(html, null, null, /(<form[^>]+name="verifyForm")/i, replaceTagsAndSpaces, html_entity_decode);
-			if (error)
-				throw new AnyBalance.Error('This account requires 2-step authorization. Turn off 2-step authorization to use this provider.');
-				
-			AnyBalance.trace(html);
-			throw new AnyBalance.Error('Can`t log in, is the site changed?');
-		}
-	} else {
+	} catch(e) {
 		var html = AnyBalance.requestGet('https://www.google.com/settings/account', g_headers);
 	}
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<span[^>]+class="errormsg[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error);
+		error = getParam(html, null, null, /(<form[^>]+name="verifyForm")/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error('This account requires 2-step authorization. Turn off 2-step authorization to use this provider.');
+			
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Can`t log in, is the site changed?');
+	}
+	
 	var result = {success: true};
 	
 	getParam(html, result, 'fio', /Account Options(?:[\s\S]*?<span[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'login_email', /Account Options(?:[\s\S]*?<span[^>]*>){8}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	// Получаем информацию из кошелька гугл
 	html = AnyBalance.requestGet(baseurl + 'merchant/pages/', g_headers);
-	if (/Google Wallet - Merchant Center/i.test(html)) {
-		var href = getParam(html, null, null, /(merchant\/pages[^"]+bcid[^"]+)\/earnings\/display"/i);
+	if (/Merchant Center/i.test(html)) {
+		var href = getParam(html, null, null, /merchant\/pages[^"]+\/transactions\/display/i);
 		if(!href) {
 			AnyBalance.trace(html);
 			throw new AnyBalance.Error('Can`t find transactions href, is the site changed?');
 		}
 		// Переходим на страницу Выплаты:
-		html = AnyBalance.requestGet(baseurl + href + '/transactions/display?selectedrange=LAST_THREE_MONTHS&filterchoice=ALL_TRANSACTIONS', g_headers);
+		html = AnyBalance.requestGet(baseurl + href, addHeaders({'Referer': 'Referer: https://wallet.google.com/'}));
 		
-		getParam(html, result, 'balance', /id="currentBalanceAmount"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, ['currency', 'balance'], /id="currentBalanceAmount"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseCurrencyMy);
-		getParam(html, result, 'last_payment', /lastSuccessfulPaymentAmount"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'last_payment_date', /lastSuccessfulPaymentAmount"[^>]*>[^>]*>[^>]*>[^>]*on([\s\S]*?)<\//i, replaceTagsAndSpaces, parseDate);
+		var pcid = getParam(html, null, null, /data-customer-id="([^"]+)/i);
+		
+		var paramsArray = [
+			'pcid=' + encodeURIComponent(pcid),
+			'style=' + encodeURIComponent('mc3:alerts=NO'),
+			'hostOrigin=' + encodeURIComponent('aHR0cHM6Ly93YWxsZXQuZ29vZ2xlLmNvbS8.'),
+			'hl=ru',
+			'ipi=' + encodeURIComponent('qhqo3ij9wquj'),
+		];
+		
+		html = AnyBalance.requestGet('https://bpui0.google.com/payments/u/0/transactions?' + paramsArray.join('&'), g_headers);
+		
+		getParam(html, result, 'balance', /id="balance"([^>]*>){2}/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, ['currency', 'balance'], /id="balance"([^>]*>){2}/i, replaceTagsAndSpaces, parseCurrency);
+		getParam(html, result, 'last_payment', /id="lastSuccessfulPayment"(?:[^>]*>){1}[^<(]+([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'last_payment_date', /id="lastSuccessfulPayment"(?:[^>]*>){1}([^<]+)/i, replaceTagsAndSpaces, parseDate);
 	} else {
 		AnyBalance.trace('Can`t login to Google Wallet, do have it on this account?');
 	}
 	AnyBalance.setResult(result);
-}
-
-function parseCurrencyMy(text) {
-	var val = getParam(html_entity_decode(text).replace(/\s+/g, ''), null, null, /(\S*?)-?\d[\d.,]*/);
-	AnyBalance.trace('Parsing currency (' + val + ') from: ' + text);
-	return val;
 }

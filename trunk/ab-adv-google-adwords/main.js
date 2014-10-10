8228de9c0746273f4a0e41ad6cc6f01bde86d9d0
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-10 10:35:52 +0000

    Google Adwords

--- trunk/ab-adv-google-adwords/main.js
+++ trunk/ab-adv-google-adwords/main.js
@@ -15,7 +15,6 @@ var g_headers = {
 }
 
 function main() {
-	var result = {success: true};
     var prefs = AnyBalance.getPreferences();
     
     AnyBalance.setDefaultCharset('utf-8');
@@ -61,53 +60,60 @@ function main() {
 			throw e;
 	}
 	
-	var lg = null;
-	var ng = function(a) {
-		for (var b = [], c = 0, d = 0; d < a.length; d++) {
-			for (var e = a.charCodeAt(d); 255 < e;) b[c++] = e & 255, e >>= 8;
-			b[c++] = e
-		}
-		//if (!da(b)) throw Error("M");
-		if (!lg)
-			for (lg = {}, mg = {}, a = 0; 65 > a; a++) lg[a] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".charAt(a), mg[a] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.".charAt(a);
-		a = mg;
-		c = [];
-		for (d = 0; d < b.length; d += 3) {
-			var f = b[d],
-				k = (e = d + 1 < b.length) ? b[d + 1] : 0,
-				l = d + 2 < b.length,
-				q = l ? b[d + 2] : 0,
-				O = f >> 2,
-				f = (f & 3) << 4 | k >> 4,
-				k = (k & 15) << 2 | q >> 6,
-				q = q & 63;
-			l || (q = 64, e || (k = 64));
-			c.push(a[O], a[f], a[k], a[q])
-		}
-		return c.join("")
-	};
-	var v = Date.now || function() {
-		return +new Date
-	};
-	
-	var ya = function() {
-		return Math.floor(2147483648 * Math.random()).toString(36) + Math.abs(Math.floor(2147483648 * Math.random()) ^ (0, v)()).toString(36)
-	};
-	
-	var pcid = getParam(html, null, null, /"pcid"[^>]*value="([^"]+)/i);
-	var hostOrigin = getParam(html, null, null, /"hostOrigin"[^>]*value="([^"]+)/i);
-	
-	hostOrigin = ng(hostOrigin || '');
-	
-	html = AnyBalance.requestGet('https://bpui0.google.com/payments/u/0/transactions?pcid=' + pcid + '&hostOrigin=' + hostOrigin + '&hl=ru&ipi=' + ya(), g_headers);
-	
-    if(/<form[^>]+name="tcaccept"/i.test(html)){
+	if(/<form[^>]+name="tcaccept"/i.test(html)){
         //Надо че-то принять, че-то у них изменилось.
         throw new AnyBalance.Error('Положения программы Google изменились. Пожалуйста, зайдите в ваш аккаунт Adwords через браузер и на вкладке "Сводка платежных данных" примите новые положения.');
     }
 	
-	getParam(html, result, 'balance', /Остаток(?:[^>]*>){2}\s*\(([^)]+)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'currency', /Остаток(?:[^>]*>){2}\s*\(([^)]+)/i, replaceTagsAndSpaces, parseCurrency);
-
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /currentBalance"(?:[^>]*>){1}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, ['currency', 'balance'], /currentBalance"(?:[^>]*>){1}([^<]+)/i, replaceTagsAndSpaces, function(str){return str.replace(/[^a-zA-ZА-Яа-я]/g, '');});
+	
+	if(!isset(result.balance)) {
+		var lg = null;
+		var ng = function(a) {
+			for (var b = [], c = 0, d = 0; d < a.length; d++) {
+				for (var e = a.charCodeAt(d); 255 < e;) b[c++] = e & 255, e >>= 8;
+				b[c++] = e
+			}
+			//if (!da(b)) throw Error("M");
+			if (!lg)
+				for (lg = {}, mg = {}, a = 0; 65 > a; a++) lg[a] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".charAt(a), mg[a] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.".charAt(a);
+			a = mg;
+			c = [];
+			for (d = 0; d < b.length; d += 3) {
+				var f = b[d],
+					k = (e = d + 1 < b.length) ? b[d + 1] : 0,
+					l = d + 2 < b.length,
+					q = l ? b[d + 2] : 0,
+					O = f >> 2,
+					f = (f & 3) << 4 | k >> 4,
+					k = (k & 15) << 2 | q >> 6,
+					q = q & 63;
+				l || (q = 64, e || (k = 64));
+				c.push(a[O], a[f], a[k], a[q])
+			}
+			return c.join("")
+		};
+		var v = Date.now || function() {
+			return +new Date
+		};
+		
+		var ya = function() {
+			return Math.floor(2147483648 * Math.random()).toString(36) + Math.abs(Math.floor(2147483648 * Math.random()) ^ (0, v)()).toString(36)
+		};
+		
+		var pcid = getParam(html, null, null, /"pcid"[^>]*value="([^"]+)/i);
+		var hostOrigin = getParam(html, null, null, /"hostOrigin"[^>]*value="([^"]+)/i);
+		
+		hostOrigin = ng(hostOrigin || '');
+		
+		html = AnyBalance.requestGet('https://bpui0.google.com/payments/u/0/transactions?pcid=' + pcid + '&hostOrigin=' + hostOrigin + '&hl=ru&ipi=' + ya(), g_headers);
+		
+		getParam(html, result, 'balance', /Остаток(?:[^>]*>){2}\s*\(([^)]+)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, ['currency', 'balance'], /Остаток(?:[^>]*>){2}\s*\(([^)]+)/i, replaceTagsAndSpaces, parseCurrency);
+	}
+	
     AnyBalance.setResult(result);
 }

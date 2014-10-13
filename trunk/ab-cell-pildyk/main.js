fd2584a4f65df79e70cec93e30aabadf3998b9e4
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-13 07:42:34 +0000

    pildyk

--- trunk/ab-cell-pildyk/main.js
+++ trunk/ab-cell-pildyk/main.js
@@ -1,8 +1,7 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс для абонента Pildyk (pildyk.lt)
 */
+
 var g_headers = {
 	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
@@ -15,6 +14,10 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     var baseurl = 'https://mano.pildyk.lt/';
     AnyBalance.setDefaultCharset('utf-8'); 
 
@@ -36,14 +39,19 @@ function main(){
 		'ctl00$ctl00$main$main$login$btnLogin':'Prisijungti'
 	}, addHeaders({Referer:baseurl+'Prisijungti.aspx?ReturnUrl=%2fdefault.aspx'}));
 
-	if(!/ctl00_ctl00_main_ctl00_userInfo_logoff/i.test(html)){
-        var error = getParam(html, null, null, /<div[\s\S]*?class="msg msg-error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
+	
+	if (!/ctl00_ctl00_main_ctl00_userInfo_logoff/i.test(html)) {
+		var error = getParam(html, null, null, /<div[\s\S]*?class="msg msg-error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}	
+	
 	var result = {success: true}; 
-	getParam(html, result, 'balance', /<p\s*id="userinfo"\s*[^>]*>[\s\S]*?Sąskaitos\s*likutis:\s*<strong>([^>]*?)\s*LT<\/strong>[\s\S]*?<\/p>/i, replaceTagsAndSpaces, parseBalance);
+	
+	getParam(html, result, 'balance', /[-\d\s,.]{2,}Eur/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'name', /<p\s*id="userinfo"\s*[^>]*>[\s\S]*?Prisijungęs:\s*<strong>([^>]*?)\s*<\/strong>[\s\S]*?<\/p>/i, replaceTagsAndSpaces, null);
     getParam(html, result, 'balanceExpire', /<th>Sąskaitos\s*likutis:[\s\S]*?<small\sclass="expiration_date">\s*galioja\s*iki\s*([^>]*?)\s*<\/small>\s*<\/th>/i, replaceTagsAndSpaces, parseDateISO);
 	

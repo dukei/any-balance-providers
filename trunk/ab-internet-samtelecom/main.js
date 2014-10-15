567c836c852a0d097cef79976e61e0e5e38142d2
julmatina@gmail.com <julmatina@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-15 15:08:27 +0000

    Новый провайдер: samtelecom

---
+++ trunk/ab-internet-samtelecom/main.js
@@ -0,0 +1,47 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://samtelecom.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'stat.php', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	html = AnyBalance.requestPost(baseurl + 'stat.php', {
+		'lic': prefs.login
+	}, addHeaders({Referer: baseurl + 'stat.php'}));
+	
+	if (/class="ballance_fail"/i.test(html)) {
+		var error = getParam(html, null, null, /class="ballance_fail"(?:[^>]*>){2}([\s\S]*?)<\/body/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /неверный формат/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось проверить баланс. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'account', /Состояние лицевого счета(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /составляет:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

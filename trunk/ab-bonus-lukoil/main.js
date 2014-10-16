3fffbd6159c676551f6e2e6f676db5802df26654
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-16 08:01:06 +0000

    Лукойл (Бонусы)

--- trunk/ab-bonus-lukoil/main.js
+++ trunk/ab-bonus-lukoil/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'http://my.licard.com/';
+    var baseurl = 'https://my.licard.com/';
     var baseurlFizik = 'http://club-lukoil.ru/';
 	
     AnyBalance.setDefaultCharset('utf-8');
@@ -20,11 +20,17 @@ function main() {
 	var result = {success: true};
 	
     if (prefs.type == 'likard') {
-        var html = AnyBalance.requestPost(baseurl + 'ru/login', {
-            submit: 'Войти',
-            login: prefs.login,
-            pass: prefs.password,
-        }, addHeaders({Referer: baseurl + 'ru/login'}));
+		var html = AnyBalance.requestGet(baseurl + 'ru/login', g_headers);
+		
+		try {
+			html = AnyBalance.requestPost(baseurl + 'ru/login', [
+				['login', prefs.login],
+				['pass' , prefs.password],
+				['submit', 'Войти'],
+			], addHeaders({Referer: baseurl + 'ru/login', 'Origin': 'https://my.licard.com'}));
+		} catch(e) {
+			html = AnyBalance.requestGet(baseurl + 'ru', addHeaders({Referer: baseurl + 'ru/login', 'Origin': 'https://my.licard.com'}));
+		}
         //получим id пользователя
         var usedId = /\/([\s\S]{1,15})\/client/i.exec(html);
         if (!usedId)
@@ -33,8 +39,7 @@ function main() {
         getParam(prefs.login, result, 'cardnum');
         getParam(html, result, 'balance', /Баланс[\s\S]*?>[\s\S]*?>([\s\S]*?)<\/b/i, replaceTagsAndSpaces, parseBalance);
         getParam(html, result, 'last_payment', /Последний платёж[\s\S]*?payments">([\s\S]*?)<\/a/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'name', /class="value user-name">\s*<b>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, null);
-        // для совместимости оставим так, потом когда все обновятся можно раскоментировать
+        getParam(html, result, 'name', /class="value user-name">\s*<b>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces);
     } else {
         var html = AnyBalance.requestGet(baseurlFizik + 'login', g_headers);
 		

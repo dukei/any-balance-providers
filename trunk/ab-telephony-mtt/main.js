8ab943bd0b17075f581cd1f5e3b8305d35962804
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-16 13:44:15 +0000

    MTT - Услуги связи

--- trunk/ab-telephony-mtt/main.js
+++ trunk/ab-telephony-mtt/main.js
@@ -1,56 +1,12 @@
 /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-MTT IP-телефония
-Сайт оператора: http://www.mtt.ru
-Личный кабинет: http://www.mtt.ru/sc
 */
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/ig, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-
-function parseBalance(text){
-    text = text.replace(/\s+|&nbsp;/ig, '');
-    var val = getParam(text, null, null, /(-?\d[\d\.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');    
-
-    var baseurl = 'http://www.mtt.ru/';
+	
+    var baseurl = 'http://old.mtt.ru/';
     
     var html = AnyBalance.requestGet(baseurl + "user/login?destination=sc");
     var form_build_id = getParam(html, null, null, /<input[^>]+name="form_build_id"[^>]*value="([^"]*)"[^>]*>/i);
@@ -71,9 +27,7 @@ function main(){
     if(error)
         throw new AnyBalance.Error(error);
      
-    var result = {
-        success: true
-    };
+    var result = {success: true};
 
     getParam(html, result, 'licschet', /<a [^>]*class="active"[^>]*>л\/с\s*(\d+)/i);
     getParam(html, result, 'balance', /Баланс:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
@@ -81,4 +35,3 @@ function main(){
 		
     AnyBalance.setResult(result);
 }
-

b2f025bdd462ed16b739a8dcc1d8a35df76b9b5c
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-21 09:05:18 +0000

    _history.bat

--- trunk/extra/development/tools/_history.bat
+++ trunk/extra/development/tools/_history.bat
@@ -1,2 +1,4 @@
-
-start wscript D:\anybalance_providers\extra\development\tools\x.wsf
+/*
+Use this file from provider folder only, otherwise it will not work properly
+*/
+start wscript ..\extra\development\tools\x.wsf

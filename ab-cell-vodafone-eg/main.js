080fc5f89b28c406b4135f6e331e32941b4c169b
stark027 <stark027@gmail.com>
2014-10-21 14:07:08 +0000

    Vodafone Egypt (ab-cell-vodafone-eg): - New counters: Data used, Date left, Data total. - Adapted to the new login procedure.

--- ab-cell-vodafone-eg/main.js
+++ ab-cell-vodafone-eg/main.js
@@ -1,18 +1,13 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плате для сотового оператора Vodafone Egypt
-
-Operator site: https://www.vodafone.com.eg
-Личный кабинет: https://www.vodafone.com.eg/My010App/
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 function main(){
@@ -20,9 +15,9 @@ function main(){
     var baseurl = "https://www.vodafone.com.eg/";
     var baseurl1 = "http://www.vodafone.com.eg/";
     AnyBalance.setDefaultCharset('utf-8'); 
-
-    var html = AnyBalance.requestGet(baseurl1 + 'vodafoneportalWeb/en/preMyBalance_Page', g_headers);
-
+	
+    var html = AnyBalance.requestGet('https://www.vodafone.com.eg/vodafoneportalWeb/en/myvodafone_login?pageCode=viewBill&&lang=en', g_headers);
+	
     if(!prefs.__dbg){
         html = AnyBalance.requestPost(baseurl + 'My010App/servlets/LoginManager?userLang=en', {
             redirectToUrl:baseurl + 'vodafoneportalWeb/en/preMyBalance_Page',
@@ -38,27 +33,44 @@ function main(){
             password:prefs.password,
             url:''
         }, addHeaders({Referer: baseurl})); 
-
-//        AnyBalance.trace(html);
-
-        if(!/window.open\('[^']*preMyBalance_Page/i.test(html)){
+		
+		var scriptRedirect = getParam(html, null, null, /window\.open\('([^'"]+)/i);
+		
+		html = AnyBalance.requestGet(scriptRedirect, g_headers);
+		
+		var res = scriptRedirect.split("?");
+		var iframeId = getParam(html, null, null, /"iframeId"[^>]*src="([^"]+)/i);
+		var url = iframeId + '?' + res[1]; 
+		
+		try {
+			html = AnyBalance.requestGet(url, g_headers);
+		} catch(e) {
+			html = AnyBalance.requestGet(url.replace(/http:/, 'https:'), g_headers);
+		}
+		
+		if(!/<h1>[^<]+Account/i.test(html)){
             throw new AnyBalance.Error('Could not enter personal account. Have you entered correct login and password?');
         }
     }
 
-    html = AnyBalance.requestGet(baseurl1 + 'My010App/pre_paid/services/ResultB.jsp?lang=en&_nfpb=true&_pageLabel=preMyBalance_Page&lang=en', g_headers);
-//        AnyBalance.trace(html);
+    // html = AnyBalance.requestGet(baseurl1 + 'My010App/pre_paid/services/ResultB.jsp?lang=en&_nfpb=true&_pageLabel=preMyBalance_Page&lang=en', g_headers);
 
     var result = {success: true};
-    getParam(html, result, 'phone', /<span[^>]+class="heading1"[^>]*>Mobile(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'fio', /<span[^>]+class="heading1"[^>]*>Name(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, function(str){ return html_entity_decode(str) || undefined } );
-    getParam(html, result, 'accnum', /Account Number(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /<span[^>]+class="heading1"[^>]*>Current Balance(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
 
-    html = AnyBalance.requestGet(baseurl1 + 'My010App/SessionDispatcher.jsp?pageCode=PPDCode&lang=en&_nfpb=true&_pageLabel=P12601258001323085993666&lang=en', g_headers);
-//        AnyBalance.trace(html);
-    getParam(html, result, '__tariff', /<span[^>]+class="currentPlan"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', />\s*Current Balance(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'phone', /Data Line Number:(?:[^>]*>){2}\s*([\d+-]+)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Data Plan:(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	
+	var usedTraf = getParam(html, result, 'traf_used', />\s*Consumed(?:[^>]*>){3}([\s\d.,GMKB]+)/i, replaceTagsAndSpaces, parseTraffic);
+	var leftTraf = getParam(html, result, 'traf_left', />\s*Remaining(?:[^>]*>){3}([\s\d.,GMKB]+)/i, replaceTagsAndSpaces, parseTraffic);
+	
+	if(isset(usedTraf) && isset(leftTraf)) {
+		getParam(usedTraf + leftTraf, result, 'traf_total');
+	}
+	
+	
+    // getParam(html, result, 'fio', /<span[^>]+class="heading1"[^>]*>Name(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, function(str){ return html_entity_decode(str) || undefined } );
+    // getParam(html, result, 'accnum', /Account Number(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 
-    //Возвращаем результат
     AnyBalance.setResult(result);
 }

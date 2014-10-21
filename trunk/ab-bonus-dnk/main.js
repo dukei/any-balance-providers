b091ea31fa5565abcce22793adc146395f138b8e
julmatina@gmail.com <julmatina@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-21 07:21:12 +0000

    Новый провайдер: днк

---
+++ trunk/ab-bonus-dnk/main.js
@@ -0,0 +1,64 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+    'X-Requested-With':'XMLHttpRequest'
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.dnk.by/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+    var form_key = getParam(html, null, null, /ajax_key["'].value=["']([\s\S]*?)"/i, replaceTagsAndSpaces, html_entity_decode);
+    
+	html = AnyBalance.requestPost(baseurl + 'ajax/ajax_auth_form.php', {
+		'USER_LOGIN': prefs.login,
+		'USER_PASSWORD': prefs.password,
+        'undefined':'Войти',
+        'AUTH_FORM':'Y',
+        'TYPE':'AUTH',
+		'ajax_key': form_key
+	}, addHeaders({Referer: baseurl}));
+	
+    
+	if (!/"type":"ok"/i.test(html)) {
+        var json = getJson(html);
+		var error = json.message;
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+    html = AnyBalance.requestGet(baseurl + 'personal/bonus_discount/', g_headers);
+    
+	var result = {success: true};
+	
+	getParam(html, result, 'account', /(?:input-certificate" value="){1}([\s\S]*?)"/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'personal_discount', /Персональная продукция:(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'prof_discount', /Профессиональная продукция:(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'name', /\/personal\/(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'bonus', /Бонусов:(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Баланс(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

5c6203110bafc6ddceacb4efe3492812a4a2a427
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-22 08:16:23 +0000

    УГМК-Телеком (ab-internet-ugmk): - Новые счетчики: Счет №, ФИО. - Поддержка отображения нескольких счетов в кабинете.

--- trunk/ab-internet-ugmk/main.js
+++ trunk/ab-internet-ugmk/main.js
@@ -2,22 +2,25 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
-function parseTrafficTotalGb(str){
-     var traffics = str.split(/\//g);
-     var total;
-     for(var i=0; i<traffics.length; ++i){
-         var val = parseBalance(traffics[i]);
-         if(typeof(val) != 'undefined')
-             total = (total || 0) + val;
-     }
-     
-     total = total && parseFloat((total/1024).toFixed(2));
-     AnyBalance.trace('Parsed total traffic ' + total + ' Gb from ' + str);
-     return total;
+function parseTrafficTotalGb(str) {
+	var traffics = str.split(/\//g);
+	var total;
+	for (var i = 0; i < traffics.length; ++i) {
+		var val = parseBalance(traffics[i]);
+		if (typeof(val) != 'undefined')
+			total = (total || 0) + val;
+	}
+	total = total && parseFloat((total / 1024).toFixed(2));
+	AnyBalance.trace('Parsed total traffic ' + total + ' Gb from ' + str);
+	return total;
 }
 
 function main(){
     var prefs = AnyBalance.getPreferences();
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     AnyBalance.setDefaultCharset('utf-8');
 
     var baseurl = "https://lk.ugmk-telecom.ru/";
@@ -28,7 +31,7 @@ function main(){
         USER_LOGIN:prefs.login,
         USER_PASSWORD:prefs.password
     });
-
+	
     if (!/logout=yes/i.test(html)) {
 		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
@@ -38,10 +41,18 @@ function main(){
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 	
+	var account = getParam(html, null, null, new RegExp('Счёт\\s+№\\s*\\d+' + (prefs.digits || '') + '(?:[^>]*>){41}(?:\\s*</div>){3}', 'i'));
+	checkEmpty(account, 'Не удалось найти ' + (prefs.digits ? 'счет с последними цифрами ' + prefs.digits : 'ни одного счета!'));
+	
     var result = {success: true};
     
-	getParam(html, result, 'balance', /На счете:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, '__tariff', /Текущий тариф:(?:[^>]*>){3}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(account, result, 'balance', /На счете:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(account, result, 'fio', /"logout"([^>]*>){7}/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(account, result, 'account', /Счёт\s*№\s*(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	var name = getParam(account, null, null, /usluga-name([^>]*>){3}/i, replaceTagsAndSpaces, html_entity_decode) || '';
+	var tarif = getParam(account, null, null, /Текущий тариф:([^>]*>){4}/i, replaceTagsAndSpaces, html_entity_decode) || '';
+	getParam(name + ' ' + tarif, result, '__tariff', null, replaceTagsAndSpaces, html_entity_decode);
 	
     AnyBalance.setResult(result);
 }

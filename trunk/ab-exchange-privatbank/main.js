aebca424094c1af6bb3f7f027ee8b4727bb33e41
julmatina@gmail.com <julmatina@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-24 14:20:33 +0000

    Курсы валют Приватбанка (ab-exchange-privatbank): Добавлен безналичный курс валют (конвертация по картам)

--- trunk/ab-exchange-privatbank/main.js
+++ trunk/ab-exchange-privatbank/main.js
@@ -6,16 +6,27 @@ function getRate(result, html, curr){
     getParam(html, result, curr + "_buy", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*buy="([^"]*)', 'i'), null, parseBalance);
     getParam(html, result, curr + "_sell", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*sale="([^"]*)', 'i'), null, parseBalance);
 }
+function getCardExchangeRate(result, html, curr){
+    getParam(html, result, curr + "_buy_card", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*buy="([^"]*)', 'i'), null, parseBalance);
+    getParam(html, result, curr + "_sell_card", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*sale="([^"]*)', 'i'), null, parseBalance);
+}
 
 function main() {
 	AnyBalance.trace('Connecting to forex...');
 
 	var info = AnyBalance.requestGet('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5');
+    
 	var result = {success: true};
 
 	getRate(result, info, 'USD');
 	getRate(result, info, 'EUR');
 	getRate(result, info, 'RUR');
 
+	var info = AnyBalance.requestGet('https://api.privatbank.ua/p24api/pubinfo?cardExchange');
+
+	getCardExchangeRate(result, info, 'USD');
+	getCardExchangeRate(result, info, 'EUR');
+	getCardExchangeRate(result, info, 'RUR');
+    
 	AnyBalance.setResult(result);
 }

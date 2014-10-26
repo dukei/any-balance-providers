42d6c8973c6a04d90e904602383de46df5a67813
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-26 10:26:59 +0000

    Payeer.com (ab-money-payeer-com): - Улучшение обработки ошибок

--- trunk/ab-money-payeer-com/main.js
+++ trunk/ab-money-payeer-com/main.js
@@ -9,11 +9,51 @@ var g_headers = {
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36',
 };
 
-var dateServer; //Дата и время загрузки страницы
+function login(baseurl, html){
+	var prefs = AnyBalance.getPreferences();
+
+	var form = getParam(html, null, null, /<form[^>]+id="(?:auth_captcha|authd)"[^>]*>([\s\S]*?)<\/form>/i);
+	
+	if(!form) {
+		if(/403 Forbidden/i.test(html))
+			throw new AnyBalance.Error('Payeer.com на время заблокировал вас. Пожалуйста, подождите часок и попробуйте снова.');
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+	}
+
+	var params = createFormParams(form);
+	params.email = prefs.login;
+	params.password = prefs.password;
+
+	//Действительно, капча, надо попробовать её ввести.
+	if(params.captcha_sid){
+    	var captcha = AnyBalance.requestGet(baseurl + '/bitrix/tools/captcha.php?captcha_sid=' + params.captcha_sid);
+    	captcha = AnyBalance.retrieveCode('Пожалуйста, введите код с картинки', captcha);
+    	AnyBalance.trace('Капча получена: ' + captcha);
+    	params.captcha = captcha;
+    }
+
+	html = AnyBalance.requestPost(baseurl + 'bitrix/components/payeer/system.auth.form/templates/index_list/ajax.php', params, addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));
+
+	var json = getJson(html).main;
+	if(json && json.location)
+		html = AnyBalance.requestGet(json.location, g_headers);
+	else if(json && json.html)
+		html = json.html;
+	else if(json.error){
+	    var errText = json.error.reduce(function(prev, cur){return prev += (prev ? ', ' : '') + cur.text}, '');
+		throw new AnyBalance.Error(errText, null, /Login\/Password incorrect|Логин\/Пароль введен неверно|Пользователь не найден|User not found/i.test(errText));
+	}else{
+	    AnyBalance.trace(html);
+	    throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+
+	return html;
+}
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://payeer.com';
+	var baseurl = 'https://payeer.com/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');
@@ -23,87 +63,17 @@ function main() {
 	AnyBalance.setCookie('payeer.com', 'BITRIX_SM_SOUND_LOGIN_PLAYED', 'Y');
 	AnyBalance.setCookie('payeer.com', 'BITRIX_SM_SALE_UID', '0');
 	
-	var html = AnyBalance.requestGet(baseurl, g_headers);
-	// AnyBalance.requestGet(baseurl + '/images/cms/pb8.ico', g_headers); //Эта штука что-то в сессию пишет, что логин правильный.
-	
-	// dateServer = getParam(html, null, null, /var\s+dateServer\s*=\s*'([^']+)/i);
-	
-	// if(!dateServer) {
-		// if(/403 Forbidden/i.test(html))
-			// throw new AnyBalance.Error('Payeer.com на время заблокировал вас. Пожалуйста, подождите часок и попробуйте снова.');
-		
-		// AnyBalance.trace(html);
-		// throw new AnyBalance.Error('Не удалось найти системную метку загрузки страницы. Сайт изменен?');
-	// }
-	
-	// var stT = jQueryEmulator.mdDONWKS();
-	// AnyBalance.sleep((2 + Math.random()*5)*1000); //Типа, вводим логин-пароль некоторое время.
+	var html = AnyBalance.requestGet(baseurl + 'ru/', g_headers);
 
-	// var hash = jQueryEmulator.modmd5(String(jQueryEmulator.mdDONWKS()-stT), true, prefs.login);
-	
-	var hash = getParam(html, null, null, /name="CHPM"[^>]*value="([^"]+)/i, replaceTagsAndSpaces);
-
-	html = AnyBalance.requestPost(baseurl + '/bitrix/components/payeer/system.auth.form/templates/index_list/ajax.php', {
-		'cmd':'Authorization',
-		'backurl':'/ru/account/',
-		'CHPM':hash,
-		'email':prefs.login,
-		'password':prefs.password,
-	}, addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));	
-	
-	// html = AnyBalance.requestPost(baseurl + '/ajax/index.php', {
-		// 'cmd':'Authorization',
-		// 'backurl':'/ru/account/',
-		// 'CHPM':hash,
-		// 'email':prefs.login,
-		// 'password':prefs.password,
-	// }, addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));
-	
-	var json = getJson(html);
-	if(json.main.location){
-		html = AnyBalance.requestGet(json.main.location, g_headers);
-	    //AnyBalance.requestGet(baseurl + '/images/cms/pb8.ico', g_headers); //Эта штука, возможно, что-то в сессию пишет, что логин правильный.
+	html = login(baseurl, html);
+	if (!/logout=yes/i.test(html)) {  //Возможно, нужна всё-таки капча
+		html = login(baseurl, html);
 	}
 
-	if (!/logout=yes/i.test(html)) {
-	    //Не пустили. Но может, капчу надо всё-таки ввести?
-	    var csid = getParam(html, null, null, /<input[^>]+name="captcha_sid"[^>]*value="([^"]*)/i, null, html_entity_decode);
-	    if(csid){ //Действительно, капча, надо попробовать её ввести.
-            if (AnyBalance.getLevel() >= 7) {
-            	AnyBalance.trace('Пытаемся ввести капчу');
-				var stT = jQueryEmulator.mdDONWKS();
-
-            	var captcha = AnyBalance.requestGet(baseurl + '/bitrix/tools/captcha.php?captcha_sid=' + csid);
-            	captcha = AnyBalance.retrieveCode('Пожалуйста, введите код с картинки', captcha);
-            	AnyBalance.trace('Капча получена: ' + captcha);
-
-				var hash = jQueryEmulator.modmd5(String(jQueryEmulator.mdDONWKS()-stT), true, prefs.login);
-
-				html = AnyBalance.requestPost(baseurl + '/ajax/index.php', {
-					'email':prefs.login,
-					'password':prefs.password,
-					show_captcha:'1',
-					captcha_sid: csid,
-					captcha_word: captcha,
-					Next: 'Войти на сайт',
-					'cmd':'auth_step1',
-					'backurl':'',
-					'CHPM':hash
-				}, addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));
-
-				var json = getJson(html);
-				if(json.location)
-					html = AnyBalance.requestGet(baseurl + json.location, g_headers);	
-            } else {
-            	throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
-            }
-        }
-    }
-	
 	if (!/logout=yes/i.test(html)) {  //А после капчи уже окончательно проверяем вход.
 		var error = getParam(html, null, null, /"form_error"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль|Пользователь не найден/i.test(error));
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль|Пользователь не найден|User not found/i.test(error));
 		
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
@@ -111,10 +81,10 @@ function main() {
 	
     var result = {success: true};
 	
-	getParam(html, result, 'acc_num', />\s*No. аккаунта(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'rub', />\s*RUB(?:[^>]*>){6}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'usd', />\s*USD(?:[^>]*>){6}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'eur', />\s*EUR(?:[^>]*>){6}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'acc_num', /<label[^>]*>No. аккаунта[\s\S]*?<span[^>]+class="val"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'rub', /<li[^>]+class="RUB"[^>]*>([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'usd', /<li[^>]+class="USD"[^>]*>([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'eur', /<li[^>]+class="EUR"[^>]*>([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, parseBalance);
 	
     AnyBalance.setResult(result);
 }

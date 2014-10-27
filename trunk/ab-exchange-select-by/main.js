aaedc5ef823636ea403249724837539211a0949a
julmatina@gmail.com <julmatina@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-10-27 14:54:16 +0000

    Исправление существующего провайдера: select-by

--- trunk/ab-exchange-select-by/main.js
+++ trunk/ab-exchange-select-by/main.js
@@ -63,42 +63,44 @@ function getText($row, result, counter, idx, bank){
 function main(){
 	AnyBalance.trace('Connecting to select.by...');
 	var prefs = AnyBalance.getPreferences();
-	var info = AnyBalance.requestGet('http://select.by/kurs/');
+    var city = prefs.city;
+	var info = AnyBalance.requestGet('http://select.by/kurs/' + city + '/');
 	var bank = prefs.bank;
 	
 	var result = {success: true};
 
         if(bank != 'nbrb'){
-            var table = getParam(info, null, null, /(<table[^>]*id="curr_table"[^>]*>[\s\S]*?<\/table>)/i);
+            var table = getParam(info, null, null, /(<table[^>]*class="tablesorter"[^>]*>[\s\S]*?<\/table>)/i);
 		if(!table)
 			throw new AnyBalance.Error('Не удаётся найти таблицу курсов!');
 	    
 		var $table = $(table);
+        var $row;
 	    
-            var $row;
 		if(bank){
 			bank = bank.toUpperCase();
-			$row = $table.find('tr>td:nth-child(2)>a').filter(
+			$row = $table.find('tr>td>a').filter(
 				function(){
-					return $.trim($(this).text().toUpperCase()) == bank
+					return $.trim($(this).text().toUpperCase()) == bank;
 				}
 			).parent().parent();
 		}else{
-			$row = $table.find('tr>th:nth-child(2):contains("Лучшие курсы")').parent();
+			$row = $table.find('tr>th:contains("Лучшие курсы")').parent();
 		}
-	    
+	               
+        
 		if($row.size() == 0)
 			throw new AnyBalance.Error('Не удаётся найти строку ' + prefs.bank);
 	    
-            getKurs($row, result, 'usdpok', 3, bank);
-            getKurs($row, result, 'usdprod', 4, bank);
-            getKurs($row, result, 'eurpok', 5, bank);
-            getKurs($row, result, 'eurprod', 6, bank);
-            getKurs($row, result, 'rurpok', 7, bank);
-            getKurs($row, result, 'rurprod', 8, bank);
-            getKurs($row, result, 'uepok', 9, bank);
-            getKurs($row, result, 'ueprod', 10, bank);
-            getText($row, result, 'tel', 11, bank);
+            getKurs($row, result, 'usdpok', 2, bank);
+            getKurs($row, result, 'usdprod', 3, bank);
+            getKurs($row, result, 'eurpok', 4, bank);
+            getKurs($row, result, 'eurprod', 5, bank);
+            getKurs($row, result, 'rurpok', 6, bank);
+            getKurs($row, result, 'rurprod', 7, bank);
+            getKurs($row, result, 'uepok', 8, bank);
+            getKurs($row, result, 'ueprod', 9, bank);
+            getText($row, result, 'tel', 10, bank);
 		if(AnyBalance.isAvailable('bank'))
 			result.bank = prefs.bank || 'Лучшие курсы';
 		result.__tariff = prefs.bank || 'Лучшие курсы';

43c5116ce75f4d7665904bf68db7e69226011230
tujger@gmail.com <tujger@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-11-06 10:27:03 +0000

    Добавлен счетчик "Рубли"

--- trunk/ab-cell-ncc/main.js
+++ trunk/ab-cell-ncc/main.js
@@ -25,38 +25,7 @@ function main(){
     }
 	
 	var result = {success: true};
-	// Зачем?!
-	// Татарстан
-	/*if(prefs.region == '1'){
-		if(AnyBalance.isAvailable('balance')){
-			if(matches = info.match(/<dd.*?>Баланс:<\/dd>\s*<dd.*?>(.*?) руб.<\/dd>/i)){
-				result.balance = parseFloat(matches[1].replace(',','.'));
-			}
-			else{
-				throw new AnyBalance.Error("Error");
-			}
-		}
-		if(AnyBalance.isAvailable('bonus')){
-			if(matches = info.match(/<dd.*?>Бонус:<\/dd>\s*<dd.*?>(.*?) руб.<\/dd>/i)){
-				result.bonus = parseFloat(matches[1].replace(',','.'));
-			}
-		}
-	}
-	else if(prefs.region == "2"){	// Нижегородский
-		if(AnyBalance.isAvailable('balance')){
-			if(matches = info.match(/<dd.*?>Баланс:<\/dd><dd.*?>(.*?) руб.<\/dd><\/dl>/i)){
-				result.balance = parseFloat(matches[1].replace(',','.'));
-			}
-			else{
-				throw new AnyBalance.Error("Error");
-			}
-		}
-		if(AnyBalance.isAvailable('bonus')){
-			if(matches = info.match(/<dd.*?>Бонус:<\/dd><dd.*?>(.*?) руб.<\/dd>/i)){
-				result.bonus = parseFloat(matches[1].replace(',','.'));
-			}
-		}
-	}*/
+
 	getParam(info, result, 'balance', /<dd[^>]*>Баланс:<\/dd>\s*<dd[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(info, result, 'bonus', /<dd[^>]*>Бонус:<\/dd>\s*<dd[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(info, result, 'filial', /<dd[^>]*>Филиал:<\/dd>\s*<dd[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
@@ -79,6 +48,8 @@ function main(){
 					getParam(row, result, 'accum_mins', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, null, parseBalance);
 				else if(/смс|sms/i.test(row))
 					getParam(row, result, 'accum_sms', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, null, parseBalance);
+				else if(/руб/i.test(row))
+					getParam(row, result, 'accum_rub', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, null, parseBalance);
 			}
 		}
 		else
@@ -88,7 +59,7 @@ function main(){
 	getParam(info, result, '__tariff', /Тарифный план(?:[\s\S]*?<td[^>]*>){3}\s*([\s\S]*?)\s*<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(info, result, 'acc_num', /Тарифный план(?:[\s\S]*?<td[^>]*>){1}\s*([\s\S]*?)\s*<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 
-	if(isAvailable('trafic'/*, 'traf1', 'traf2', 'traf3'*/)){
+	if(isAvailable('trafic')){
 		AnyBalance.trace('Fetching trafic info...');
 		var table = getParam(info, null, null, /Трафик за текущий месяц[\s\S]*?(<table[\s\S]*?<\/table>)/i, null, html_entity_decode);
 		if(table){
@@ -103,49 +74,6 @@ function main(){
 		}
 		else
 			AnyBalance.trace('Не удалось найти таблицу с трафиком. Сайт изменен?');
-		/*if(matches = info.match(/Трафик за текущий месяц[\s\S]*?Использовано([\s\S]*?)<\/table>/))
-		{
-			AnyBalance.trace("Fetching trafic info...");
-			var trafinfo = matches[1];
-
-			if(matches = trafinfo.match(/<tr>([\s\S]*?)<\/tr>/g))
-			{
-				/* всё равно закомментарено в манифесте 
-				result.traf1 = 0;
-				result.traf2 = 0;
-				result.traf3 = 0;
-				*
-				var globaltraf = 0;
-
-				for (i=0; i<matches.length; i++){
-	                		if(matches_line = matches[i].match(/<td.*?>(?:\s*)(.*?)<\/td>[\s\S]*?<td.*?>(?:\s*)(.*?) Кбайт<\/td>/)){
-						type = matches_line[1].toLowerCase();
-						var traf = parseFloat(matches_line[2].replace(',','.'))/1024;
-
-						/* всё равно закомментарено в манифесте 
-						if(type.indexOf("internet")>=0)
-							result.traf1 = result.traf1 + traf;
-						else
-						if(type.indexOf("wi-fi")>=0)
-							result.traf2 = result.traf2 + traf;
-						else
-						if(type.indexOf("wap")>=0)
-							result.traf3 = result.traf3 + traf;
-				                *
-						// Можно писать так globaltraf += traf;
-						globaltraf = globaltraf + traf;
-					}
-				}
-				/* всё равно закомментарено в манифесте 
-				result.traf1 = result.traf1.toFixed(3);
-				result.traf2 = result.traf2.toFixed(3);
-				result.traf3 = result.traf3.toFixed(3);*
- 				
-				// Опять же зачем? isAvailable('trafic') == true в данном случае всегда!
-				//if(AnyBalance.isAvailable('trafic'))
-				result.trafic = globaltraf.toFixed(3);
-			}
-		}*/
 	}
 	AnyBalance.setResult(result);
 };

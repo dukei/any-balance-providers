62f19841c8c92ba4b27e620d7173da5237a02b65
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-11-07 10:45:47 +0000

    УКРСИББАНК (ab-money-ukrsibbank): - Новые счетчики:Собственные средства, Доступная сумма, Общая задолженность, Пополнение за месяц, Списание за месяц, Доступный овердрафт.

--- trunk/ab-money-ukrsibbank/main.js
+++ trunk/ab-money-ukrsibbank/main.js
@@ -69,28 +69,64 @@ function fetchCard(html, baseurl) {
 	if(!tr)
 		throw new AnyBalance.Error('Не удалось найти ' + (prefs.lastdigits ? 'карту или счет с последними цифрами '+prefs.lastdigits : 'ни одного счета или карты!'));
 	
+	AnyBalance.trace(tr);
+	
 	var result = {success: true};
 	
 	getParam(tr, result, 'balance', /(?:[^>]*>){20}([\d\s.,]+)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(tr, result, ['currency', 'balance'], /(?:[^>]*>){18}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, '__tariff', /(?:[^>]*>){15}(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'account', /(?:[^>]*>){15}(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(tr, result, '__tariff', /(?:[^>]*>){15}(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(tr, result, 'account', /(?:[^>]*>){15}(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(tr, result, 'acc_name', /(?:[^>]*>){2}[^>]*title="([^"]+)/i, replaceTagsAndSpaces, html_entity_decode);
-	/*
+	
 	// Дополнительная инфа по картам.
-	if (AnyBalance.isAvailable('till', 'cardName', '', '', '', '', '', '', '', '')) {
-		//https://ibank.sbrf.com.ua/ifobsClientSBRF/BankingCardShow.action?accountid=1027820&cardid=52ec16d582d302adec054869fa0030ee
-		var href = getParam(tr, null, null, /<a\s+href="([^"]*)/i);
-		if(href) {
-			html = AnyBalance.requestGet(baseurl + 'ifobsClientSBRF/' + href, g_headers);
+	if (AnyBalance.isAvailable('own', 'avail', 'debt', 'fill', 'expense', 'overdraft')) {
+		var wForm = getParam(tr, null, null, /(welcomeForm:j_id_jsp_[^']+)/i);
+		var accountId = getParam(tr, null, null, /accountId'\s*,\s*'(\d+)/i);
+		
+		if(wForm && accountId) {
+			html = AnyBalance.requestPost(baseurl + 'protected/welcome.jsf', {
+				'welcomeForm_SUBMIT':'1',
+				'javax.faces.ViewState':getViewState(html),
+				'accountId':accountId,
+				'welcomeForm:_idcl': wForm,
+			}, addHeaders({Referer: baseurl + 'protected/welcome.jsf'}));
 			
-			getParam(html, result, 'till', /Срок действия карты:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
-			getParam(html, result, 'cardName', /Имя держателя карты:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
-			getParam(html, result, 'accNum', /Номер счета:(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+			getParam(html, result, 'own', /&#1042;&#1083;&#1072;&#1089;&#1085;&#1110; &#1082;&#1086;&#1096;&#1090;&#1080;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'avail', /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1072; &#1089;&#1091;&#1084;&#1072;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'debt', /&#1047;&#1072;&#1075;&#1072;&#1083;&#1100;&#1085;&#1072; &#1079;&#1072;&#1073;&#1086;&#1088;&#1075;&#1086;&#1074;&#1072;&#1085;&#1110;&#1089;&#1090;&#1100; &#1079;&#1072; &#1088;&#1072;&#1093;&#1091;&#1085;&#1082;&#1086;&#1084; &#1089;&#1082;&#1083;&#1072;&#1076;&#1072;&#1108;([\d\s.,]+)/i, replaceTagsAndSpaces, parseBalance);
+			
+			if(isAvailable(['fill', 'expense', 'overdraft'])) {
+				var periodParams = {
+					'cardAccountInfoForm:reportPeriod':'1',
+					'accountId':accountId,
+					'cardAccountInfoForm_SUBMIT':'1',
+					'javax.faces.ViewState':getViewState(html),
+				};
+				
+				var params = sumParam(html, null, null, /(cardAccountInfoForm:[^']+)'\),'dd.MM.yyyy'/ig);
+				
+				var dt = new Date();
+				var month = dt.getMonth()+1;
+				var year = dt.getFullYear();
+				
+				periodParams[params[0]] = '01.' + month + '.' + year;
+				periodParams[params[1]] = dt.getDate() + '.' + month + '.' + year;
+				
+				html = AnyBalance.requestPost(baseurl + 'protected/reports/sap_card_account_info.jsf', periodParams, addHeaders({Referer: baseurl + 'protected/welcome.jsf'}));
+				
+				getParam(html, result, 'fill', /&#1056;&#1072;&#1079;&#1086;&#1084; &#1089;&#1087;&#1080;&#1089;&#1072;&#1085;&#1085;&#1103; &#1082;&#1086;&#1096;&#1090;&#1110;&#1074;([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
+				getParam(html, result, 'expense', /&#1056;&#1072;&#1079;&#1086;&#1084; &#1087;&#1086;&#1087;&#1086;&#1074;&#1085;&#1077;&#1085;&#1100;([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
+				getParam(html, result, 'overdraft', /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1080;&#1081; &#1086;&#1074;&#1077;&#1088;&#1076;&#1088;&#1072;&#1092;&#1090;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			}
 		} else {
-			AnyBalance.trace('Не нашли ссылку на дополнительную информацию по картам, возможно, сайт изменился?');
+			AnyBalance.trace('Не нашли ссылку на дополнительную информацию, возможно, сайт изменился?');
 		}
-	}*/
+	}
 	
 	AnyBalance.setResult(result);
+}
+
+function getViewState(html) {
+	return getParam(html, null, null, /javax.faces.ViewState[^>]*value="([^"]+)/i);
 }

a45003f7ec50a3c13071da2f00ccc34ff1f5b843
stark027 <stark027@gmail.com>
2014-11-11 09:30:32 +0000

    Пермэнергосбыт (Пермь)

--- ab-municipal-permenergosbyt/main.js
+++ ab-municipal-permenergosbyt/main.js
@@ -8,17 +8,18 @@ Operator site: http://permenergosbyt.ru/
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 function main(){
     var prefs = AnyBalance.getPreferences();
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.surname, 'Введите фамилию!');
+	checkEmpty(prefs.phone, 'Введите телефон!');
 
     var baseurl = "https://test.permenergosbyt.ru/";
 
@@ -35,6 +36,10 @@ function main(){
             throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
+	
+	html = AnyBalance.requestPost(baseurl + 'Auth/Mobile', {
+        "Mobile":prefs.phone
+    }, addHeaders({Referer: baseurl + 'Auth/Mobile'})); 
 
     var result = {success: true};
     getParam(html, result, 'balance', /((?:Долг|Переплата)(?:\s|&nbsp;)*:\s*<span[^>]*>[\s\S]*?)<\/span>/i, [/Долг(?:\s|&nbsp;)*:/i, '-', replaceTagsAndSpaces], parseBalance);

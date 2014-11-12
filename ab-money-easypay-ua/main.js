2c670ae32799efc848148f34df09d3a98fa03c6e
stark027 <stark027@gmail.com>
2014-11-12 17:44:15 +0000

    EasyPay (Украина) (ab-money-easypay-ua): - Поддержка изменений на сайте.

--- ab-money-easypay-ua/main.js
+++ ab-money-easypay-ua/main.js
@@ -1,23 +1,21 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс кошелька системы электронных платежей EasyPay (https://easypay.ua)
-
-Operator site: http://xxxxxx.ru
-Личный кабинет: https://kabinet.xxxxxx.ru/login
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     var baseurl = "https://easypay.ua/";
 
     AnyBalance.setDefaultCharset('utf-8'); 
@@ -27,18 +25,22 @@ function main(){
         password:prefs.password
     }, addHeaders({Referer: baseurl + 'auth/signin'})); 
 
-    if(!/class="exit"/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]+class="error1"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
+	if(!/class="exit"/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="error1"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+    var result = {success: true};
+	
+	var tr = getParam(html, null, null, new RegExp("<tr>\\s*<td[^>]*>\\s*" + (prefs.number || '\\d+')+ "\\s*</td>\\s*<td[^>]*>[\\s\\S]*?<\/td>", "i"));
 
-    var result = {success: true},
-        reg = new RegExp("<tr>\\s*<td[^>]*>" + prefs.number + "<\/td>\\s*<td[^>]*>([\\s\\S]*?)<\/td>", "i");
-    getParam(html, result, 'balance', reg, replaceTagsAndSpaces, parseBalance);
-
-    console.log(reg); 
-
+	checkEmpty(tr, 'Не удалось найти ' + (prefs.number ? 'кошелек с последними цифрами ' + prefs.number : 'ни одного кошелька!'), true);
+	
+    getParam(tr, result, 'balance', /([^>]*>){5}/i, replaceTagsAndSpaces, parseBalance);
+	
     AnyBalance.setResult(result);
 }

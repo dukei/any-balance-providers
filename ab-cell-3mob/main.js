8659a18fbc3e94095ff2bd80762470da23d8f94f
igoloviznin <igoloviznin@gmail.com>
2014-11-12 13:41:22 +0000

    3моб. Вход в новый кабинет.

--- ab-cell-3mob/main.js
+++ ab-cell-3mob/main.js
@@ -7,52 +7,52 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36',
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://cabinet.3mob.ua';
+	var baseurl = 'https://my.3mob.ua';
 	AnyBalance.setDefaultCharset('utf-8');
 
-	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.login, 'Введите номер телефона!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 
 	var html = AnyBalance.requestPost(baseurl + '/ua/login', {
-		phone: prefs.login, 
+		phone: prefs.login,
 		password: prefs.password
 	}, addHeaders({Referer: baseurl + '/ua/', 'X-Requested-With': 'XMLHttpRequest'}));
 
-	var json = getJson(html);
+	if (/error/i.test(html)) {
+	error = getJson(html);  
+        if (error)
+		throw new AnyBalance.Error(error, null, / /i.test(error));
 
-	if(!json.result) {
-		var error = json.message;
-		if (error)
-			throw new AnyBalance.Error(error, null, /Невірний логін або пароль/i.test(error));
-
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-	}
-
-	html = AnyBalance.requestGet(baseurl + 'application/customer/balances', g_headers);
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+        }
 
 	var result = {success: true};
 
-	getParam(html, result, 'deadline', />\s*Дата деактивації:([^<]+)/i, replaceTagsAndSpaces, parseDate);
-	getParam(html, result, 'balance', />\s*Основний грошовий баланс([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'balance_bonus', />\s*Бонусний грошовий баланс([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'minutes_local', />\s*Голосовий баланс у мережі ТриМоб,&nbsp;сек.([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'minutes_roam', />\s*Голосовий баланс у мережі ТриМоб та нац. роумінгу,&nbsp;сек.([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'internet_roam', />\s*Інтернет у мережі ТриМоб та нац. роумінгу,&nbsp;Кб([^>]*>){4}/i, [replaceTagsAndSpaces, /(.*)/i, '$1 kb'], parseTraffic);
-	getParam(html, result, 'internet_local', />\s*Інтернет у мережі ТриМоб,&nbsp;Кб([^>]*>){4}/i, [replaceTagsAndSpaces, /(.*)/i, '$1 kb'], parseTraffic);
-	getParam(html, result, 'internet_roaming', />\s*Інтернет у нац. Роумінгу,&nbsp;Кб([^>]*>){4}/i, [replaceTagsAndSpaces, /(.*)/i, '$1 kb'], parseTraffic);
-	getParam(html, result, 'balance_sms', />\s*SMS([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'money_transfer', />\s*Money Transfer([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
+	html = AnyBalance.requestGet(baseurl + '/ua/profile', g_headers);
 
-	html = AnyBalance.requestGet(baseurl + 'application/customer', g_headers);
-	//Когда появится сменить __tariff на название тарифного плана
-	getParam(html, result, '__tariff', />\s*Номер телефону<[^>]*>\s*<[^>]*>\s*<[^>]*>([^<]*)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'mobphone', />\s*Номер телефону<[^>]*>\s*<[^>]*>\s*<[^>]*>([^<]*)</i, replaceTagsAndSpaces);
+	getParam(html, result, '__tariff', /Назва<[^>]*><[^>]*>([^<]*)</i, replaceTagsAndSpaces);
+	getParam(html, result, 'deadline', /Дата деактивації<[^>]*><[^>]*><[^>]*>([^<]+)</i, replaceTagsAndSpaces, parseDate);
+	getParam(html, result, 'balance', /Основний баланс<[^>]*><[^>]*>([^>]*) грн</i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance_bonus', /Бонусний баланс<[^>]*><[^>]*>([^>]*) грн</i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'mobphone', />\s*Номер телефону<[^>]*><[^>]*><[^>]*>([^<]*)</i, replaceTagsAndSpaces);
+
+	html = AnyBalance.requestGet(baseurl + '/ua/finance/balance', g_headers);
+
+	getParam(html, result, 'minutes_local', /Голосовий баланс в мережі Тримоб<[^>]*><[^>]*>([^<]*) сек</i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'minutes_roam', /Голосовий баланс в мережі Тримоб та нац.роумінгу<[^>]*><[^>]*>([^>]*) сек</i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'internet_local', /Інтернет в мережі Тримоб<[^>]*><[^>]*>([^>]*) Кбайт</i, [replaceTagsAndSpaces, /(.*)/i, '$1 kb'], parseTraffic);
+	getParam(html, result, 'internet_roam', /Інтернет в мережі Тримоб та нац.роумінгу<[^>]*><[^>]*>([^>]*) Кбайт</i, [replaceTagsAndSpaces, /(.*)/i, '$1 kb'], parseTraffic);
+	getParam(html, result, 'internet_roaming', /Інтернет в  нац.роумінгу<[^>]*><[^>]*>([^>]*) Кбайт</i, [replaceTagsAndSpaces, /(.*)/i, '$1 kb'], parseTraffic);
+	getParam(html, result, 'balance_sms', /СМС<[^>]*><[^>]*>([^>]*) штук</i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'money_transfer', /U\'transfer<[^>]*><[^>]*>([^>]*) грн</i, replaceTagsAndSpaces, parseBalance);
+
+	html = AnyBalance.requestGet(baseurl + '/logout', g_headers);
 
 	AnyBalance.setResult(result);
 }

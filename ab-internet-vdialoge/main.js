fc36de5f20a3e2ed7519b7441ac7ce1154b8f672
julmatina <julmatina@gmail.com>
2014-11-13 13:22:44 +0000

    Новый провайдер: vdialoge

---
+++ ab-internet-vdialoge/main.js
@@ -0,0 +1,46 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://vdialoge.com/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'stat/', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	html = AnyBalance.requestPost(baseurl + 'stat/', {
+		userlogin: prefs.login,
+		userpasswd: prefs.password
+	}, addHeaders({Referer: baseurl + 'stat/'}));
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /ui-icon-alert"[^>]*>[\s\S]*?([\s\S]*?)<\/div/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неправильные имя пользователя или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Состояние счета:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'number', /Номер вашего электронного лицевого счета[\s]*?:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

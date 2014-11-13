fbb4b1238e35a1771f24511ee62e30deed4c7fb8
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-11-13 09:24:51 +0000

    ОКейСити (ab-bonus-okeycity): - Поддержка изменений на сайте.

--- trunk/ab-bonus-okeycity/main.js
+++ trunk/ab-bonus-okeycity/main.js
@@ -14,24 +14,36 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = 'https://okeycity.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
-
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'site/login', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
 	var html = AnyBalance.requestPost(baseurl + 'site/login', {
-        'LoginForm[login]':prefs.login,
-        'LoginForm[password]=282485':prefs.password,
-		'yt0.x':35,
-		'yt0.y':4
-    }, addHeaders({Referer: baseurl + 'login'})); 
-
-    if(!/\/Logout/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-
+		'LoginForm[redirect]':'',
+		'LoginForm[login]': prefs.login,
+		'LoginForm[password]': prefs.password,
+		'LoginForm[rememberMe]':'0',
+		'yt0':'Вход'
+    }, addHeaders({Referer: baseurl + 'site/login'})); 
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
     var result = {success: true};
+	
     getParam(html, result, 'balance', /Баланс([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'fio', /leftuserinfo">[\s\S]{1,100}>\s*([\s\S]*?)\s*<\//i, replaceTagsAndSpaces, html_entity_decode);
-
+	
     AnyBalance.setResult(result);
 }

062cc466fffe7efc3b04058f9b58c015c0ac7a7a
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-11-13 09:40:07 +0000

    ОКейСити

--- trunk/ab-bonus-okeycity/main.js
+++ trunk/ab-bonus-okeycity/main.js
@@ -3,16 +3,16 @@
 */
 
 var g_headers = {
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'https://okeycity.ru/';
+    var baseurl = 'https://m.okeycity.ru/';
     AnyBalance.setDefaultCharset('utf-8'); 
 	
 	checkEmpty(prefs.login, 'Введите логин!');
@@ -42,8 +42,9 @@ function main(){
 	
     var result = {success: true};
 	
-    getParam(html, result, 'balance', /Баланс([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'fio', /leftuserinfo">[\s\S]{1,100}>\s*([\s\S]*?)\s*<\//i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Баланс:([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /Карта:([^>]*>){3}/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'fio', /([^>]+)<a href[^>]*logout/i, replaceTagsAndSpaces, html_entity_decode);
 	
     AnyBalance.setResult(result);
 }

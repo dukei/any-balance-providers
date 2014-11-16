fc21001ad0722b95b5ca7f80cee3941f275e10f8
stark027 <stark027@gmail.com>
2014-11-16 08:40:48 +0000

    Сибирские Сети (ab-internet-211): - Исправлена ошибка авторизации.

--- ab-internet-211/main.js
+++ ab-internet-211/main.js
@@ -10,15 +10,23 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('UTF-8');
 
-    var baseurl = 'https://cabinet.211.ru/';
+    // var baseurl = 'https://cabinet.211.ru/';
+    var baseurl = 'http://passport.211.ru/';
 
-    var html = AnyBalance.requestGet(baseurl + 'auth/index', g_headers);
+    var html = AnyBalance.requestGet(baseurl + 'user/index', g_headers);
 	
-	html = AnyBalance.requestPost(baseurl + 'auth/index', {
-		retpath: baseurl,
-		login:prefs.login,
-		password:prefs.password
-	}, addHeaders({Referer: baseurl + '/user/index'}));
+	try {
+		html = AnyBalance.requestPost('http://header.211.ru/', {
+			retpath: baseurl,
+			login:prefs.login,
+			password:prefs.password
+		}, addHeaders({Referer: baseurl + '/user/index'}));
+	} catch(e) {
+		if(prefs.dbg)
+			html = AnyBalance.requestGet(baseurl + 'profile/', g_headers);
+		else
+			throw e;
+	}
 	
     if(!/(\/logout)/i.test(html)){
         var error = sumParam(html, null, null, /<font[^>]*class="error"[^>]*>([\s\S]*?)<\/font>/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
@@ -27,13 +35,15 @@ function main(){
         error = sumParam(html, null, null, /<span[^>]*class="input-message"[^>]*>([\s\S]*?)<\/span>/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
         if(error)
             throw new AnyBalance.Error(error);
+			
+		AnyBalance.trace(html);
         throw new AnyBalance.Error("Не удалось войти в паспорт! Изменения на сайте?");
     }
-
+	
     var result = {success: true};
 
-    getParam(html, result, 'balance', /Итого на[\s\d.]{8,}(?:[^>]*>){4}([-\s\d.,]+)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, '__tariff', /Ваш тариф:([^<]*)/i, replaceTagsAndSpaces);
+	getParam(html, result, 'balance', /<a[^>]*class="header-balance-button[^"]*"[^>]*>([\S\s]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, '__tariff', /Ваш тариф:([^<]+)/i, replaceTagsAndSpaces);
     getParam(html, result, 'bonus', /<a[^>]*class="header-bonus-button[^"]*"[^>]*>([\S\s]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance);
 
     AnyBalance.setResult(result);

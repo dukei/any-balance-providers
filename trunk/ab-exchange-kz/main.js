acd3b66d5fb58bfd47bf74875e8512240e4b7f6b
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-11-18 07:52:16 +0000

    Курсы валют Нацбанка РК (ab-exchange-kz): - Поддержка изменений на сайте.

--- trunk/ab-exchange-kz/main.js
+++ trunk/ab-exchange-kz/main.js
@@ -1,20 +1,26 @@
-﻿function getRate(result, item, code){
-        if($(item).find("title").text() == code && AnyBalance.isAvailable(code)){
-                       result[code] = $(item).find("description").text();
-         }
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
 
-} 
-        
-function main(){
-        AnyBalance.trace('Connecting to www.nationalbank.kz');        
-        var result = {success: true};          
-        var info = AnyBalance.requestGet('http://www.nationalbank.kz/rss/rates_all.xml');
-        var xmlDoc = $.parseXML(info), $xml = $(xmlDoc);
-        $xml.find("item").each(
-                function(){
-                   getRate(result,$(this),'USD');
-                   getRate(result,$(this),'EUR');
-                   getRate(result,$(this),'RUB');
-                }); 
-         AnyBalance.setResult(result);
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.nationalbank.kz/rss/rates_all.xml';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	var html = AnyBalance.requestGet(baseurl);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	var result = {success: true};
+	
+	getRate(html, result, 'USD');
+	getRate(html, result, 'EUR');
+	getRate(html, result, 'RUB');
+	
+	AnyBalance.setResult(result);
+}
+
+function getRate(html, result, counter){
+	getParam(html, result, counter, '<title>\\s*' + counter + '\\s*</title>(?:[^>]*>){2}\\s*<description>([\\s\\d.,]+)', replaceTagsAndSpaces, parseBalance);
 }

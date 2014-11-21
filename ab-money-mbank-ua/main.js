669448b45d158b1b9427500fc8ef9af23e574956
igoloviznin <igoloviznin@gmail.com>
2014-11-21 14:34:13 +0000

    Банк Михайловский. Доделан до рабочего состояния.

--- ab-money-mbank-ua/main.js
+++ ab-money-mbank-ua/main.js
@@ -7,37 +7,44 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36',
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'https://mpay.mbank.kiev.ua/';
 	AnyBalance.setDefaultCharset('utf-8');
-	
-	checkEmpty(prefs.login, 'Введите логин!');
-	checkEmpty(prefs.password, 'Введите пароль!');
-	
+
+	checkEmpty(prefs.login, 'Введіть логін!');
+	checkEmpty(prefs.password, 'Введіть пароль!');
+
 	var html = AnyBalance.requestPost(baseurl + 'uk/security/logon', {
 		login: prefs.login,
 		password: prefs.password
 	}, addHeaders({Referer: baseurl + 'uk/security/logon'}));
-	
+
 	if (!/btn-logout/i.test(html)) {
 		var error = getParam(html, null, null, /<div[^>]+class="[^"]*alert[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
 			throw new AnyBalance.Error(error, null, /Недійсний логін чи пароль/i.test(error));
-		
+
 		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+		throw new AnyBalance.Error('Не вдалося зайти в інтернет-банкінг. Сайт змінено?');
 	}
-	
+
 	var result = {success: true};
 
-	var html = AnyBalance.requestGet(baseurl + 'uk/widgets/products/balance/data/UAH?_=' + new Date().getTime(), g_headers);
+	getParam(html, result, '__tariff', /home.png" \/>\s*<\/span>\s*<span class=[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+
+	html = AnyBalance.requestGet(baseurl + 'uk/widgets/products/balance/data/UAH?_=' + new Date().getTime(), g_headers);
 	var json = getJson(html);
-	
+
 	getParam(json.AccountsAvailableAmount/100, result, 'balance');
-	
+	getParam(json.AccountsAvailableAmountOwn/100, result, 'balance_my');
+	getParam(json.DepositsBalanceAmount/100, result, 'balance_vklad');
+	getParam(json.LoansBalanceAmount/100, result, 'balance_kred');
+
+	html = AnyBalance.requestGet(baseurl + '/uk/security/logout', g_headers);
+
 	AnyBalance.setResult(result);
 }

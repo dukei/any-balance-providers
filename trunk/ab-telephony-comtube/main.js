1827929041cfbb946ddc7432be24e7d7d4431ebb
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-11-24 12:45:33 +0000

    ComTube (ab-telephony-comtube): - Поддержка изменений на сайте.

--- trunk/ab-telephony-comtube/main.js
+++ trunk/ab-telephony-comtube/main.js
@@ -1,9 +1,5 @@
  /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-ComTube IP-телефония
-Сайт оператора: https://www.comtube.com/
-Личный кабинет: https://www.comtube.com/
 */
 
 function main(){
@@ -25,22 +21,17 @@ function main(){
             throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удаётся войти в личный кабинет. Сайт изменен?');
     }
-     
-    var result = {
-        success: true
-    };
-
-    var pset = getParam(info, null, null, /_pset\s*=\s*(\{[\s\S]*?\});/, null, getJson);
-
-    getParam(pset.curr_balance, result, 'balance', null, replaceTagsAndSpaces, parseBalance);
-    getParam(pset.currency, result, ['currency', 'balance'], null, replaceTagsAndSpaces);
+	
+	var result = {success: true};
+	
+    getParam(info, result, 'balance', /curr_balance":([\d\s".,\-]+)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(info, result, ['currency', 'balance'], /currency":"([^"]+)/i, replaceTagsAndSpaces);
     getParam(info, result, 'number', /(?:Ваш номер внутри|You number in) Comtube:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
 
     if(AnyBalance.isAvailable('invest')){
         info = AnyBalance.requestGet(baseurl + 'index/payment_invest_info');
         getParam(info, result, 'invest', /Баланс инвестиционного счета:[\s\S]*?<span[^>]+class="p-big"[^>]*>([\s\S]*?)<\/span>/i, [/,/g, '', replaceTagsAndSpaces], parseBalance);
     }
-		
-    AnyBalance.setResult(result);
+	
+	AnyBalance.setResult(result);
 }
-

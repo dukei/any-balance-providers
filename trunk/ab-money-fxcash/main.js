d5a878e6108d04f2a6b1b9249ea78763906a2dd7
tujger@gmail.com <tujger@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-11-25 15:28:31 +0000

    Добавлен мониторинг forexcashbackrebate.com

--- trunk/ab-money-fxcash/main.js
+++ trunk/ab-money-fxcash/main.js
@@ -13,32 +13,77 @@ var g_headers = {
 function main() {
 	var html;
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://my.fxcash.ru/';
+	var baseurl;
 	AnyBalance.setDefaultCharset('utf-8');
-
-	
-	checkEmpty(prefs.login, 'Enter login!');
-	checkEmpty(prefs.password, 'Enter password!');
-
-	try {
-		html = AnyBalance.requestPost(baseurl, {
-			'login':prefs.login,
-			'password':prefs.password,
-			'auth':'1',
-		}, addHeaders({Referer: baseurl}));
-	} catch(e) {
-		throw new AnyBalance.Error('Ошибка при запросе авторизации.');
-	}
-	if (/Имя пользователя или пароль введен неверно/i.test(html)) {
-		throw new AnyBalance.Error('Имя пользователя или пароль введен неверно.');
-	}
 
 	var result = {success: true};
 
-	result.__tariff = prefs.login;
-	getParam(html, result, 'accessible', /Доступно: <b>(.*?)</, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'balance', /Баланс: <b>(.*?)</, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'bonus', /Ваш бонус: <span .*?>(.*?)</, replaceTagsAndSpaces, parseBalance);
+
+	result.balance=0;
+	result.balance_fcbr=0;
+
+//fxcash.ru
+	if(prefs.fxc==1){
+		baseurl = 'https://my.fxcash.ru/';
+
+		checkEmpty(prefs.login, 'Enter login!');
+		checkEmpty(prefs.password, 'Enter password!');
+
+		g_headers['Referer']=baseurl;
+
+		try {
+			html = AnyBalance.requestPost(baseurl, {
+				'login':prefs.login,
+				'password':prefs.password,
+				'auth':'1',
+			}, g_headers);
+		} catch(e) {
+			throw new AnyBalance.Error('Error on authorization on fxcash.ru.');
+		}
+		if (/Имя пользователя или пароль введен неверно/i.test(html)) {
+			throw new AnyBalance.Error('Incorrect login or password on fxcash.ru.');
+		}
+
+		result.__tariff = prefs.login;
+		getParam(html, result, 'accessible', /Доступно: <b>(.*?)</, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'balance', /Баланс: <b>(.*?)</, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'bonus', /Ваш бонус: <span .*?>(.*?)</, replaceTagsAndSpaces, parseBalance);
+	}
+
+//forexcashbackrebate.com
+	if(prefs.fcbr==1){
+		baseurl = 'http://www.forexcashbackrebate.com/';
+		html = AnyBalance.requestGet(baseurl, g_headers);
+		var aret=getParam(html, null, null, /input type="hidden" name="return" value="(.*?)"/, null, null);
+		var bret=getParam(html, null, null, /input type="hidden" name="([^<>]*?)" value="1" /, null, null);
+
+		var p_headers={
+			'username':prefs.login_fcbr,
+			'password':prefs.password_fcbr,
+			'Submit':'',
+			'remember':'yes',
+			'option':'com_users',
+			'task':'user.login',
+			'return':aret,
+		};
+		p_headers[bret]='1';
+		g_headers['Referer']=baseurl;
+
+		html = AnyBalance.requestPost(baseurl,p_headers,g_headers);
+
+		var err=getParam(html, null,null, /"alert-heading">Warning[\s\S]*?<div>([\s\S]*?)<\/div>/, replaceTagsAndSpaces, html_entity_decode);
+		if(err!=null){
+			throw new AnyBalance.Error(err+" (forexcashbackreturn.com)");
+		}
+
+
+
+		getParam(html, result, 'balance_fcbr', /Account Balance \(\$\) :<\/b>(.*?)<br>/, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'withdrawn_fcbr', /Net Earnings Paid Out \(\$\) :<\/b>(.*?)<\/div>/, replaceTagsAndSpaces, parseBalance);
+
+	}
+
+	result.total_balance=result.balance*1+result.balance_fcbr*1;
 	
 	AnyBalance.setResult(result);
 }
---
+++ trunk/ab-money-fxcash/preferences.js
@@ -0,0 +1,50 @@
+﻿/*
+Provider of AnyBalance (http://any-balance-providers.googlecode.com)
+
+Provider for FX Rebate
+WWW: http://www.fxcash.ru
+WWW: http://www.forexcashbackrebate.com
+mailto:wtiger@mail.ru
+*/
+function onChangeType(){
+
+	var props = AnyBalance.getPreferenceProperties({
+		fxc: {value: ''},
+		login: {visible: ''},
+		password: {visible: ''},
+		fcbr: {value: ''},
+		login_fcbr: {visible: ''},
+		password_fcbr: {visible: ''}
+	});
+
+	if(props.fxc.value == 1){
+		AnyBalance.setPreferenceProperties({
+			login: {visible: true},
+			password: {visible: true}
+		});
+	}else{
+		AnyBalance.setPreferenceProperties({
+			login: {visible: false},
+			password: {visible: false}
+		});
+	}
+
+	if(props.fcbr.value == 1){
+		AnyBalance.setPreferenceProperties({
+			login_fcbr: {visible: true},
+			password_fcbr: {visible: true}
+		});
+	}else{
+		AnyBalance.setPreferenceProperties({
+			login_fcbr: {visible: false},
+			password_fcbr: {visible: false}
+		});
+	}
+
+}
+
+function main(){
+	AnyBalance.addCallback('change#fxc', onChangeType);
+	AnyBalance.addCallback('change#fcbr', onChangeType);
+	onChangeType();
+}

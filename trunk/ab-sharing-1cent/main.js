762af3a1d41dd68d04e272869a4d2403d183ff0c
julmatina@gmail.com <julmatina@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-12-02 08:32:07 +0000

    Новый провайдер: 1cent

---
+++ trunk/ab-sharing-1cent/main.js
@@ -0,0 +1,52 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://1cent.in/';
+	AnyBalance.setOptions({forceCharset:'utf-8'});
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'login.php', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+        
+	html = AnyBalance.requestPost(baseurl + 'login.php', {
+		'userName': prefs.login,
+		'userPass': prefs.password,
+		'loginIn': 'ok'
+	}, addHeaders({Referer: baseurl}));
+    
+	if (!/Добро пожаловать/i.test(html)) {
+		var error = getParam(html, null, null, /(?:system message[\s\S]*?<td class="head5">){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /неправильно введен логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+    html = AnyBalance.requestGet(baseurl + 'dealer.php', g_headers);
+    
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Баланс:([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);    
+	getParam(html, result, 'dw_key', /(?:Cтоимость одного dw \(ключа\):[\s]*?\[)([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'dw_users', /(?:Сумма dw юзеров[\s:]*?\[)([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'sum', /(?:На сумму[\s:]*?\[)([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'status', /(?:font color="#006600">)([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

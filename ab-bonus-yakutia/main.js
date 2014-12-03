328b46035c57084ab8d938a926996673bb3fc4bc
julmatina <julmatina@gmail.com>
2014-12-03 08:28:05 +0000

    Новый провайдер: yakutia

---
+++ ab-bonus-yakutia/main.js
@@ -0,0 +1,59 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://ffr3.loyaltyplus.aero/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'loyalty/home.seam', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	html = AnyBalance.requestPost(baseurl + 'loyalty/home.seam', {
+        'login': 'login',
+        'login:usernameDecorate:username': prefs.login,
+        'login:passwordDecorate:password': prefs.password,
+        'login:login': 'Войти в аккаунт',
+        'javax.faces.ViewState': 'j_id1'
+	}, addHeaders({Referer: baseurl + 'loyalty/home.seam'}));
+    
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /class="errors"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /неверные учетные данные/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+    html = AnyBalance.requestGet(baseurl + 'loyalty/main.seam', g_headers);
+	
+    html = html_entity_decode(html);
+    
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Доступные баллы:(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'basic', /Основные баллы:(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'premium', /Премиальные баллы:(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'used', /Использованные баллы:(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'expired', /Истекшие баллы:(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'card_number', /class="profile_welcome">(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'deadline', /Срок действия карточки:([\s\S]*?)<\//i, replaceTagsAndSpaces, parseDate);
+	getParam(html, result, '__tariff', /Уровень:([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

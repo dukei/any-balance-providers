3e548c982f32d1ce26b72b4e3416fdffcbe94de0
anyremotemail <anyremotemail@gmail.com>
2014-12-07 11:25:56 +0000

    new provider ab-municipal-korolev-elektroset

---
+++ ab-municipal-korolev-elektroset/main.js
@@ -0,0 +1,54 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+var g_headers = {
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+};
+
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    var baseurl = 'https://lkk.kensbyt.ru/';
+    AnyBalance.setDefaultCharset('windows-1251'); 
+
+    if(!prefs.login)
+	    throw new AnyBalance.Error('Вы не ввели логин');
+    if(!prefs.password)
+	    throw new AnyBalance.Error('Вы не ввели пароль');
+
+    var html = AnyBalance.requestPost(baseurl+"auth", {
+                username: prefs.login,
+                password: prefs.password
+    });
+
+    //AnyBalance.trace('GOT ' + html);
+
+    var p1 = html.lastIndexOf('Неверный пароль');
+    if (p1 > 0)
+        throw new AnyBalance.Error('Неверный логин или пароль.');
+    
+    var html = AnyBalance.requestGet(baseurl+"personal");
+    //AnyBalance.trace('GOT ' + html);
+
+    //Проверяем маркер успешного входа
+    p1 = html.lastIndexOf('Потребитель:');
+    if(p1 < 0){
+         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+
+    var result = {success: true};
+    var v = getParam(html, result, 'credit', /Ваша задолженность по состоянию[^>]*<strong>(\S+)\s*<\/strong>/i, replaceTagsAndSpaces, parseBalance);
+    //AnyBalance.trace('BL  ' + v);
+
+    v = getParam(html, result, 'id', /Потребитель[^>]+>[^>]+>[^>]+>([^<]+)</, replaceTagsAndSpaces, html_entity_decode);
+    //AnyBalance.trace('ID  ' + v);
+
+    v = getParam(html, result, 'period',  /Отчетный период[^>]+>[^>]+>[^>]+>([^<]+)</i, replaceTagsAndSpaces, html_entity_decode);
+    //AnyBalance.trace('period  ' + v);
+
+    //Возвращаем результат
+    AnyBalance.setResult(result);
+}

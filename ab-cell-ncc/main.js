f952da26698e92491907d0a1de40265c037eab62
dmitry.kochin <dmitry.kochin@gmail.com>
2014-12-07 20:26:31 +0000

    Ростелеком-Волга (НСС) (ab-cell-ncc): - Переезд личного кабинета на https://login.rt.ru

--- ab-cell-ncc/main.js
+++ ab-cell-ncc/main.js
@@ -8,7 +8,117 @@
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://iserve.ncc-volga.ru/';
+	var baseurl = 'https://login.rt.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	var html = AnyBalance.requestGet(baseurl);
+	
+	var matches = html.match(/<input[^>]*name="(csrf[^"]*)"[^>]*value="([^"]*)"/i);
+	if (!matches) {
+		var error = getParam(html, null, null, /<div[^>]+id="error-wrapper"[^>]*>([\s\S]*?)(?:<a |<\/div>)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error) throw new AnyBalance.Error(error);
+		throw new AnyBalance.Error("Не удаётся найти код безопасности для входа. Проблемы на сайте или сайт изменен.");
+	}
+	AnyBalance.trace("Trying to enter selfcare at address: " + baseurl);
+	var params = {
+		j_username: prefs.userv,
+		j_password: prefs.passv,
+		_redirectToUrl: '',
+		is_ajax: true,
+	};
+	params[matches[1]] = matches[2]; //Код безопасности
+
+	html = AnyBalance.requestPost(baseurl + "public/security/check", params, {"X-Requested-With": "XMLHttpRequest"});
+	
+	var error = getParam(html, null, null, /<div id="error-wrapper">[\s\S]*?<p>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces);
+	if (error) throw new AnyBalance.Error(error, null, /Ошибка в номере телефона|Пароль неправильный/i.test(error));
+	var json = JSON.parse(html.replace(/[\x0D\x0A]+/g, ' '));
+	if (!json.success) throw new AnyBalance.Error(json.error, null, /Ошибка в номере телефона|Пароль неправильный/i.test(json.error));
+	var result = {success: true}; //Баланс нельзя не получить, не выдав ошибку!
+	if (AnyBalance.isAvailable('balance')) {
+		result.balance = null; //Баланс должен быть всегда, даже если его не удаётся получить. 
+		//Если его не удалось получить, то передаём null, чтобы значение взялось из предыдущего запроса
+	}
+	html = AnyBalance.requestGet(baseurl + "home");
+	
+	getParam(html, result, "userName", /<div[^>]+id="top-profile-subscriber-name"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, "phone", /<div[^>]+id="top-profile-subscriber-phone"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, '__tariff', /Тариф<\/h2>[\s\S]*?>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	var matches = html.match(/(csrf[^:]*):\s*'([^']*)'/i);
+	if (!matches) throw new AnyBalance.Error("Не удаётся найти код безопасности для запроса балансов. Свяжитесь с автором провайдера для исправления.");
+	var tokName = matches[1],
+		tokVal = matches[2];
+	if (AnyBalance.isAvailable('balance')) {
+		AnyBalance.trace("Searching for balance");
+		var params = {};
+		params[tokName] = tokVal;
+		params.isBalanceRefresh = false;
+		html = AnyBalance.requestPost(baseurl + "balance/json", params);
+		json = JSON.parse(html);
+		result.balance = parseBalance(json.balance);
+	}
+	if (AnyBalance.isAvailable('accum_sms', 'accum_mins', 'accum_trafic')) {
+		AnyBalance.trace("Searching for used resources in this month");
+		var params = {};
+		params[tokName] = tokVal;
+		params.isBalanceRefresh = false;
+		html = AnyBalance.requestPost(baseurl + "payments/summary/json", params);
+		json = JSON.parse(html);
+		for (var i = 0; i < json.length; ++i) {
+			var name = json[i].name;
+			var matches;
+			if (AnyBalance.isAvailable('accum_mins')) {
+				if (matches = /(\d+).*минут/i.exec(name)) {
+					result.accum_mins = parseInt(matches[1]);
+				}
+			}
+			if (AnyBalance.isAvailable('accum_sms')) {
+				if (matches = /(\d+).*SMS/i.exec(name)) {
+					result.accum_sms = parseInt(matches[1]);
+				}
+			}
+			if (AnyBalance.isAvailable('accum_trafic')) {
+				matches = /GPRS.*?([\d\.\,]+)\s*(Гб|Мб|Кб)/i.exec(name);
+				if (!matches) matches = /([\d\.\,]+)\s*(Гб|Мб|Кб).*GPRS/i.exec(name);
+				if (matches) {
+					var val = parseFloat(matches[1].replace(/^[\s,\.]*|[\s,\.]*$/g, '').replace(',', '.'));
+					switch (matches[2]) {
+					case 'Гб':
+						val *= 1000;
+						break;
+					case 'Мб':
+						break;
+					case 'Кб':
+						val /= 1000;
+						break;
+					}
+					result.accum_trafic = val;
+				}
+			}
+		}
+	}
+	if (AnyBalance.isAvailable('history')) {
+		AnyBalance.trace("Searching for history");
+		html = prefs.testPage || AnyBalance.requestGet(baseurl + "payments/history/last_10");
+		
+		var table = getParam(html, null, null, /Время платежа([\s\S]*?)<\/table>/i);
+		if(table) {
+			sumParam(table, result, 'history', /<a[^>]*name="pos(?:[^>]*>){2}((?:[^>]*>){3})/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
+			// Отрицательные
+			sumParam(table, result, 'history_out', /<a[^>]*name="pos(?:[^>]*>){4}(\s*-\s*[\d,.]+)/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+			// Положительные
+			sumParam(table, result, 'history_income', /<a[^>]*name="pos(?:[^>]*>){4}\s*([\d,.]+)/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+		} else {
+			AnyBalance.trace('Не удалось найти таблицу платежей.');
+		}
+	}	
+	AnyBalance.setResult(result);
+}
+
+function mainOld(){
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://login.rt.ru/';
 	AnyBalance.setDefaultCharset('koi8-r');
 	// Заходим на главную страницу
 	var info = AnyBalance.requestPost(baseurl ,{

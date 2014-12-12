eb1b373bd643f29368a45dc9df7dfdb4278d0b9a
julmatina <julmatina@gmail.com>
2014-12-12 14:13:40 +0000

    Новый провайдер: tatenergosbyt

---
+++ ab-municipal-tatenergosbyt/main.js
@@ -0,0 +1,54 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://lkfl.tatenergosbyt.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'ctl00$PlaceHolderMain$LoginIndividualTextBox') 
+			return prefs.login;
+		else if (name == 'ctl00$PlaceHolderMain$PasswordIndividualTextBox')
+			return prefs.password;
+
+		return value;
+	});
+    
+	html = AnyBalance.requestPost(baseurl, params, addHeaders({Referer: baseurl}));
+    
+	if (!/SignOut/i.test(html)) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+    html = AnyBalance.requestGet(baseurl + '_layouts/15/TES.PersonalAccountIndividualPages/CurrentReceipt.aspx', g_headers); 
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Итого(?:[^>]*>){14}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'period', /Квитанция за([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, '__tariff', /(?:class="evenRow"[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'counter', /class="evenRow"(?:[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'counter_cur', /class="evenRow"(?:[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'zone', /class="evenRow"(?:[^>]*>){8}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

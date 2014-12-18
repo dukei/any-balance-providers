d72c7888f0426af4399481cf7117d68453b48108
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-12-18 15:32:57 +0000

    FonBet (Букмекерская контора) (ab-gambling-fonbet): - Поддержка изменений на сайте.

--- trunk/ab-gambling-fonbet/main.js
+++ trunk/ab-gambling-fonbet/main.js
@@ -25,36 +25,45 @@ function main(){
 	AnyBalance.setDefaultCharset('utf-8');
 
     // Заходим на главную страницу
-        var info = AnyBalance.requestGet(baseurl + 'login.xhtml', g_headers);
-
-	info = AnyBalance.requestPost(baseurl + 'login.xhtml', {
+	var html = AnyBalance.requestGet(baseurl + 'login.xhtml', g_headers);
+	
+	var captchaa;
+	if(AnyBalance.getLevel() >= 7) {
+		AnyBalance.trace('Пытаемся ввести капчу');
+		AnyBalance.setOptions({forceCharset:'base64'});
+		var href = getParam(html, null, null, /captcha[^>]*src="\/([^"]+)/i) || '';
+		var captcha = AnyBalance.requestGet('https://account.fonbet.com/' + href);
+		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaa);
+		AnyBalance.setOptions({forceCharset:'utf-8'});
+	}else{
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + 'login.xhtml', {
 		'login:loginForm':'login:loginForm',
-                'login:loginForm:loginField':prefs.login,
-                'login:loginForm:passwordField':prefs.password,
-                'login:loginForm:submitButton':'Войти',
-                'javax.faces.ViewState':getParam(info, null, null, /<input[^>]+name="javax.faces.ViewState"[^>]*value="([^"]*)/i, null, html_entity_decode)
+		'login:loginForm:loginField':prefs.login,
+		'login:loginForm:passwordField':prefs.password,
+		'login:loginForm:submitButton':'Войти',
+		'login:loginForm:captcha:capInput': captchaa,
+		'javax.faces.ViewState':getParam(html, null, null, /<input[^>]+name="javax.faces.ViewState"[^>]*value="([^"]*)/i, null, html_entity_decode)
 	}, g_headers);
-    
-        //Выход|Logout
-        if(!/(?:&#1042;&#1099;&#1093;&#1086;&#1076;|Logout)\s*<\/a>/i.test(info)){
-           var error = getParam(info, null, null, /<li[^>]+class="messagesError"[^>]*>([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
-           if(error)
-               throw new AnyBalance.Error(error);
-           throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-        }
-    
-        var result = {success: true};
+	
+	//Выход|Logout
+    if(!/(?:&#1042;&#1099;&#1093;&#1086;&#1076;|Logout)\s*<\/a>/i.test(html)){
+		var error = getParam(html, null, null, /<li[^>]+class="messagesError"[^>]*>([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, html_entity_decode);
+		if(error)
+			throw new AnyBalance.Error(error);
+		
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
+	
+	var result = {success: true};
 
-        //Баланс|Balance
-        getParam(info, result, 'balance', /(?:&#1041;&#1072;&#1083;&#1072;&#1085;&#1089;|Balance)[\s\S]*?<span[^>]+class="fontBold"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(info, result, 'pin', /<span[^>]+id="header:accountInfo:pinOutput"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-        getParam(info, result, '__tariff', /<table(?:[\s\S](?!<\/?table))*?<tr[^>]*>((?:[\s\S](?!<\/?table))*?)<\/tr>(?:[\s\S](?!<\/?table))*?header:accountInfo:pinOutput/i, replaceTagsAndSpaces, html_entity_decode);
-
+	//Баланс|Balance
+	getParam(html, result, 'balance', /(?:&#1041;&#1072;&#1083;&#1072;&#1085;&#1089;|Balance)[\s\S]*?<span[^>]+class="fontBold"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'pin', /&#1053;&#1086;&#1084;&#1077;&#1088; &#1089;&#1095;&#1077;&#1090;&#1072;:([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, '__tariff', /&#1053;&#1086;&#1084;&#1077;&#1088; &#1089;&#1095;&#1077;&#1090;&#1072;:([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	
 	AnyBalance.setResult(result);
 };
-
-
-
-
-
-

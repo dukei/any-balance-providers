83f1dd0e5dbb85d0855ccf98d2ff6e6df766faec
stark027 <stark027@gmail.com>
2014-12-19 15:00:55 +0000

    PopUnder (ab-affilate-popunder): - Поддержка изменений на сайте.

--- ab-affilate-popunder/main.js
+++ ab-affilate-popunder/main.js
@@ -3,37 +3,43 @@
 */
 
 var g_headers = {
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Cache-Control':'max-age=0',
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Origin':'http://popunder.ru',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+	'Referer':'http://popunder.ru/main/auth/',
+	'Accept-Language':'ru,en;q=0.8'
 };
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://www.popunder.ru/';
+	var baseurl = 'http://popunder.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
-	if(!prefs.login)
-		throw new AnyBalance.Error('Введите логин!');
-	if(!prefs.password)
-		throw new AnyBalance.Error('Введите пароль!');
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
 		
-	var	html = AnyBalance.requestPost(baseurl + 'main/auth/', {
-		login:prefs.login,
-        pass:prefs.password,
-		x:30,
-		y:22,
-		authsubmit:'Вход'
-    }, addHeaders({Referer: baseurl + 'login'}));
+	var html = AnyBalance.requestGet(baseurl + 'main/auth/', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	
+	html = AnyBalance.requestPost(baseurl + 'main/auth/', [
+		['login', prefs.login],
+		['pass', prefs.password],
+		['authsubmit', 'Вход'],
+    ], addHeaders({Referer: baseurl + 'main/auth/'}));
 	
 	if(!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /class="pcb"[^>]*><h1>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		var error = getParam(html, null, null, /class="pcb"[^>]*><h1>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 		if(error)
 			throw new AnyBalance.Error(error);
+		
+		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
     var result = {success: true};
+	
 	getParam(html, result, 'balance', /Ваш баланс:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'today', /Доход сегодня:[\s\S]*?<td[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	

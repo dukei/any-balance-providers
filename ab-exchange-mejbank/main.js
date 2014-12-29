10bd1b8efcf341bc17d01f7760e57e561f136d7c
julmatina <julmatina@gmail.com>
2014-12-29 09:11:14 +0000

    Курсы валют Межбанка Украины (ab-exchange-mejbank): Добавлены "индикативный курс НБУ", "курсы от банков", "рыночные курсы", "валютный аукцион"

--- ab-exchange-mejbank/main.js
+++ ab-exchange-mejbank/main.js
@@ -13,22 +13,73 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://minfin.com.ua/currency/mb/';
+	var baseurl = 'http://minfin.com.ua/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
-	var html = AnyBalance.requestGet(baseurl, g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'currency/mb/', g_headers);
 	
-	var result = {success: true};
-	
-	getParam(html, result, 'purchase_usd', /USD(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'sell_usd', /USD(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
-	
-	
-	getParam(html, result, 'purchase_eur', /EUR(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){8}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'sell_eur', /EUR(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){8}/i, replaceTagsAndSpaces, parseBalance);
-	
-	getParam(html, result, 'purchase_rub', /RUB(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){12}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'sell_rub', /RUB(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){12}/i, replaceTagsAndSpaces, parseBalance);
-	
+	if(!html || AnyBalance.getLastStatusCode() > 400)
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');    
+    
+    var course = prefs.course || '1';
+    
+	var result = {success: true};    
+    
+    if (course == '1') {
+        var table = getParam(html, null, null, /(Курсы от банков[\s\S]*?)<\/table>/i);        
+    
+        getParam(table, result, '_tariff', /Курсы от банков/i, null, null);
+        getParam(table, result, 'purchase_usd', /USD(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_usd', /USD(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
+        
+        getParam(table, result, 'purchase_eur', /EUR(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){6}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_eur', /EUR(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){6}/i, replaceTagsAndSpaces, parseBalance);
+         
+        getParam(table, result, 'purchase_rub', /RUB(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){8}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_rub', /RUB(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){8}/i, replaceTagsAndSpaces, parseBalance);
+        
+    } else if (course == '2') {
+    
+        var table = getParam(html, null, null, /(Рыночные курсы[\s\S]*?)<\/table>/i);
+        
+        getParam(table, result, '_tariff', /Рыночные курсы/i, null, null);    
+        getParam(table, result, 'purchase_usd', /USD(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){2}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_usd', /USD(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){2}/i, replaceTagsAndSpaces, parseBalance);
+        
+        getParam(table, result, 'purchase_eur', /EUR(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_eur', /EUR(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
+        
+        getParam(table, result, 'purchase_rub', /RUB(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){6}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_rub', /RUB(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){6}/i, replaceTagsAndSpaces, parseBalance);
+        
+    } else if (course == '3')  {
+    
+        getParam(html, result, '_tariff', /(Индикативные курсы НБУ на[\s\S]*?):<\//i, replaceTagsAndSpaces, html_entity_decode);   
+        getParam(html, result, 'course_date', /Индикативные курсы НБУ на([\s\S]*?):<\//i, replaceTagsAndSpaces, parseDate);
+        getParam(html, result, 'course_USD', /Индикативные курсы НБУ на(?:[^>]*>){2}доллар&nbsp;([^;]+)/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'course_EUR', /Индикативные курсы НБУ на(?:[^>]*>){2}[\s\S]*?евро&nbsp;([^;]+)/i, replaceTagsAndSpaces, parseBalance);
+        getParam(html, result, 'course_RUB', /Индикативные курсы НБУ на(?:[^>]*>){2}[\s\S]*?рубль&nbsp;([\s\S]*?)\.\s*?</i, replaceTagsAndSpaces, parseBalance);  
+        
+    } else {
+    
+        html = AnyBalance.requestGet(baseurl + 'currency/auction/', g_headers);
+        
+        if(!html || AnyBalance.getLastStatusCode() > 400)
+            throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');  
+            
+        var table = getParam(html, null, null, /(<h1>Валютный аукцион[\s\S]*?)<\/table>/i);
+    
+        getParam(table, result, '_tariff', /Валютный аукцион/i, null, null);
+        getParam(table, result, 'purchase_usd', /USD(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_usd', /USD(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){4}/i, replaceTagsAndSpaces, parseBalance);
+        
+        getParam(table, result, 'purchase_eur', /EUR(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){6}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_eur', /EUR(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){6}/i, replaceTagsAndSpaces, parseBalance);
+         
+        getParam(table, result, 'purchase_rub', /RUB(?:[\s\S]*?<tr[^>]*>){1}([^>]*>){8}/i, replaceTagsAndSpaces, parseBalance);
+        getParam(table, result, 'sell_rub', /RUB(?:[\s\S]*?<tr[^>]*>){2}([^>]*>){8}/i, replaceTagsAndSpaces, parseBalance);
+        
+    }
+    
 	AnyBalance.setResult(result);
 }

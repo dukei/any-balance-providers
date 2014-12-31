f3a594f2164c896971e9895b4ace58aabc4a8de7
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-12-31 09:37:03 +0000

    Трансаэро Привилегия (ab-bonus-transaero): - Преодоление защиты сайта от автоматического входа

--- trunk/ab-bonus-transaero/main.js
+++ trunk/ab-bonus-transaero/main.js
@@ -2,19 +2,31 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Language': 'en-US;q=0.8,en;q=0.6',
+	'Cache-Control': 'max-age=0',
+	'Connection':'keep-alive',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
+};
+
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 	
     var baseurl = "http://m.transaero.ru/";
 
-    var html = AnyBalance.requestGet(baseurl);
+    var incapsule = Incapsule(baseurl + 'http://m.transaero.ru/');
+    var html = AnyBalance.requestGet(baseurl, g_headers);
+    if(incapsule.isIncapsulated(html))
+        html = incapsule.executeScript(html);
 	
     var action = getParam(html, null, null, /<a href="\/([^"]+)(?:[^>]*>){3}\s*Привилегия/i, null, html_entity_decode);
     if(!action)
         throw new AnyBalance.Error('Не удаётся найти форму входа. Сайт изменен или проблемы на сайте.');
 	
-	html = AnyBalance.requestGet(baseurl + action);
+	html = AnyBalance.requestGet(baseurl + action, g_headers);
 
     var action = getParam(html, null, null, /<form[^>]*action="\/([^"]+)/i, null, html_entity_decode);
     if(!action)
@@ -23,8 +35,8 @@ function main(){
     html = AnyBalance.requestPost(baseurl + action, {
         lastName:prefs.surname,
         cardno:prefs.login,
-        pin:prefs.password,
-    });
+        pin:prefs.password
+    }, g_headers);
 
 	if (!/"Выход"/i.test(html)) {
 		var error = sumParam(html, null, null, /<span style=[^>]*color:#990000[^>]*>([\s\S]*?)<\//ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);

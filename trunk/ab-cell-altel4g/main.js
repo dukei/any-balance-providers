0802170d3accb55f51d05587a9c04eeee21b03fb
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2014-12-31 11:21:46 +0000

    ALTEL 4G (Казахстан) (ab-cell-altel4g): - Добавлены сроки действия минут, смс и трафика

--- trunk/ab-cell-altel4g/main.js
+++ trunk/ab-cell-altel4g/main.js
@@ -38,20 +38,52 @@ function main(){
     getParam(html, result, 'fio', /"account-title"[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'phone', /(?:Ваш номер|Сіздің нөміріңіз):([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, '__tariff', /"plan-title"[^>]*>([^<]+)[^>]*>[^>]*plan-subtitle/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'balance', /"split-title"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'till', /бонусный счет[^>]*>\s*до([^<]+)/i, replaceTagsAndSpaces, parseDate);
-	// трафик
-	getParam(html, result, 'traffic_used', /counter"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseTraffic);
-    getParam(html, result, 'traffic_left', /([\d.,\s]+\s*(?:[^>]*>){2}\s*Мб)/i, replaceTagsAndSpaces, parseTraffic);
-	// Минуты внутри сети
-	getParam(html, result, 'min_left', /(\d+:\d+)\s*(?:[^>]*>){2}\s*Мин(?:[^>]*>){3}\s*Внутри сети/i, replaceTagsAndSpaces, parseMinutes);
-	// Минуты на GSM
-	getParam(html, result, 'min_left_gsm', /(\d+:\d+)\s*(?:[^>]*>){2}\s*Мин(?:[^>]*>){3}\s*на GSM/i, replaceTagsAndSpaces, parseMinutes);
-	// Минуты на город
-	getParam(html, result, 'min_left_city', /(\d+:\d+)\s*(?:[^>]*>){2}\s*Мин(?:[^>]*>){3}\s*на город/i, replaceTagsAndSpaces, parseMinutes);	
-	
-	// Смс внутри сети
-	getParam(html, result, 'sms_left', /(\d+)\s*(?:[^>]*>){2}\s*СМС(?:[^>]*>){3}\s*Внутри сети/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balance', /"split-title"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'till', /бонусный счет[^>]*>\s*до([^<]+)/i, replaceTagsAndSpaces, parseDate);
+
+	var counters = sumParam(html, null, null, /<div[^>]+class="[^"]*\bcounter\b[^>]*>([\s\S]*?)<\/div>/ig);
+	AnyBalance.trace('Found counters: ' + counters.length);
+	for(var i=0; i<counters.length; ++i){
+		var counter = counters[i];
+		var units = getParam(counter, null, null, /<span[^>]+class="icon-[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+		var note = getParam(counter, null, null, /<div[^>]+class="counter-note"[^>]*>([\s\S]*)/i, null, html_entity_decode);
+		var counter_name = null;
+		if(/Мин/i.test(units)){
+			if(/Внутри сети|Желі ішінде/i.test(note)){
+				counter_name = 'min_left'; // Минуты внутри сети
+			}else if(/GSM/i.test(note)){
+				counter_name = 'min_left_gsm'; // Минуты на GSM
+			}else if(/на город/i.test(note)){
+				counter_name = 'min_left_city'; // Минуты на город
+			}
+			if(counter_name)
+				getParam(counter, result, counter_name, /(\d+:\d+)/i, replaceTagsAndSpaces, parseMinutes);
+		}else if(/[МГКMGK][бb]/i.test(units)){
+			// трафик
+			counter_name = 'traffic_left';
+			getParam(counter, result, counter_name, /([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseTraffic);
+		}else if(/СМС|SMS/i.test(units)){
+			// Смс внутри сети
+			counter_name = 'sms_left';
+			getParam(counter, result, counter_name, /([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+		}
+		if(!counter_name){
+			AnyBalance.trace('Неизвестная опция: ' + counter);
+		}else{
+			getParam(note, result, counter_name + '_till', /<br[^>]*>([^>]*)/i, replaceTagsAndSpaces, parseDateLocal);
+		}
+	}
     
     AnyBalance.setResult(result);
+}
+
+function parseDateLocal(str){
+	if(/До конца дня|Күннің соңына дейін/i.test(str)){
+		var dt = new Date();
+		var today = new Date(dt.getYear(), dt.getMonth(), dt.getDate(), 23, 59, 59);
+		AnyBalance.trace('Parsed date ' + today + ' from ' + str);
+		return dt.getTime();
+	}else{
+		return parseDate(str);
+	}
 }

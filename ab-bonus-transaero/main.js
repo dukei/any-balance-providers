4acf98a6db1bbc466dafc7b739c3faeb1045bef0
dmitry.kochin <dmitry.kochin@gmail.com>
2015-01-03 18:50:00 +0000

    Трансаэро Привилегия (ab-bonus-transaero): - Улучшен обход защиты сайта - Добавлен вывод фио

--- ab-bonus-transaero/main.js
+++ ab-bonus-transaero/main.js
@@ -15,6 +15,55 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 	
+    var baseurl = "http://transaero.ru/";
+
+    var incapsule = Incapsule(baseurl);
+    var html = AnyBalance.requestGet(baseurl + 'ru/privilege/argo-login', g_headers);
+    if(incapsule.isIncapsulated(html))
+        html = incapsule.executeScript(html);
+	
+	var form = getParam(html, null, null, /<form(?:[\s\S](?!<\/form>))*<input[^>]+name="FORM_CARDNO"[\s\S]*?<\/form>/i);
+	if(!form){
+			AnyBalance.trace(html);
+			throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+	}
+
+    var action = getParam(form, null, null, /<form[^>]+action="\/([^"]*)/i, null, html_entity_decode);
+    if(!action){
+    	AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удаётся найти ссылку на вход. Сайт изменен или проблемы на сайте.');
+    }
+	
+	html = AnyBalance.requestPost(baseurl + action, {
+		FORM_LASTNAME: prefs.surname,
+		FORM_CARDNO: prefs.login,
+		FORM_PINCODE: prefs.password,
+		ArgoPortletFormSubmit: 'Войти',
+	}, addHeaders({Referer: baseurl + 'ru/privilege/argo-login'}));
+
+	if (!/Уважаемый[^<]*!/i.test(html)) {
+		var error = getParam(html, null, null, /<h1[^>]*>\s*Просмотр состояния счёта\s*<\/h1>\s*<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Регистрация не верна/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+    var result = {success: true};
+
+    getParam(html, result, 'balance', />\s*Вы набрали\s*<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, '__tariff', /на ваш персональный счет\s*([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'cardnum', /на ваш персональный счет\s*([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'fio', /Уважаемый([^<]*)!/i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}
+
+function mainMobile(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+	
     var baseurl = "http://m.transaero.ru/";
 
     var incapsule = Incapsule(baseurl);

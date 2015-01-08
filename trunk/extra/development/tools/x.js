ab4d8f5890c0e7e39d0d90ca70ea783667cc571a
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-01-08 16:58:51 +0000

    history: автоматическое создание хистори файла, если его ещё нет

--- trunk/extra/development/tools/x.js
+++ trunk/extra/development/tools/x.js
@@ -8,6 +8,7 @@ var g_prov_major_version = '';
 var g_prov_text_id = '';
 var g_prov_name = '';
 var g_history_file = '';
+var g_new_history_file_name = ''; //Имя файла истории, если его раньше не было в манифесте
 var g_prefs_file = '';
 var g_defaultHistoryLine = '- Поддержка изменений на сайте.';
 
@@ -22,71 +23,86 @@ var vbNo = 7;
 
 var title = "Add history line";
 var prompt = "Enter a line to history:";
+var cancel = "User asked to terminate";
 
-// Create the WshShell object (needed for using Popup).
-var WshShell = WScript.CreateObject("WScript.Shell");
-// Open the input dialog box using a function in the .wsf file.
-//var result = WSHInputBox(prompt, title, "- ");
-var result = returnValue;
-// Test whether the Cancel button was clicked.
-if (result) {
-	var objStream = new ActiveXObject("ADODB.Stream");
-	objStream.CharSet = "utf-8";
-	
-	var manifest = openManifest(objStream);
-	getManifestData(manifest);
-	
-	// Проверим не заменены ли преференсы в файле
-	var mainJs = readFileToString('main.js');
-	if(g_prefs_file) {
-		var msg = ' Check you main.js file for stupid errors!';
+try{
+	// Create the WshShell object (needed for using Popup).
+	var WshShell = WScript.CreateObject("WScript.Shell");
+	// Open the input dialog box using a function in the .wsf file.
+	//var result = WSHInputBox(prompt, title, "- ");
+	var result = returnValue;
+	// Test whether the Cancel button was clicked.
+	if (result) {
+		var objStream = new ActiveXObject("ADODB.Stream");
+		objStream.CharSet = "utf-8";
 		
-		var prefsName = searchRegExpSafe(/(?:var\s+)?([^\s]+)\s*=\s*AnyBalance\.getPreferences\s*\(\)/i, mainJs);
-		//var prefsName = /(?:var\s+)?([^\s]+)\s*=\s*AnyBalance\.getPreferences\s*\(\)/i.exec(mainJs)[1];
-		if(!prefsName) {
-			throw new Error('We don`t found AnyBalance.getPreferences()!' + msg);
-		}
+		var manifest = openManifest(objStream);
+		getManifestData(manifest);
 		
-		// Нельзя хардкодить преференсы!
-		var reg = new RegExp('(?:var\\s+)?' + prefsName + '\\s*=\\s*([^,;]+)', 'ig');
-		var r_result;
-		while((r_result = reg.exec(mainJs)) !== null) {
-			if(!/AnyBalance\.getPreferences\s*\(\)/i.test(r_result[1])) {
-				throw new Error('You have overrided your preferences!' + msg);
+		// Проверим не заменены ли преференсы в файле
+		var mainJs = readFileToString('main.js');
+		if(g_prefs_file) {
+			var msg = ' Check you main.js file for stupid errors!';
+			
+			var prefsName = searchRegExpSafe(/(?:var\s+)?([^\s]+)\s*=\s*AnyBalance\.getPreferences\s*\(\)/i, mainJs);
+			//var prefsName = /(?:var\s+)?([^\s]+)\s*=\s*AnyBalance\.getPreferences\s*\(\)/i.exec(mainJs)[1];
+			if(!prefsName) {
+				throw new Error('We don`t found AnyBalance.getPreferences()!' + msg);
+			}
+			
+			// Нельзя хардкодить преференсы!
+			var reg = new RegExp('(?:var\\s+)?' + prefsName + '\\s*=\\s*([^,;]+)', 'ig');
+			var r_result;
+			while((r_result = reg.exec(mainJs)) !== null) {
+				if(!/AnyBalance\.getPreferences\s*\(\)/i.test(r_result[1])) {
+					throw new Error('You have overrided your preferences!' + msg);
+				}
 			}
 		}
-	}
-	
-	// var intDoIt = WshShell.Popup('Do you want to use new library.js?', 0, "Result", vbYesNo + vbInformation + stayOnTop);
-	// if(intDoIt == vbYes) {
 		
-	// }
-	
-	// Запишем манифест
-	writeManifest(objStream, manifest, WshShell);
-	
-	// История 
-	var originalHistory = readFileToString(g_history_file);
-	var dt = new Date();
-	
-	var major_version_str = (g_prov_major_version ? 'major_version="' + g_prov_major_version + '" ' : '');
-	
-	originalHistory = originalHistory.replace(/<history>/, '<history>\n\t<change ' + major_version_str + 'version="' + g_prov_version + '" date="' + dt.getYear() + '-' + addZeros(dt.getMonth()+1) + '-' + addZeros(dt.getDate()) + '">\n\t' + result.replace(/\n/g, '\n\t') + '\n\t</change>');
-	originalHistory = originalHistory.replace(/^\s*|\s*$/g, '');
-	
-	objStream.Open();
-	objStream.WriteText(originalHistory);
-	objStream.SaveToFile (g_history_file, 2);
-	objStream.Close();
-	
-	var intDoIt = WshShell.Popup('Provider: ' + g_prov_text_id + ' v.' + g_prov_major_version + '.' + g_prov_version + ' edited.\nAdded new history line: ' + result + '\n\nDo you want to commit via SVN?', 0, "Result", vbYesNo + vbInformation + stayOnTop);
-	
-	if(intDoIt == vbYes) {
-		// Want to commit
-		commit(WshShell, result);
+		// var intDoIt = WshShell.Popup('Do you want to use new library.js?', 0, "Result", vbYesNo + vbInformation + stayOnTop);
+		// if(intDoIt == vbYes) {
+			
+		// }
+		
+		// Запишем манифест
+		writeManifest(objStream, manifest, WshShell);
+		
+		// История 
+		var originalHistory = '<?xml version="1.0" encoding="utf-8"?>\n\
+<history>\n\
+</history>';
+
+		if(g_history_file)
+			originalHistory = readFileToString(g_history_file);
+			
+		var dt = new Date();
+		
+		var major_version_str = (g_prov_major_version ? 'major_version="' + g_prov_major_version + '" ' : '');
+		
+		originalHistory = originalHistory.replace(/<history>/, '<history>\n\t<change ' + major_version_str + 'version="' + g_prov_version + '" date="' + dt.getYear() + '-' + addZeros(dt.getMonth()+1) + '-' + addZeros(dt.getDate()) + '">\n\t' + result.replace(/\n/g, '\n\t') + '\n\t</change>');
+		originalHistory = originalHistory.replace(/^\s*|\s*$/g, '');
+		
+		objStream.Open();
+		objStream.WriteText(originalHistory);
+		if(g_history_file)
+			objStream.SaveToFile (g_history_file, 2);
+		else
+			objStream.SaveToFile (g_history_file || g_new_history_file_name, 1);
+		objStream.Close();
+		
+		var intDoIt = WshShell.Popup('Provider: ' + g_prov_text_id + ' v.' + g_prov_major_version + '.' + g_prov_version + ' edited.\nAdded new history line: ' + result + '\n\nDo you want to commit via SVN?', 0, "Result", vbYesNo + vbInformation + stayOnTop);
+		
+		if(intDoIt == vbYes) {
+			// Want to commit
+			commit(WshShell, result);
+		}
+	} else { // Cancel button was clicked.
+		//var intDoIt = WshShell.Popup("Sorry, no input", 0, "Result", vbOKOnly + vbInformation + stayOnTop);
 	}
-} else { // Cancel button was clicked.
-	//var intDoIt = WshShell.Popup("Sorry, no input", 0, "Result", vbOKOnly + vbInformation + stayOnTop);
+}catch(e){
+	if(e.message != cancel)
+		throw e;		
 }
 
 function commit(WshShell, mesg) {
@@ -113,8 +129,11 @@ function getManifestData(manifest) {
 	if(!g_prov_name)
 		throw new Error('No name specified in the manifest!');		
 	g_history_file = searchRegExpSafe(/<history>([^<]+)<\/history>/i, manifest);
-	if(!g_history_file)
-		throw new Error('No history file specified in the manifest!');
+	if(!g_history_file){
+		g_new_history_file_name = VBInputBox("No history file specified in the manifest! Would you like to create it? Enter the name of history file.", "history.xml")
+		if(!g_new_history_file_name)
+			throw new Error(cancel);
+	}
 		
 	g_prefs_file = searchRegExpSafe(/<preferences>([^<]+)<\/preferences>/i, manifest);	
 }
@@ -135,12 +154,15 @@ function writeManifest(objStream, manifest, WshShell) {
 			// already has some flags
 			if(apiFlags) {
 				manifest = manifest.replace(/flags\s*=\s*"([^"]+)"/, 'flags="no_browser|$1"');
-				
 			} else {
 				manifest = manifest.replace(/<provider>/, '<provider>\n\t<api flags="no_browser"/>');
 			}
 		}
 	}
+	
+	if(!g_history_file)
+		manifest = manifest.replace(/(\s*)<\/files>/i, '$1\t<history>' + g_new_history_file_name + '</history>$1</files>');
+	
 	objStream.Open();
 	objStream.WriteText(manifest.replace(/\sversion="\d+"/i, ' version="'+g_prov_version+'"'));
 	objStream.SaveToFile ("anybalance-manifest.xml", 2);

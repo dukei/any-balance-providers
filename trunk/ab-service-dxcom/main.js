fe52eeb3194ce6120c4cadf97b8024b9389fe7f6
sergey.bashmanov@gmail.com <sergey.bashmanov@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-01-09 16:14:39 +0000

    ab-service-dxcom - Now provider use main version of site instead mobile version.

--- trunk/ab-service-dxcom/main.js
+++ trunk/ab-service-dxcom/main.js
@@ -13,54 +13,38 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://m.dx.com/';
+	var baseurl = 'https://my.dx.com/';
+	var loginurl = 'https://passport.dx.com/';
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Enter login!');
 	checkEmpty(prefs.password, 'Enter password!');
 
-	var html = AnyBalance.requestGet(baseurl + 'passport', addHeaders({Referer: baseurl}));
+	AnyBalance.setExceptions(false);
+	html = AnyBalance.requestPost(loginurl + '?redirect=http%3A%2F%2Fwww.dx.com', {
+		'AccountName':prefs.login,
+		'Password':prefs.password,
+	}, addHeaders({Referer: loginurl + '?redirect=http%3A%2F%2Fwww.dx.com'}));
+	AnyBalance.setExceptions(true);
 
-	https://m.dx.com/passport/?r=https%3A%2F%2Fm.dx.com%2Fmy
-	
-	html = AnyBalance.requestPost(baseurl + 'passport/?r=https%3A%2F%2Fm.dx.com%2Fmy', {
-		'ReturnUrl':'https://m.dx.com/my',
-		'username':prefs.login,
-		'password':prefs.password,
-	}, addHeaders({Referer: baseurl + 'accounts/login.dx'}));
+	html = AnyBalance.requestGet(baseurl);
 
-	if (!/logout|выход/i.test(html)) {
-		var error = sumParam(html, null, null, /<div>\s*<\s*span[^>]*color:Red[^>]*>([^<]+)/ig, replaceTagsAndSpaces, html_entity_decode, aggregate_join);
-		if (error && /appears to be invalid/i.test(error))
-			throw new AnyBalance.Error(error, null, true);
-		if (error)
-			throw new AnyBalance.Error(error);
+	if (!/My Orders/i.test(html)) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Can`t login, is the site changed?');
 	}
-	
-	html = AnyBalance.requestGet(baseurl + 'my/OrderList', addHeaders({Referer: baseurl + 'passport'}));
 
 	var result = {success: true};
-	
-	// getParam(html, result, 'points', /DX Points:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-	
-	var firstTr = getParam(html, null, null, /<a href="\/my\/OrderDetail(?:[^>]+>){8,30}\s*<\/a>/i);
-	if(firstTr) {
-		getParam(firstTr, result, 'order_num', /OrderDetail\/(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
-		getParam(firstTr, result, 'order_date', /<em>([^<]+)<\/em>/i, replaceTagsAndSpaces, parseDate);
-		getParam(firstTr, result, 'order_status', /<span>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
-		
-		if(isAvailable('order_sum')) {
-			var href = getParam(firstTr, null, null, /href="\/([^"]+)/i);
-			if(href) {
-				html = AnyBalance.requestGet(baseurl + href, addHeaders({Referer: baseurl + 'passport'}));
-				getParam(html, result, 'order_sum', /Grand Total(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-			}
-		}
-	} else {
-		AnyBalance.trace('Can`t find any order.');
-	}
-	
+
+	getParam(html, result, 'points', /DX Points:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'order_num', /<td class="number">[\s\S]*?<a.*?>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'order_date', /<td class="number">[\s\S]*?<td>[\s\S]*?(\d\d\/\d\d\/\d\d\d\d)/i, [replaceTagsAndSpaces, /(\d\d)\/(\d\d)/, "$2/$1"], parseDate);
+	getParam(html, result, 'order_sum', /<td class="total">[\s\S]*?([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'order_status', /<td class="status">[\s\S]*?([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+
+	AnyBalance.setExceptions(false);
+	html = AnyBalance.requestGet('https://passport.dx.com/accounts/summaries.dx/logout.account', addHeaders({Referer: baseurl}));
+	AnyBalance.setExceptions(true);
+
 	AnyBalance.setResult(result);
 }

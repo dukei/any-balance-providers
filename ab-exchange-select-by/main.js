f92794644a0b0a6482df66e92a2133511ea74af6
dmitry.kochin <dmitry.kochin@gmail.com>
2015-01-11 22:38:04 +0000

    Курсы валют Банков Беларуси (ab-exchange-select-by): - Исправлены ошибки - Поддержка нескольких городов - Ручной ввод имени банка при необходимости

--- ab-exchange-select-by/main.js
+++ ab-exchange-select-by/main.js
@@ -7,106 +7,75 @@
 Личный кабинет: http://select.by/kurs/
 */
 
-function getKurs($row, result, counter, idx, bank){
-	if(AnyBalance.isAvailable(counter)){
-		var text = $row.find(bank ? 'td:nth-child('+idx+')' : 'th:nth-child('+idx+')>b').text();
-		if(text){
-			var price = parseFloat(text);
-			result[counter] = price;
-			return price;
-		}
-	}
-}
-
-function getNBKurs($table, result, counter, curr){
-	if(AnyBalance.isAvailable(counter)){
-		var text = $table.find('tr:contains("'+curr+'")>td:nth-child(3)').text();
-		if(text){
-			var price = parseFloat(text);
-			result[counter] = price;
-			return price;
-		}
-	}
-}
-
-function getText($row, result, counter, idx, bank){
-	if(AnyBalance.isAvailable(counter)){
-		var text = $row.find(bank ? 'td:nth-child('+idx+')' : 'th:nth-child('+idx+')>b').text();
-		result[counter] = text;
-		return text;
-	}
-}
-
-
 function main(){
 	AnyBalance.trace('Connecting to select.by...');
 	var prefs = AnyBalance.getPreferences();
     var city = prefs.city || '';
 	var info = AnyBalance.requestGet('http://select.by/kurs/' + city + '/');
 	var bank = prefs.bank, table;
+	if(bank == '!@other'){
+	    checkEmpty(prefs.other, 'Введите часть названия другого банка');
+		bank = prefs.other;
+	}
 	
 	var result = {success: true};
 
         if(bank != 'nbrb'){
             table = getParam(info, null, null, /(<table[^>]*class="tablesorter"[^>]*>[\s\S]*?<\/table>)/i);
-		if(!table){
-		    AnyBalance.trace(info);
-			throw new AnyBalance.Error('Не удаётся найти таблицу курсов!');
-		}
-	    
-		var $table = $(table);
-        var $row;
-	    
-		if(bank){
-			bank = bank.toUpperCase();
-			$row = $table.find('tr>td>a').filter(
-				function(){
-					return $(this).text().toUpperCase().indexOf(bank) >= 0;
-				}
-			).parent().parent();
 
-			}else{
-				$row = $table.find('tr>th:contains("Лучшие курсы")').parent();
-			}
-	                   
-            
-			if($row.size() == 0){
+			if(!table){
 			    AnyBalance.trace(info);
-				throw new AnyBalance.Error('Не удаётся найти строку ' + prefs.bank);
+				throw new AnyBalance.Error('Не удаётся найти таблицу курсов!');
 			}
 	        
-            getKurs($row, result, 'usdpok', 3, bank);
-            getKurs($row, result, 'usdprod', 4, bank);
-            getKurs($row, result, 'eurpok', 5, bank);
-            getKurs($row, result, 'eurprod', 6, bank);
-            getKurs($row, result, 'rurpok', 7, bank);
-            getKurs($row, result, 'rurprod', 8, bank);
-            getKurs($row, result, 'uepok', 9, bank);
-            getKurs($row, result, 'ueprod', 10, bank);
-            getText($row, result, 'tel', 11, bank);
-            
-			if(AnyBalance.isAvailable('bank'))
-				result.bank = prefs.bank || 'Лучшие курсы';
-		    
-			result.__tariff = prefs.bank || 'Лучшие курсы';
+	        var row;
+			if(!bank)
+			    bank = 'Лучшие курсы';
+
+			var bankRe = replaceAll(bank, [/([(\\{}).*?])/g, '\\$1', /\s+/g, '(?:(?:<[^>]*>|\\s)(?!</td>))+']); //Превращаем название банка в регулярку, не учитываем тэги и пробелы
+	        var regExp = new RegExp('<tr[^>]*>(?:[\\s\\S](?!</tr>))*?(' + bankRe + '[\\s\\S]*?</tr>)', 'i');
+	        
+	        row = getParam(table, null, null, regExp);
+                
+			if(!row){
+			    AnyBalance.trace(info);
+				throw new AnyBalance.Error('Не удаётся найти строку ' + bank);
+			}
+
+			//Находим полную ячейку, содержащую название банка
+	        result.__tariff = getParam(table, null, null, new RegExp('<t[dh][^>]*>(?:[\\s\\S](?!</td>))*?' + bankRe + '[\\s\\S]*?</t[dh]>', 'i'), replaceTagsAndSpaces, html_entity_decode);
+	        getParam(result.__tariff, result, 'bank');
+	        
+	        var replaceDotsToo = [replaceTagsAndSpaces, /(\D)[\.,]/g, '$1']; //Убираем лишние точки без цифр, чтобы не мешались
+            getParam(row, result, 'usdpok', /(?:[\s\S]*?<t[dh][^>]*>){1}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(row, result, 'usdprod', /(?:[\s\S]*?<t[dh][^>]*>){2}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(row, result, 'eurpok', /(?:[\s\S]*?<t[dh][^>]*>){3}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(row, result, 'eurprod', /(?:[\s\S]*?<t[dh][^>]*>){4}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(row, result, 'rurpok', /(?:[\s\S]*?<t[dh][^>]*>){5}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(row, result, 'rurprod', /(?:[\s\S]*?<t[dh][^>]*>){6}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+//            getParam(row, result, 'uepok', /(?:[\s\S]*?<t[dh][^>]*>){7}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+//            getParam(row, result, 'ueprod', /(?:[\s\S]*?<t[dh][^>]*>){8}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(row, result, 'tel', /(?:[\s\S]*?<t[dh][^>]*>){7}([\s\S]*?)<\/t[dh]>/i, replaceTagsAndSpaces, html_entity_decode);
         }else{
             var table = getParam(info, null, null, /Курсы валют НБ РБ[\s\S]*?(<table[^>]*>[\s\S]*?<\/table>)/i);
 			if(!table){
 		        AnyBalance.trace(info);
 				throw new AnyBalance.Error('Не удаётся найти таблицу НБ РБ!');
 			}
-			var $table = $(table);
-                    var date = $table.find('tr:nth-child(1)>td:nth-child(3)').text();
+
+			var date = getParam(table, null, null, /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 			result.__tariff = 'НБ РБ на ' + date;
+
 			if(AnyBalance.isAvailable('bank'))
 				result.bank = result.__tariff;
 
-            getNBKurs($table, result, 'usdpok', 'USD');
-            getNBKurs($table, result, 'usdprod', 'USD');
-            getNBKurs($table, result, 'eurpok', 'EUR');
-            getNBKurs($table, result, 'eurprod', 'EUR');
-            getNBKurs($table, result, 'rurpok', 'RUB');
-            getNBKurs($table, result, 'rurprod', 'RUB');
+            getParam(table, result, 'usdpok', /USD<(?:[\s\S]*?<t[dh][^>]*>){2}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(table, result, 'usdprod', /USD<(?:[\s\S]*?<t[dh][^>]*>){2}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(table, result, 'eurpok', /EUR<(?:[\s\S]*?<t[dh][^>]*>){2}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(table, result, 'eurprod', /EUR<(?:[\s\S]*?<t[dh][^>]*>){2}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(table, result, 'rurpok', /RUB<(?:[\s\S]*?<t[dh][^>]*>){2}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+            getParam(table, result, 'rurprod', /RUB<(?:[\s\S]*?<t[dh][^>]*>){2}([\s\S]*?)<\/t[dh]>/i, replaceDotsToo, parseBalance);
+
             if(AnyBalance.isAvailable('tel'))
                 result.tel = date; //В случае нац. банка вместо телефона кладем дату курса
         }

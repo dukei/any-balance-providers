9007b28a8577f1ce041bb72a994b334c71189f7b
stark027 <stark027@gmail.com>
2015-01-14 09:24:49 +0000

    П-Т-К (ab-internet-ptk): - Поддержка изменений на сайте.

--- ab-internet-ptk/main.js
+++ ab-internet-ptk/main.js
@@ -13,38 +13,48 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
     var baseurl = 'https://stat.p-t-k.net/';
     AnyBalance.setDefaultCharset('utf-8'); 
 
-	var html = AnyBalance.requestGet(baseurl + 'StatSrv/start.php');
-	var session = getParam(html, null, null, /<form[^>]*action="login\.php\?PHPSESSID=([^"]*)/i);
-
+	var html = AnyBalance.requestGet(baseurl + 'lk/start.php');
+	var session = getParam(html, null, null, /PHPSESSID=([^"]+)/i);
+	if(!session) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти код сессии, сайт изменен?');
+	}
 	var captchaa;
 	if(AnyBalance.getLevel() >= 7){
 		AnyBalance.trace('Пытаемся ввести капчу');
-		var captcha = AnyBalance.requestGet(baseurl+ 'StatSrv/capt/?PHPSESSID='+session);
+		var captcha = AnyBalance.requestGet(baseurl+ 'lk/capt/?PHPSESSID='+session);
 		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
 		AnyBalance.trace('Капча получена: ' + captchaa);
 	}else{
 		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
 	}
     
-	html = AnyBalance.requestPost(baseurl + 'StatSrv/login.php?PHPSESSID='+session, {
+	html = AnyBalance.requestPost(baseurl + 'lk/login.php?PHPSESSID='+session, {
         PHPSESSID:session,
         uname:prefs.login,
         passwd:prefs.password,
         keystring:captchaa,
 		submit:'вход',
-    }, addHeaders({Referer: baseurl + 'StatSrv/start.php'})); 
+    }, addHeaders({Referer: baseurl + 'lk/start.php'})); 
 	
-    if(!/exit\.php/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-
+	if (!/exit\.php/i.test(html)) {
+		var error = getParam(html, null, null, /class="errcontent"[^>]*>([\s\S]*?)<\/p/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверное имя пользователя/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
     var result = {success: true};
+	
     getParam(html, result, 'fio', /<table(?:[\s\S]*?<td[^>]*align="left"[^>]*bgcolor="#eeeeee"[^>]*>){1}\s*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'acc_num', /<table(?:[\s\S]*?<td[^>]*align="left"[^>]*bgcolor="#eeeeee"[^>]*>){2}\s*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'balance', /<table(?:[\s\S]*?<td[^>]*align="left"[^>]*bgcolor="#eeeeee"[^>]*>){3}\s*<[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);

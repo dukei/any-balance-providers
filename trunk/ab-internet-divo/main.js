fb739b90226d547abaf3c3819df3a7f1f5082892
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-01-21 08:45:03 +0000

    ДИВО (Сергиев-Посад) (ab-internet-divo): - Поддержка изменений на сайте.

--- trunk/ab-internet-divo/main.js
+++ trunk/ab-internet-divo/main.js
@@ -1,18 +1,13 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плане для интернет провайдера Диво
-
-Operator site: http://divo.ru
-Личный кабинет: https://lc.divo.ru
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 function getViewState(html){
@@ -25,7 +20,7 @@ function getEventValidation(html){
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = "https://lc.divo.ru/";
+    var baseurl = 'http://lc.divo.ru/';
 
     AnyBalance.setDefaultCharset('utf-8'); 
 

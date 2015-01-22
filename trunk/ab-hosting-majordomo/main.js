51e408d04f6066d72b49f0539b35ff168058bf97
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-01-22 09:48:46 +0000

    Majordomo (хостинг) (ab-hosting-majordomo): - Поддержка изменений на сайте.

--- trunk/ab-hosting-majordomo/main.js
+++ trunk/ab-hosting-majordomo/main.js
@@ -1,41 +1,36 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о домене для хостинг провайдера majordomo
-
-Сайт оператора: http://majordomo.ru
-Личный кабинет: https://control.majordomo.ru
 */
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    AnyBalance.setDefaultCharset('koi8-r');
+    AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "https://control.majordomo.ru/";
+    var baseurl = "https://control2.majordomo.ru/";
 
-    var html = AnyBalance.requestPost(baseurl, {
-        start:'0',
+    var html = AnyBalance.requestPost(baseurl + 'user/login/', {
         login:prefs.login,
         password:prefs.password
     });
-
-    //AnyBalance.trace(html);
-    if(!/logout.php/i.test(html)){
-        var error = getParam(html, null, null, /<span[^>]*color:\s*red[^>]*>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
-    }
-
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<span[^>]*color:\s*red[^>]*>([\s\S]*?)<\/span>/, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
     var result = {success: true};
 
-    getParam(html, result, 'balance', /Состояние счета[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balance', /Баланс(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, '__tariff', /.>\s*Тарифный план[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'status', /Статус[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'domains', /Доменов на аккаунте:[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-
+    getParam(html, result, 'domains', /доменов[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	
     if(prefs.domains){
-        html = AnyBalance.requestGet(baseurl + 'renew.php');
+        html = AnyBalance.requestGet(baseurl + 'domains');
 
         var notfound = [];
         var found = [];
@@ -50,19 +45,22 @@ function main(){
             if(_domain == '*')
                 _domain = '\\w+\\.\\w+';
            
-            var tr = getParam(domainsHtml, null, null, new RegExp('(<tr(?:[\\s\\S](?!</tr>))*?<input[^>]+name=[\'"]renewname[^>]*value=["\'][^\'"]*' + _domain + '[\\s\\S]*?</tr>)', 'i'));
-        
-            if(!tr){
-                if(domain != '*')
-                    notfound[notfound.length] = domain; 
-            }else{
-                var suffix = ind > 0 ? ind : '';
-                var domain_name = getParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-                getParam(tr, result, 'domain' + suffix, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-                getParam(tr, result, 'domain_till' + suffix, /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
-                found[found.length] = domain_name;
-            }
-        
+			try {
+				var tr = getParam(domainsHtml, null, null, new RegExp('(<tr(?:[\\s\\S](?!</tr>))*?<input[^>]+name=[\'"]renewname[^>]*value=["\'][^\'"]*' + _domain + '[\\s\\S]*?</tr>)', 'i'));
+				
+				if(!tr){
+					if(domain != '*')
+						notfound[notfound.length] = domain; 
+				}else{
+					var suffix = ind > 0 ? ind : '';
+					var domain_name = getParam(tr, null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+					getParam(tr, result, 'domain' + suffix, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+					getParam(tr, result, 'domain_till' + suffix, /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+					found[found.length] = domain_name;
+				}
+			} catch(e) {
+				AnyBalance.trace('Ошибка получения данных по доменам: ' + e.message);
+			}
             ++ind;
         }
         
@@ -80,7 +78,6 @@ function main(){
             AnyBalance.trace('Следующие домены не найдены: ' + notfound.join(', '));
         
     }
-
-    
+	
     AnyBalance.setResult(result);
 }

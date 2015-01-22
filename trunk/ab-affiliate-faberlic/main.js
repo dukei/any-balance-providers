9bbf37a3cccae16f6288fc02d9e621a6261b96a1
julmatina@gmail.com <julmatina@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-01-22 09:53:53 +0000

    Faberlic (ab-affiliate-faberlic): - Поддержка изменений на сайте.

--- trunk/ab-affiliate-faberlic/main.js
+++ trunk/ab-affiliate-faberlic/main.js
@@ -1,71 +1,71 @@
 ﻿/**
-Фаберлик (http://any-balance-providers.googlecode.com)
-
-Получает информацию из личного кабинета сайта компании Фаберлик
-
-Operator site: https://faberlic.com/
-Личный кабинет: https://faberlic.com/index.php?option=com_user&view=login&lang=ru
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
-function main(){
-    var prefs = AnyBalance.getPreferences();
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://faberlic.com/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
 
-    var baseurl = "https://faberlic.com/";
+	var html = AnyBalance.requestPost(baseurl + 'index2.php?option=com_module&module=flregionselection&no_html=1&task=getmodule&act=setRegion&reg_id=1000034210371', null, addHeaders({Referer: baseurl + 'index.php?option=com_user&view=login&lang=ru', 'X-Requested-With':'XMLHttpRequest'}));
+		
+	html = AnyBalance.requestGet(baseurl + 'index.php?option=com_user&view=login&lang=ru', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'username') 
+			return prefs.login;
+		else if (name == 'passwd')
+			return prefs.password;
 
-    AnyBalance.setDefaultCharset('UTF-8'); 
-
-    AnyBalance.requestGet("https://faberlic.com/index2.php?option=com_module&module=flregionselection&no_html=1&task=getmodule&act=setRegion&reg_id=1000034185667");
+		return value;
+	});
     
-    var html = AnyBalance.requestGet(baseurl + 'index.php?option=com_user&view=login&lang=ru', g_headers);
-
-    var form = getParam(html, null, null, /<form[^>]+id="com-form-login"[^>]*>([\s\S]*?)<\/form>/i);    
-    if(!form)
-        throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
-
-    var params = createFormParams(form);
-    params['username'] = prefs.login;
-    params['passwd'] = prefs.password;
-
-    html = AnyBalance.requestPost(baseurl + 'index.php?option=com_user&view=login&lang=ru', params, addHeaders({Referer: baseurl + 'index.php?option=com_user&view=login&lang=ru'})); 
-
-    var meta = getParam(html, null, null, /<meta[^>]+http-equiv="refresh"[^>]+content="0;([\s\S]*?)\"/i, replaceTagsAndSpaces, html_entity_decode);
-    console.log(meta);
-    html = AnyBalance.requestGet(meta, g_headers);     
-
-    if(!/logout-link/i.test(html)){
-        var error = getParam(html, null, null, /<dd[^>]+class="error message fade"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-
-    var result = {success: true};
-    getParam(html, result, 'fio', /<div[^>]+class="userscaption"[^>]*><a[^>]*>([\s\S]*?)-/i, replaceTagsAndSpaces, html_entity_decode);
-    //мой счет
-    getParam(html, result, 'balance', /<div[^>]+class="block-myaccount"[^>]*>(?:[\s\S]*?<div[^>]+class="mod-ourorders-famount"[^>]*>){1}([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-    // моя ОС
-    getParam(html, result, 'myOS', /<div[^>]+class="block-myaccount"[^>]*>(?:[\s\S]*?<div[^>]+class="mod-ourorders-famount"[^>]*>){2}([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-    // К оплате
-    getParam(html, result, 'payable', /<div[^>]+class="block-myaccount"[^>]*>(?:[\s\S]*?<div[^>]+class="mod-ourorders-famount"[^>]*>){3}([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-    // ближайщая доставка
-    getParam(html, result, 'delivery', /Ближайшая доставка<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    // консультант
-    getParam(html, result, 'consultant', /<td[^>]*>\s*Текущий период\s*<\/td>(?:[\s\S]*?<td[^>]*>){2}[\s\S]*?<span[^>]+class="red"[^>]*>\s*-([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    // ЛО
-    getParam(html, result, 'LO', /<td[^>]*>\s*Текущий период\s*<\/td>(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    // ОЛГ
-    getParam(html, result, 'OLG', /<td[^>]*>\s*Текущий период\s*<\/td>(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    // ГО
-    getParam(html, result, 'GO', /<td[^>]*>\s*Текущий период\s*<\/td>(?:[\s\S]*?<td[^>]*>){8}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	html = AnyBalance.requestPost(baseurl + 'index.php?option=com_user&view=login&lang=ru', params, addHeaders({Referer: baseurl + 'index.php?option=com_user&view=login&lang=ru'}));
+	
+    html = AnyBalance.requestGet(baseurl + 'index.php?option=com_user&view=cabinet', g_headers);
     
-
-    AnyBalance.setResult(result);
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<dd[^>]+class="error message fade"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неправильный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}    
+    
+    html = AnyBalance.requestGet(baseurl + 'index.php?option=com_user&view=bill&Itemid=2184&lang=ru', g_headers);
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Доступно(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /Личный кабинет(?:[^>]*>){9}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+    
+    html = AnyBalance.requestGet(baseurl + 'index.php?option=com_list&view=list&listId=84&Itemid=1679&lang=ru', g_headers);
+    
+	getParam(html, result, 'orders_sum', /Итоговая сумма заказов(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'points_sum', /Итоговая сумма баллов(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+    
+    html = AnyBalance.requestGet(baseurl + 'index.php?option=com_user&view=cabinet&Itemid=2069&lang=ru', g_headers);
+    
+	getParam(html, result, 'lo', /<td[^>]*>\s*Текущий период\s*<\/td>(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'olg', /<td[^>]*>\s*Текущий период\s*<\/td>(?:[\s\S]*?<td[^>]*>){9}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'go', /<td[^>]*>\s*Текущий период\s*<\/td>(?:[\s\S]*?<td[^>]*>){12}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
 }

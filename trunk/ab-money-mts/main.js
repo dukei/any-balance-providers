41ba15ff8d402e56f6744491d09473faeafde216
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-01-31 16:40:42 +0000

    МТС Банк (ab-money-mts): - Улучшена обработка ошибок.

--- trunk/ab-money-mts/main.js
+++ trunk/ab-money-mts/main.js
@@ -69,9 +69,10 @@ function main(){
     }, headers);
 
     var error = getParam(html, null, null, /<BSS_ERROR>\d*\|?([\s\S]*?)(?:EXT\(|<\/BSS_ERROR>)/i, replaceTagsAndSpaces, html_entity_decode);
-    if(error && /Вы ошиблись при вводе логина или пароля|Логин или пароль введены неверно/i.test(error))
-	error = replaceAll(decodeURIComponent(error), replaceTagsAndSpaces);
+    if(error && /Вы ошиблись при вводе логина или пароля|Логин или пароль введены неверно/i.test(error)){
+		error = replaceAll(decodeURIComponent(error), replaceTagsAndSpaces);
         throw new AnyBalance.Error(error, null, true);
+    }
     if(error)
         throw new AnyBalance.Error(error);
 

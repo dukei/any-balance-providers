a1a455439cc8347b70b59b793494a655c330aed6
dmitry.kochin <dmitry.kochin@gmail.com>
2015-02-02 07:07:26 +0000

    Банк УРАЛСИБ (ab-money-uralsib): - Исправлена ошибка JS

---
+++ ab-money-uralsib/env.js
@@ -0,0 +1,6 @@
+﻿//Сэмулируем нужные переменные из браузерного окружения
+var navigator = {
+	appName: 'Netscape',
+	appVersion: "5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36"
+};
+var window = {};

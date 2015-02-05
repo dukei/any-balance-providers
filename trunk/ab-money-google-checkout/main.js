07c46589e8f2dfea181757afcb4c5b74793e1c74
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-02-05 13:52:54 +0000

    Google Wallet (Checkout) (ab-money-google-checkout): - Added support of two-factor authorization.

--- trunk/ab-money-google-checkout/main.js
+++ trunk/ab-money-google-checkout/main.js
@@ -6,8 +6,92 @@ var g_headers = {
 	'Cache-Control': 'max-age=0',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Origin': 'https://accounts.google.com',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.91 Safari/537.36',
-	'Accept-Language': 'ru,en;q=0.8'
+	'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.0.4; ru-ru; Android SDK built for x86 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+	'Accept-Language': 'ru-RU, en-US',
+
+}
+
+/** вынес в отдельную функцию, чтобы было легко скопировать ее в другие провайдеры */
+function googleLogin(prefs) {
+	var baseurlLogin = 'https://accounts.google.com/';
+
+	var html = AnyBalance.requestGet(baseurlLogin + 'ServiceLoginAuth', g_headers);
+
+	var GALX = getParam(html, null, null, /GALX[^>]*value=['"]([^"']+)/i);
+	if(!GALX)
+		throw new AnyBalance.Error('Can`t find find login form, please, contact the developers.');
+	
+	// Прежде чем входить, вынем куки
+	AnyBalance.restoreCookies();
+	
+	html = AnyBalance.requestPost(baseurlLogin + 'ServiceLoginAuth', {
+		'osid': '1',
+		'_utf8': '☃',
+		'bgresponse': 'js_disabled',
+		'pstMsg': '0',
+		'dnConn': '',
+		'checkConnection': '',
+		'checkedDomains': 'youtube',
+		'signIn': 'Войти',
+		'rmShown': '1',
+		'Email': prefs.login,
+		'Passwd': prefs.password,
+		'GALX': GALX,
+		'PersistentCookie': 'yes',
+	}, g_headers);
+	
+	// Двухэтапная авторизация...
+	if(/secondfactor/i.test(html)) {
+		// throw new AnyBalance.Error('Two-factor authorization is enabled. Just now we can`t deal with this. Login attempt has failed.');
+		if(AnyBalance.getLevel() >= 7) {
+			AnyBalance.trace('Trying to get code...');
+			var promt = getParam(html, null, null, /"deliverymethodcontainer"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+			var code = AnyBalance.retrieveCode(promt || 'Plaese, enter code from Authentificator');
+			AnyBalance.trace('Got code: ' + code);
+		}
+		if(!isset(code))
+			throw new AnyBalance.Error('Two-factor authorization is enabled. Just now we can`t deal with this. Login attempt has failed.');
+		
+		var secTok = getParam(html, null, null, /['"]secTok['"][^>]*value=['"]([^'']+)/i);
+		var timeStmp = getParam(html, null, null, /['"]timeStmp['"][^>]*value=['"]([^'']+)/i);
+		
+		html = AnyBalance.requestPost(baseurlLogin + 'SecondFactor', [
+			['checkedDomains', 'youtube'],
+			['pstMsg', '0'],
+			['timeStmp', timeStmp],
+			['secTok', secTok],
+			['smsToken', ''],
+			['smsUserPin', code],
+			['smsVerifyPin', 'Подтвердить'],
+			['PersistentOptionSelection', '1'],
+			['PersistentCookie', 'on'],
+		], addHeaders({Referer: baseurlLogin + 'SecondFactor?checkedDomains=youtube&pstMsg=0'}));
+		
+		if(isLoginSuccesful(html)) {
+			// Если вошли, то это повод сохранить все куки чтобы больше не донимать юзера окошками
+			AnyBalance.saveCookies();
+			AnyBalance.saveData();
+		}
+	}
+	// Еще раз проверим правильность входа
+	isLoginSuccesful(html);
+	
+	function isLoginSuccesful(html) {
+		if (!/logout/i.test(html)) {
+			var error = getParam(html, null, null, /<span[^>]+class="errormsg[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+			if (error)
+				throw new AnyBalance.Error(error);
+			error = getParam(html, null, null, /(<form[^>]+name="verifyForm")|secondfactor/i, replaceTagsAndSpaces, html_entity_decode);
+			if (error)
+				throw new AnyBalance.Error('This account requires 2-step authorization. Turn off 2-step authorization to use this provider.');
+			
+			AnyBalance.trace(html);
+			throw new AnyBalance.Error('Can`t log in, is the site changed?');
+		}
+		return true;
+	}
+	
+	return html;
 }
 
 function main() {
@@ -16,63 +100,72 @@ function main() {
 	checkEmpty(prefs.password, 'Enter password!');
 	
 	AnyBalance.setDefaultCharset('utf-8');
-	
 	var baseurl = 'https://wallet.google.com/';
-	var baseurlLogin = 'https://accounts.google.com/';
-
-	try {
-		var html = AnyBalance.requestGet(baseurlLogin + 'ServiceLoginAuth', g_headers);
-
-		var GALX = getParam(html, null, null, /GALX[^>]*value="([^"]*)/i);
-		if(!GALX)
-			throw new AnyBalance.Error('Can`t find find login form, please, contact the developers.');
-
-		html = AnyBalance.requestPost(baseurlLogin + 'ServiceLoginAuth', {
-			'Passwd': prefs.password,
-			'Email': prefs.login,
-			'GALX': GALX,
-			'PersistentCookie': 'yes',
-		}, g_headers);
-	} catch(e) {
-		var html = AnyBalance.requestGet('https://www.google.com/settings/account', g_headers);
-	}
 	
-	// Двухэтапная авторизация...
-	if(/secondfactor/i.test(html)) {
-		throw new AnyBalance.Error('Two-factor authorization is enabled. Just now we can`t deal with this. Login attempt has failed.');
-		
-		if(AnyBalance.getLevel() >= 7) {
-			AnyBalance.trace('Trying to get code...');
-			var code = AnyBalance.retrieveCode("Plaese, enter code from Authentificator");
-			AnyBalance.trace('Got code: ' + code);
-		} else {
-			throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
-		}
-		
-		var secTok = getParam(html, null, null, /['"]secTok['"][^>]*value=['"]([^'']+)/i);
-		
-		html = AnyBalance.requestPost(baseurlLogin + 'SecondFactor', {
-			'timeStmp': Math.round(new Date().getTime()/1000),
-			'secTok': secTok,
-			'smsToken':'',
-			'smsUserPin': code,
-			'smsVerifyPin': 'Подтвердить',
-			'PersistentOptionSelection':'1',
-			'PersistentCookie': 'on'
-		}, addHeaders({Referer: baseurlLogin + 'SecondFactor'}));
-	}
+	var html = googleLogin(prefs);
+	// var baseurlLogin = 'https://accounts.google.com/';
+
+	// var html = AnyBalance.requestGet(baseurlLogin + 'ServiceLoginAuth', g_headers);
+
+	// var GALX = getParam(html, null, null, /GALX[^>]*value=['"]([^"']+)/i);
+	// if(!GALX)
+		// throw new AnyBalance.Error('Can`t find find login form, please, contact the developers.');
+
+	// html = AnyBalance.requestPost(baseurlLogin + 'ServiceLoginAuth', {
+		// 'osid': '1',
+		// '_utf8': '☃',
+		// 'bgresponse': 'js_disabled',
+		// 'pstMsg': '0',
+		// 'dnConn': '',
+		// 'checkConnection': '',
+		// 'checkedDomains': 'youtube',
+		// 'signIn': 'Войти',
+		// 'rmShown': '1',
+		// 'Email': prefs.login,
+		// 'Passwd': prefs.password,
+		// 'GALX': GALX,
+		// 'PersistentCookie': 'yes',
+	// }, g_headers);
 	
-	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /<span[^>]+class="errormsg[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error);
-		error = getParam(html, null, null, /(<form[^>]+name="verifyForm")/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error('This account requires 2-step authorization. Turn off 2-step authorization to use this provider.');
+	// // Двухэтапная авторизация...
+	// if(/secondfactor/i.test(html)) {
+		// // throw new AnyBalance.Error('Two-factor authorization is enabled. Just now we can`t deal with this. Login attempt has failed.');
+		// if(AnyBalance.getLevel() >= 7) {
+			// AnyBalance.trace('Trying to get code...');
+			// var promt = getParam(html, null, null, /"deliverymethodcontainer"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+			// var code = AnyBalance.retrieveCode(promt || 'Plaese, enter code from Authentificator');
+			// AnyBalance.trace('Got code: ' + code);
+		// }
+		// if(!isset(code))
+			// throw new AnyBalance.Error('Two-factor authorization is enabled. Just now we can`t deal with this. Login attempt has failed.');
+		
+		// var secTok = getParam(html, null, null, /['"]secTok['"][^>]*value=['"]([^'']+)/i);
+		// var timeStmp = getParam(html, null, null, /['"]timeStmp['"][^>]*value=['"]([^'']+)/i);
+		
+		// html = AnyBalance.requestPost(baseurlLogin + 'SecondFactor', [
+			// ['checkedDomains', 'youtube'],
+			// ['pstMsg', '0'],
+			// ['timeStmp', timeStmp],
+			// ['secTok', secTok],
+			// ['smsToken', ''],
+			// ['smsUserPin', code],
+			// ['smsVerifyPin', 'Подтвердить'],
+			// ['PersistentOptionSelection', '1'],
+			// ['PersistentCookie', 'on'],
+		// ], addHeaders({Referer: baseurlLogin + 'SecondFactor?checkedDomains=youtube&pstMsg=0'}));
+	// }
+	
+	// if (!/logout/i.test(html)) {
+		// var error = getParam(html, null, null, /<span[^>]+class="errormsg[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+		// if (error)
+			// throw new AnyBalance.Error(error);
+		// error = getParam(html, null, null, /(<form[^>]+name="verifyForm")|secondfactor/i, replaceTagsAndSpaces, html_entity_decode);
+		// if (error)
+			// throw new AnyBalance.Error('This account requires 2-step authorization. Turn off 2-step authorization to use this provider.');
 			
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Can`t log in, is the site changed?');
-	}
+		// AnyBalance.trace(html);
+		// throw new AnyBalance.Error('Can`t log in, is the site changed?');
+	// }
 	
 	var result = {success: true};
 	

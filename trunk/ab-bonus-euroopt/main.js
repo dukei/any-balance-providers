eb14dbd323ddfd9547bf91ee5e81f56045838175
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-02-09 08:30:24 +0000

    Евроопт (Беларусь) (ab-bonus-euroopt): - Поддержка изменений на сайте.

--- trunk/ab-bonus-euroopt/main.js
+++ trunk/ab-bonus-euroopt/main.js
@@ -1,24 +1,26 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает информацию о скидке в белорусской компании евроопт.
-Использует API версии не ниже 3.
-
-Сайт магазина: http://www.euroopt.by
-Личный кабинет: http://www.euroopt.by/otchet-po-diskontnoj-karte-2
 */
 
-function main () {
-    if (AnyBalance.getLevel () < 3) {
-        throw new AnyBalance.Error ('Для этого провайдера необходима версия программы не ниже 1.2.436. Пожалуйста, обновите программу.');
-    }
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
+function main () {
+    if (AnyBalance.getLevel () < 3)
+        throw new AnyBalance.Error ('Для этого провайдера необходима версия программы не ниже 1.2.436. Пожалуйста, обновите программу.');
+	
     var prefs = AnyBalance.getPreferences ();
-    var baseurl = 'http://www.euroopt.by/';
+	
+    var baseurl = 'http://www.evroopt.by/';
     if(!prefs.login)
         throw new AnyBalance.Error('Введите № карты');
-
-    var html = AnyBalance.requestGet(baseurl + 'otchet-po-diskontnoj-karte-2');
+	
+    var html = AnyBalance.requestGet(baseurl + 'otchet-po-diskontnoj-karte-2', g_headers);
     var form = getParam(html, null, null, /<div[^>]+class="enter_number_form"[^>]*>([\s\S]*?)<\/form>/i);
     if(!form){
 	AnyBalance.trace(html);
@@ -43,8 +45,8 @@ function main () {
        
         return value;
     });
-
-    var html = AnyBalance.requestPost(baseurl + 'otchet-po-diskontnoj-karte-2', params);
+	
+    var html = AnyBalance.requestPost(baseurl + 'otchet-po-diskontnoj-karte-2', params, addHeaders({Referer: baseurl + 'otchet-po-diskontnoj-karte-2'}));
     
     if(!/<tr[^>]*class="itog"/i.test(html)){
         var error = getParam(html, null, null, /<ul[^>]+class="errors"[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -54,6 +56,7 @@ function main () {
         error = getParam(html, null, null, /Вы не заполнили сведения о себе|Для идентификации пользователя/);
         if(error)
             throw new AnyBalance.Error("Евроопт требует заполнить форму регистрации. Вам необходимо зайти на сайт http://www.euroopt.by/otchet-po-diskontnoj-karte-2 через браузер и заполнить форму");
+		
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось получить данные по карте. Сайт изменен?');
     }

4d9a28288a22673c7305a81b79154d54f9fc3cc7
julmatina@gmail.com <julmatina@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-02-10 14:18:35 +0000

    ACBT (ab-telephony-acbt): Добавлено получение информации о балансе по номеру договора

--- trunk/ab-telephony-acbt/main.js
+++ trunk/ab-telephony-acbt/main.js
@@ -7,9 +7,6 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
@@ -25,15 +22,6 @@ function main() {
 	
 	if(!html || AnyBalance.getLastStatusCode() > 400)
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
-	
-	// var params = createFormParams(html, function(params, str, name, value) {
-		// if (name == 'userlogin') 
-			// return prefs.login;
-		// else if (name == 'userpassword')
-			// return prefs.password;
-
-		// return value;
-	// });
 	
 	html = AnyBalance.requestPost(baseurl + 'billing/!w3_p_main.showform?CONFIG=CONTRACT', {
         IDENTIFICATION: 'CONTRACT',
@@ -56,21 +44,31 @@ function main() {
     var menu = getParam(html, null, null, /name="menu"\s+src="([^"]+)/i, replaceTagsAndSpaces, html_entity_decode);
     checkEmpty(menu, 'Не удалось найти ссылку на переадресацию, сайт изменен?', true);
     
-    html = AnyBalance.requestGet(baseurl + 'billing/!w3_p_main.showform'+menu, g_headers);
+    html = AnyBalance.requestGet(baseurl + 'billing/!w3_p_main.showform' + menu, g_headers);
     
-    var contract = getParam(html, null, null, /\?FORMNAME=QCURRACC&CONTR_ID=\d+[^"']+/i, replaceTagsAndSpaces, html_entity_decode);
-    checkEmpty(contract, 'Не удалось найти ссылку на информацию по контракту, сайт изменен?', true);
+    if(prefs.accnum && !/^\d{5}$/.test(prefs.accnum))
+        throw new AnyBalance.Error("Введите номер договора или не вводите ничего, чтобы показать информацию по первому договору");
+
+    var href = getParam(html, null, null, new RegExp("javascript:Click\\('R!C\\d*?!','\\?FORMNAME=\\S*?&CONTR_ID=\\d*?&SID=\\S*?&NLS=WR','data'\\)\">Договор\\s" + (prefs.accnum || "\\d{5}") + "</a>", 'i'), replaceTagsAndSpaces);
+    AnyBalance.trace(href);
     
-    html = AnyBalance.requestGet(baseurl + 'billing/!w3_p_main.showform'+contract, g_headers);    
+    if(!href)
+		throw new AnyBalance.Error("Не удалось найти " + (prefs.accnum ? ' номер договора ' + prefs.accnum : 'ни одного договора!'));        
     
+    var formname = getParam(href, null, null, /FORMNAME=([\s\S]*?)&/i);
+    var contr_id = getParam(href, null, null, /CONTR_ID=([\s\S]*?)&/i);
+    var sid = getParam(href, null, null, /SID=([\s\S]*?)&/i);
+    
+ 	html = AnyBalance.requestPost(baseurl + 'billing/!w3_p_main.showform?FORMNAME=' + formname + '&CONTR_ID=' + contr_id + '&SID=' + sid + '&NLS=WR', {}, addHeaders({Referer: baseurl + 'billing/!w3_p_main.showform?CONFIG=CONTRACT'}));  
     
 	var result = {success: true};
     
 	getParam(html, result, 'balance', /Текущий баланс \(на(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'recommended', /Рекомендуемая сумма платежа:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'fio', /Абонент:[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'type', /Тип объекта договора:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'account', /Состояние счёта по договору № (\d+)/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'phone', /Внутренний идентификатор:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'inner_id', /Внутренний идентификатор:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'state', /Состояние:(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
     
 	AnyBalance.setResult(result);

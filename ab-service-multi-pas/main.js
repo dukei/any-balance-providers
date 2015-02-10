0458fce80aef5d127b867c449da072bd09483c4d
julmatina <julmatina@gmail.com>
2015-02-10 08:46:21 +0000

    Новый провайдер: multi-pas

---
+++ ab-service-multi-pas/main.js
@@ -0,0 +1,56 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Cache-Control': 'max-age=0',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Origin': 'http://multi-pas.ru',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://multi-pas.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'personal/', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+    
+	html = AnyBalance.requestPost(baseurl + 'personal/?login=yes', [
+        ['AUTH_FORM', 'Y'],
+        ['TYPE', 'AUTH'],
+        ['backurl', '/personal/'],
+        ['AUTH_FORM', 'Y'],
+        ['TYPE', 'AUTH'],
+        ['Login', 'Войти'],
+        ['USER_LOGIN', prefs.login], 
+        ['USER_PASSWORD', prefs.password],
+        ['Login', 'Войти']
+	], addHeaders({Referer: baseurl + 'personal/?login=yes'}));
+	
+	if (/form name="form_auth"/i.test(html)) {
+		var error = getParam(html, null, null, /class="errortext"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /баланс:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /Тарифный план:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'school', /p\sclass="school"(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

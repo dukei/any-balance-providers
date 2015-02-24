58e5e318dda0b10ec06a10ea4c8fddb59b7d4cd5
kirillpisarev23@gmail.com <kirillpisarev23@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-02-24 10:22:32 +0000

    21 Век Бонус (Беларусь) (ab-bonus-21century): - Поддержка изменений на сайте.

--- trunk/ab-bonus-21century/main.js
+++ trunk/ab-bonus-21century/main.js
@@ -7,7 +7,7 @@ var g_headers = {
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
 };
 
 function main(){
@@ -26,16 +26,14 @@ function main(){
 	var pass = '';
 	var parts = /(\d{2})(\d{2})(\d{4})/i.exec(prefs.password)
 	if(parts)
-	{
 		pass += parts[1]+' . '+ parts[2] +' . '+ parts[3]
-	}
 	else
 		throw new AnyBalance.Error('Дату рождения не удалось преобразовать в нужный формат, введите дату рождения без пробелов и точек, например 15101988');
         
 	html = AnyBalance.requestPost(baseurl + 'balance/', {
         'data[number]': prefs.login,
         'data[birthday]': pass
-    }, addHeaders({'X-Requested-With': 'XMLHttpRequest'})); 
+    }, addHeaders({'X-Requested-With': 'XMLHttpRequest', 'Refer': baseurl})); 
 
 	var json = getJson(html);
     

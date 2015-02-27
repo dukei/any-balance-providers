07bce02e06cb113dc4ebd0cb7cb64250db1a9707
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-02-27 07:52:44 +0000

    Л'Этуаль (ab-bonus-letoile): - Мелкие улучшения.

--- ab-bonus-letoile/main.js
+++ ab-bonus-letoile/main.js
@@ -2,6 +2,14 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
+var g_headers = {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Connection':'keep-alive',
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
+}
+
 function main () {
     var prefs = AnyBalance.getPreferences ();
     var baseurl = 'http://www.letoile.ru/';
@@ -9,8 +17,15 @@ function main () {
     checkEmpty (prefs.number, 'Введите номер карты');
     checkEmpty (prefs.color, 'Выберите цвет карты');
 
-    var html = AnyBalance.requestGet(baseurl + 'ajax/check_account.php?card_type=' + prefs.color + '&card_number=' + prefs.number);
-    // Проверка неправильной пары логин/пароль
+    var html = AnyBalance.requestGet(baseurl, g_headers);
+
+    html = AnyBalance.requestGet(
+    	baseurl + 'ajax/check_account.php?card_type=' + prefs.color + '&card_number=' + prefs.number,
+    	addHeaders({
+    		Referer:http: baseurl,
+    		'X-Requested-With': 'XMLHttpRequest'
+    });
+
 	if (!/Баланс\s*карты:/i.test(html)) {
 		var error = getParam(html, null, null, /class="g-error"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
@@ -18,12 +33,10 @@ function main () {
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
     var result = {success: true};
-    // Баланс
 	getParam(html, result, 'balance', /Баланс\s*карты:[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 
 	if(isAvailable('dateOf')) {
-		html = AnyBalance.requestGet (baseurl + 'club/cards/account/');
-		// Дата актуализации баланса
+		html = AnyBalance.requestGet (baseurl + 'club/cards/account/', g_headers);
 		getParam(html, result, 'dateOf', /Данные\s*о\s*балансе(?:[^>]*>){2}(\d{1,2}.\d{1,2}.\d{4} \d{1,2}:\d{2})/i, replaceTagsAndSpaces, parseDate);	
 	}
 

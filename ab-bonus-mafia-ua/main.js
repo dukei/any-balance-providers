f86ff99ae89a0498711df9466e7591cbd30819de
stark027 <stark027@gmail.com>
2015-03-02 15:42:16 +0000

    MAFIA (Украина) (ab-bonus-mafia-ua): - Поддержка изменений на сайте.

--- ab-bonus-mafia-ua/main.js
+++ ab-bonus-mafia-ua/main.js
@@ -1,16 +1,11 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получается баланс бонусной карты для ресторана MAFIA 
-
-Operator site: http://mafia.ua/
-Личный кабинет: http://mafia.ua/cabinet/
 */
 
 var g_headers = {
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36'
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36'
 };
 
 function main(){
@@ -20,7 +15,7 @@ function main(){
 
     AnyBalance.setDefaultCharset('utf-8'); 
 
-    var html = AnyBalance.requestGet(baseurl + 'cabinet/auth/', g_headers);
+    var html = AnyBalance.requestGet(baseurl + 'cabinet/login/', g_headers);
 
     var tform = getParam(html, null, null, /<input[^>]+name='csrfmiddlewaretoken'[^>]*value=\'([\s\S]*?)\'[^>]*\/>/i, null, html_entity_decode);
     if(!tform) 
@@ -32,30 +27,33 @@ function main(){
     params['password'] = prefs.password;
     params['action'] = 'login';
 
-    html = AnyBalance.requestPost(baseurl + 'cabinet/auth/', params, addHeaders({
-        Referer: baseurl + 'cabinet/auth/', 
+    html = AnyBalance.requestPost(baseurl + 'cabinet/login/', params, addHeaders({
+        Referer: baseurl + 'cabinet/login/', 
         'Origin':'http://mafia.ua',
         'X-Requested-With':'XMLHttpRequest'
     })); 
-
+	
     var json = getJson(html);
-    console.log(json);
-
+	
     if(json.status != "ok"){
         var error = getParam(json.text, null, null, null, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
+		
+		AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 
     html = AnyBalance.requestGet(baseurl + 'cabinet/', g_headers);
 
     var result = {success: true};
-    getParam(html, result, 'fio', /<td[^>]*>Имя<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'phone', /<td[^>]*>Мобильный<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /<td[^>]*>Накопления<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'bonus', /<td[^>]*>Бонусы<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'discount', /<td[^>]*>Скидка<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+    getParam(html, result, 'bonus', />\s*Сумма бонусов(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'balance', />\s*Потраченная сумма(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'fio', />\s*Имя(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'phone', />\s*Мобильный(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	
+    getParam(html, result, 'discount', />\s*Привелегии карты(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 
     AnyBalance.setResult(result);
 }

2c545271f37099e57eb474792a76519e376af6ba
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-03-03 11:02:59 +0000

    TelinCom (Москва) (ab-internet-telincom): - Поддержка изменений на сайте.

--- ab-internet-telincom/main.js
+++ ab-internet-telincom/main.js
@@ -1,92 +1,58 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плане для московского интернет-провайдера telincom
-
-Сайт оператора: http://www.telincom.ru
-Личный кабинет: http://stat.telincom.ru/site/
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var matches = regexp.exec (html), value;
-	if (matches) {
-		value = matches[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-	}
-   return value
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseTrafficGb(str){
-  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-  return parseFloat((val/1024).toFixed(2));
-}
+var g_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
+    var baseurl = "http://telincom.ru/";
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "http://stat.telincom.ru/site/stat.php";
+    checkEmpty(prefs.login, 'Введите логин!');
+    checkEmpty(prefs.password, 'Введите пароль!');
 
-    var html = AnyBalance.requestPost(baseurl, {
-        cmd: 'login',
-        login:prefs.login,
-        password:prefs.password
+    var html = AnyBalance.requestGet(baseurl + 'personal/', g_headers);
+    
+    if(!html || AnyBalance.getLastStatusCode() > 400){
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+    }
+
+    var html = AnyBalance.requestPost(baseurl + 'personal/', {
+        login: prefs.login,
+        password: prefs.password,
+        submit: 'Войти'
     });
 
-    //AnyBalance.trace(html);
-    if(!/stat.php\?cmd=logout/.test(html)){
-        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неверный логин-пароль?');
+    if (!/logout/i.test(html)) {
+        var error = getParam(html, null, null, /<div[^>]+class="error-message"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if (error)
+            throw new AnyBalance.Error(error, null, /Логин или пароль не верен/i.test(error));
+        
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 
     var result = {success: true};
 
-    getParam(html, result, 'balance', /Баланс:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'credit', /Кредит:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'status', /Статус Интернета:[\S\s]*?<td[^>]*>([\S\s]*?)(?:<a[^>]*>|<\/td>)/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'licschet', /Л\/С:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-
-    getParam(html, result, '__tariff', />Название<[\s\S]*?<td[^>]*class=['"]utm-table[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /balanse(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+    //getParam(html, result, 'credit', /Кредит:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    //getParam(html, result, 'status', /Статус Интернета:[\S\s]*?<td[^>]*>([\S\s]*?)(?:<a[^>]*>|<\/td>)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /лицевой счет(?:[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /data-name="Интернет"[^>]+data-tarif-name="([^>]*?)"/i, replaceTagsAndSpaces, html_entity_decode);
 
     if(AnyBalance.isAvailable('trafficIn', 'trafficOut')){
-        var sid = getParam(html, null, null, /SID=([a-f0-9]{32})/i);
-        if(!sid){
-            AnyBalance.trace('Не удаётся получить идентификатор сессии, трафик пропускаем...');
-        }else{
-            html = AnyBalance.requestGet(baseurl + '?cmd=u_traff&SID='+sid);
-            getParam(html, result, 'trafficIn', /ИТОГО(?:[\s\S]*?<td[^>]*>)([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
-            getParam(html, result, 'trafficOut', /ИТОГО(?:[\s\S]*?<td[^>]*>){2}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
-        }
+        html = AnyBalance.requestGet(baseurl + 'personal/traffic.php');
+        getParam(html, result, 'trafficIn', /Всего за период(?:[\s\S]*?<td[^>]*>)([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+        getParam(html, result, 'trafficOut', /Всего за период(?:[\s\S]*?<td[^>]*>){2}([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
     }
     
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

732e6b84ec1026f53ab5009355985bb3579de7ea
kirillpisarev23@gmail.com <kirillpisarev23@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-03-05 11:08:32 +0000

    Новый провайдер: Простоквашино (Беларусь)(ab-bonus-prostokvashino)

---
+++ trunk/ab-bonus-prostokvashino/main.js
@@ -0,0 +1,58 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://prostokvashino.by/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'users/login', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'user[email]') 
+			return prefs.login;
+		else if (name == 'user[password]')
+			return prefs.password;
+
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + '/users/login', params, addHeaders({Referer: baseurl + 'login'}));
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+
+	html = AnyBalance.requestGet(baseurl + 'user/profile', g_headers);
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Баллов на счету:[^>]+>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /userInfo[\s\S]+?<strong>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'email', /E-mail:[^>]+>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'gender', /Пол:[^>]+>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

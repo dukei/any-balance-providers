c56f09c10bb9e026bf42757c2eb8b10e1bc5667d
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-03-11 10:44:00 +0000

    Новый провайдер: Л'Этуаль (Украина)(ab-bonus-letoile-ua)

---
+++ ab-bonus-letoile-ua/main.js
@@ -0,0 +1,62 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.letu.ua/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'login') 
+			return prefs.login;
+		else if (name == 'password')
+			return prefs.password;
+
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + '?_DARGS=/navigation/gadgets/homePageLogin.jsp', params, 
+		addHeaders({Referer: baseurl}));
+	
+	if (!/LoginFormHandler.logout/i.test(html)) {
+		var error = getParam(html, null, null, /Пользователь с указанным e-mail не зарегистрирован|Пожалуйста, проверьте правильность логина и пароля|Неверный формат e-mail/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, true);
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+
+	html = AnyBalance.requestGet(baseurl + 'myaccount/profile/accountProfileEdit.jsp', g_headers);
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Баланс Вашей карты\s*<b[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'discount', /скидка (\d+)%/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'nextLevel', /необходимо совершить покупок на общую сумму\s*<b[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}
+
+
+
+

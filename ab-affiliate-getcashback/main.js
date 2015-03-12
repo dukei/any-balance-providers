5daa5bfac42cc225222efef5f8ee0a4d3b03542f
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-03-12 13:18:33 +0000

    Getcashback (ab-affiliate-getcashback): - Поддержка изменений на сайте.

--- ab-affiliate-getcashback/main.js
+++ ab-affiliate-getcashback/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://getcashback.ru/';
+	var baseurl = 'http://www.getcashback.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');

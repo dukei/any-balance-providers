c9d6876f86d7a8c6c94396a83062c9410f723ba1
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-03-23 08:26:32 +0000

    Volia Broadband (Украина) (ab-internet-volia-broadband): - Поддержка изменений на сайте. - Добавлен счетчик 'Рекомендовано оплатить'

--- ab-internet-volia-broadband/main.js
+++ ab-internet-volia-broadband/main.js
@@ -1,88 +1,72 @@
 /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Высокоскоростной интернет и цифровое телевидение Воля
-Сайт оператора: http://volia.com
-Личный кабинет: https://stat.volia.com:8443/ktvinet/main.do
 */
 
-function findGoodService(services){
-    for(var i=0; services && i<services.length; ++i){
-        if(!services[i].finishDate)
-            return services[i];
-        var matches = /\w+\s+(\w+)\s+(\d+).*(\d{4})/i.exec(services[i].finishDate);
-        if(matches){
-             var dt = Date.parse(matches[1] + ' ' + matches[2] + ', ' + matches[3]);
-             AnyBalance.trace("Parsed date " + new Date(dt) + " from " + services[i].finishDate);
-             if(dt > new Date().getTime())
-                 return services[i];
-        }
-    }
-    return null;
-}
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
-	var pass = prefs.pass;
-	var login = prefs.login;
-	var info = AnyBalance.requestGet('https://stat.volia.com:8443/ktvinet/vapi/jstat.jsp?code=' + login + '&pass=' + pass);
-	if (info){
-		var result = {success: true};
-		var v = JSON.parse(info);
-		var resst = v.responseStatus.responseValue;
-		var tariff_type = prefs.tariff_type || '3';
-		if (resst == 'ok'){
-			result.__tariff = v.abonent.fullname;
-                        if(AnyBalance.isAvailable('fio'))
-                            result.fio = v.abonent.fullname;
-                        var service = findGoodService(v.services);
-                        //if(service && service.model) //Старое отображение тарифа
-                        //    result.__tariff = ((service.name && service.code != 'inet') ? service.name + ': ' + service.model : service.model);
-			if(v.services[tariff_type])
-                            result.__tariff = v.services[tariff_type].name + ': ' + v.services[tariff_type].model;
-			//Баланс
-			if(AnyBalance.isAvailable('balance') && v.services[0]){
-                                AnyBalance.trace('balance is ' + v.services[0].saldo + ', ' + typeof(v.services[0].saldo));
-				result['balance'] = v.services[0].saldo;
-			}
-			//Общий трафик
-			if(AnyBalance.isAvailable('traffic') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].traffic){
-				result['traffic'] = (v.traffic[0].rows[2].traffic / 1000000000).toFixed(1);
-			}
-			if(AnyBalance.isAvailable('traffic_in_lim') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].underTraffic){
-				result['traffic_in_lim'] = (v.traffic[0].rows[2].underTraffic / 1000000000).toFixed(1);
-			}
-			if(AnyBalance.isAvailable('traffic_over_lim') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[2] && v.traffic[0].rows[2].overTraffic){
-				result['traffic_over_lim'] = (v.traffic[0].rows[2].overTraffic / 1000000000).toFixed(1);
-			}
-			//Трафик внешний зарубежный
-			if(AnyBalance.isAvailable('traffic_SatEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[1] && v.traffic[0].rows[1].traffic){
-				result['traffic_SatEx'] = (v.traffic[0].rows[1].traffic / 1000000000).toFixed(1);
-			}
-			if(AnyBalance.isAvailable('traffic_in_lim_SatEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[1] && v.traffic[0].rows[1].underTraffic){
-				result['traffic_in_lim_SatEx'] = (v.traffic[0].rows[1].underTraffic / 1000000000).toFixed(1);
-			}
-			if(AnyBalance.isAvailable('traffic_over_lim_SatEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[1] && v.traffic[0].rows[1].overTraffic){
-				result['traffic_over_lim_SatEx'] = (v.traffic[0].rows[1].overTraffic / 1000000000).toFixed(1);
-			}
-			//Трафик внутренний украинский
-			if(AnyBalance.isAvailable('traffic_UkrEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[0] && v.traffic[0].rows[0].traffic){
-				result['traffic_UkrEx'] = (v.traffic[0].rows[0].traffic / 1000000000).toFixed(1);
-			}
-			if(AnyBalance.isAvailable('traffic_in_lim_UkrEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[0] && v.traffic[0].rows[0].underTraffic){
-				result['traffic_in_lim_UkrEx'] = (v.traffic[0].rows[0].underTraffic / 1000000000).toFixed(1);
-			}
-			if(AnyBalance.isAvailable('traffic_over_lim_UkrEx') && v.traffic && v.traffic[0] && v.traffic[0].rows && v.traffic[0].rows[0] && v.traffic[0].rows[0].overTraffic){
-				result['traffic_over_lim_UkrEx'] = (v.traffic[0].rows[0].overTraffic / 1000000000).toFixed(1);
-			}
-			
-			AnyBalance.setResult(result);
-		} else if (resst == 'bad login'){
-			throw new AnyBalance.Error('Неправильный логин или пароль');
-		} else {
-			throw new AnyBalance.Error('Неизвестный ответ сервера: ' + resst);
-		}
-	} else {
-		throw new AnyBalance.Error('Неизвестная ошибка');
+	var baseurl = 'https://my.volia.com/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.pass, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'kiev/ru/faq', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
+
+	var form = getParam(html, null, null, /<form[^>]+id="userLoginForm"[^>]+>[^]+?<\/form>/i);
+	
+	var params = createFormParams(form, function(params, str, name, value) {
+		AnyBalance.trace(name + ' = ' + value);
+		if (name == 'username') 
+			return prefs.login;
+		else if (name == 'password')
+			return prefs.pass;
+
+		return value;
+	});
+	
+	html = AnyBalance.requestPost(baseurl + 'kiev/ru/signin', params, addHeaders({
+		Referer: baseurl + 'kiev/ru/faq',
+		'X-Requested-With':'XMLHttpRequest'
+	}));
+
+	var redirect = AnyBalance.getLastResponseHeader('X-Success-Url');
+
+	if(!redirect){
+		if(AnyBalance.getLastStatusCode() == 403)
+			throw new AnyBalance.Error('Не перный логин или пароль.', null, true);	
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+
+	html = AnyBalance.requestGet(baseurl + redirect, g_headers);	
+	
+	if (!/signout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Текущий баланс[^]+?<div/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /\/signout[^>]*>\s*<\/a>\s*<\/div>\s*<span[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'amountToPay', /name="amountToPay"[^>]*value="([^"]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /№ Договора[^>]*>\s*<span[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
 }

ab0ffe89dcb71ad788940d69d62d76cb9efb33b8
igoloviznin@gmail.com <igoloviznin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-03-25 09:05:21 +0000

    Воля. Изменения связанные с вводом нового кабинета м закрытия старого сайта.

--- trunk/ab-internet-volia-broadband/main.js
+++ trunk/ab-internet-volia-broadband/main.js
@@ -7,26 +7,26 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36',
 };
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'https://my.volia.com/';
 	AnyBalance.setDefaultCharset('utf-8');
-	
+
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.pass, 'Введите пароль!');
-	
+
 	var html = AnyBalance.requestGet(baseurl + 'kiev/ru/faq', g_headers);
-	
+
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 
 	var form = getParam(html, null, null, /<form[^>]+id="userLoginForm"[^>]+>[^]+?<\/form>/i);
-	
+
 	var params = createFormParams(form, function(params, str, name, value) {
 		AnyBalance.trace(name + ' = ' + value);
 		if (name == 'username') 
@@ -36,7 +36,7 @@ function main(){
 
 		return value;
 	});
-	
+
 	html = AnyBalance.requestPost(baseurl + 'kiev/ru/signin', params, addHeaders({
 		Referer: baseurl + 'kiev/ru/faq',
 		'X-Requested-With':'XMLHttpRequest'
@@ -51,22 +51,27 @@ function main(){
 	}
 
 	html = AnyBalance.requestGet(baseurl + redirect, g_headers);	
-	
+
 	if (!/signout/i.test(html)) {
 		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
 			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
-		
+
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
-	
+
 	var result = {success: true};
-	
+
+	getParam(html, result, '__tariff', /<div class="packageDescrption ">\s*<p>\s*<span>[^<]*<\/span>\s*<span class=[^>]*>\s*<img [^>]*>\s*<span>[^>]*<\/span>\s*<\/span>\s*<\/p>\s*<span class=[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'balance', /Текущий баланс[^]+?<div/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'fio', /\/signout[^>]*>\s*<\/a>\s*<\/div>\s*<span[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'amountToPay', /name="amountToPay"[^>]*value="([^"]+)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, '__tariff', /№ Договора[^>]*>\s*<span[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-	
+	getParam(html, result, 'ndogovor', /№ Договора[^>]*>\s*<span[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /\/signout[^>]*>\s*<\/a>\s*<\/div>\s*<span[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'kodtarif', /cockpit\/services\/channel\/compare" id="(\d+)"/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'kkan', /Телевидение\s*<\/b>\s*<span>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'speed', /Интернет\s*<\/b>\s*<span>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'ktel', /tvCounter">([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+
 	AnyBalance.setResult(result);
 }

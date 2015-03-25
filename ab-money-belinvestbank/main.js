9f6e0c03cd79772e7c6f4562d0667dcb6e5f4de5
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-03-25 10:35:21 +0000

    Белинвестбанк (Беларусь) (ab-money-belinvestbank): - Поддержка изменений на сайте.

--- ab-money-belinvestbank/main.js
+++ ab-money-belinvestbank/main.js
@@ -16,7 +16,7 @@ function main() {
 	
 	checkEmpty(prefs.login, "Введите логин!");
 	checkEmpty(prefs.password, "Введите пароль!");
-	checkEmpty(prefs.keyword, "Введите ключевое слово!");
+	checkEmpty(!prefs.cardnum || /^\d{4}$/i.test(prefs.cardnum), 'Необходимо ввести 4 последние цифры номера карты, либо не вводить ничего!');
 		
 	var html = AnyBalance.requestGet(baseurl + 'signin', g_headers);
 
@@ -25,19 +25,35 @@ function main() {
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 
-	html = AnyBalance.requestPost(baseurl + 'signin', {
-        login:prefs.login,
-        keyword:prefs.keyword,
-    }, addHeaders({Referer: baseurl + 'signin'}));
-	
 	var encryptArrayVar = getParam(html, null, null, /var\s*keyLang\s*=\s*\[([^\]]*)/i);
-	checkEmpty(encryptArrayVar, "Не нашли ключ шифрования, свяжитесь с разработчиком", true);
 	var encryptArray = sumParam(encryptArrayVar, null, null, /\d+/ig);
-	
+
+	html = AnyBalance.requestPost(baseurl + 'signin', {
+        login: prefs.login,
+        password: cod(prefs.password, encryptArray)
+    }, addHeaders({Referer: baseurl + 'signin'}));
+
+	if(!/на ваш сеансовый номер телефона/i.test(html)){
+		var error = getParam(html, null, null, /<div[^>]+id="error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		if(error)
+			throw new AnyBalance.Error(error, null, /Пароль введен неверно|Имя пользователя или пароль введены неверно/i.test(error));
+	}
+
+	var smsKey;
+	if(AnyBalance.getLevel() >= 7){
+		AnyBalance.trace('Пытаемся ввести смс код.');
+		smsKey = AnyBalance.retrieveCode("Пожалуйста, введите код из смс");
+		AnyBalance.trace('Код из смс получен: ' + smsKey);
+	} else {
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}
+
 	html = AnyBalance.requestPost(baseurl + 'signin2', {
-        password:cod(prefs.password, encryptArray),
+        action: 1,
+        key: smsKey
     }, addHeaders({Referer: baseurl + 'signin2'}));
-	
+
+	// ПРОВЕРКА НА НЕВЕРНЫЙ КОД ИЗ СМС!
 	if(!/logout/i.test(html)) {
 		var error = getParam(html, null, null, /class="attention"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
 		if(error)
@@ -46,35 +62,34 @@ function main() {
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 	
-	html = AnyBalance.requestGet(baseurl + 'cards', addHeaders({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',}));
+	html = AnyBalance.requestGet(baseurl + 'cards', addHeaders({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'}));
 	
-	var table = getParam(html, null, null, /(<table[^>]*class="datatable"[\s\S]*?<\/table>)/i);
-	checkEmpty(table, "Не нашли ни одной карты.", true);
-	
-	if(prefs.cardnum && !/^\d{4}$/i.test(prefs.cardnum)) {
-		throw new AnyBalance.Error('Необходимо ввести 4 последние цифры номера карты, либо не вводить ничего!');
-	}
-	
-	var tr = getParam(table, null, null, new RegExp('<tr\\s+id(?:[\\s\\S](?!</tr>))*?(?:\\*\\*\\*\\*\\s+){3}'+ (prefs.cardnum || '\\d{4}') +'[\\s\\S]*?</tr>', 'i'));
-	if(!tr) {
-		throw new AnyBalance.Error('Не удалось найти ' + (prefs.cardnum ? 'карту с последними цифрами ' + prefs.cardnum : 'ни одной карты!'));
-	}
-	
+	var cards = sumParam(html, null, null, /<table class="table-name-card">[^]+?<\/table>/ig);
+
+	if(!cards.length)
+		throw new AnyBalance.Error('Не удалось найти ни одной карты!');
+
+	var card = prefs.cardnum ?
+			cards.filter(function(card){ return new RegExp('\\*\\*\\*\\*\\s*' + prefs.cardnum, 'i').test(card); })[0] : cards[0];
+
+	checkEmpty(card, AnyBalance.Error('Не удалось найти' + prefs.cardnum ? ' карту с последними цифрами' + prefs.cardnum : ' ни одной карты!'));
+
     var result = {success: true};
 	
-	getParam(tr, result, '__tariff', /(\*\*\*\*\s*\*\*\*\*\s*\*\*\*\*\s*\d{4})/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(tr, result, 'balance', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(tr, result, 'currency', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(tr, result, 'validto', /Срок действия:([^<]*)/i, replaceTagsAndSpaces, parseDate);
-	getParam(tr, result, 'status', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(card, result, '__tariff', /(\*\*\*\*\s*\*\*\*\*\s*\*\*\*\*\s*\d{4})/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(card, result, 'balance', /card-sum[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(card, result, 'currency', /item-card-currency[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(card, result, 'validto', /item-card-expdate[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseDate);
+	getParam(card, result, 'status', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+
+	// Выходим, чтобы закончить сессию. Нужно, так как запрещено 2 одновременных подключения.
+	AnyBalance.requestGet(baseurl + 'logout', g_headers);
 	
     AnyBalance.setResult(result);
 }
 
 function cod(str, keyLang) {
-	var lang = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0','_','.','-'];
-	// Этот аррей динамический, надо его передавать сюда
-	//var keyLang = ['48','117','121','119','66','88','89','78','57','45','71','99','80','65','87','75','52','113','84','101','85','102','95','103','76','46','53','98','100','116','56','105','86','77','109','81','106','110','54','120','82','90','118','97','111','50','114','73','67','49','51','79','68','72','115','74','70','122','108','107','69','55','112','104','83'];
+	var lang = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Й','Ц','У','К','Е','Н','Г','Ш','Щ','З','Х','Ъ','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Я','Ч','С','М','И','Т','Ь','Б','Ю','й','ц','у','к','е','н','г','ш','щ','з','х','ъ','ф','ы','в','а','п','р','о','л','д','ж','э','я','ч','с','м','и','т','ь','б','ю','1','2','3','4','5','6','7','8','9','0','_','.','-'];
 	var result = '';
 	var pass = str.split('');
 	for (n = 0; n < pass.length; n++) {

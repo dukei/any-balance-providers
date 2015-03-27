5a08f942d2b7344eb7e16f488c2f2753d6e94a0e
julmatina <julmatina@gmail.com>
2015-03-27 11:48:37 +0000

    Инфолинк (ab-internet-infolink): - Добавлены счетчики ФИО и тариф

--- ab-internet-infolink/main.js
+++ ab-internet-infolink/main.js
@@ -22,8 +22,8 @@ function main(){
 	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
 
 	// HttpOnly cookie
-	var cookie = AnyBalance.getLastResponseHeader('Set-Cookie'),
-		session = getParam(cookie, null, null, /_lk_session=([^;]+)/i);
+	var cookie = AnyBalance.getLastResponseHeader('Set-Cookie');
+	var session = getParam(cookie, null, null, /_lk_session=([^;]+)/i);
 	AnyBalance.setCookie('user.infolink.ru', '_lk_session', session);
 
 	var params = createFormParams(html, function(params, str, name, value) {
@@ -44,10 +44,15 @@ function main(){
     }
     var result = {success: true};
 	
-	getParam(html, result, '__tariff', /(Интернет[^>]*>[^>]*href="\/services\/list[^>]*>[^>]*>[^>]*>)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'acc_num', /ID абонента[^]+?(\d+)\s+<span class=['"]caret['"]>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'balance', /Баланс:[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'acc_num', /ID абонента[^]+?(\d+)\s+<span class=['"]caret['"]>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'fio', /user\/info[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'bonuses', /Бонус[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+    
+    html = AnyBalance.requestGet(baseurl + 'services/index', g_headers);
+    
+	getParam(html, result, '__tariff', /Текущий тариф(?:[^>]*>){1}([\s\S]*?)<\/div/i, replaceTagsAndSpaces, html_entity_decode);
+    
 	// Посчитаем дату отключения, чтобы можно было назначить нотиф на нее
 	// if(isAvailable('deadline')){
 	// 	var days = getParam(html, null, null, /До отключения:\s*(\d+)\s*дн/i, null, parseBalance);

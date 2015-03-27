800990eade918efa13711b47aba7ca7412b74e78
stark027 <stark027@gmail.com>
2015-03-27 08:07:09 +0000

    Яндекс.Деньги (ab-money-yandex): - Поддержка изменений на сайте.

--- ab-money-yandex/main.js
+++ ab-money-yandex/main.js
@@ -12,7 +12,6 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	
 	checkEmpty(prefs.login, 'Введите логин в Яндекс.Деньги!');
 	checkEmpty(prefs.password, 'Введите пароль, используемый для входа в систему Яндекс.Деньги. Не платежный пароль, а именно пароль для входа!');
 	
@@ -31,35 +30,22 @@ function main() {
 	
 	getParam(html, result, '__tariff', /Номер кошелька(?:[^>]*>){2}(\d{10,20})/i, replaceTagsAndSpaces);
 	getParam(result['__tariff'], result, 'number');
-	getParam(html, result, 'balance', /b-sum__amount[^>]*>\s*([-\d,.]+)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-
-	// if(/sum__amount[^>]*>\s*\*{3}/i.test(html)) {
-	//     AnyBalance.trace('Сумма спрятана. Будем пытаться найти...');
-	// 	var text = AnyBalance.requestGet(baseurl + "tunes.xml", g_headers);
-	// 	var sk = getParam(text, null, null, /name="sk"[^>]*value="([^"]+)/i, replaceTagsAndSpaces);
-	// 	if(!sk){
-	// 		AnyBalance.trace(html);
-	// 		throw new AnyBalance.Error('Не удаётся найти ключ для получения баланса! Сайт изменен?');
-	// 	}
+	
+	if(/sum__amount[^>]*>\s*\*{3}/i.test(html)) {
+	    AnyBalance.trace('Сумма спрятана. Будем пытаться найти...');
+		var text = AnyBalance.requestGet(baseurl + "tunes.xml", g_headers);
+		var sk = getParam(text, null, null, /name="sk"[^>]*value="([^"]+)/i, replaceTagsAndSpaces);
+		if(!sk){
+			AnyBalance.trace(html);
+			throw new AnyBalance.Error('Не удаётся найти ключ для получения баланса! Сайт изменен?');
+		}
 		
-	// 	text = AnyBalance.requestGet(baseurl + "internal/index-ajax.xml?action=updateSumVisibility&sk=" + sk + "&showSum=1", addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));
-	// 	var json = getJson(text);
-	//     getParam('' + json.sum, result, 'balance', null, null, parseBalance);
-	// } else {
-	//     getParam(html, result, 'balance', /balance[^>]*button(?:[^>]*>){3}[^>]*amount[^>]*>([\s\S]*?)<d/i, replaceTagsAndSpaces, parseBalance);
-	// }
+		text = AnyBalance.requestGet(baseurl + "internal/index-ajax.xml?action=updateSumVisibility&sk=" + sk + "&showSum=1", addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));
+		var json = getJson(text);
+	    getParam('' + json.sum, result, 'balance', null, null, parseBalance);
+	} else {
+	    getParam(html, result, 'balance', /b-sum__amount[^>]*>\s*([-\d,.]+)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+	}
 	
 	AnyBalance.setResult(result);
-}
-
-function parseBalanceRK(_text){
-    var text = _text.replace(/\s+/g, '');
-    var rub = getParam(text, null, null, /([\s\S]*)руб/i, replaceTagsAndSpaces, parseBalance);
-    var kop = getParam(text, null, null, /((?:[\s\S](?!руб))*)коп/i, [/руб\.?/i, '', replaceTagsAndSpaces], parseBalance);
-    var val;
-    if(isset(rub) || isset(kop)){
-    	val = (rub || 0) + (kop || 0)/100;
-    }
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + _text);
-    return val;
 }

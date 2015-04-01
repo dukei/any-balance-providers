da9d71e298bbd89158146106ec3b2b7a1ff3ef43
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-04-01 13:05:19 +0000

    Новый провайдер: ЛигаЛинк (ab-internet-ligalink)

---
+++ ab-internet-ligalink/main.js
@@ -0,0 +1,55 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://www.ligalink.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+
+	var loginURL = getParam(html, null, null, /form-login[^>]*action="([^"]+)/i);
+	
+	html = AnyBalance.requestPost(baseurl + loginURL, {
+		login: prefs.login,
+		password: prefs.password,
+		cmd: 'login'
+	}, addHeaders({Referer: baseurl}));
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Баланс счета\s*<\/td>\s*<td[^>]*>([^]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'status', /Состояние<\/td>\s*<td[^>]*>([^]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'fio', /Здравствуйте,\s*([^<!]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'bonus', /Бонусы<\/td>\s*<td[^>]*>([^]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, '__tariff', /Услуги\s*<\/td>\s*<td[^>]*>([^]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'tariffCost', /Стоимость\s*<\/td>\s*<td[^>]*>([^]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

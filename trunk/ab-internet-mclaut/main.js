8e9b8a66895e974aa24e37c5944fb2d278697ec5
kirillpisarev23@gmail.com <kirillpisarev23@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-04-03 13:07:48 +0000

    McLaut (Черкассы) (ab-internet-mclaut): - Поддержка изменений на сайте.

--- trunk/ab-internet-mclaut/main.js
+++ trunk/ab-internet-mclaut/main.js
@@ -1,49 +1,66 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Текущий баланс у черкасского оператора интернет McLaut 
-
-Сайт оператора: http://mclaut.com
-Личный кабинет: https://bill.mclaut.com (закрыт для доступа извне)
 */
+
+var g_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "https://bill.mclaut.com/index.php";
+    var baseurl = "https://cherkassy.mclaut.com/";
 
-    var html = AnyBalance.requestPost(baseurl + '?module=SYS_users&func=login', {
-        login:'Вход',
-        user_name:prefs.login,
-        user_pass:prefs.password
-    });
+    checkEmpty(prefs.login, 'Введите логин!');
+    checkEmpty(prefs.password, 'Введите пароль!');
+    
+    var html = AnyBalance.requestGet(baseurl, g_headers);
+    
+    if(!html || AnyBalance.getLastStatusCode() > 400){
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+    }
 
-    if(!/Вход выполнен/i.test(html)){
+    var html = AnyBalance.requestPost(baseurl + 'index.php', {
+        query: 'ajax',
+        app: 'frontend',
+        module: 'user',
+        action: 'clientLogIn',
+        login: prefs.login,
+        pass: prefs.password,
+        city: 1,
+        lang: 'ru'
+    }, addHeaders({ Referer: baseurl, 'X-Requested-With': 'XMLHttpRequest' }));
+
+    if(!html)
+        throw new AnyBalance.Error('Неверный логин или пароль или сайт изменен.', null, true);
+    
+    if(html != '1')
+        throw new AnyBalance.Error('Неизвестная ошибка. Сайт изменен?');
+
+    html = AnyBalance.requestGet(baseurl, g_headers);
+
+    if(!/logout/i.test(html)){
         var error = getParam(html, null, null, /403 Forbidden/i);
         if(error)
             throw new AnyBalance.Error('Личный кабинет доступен только из сети McLaut. Установите WiFi соединение с домашним роутером.');
-        error = getParam(html, null, null, /<span[^>]*style="[^"]*bold[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 
-    html = AnyBalance.requestGet(baseurl + '?module=SYS_users');
-
-    var href = getParam(html, null, null, /<a[^>]+href="\/index.php([^"]*client_view[^"]*)/i, null, html_entity_decode);
-    if(!href)
-        throw new AnyBalance.requestGet('Не удаётся найти ссылку на информацию. Сайт изменен?');
-
-    html = AnyBalance.requestGet(baseurl + href);
-
     var result = {success: true};
 
-    getParam(html, result, 'status', /<td[^>]*>\s*Активен[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'fio', /<td[^>]*>\s*Имя[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'licschet', /Номер лицевого счета[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'balance', /<td[^>]*>\s*Баланс[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, function(str){var val = parseBalance(str); return val && Math.round(val*100)/100;});
-    getParam(html, result, '__tariff', /Пользователи клиента(?:(?:[\s\S](?!<\/table>))*?<td[^>]*>){15}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'status', /<td[^>]*>\s*Активен[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'fio', /<td[^>]*>\s*ФИО[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /<td[^>]*>\s*Номер лицевого счета[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /<td[^>]*>\s*Логин[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    
+
 
     AnyBalance.setResult(result);
 }

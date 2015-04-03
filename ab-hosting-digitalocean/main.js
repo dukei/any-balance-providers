77beadf1539afe6da1f60cb972ee29c642aad81b
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-04-03 07:38:11 +0000

    DigitalOcean (ab-hosting-digitalocean): - Поддержка изменений на сайте.

--- ab-hosting-digitalocean/main.js
+++ ab-hosting-digitalocean/main.js
@@ -1,25 +1,46 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Информация о балансе у хостинга Digital Ocean.
-
-Сайт оператора: https://www.digitalocean.com/
-Личный кабинет: https://cloud.digitalocean.com/
 */
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
+
+	checkEmpty(prefs.email, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
 
 	AnyBalance.setDefaultCharset('utf-8');
 
 	var baseurl = 'https://cloud.digitalocean.com';
 
-	var html = AnyBalance.requestGet(baseurl + '/login');;	
-	html = AnyBalance.requestPost(baseurl + '/sessions', {
-		'user[email]': prefs.email,
-		'user[password]': prefs.password,
-		'user[redirect]': ''
+	var html = AnyBalance.requestGet(baseurl + '/login', g_headers);
+
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'user[email]') 
+			return prefs.email;
+		else if (name == 'user[password]')
+			return prefs.password;
+
+		return value;
 	});
+
+	html = AnyBalance.requestPost(baseurl + '/sessions', params, g_headers);
+
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<ul[^>]+class=['"]notice errors['"][^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Invalid email or password/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
 
 	var status = AnyBalance.getLastStatusCode();
 
@@ -27,19 +48,18 @@ function main(){
 		throw new AnyBalance.Error('Ошибка авторизации ');
 	}
 
-	html = AnyBalance.requestGet(baseurl + '/billing');
+	html = AnyBalance.requestGet(baseurl + '/settings/billing', g_headers);
 
 	status = AnyBalance.getLastStatusCode();
 
-	if( status != 200){
+	if(status != 200){
 		throw new AnyBalance.Error('Ошибка получения данных');
 	}
-	
 
 	var result = {success: true};
 
-	getParam(html, result, 'balance', /<h3 class='credit'>\s*\$([\d+\.]+)/i, null, parseBalance);
-	getParam(html, result, 'usage', /<h3 class='outstanding'>\s*\$([\d+\.]+)/i, null, parseBalance);
+	getParam(html, result, 'balance', /<div class='amount credit'>\s*\$([\d+\.]+)/i, null, parseBalance);
+	getParam(html, result, 'usage', /<div class='amount zero'>\s*\$([\d+\.]+)/i, null, parseBalance);
 	AnyBalance.setResult(result);
 }
 

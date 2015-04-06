cee026daadea47d8606ff7d0373d48d8afa698da
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-04-06 09:25:34 +0000

    Наличный курс валют Украины (ab-exchange-nalu): - Поддержка изменений на сайте.

--- ab-exchange-nalu/main.js
+++ ab-exchange-nalu/main.js
@@ -2,9 +2,10 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
-function getRate(result, info, currency) {
+function getRate(result, html, currency) {
 	// $1 - покупка $2 - продажа
-	var match = new RegExp('"currency_'+currency+'"(?:[^>]*>){2}[^>]*bid_value[^>]*>([^<]+)(?:[^>]*>){6}([^<]+)', 'i').exec(info);
+	var match = new RegExp(currency + '<\\/th>\\s*<td[^>]*class="bid_hide[^>]*>([^<]+)(?:[^>]*>){8}([^<]+)', 'i').exec(html);
+	//var match = new RegExp('"currency_'+currency+'"(?:[^>]*>){2}[^>]*bid_value[^>]*>([^<]+)(?:[^>]*>){6}([^<]+)', 'i').exec(html);
 	if(!match)
 		throw new AnyBalance.Error("Не удалось получить данные, свяжитесь с разработчиком.");
 	
@@ -16,12 +17,12 @@ function main() {
 	if (AnyBalance.getLevel() < 5)
 		throw new AnyBalance.Error("Для этого провайдера необходимо AnyBalance API v.5+");
 
-	var info = AnyBalance.requestGet('http://banker.ua/marketindex/course/');
+	var html = AnyBalance.requestGet('http://banker.ua/marketindex/course/');
 	var result = {success: true};
 	
-	getRate(result, info, 'USD');
-	getRate(result, info, 'EUR');
-	getRate(result, info, 'RUB');
+	getRate(result, html, 'USD');
+	getRate(result, html, 'EUR');
+	getRate(result, html, 'RUB');
 	
 	AnyBalance.setResult(result);
 }

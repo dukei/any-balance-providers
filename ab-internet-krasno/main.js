63c7a47dca5c1f17f733d8274e429d2906433dda
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-04-06 06:47:41 +0000

    Диалог-К (Красноармейск, МО) (ab-internet-krasno): - Поддержка изменений на сайте.

--- ab-internet-krasno/main.js
+++ ab-internet-krasno/main.js
@@ -1,56 +1,23 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Текущий баланс у оператора интернет Диалог-К (Красноармейск, Моск. обл.)
-
-Сайт оператора: http://krasno.ru
-Личный кабинет: https://billing.krasno.ru/login.php
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/Задолженность/i, '-', /\s+/g, '', /,/g, '.'];
-
-function parseTrafficGb(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    val = Math.round(val/1024*100)/100; //Перевели в Гб с двумя знаками после запятой
-    AnyBalance.trace('Parsing traffic (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
+var g_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Connection': 'keep-alive',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-
     var baseurl = "https://billing.krasno.ru/";
-    
-    var html = AnyBalance.requestGet(baseurl + 'login.php');
+
+    checkEmpty(prefs.login, 'Введите логин!');
+    checkEmpty(prefs.password, 'Введите пароль!');
+
+    var html = AnyBalance.requestGet(baseurl + 'login.php', g_headers);
     var login_name = getParam(html, null, null, /(_auth_username\w+)/i);
     var pass_name = getParam(html, null, null, /(_auth_password\w+)/i);
 
@@ -61,29 +28,29 @@ function main(){
     params[login_name] = prefs.login;
     params[pass_name] = prefs.password;
 
-    var html = AnyBalance.requestPost(baseurl + 'info.php', params);
+    var html = AnyBalance.requestPost(baseurl + 'info.php', params, g_headers);
 
-    var error = getParam(html, null, null, /(Вы вошли в систему)/i, replaceTagsAndSpaces, html_entity_decode);
-    if(!error)
-        throw new AnyBalance.Error("Не удалось войти. Проверьте логин и пароль.");
+    if (!/logout/i.test(html)) {
+        var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+        if (error)
+            throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+
+        error = getParam(html, null, null, /Неправильное имя пользователя или пароль/i);
+        if(error)
+            throw new AnyBalance.Error(error, null, true);
+
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
 
     var result = {success: true};
 
     getParam(html, result, 'userName', /Абонент:([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'licschet', /Лицевой счет[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /Тарифный план[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /Актуальный баланс[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'trafficLeft', /Остаток оплаченного трафика[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
-    getParam(html, result, 'block', /Блокировка[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'licschet', /Лицевой счет\s*<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Тарифный план\s*<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /Актуальный баланс\s*<\/b>\s*<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'trafficLeft', /Остаток предоплаченного трафика\s*<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+    getParam(html, result, 'block', /Блокировка лицевого счета\s*<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

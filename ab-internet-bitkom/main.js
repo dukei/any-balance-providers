12d6d915ecb68826eaf80fb492ad1a6ac5ca8dc3
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-04-06 09:58:33 +0000

    БитКом (ab-internet-bitkom): - Поддержка изменений на сайте.

--- ab-internet-bitkom/main.js
+++ ab-internet-bitkom/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://stat.bit-com.ru/';
+	var baseurl = 'https://stat.bit-com.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');

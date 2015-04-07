56e757405820d01133fea9b107ee2f99d47b2aa6
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-04-07 13:39:50 +0000

    ALTEL 4G (Казахстан) (ab-cell-altel4g): - Поддержка изменений на сайте.

--- ab-cell-altel4g/main.js
+++ ab-cell-altel4g/main.js
@@ -38,8 +38,8 @@ function main(){
     getParam(html, result, 'fio', /"account-title"[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'phone', /(?:Ваш номер|Сіздің нөміріңіз):([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, '__tariff', /"plan-title"[^>]*>([^<]+)[^>]*>[^>]*plan-subtitle/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'balance', /"account-balance split">[\s\S]*?"split-title"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'bonus', /"bonus-balance split">[\s\S]*?"split-title"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance', /"account-balance split"[\s\S]*?"split-title"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'bonus', /"bonus-balance split"[\s\S]*?"split-title"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'till', /бонусный счет[^>]*>\s*до([^<]+)/i, replaceTagsAndSpaces, parseDate);
 
 	var counters = sumParam(html, null, null, /<div[^>]+class="[^"]*\bcounter\b[^>]*>([\s\S]*?)<\/div>/ig);

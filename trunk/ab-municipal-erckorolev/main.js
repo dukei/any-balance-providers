d9afc1dbadba05c07e917ab99119f76d269021fa
kirillpisarev23@gmail.com <kirillpisarev23@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-04-07 14:07:05 +0000

    Новый провайдер: Королевский расчетно-кассовый центр (АНО КРКЦ) (ab-municipal-erckorolev)

---
+++ trunk/ab-municipal-erckorolev/main.js
@@ -0,0 +1,65 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://erc-korolev.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + '?id=cabinet', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	var captchaKey, captchaSrc, captcha;
+	if(AnyBalance.getLevel() >= 7){
+		AnyBalance.trace('Пытаемся ввести капчу');
+		captchaSrc = getParam(html, null, null, /src="(cabinet\/mod\/kcaptcha\/[^"]+)/i);
+		captcha = AnyBalance.requestGet(baseurl + captchaSrc);
+		if(!captcha)
+			throw new AnyBalance.Error('Не удалось получить капчу! Попробуйте обновить данные позже.');
+		captchaKey = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaKey);
+	} else {
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + '?id=cabinet', {
+		ls: prefs.login,
+		pwd: prefs.password,
+		keystring: captchaKey,
+		action: 'CheckLoginPwd',
+		authradio: 'abonent',
+	}, addHeaders({Referer: baseurl + '?id=cabinet'}));
+	
+	if (!/exit/i.test(html)) {
+		var error = getParam(html, null, null, /<p[^>]+style="color: red"[^>]*>\s*<b>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Лицевой счет не существует, либо пароль указан не верно/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+
+	getParam(html, result, 'account', /Номер счета<\/td>\s*<td[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'fio', /Наниматель\s*\/\s*собственник<\/td>\s*<td[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'debt', /Итого к оплате<\/td>\s*<td[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

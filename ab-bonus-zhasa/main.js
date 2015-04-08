d02089541c32689bb95754acc1e83f30899a4b92
kirillpisarev23 <kirillpisarev23@gmail.com>
2015-04-08 08:26:13 +0000

    Новый провайдер: Жаса (ab-bonus-zhasa)

---
+++ ab-bonus-zhasa/main.js
@@ -0,0 +1,50 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://zhasa.kz/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.phone, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'login.do', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + 'login.do', {
+		def: prefs.phone.substr(0, 3),
+		phonePart: prefs.phone.substr(3),
+		login: '7' + prefs.phone,
+		password: prefs.password,
+	}, addHeaders({Referer: baseurl + 'login.do'}));
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<p[^>]+class="errors"[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неправильные логин(телефон) и пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(html, result, 'fio', /Здравствуйте,\s*<strong>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'bonus', /class="balance"[^>]*>\s*<strong>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

1b4793c734286fbf34d1d67626acdae032d03ceb
etamin419 <etamin419@gmail.com>
2015-04-14 16:47:24 +0000

    Added counters, exception Handling

--- ab-internet-StelsCom_MobileTeleCom/main.js
+++ ab-internet-StelsCom_MobileTeleCom/main.js
@@ -1,10 +1,10 @@
 /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-Получает баланс и информацию о тарифе для интернет провайдера Орион телеком 
+Получает баланс и информацию о тарифе для интернет провайдера Мобил ТелеКом Плюс
 
 Site: http://stat.satka.ru/
-Личный кабинет: http://utm.xn--80aa3a0ag.xn--80asehdb/index.php
+Личный кабинет: http://utm.сатка.онлайн/index.php
 */
 
 var g_headers = {
@@ -36,17 +36,25 @@ function main(){
 	}, addHeaders({Referer: baseurl + 'index.php'}));
 
 	if (!/\/\?module=zz_logout/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		var error = sumParam(html, null, null, /<br><p[^>]*>([\s\S]*?)<\/p>/gi, replaceTagsAndSpaces, html_entity_decode, aggregate_join) || '';
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверно указаны логин или пароль/i.test(error));
 		AnyBalance.trace(error);
-		if (error){
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
-			AnyBalance.trace(html);}
-		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 	
 	var result = {success: true};
-	getParam(html, result, 'balance', /Баланс<\/td>*<td*>*<\/td>/i, replaceTagsAndSpaces, parseBalance);
-	AnyBalance.trace(html);
+	getParam(html, result, 'ID', /ID<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'login', /Логин<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'main_expense', /Основной лицевой счет<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'balance', /Баланс<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'credit', /Кредит<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'nds', /НДС<\/td>\s*<td[^>]*>([\s\S]*?)\%<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'date', /Дата создания<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'fio', /ФИО<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'status', /Состояние интернета<\/td>\s*<td[^>]*>([\s\S]*?)<A[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	html = AnyBalance.requestGet(baseurl + '/?module=40_tariffs', g_headers);
+	getParam(html, result, 'TP', /Изменить<\/td>\s*<\/tr>\s*<tr>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 	AnyBalance.setResult(result);
 }

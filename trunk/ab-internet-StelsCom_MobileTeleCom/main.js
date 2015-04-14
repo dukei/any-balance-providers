1fb60c435b5b0cda08ca8ffeee64ebbde9cb5593
etamin419@gmail.com <etamin419@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-04-14 05:02:37 +0000

    fix errors

--- trunk/ab-internet-StelsCom_MobileTeleCom/main.js
+++ trunk/ab-internet-StelsCom_MobileTeleCom/main.js
@@ -1,55 +1,52 @@
-﻿/**
+/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает баланс и информацию о тарифе для интернет провайдера Орион телеком 
+
+Site: http://stat.satka.ru/
+Личный кабинет: http://utm.xn--80aa3a0ag.xn--80asehdb/index.php
 */
 
 var g_headers = {
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
-function main() {
-	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://utm.xn--80aa3a0ag.xn--80asehdb/';
-	AnyBalance.setDefaultCharset('utf-8');
 
-	var xml = AnyBalance.requestPost(baseurl, {
-		user:prefs.login,
-		passwd:prefs.password,
-    }, g_headers);
-	
-	var str_end = -1 ;
-	var str_begin = xml.indexOf( '<div id=info_message>' );
-	if( str_begin != -1 ){
-		str_begin = xml.indexOf('<div class=err_message>Ошибка</div>' , str_begin );
-		if( str_begin != -1 ){
-			str_begin = xml.indexOf( '<div><img src="/img/attention.png" border="0" hspace="10" dir="ltr" alt="Error">' ,str_begin );
-			if( str_begin != -1 ){
-				str_end = xml.indexOf( '</div>' , str_begin );
-				if( str_end != -1 ){
-					var result = {success: true, balance: parseFloat(xml.substring(str_begin+80 , str_end  ))};
-	
-					AnyBalance.setResult(result);
-					return;
-				}
-			}
-		}
-		
-	}
-	
-	str_begin = xml.indexOf( "<TR><TD class='utm-cell'><b>Баланс:</b></TD><TD>" );
-	if( str_begin != -1 ){
-		str_end = xml.indexOf( '&nbsp;',str_begin );
-		if( str_end != -1 ){
-			var result = {success: true, balance: parseFloat(xml.substring(str_begin+44 , str_end))};
-			
-			AnyBalance.setResult(result);
-		}
-	}
+function main(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8'); 
+    var baseurl = 'http://utm.xn--80aa3a0ag.xn--80asehdb/';
+    
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
 
-	if(!AnyBalance.isSetResultCalled()){
-		AnyBalance.trace(xml);
-		throw AnyBalance.Error('Неизвестная ошибка. Сайт изменен?');
+	var html = AnyBalance.requestGet(baseurl + 'index.php', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
+	
+	html = AnyBalance.requestPost(baseurl + 'index.php', {
+		login: prefs.login,
+		password: prefs.password,
+	}, addHeaders({Referer: baseurl + 'index.php'}));
+
+	if (!/\/\?module=zz_logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		AnyBalance.trace(error);
+		if (error){
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+			AnyBalance.trace(html);}
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	getParam(html, result, 'balance', /Баланс<\/td>*<td*>*<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	AnyBalance.trace(html);
+	AnyBalance.setResult(result);
 }

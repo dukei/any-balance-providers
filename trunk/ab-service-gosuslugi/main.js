50b51e12d2bba7ca9dcdca0e42f6e8b9b3787be7
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-05-15 10:37:28 +0000

    Госуслуги (Штрафы, Налоги) (ab-service-gosuslugi): - Поддержка изменений на сайте.

--- trunk/ab-service-gosuslugi/main.js
+++ trunk/ab-service-gosuslugi/main.js
@@ -50,11 +50,18 @@ function main() {
 	if(!isLoggedIn(html)) {
 		html = performRedirect(html);
 		
-		html = AnyBalance.requestPost('https://esia.gosuslugi.ru/idp/authn/UsernamePasswordLogin', {
-			username: formattedLogin,
+		var command = getParam(html, null, null, /new LoginViewModel\((?:[^,]+,){1,2}'([^"']+)'/i);
+		if(!command) {
+			AnyBalance.trace(html);
+			throw new AnyBalance.Error('Не удалось найти идентификатор команды для входа.');
+		}
+		
+		html = AnyBalance.requestPost('https://esia.gosuslugi.ru/idp/login/pwd/do', {
+			login: formattedLogin,
 			password: prefs.password,
 			idType:loginType,
-		}, addHeaders({Referer: 'https://esia.gosuslugi.ru/idp/authn/CommonLogin'}));
+			'command': command
+		}, addHeaders({Referer: 'https://esia.gosuslugi.ru/idp/rlogin?cc=bp'}));
 		
 		//Попытаемся получить ошибку авторизации на раннем этапе. Тогда она точнее.
 		var errorCode = getParam(html, null, null, /authn\.error\.([^"']+)/i);

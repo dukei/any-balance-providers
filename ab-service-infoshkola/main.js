3345186bfb590051503a0a7c4cb9fe9661aae364
stark027 <stark027@gmail.com>
2015-05-20 09:12:55 +0000

    Инфошкола (ab-service-infoshkola): - Поддержка изменений на сайте.

--- ab-service-infoshkola/main.js
+++ ab-service-infoshkola/main.js
@@ -15,31 +15,35 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = 'http://www.infoshkola.net/';
     AnyBalance.setDefaultCharset('utf-8'); 
+	AnyBalance.setOptions({forceCharset: 'windows-1251'});
 	
     var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
 	
 	html = AnyBalance.requestPost(baseurl + 'login', {
         login:prefs.login,
         pass:prefs.password,
-        Submit:''
+        remember:1
     }, addHeaders({Referer: baseurl + 'login'})); 
 	
     if(!/\/Logout/i.test(html)){
         var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
+		
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 	
-	html = AnyBalance.requestGet(baseurl + 'sdpupil/include/auth.php?type=1', g_headers);
-	html = AnyBalance.requestGet(baseurl + 'sdpupil/index.php?page=personal', g_headers);
+	AnyBalance.setOptions({forceCharset: 'utf-8'});
+	
+	html = AnyBalance.requestGet(baseurl + 'sdpupil_pg/index.php?page=personal', g_headers);
 	
     var result = {success: true};
+	
     getParam(html, result, 'balance', /Остаток на счете:([\s\S]*?)руб/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'acc_type', /Тип счета:(?:[\s\S]*?<[^>]*>)([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'dogovor', /Договор:(?:[\s\S]*?<[^>]*>)([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'fio', /Абонент:(?:[\s\S]*?<[^>]*>)([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /Тариф:(?:[\s\S]*?<[^>]*>)([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', />\s*Тариф:([\s\S]*?)<\/span/i, replaceTagsAndSpaces, html_entity_decode);
 	
     AnyBalance.setResult(result);
 }

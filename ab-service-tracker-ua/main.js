43392a022349ede5d6f81423ff8f118a38877224
stark027 <stark027@gmail.com>
2015-05-27 07:30:33 +0000

    GPS Tracker.com.ua (ab-service-tracker-ua): - Первая версия провайдера.

---
+++ ab-service-tracker-ua/main.js
@@ -0,0 +1,80 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://gps-tracker.com.ua/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + '', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + 'login.php', {
+		login: prefs.login,
+		password: prefs.password,
+		'remember': 'savecookies'
+	}, addHeaders({Referer: baseurl + ''}));
+	
+	var json = getJson(html);
+	
+	if (!json.success) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + 'billing/billing_main.php?param=load', {
+		login: prefs.login,
+		password: prefs.password,
+		'remember': 'savecookies'
+	}, addHeaders({
+		'X-Requested-With': 'XMLHttpRequest',
+		'Accept': '*/*',
+		'Referer': baseurl + 'tracker.php'
+	}));
+	
+	json = getJson(html);
+	
+	var result = {success: true};
+	
+	getParam(json.balance + '', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.bonus + '', result, 'bonus', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.discount + '', result, 'discount', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.sum_all + '', result, 'sum_all', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.sum_cur + '', result, 'sum_cur', null, replaceTagsAndSpaces, parseBalance);
+	
+	
+	
+	
+	
+	getParam(html, result, ['currency', 'balance'], /Текущий баланс:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseCurrency);
+	getParam(html, result, 'fio', /Имя абонента:(?:[\s\S]*?<b[^>]*>){1}([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, '__tariff', /Тарифный план:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'phone', /Номер:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'deadline', /Действителен до:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseDate);
+	getParam(html, result, 'status', /Статус:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	AnyBalance.setResult(result);
+}

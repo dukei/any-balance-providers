8c3d797eca4f24705f5ecd0beab3a4088b541668
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-05-27 09:03:50 +0000

    Олди (Бонусы) (ab-bonus-oldi): - Поддержка изменений на сайте.

--- trunk/ab-bonus-oldi/main.js
+++ trunk/ab-bonus-oldi/main.js
@@ -30,15 +30,53 @@ function main(){
         var error = getParam(html, null, null, /<[^>]*class="error"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
+		
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
+	
+	var result = {success: true};
 
-    var result = {success: true};
-	// parseFloat используется намеренно, т.к. value="1.11242e+06" что на самом деле значит 1 112 420
-    getParam(html, result, 'suumazakazov', /Сумма заказов, руб(?:[\s\S]*?<[^>]*>){2}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, parseFloat);
-	getParam(html, result, '__tariff', /№ карты покупателя(?:[\s\S]*?<[^>]*>){2}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'zakazy', /Количество заказов[\s\S]*?Количество заказов<\/span>(?:[\s\S]*?<[^>]*>){1}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'bonus', /Накоплено бонусов(?:[\s\S]*?<[^>]*>){2}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, parseBalance);
+	var select = getParam(html, null, null, /<select\s+name="priceopt_RUB"[\s\S]*?<\/select>/i);
+	var options = {};
+	var selected = 0;
+	if(select) {
+		var match = /<option value="(\d+)"[^>]*>([^<]+)/ig.exec(select);
+		var len = match.length;
+		for (var i = 0; i < len; i++) {
+			options[match[1]] = match[2];
+			
+			if(/selected/i.test(match[0])) 
+				selected = match[1];
+			
+			match = /<option value="(\d+)"[^>]*>([^<]+)/ig.exec(select = select.replace(match[0], ''));
+		}
+	}
+	
+	var card = getParam(html, null, null, /var\s+card\s*=\s*"([^'"]+)"/i);
+	if(card) {
+		AnyBalance.trace('Нашли хэш карты, надо обновить данные...');
+		
+		html = AnyBalance.requestPost(baseurl + 'catalog/main_profile_upd_card_ajax.php', {
+			CARD_NUMBER: card
+		}, addHeaders({Referer: baseurl + 'personal/status/index.php?login=yes'})); 
+
+		// parseFloat используется намеренно, т.к. value="1.11242e+06" что на самом деле значит 1 112 420
+		getParam(html, result, 'suumazakazov', /Сумма заказов(?:[\s\S]*?<[^>]*>){1}[^>]*value="([\d]+)/i, replaceTagsAndSpaces, parseFloat);
+		getParam(html, result, 'zakazy', /Количество заказов<\/span>(?:[\s\S]*?<[^>]*>){1}[^>]*value="([\d]+)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, '__tariff', /#usercard[^(]+\(['"](\d+)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'bonus', /#dbonus[^(]+\(['"](\d+)/i, replaceTagsAndSpaces, parseBalance);
+		
+		selected = getParam(html, null, null, /#priceopt_RUB[^(]+\(['"](\d+)/i);
+	} else {
+		// parseFloat используется намеренно, т.к. value="1.11242e+06" что на самом деле значит 1 112 420
+		getParam(html, result, 'suumazakazov', /Сумма заказов, руб(?:[\s\S]*?<[^>]*>){2}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, parseFloat);
+		getParam(html, result, 'zakazy', /Количество заказов[\s\S]*?Количество заказов<\/span>(?:[\s\S]*?<[^>]*>){1}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, '__tariff', /№ карты покупателя(?:[\s\S]*?<[^>]*>){2}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'bonus', /Накоплено бонусов(?:[\s\S]*?<[^>]*>){2}[^>]*value="([^"]*)/i, replaceTagsAndSpaces, parseBalance);		
+	}
+	
+	// Выберем цену, после обновления данных она может измениться поэтому так заморочено сделано
+	getParam(options[selected], result, 'price');
 	
     AnyBalance.setResult(result);
 }

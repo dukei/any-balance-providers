36859fc6d40824063bc5f814bb2d9aaa271395d1
stark027 <stark027@gmail.com>
2015-06-02 17:53:20 +0000

    Obmenka.kharkov.ua (ab-obmenka-kharkov-ua): - Розовцы полностью изменили сайт.

--- ab-obmenka-kharkov-ua/main.js
+++ ab-obmenka-kharkov-ua/main.js
@@ -3,21 +3,42 @@
 */
 
 function main() {
-	var info = AnyBalance.requestGet('http://obmenka.kharkov.ua');
+  var prefs = AnyBalance.getPreferences();
+  var opt = prefs.checkboxPref;
+
+	var info = AnyBalance.requestGet('https://obmenka.kharkov.ua');
 	
 	if(!info || AnyBalance.getLastStatusCode() > 400)
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	
 	var result = {success: true};
 	
-	getParam(info, result, 'USDpok', /Доллар\/Гривна([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'USDpro', /Доллар\/Гривна([^>]*>){5}/i, replaceTagsAndSpaces, parseBalance);
-	
-	getParam(info, result, 'EURpok', /Евро\/Гривна([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'EURpro', /Евро\/Гривна([^>]*>){5}/i, replaceTagsAndSpaces, parseBalance);
-	
-	getParam(info, result, 'RUBpok', /Рубль\/Гривна([^>]*>){3}/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'RUBpro', /Рубль\/Гривна([^>]*>){5}/i, replaceTagsAndSpaces, parseBalance);
-	
+	if (opt == true) {
+	 AnyBalance.trace('opt=true');
+	   if(matches = info.match(/amountWholesaleFrom([^>]){7}/g)){
+                        result.USDpok = parseBalance(matches[0]);
+                        result.EURpok = parseBalance(matches[1]);
+                        result.RUBpok = parseBalance(matches[2]);
+                        }
+        if(matches = info.match(/amountWholesaleTo([^>]){7}/g)){
+                        result.USDpro = parseBalance(matches[0]);
+                        result.EURpro = parseBalance(matches[1]);
+                        result.RUBpro = parseBalance(matches[2]);
+                        }                
+                     }
+	else {
+	   if(matches = info.match(/amountRetailFrom([^>]){7}/g)){
+                        result.USDpok = parseBalance(matches[0]);
+                        result.EURpok = parseBalance(matches[1]);
+                        result.RUBpok = parseBalance(matches[2]);
+                        }
+        if(matches = info.match(/amountRetailTo([^>]){7}/g)){
+                        result.USDpro = parseBalance(matches[0]);
+                        result.EURpro = parseBalance(matches[1]);
+                        result.RUBpro = parseBalance(matches[2]);
+                        }                
+                     }				 
+
 	AnyBalance.setResult(result);
+	
 }

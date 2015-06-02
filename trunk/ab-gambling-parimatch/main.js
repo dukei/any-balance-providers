5fdb8146b6dc273d68fd3cc694b29ba4f20a990e
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-06-02 14:16:56 +0000

    Parimatch (ab-gambling-parimatch): - Поддержка изменений на сайте.

--- trunk/ab-gambling-parimatch/main.js
+++ trunk/ab-gambling-parimatch/main.js
@@ -1,18 +1,13 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс на счету сайта Parimatch 
-
-Operator site: http://www.parimatch.com/
-Личный кабинет: https://www.parimatch.com/
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
 function main(){
@@ -20,7 +15,7 @@ function main(){
 
     var baseurl = "https://www.parimatch.com/";
 
-    AnyBalance.setDefaultCharset('WINDOWS-1251'); 
+    AnyBalance.setDefaultCharset('utf-8'); 
 
     var html = AnyBalance.requestPost(baseurl + '?login=1', {
         username:prefs.login,

c929bc417455c259a6619284f1fbe27a5b6071ab
stark027 <stark027@gmail.com>
2015-06-05 13:21:56 +0000

    Emex.ru (ab-service-emex): - Поддержка изменений на сайте.

--- ab-service-emex/main.js
+++ ab-service-emex/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://www.emex.ru/';
+	var baseurl = 'https://www.emex.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');

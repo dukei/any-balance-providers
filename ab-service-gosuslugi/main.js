1afc394e255c2c4c84b612092c1e4f1fecd6e9a0
stark027 <stark027@gmail.com>
2015-06-05 11:32:06 +0000

    Госуслуги (Штрафы, Налоги) (ab-service-gosuslugi): - Госуслуги опять изменили форму входа.

--- ab-service-gosuslugi/main.js
+++ ab-service-gosuslugi/main.js
@@ -52,25 +52,25 @@ function main() {
 		
 		// Госуслуги издеваются. Сначала выкатили новую форму входа, потом спрятали
 		// Пока используем старую
-		html = AnyBalance.requestPost('https://esia.gosuslugi.ru/idp/authn/UsernamePasswordLogin', {
-			username: formattedLogin,
-			password: prefs.password,
-			idType:loginType,
-		}, addHeaders({Referer: 'https://esia.gosuslugi.ru/idp/authn/CommonLogin'}));
-		
-		// А новую оставим на всякий
-		// var command = getParam(html, null, null, /new LoginViewModel\((?:[^,]+,){1,2}'([^"']+)'/i);
-		// if(!command) {
-			// AnyBalance.trace(html);
-			// throw new AnyBalance.Error('Не удалось найти идентификатор команды для входа.');
-		// }
-		
-		// html = AnyBalance.requestPost('https://esia.gosuslugi.ru/idp/login/pwd/do', {
-			// login: formattedLogin,
+		// html = AnyBalance.requestPost('https://esia.gosuslugi.ru/idp/authn/UsernamePasswordLogin', {
+			// username: formattedLogin,
 			// password: prefs.password,
 			// idType:loginType,
-			// 'command': command
-		// }, addHeaders({Referer: 'https://esia.gosuslugi.ru/idp/rlogin?cc=bp'}));
+		// }, addHeaders({Referer: 'https://esia.gosuslugi.ru/idp/authn/CommonLogin'}));
+		
+		// А новую оставим на всякий
+		var command = getParam(html, null, null, /new LoginViewModel\((?:[^,]+,){1,2}'([^"']+)'/i);
+		if(!command) {
+			AnyBalance.trace(html);
+			throw new AnyBalance.Error('Не удалось найти идентификатор команды для входа.');
+		}
+		
+		html = AnyBalance.requestPost('https://esia.gosuslugi.ru/idp/login/pwd/do', {
+			login: formattedLogin,
+			password: prefs.password,
+			idType:loginType,
+			'command': command
+		}, addHeaders({Referer: 'https://esia.gosuslugi.ru/idp/rlogin?cc=bp'}));
 		
 		//Попытаемся получить ошибку авторизации на раннем этапе. Тогда она точнее.
 		var errorCode = getParam(html, null, null, /authn\.error\.([^"']+)/i);

5188cb4ea82f764fee460ea0cd1f91b0f0f9f6c6
kirillpisarev23@gmail.com <kirillpisarev23@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-06-08 12:35:47 +0000

    Emirates Skywards (ab-bonus-skywards): - Поддержка изменений на сайте.

--- trunk/ab-bonus-skywards/main.js
+++ trunk/ab-bonus-skywards/main.js
@@ -1,27 +1,18 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс баллов и другую информацию по бонусной программе Skywards
-
-Operator site: https://www.skywards.com/
-Личный кабинет: https://www.skywards.com/
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Cache-Control': 'max-age=0',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22'
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Cache-Control': 'max-age=0',
+    'Connection':'keep-alive',
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22'
 };
 
-function getViewState(html){
-    return getParam(html, null, null, /name="__VIEWSTATE".*?value="([^"]*)"/);
-}
-
 function parseDateMoment(str){
-    var mom = moment(str.replace(/i/ig, 'і'), ['DD MMM YYYY', 'HH:mm-D MMM YYYY']);
+    var mom = moment(str.replace(/i/ig, 'і'), ['DD MMM YYYY', 'HH:mm-D MMM YY']);
     if(!mom.isValid()){
         AnyBalance.trace('Failed to parse date from ' + str);
     }else{
@@ -33,7 +24,7 @@ function parseDateMoment(str){
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = "https://www.emirates.com/account/english/login/login.aspx?mode=ssl";
+    var baseurl = "https://www.emirates.com/";
     AnyBalance.setDefaultCharset('utf-8'); 
 
     checkEmpty(prefs.login, "Please enter your Emirates ID");
@@ -41,11 +32,11 @@ function main(){
 
     moment.lang('en');
 
-    var html = AnyBalance.requestGet(baseurl, g_headers);
+    var html = AnyBalance.requestGet(baseurl + 'account/english/login/login.aspx?mode=ssl', g_headers);
 
     var form = getParam(html, null, null, /<form[^>]+id="aspnetForm"[^>]*>([\s\S]*?)<\/form>/i);
 
-    if(!form) //Если параметр не найден, то это, скорее всего, свидетельствует об изменении сайта или о проблемах с ним
+    if(!form)
         throw new AnyBalance.Error('Could not find login form. Is the site changed?');
 
     var params = createFormParams(form);
@@ -53,17 +44,31 @@ function main(){
     params.txtPassword = prefs.password;
     params.btnHeaderSearch = params.siteSelectorSubmit = params.chkRememberMe = params.btnForgotPasswordSubmit = params.btnMembershipNoSubmit = undefined;
 
-    //Теперь, когда секретный параметр есть, можно попытаться войти
-    try{
-    	html = AnyBalance.requestPost(baseurl, params, addHeaders({Referer: baseurl})); 
-    }catch(e){
-	if(prefs.__debug)
-            html = AnyBalance.requestGet("http://www.emirates.com/english/index.aspx", g_headers);
-        else
-	    throw e;
+    var captchaKey, captchaSrc, captcha;
+    if(AnyBalance.getLevel() >= 7){
+        AnyBalance.trace('Пытаемся ввести капчу');
+        captchaSrc = getParam(html, null, null, /(account\/english\/login\/login.aspx\?cptLogin_captcha=[^"]+)"/i);
+        captcha = AnyBalance.requestGet(baseurl + captchaSrc, addHeaders({ Referer: baseurl }));
+        if(!captchaSrc || !captcha)
+            throw new AnyBalance.Error('Captcha error! Try update later.');
+        captchaKey = AnyBalance.retrieveCode("Please, enter the code as you see in image", captcha);
+        AnyBalance.trace('Капча получена: ' + captchaKey);
+    } else {
+        throw new AnyBalance.Error('Provider requires AnyBalance API v7, please, update AnyBalance!');
     }
 
-    if(!/Log out/i.test(html)){
+    params.txtCaptcha = captchaKey;
+
+    try {
+    	html = AnyBalance.requestPost(baseurl + 'account/english/login/login.aspx?mode=ssl', params, addHeaders({Referer: baseurl + 'account/english/login/login.aspx?mode=ssl'})); 
+    } catch(e) {
+    	if(prefs.__debug)
+            html = AnyBalance.requestGet("http://www.emirates.com/english/index.aspx", g_headers);
+        else
+	       throw e;
+    }
+
+    if(!/Welcome to/i.test(html)){
         var error = getParam(html, null, null, /<div[^>]+class="errorPanel"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
@@ -71,17 +76,15 @@ function main(){
         throw new AnyBalance.Error('Could not login to the personal account. Is site changed?');
     }
 
-    var myReplaceTagsAndSpaces = [/,/g, '', replaceTagsAndSpaces]; 
-
     var result = {success: true};
-    getParam(html, result, 'fio', /<span[^>]+spnMemberName[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'num', /<div[^>]+divSkywardsNo"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /<div[^>]+divSkywardsNo"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /<span[^>]+spnMemberMiles[^>]*>([\s\S]*?)<\/span>/i, myReplaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'tier', /<span[^>]+lblSkywardsTierMiles[^>]*>([\s\S]*?)<\/span>/i, myReplaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'fio', /<div[^>]*class="membershipName"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'num', /<span[^>]*class="armembershipNumber"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /<span[^>]*class="armembershipNumber"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /<div[^>]*class="membershipSkywardsMiles"[^>]*>\s*<div[^>]*class="milesCount"[^>]*>([\s\S]*?)<div>/i, [replaceTagsAndSpaces, /[,.]/, ''], parseBalance);
+    getParam(html, result, 'tier', /<div[^>]*class="membershipTierMiles"[^>]*>\s*<div[^>]*class="milesCount"[^>]*>([\s\S]*?)<div>/i, replaceTagsAndSpaces, parseBalance);
 
-    getParam(html, result, 'till', /<span[^>]+lblSkyWardsMilesExpiryDate"[^>]*>([\s\S]*?)<\/span>/ig, replaceTagsAndSpaces, parseDateMoment);
-    getParam(html, result, 'burn', /<span[^>]+lblSkyWardsMilesExpiry"[^>]*>([\s\S]*?)<\/span>/ig, myReplaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'till', /<\/strong> Miles are due to expire on <strong>([^<]+)/i, replaceTagsAndSpaces, parseDateMoment);
+    getParam(html, result, 'burn', /<strong>([^<]+)<\/strong> Miles are due to expire on/i, [replaceTagsAndSpaces, /[,.]/, ''], parseBalance);
 
     AnyBalance.setResult(result);
 }

246ce8a0b2e8ec9b1ac03395899bc1a42c30d62f
stark027 <stark027@gmail.com>
2015-06-11 14:56:45 +0000

    АКАДО Екатеринбург (ab-internet-akado-ural): - Поддержка изменений на сайте.

--- ab-internet-akado-ural/main.js
+++ ab-internet-akado-ural/main.js
@@ -1,63 +1,42 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Текущий баланс у оператора интернет Акадо-урал.
-
-Сайт оператора: http://akado-ural.ru/
-Личный кабинет: https://stat.akado-ural.ru
 */
-
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && !AnyBalance.isAvailable (param))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-
     var baseurl = "https://stat.akado-ural.ru/";
-
-    AnyBalance.setDefaultCharset('windows-1251');
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
     
     var html = AnyBalance.requestGet(baseurl + 'login.php?in=1');
-
+	
     html = AnyBalance.requestPost(baseurl + 'login.php?in=1', {
         r: getParam(html, null, null, /name="r" value="([^"]*)"/),
         login: prefs.login,
         pass: prefs.password
     });
 
-    var error = getParam(html, null, null, /class="redlight"[^>]*>([\s\S]*?)<\/div>/i, [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/, '']);
-    if(error && !(error.match(/Баланс отрицательный/i))) //http://code.google.com/p/any-balance-providers/issues/detail?id=62
-        throw new AnyBalance.Error(error);
-
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /class="redlight"[^>]*>([\s\S]*?)<\/div>/i, [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/, '']);
+		if (error)
+			throw new AnyBalance.Error(error, null, /введен неверно/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
     var result = {success: true};
     
     getParam(html, result, 'balance', /<th[^>]*>\s*Баланс:[\s\S]*?<td[^>]*>\s*([^<]*)</i, null, parseFloat);
     getParam(html, result, 'credit', /<th[^>]*>\s*Кредит:[\s\S]*?<td[^>]*>\s*([^<]*)</i, null, parseFloat);
     getParam(html, result, 'blocked', /<th[^>]*>\s*Блокировка в биллинге:[\s\S]*?<td[^>]*>\s*([^<]*)</i);
     getParam(html, result, 'agreement', /<th[^>]*>\s*Договор:[\s\S]*?<td[^>]*>\s*([^<]*)</i);
-    result.__tariff = getParam(html, null, null, /<td[^>]*>\s*Тарифный план:[\s\S]*?<td[^>]*>\s*([^<]*)</i, [/\s+$/,'']);
+    getParam(html, result, '__tariff', /<td[^>]*>\s*Тарифный план:[\s\S]*?<td[^>]*>\s*([^<]*)</i, [/\s+$/,'']);
 
     if(AnyBalance.isAvailable('bonus','friends')){
-	var html = AnyBalance.requestGet(baseurl + "?page=24");
+		var html = AnyBalance.requestGet(baseurl + "?page=24");
     	getParam(html, result, 'friends', /<td[^>]*>\s*Количество друзей:[\s\S]*?<td[^>]*>\s*(\d[\s\d\.,]*)[^<]*</i, [/\s+/g, '', /,/g, '.'], parseInt);
     	getParam(html, result, 'bonus', /<td[^>]*>\s*Сумма бонуса:[\s\S]*?<td[^>]*>\s*(-?\s*\d[\s\d\.,]*)[^<]*</i, [/\s+/g, '', /,/g, '.'], parseFloat);
     }

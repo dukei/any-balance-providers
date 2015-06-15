7d090da32697e6e5b1411584694198bd0f30133b
igoloviznin@gmail.com <igoloviznin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-06-15 15:25:02 +0000

    Life UA. Исправлено распознавание счетчика.

--- trunk/ab-cell-life-ukr/main.js
+++ trunk/ab-cell-life-ukr/main.js
@@ -7,7 +7,7 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36',
 };
 
 function main() {
@@ -316,7 +316,9 @@ function mainMobileApp(prefs, baseurl){
     	//Минуты по сети Life:)
     	sumParam(xml, result, 'mins_life', /<balance[^>]+code="Bundle_Voice_Onnet"[^>]*amount="([^"]*)/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
     	//Минуты по сети Life:) западных тарифов
+	if (result.__tariff != 'Вільний life:)' && result.__tariff != 'Свободный life:)' && result.__tariff != 'Free life:)') {
     	sumParam(xml, result, 'mins_life', /<balance[^>]+code="Bundle_Voice_Onnet_West"[^>]*amount="([^"]*)/ig, replaceTagsAndSpaces, parseBalanceLeft, aggregate_sum);
+	}
     	//Минуты на номера фиксированной связи Украины
     	sumParam(xml, result, 'mins_fixed', /<balance[^>]+code="Bundle_Voice_Pstn[^>]*amount="([^"]*)/ig, replaceTagsAndSpaces, parseBalance, aggregate_sum);
     	//Минуты на номера других операторов и фиксированной связи Украины

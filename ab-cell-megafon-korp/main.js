2c583d48172f7e94b66f8fc55d1b290f542acecd
stark027 <stark027@gmail.com>
2015-06-17 13:10:27 +0000

    Мегафон корпоративный (ab-cell-megafon-korp): - Добавлено отображение данных по номеру телефона. - Новые счетчики: Расходы с начала периода, Трафик, Абонентская плата, Разовые начисления, Номер телефона, Название.

--- ab-cell-megafon-korp/main.js
+++ ab-cell-megafon-korp/main.js
@@ -12,10 +12,12 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
+	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
-	var baseurl = 'https://' + (prefs.region || 'ural') + '.b2blk.megafon.ru/';
+	
 	AnyBalance.setDefaultCharset('utf-8');
+	var baseurl = 'https://' + (prefs.region || 'ural') + '.b2blk.megafon.ru/';
 	
 	var html = AnyBalance.requestGet(baseurl + 'sc_cp_apps/login', g_headers);
 	
@@ -24,15 +26,13 @@ function main() {
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 	
-	var html = AnyBalance.requestPost(baseurl + 'sc_cp_apps/loginProcess', {
+	html = AnyBalance.requestPost(baseurl + 'sc_cp_apps/loginProcess', {
 		j_username: prefs.login,
 		j_password: prefs.password,
-	}, addHeaders({
-		Referer: baseurl + 'sc_cp_apps/login'
-	}));
+	}, addHeaders({Referer: baseurl + 'sc_cp_apps/login'}));
 
 	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /"error.message"\s*:\s*"([^"]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		var error = getParam(html, null, null, /"error\.message"\s*:\s*"([^"]+)/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
 			throw new AnyBalance.Error(error, null, /Вы ввели неправильный логин или пароль/i.test(error));
 		
@@ -42,9 +42,58 @@ function main() {
 	
 	var result = {success: true};
 	
-	getParam(html, result, 'balance', /<dt>Текущий баланс[^]*?class="money[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'abonCount', /<dt>Абонентов[^]*?class="span28[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance', /<dt>Текущий баланс[^]*?class="money[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'abonCount', /<dt>Абонентов[^]*?class="span28[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, '__tariff', /accountInfo_name[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	var href = getParam(html, null, null, /sc_cp_apps\/expenses\/account\/[^"']+/i);
+	if(!href) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти ссылку на детализацию, сайт изменен?');
+	}
+	
+	html = AnyBalance.requestGet(baseurl + href + '/list?from=0&size=128', addHeaders({'X-Requested-With':'XMLHttpRequest'}));
+	
+	try {
+		var json = getJson(html);
+		
+		var account;
+		for(var i = 0; i < json.expenses.length; i++) {
+			var curr = json.expenses[i];
+			if(!prefs.phone) {
+				account = curr;
+				AnyBalance.trace('Номер в настройках не задан, возьмем первый: ' + curr.msisdn);
+				break;				
+			}
+			
+			if(endsWith(curr.msisdn, prefs.phone)) {
+				account = curr;
+				AnyBalance.trace('Нашли нужный номер: ' + curr.msisdn);
+				break;
+			}
+		}
+		
+		if(!account) {
+			AnyBalance.trace(JSON.stringify(json));
+			throw new AnyBalance.Error('Не удалось найти ' + (prefs.phone ? 'номер телефона с последними цифрами ' + prefs.phone : 'ни одного номера телефона!'));
+		}
+		
+		AnyBalance.trace('Успешно получили данные по номеру: ' + curr.msisdn);
+		AnyBalance.trace(JSON.stringify(account));
+		
+		getParam(account.amountTotal + '', result, 'amountTotal', null, replaceTagsAndSpaces, parseBalance);
+		getParam(account.amountLocal + '', result, 'amountLocal', null, replaceTagsAndSpaces, parseBalance);
+		getParam(account.charges + '', result, 'charges', null, replaceTagsAndSpaces, parseBalance);
+		
+		sumParam(account.monthChargeRTPL + '', result, 'abon', null, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+		sumParam(account.monthChargeSRLS + '', result, 'abon', null, replaceTagsAndSpaces, parseBalance, aggregate_sum);
+		
+		getParam(account.msisdn, result, 'phone_name', null, replaceTagsAndSpaces);
+		getParam(account.name, result, 'name_name', null, replaceTagsAndSpaces);
+	} catch (e) {
+		AnyBalance.trace(e.message);
+		AnyBalance.trace('Не удалось получить данные по номеру телефона, свяжитесь, пожалуйста, с разработчиками.');
+	}
 	
 	AnyBalance.setResult(result);
 }

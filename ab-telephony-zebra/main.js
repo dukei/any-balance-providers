cbbd3ed8368b00dbe68557873072b084f9e4cb3f
stark027 <stark027@gmail.com>
2015-06-19 16:22:32 +0000

    Зебра Телеком (ab-telephony-zebra): - Поддержка изменений на сайте.

--- ab-telephony-zebra/main.js
+++ ab-telephony-zebra/main.js
@@ -1,73 +1,47 @@
  /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Zebra Telecom Телефония
-Сайт оператора: http://www.zebratelecom.ru
-Личный кабинет: http://www.zebratelecom.ru/cabinet/
-
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp ? regexp.exec (html) : html;
-	if (value) {
-                if(regexp)
-		    value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.', /(\d)\-(\d)/g, '$1.$2'];
-
-function parseBalance(text){
-    var _text = text.replace(/\s+/g, '');
-    var val = getParam(_text, null, null, /(-?\d[\d\.,\-]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseCurrency(text){
-    var _text = text.replace(/\s+/g, '');
-    var val = getParam(_text, null, null, /[\d\.,\-]+(\S*)/);
-    AnyBalance.trace('Parsing currency (' + val + ') from: ' + text);
-    return val;
-}
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');    
 
-    var baseurl = "http://www.zebratelecom.ru/private_clients/cabinet/check.php";
-    
-    var html = AnyBalance.requestPost(baseurl, {
-        fname:'auth',
-	login:prefs.login,
-	pass:prefs.password
-    });
-
-    var error = getParam(html, null, null, /<p[^>]*style="color:\s*red;"[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces);
-    if(error)
-        throw new AnyBalance.Error(error);
-    
-    var result = {
-        success: true
-    };
-
-    var matches;
+    var baseurl = 'http://www.zebratelecom.ru/services/';
+ 	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'cabinet', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}   
+	
+	html = AnyBalance.requestPost(baseurl + 'cabinet/check.php', {
+		'fname': 'auth',
+		'login': prefs.login,
+		'pass': prefs.password,
+	}, addHeaders({Referer: baseurl + 'cabinet'}));
+	
+	if (!/exit\.php/i.test(html)) {
+		var error = getParam(html, null, null, /<p[^>]*style="color:\s*red;"[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+    var result = {success: true};
 
     getParam(html, result, 'balance', /<a[^>]*id="mcount"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'currency', /<a[^>]*id="mcount"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseCurrency);
@@ -77,12 +51,3 @@ function main(){
 		
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

95f67e049a2f8a509a567c4106711f8143e1d903
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-06-19 06:26:35 +0000

    ipstream - исправления

--- trunk/ab-internet-ipstream/main.js
+++ trunk/ab-internet-ipstream/main.js
@@ -5,21 +5,21 @@
   //Получаем значения счетчиков
   //что-то для этого делаем
   
-  
   var url = 'https://bs.ip-stream.ru/api/';
   var strPost = AnyBalance.requestPost(url, {login: prefs.login, password: prefs.password});
 
   if(!strPost || AnyBalance.getLastStatusCode() > 400)
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
   if (/error/i.test(strPost))
-		throw new AnyBalance.Error('Неверный логин или пароль');
+		throw new AnyBalance.Error('Неверный логин или пароль', null, true);
 	
   //извлекаем из строк значения счетчиков
   //...
-	arr=strPost.split('|');
+  var arr=strPost.split('|');
 	
-	tariff=arr[0];
-	balance=arr[1];
+  var tariff=arr[0];
+  var balance=parseFloat(arr[1]);
+
   //Возвращаем результат
   AnyBalance.setResult({success: true, __tariff: tariff, balance: balance});
 }

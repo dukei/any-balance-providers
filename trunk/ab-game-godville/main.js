e166f1296cf217fc2b06854dfeeae4a0fe0e62ad
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-06-19 12:08:43 +0000

    Godville.net (ab-game-godville): - Поддержка входа по паролю.

--- trunk/ab-game-godville/main.js
+++ trunk/ab-game-godville/main.js
@@ -1,92 +1,94 @@
-/*
+/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Godville — многопользовательская бесплатная игра жанра ZPG
-Сайт http://godville.net/
 */
 
-function main(){
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+var baseurl = 'http://godville.net/';
+
+function main() {
 	var prefs = AnyBalance.getPreferences();
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	if(prefs.login && !prefs.password) {
+		AnyBalance.trace('Входим по имени Бога...');
+		mainOld(prefs);
+	} else {
+		AnyBalance.trace('Входим по имени Бога и паролю...');
+		mainNew(prefs);
+		AnyBalance.trace('Авторизовались, переходим к получению данных...');
+		mainOld(prefs);
+	}
+}
 
-	if (!prefs.login || prefs.login == '')
-		throw new AnyBalance.Error('Введите ваше имя бога');
+function mainNew(prefs) {
+	checkEmpty(prefs.login, 'Введите ваше имя бога!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + 'login/login', {
+		username: prefs.login,
+		password: prefs.password,
+		'commit': 'Войти!'
+	}, addHeaders({Referer: baseurl + 'login/login'}));
+	
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+}
 
-	var pd = AnyBalance.requestGet('http://godville.net/gods/api/' + prefs.login.replace(' ', '%20') + '.json');
-
-	if(!pd || pd == '')
-		throw new AnyBalance.Error('Не удалось получить данные');
+function mainOld(prefs) {
+	checkEmpty(prefs.login, 'Введите ваше имя бога!');
+	
+	var pd = AnyBalance.requestGet(baseurl + 'gods/api/' + encodeURIComponent(prefs.login) + '.json');
+	
+	if(!pd)
+		throw new AnyBalance.Error('Не удалось получить данные!');
 
 	if(pd == 'Not found')
 		throw new AnyBalance.Error('Герой не найден');
-
-	var js = $.parseJSON(pd);
+	
+	var json = getJson(pd);
 
 	var result = {success: true};
 
-	var hname = js.name;
-
-	result.__tariff = hname;
-	
-	if(AnyBalance.isAvailable('name'))
-		result['name'] = hname;
-
-	if(AnyBalance.isAvailable('level'))
-		result['level'] = js.level;
-
-	if(AnyBalance.isAvailable('health'))
-		result['health'] = js.health;
-
-	if(AnyBalance.isAvailable('health_t'))
-		result['health_t'] = js.health + "/" +  js.max_health;
-
-	if(AnyBalance.isAvailable('bricks_p'))
-		result['bricks_p'] = js.bricks_cnt / 10;
-
-	if(AnyBalance.isAvailable('bricks'))
-		result['bricks'] = js.bricks_cnt;
-
-	if(AnyBalance.isAvailable('godpower'))
-		result['godpower'] = js.godpower;
-
-	if(AnyBalance.isAvailable('wood_p'))
-		result['wood_p'] = js.wood_cnt ? js.wood_cnt / 10 : 0;
-
-	if(AnyBalance.isAvailable('wood'))
-		result['wood'] = js.wood_cnt;
-	
-	if(AnyBalance.isAvailable('inventory'))
-		result['inventory'] = js.inventory_num;
-	
-	if(js.diary_last){
-
-		if(AnyBalance.isAvailable('health_t'))
-			result['health_t'] = js.health + "/" +  js.max_health;
-
-		if(AnyBalance.isAvailable('health_p'))
-			result['health_p'] = ((js.health / js.max_health) * 100).toFixed(1);
-
-		if(AnyBalance.isAvailable('level_p'))
-			result['level_p'] = js.exp_progress;
-
-		if(AnyBalance.isAvailable('quest_p'))
-			result['quest_p'] = js.quest_progress;
-
-	} else {
-		throw new AnyBalance.Error('Включите расширенное API а настройках игры');
-	}
+	getParam(json.name, result, '__tariff', null, replaceTagsAndSpaces);
+	getParam(json.name, result, 'name', null, replaceTagsAndSpaces);
+	getParam(json.level + '', result, 'level', null, replaceTagsAndSpaces);
+	getParam(json.health + '', result, 'health', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.health + '/' + json.max_health, result, 'health_t', null, replaceTagsAndSpaces);
+	getParam(json.bricks_cnt/10 + '', result, 'bricks_p', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.bricks_cnt + '', result, 'bricks', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.godpower + '', result, 'godpower', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.wood_cnt ? json.wood_cnt / 10 : 0 + '', result, 'wood_p', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.wood_cnt + '', result, 'wood', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.inventory_num + '', result, 'inventory', null, replaceTagsAndSpaces, parseBalance);
+	getParam(((json.health / json.max_health) * 100).toFixed(1) + '', result, 'health_p', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.exp_progress + '', result, 'level_p', null, replaceTagsAndSpaces, parseBalance);
+	getParam(json.quest_progress + '', result, 'quest_p', null, replaceTagsAndSpaces, parseBalance);
 	
 	if(AnyBalance.isAvailable('savings')) {
-			var pd = AnyBalance.requestGet('http://godville.net/gods/' + prefs.login.replace(' ', '%20'));
-			var matches = pd.match(/<td class="label">Сбережения<\/td>\s+?<td class="name">(\d+) /i);
-			var matches2 = pd.match(/Кирпичей для храма/i);
-			if (matches2) {
-				result['savings'] = 0
-			} else if (matches) {
-				result['savings'] = matches[1]
-			} else {
-				throw new AnyBalance.Error("Сбережений не обнаружено");
-			}
-		}
+		var html = AnyBalance.requestGet(baseurl + 'gods/' + encodeURIComponent(prefs.login));
+		
+		getParam(html, result, 'savings', /<td class="label">Сбережения<\/td>\s+?<td class="name">(\d+)/i, replaceTagsAndSpaces, parseBalance);
+	}
 
 	AnyBalance.setResult(result);
 

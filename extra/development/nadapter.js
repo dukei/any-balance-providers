5bfaa4f1de3b4f298386671881d1b12a526ac56f
dmitry.kochin <dmitry.kochin@gmail.com>
2015-07-01 14:25:47 +0000

    адаптер исправлен, чтобы простые поля конвертировать без объявления

--- extra/development/nadapter.js
+++ extra/development/nadapter.js
@@ -1,41 +1,43 @@
-function NAdapter(k,g){var l={};
-var e=AnyBalance.isAvailable;
-for(var h in k){if(AnyBalance.isAvailable(h)){var j=k[h];
-do{l[j]=true;
-j=j.indexOf(".")>=0?j.replace(/\.[^.]*$/,""):null
-}while(j!==null)
-}}var i={};
-AnyBalance.shouldProcess=function(m,n){if(i[m]){return n.__id==g_productIds[m]
-}var c=g(m,n);
-if(c){i[m]=n.__id
+function NAdapter(l,h){var m={};
+var f=AnyBalance.isAvailable;
+for(var i in l){if(AnyBalance.isAvailable(i)){var k=l[i];
+do{m[k]=true;
+k=k.indexOf(".")>=0?k.replace(/\.[^.]*$/,""):null
+}while(k!==null)
+}}var j={};
+AnyBalance.shouldProcess=function(n,o){if(j[n]){return o.__id==g_productIds[n]
+}var c=h(n,o);
+if(c){j[n]=o.__id
 }return c
 };
-function a(m){if(!isArray(m)){m=[m]
+function a(n){if(!isArray(n)){n=[n]
 }for(var c=0;
-c<m.length;
-++c){if(l[m[c]]){return true
-}if(m[c].indexOf(".")<0){return e.call(AnyBalance,m[c])
-}}return false
-}function d(c){for(var m=0;
-m<arguments.length;
-++m){if(a(arguments[m])){return true
-}}return false
-}function f(m,o){var n=o.split(/\./g);
-var p;
-for(var c=0;
 c<n.length;
-++c){p=m[n[c]];
-if(isArray(p)){p=p[0]
-}if(!isset(p)||p===null){return p
-}m=p
-}return p
-}function b(m,c){AnyBalance.isAvailable=d;
-try{return m.apply(null,c)
-}finally{AnyBalance.isAvailable=e
-}}return{exec:b,convert:function(n){if(!n.success){return n
-}var m={success:true};
-for(var o in k){if(isAvailable([o])){m[o]=f(n,k[o])
-}}return m
+++c){if(m[n[c]]){return true
+}if(e(n[c])){return f.call(AnyBalance,n[c])
+}}return false
+}function e(c){return c.indexOf(".")<0
+}function d(c){for(var n=0;
+n<arguments.length;
+++n){if(a(arguments[n])){return true
+}}return false
+}function g(n,p){var o=p.split(/\./g);
+var q;
+for(var c=0;
+c<o.length;
+++c){q=n[o[c]];
+if(isArray(q)){q=q[0]
+}if(!isset(q)||q===null){return q
+}n=q
+}return q
+}function b(n,c){AnyBalance.isAvailable=d;
+try{return n.apply(null,c)
+}finally{AnyBalance.isAvailable=f
+}}return{exec:b,convert:function(o){if(!o.success){return o
+}var n={success:true};
+for(var p in o){if(!m[p]&&!isArray(o[p])){n[p]=o[p]
+}}for(var p in l){if(isAvailable([p])){n[p]=g(o,l[p])
+}}return n
 },envelope:function(c){return function(){return b(c,arguments)
 }
 }}

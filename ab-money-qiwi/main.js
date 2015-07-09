31a6b24f80e4896d95641e0ed8c7347e723aeb88
stark027 <stark027@gmail.com>
2015-07-09 15:19:27 +0000

    QIWI Кошелек (ab-money-qiwi): - Поддержка изменений на сайте.

--- ab-money-qiwi/main.js
+++ ab-money-qiwi/main.js
@@ -2,67 +2,16 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
-function jsonp (obj) {
-	return obj;
-}
-
 var g_headers = {
-    Accept:'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-    'Cache-Control':'max-age=0',
-    Connection:'keep-alive',
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.60 Safari/537.1'
+    Accept: 'application/vnd.qiwi.sso-v1+json',
+	Origin: 'https://w.qiwi.com',
+	'Accept-Language': 'ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36',
+	'Content-Type': 'application/json',
 };
 
-function main(){
-    var prefs = AnyBalance.getPreferences ();
-	
-	mainNew();
-	// Вроде старого кабинета больше нет.
-    // switch(prefs.type){
-    // case 'new':
-        // mainNew();
-        // break;
-    // case 'old':
-        // mainOld();
-        // break;
-    // case 'auto':
-    // default:
-        // try{
-			// mainNew();
-        // }catch(e){
-            // if(e.fatal)
-                // throw e;
-            // AnyBalance.trace('Ошибка подключения к новому кабинету: ' + e.message + '\nПробуем старый...');
-            // mainOld();
-        // }
-        // break;
-    // }
-}
-
-function getFatalError(str){
-    var e = new AnyBalance.Error(str);
-    e.fatal = true;
-    return e;
-}
-
-var g_currency = {
-    RUB: 'р',
-    USD: '$',
-	EUR: '€',
-    KZT: '〒',
-    UAH: '₴',
-};
-
-function parseCurrencyMy(text){
-    var currency = parseCurrency(text);
-    return g_currency[currency] ? ' ' + g_currency[currency] : currency;
-}
-
-function mainNew () {
-    var prefs = AnyBalance.getPreferences ();
-    var baseurl = 'https://w.qiwi.com/';
+function main() {
+    var prefs = AnyBalance.getPreferences();
 	
 	checkEmpty(prefs.login, 'Введите номер телефона!');
 	checkEmpty(prefs.password, 'Введите пароль!');
@@ -72,49 +21,18 @@ function mainNew () {
     AnyBalance.trace ('Trying to enter NEW account at address: ' + baseurl);
 	
     var login = /^\s*\+/.test(prefs.login) ? prefs.login : '+7' + prefs.login;
-    var params = {
-        source: 'MENU',
+	
+    var response = requestAPI('cas/tgts', {
         login: login,
         password: prefs.password
-    };
+    });
 	
-    var info = AnyBalance.requestPost (baseurl + 'auth/login.action', params, addHeaders({Accept: 'application/json, text/javascript', 'X-Requested-With':'XMLHttpRequest'}));
-    AnyBalance.trace ('Login result: ' + info);
+	response = requestAPI('cas/sts', {
+		"ticket": response.entity.ticket,
+		"service":"https://w.qiwi.com/j_spring_cas_security_check"
+	});
 	
-    var res = getJson(info);
-	
-    if(res.code.value == '7'){
-        AnyBalance.trace('Требуется ввести токен авторизации');
-        params.loginToken = res.data.token;
-        var info = AnyBalance.requestPost (baseurl + 'auth/login.action', params, addHeaders({Accept: 'application/json, text/javascript', 'X-Requested-With':'XMLHttpRequest'}));
-        AnyBalance.trace ('Login result 2: ' + info);
-
-        res = getJson(info);
-    }
-	
-    // Проверка ошибки входа
-    if (res.code.value != '0') {
-    	var errors = {
-    		0: "ОК",
-    		1: "Ошибка",
-    		2: "Ошибка",
-    		3: "Требуется авторизация",
-    		4: "Требуется подтверждение",
-    		5: "Требуется подтверждение",
-    		6: "Срок действия пароля истек",
-    		7: "Требуется ввод токена авторизации",
-    		success: "Успех",
-    		timeout: "Время соединения истекло. Проверьте подключение к сети",
-    		error: "Ошибка соединения. Проверьте подключение к сети",
-    		abort: "Запрос отменен",
-    		parsererror: "Синтаксическая ошибка"
-    	};
-		
-		var errors = res.message || (res.messages ? res.messages.join('\n') : 'Не удаётся войти в QIWI кошелек: ' + (errors[res.code.value] || res.code._NAME));
-    	throw new AnyBalance.Error(errors, null, /Неверный логин или пароль|Неправильный номер телефона или пароль/i.test(errors));
-    }
-	
-    var html = AnyBalance.requestGet (baseurl + 'payment/main.action');
+	var html = AnyBalance.requestGet(baseurl + 'j_spring_cas_security_check?ticket=' + response.entity.ticket, addHeaders({'Referer': baseurl}));
 	
 	if(/Внимание! Срок действия вашего пароля истек/i.test(html)) {
 		throw new AnyBalance.Error('Внимание! Срок действия вашего пароля истек. Зайдите в кошелек через браузер и следуйте инструкции.', null, true);
@@ -126,22 +44,22 @@ function mainNew () {
 	
 	info = AnyBalance.requestPost(baseurl + 'person/state.action', '', addHeaders({Accept: 'application/json, text/javascript', 'X-Requested-With':'XMLHttpRequest'}));
 	
-	res = getJson(info);
+	response = getJson(info);
 	
 	var i = 0;
 	for (var balance in g_currency) {
-		if (!isset(res.data.balances[balance]))
+		if (!isset(response.data.balances[balance]))
 			continue;
 
 		var balanceVar = (i >= 1 ? 'balance' + (i + 1) : 'balance');
-		getParam(res.data.balances[balance] + '', result, balanceVar, null, replaceTagsAndSpaces, parseBalance);
+		getParam(response.data.balances[balance] + '', result, balanceVar, null, replaceTagsAndSpaces, parseBalance);
 		getParam(g_currency[balance] + '', result, [(i >= 1 ? 'currency' + (i + 1) : 'currency'), balanceVar]);
 		i++
 	}
 	
-	getParam(res.data.person, result, '__tariff');
-	getParam(res.data.messages, result, 'messages');
-	getParam(res.data.unpaidOrderCount, result, 'bills');
+	getParam(response.data.person, result, '__tariff');
+	getParam(response.data.messages, result, 'messages');
+	getParam(response.data.unpaidOrderCount, result, 'bills');
 	
 	// Баланс мегафона
 	if(AnyBalance.isAvailable('megafon_balance', 'megafon_can_pay')) {
@@ -164,7 +82,7 @@ function mainNew () {
 		month = ('0'+month).slice(-2);
 		var day = ('0'+today.getDate()).slice(-2);
 		// На валидность проверяется только дата окончания отчета
-		html = AnyBalance.requestGet(baseurl + 'qvc/reports.action?number='+card+'&daterange=true&start=24.06.2013&finish='+day+'.'+month+'.'+yr);
+		html = AnyBalance.requestGet(baseurl + 'qvc/reports.action?number='+card+'&daterange=true&start=01.01.2010&finish='+day+'.'+month+'.'+yr);
 		
 		var element = getParam (html, null, null, /<div[^>]*class="reportsLine(?:[^>]*>){9}\s*\d{1,2}.\d{1,2}.\d{2,4}(?:[^>]*>){25,30}[^>]*clearBoth/i, null, html_entity_decode);
 		if(element) {
@@ -183,91 +101,36 @@ function mainNew () {
     AnyBalance.setResult (result);
 }
 
-function mainOld () {
-    var prefs = AnyBalance.getPreferences ();
-    var baseurl = 'https://w.qiwi.ru/';
+var g_currency = {
+    RUB: 'р',
+    USD: '$',
+	EUR: '€',
+    KZT: '〒',
+    UAH: '₴',
+};
 
-    if (!prefs.login || prefs.login == '')
-        throw getFatalError ('Введите номер телефона');
+function parseCurrencyMy(text){
+    var currency = parseCurrency(text);
+    return g_currency[currency] ? ' ' + g_currency[currency] : currency;
+}
 
-    if (!prefs.password || prefs.password == '')
-        throw getFatalError ('Введите пароль');
-
-    AnyBalance.trace ('Trying to enter OLD account at address: ' + baseurl);
-    var login = /^\s*\+/.test(prefs.login) ? prefs.login.replace(/\+/g, '') : '7' + prefs.login;
-    var info = AnyBalance.requestGet (baseurl +
-                                      'login.action?source=0&phone=' +
-                                      encodeURIComponent(login) +
-                                      '&password=' +
-                                      encodeURIComponent(prefs.password) +
-                                      '&captcha=0&callback=jsonp');
-    AnyBalance.trace ('Login result: ' + info);
-
-    try{
-       var res = eval(info);
-    }catch(e){
-       throw new AnyBalance.Error('Неверный ответ сервера (' + e.message + '). Сайт изменен?');
-    }
-
+var baseurlAuth = 'https://auth.qiwi.com/';
+var baseurl = 'https://w.qiwi.com/';
+	
+function requestAPI(action, params) {
+    var info = AnyBalance.requestPost(baseurlAuth + action, JSON.stringify(params), g_headers);
+    AnyBalance.trace ('Request result: ' + info);
+	
+    var response = getJson(info);	
+	
     // Проверка ошибки входа
-    if (res.error != 0) {
-        var err = '';
-
-        switch (res.error) {
-            case 3:
-                err = 'Ошибка входа. Возможно, неверно введен пароль';  // На самом деле, не факт, что это так, но на телефоне такая ситуация была, код - 3, описания ошибок нет.
-                break;
-        }
-
-        for (var i in res.errors) {
-            if (res.errors[i] == '')
-                continue;
-
-            if (err != '')
-                err += '; ';
-            err += res.errors[i];
-        }
-
-        if (err == '')
-            err = 'Неопределенная ошибка. Пожалуйста, свяжитесь с автором скрипта.';
-        else if (err == 'Введенные цифры неверны')  // Возможно, эта ситуация будет срабатывать только при отладке в Chrome
-            err = 'Требуется ручной ввод. Пожалуйста, войдите в личный кабинет на <a href=\'https://w.qiwi.ru\'>домашней странице</a> QIWI Кошелька.';
-        throw new AnyBalance.Error (err);
-    }
-
-    var html = AnyBalance.requestGet (baseurl + 'userdata.action');
-
-    // Проверка на корректный вход
-    if (/<h1>Кошелёк:/i.exec(html)){
-    	AnyBalance.trace ('It looks like we are in selfcare...');
-    }else if(/passwordchangesuccess.action/i.test(html)){
-        throw getFatalError('Срок действия пароля истек. Смените пароль, зайдя в свой QIWI-кошелек (https://w.qiwi.ru) через браузер.');
-    }else {
-        AnyBalance.trace ('Have not found logout... Unknown error. Please contact author.');
-        throw new AnyBalance.Error ('Неизвестная ошибка. Пожалуйста, свяжитесь с автором провайдера.');
-    }
-
-    var result = {success: true};
-
-    // Баланс
-    getParam (html, result, 'balance', /id="balance".*?>([+-]?(?:\d+[.,]?\d*|\d*[.,]?\d+))/i, [',', '.'], parseFloat);
-    getParam (' р', result, ['currency', 'balance']);
-
-    // Сообщения
-    getParam (html, result, 'messages', /Сообщения:.*?>(\d+)/i, [], parseInt);
-
-    // Счета
-    getParam (html, result, 'bills', /Счета:.*?>(\d+)/i, [], parseInt);
-
-	
-	// QVC
-	if(AnyBalance.isAvailable('qvc_card'))
-	{
-        html = AnyBalance.requestGet(baseurl + 'qvc.action');
+    if (!response.entity.ticket) {
+		var error = response.entity.error.message;
+		if(error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль|Неправильный номер телефона или пароль/i.test(error));
 		
-		getParam (html, result, 'qvc_card', /Номер карты:[\s\S]{1,6}>\s*([\s\S]*?)\s*<\//i, replaceTagsAndSpaces, html_entity_decode);
-		getParam (html, result, 'qvc_exp', /Срок действия:[\s\S]{1,6}>\s*([\s\S]*?)\s*<\//i, replaceTagsAndSpaces, parseDate);
-    }
+		throw new AnyBalance.Error('Не удалось войти в Qiwi Visa Wallet, сайт изменен?');
+    }	
 	
-    AnyBalance.setResult (result);
+	return response;
 }

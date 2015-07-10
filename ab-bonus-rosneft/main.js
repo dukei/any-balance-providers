f151254d0773df8161961c0456215328083328e4
stark027 <stark027@gmail.com>
2015-07-10 08:49:16 +0000

    Роснефть (Бонусы) (ab-bonus-rosneft): - Поддержка изменений на сайте.

--- ab-bonus-rosneft/main.js
+++ ab-bonus-rosneft/main.js
@@ -3,10 +3,11 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
+	'Origin': 'http://www.podarok.rn-card.ru',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
@@ -19,20 +20,22 @@ function main() {
 
 	var html = AnyBalance.requestGet(baseurl + '?q=cabinet', g_headers);
 
-	var params = createFormParams(html, function(params, str, name, value) {
-		if (name == 'number') 
-			return prefs.login;
-
-		return value;
-	});
+	var formId = getParam(html, null, null, /name="form_build_id"[^>]*value="([^"]+)/i);
 	
-	html = AnyBalance.requestPost(baseurl + '?q=cabinet', params, addHeaders({Referer: baseurl + '?q=cabinet'}));
+	html = AnyBalance.requestPost(baseurl + '?q=cabinet', {
+		'number': prefs.login,
+		'op': 'Отправить',
+		'bonus': '',
+		'form_build_id': formId,
+		'form_id': '_forte_cabinet_form'
+	}, addHeaders({Referer: baseurl + '?q=cabinet'}));
 
 	if (!/name="bonus"[^>]*value="\d+"/i.test(html)) {
 		var error = getParam(html, null, null, /Сообщение об ошибке(?:[^>]*>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
 			throw new AnyBalance.Error(error);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+		
+		throw new AnyBalance.Error('Не удалось получить данные по бонусам. Сайт изменен?');
 	}
 	var result = {success: true};
 	

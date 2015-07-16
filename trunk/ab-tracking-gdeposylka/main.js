d947c20d0236c3965f6cf50e449db52a93a435b7
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-07-16 11:13:16 +0000

    Где посылка (ab-tracking-gdeposylka): - Поддержка изменений на сайте.

--- trunk/ab-tracking-gdeposylka/main.js
+++ trunk/ab-tracking-gdeposylka/main.js
@@ -2,77 +2,117 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
 function main(){
 	var prefs = AnyBalance.getPreferences();
 	checkEmpty(prefs.track_id, 'Введите номер почтового отправления!');
 	
 	var baseurl = "http://gdeposylka.ru/";
-	var html = AnyBalance.requestGet(baseurl + prefs.track_id);
 	
-	var error = getParam(html, null, null, [/<div class="errorBox"[^>]*>([\s\S]*?)<\/div>/i, /<span class="error"[^>]*>([\s\S]*?)<\/span>/i], replaceTagsAndSpaces);
-	if(error)
-		throw new AnyBalance.Error(error);
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	html = AnyBalance.requestPost(baseurl + 'form', {
+		'tracking[tracking_number]': prefs.track_id,
+		'tracking[track]': '',	
+	}, addHeaders({Referer: baseurl + 'form'}));
+	
+	if (!/Посылка в пути/i.test(html)) {
+		var error = getParam(html, null, null, [/<div class="errorBox"[^>]*>([\s\S]*?)<\/div>/i, /<span class="error"[^>]*>([\s\S]*?)<\/span>/i], replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти данные по отправлению ' + prefs.track_id);
+	}
 	
 	var result = {success: true};
-	var days = getParam(html, null, null, /"parcelin-days"(?:[^>]*>){1}([^<\|]*)/i, replaceTagsAndSpaces, parseBalance);
 	
-	var table = getParam(html, null, null, /class="parcelin-table"([\s\S]*?)<\/table>/i);
+	getParam(html, result, '__tariff', /<h2>\s*<strong>([^<]+)/i, replaceTagsAndSpaces);
+	getParam(html, result, ['days' , '__tariff'], /В пути(?:[^>]*>){2}\s*(\d+)\s*д/i, replaceTagsAndSpaces, parseBalance);
+	
+	var table = getElement(html, /<ul\s+class="checkpoints">/i);
 	if(!table)
 		throw new AnyBalance.Error("Не удалось найти статус посылки, возможно, из-за изменений на сайте");
 	
-	getParam(table, result, 'geo', /class="city"[^>]*>([\s\S]*?)<\/div>/i, [/Неизвестное местоположение/i, '', replaceTagsAndSpaces]);
+	var checkpoints = getElements(table, /<li[^>]*>/ig);
 	
-	var status = getParam(table, null, null, /parcel-info_mod"[^>]*>([^<]*)/i, replaceTagsAndSpaces);
-	var date = getParam(table, null, null, /parcelin-received-date"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseDateW);
+	AnyBalance.trace('checkpoints ' + checkpoints.length);
 	
-	getParam(status, result, 'status');
-	getParam(date, result, 'date');
-	getParam(days, result, 'days');
-	
-	// Фисксим <b>undefined</b><br/>\n<small>NaN/NaN/NaN NaN:NaN</small>: в пути 0 дн.
-	if(AnyBalance.isAvailable('fulltext')){
-		if(status && date && (days > 0)) {
-			result.fulltext = '<b>' + status + '</b><br/>\n' + '<small>' + getDateString(date) + '</small> в пути ' + days + ' дн.';
+	if(AnyBalance.isAvailable('fulltext')) {
+		if(checkpoints.length == 0) {
+			result.fulltext = 'Информации о местоположении посылки пока нет.';
 		} else {
-			result.fulltext = 'Информация недоступна.'
+			result.fulltext = '';
+			
+			for(var i = 0; i < checkpoints.length; i++) {
+				var date = getParam(checkpoints[i], null, null, /<time[^>]*datetime="([^"]+)/i, replaceTagsAndSpaces, parseDateISO);
+				var status = getParam(checkpoints[i], null, null, /"checkpoint-status"(?:[^>]*>)([^<]+)/i, replaceTagsAndSpaces);
+				var geo = getParam(checkpoints[i], null, null, /class="text-muted"[^>]*>([\s\S]*?)<\/div/i, replaceTagsAndSpaces);
+				
+				if(i == 0) {
+					getParam(status, result, 'status');
+					getParam(date, result, 'date');
+					getParam(geo, result, 'geo');
+				}
+				
+				result.fulltext += '<b>' + status + ' (' + geo + ')</b><br/>' + '<small>' + getDateString(date) + '</small><br/><br/>';
+			}
 		}
 	}
+	
+	// var days = getParam(html, null, null, /"parcelin-days"(?:[^>]*>){1}([^<\|]*)/i, replaceTagsAndSpaces, parseBalance);
+	
+	// var table = getParam(html, null, null, /class="parcelin-table"([\s\S]*?)<\/table>/i);
+	// if(!table)
+		// throw new AnyBalance.Error("Не удалось найти статус посылки, возможно, из-за изменений на сайте");
+	
+	// getParam(table, result, 'geo', /class="city"[^>]*>([\s\S]*?)<\/div>/i, [/Неизвестное местоположение/i, '', replaceTagsAndSpaces]);
+	
+	// var status = getParam(table, null, null, /parcel-info_mod"[^>]*>([^<]*)/i, replaceTagsAndSpaces);
+	// var date = getParam(table, null, null, /parcelin-received-date"[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseDateW);
+	
+	// getParam(status, result, 'status');
+	// getParam(date, result, 'date');
+	// getParam(days, result, 'days');
+	
+	// // Фисксим <b>undefined</b><br/>\n<small>NaN/NaN/NaN NaN:NaN</small>: в пути 0 дн.
+	// if(AnyBalance.isAvailable('fulltext')){
+		// if(status && date && (days > 0)) {
+			// result.fulltext = '<b>' + status + '</b><br/>\n' + '<small>' + getDateString(date) + '</small> в пути ' + days + ' дн.';
+		// } else {
+			// result.fulltext = 'Информация недоступна.'
+		// }
+	// }
 	
 	AnyBalance.setResult(result);
 }
 
-function numSize(num, size) {
-    var str = num + '';
-    if (str.length < size) {
-        for (var i = str.length; i < size; ++i) {
-            str = '0' + str;
-        }
-    }
-    return str;
-}
-
 function getDateString(dt) {
+	function numSize(num, size) {
+		var str = num + '';
+		if (str.length < size) {
+			for (var i = str.length; i < size; ++i) {
+				str = '0' + str;
+			}
+		}
+		return str;
+	}
+
     if (typeof dt != 'object') 
 		dt = new Date(dt);
     return numSize(dt.getDate(), 2) + '/' + numSize(dt.getMonth() + 1, 2) + '/' + dt.getFullYear() + " " + dt.getHours() + ':' + dt.getMinutes();
 }
 
-/** Парсит дату из такого вида: 27 июля 2013 без использования сторонних библиотек, результат в мс */
-function parseDateW(str){
-	var year = new Date().getFullYear();
-	
-	AnyBalance.trace('Trying to parse date from ' + str);
-	return getParam(str, null, null, null, [ 
-		/\D*(?:январ(?:я|ь)|янв|january|jan)\D*/i, '.01.' + year + ' ', 
-		/\D*(?:феврал(?:я|ь)|фев|febrary|feb)\D*/i, '.02.' + year + ' ', 
-		/\D*(?:марта|март|мар|march|mar)\D*/i, '.03.' + year + ' ', 
-		/\D*(?:апрел(?:я|ь)|апр|april|apr)\D*/i, '.04.' + year + ' ', 
-		/\D*(?:ма(?:я|й)|may)\D*/i, '.05.' + year + ' ', 
-		/\D*(?:июн(?:я|ь)|июн|june|jun)\D*/i, '.06.' + year + ' ', 
-		/\D*(?:июл(?:я|ь)|июл|july|jul)\D*/i, '.07.' + year + ' ', 
-		/\D*(?:августа|август|авг|august|aug)\D*/i, '.08.' + year + ' ', 
-		/\D*(?:сентябр(?:я|ь)|сен|september|sep)\D*/i, '.09.' + year + ' ', 
-		/\D*(?:октябр(?:я|ь)|окт|october|oct)\D*/i, '.10.' + year + ' ', 
-		/\D*(?:ноябр(?:я|ь)|ноя|november|nov)\D*/i, '.11.' + year + ' ', 
-		/\D*(?:декабр(?:я|ь)|dec|december|dec)\D*/i, '.12.'], parseDate);
-}
+

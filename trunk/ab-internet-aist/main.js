ccce449325bcec78b6384eb6ec37e9a16a49bdb2
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-07-16 12:51:11 +0000

    AIST (Самара, Тольятти) (ab-internet-aist): - Поддержка изменений на сайте.

--- trunk/ab-internet-aist/main.js
+++ trunk/ab-internet-aist/main.js
@@ -1,18 +1,13 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифном плане для интернет провайдера АИСТ
-
-Operator site: aist.net.ru
-Личный кабинет:  http://tlt.class.avtograd.ru/login/default.aspx?back_url=%2fUser%2fDefault.aspx
 */
 
 var g_headers = {
-'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-'Connection':'keep-alive',
-'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':'keep-alive',
+	'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
 };
 
 function getViewState(html){
@@ -32,7 +27,7 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     var region = g_region[prefs.region] ? prefs.region : 'tlt';
     AnyBalance.trace('Используется регион: ' + region);
-    var baseurl = "http://" + region + ".class.avtograd.ru/";
+    var baseurl = "https://" + region + ".class.avtograd.ru/";
     var loginpart = "login/default.aspx?back_url=%2fUser%2fDefault.aspx";
     AnyBalance.setDefaultCharset('utf-8'); 
 

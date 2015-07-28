3e091e5b9c86f049574baa0c6cadeb2c1fa4f95c
stark027@gmail.com <stark027@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-07-28 10:25:58 +0000

    Домашний Интернет и Телевидение МТС (ab-internet-mts): - Исправлен регион: Курган.

--- trunk/ab-internet-mts/library.js
+++ trunk/ab-internet-mts/library.js
@@ -251,10 +251,11 @@ for(var c=0;
 c<a.length;
 ++c){if(a[c]!=="--auto--"&&!isset(b[a[c]])){b[a[c]]=null
 }}if(!isset(b.__tariff)){b.__tariff=null
-}}function getElement(j,m,e,a){var d=getParam(m.toString(),null,null,/<(\w+)/);
-var c=m.exec(j);
+}}function getElement(j,n,e,a){var c=n.exec(j);
 if(!c){return null
 }var l=c.index;
+var m=j.substr(l,c[0].length);
+var d=getParam(m,null,null,/<(\w+)/);
 var f=new RegExp("(?:<"+d+"|</"+d+")[^>]*>","ig");
 f.lastIndex=l+c[0].length;
 var g=0;
@@ -267,7 +268,7 @@ if(b.charAt(1)=="/"){if(g==0){break
 }f.lastIndex=c.index+b.length
 }var h=j.length;
 if(c){h=c.index+c[0].length
-}m.lastIndex=h;
+}n.lastIndex=h;
 var k=j.substring(l,h);
 if(e){k=replaceAll(k,e)
 }if(a){k=a(k)

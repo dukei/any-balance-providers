445964c1d6cb9d975a0ee641ad36d12ed6835fad
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-08-01 12:44:03 +0000

    Onlinetrade (ab-bonus-onlinetrade): - Поддержка изменений в процедуре входа

--- trunk/ab-bonus-onlinetrade/main.js
+++ trunk/ab-bonus-onlinetrade/main.js
@@ -7,10 +7,7 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
 };
 
 function main() {
@@ -21,6 +18,8 @@ function main() {
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
+	AnyBalance.setCookie('www.onlinetrade.ru', 'beget', 'begetok');
+
 	try {
 		var html = AnyBalance.requestGet(baseurl + 'member/login.html', g_headers);
 	} catch(e){}

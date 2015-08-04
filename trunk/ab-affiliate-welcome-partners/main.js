47f0216f0cccf14a73e70980245a43bcf1dbe291
kirillpisarev23@gmail.com <kirillpisarev23@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-08-04 15:00:10 +0000

    Welcome Partners (ab-affiliate-welcome-partners): - Поддержка изменений на сайте.

--- trunk/ab-affiliate-welcome-partners/main.js
+++ trunk/ab-affiliate-welcome-partners/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://m.welcomepartners.com/';
+	var baseurl = 'https://m.welcomepartners.com/';
 	AnyBalance.setDefaultCharset('utf-8');
     
 	checkEmpty(prefs.login, 'Введите логин!');

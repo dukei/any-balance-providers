c9dbdc38728877c1d2ce1466516782eba96b625a
dmitry.kochin@gmail.com <dmitry.kochin@gmail.com@1f0edefa-e828-6bf1-598a-efb20914ff10>
2015-08-04 06:52:00 +0000

    PopUnder (ab-affilate-popunder): - Изменения в процедуре входа

--- trunk/ab-affilate-popunder/main.js
+++ trunk/ab-affilate-popunder/main.js
@@ -6,8 +6,7 @@ var g_headers = {
 	'Cache-Control':'max-age=0',
 	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Origin':'http://popunder.ru',
-	'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-	'Referer':'http://popunder.ru/main/auth/',
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
 	'Accept-Language':'ru,en;q=0.8'
 };
 
@@ -20,6 +19,12 @@ function main() {
 	checkEmpty(prefs.password, 'Введите пароль!');
 		
 	var html = AnyBalance.requestGet(baseurl + 'main/auth/', g_headers);
+
+	var cookieName = getParam(html, null, null, /\$\.cookie\(\s*'([^']*)/, replaceSlashes);
+	var cookieValue = getParam(html, null, null, /\$\.cookie\(\s*'[^']*',\s*'([^']*)/, replaceSlashes);
+
+	if(cookieName)
+		AnyBalance.setCookie('.popunder.ru', cookieName, cookieValue);
 	
 	if(!html || AnyBalance.getLastStatusCode() > 400)
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
@@ -27,6 +32,8 @@ function main() {
 	html = AnyBalance.requestPost(baseurl + 'main/auth/', [
 		['login', prefs.login],
 		['pass', prefs.password],
+		['x', '22'],
+		['y', '12'],
 		['authsubmit', 'Вход'],
     ], addHeaders({Referer: baseurl + 'main/auth/'}));
 	

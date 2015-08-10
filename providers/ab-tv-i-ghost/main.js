347180df540942fcd9f2ac1d28808ca2c334667d
Dmitry Kochin <kochin.du@krawlly.com>
2015-08-10 18:42:04 +0300

    i-ghost cardsharing

---
+++ providers/ab-tv-i-ghost/main.js
@@ -0,0 +1,48 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://www.i-ghost.biz/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestPost(baseurl + 'auth/auth_login', {
+		login: prefs.login,
+		password: prefs.password
+	}, addHeaders({Referer: baseurl + 'login'}));
+
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+
+	if (html !== "true") {
+		var error = html;
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error(error, null, /Ошибка авторизации/i.test(error));
+	}
+	
+	var result = {success: true};
+	
+	html = AnyBalance.requestGet(baseurl + 'setting/balance?_=' + new Date().getTime(), addHeaders({'X-Requested-With':'XMLHttpRequest', Referer: baseurl}));
+	getParam(html, result, 'balance', /Ваш Баланс:(.*)/i, replaceTagsAndSpaces, parseBalance);
+
+	html = AnyBalance.requestGet(baseurl + 'packet/show?_=' + new Date().getTime(), addHeaders({'X-Requested-With':'XMLHttpRequest', Referer: baseurl}));
+	getParam(html, result, '__tariff', /<h4[^>]*>([\s\S]*?)<\/h4>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'daysleft', /<b[^>]+title="До [^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'till', /<b[^>]+title="До ([^"]*)/i, replaceTagsAndSpaces, parseDateISO);
+	
+	AnyBalance.setResult(result);
+}

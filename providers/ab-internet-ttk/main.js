4b0aa259ae73bce2f3cbdfa8d8c0d7637a22503d
Dmitry Kochin <kochin.du@krawlly.com>
2015-09-01 20:30:18 +0300

    ТТК (Единый кабинет) (ab-internet-ttk): Поддержка нескольких лицевых счетов в кабинете

--- providers/ab-internet-ttk/main.js
+++ providers/ab-internet-ttk/main.js
@@ -12,52 +12,68 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://lk.ttk.ru/';
+	var baseurl = 'https://lk.ttk.ru';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login && /^\d{9}$/.test(prefs.login), 'Логин должен состоять только из девяти цифр!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	var html = AnyBalance.requestGet(baseurl + 'po/LoginForm.jsp', g_headers);
+	var html = AnyBalance.requestGet(baseurl + '/po/LoginForm.jsp', g_headers);
 	
-	html = AnyBalance.requestGet(baseurl + 'po/accounts?account=' + prefs.login, g_headers);
+	html = AnyBalance.requestGet(baseurl + '/po/accounts?account=' + prefs.login, g_headers);
 	
 	var j_username = getParam(html, null, null, /"value(?:[^"]*"){2}([^"]+)"/i, replaceTagsAndSpaces, html_entity_decode);
 	checkEmpty(j_username, 'Не удалось найти id пользователя с номером счета ' + prefs.login);
 	
-	html = AnyBalance.requestPost(baseurl + 'po/LoginSSO', {
+	html = AnyBalance.requestPost(baseurl + '/po/LoginSSO', {
 		'username':prefs.login,
 		'j_username':j_username,
 		'j_password':prefs.password,
 		'loginSource':'form'
-	}, addHeaders({Referer: baseurl + 'po/LoginForm.jsp'}));
+	}, addHeaders({Origin: baseurl, Referer: baseurl + 'po/LoginForm.jsp'}));
+
+	if(/errLogin/i.test(AnyBalance.getLastUrl())){
+		var error = getParam(html, null, null, /<em[^>]+id="[^"]*error(?:[^>](?!display:\s*none))*>([\s\S]*?)<\/em>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный пароль|Неверный лицевой/i.test(error));
+
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+
+	}
 	
-	html = AnyBalance.requestGet(baseurl + 'po/index.jsf', g_headers);
+	html = AnyBalance.requestGet(baseurl + '/po/index.jsf', g_headers);
 	
 	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
-		
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 	// Вошли, там может быть и несколько счетов, но пока нет доступа к такому кабинету, сделаем пока с одним
 	var result = {success: true};
 	
-	html = AnyBalance.requestGet(baseurl + 'po/rest/client/accounts/', g_headers);
+	html = AnyBalance.requestGet(baseurl + '/po/rest/client/accounts/', g_headers);
 	
 	var json = getJson(html);
 	
 	// возвращается массив со счетами, можно потом сделать поддержку нескольких счетов
-	var currAcc = json[0];
-	
+	var currAcc;
+	for(var i=0; i<json.length; ++i){
+		currAcc = json[i];
+		if(currAcc.accountNumber == prefs.login)
+			break;
+	}
+
+	if(!currAcc) {
+		AnyBalance.trace("Не удалось найти счет " + prefs.login + ", возьмем первый счет");
+		currAcc = json[0];
+	}
+
 	getParam(currAcc.accountNumber + '', result, '__tariff', null, replaceTagsAndSpaces, html_entity_decode);
 	getParam(currAcc.accountNumber + '', result, 'account', null, replaceTagsAndSpaces, html_entity_decode);
 	getParam(currAcc.accountBalance + '', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
 	
 	if(isAvailable('fio')) {
-		html = AnyBalance.requestGet(baseurl + 'po/rest/client/info/', g_headers);
+		html = AnyBalance.requestGet(baseurl + '/po/rest/client/info/', g_headers);
 		
 		json = getJson(html);
 

f7a287ffeb863239eff5023be64c0a5311b310b4
Dmitry Kochin <dco@mail.ru>
2015-09-12 15:14:04 +0300

    УКРСИББАНК (ab-money-ukrsibbank): Исправление получения информации по картам и счетам

--- providers/ab-money-ukrsibbank/extract.js
+++ providers/ab-money-ukrsibbank/extract.js
@@ -58,6 +58,8 @@ function login() {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
+
+	__setLoginSuccessful();
 	
 	return html;
 }
@@ -92,8 +94,10 @@ function processCards(html, result){
 }
 
 function processCard(html, result) {
+	if(!AnyBalance.isAvailable('cards'))
+		return;
+
 	getParam(result.__id, result, 'cards.num'); 
-	getParam(html, result, 'cards.type', /(<td[^>]*>[\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, ['cards.currency', 'cards.balance'], /(<td[^>]*>[\s\S]*?){7}<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'cards.balance', /(<td[^>]*>[\s\S]*?){8}<\/td>/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'cards.till', /(<td[^>]*>[\s\S]*?){2}<\/td>/i, replaceTagsAndSpaces, parseDate);
@@ -103,8 +107,8 @@ function processCard(html, result) {
 
 	var accinfo = getParam(html, null, null, /(<td[^>]*>[\s\S]*?){6}<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 	if(accinfo){
-		getParam(html, result, 'cards.accname', /([\s\S]*?)(?:\/|<br|$)/i, replaceTagsAndSpaces, html_entity_decode);
-		getParam(html, result, 'cards.accnum', /(?:<br[^>]*>)([\s\S]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(accinfo, result, 'cards.accname', /([\s\S]*?)(?:\/|$)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(accinfo, result, 'cards.accnum', /\/([\s\S]*)/i, replaceTagsAndSpaces, html_entity_decode);
 	}
 }
 
@@ -153,7 +157,9 @@ function processAccount(page, html, result) {
 	getParam(html, result, 'accounts.balance', /<td[^>]+class="amountColumn"[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
 
 	// Дополнительная инфа по счетам.
-	if (AnyBalance.isAvailable('own', 'avail', 'debt', 'fill', 'expense', 'overdraft', 'blocked')) {
+	if (AnyBalance.isAvailable('accounts.own', 'accounts.avail', 'accounts.debt', 'accounts.overdraft', 'accounts.blocked',
+			'accounts.balance_min', 'accounts.date_start', 'accounts.pct', 'accounts.transactions')) {
+
 		var wForm = getParam(html, null, null, /(welcomeForm:j_id_jsp_[^'"]+)/i, null, html_entity_decode);
 		var accountId = getParam(html, null, null, /accountId'\s*,\s*'(\d+)/i);
 		
@@ -166,47 +172,47 @@ function processAccount(page, html, result) {
 			}, addHeaders({Referer: g_baseurl + 'protected/welcome.jsf'}));
 			
 			//Собственные средства
-			getParam(html, result, ['own', 'blocked'], /&#1057;&#1086;&#1073;&#1089;&#1090;&#1074;&#1077;&#1085;&#1085;&#1099;&#1077; &#1089;&#1088;&#1077;&#1076;&#1089;&#1090;&#1074;&#1072;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, ['accounts.own', 'accounts.blocked'], /&#1057;&#1086;&#1073;&#1089;&#1090;&#1074;&#1077;&#1085;&#1085;&#1099;&#1077; &#1089;&#1088;&#1077;&#1076;&#1089;&#1090;&#1074;&#1072;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 			//Власні кошти
-			getParam(html, result, ['own', 'blocked'], /&#1042;&#1083;&#1072;&#1089;&#1085;&#1110; &#1082;&#1086;&#1096;&#1090;&#1080;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-			getParam(html, result, ['own', 'blocked'], /Own amount:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, ['accounts.own', 'accounts.blocked'], /&#1042;&#1083;&#1072;&#1089;&#1085;&#1110; &#1082;&#1086;&#1096;&#1090;&#1080;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, ['accounts.own', 'accounts.blocked'], /Own amount:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 			
 			//Доступная сумма
-			getParam(html, result, ['avail', 'blocked'], /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1072;&#1103; &#1089;&#1091;&#1084;&#1084;&#1072;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, ['accounts.avail', 'accounts.blocked'], /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1072;&#1103; &#1089;&#1091;&#1084;&#1084;&#1072;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 			//Доступна сума
-			getParam(html, result, ['avail', 'blocked'], /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1072; &#1089;&#1091;&#1084;&#1072;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-			getParam(html, result, ['avail', 'blocked'], /Available amount:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, ['accounts.avail', 'accounts.blocked'], /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1072; &#1089;&#1091;&#1084;&#1072;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, ['accounts.avail', 'accounts.blocked'], /Available amount:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 
-			getParam(html, result, 'debt', /&#1047;&#1072;&#1075;&#1072;&#1083;&#1100;&#1085;&#1072; &#1079;&#1072;&#1073;&#1086;&#1088;&#1075;&#1086;&#1074;&#1072;&#1085;&#1110;&#1089;&#1090;&#1100; &#1079;&#1072; &#1088;&#1072;&#1093;&#1091;&#1085;&#1082;&#1086;&#1084; &#1089;&#1082;&#1083;&#1072;&#1076;&#1072;&#1108;([\d\s.,]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.debt', /&#1047;&#1072;&#1075;&#1072;&#1083;&#1100;&#1085;&#1072; &#1079;&#1072;&#1073;&#1086;&#1088;&#1075;&#1086;&#1074;&#1072;&#1085;&#1110;&#1089;&#1090;&#1100; &#1079;&#1072; &#1088;&#1072;&#1093;&#1091;&#1085;&#1082;&#1086;&#1084; &#1089;&#1082;&#1083;&#1072;&#1076;&#1072;&#1108;([\d\s.,]+)/i, replaceTagsAndSpaces, parseBalance);
 			
 			//Неснижаемый остаток
-			getParam(html, result, 'balance_min', /&#1053;&#1077;&#1089;&#1085;&#1080;&#1078;&#1072;&#1077;&#1084;&#1099;&#1081; &#1086;&#1089;&#1090;&#1072;&#1090;&#1086;&#1082;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.balance_min', /&#1053;&#1077;&#1089;&#1085;&#1080;&#1078;&#1072;&#1077;&#1084;&#1099;&#1081; &#1086;&#1089;&#1090;&#1072;&#1090;&#1086;&#1082;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 			//Незнижувальний залишок
-			getParam(html, result, 'balance_min', /&#1053;&#1077;&#1079;&#1085;&#1080;&#1078;&#1091;&#1074;&#1072;&#1083;&#1100;&#1085;&#1080;&#1081; &#1079;&#1072;&#1083;&#1080;&#1096;&#1086;&#1082;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-			getParam(html, result, 'balance_min', /Minimum balance:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.balance_min', /&#1053;&#1077;&#1079;&#1085;&#1080;&#1078;&#1091;&#1074;&#1072;&#1083;&#1100;&#1085;&#1080;&#1081; &#1079;&#1072;&#1083;&#1080;&#1096;&#1086;&#1082;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.balance_min', /Minimum balance:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 
 			//Дата открытия
-			getParam(html, result, 'date_start', /&#1044;&#1072;&#1090;&#1072; &#1086;&#1090;&#1082;&#1088;&#1099;&#1090;&#1080;&#1103;:([^<]+)/i, replaceTagsAndSpaces, parseDate);
+			getParam(html, result, 'accounts.date_start', /&#1044;&#1072;&#1090;&#1072; &#1086;&#1090;&#1082;&#1088;&#1099;&#1090;&#1080;&#1103;:([^<]+)/i, replaceTagsAndSpaces, parseDate);
 			//Дата вiдкриття
-			getParam(html, result, 'date_start', /&#1044;&#1072;&#1090;&#1072; &#1074;i&#1076;&#1082;&#1088;&#1080;&#1090;&#1090;&#1103;:([^<]+)/i, replaceTagsAndSpaces, parseDate);
-			getParam(html, result, 'date_start', /Open date:([^<]+)/i, replaceTagsAndSpaces, parseDate);
+			getParam(html, result, 'accounts.date_start', /&#1044;&#1072;&#1090;&#1072; &#1074;i&#1076;&#1082;&#1088;&#1080;&#1090;&#1090;&#1103;:([^<]+)/i, replaceTagsAndSpaces, parseDate);
+			getParam(html, result, 'accounts.date_start', /Open date:([^<]+)/i, replaceTagsAndSpaces, parseDate);
 
 			//Ставка на остаток по счету:
-			getParam(html, result, 'pct', /&#1057;&#1090;&#1072;&#1074;&#1082;&#1072; &#1085;&#1072; &#1086;&#1089;&#1090;&#1072;&#1090;&#1086;&#1082; &#1087;&#1086; &#1089;&#1095;&#1077;&#1090;&#1091;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.pct', /&#1057;&#1090;&#1072;&#1074;&#1082;&#1072; &#1085;&#1072; &#1086;&#1089;&#1090;&#1072;&#1090;&#1086;&#1082; &#1087;&#1086; &#1089;&#1095;&#1077;&#1090;&#1091;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 			//Ставка на залишок за рахунком
-			getParam(html, result, 'pct', /&#1057;&#1090;&#1072;&#1074;&#1082;&#1072; &#1085;&#1072; &#1079;&#1072;&#1083;&#1080;&#1096;&#1086;&#1082; &#1079;&#1072; &#1088;&#1072;&#1093;&#1091;&#1085;&#1082;&#1086;&#1084;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.pct', /&#1057;&#1090;&#1072;&#1074;&#1082;&#1072; &#1085;&#1072; &#1079;&#1072;&#1083;&#1080;&#1096;&#1086;&#1082; &#1079;&#1072; &#1088;&#1072;&#1093;&#1091;&#1085;&#1082;&#1086;&#1084;:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 			//Rate on the account balance
-			getParam(html, result, 'pct', /Rate on the account balance:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.pct', /Rate on the account balance:([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 
 			//Доступный овердрафт:
-			getParam(html, result, 'overdraft', /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1099;&#1081; &#1086;&#1074;&#1077;&#1088;&#1076;&#1088;&#1072;&#1092;&#1090;:([^<]+)<\/td>\s*<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.overdraft', /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1099;&#1081; &#1086;&#1074;&#1077;&#1088;&#1076;&#1088;&#1072;&#1092;&#1090;:([^<]+)<\/td>\s*<\/tr>/i, replaceTagsAndSpaces, parseBalance);
 			//Доступний овердрафт:
-			getParam(html, result, 'overdraft', /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1080;&#1081; &#1086;&#1074;&#1077;&#1088;&#1076;&#1088;&#1072;&#1092;&#1090;:([^<]+)<\/td>\s*<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.overdraft', /&#1044;&#1086;&#1089;&#1090;&#1091;&#1087;&#1085;&#1080;&#1081; &#1086;&#1074;&#1077;&#1088;&#1076;&#1088;&#1072;&#1092;&#1090;:([^<]+)<\/td>\s*<\/tr>/i, replaceTagsAndSpaces, parseBalance);
 			//Available overdraft:
-			getParam(html, result, 'overdraft', /Available overdraft:([^<]+)<\/td>\s*<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+			getParam(html, result, 'accounts.overdraft', /Available overdraft:([^<]+)<\/td>\s*<\/tr>/i, replaceTagsAndSpaces, parseBalance);
 			
-			if(isAvailable(['blocked'])) {
-				getParam(result.own - result.avail, result, 'blocked');
+			if(isAvailable('accounts.blocked') && isset(result.own) && isset(result.avail)) {
+				getParam(result.own - result.avail, result, 'accounts.blocked');
 			}
 			
 			if(isAvailable('accounts.transactions')) {
@@ -226,8 +232,8 @@ function processAccountTransactions(html, accountId, result){
 	var year = dt.getFullYear()-1;
 	
 	var periodParams = createFormParams(form, function(params, str, name, value) {
-//		if (name == 'accountInfoForm:reportPeriod') 
-//			return '0';
+		if (/reportPeriod/i.test(name)) 
+			return '0';
 		if (/class="calendar"/i.test(str))
 			return n2(dt.getDate()) + '.' + month + '.' + (year++);
 
@@ -237,14 +243,19 @@ function processAccountTransactions(html, accountId, result){
 	
 	html = AnyBalance.requestPost(g_baseurl + action, periodParams, addHeaders({Referer: g_baseurl + 'protected/welcome.jsf'}));
 
-	var tables = getElements(html, /<table[^>]+class="opersTable"[^>]*>/ig);
+	var error = getElement(html, /<table[^>]+class="error"[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
+	if(error)
+		AnyBalance.trace('Выписка, похоже, отсутствует: ' + error);
+
+	var tables = getElements(html, [/<table[^>]*>/ig, /class="dateColumn"/i]);
 	for(var i=0; i<tables.length; ++i){
 		var table = tables[i];
-		var caption = getParam(table, null, null, /<caption>([\s\S]*?)<\/table>/i, replaceTagsAndSpaces, html_entity_decode);
+		var caption = getParam(table, null, null, /<caption>([\s\S]*?)<\/caption>/i, replaceTagsAndSpaces, html_entity_decode);
 		var cardid = caption && getParam(caption, null, null, /\d{6}\*{4}\d{4}/);
 		var comission = caption && caption.indexOf(result.__id) >= 0;
 		var transactions;
 		var path = '';
+		var balance = false;
 
 		if(cardid){
 			//Это карточные транзакции
@@ -257,12 +268,12 @@ function processAccountTransactions(html, accountId, result){
 			}
 
 			result.transactions_card.push(c);
-			transactions = c.transactions_card;
+			transactions = c.transactions;
 			path = 'accounts.transactions_card.transactions';
 		}else if(comission){
 			//Это списания комиссий
-			if(!result.transaction_comissions)
-				result.transaction_comissions = [];
+			if(!result.transactions_comission)
+				result.transactions_comission = [];
 
 			transactions = result.transactions_comission;
 			path = 'accounts.transactions_comission';
@@ -271,19 +282,27 @@ function processAccountTransactions(html, accountId, result){
 				result.transactions = [];
 			transactions = result.transactions;
 			path = 'accounts.transactions';
+			balance = /<th[^>]*>\s*Balance/i.test(table) ||
+				/<th[^>]*>\s*&#1054;&#1089;&#1090;&#1072;&#1090;&#1086;&#1082;/i.test(table) || //Остаток
+				/<th[^>]*>\s*&#1047;&#1072;&#1083;&#1080;&#1096;&#1086;&#1082;/i.test(table); //Залишок
 		}
 
-		var rows = getElements(table, [/<tr[^>]*>/ig, /<td[^>]*>/i]);
+		var rows = getElements(table, [/<tr[^>]*>/ig, /<td[^>]+class="dateColumn"[^>]*>/i]);
 		for(var j=0; j<rows.length; ++j){
 			var row = rows[j];
 			var t = {};
 			getParam(row, t, path + '.date', /(<td[^>]+class="dateColumn"[^>]*>[\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
 			getParam(row, t, path + '.date_done', /(<td[^>]+class="dateColumn"[^>]*>[\s\S]*?){2}<\/td>/i, replaceTagsAndSpaces, parseDate);
 			getParam(row, t, path + '.auth', /(<td[^>]+class="dateColumn"[^>]*>[\s\S]*?){3}<\/td>/i, replaceTagsAndSpaces, parseDate);
+			getParam(row, t, path + '.contra', /(<td[^>]+class="recipientColumn"[^>]*>[\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 			getParam(row, t, path + '.descr', /(<td\s*>[\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 			getParam(row, t, path + '.currency', /(<td[^>]+class="currencyColumn"[^>]*>[\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 			getParam(row, t, path + '.sum', /(<td[^>]+class="amountColumn"[^>]*>[\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-			getParam(row, t, path + '.sum_account', /(<td[^>]+class="amountColumn"[^>]*>[\s\S]*?){2}<\/td>/i, replaceTagsAndSpaces, parseBalance);
+			if(balance){
+				getParam(row, t, path + '.balance', /(<td[^>]+class="amountColumn"[^>]*>[\s\S]*?){2}<\/td>/i, replaceTagsAndSpaces, parseBalance);
+			}else{
+				getParam(row, t, path + '.sum_account', /(<td[^>]+class="amountColumn"[^>]*>[\s\S]*?){2}<\/td>/i, replaceTagsAndSpaces, parseBalance);
+			}
 
 			transactions.push(t);
 		}
@@ -292,4 +311,24 @@ function processAccountTransactions(html, accountId, result){
 
 function getViewState(html) {
 	return getParam(html, null, null, /javax.faces.ViewState[^>]*value="([^"]+)/i, null, html_entity_decode);
+}
+
+function processInfo(html, result){
+	//Добро пожаловать
+	getParam(html, result, 'fio', /&#1044;&#1086;&#1073;&#1088;&#1086; &#1087;&#1086;&#1078;&#1072;&#1083;&#1086;&#1074;&#1072;&#1090;&#1100;,([^<]*?)[.!]/i, replaceTagsAndSpaces, html_entity_decode);
+	//Ласкаво просимо
+	getParam(html, result, 'fio', /&#1051;&#1072;&#1089;&#1082;&#1072;&#1074;&#1086; &#1087;&#1088;&#1086;&#1089;&#1080;&#1084;&#1086;,([^<]*?)[.!]/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'fio', /Welcome,([^<]*?)[.!]/i, replaceTagsAndSpaces, html_entity_decode);
+
+	if(AnyBalance.isAvailable('mphone', 'email')){
+		html = AnyBalance.requestGet(g_baseurl + 'protected/service/sms/sms_channels.jsf', g_headers);
+		var table = getElement(html, /<tbody[^>]+smsForm:smsChannels[^>]*>/i);
+		if(table){
+			var rows = getElements(table, /<tr[^>]*>/ig);
+			for(var i = 0; i<rows.length; ++i){
+				var type = getParam(rows[i], null, null, /<td[^>]+channelColumn[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+				getParam(rows[i], result, /sms/i.test(type+'') ? 'mphone' : 'email', /<td[^>]+phonemailColumn[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+			}
+		}
+	}
 }

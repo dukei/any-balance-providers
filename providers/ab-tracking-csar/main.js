584f887633db07d43165217a82f625fa7ea0512f
Dmitry Kochin <kochin.du@krawlly.com>
2015-10-09 14:35:56 +0300

    Цезарь-Сателлит (ab-tracking-csar): Полное изменение личного кабинета

--- providers/ab-tracking-csar/main.js
+++ providers/ab-tracking-csar/main.js
@@ -37,6 +37,41 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 
+    var baseurl = "http://lk.csat.ru";
+
+    var html = AnyBalance.requestPost(baseurl + '/?login=yes', {
+    	AUTH_FORM: 'Y',
+    	TYPE: 'AUTH',
+    	USER_LOGIN: prefs.login,
+    	USER_PASSWORD: prefs.password,
+    	Login: 'Войти'
+    }, g_headers);
+
+    if(!/Logout/i.test(html)){
+        var error = getParam(html, null, null, /create_modal\s*\(\s*'error',\s*'[^']*','((?:[^']|\\')*)/, [replaceSlashes, replaceTagsAndSpaces]);
+        if(error)
+            throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+
+    var result = {success: true};
+
+    getParam(html, result, 'balance', /<li[^>]+class="balans"[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'daysleft', /<li[^>]+class="days"[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'plate', /<span[^>]+class="model"[^>]*>[\s\S]*?<\/div>/i, [/<span[^>]*>[^<]*<\/span>/i, '', replaceTagsAndSpaces], html_entity_decode);
+    getParam(html, result, '__tariff', /<li[^>]+class="tariff"[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'fio', /<li[^>]+class="name"[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'status', /<li[^>]+status[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'pin', /<li[^>]+pin[^>]*>([\s\S]*?)<\/strong>/i, replaceTagsAndSpaces, html_entity_decode);
+
+    AnyBalance.setResult(result);
+}
+
+function mainOld(){
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
+
     var baseurl = "http://cp.csat.ru/";
 
     var html = AnyBalance.requestGet(baseurl + 'Login.aspx', g_headers);

ea8cf50c7e2212e20d2b62763ccc946e53f6a2cf
Edward Anisonyan <stark@starksoft.ru>
2015-10-28 12:38:43 +0300

    Группа Компаний Электрон (Цифровое ТВ, Интернет) (ab-tv-itce): - Поддержка изменений на сайте.

--- providers/ab-tv-itce/main.js
+++ providers/ab-tv-itce/main.js
@@ -8,7 +8,6 @@ var g_headers = {
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
-	'X-Requested-With':'XMLHttpRequest'
 };
 
 function main() {
@@ -18,31 +17,32 @@ function main() {
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');	
 	
-	if(prefs.type == 'inet') {
-		mainInet(prefs);
-	} else {
-		mainTV(prefs);
+	var host = prefs.type || 'tv';
+	var baseurl = 'https://itce.ru/' + host + '/billing/';
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
-}
-
-function mainInet(prefs) {
-	var baseurl = 'https://itce.ru/inet/billing/';
 	
 	var html = AnyBalance.requestPost(baseurl + '?page=billing', {
-		'host':'inet.itce.ru',
+		'host': host + '.itce.ru',
 		'options[page]':'billing',
 		'module':'inetBilling',
 		'account':prefs.login,
 		'password':prefs.password,
 		'undefined':'Вход',
-    }, addHeaders({Referer: baseurl + '?page=billing'}));
+    }, addHeaders({Referer: baseurl + '?page=billing', 'X-Requested-With':'XMLHttpRequest'}));
 	
 	if(!/>выход<|^ok$/i.test(html)) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
+	
 	html = AnyBalance.requestPost(baseurl + 'core.php', {
-		'host':'inet.itce.ru',
+		'host': host + '.itce.ru',
 		'options[page]':'billing',
 		'module':'inetBilling',
     }, addHeaders({Referer: baseurl + 'core.php'}));
@@ -50,34 +50,8 @@ function mainInet(prefs) {
     var result = {success: true};
 	getParam(html, result, 'balance', /Ваш баланс:[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'fio', /<h2>([^<.]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'agreement', /Договор:([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 	
-    AnyBalance.setResult(result);
-}
-
-function mainTV(prefs) {
-	var baseurl = 'http://tv.itce.ru/';
-	var html = AnyBalance.requestPost(baseurl + 'core.php', {
-		'host':'tv.itce.ru',
-		'options[page]':'billing',
-		'module':'ctvBilling',
-		'account':prefs.login,
-		'password':prefs.password,
-		'undefined':'Вход',
-    }, addHeaders({Referer: baseurl + 'core.php'}));
-	
-	if(!/ok/i.test(html)) {
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-	}
-	html = AnyBalance.requestPost(baseurl + 'core.php', {
-		'host':'tv.itce.ru',
-		'options[page]':'billing',
-		'module':'ctvBilling',
-    }, addHeaders({Referer: baseurl + 'core.php'}));
-	
-    var result = {success: true};
-	getParam(html, result, 'balance', /Ваш баланс:[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'fio', /<h2>([^\.]*)\.\s*Договор/i, replaceTagsAndSpaces, html_entity_decode);
 	
     AnyBalance.setResult(result);
 }

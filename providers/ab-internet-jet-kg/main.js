3383277422dc71c2cace6ab4cb4b7bbedda727fc
Xenia <adri.espanol@gmail.com>
2015-11-08 17:07:18 +0300

    Добавлен провайдер 'Jet.kg'

---
+++ providers/ab-internet-jet-kg/main.js
@@ -0,0 +1,52 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://jet.kg/jet/';
+	AnyBalance.setDefaultCharset('Windows-1251');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+	
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'login') 
+			return prefs.login;
+		return value;
+	});
+	
+	html = AnyBalance.requestGet(baseurl + 'balance_show.php?bill_type=2&acc_id=' + prefs.login)
+	
+	if (!/Состояние лицевого счёта/i.test(html)) {
+		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+
+	var result = {success: true};
+	
+	getParam(html, result, 'balance', /Состояние лицевого счёта:[\s\S]*?<font(.*)>-?(\d+)?[.,]\d+(.*?)\/font>/g , replaceTagsAndSpaces, parseBalance);
+	
+	AnyBalance.setResult(result);
+}

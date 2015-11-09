24fe4be5bcaa74395c0e73bc08d387c0e4a280a8
Xenia <adri.espanol@gmail.com>
2015-11-09 13:04:56 +0300

    Изменила регулярное выражение

--- providers/ab-internet-jet-kg/main.js
+++ providers/ab-internet-jet-kg/main.js
@@ -7,9 +7,6 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
@@ -26,21 +23,18 @@ function main() {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
-	
+
 	html = AnyBalance.requestGet(baseurl + 'balance_show.php?bill_type=2&acc_id=' + prefs.login)
 	
 	if (!/Состояние лицевого счёта/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
-		
+			throw new AnyBalance.Error(error, null, /Неверный номер лицевого счёта/i.test(error));
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 
 	var result = {success: true};
 	
-	getParam(html, result, 'balance', /Состояние лицевого счёта:[\s\S]*?<font(.*)>-?(\d+)?[.,]\d+(.*?)\/font>/g , replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance', /<div[^>]+?class="balans"[^>]*>[\s\S]*?Состояние\s+лицевого\s+счёта[\s\S]*?<\/div>/g , replaceTagsAndSpaces, parseBalance);
 	
 	AnyBalance.setResult(result);
 }

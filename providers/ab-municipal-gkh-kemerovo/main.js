c0c1cca9ffa6e4cf63b991ef535d09c83c4df3db
Xenia <adri.espanol@gmail.com>
2015-11-12 22:36:29 +0300

    Добавила счётчики

--- providers/ab-municipal-gkh-kemerovo/main.js
+++ providers/ab-municipal-gkh-kemerovo/main.js
@@ -7,18 +7,19 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36}'
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'https://www.gkh-kemerovo.ru/portal/';
 	AnyBalance.setDefaultCharset('utf-8');
-	
+
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
-	
+
 	var html = AnyBalance.requestGet(baseurl + 'index.php', g_headers);
-	
+
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
@@ -28,7 +29,8 @@ function main() {
 		t:'street'
 	})
 
-	var treadValue = getParam(hintPage, null, null, /\/|(\d+)$/i, replaceTagsAndSpaces);
+	var treadValue = getParam(hintPage, null, null, /(\d+)$/i, replaceTagsAndSpaces);
+
 
 	html = AnyBalance.requestPost(baseurl+'index.php', {
 		logintype:	'1',
@@ -39,22 +41,24 @@ function main() {
 		'Remember': 'false'
 	}, addHeaders({Referer: baseurl + 'index.php'}));
 
+
 	if (!/\?s=exit/i.test(html)) {
-		var error = getParam(html, null, null, /<td[^>]+id=(?:'|")el(?:'|")[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+		var error = getParam(html, null, null, /id=["']el["'][^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
 		if(error)
 			throw new AnyBalance.Error(error, null, /Неверный логин\/пароль/i.test(error));
 
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error("Не удалось зайти в личный кабинет. Сайт изменен?");
-		
 	}
 
 	var result = {success: true};
 
-	if(!/Задолженность/i.test(html))
-		getParam(html, result, 'balance', /<td[^>]*>(?:(?!<\/?td>)[\s\S])*?вашего счета((?:(?!<\/?td>)[\s\S])*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-	else
-		getParam(html, result, 'balance', /<td[^>]*>(?:(?!<\/?td>)[\s\S])*?вашего счета((?:(?!<\/?td>)[\s\S])*?)<\/td>/i, [[ /(задолженность:)/i, '-'], replaceTagsAndSpaces], parseBalance);
+	getParam(html, result, 'balance', /<td[^>]*>(?:(?!<\/?td>)[\s\S])*?вашего счета((?:(?!<\/?td>)[\s\S])*?)<\/td>/i, [[ /(задолженность:)/i, '-'], replaceTagsAndSpaces], parseBalance);
+	getParam(html, result, 'acc_num', /Лицевой счет[\s\S]*?<\/big>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'adress', /<big[^>]*>\(([\s\S]*?)\)[\s\S]*?<\/big>/i);
 
+	html = AnyBalance.requestGet(baseurl+'index.php?show=param', g_headers);
+
+	getParam(html, result, 'fio', /name=["']lgn["'][\s\S]*?value=['"]([\s\S]*?)['"][^>]*>/i)
 	AnyBalance.setResult(result);
 }

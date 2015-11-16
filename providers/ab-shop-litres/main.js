0ed2bcec98861384f69a11dbd2aae4cd74f60395
Dmitry Kochin <kochin.du@krawlly.com>
2015-11-16 18:41:29 +0300

    ЛитРес (ab-shop-litres): - Поддержка изменений на сайте.

--- providers/ab-shop-litres/main.js
+++ providers/ab-shop-litres/main.js
@@ -7,9 +7,6 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
@@ -28,15 +25,6 @@ function main() {
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 	
-	var params = createFormParams(html, function(params, str, name, value) {
-		if (name == 'login') 
-			return prefs.login;
-		else if (name == 'password')
-			return prefs.password;
-
-		return value;
-	});
-	
 	html = AnyBalance.requestPost(baseurl, {
 		'pre_action': 'login',
 		'login': prefs.login,
@@ -47,7 +35,7 @@ function main() {
 	}, addHeaders({Referer: baseurl + 'pages/login/'}));
 	
 	if (!/logoff/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+		var error = getParam(html, null, null, /alert\s*\(\s*["']([^"']*)/i, replaceSlashes);
 		if (error)
 			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
 		
@@ -57,7 +45,7 @@ function main() {
 	
 	var result = {success: true};
 	
-	getParam(html, result, 'balance', /amount\s*:\s*([-.\d]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance', /<li[^>]+"cash"[^>]*>([\s\S]*?)<\/li>/i, replaceTagsAndSpaces, parseBalance);
 	
 	AnyBalance.setResult(result);
 }

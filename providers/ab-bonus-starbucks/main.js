67c52c779855ea9f197ef7434a387d31ab22410f
Xenia <adri.espanol@gmail.com>
2015-11-20 12:51:02 +0300

    Мелкие изменения

--- providers/ab-bonus-starbucks/main.js
+++ providers/ab-bonus-starbucks/main.js
@@ -7,7 +7,7 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
 };
 
 function main() {
@@ -25,19 +25,15 @@ function main() {
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 
-	var eventValidation = getParam(html, null, null, /id="__EVENTVALIDATION"[\s\S]*?value="([\s\S]*?)"[^>]*>/i);
-	var viewStateGen = getParam(html, null, null, /id="__VIEWSTATEGENERATOR"[\s\S]*?value="([\s\S]*?)"[^>]*>/i);
-	var viewState = getParam(html, null, null, /id="__VIEWSTATE"[\s\S]*?value="([\s\S]*?)"[^>]*>/i);
-	html = AnyBalance.requestPost(baseurl + 'mainlogin=true&style=starbucks', {
-		LoginTextBox1: prefs.login,
-		PasswordTextBox2: prefs.password,
-		__EVENTVALIDATION: eventValidation,
-		__VIEWSTATE: viewState,
-		__VIEWSTATEGENERATOR: viewStateGen,
-		LoginLinkButtonDynamic: 'Войти',
-		offset: 3,
-		'Remember': 'false'
-	}, addHeaders({Referer: baseurl + 'mainlogin=true&style=starbucks'}));
+	var params = createFormParams(html, function(params, str, name, value) {
+		if (name == 'LoginTextBox1')
+			return prefs.login;
+		else if (name == 'PasswordTextBox2')
+			return prefs.password;
+		return value;
+	});
+
+	html = AnyBalance.requestPost(baseurl + 'mainlogin=true&style=starbucks', params, addHeaders({Referer: baseurl + 'mainlogin=true&style=starbucks'}));
 
 	if (!/Баланс карты/i.test(html)) {
 		var error = getParam(html, null, null, /<div[^>]+id="pnlMessage"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -51,21 +47,24 @@ function main() {
 	var result = {success: true};
 	
 	getParam(html, result, 'stars', /pnlCardInfo[^>]*>[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'cardNumber', /lblCardNumber[^>]*>[\s\S]*?<\//i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'cardNumber', /lblCardNumber[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'balance', /lblCardBalance[^>]*>[\s\S]*?<\//i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'status', /lblCardStatus[^>]*>([^;]*);/i, replaceTagsAndSpaces);
 
-	eventValidation = getParam(html, null, null,/id="__EVENTVALIDATION"[\s\S]*?value="([\s\S]*?)"[^>]*>/i);
-	viewState = getParam(html, null, null, /id="__VIEWSTATE"[\s\S]*?value="([\s\S]*?)"[^>]*>/i);
+	var eventValidation = getParam(html, null, null,/id="__EVENTVALIDATION"[^>]*value="([^"]+)/i);
+	var viewState = getParam(html, null, null, /id="__VIEWSTATE"[^>]*value="([^"]+)/i);
 	html = AnyBalance.requestPost(baseurl+'style=starbucks',{
-		__VIEWSTATEGENERATOR: viewStateGen,
+		__VIEWSTATEGENERATOR: params['__VIEWSTATEGENERATOR'],
 		__VIEWSTATE: viewState,
 		offset:3,
 		__EVENTVALIDATION:eventValidation,
-		__EVENTTARGET: 'CardsEnableForm',
+		__EVENTTARGET: 'CardsEnableForm'
 	}, addHeaders({Referer: baseurl+ 'style=starbucks'}));
 
-	getParam(html, result, 'fName', /name="tbProfileFirstName"[^>]+value="([\s\S]*?)"[^>]*>/i, replaceTagsAndSpaces);
-	getParam(html, result, 'sName', /name="tbProfileSurname"[^>]+value="([\s\S]*?)"[^>]*>/i, replaceTagsAndSpaces)
+	var fName = getParam(html, null, null, /name="tbProfileFirstName"[^>]+value="([^"]+)/i, replaceTagsAndSpaces, html_entity_decode) || '';
+	var sName = getParam(html, null, null, /name="tbProfileSurname"[^>]+value="([^"]+)/i, replaceTagsAndSpaces, html_entity_decode || '');
+
+	getParam(fName + ' ' + sName, result, 'fio');
+
 	AnyBalance.setResult(result);
 }

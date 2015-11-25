9c7504dc8cd3c94fe7078d7da46645a47ec7e177
Olga Malanova <olgamalanova@gmail.com>
2015-11-25 17:08:16 +0300

    #948071

--- providers/ab-service-arms/main.js
+++ providers/ab-service-arms/main.js
@@ -13,41 +13,42 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://www.smartutilities.com.mt/wps/portal/';
-	// AnyBalance.setDefaultCharset('utf-8');
+	var baseurl = 'https://www.smartutilities.com.mt/wps/';
+	AnyBalance.setDefaultCharset('utf-8');
 	
-	checkEmpty(prefs.login, 'Input user ID');
+	checkEmpty(prefs.userid, 'Input user ID');
 	checkEmpty(prefs.password, 'Input password');
 	
-	var html = AnyBalance.requestGet(baseurl + 'Public%20Area/wps.Login/!ut/p/b1', g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'portal/Public%20Area/wps.Login/!ut/p/b1', g_headers);
 	
 	if(!html || AnyBalance.getLastStatusCode() > 400)
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	
     var params = {
-    	'wps.portlets.userid': prefs.userid,
-    	'password'=prefs.password,
-    	'ns_Z7_CGAH47L008LG50IAHUR9Q330U1__login': 'Login'
-    }
-	html = AnyBalance.requestPost(baseurl + 'Public%20Area/wps.Login/!ut/p/b1', params, addHeaders({Referer: baseurl + 'login.html'}));
+		'wps.portlets.userid': prefs.userid,
+		'password': prefs.password,
+		'ns_Z7_CGAH47L008LG50IAHUR9Q330U1__login': 'Вход в систему'
+    };
+    var loginurl = 'portal/!ut/p/b0/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKd3R09TMx9DAwsfNxNDTwdPUKDLAONjQ1czfULsh0VAW9IRPw!/';
+	html = AnyBalance.requestPost(baseurl + loginurl, params);
 	
-	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /[^>]+class="error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
+	if (!/Выход из системы/i.test(html)) {
+		var error = getParam(html, null, null, /class="wpsFieldSuccessText"[\s\S]*?<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль|Имя пользователя и пароль не совпадают/i.test(error));
+			throw new AnyBalance.Error(error, null, true);
 		
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
     
-    html = AnyBalance.requestGet(baseurl + 'moi-zakazay.html', g_headers);
+    html = AnyBalance.requestGet(baseurl + 'myportal/!ut/p/b1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKd3Y0CzYzdfMw8Q11dDBxNfENcQr19DA28zfULsh0VAWCRPls', g_headers);
 	
 	var result = {success: true};
 	
-	getParam(html, result, 'order', /Заказ\s№(\d*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'date', /(?:Заказ\s№[^>]*>)([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'status', /Статус[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'sum', /Общая сумма([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
+	// getParam(html, result, 'subtotal', /Payment Subtotal[\s\S]*?\/>/i, replaceTagsAndSpaces, parseBalance);
+	// getParam(html, result, 'date', /(?:Заказ\s№[^>]*>)([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	// getParam(html, result, 'status', /Статус[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	// getParam(html, result, 'sum', /Общая сумма([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 	
 	AnyBalance.setResult(result);
 }

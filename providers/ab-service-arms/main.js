e32122aa54fad6c6e04cbf97a56ab136161fa56b
Olga Malanova <olgamalanova@gmail.com>
2015-11-26 00:52:13 +0300

    arms 400 error POST

--- providers/ab-service-arms/main.js
+++ providers/ab-service-arms/main.js
@@ -19,7 +19,9 @@ function main() {
 	checkEmpty(prefs.userid, 'Input user ID');
 	checkEmpty(prefs.password, 'Input password');
 	
-	var html = AnyBalance.requestGet(baseurl + 'portal/Public%20Area/wps.Login/!ut/p/b1', g_headers);
+
+	var loginurl = 'portal/Public%20Area/wps.Login/!ut/p/b1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKd3R09TMx9DAwsfNxNDTwdPUKDLAONjQ1czYEKIoEKDHAARwOofqNAM2M3H7NAc2dXA0cTM18XF_8wIwNPI6h-XBY4GhNnPx4LCOgP14_CqwTkArACPF7088jPTdUvyA0NjTDIMgEAMTbqew!!/dl4/d5/L0lDUWtpQ1NZSkNncFJBISEvb0VvZ0FFQ1FRREdJUXBTR0djRndUT0EhLzRHMGhSQjdRUjM1UWhTWkNuNnBoL1o3X0NHQUg0N0wwMDhMRzUwSUFIVVI5UTMzMFUxLzAvd3BzLnBvcnRsZXRzLmxvZ2lu/';
+	var html = AnyBalance.requestGet(baseurl + loginurl, g_headers);
 	
 	if(!html || AnyBalance.getLastStatusCode() > 400)
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
@@ -27,11 +29,15 @@ function main() {
     var params = {
 		'wps.portlets.userid': prefs.userid,
 		'password': prefs.password,
-		'ns_Z7_CGAH47L008LG50IAHUR9Q330U1__login': 'Вход в систему'
+		'ns_Z7_CGAH47L008LG50IAHUR9Q330U1__login': 'Log in'
     };
-    var loginurl = 'portal/!ut/p/b0/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKd3R09TMx9DAwsfNxNDTwdPUKDLAONjQ1czfULsh0VAW9IRPw!/';
-	html = AnyBalance.requestPost(baseurl + loginurl, params);
+    // var loginurl = 'portal/!ut/p/b0/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKd3R09TMx9DAwsfNxNDTwdPUKDLAONjQ1czfULsh0VAW9IRPw!/';
 	
+	html = AnyBalance.requestPost(baseurl + loginurl, params,
+		addHeaders({'Referer':'https://www.smartutilities.com.mt/wps/portal/Public%20Area/wps.Login/!ut/p/b1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKd3Y0CzYzdfMwCzZ1dDRxNzHxdXPzDjAw8jYAKIoEKDHAARwNC-sP1o6BKHD1MzH0MDCx83E0NPB09QoMsA42NDRyNoQrwWOHnkZ-bql-QG2GQZeKoCABxOILM/dl4/d5/L2dJQSEvUUt3QS80SmtFL1o2X0NHQUg0N0wwMDhMRzUwSUFIVVI5UTMzMEU3/'}));
+	html = AnyBalance.requestGet(baseurl + 'https://www.smartutilities.com.mt/wps/myportal/Public%20Area/ARMS.CustomerArea.loginPage/ARMS.AccountManagement/billingOverview2/!ut/p/b1/04_SjzSyMDI3MjMztNCP0I_KSyzLTE8syczPS8wB8aPM4p3djQLNjN18zDxDXV0MHE18Q1xCvX0MDbzNgQoiwQocPUzMfQwMLHzcTQ08HT1CgywDjY0NHI2J02-AAzgaENIfrh-FX4kpAQUmhnAFuP0AVoDHkX4e-bmp-rlROW6WnpkB6Y6KigAYi_Lk/dl4/d5/L2dBISEvZ0FBIS9nQSEh/',
+		addHeaders({'Referer':'https://www.smartutilities.com.mt/wps/portal/Public%20Area/wps.Login/!ut/p/b1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOKd3Y0CzYzdfMwCzZ1dDRxNzHxdXPzDjAw8jYAKIoEKDHAARwNC-sP1o6BKHD1MzH0MDCx83E0NPB09QoMsA42NDRyNoQrwWOHnkZ-bql-QG2GQZeKoCABxOILM/dl4/d5/L2dJQSEvUUt3QS80SmtFL1o2X0NHQUg0N0wwMDhMRzUwSUFIVVI5UTMzMEU3/'}));
+
 	if (!/Выход из системы/i.test(html)) {
 		var error = getParam(html, null, null, /class="wpsFieldSuccessText"[\s\S]*?<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)

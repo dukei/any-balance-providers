50949dd35afcad0f6f6d04e681e7e0d125c20b22
Dmitry Kochin <kochin.du@krawlly.com>
2015-11-25 11:59:51 +0300

    Красноярскэнергосбыт (ab-municipal-krasenergo): - Поддержка изменений на сайте.

--- providers/ab-municipal-krasenergo/main.js
+++ providers/ab-municipal-krasenergo/main.js
@@ -22,48 +22,49 @@ function main(){
 
     // Передача фактических показаний прибора учета не работает на сайте
     var html = AnyBalance.requestPost(baseurl, {
-        abonentid: prefs.abonentid,
-        fam: prefs.fam,
-        login: prefs.login,
-        password: prefs.password,
-		//SendCounter:'Передача фактических показаний прибора учета',
-        qm:threeMonthsAgo.getMonth(),
-        qy:threeMonthsAgo.getFullYear(),
-        ShowState:'Просмотреть состояние счета, оплаты, начисления ...'
+        abonentid: prefs.login || prefs.abonentid,
+        fam: prefs.password  || prefs.fam,
+        showpass: 'on',
+        main: 'Войти'
     }, g_headers);
 
     //Ссылка на печать квитанции
     if(!/Лицевой cчет N/i.test(html)){
         var error = getParam(html, null, null, [/<div[^>]+class="error"[^>]*>([\s\S]*?)<\/div>/i, /<\/form>(?:\s+|<br[^>]*>)*<strong[^>]*>([\s\S]*?)<\/strong>/i], replaceTagsAndSpaces, html_entity_decode);
         if(error)
-            throw new AnyBalance.Error(error, null, /Введены некорректные данные/i.test(html));
+            throw new AnyBalance.Error(error, null, /некорректные данные/i.test(html));
         AnyBalance.trace(html);
-        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неправильный номер счета или фамилия?');
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
     }
 
-    var result = {success: true};
+    var result = {success: true}, tr;
 
     getParam(html, result, 'fio', /Абонент:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'licschet', /Лицевой cчет N:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'balance', /((?:Переплата|Задолженность):[\s\S]*?<td[^>]*>[\s\S]*?)<\/td>/i, [/Задолженность:/ig, '-', replaceTagsAndSpaces], parseBalance);
-	getParam(html, result, 'indication', /<th>\s*Предыдущее показание(?:[\s\S]*?<td[^>]*>){4}([\s\d]+)/i, replaceTagsAndSpaces, parseBalance);
 	
-    var tr = getParam(html, null, null, /История платежей:(?:[\s\S](?!<\/table>))*?(<tr[^>]*>(?:[\s\S](?!<\/tr>|<\/table>))*?[\s\S]<\/tr>)\s*<\/table>/i);
-    if(tr){
-        getParam(tr, result, 'lastpaydate', /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
-        getParam(tr, result, 'lastpaysum', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	if(AnyBalance.isAvailable('lastpaydate', 'lastpaysum')){
+		html = AnyBalance.requestGet(baseurl + '?action=payment_history', g_headers);
+        tr = getParam(html, null, null, /<table[^>]+quasar_table[^>]*>(?:[\s\S](?!<\/table>))*?(<tr[^>]*>(?:[\s\S](?!<\/tr>|<\/table>))*?[\s\S]<\/tr>)\s*<\/table>/i);
+        if(tr){
+            getParam(tr, result, 'lastpaydate', /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+            getParam(tr, result, 'lastpaysum', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        }
     }
 
-    tr = getParam(html, null, null, /История начислений:(?:[\s\S](?!<\/table>))*?(<tr[^>]*>(?:[\s\S](?!<\/tr>|<\/table>))*?[\s\S]<\/tr>)\s*<\/table>/i);
-    if(tr){
-        getParam(tr, result, 'lastbilldate', /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
-        getParam(tr, result, 'indication', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(tr, result, 'cons_ind', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(tr, result, 'cost_ind', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(tr, result, 'cons_com', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(tr, result, 'cost_com', /(?:[\s\S]*?<td[^>]*>){7}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(tr, result, 'cons_tot', /(?:[\s\S]*?<td[^>]*>){8}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(tr, result, 'cost_tot', /(?:[\s\S]*?<td[^>]*>){9}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+    if(AnyBalance.isAvailable('lastbilldate', 'indication','cons_ind','cost_ind','cons_com','cost_com','cons_tot','cost_tot')){
+		html = AnyBalance.requestGet(baseurl + '?action=invoice_history', g_headers);
+        tr = getParam(html, null, null, /<table[^>]+quasar_table[^>]*>(?:[\s\S](?!<\/table>))*?(<tr[^>]*>(?:[\s\S](?!<\/tr>|<\/table>))*?[\s\S]<\/tr>)\s*<\/table>/i);
+        if(tr){
+            getParam(tr, result, 'lastbilldate', /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+            getParam(tr, result, 'indication', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+            getParam(tr, result, 'cons_ind', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+            getParam(tr, result, 'cost_ind', /(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+            getParam(tr, result, 'cons_com', /(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+            getParam(tr, result, 'cost_com', /(?:[\s\S]*?<td[^>]*>){7}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+            getParam(tr, result, 'cons_tot', /(?:[\s\S]*?<td[^>]*>){8}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+            getParam(tr, result, 'cost_tot', /(?:[\s\S]*?<td[^>]*>){9}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+        }
     }
 
     AnyBalance.setResult(result);

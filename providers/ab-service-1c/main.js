10a2169ab10225946dc7295cdb0c9f2178190547
+ViktorSinitsyn <mousesport007@ya.ru>
2015-11-28 19:26:05 +0300

    ab-service-1c - так и не сделал(

--- providers/ab-service-1c/main.js
+++ providers/ab-service-1c/main.js
@@ -9,7 +9,7 @@ var g_headers = {
 	'Connection':'keep-alive',
 	'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
 };
-
+	
 function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = 'https://login.1c.ru/';
@@ -27,18 +27,44 @@ function main(){
 	});	
 	
 	params['rememberMe'] = undefined;
-	
-	html = AnyBalance.requestPost(baseurl + 'login', params, addHeaders({Referer: baseurl + 'login'})); 
-
-    if(!/logout/i.test(html)){
-        var error = getParam(html, null, null, /msg"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
 		
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-	AnyBalance.trace('Авторизация выполнена, начинаю парсить'); 
-    html = AnyBalance.requestGet('https://portal.1c.ru/subscription/contract/list', g_headers);
+	//html = AnyBalance.requestPost(baseurl + 'login', params, addHeaders({Referer: 'https://users.v8.1c.ru/'})); 
+	//AnyBalance.sleep(1000);
+	/*
+	Cookies					136				
+JSESSIONID	C72B772CDFCEECC8FAC9F5D1B6B36D3E.usersv81c8010	N/A	N/A	N/A	59				
+_ga	GA1.2.1963939383.1448058349	N/A	N/A	N/A	33				
+_gat	1	N/A	N/A	N/A	6				
+client_language	ru_RU	N/A	N/A	N/A	23				
+i18next	ru-RU	N/A	N/A	N/A	15				
+Response Cookies					0				
+
+	*/
+	
+	//AnyBalance.setCookie('https://portal.1c.ru/software', 'JSESSIONID', null, {path: '/'});
+	//AnyBalance.setCookie('https://portal.1c.ru/software', '_gat', null, {path: '/'});
+	//html = AnyBalance.requestGet('https://users.v8.1c.ru/', g_headers); 
+	//function randomString(length) {var result = '', chars = '0123456789';for (var i = length; i > 0; --i) {	result += chars[Math.round(Math.random() * (chars.length - 1))];}return result;}
+	// Ищи новый способ, как нас заблокировать.
+	//AnyBalance.setCookie('https://portal.1c.ru/software', '_ga', 'GA1.2.' + randomString(7) + '.' + randomString(10));
+	//html = AnyBalance.requestGet('https://portal.1c.ru/software', g_headers);
+	// AnyBalance.setCookie('https://users.v8.1c.ru/', '_ga', 'GA1.2.' + randomString(9) + '.' + randomString(10));
+	// html = AnyBalance.requestGet('https://portal.1c.ru/software', g_headers); 
+
+	// var ticket = getParam(html, null, null, /\?ticket=([^"]+)/i);
+
+	// html = AnyBalance.requestGet('https://login.1c.ru/user/profile'); 
+
+	// if(!/logout/i.test(html)){
+	// var error = getParam(html, null, null, /msg"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	// if(error)
+		// throw new AnyBalance.Error(error);
+
+	// throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	// }
+	// AnyBalance.trace('Авторизация выполнена, начинаю парсить'); 
+    var html = AnyBalance.requestGet('https://portal.1c.ru/subscription/contract/list', g_headers);
+	html = AnyBalance.requestGet('https://login.1c.ru/login?service=https%3A%2F%2Fportal.1c.ru%2Fpublic%2Fsecurity_check', g_headers); 
 	
 	if(/У Вас нет действующих или будущих договоров 1С/i.test(html))
 		throw new AnyBalance.Error('У Вас нет активных подписок на ИТС');

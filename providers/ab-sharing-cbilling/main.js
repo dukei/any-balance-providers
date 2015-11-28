7624c82c3dd9bf1b030c3660b27dbeb00cf90635
+ViktorSinitsyn <mousesport007@ya.ru>
2015-11-28 19:12:21 +0300

    ab-sharing-cbilling - поддержка изменений

--- providers/ab-sharing-cbilling/main.js
+++ providers/ab-sharing-cbilling/main.js
@@ -11,39 +11,34 @@ var g_headers = {
 };
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://cbilling.net/';
+	var baseurl = 'https://cbilling.bz/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	if(!prefs.dbg) {
-		var html = AnyBalance.requestGet(baseurl + '?mode=auth', g_headers);
+	//if(!prefs.dbg) {
+	var html = AnyBalance.requestGet(baseurl + '?mode=auth', g_headers);
 		
-		var captchaa, challenge, site;
-		if(AnyBalance.getLevel() >= 7){
-			AnyBalance.trace("Пытаемся ввести капчу");
-			var captchascript = AnyBalance.requestGet(getParam(html, null, null, /<script[^>]*src="(https:\/\/www.google.com\/recaptcha\/api\/challenge[^"]*)/i), g_headers);
-			
-			challenge = getParam(captchascript, null, null, /challenge\s*:\s*'([^']*)/i, replaceTagsAndSpaces);
-			site = getParam(captchascript, null, null, /site\s*:\s*'([^']*)/i, replaceTagsAndSpaces);
-			
-			//https://www.google.com/recaptcha/api/image?c=03AHJ_VuuzqNlBBECwEQGKOXBge-m_CxMRdNu9fcuFbOerxQt5RURzXmCdP-cKgAYBJ4xdpNwO9WRlfoUfNFygz-UV4v03Z41GI6XWioxMy-uxJFQNUtQ9qNtQ3ytOMOjFZwPNRICHfapiiRUsN9_8CW_j54wyjGbujg
-			AnyBalance.setOptions({FORCE_CHARSET: 'base64'});
-			var captcha = AnyBalance.requestGet('https://www.google.com/recaptcha/api/image?c=' + challenge + '&k=' + site, g_headers);
-			captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
-			AnyBalance.setOptions({FORCE_CHARSET: null});
-			AnyBalance.trace('Капча получена: ' + captchaa);
-		}else{
-			throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
-		}
+	
+	
+	var captchaa;
+	if(AnyBalance.getLevel() >= 7){
+		AnyBalance.trace('Пытаемся ввести капчу');
+		var href = getParam(html, null, null, /libs\/securimage\/securimage_show[^"]*/i);
+		var captcha = AnyBalance.requestGet(baseurl + href);
+		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaa);
+	}else{
+		throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	}
 
-		html = AnyBalance.requestPost(baseurl + '?mode=auth', {
-			'recaptcha_challenge_field':challenge,
-			'recaptcha_response_field':captchaa,
-			'do':'Войти',
+			html = AnyBalance.requestPost(baseurl + '?mode=auth', {
 			login:prefs.login,
 			password:prefs.password,
+			'code':captchaa,
+			'do':'Войти',
+			
 		}, addHeaders({Referer: baseurl + '?mode=auth'}));
 		
 		if(!/mode=logout/i.test(html)) {
@@ -53,15 +48,15 @@ function main() {
 				throw new AnyBalance.Error('Вам необходимо подтвердить лицензионное соглашение!');
 			if(/Проверьте ввод кода с картинки/i.test(html))
 				throw new AnyBalance.Error('Вы ввели неверные символы с картинки!');
-			var error = getParam(html, null, null, /<div[^>]+class=["']MessageError["'][^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+			var error = getParam(html, null, null, /MessageError["'][^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
 			if(error)
 				throw new AnyBalance.Error(error, null, /Проверьте логин и пароль/i.test(error));
 				
 			throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 		}
-	}
 	
-	html = AnyBalance.requestGet(baseurl + 'index.php?mod=idx', addHeaders({'Referer': 'http://cbilling.net/index.php?mode=browser'}));
+	
+	html = AnyBalance.requestGet(baseurl + 'index.php?mod=idx', addHeaders({'Referer': baseurl+'index.php?mode=browser'}));
 	
     var result = {success: true};
 	

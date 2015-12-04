afad347e21edfef0ac14a87673fb45f896a846a7
Edward Anisonyan <stark@starksoft.ru>
2015-12-04 12:46:52 +0300

    Facebook (ab-social-facebook): - Поддержка изменений на сайте.

--- providers/ab-social-facebook/main.js
+++ providers/ab-social-facebook/main.js
@@ -7,8 +7,9 @@ var g_headers = {
 	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36',
 };
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'https://m.facebook.com/';
@@ -16,34 +17,43 @@ function main() {
 	
 	checkEmpty(prefs.login, 'Please, enter login!');
 	checkEmpty(prefs.password, 'Please, enter password');
-		
+	
 	var html = AnyBalance.requestGet(baseurl, g_headers);
-
+	
 	var params = createFormParams(html, function(params, str, name, value){
 		if(name == 'email')
 			return prefs.login;
 		if(name == 'pass')
 			return prefs.password;			
+		if(name == 'version')
+			return '1';
+		if(name == '_fb_noscript')
+			return undefined;
+		
 		return value;
 	});
 	
-	html = AnyBalance.requestPost(baseurl + 'login.php?refsrc=https%3A%2F%2Fm.facebook.com%2F&refid=8', params, addHeaders({Referer: baseurl + ''}));
+	html = AnyBalance.requestPost(baseurl + 'login.php?refsrc=https%3A%2F%2Fm.facebook.com%2F&lwv=100&refid=8', params, addHeaders({Referer: baseurl}));
 	
-	if(!/logout/i.test(html)) {
-		if(/Нам не удалось опознать ваш электронный адрес/i.test(html))
-			throw new AnyBalance.Error('Your have entered wrong login or password.', null, true);
-		throw new AnyBalance.Error('Can`t login. Site changed?');
+	if (!/logout/i.test(html)) {
+		var error = getParam(html, null, null, /<div class="x">([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Нам не удалось опознать|не соответствует ни одному аккаунту/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Can`t login. Site has been changed?');
 	}
 	
 	html = AnyBalance.requestGet(baseurl, g_headers);
+	
 	var result = {success: true};
 
-	var userid = getParam(html, null, null, /"USER_ID":"([^"]+)/i, replaceTagsAndSpaces, parseBalance);
+	var userid = getParam(html, null, null, /"USER_ID":"([^"]+)/i, replaceTagsAndSpaces);
 
-	getParam(html, result, 'messages', /Входящие(?:[^>]*>){2}(\d+)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'friends', /добавление в друзья(?:[^>]*>){2}(\d+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'messages', /Входящие(?:[^>]*>){1,7}(\d+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'friends', /Запросы на добавление в друзья(?:[^>]*>){1,7}(\d+)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, '__tariff', new RegExp(userid + ',"name":"([^"]+)', 'i'), replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'name', new RegExp(userid + ',"name":"([^"]+)', 'i'), replaceTagsAndSpaces, html_entity_decode);
+	getParam(result.__tariff, result, 'name');
 	
     AnyBalance.setResult(result);
 }

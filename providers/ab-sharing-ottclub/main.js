0a1983411ccae32f4e3276367ba4a6f2d5bdf531
Edward Anisonyan <stark@starksoft.ru>
2015-12-07 18:17:43 +0300

    OTTCLUB (ab-sharing-ottclub): - Поддержка изменений на сайте.

--- providers/ab-sharing-ottclub/main.js
+++ providers/ab-sharing-ottclub/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://ottclub.no-ip.org/';
+	var baseurl = 'http://ottclub.cc/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');

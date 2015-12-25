79b6eded49011783603164c1d8fcbfdc81505272
z-z <bazzaurik@gmail.com>
2015-12-25 14:02:28 +0300

    переделали логику проверки ошибок

--- providers/ab-finance-hermesltd/main.js
+++ providers/ab-finance-hermesltd/main.js
@@ -1,3 +1,7 @@
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
 var g_headers = {
 	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Accept-Encoding':'gzip, deflate, sdch',
@@ -5,8 +9,6 @@ var g_headers = {
 	'Connection':'keep-alive',
 	'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'
 };
-
-
 
 function main(){
 	var prefs = AnyBalance.getPreferences();
@@ -16,70 +18,60 @@ function main(){
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 
-
 	var html = AnyBalance.requestGet(baseurl + '/lk/aut', g_headers);
 
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
-
-
-	html = AnyBalance.requestPost(baseurl + '/lk/aut?from=autpage', {nomer_s: prefs.login, pass: prefs.password}, addHeaders({
+	
+	html = AnyBalance.requestPost(baseurl + '/lk/aut?from=autpage', {
+		nomer_s: prefs.login, 
+		pass: prefs.password
+	}, addHeaders({
 		'Origin':baseurl,
 		'Referer':baseurl+'/lk/aut'
 	}));
-
-
-	var capcha = getElement(html, /<img\b[^>]*id="img_capcha"[^>]*>/gi);
-	html = false;
-
+	
+	var capcha = getElement(html, /<img\b[^>]*id="img_capcha"[^>]*>/i);
+	
 	if(capcha) {
 		var captchaimg = AnyBalance.requestGet(baseurl + '/secpic.php');
 		var value = AnyBalance.retrieveCode("Пожалуйста, введите цифры с картинки.", captchaimg, {inputType: 'string'});
-		html = AnyBalance.requestPost(baseurl + '/lk/aut?from=autpage', {nomer_s: prefs.login, pass: prefs.password, capcha: value}, addHeaders({
+		html = AnyBalance.requestPost(baseurl + '/lk/aut?from=autpage', {
+			nomer_s: prefs.login, 
+			pass: prefs.password, 
+			capcha: value
+		}, addHeaders({
 			'Origin':baseurl,
 			'Referer':baseurl+'/lk/aut?from=autpage'
 		}));
-	} else {
-		// в качесве ошибки получаю первый span страницы,
-		// очень ненадежно, но по другому не смог до ошибки добраться
-		// это ошибки до капчи
-		var err = getElement(html, /<span\b[^>]*>([^>]*)/);
-		err = replaceAll(err, replaceTagsAndSpaces);
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error(err);
 	}
 	
-
-	html = AnyBalance.requestGet(baseurl + "/lk/my_ch", null, addHeaders({'Referer':baseurl+'/lk/aut?from=autpage'}));
-
-	// именно logaut
-	if(!/logaut/.test(html)) {
-		var err = getElement(html, /<span\b[^>]*>([^>]*)/);
-		err = replaceAll(err, replaceTagsAndSpaces);
+	if (!/logaut/i.test(html)) {
+		var authBox = getElement(html, /<div class="auth_box">/i);
+		var error = getParam(authBox, null, null, /<span[^>]*style="display:block;[^>]*font-size:20px[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Введены неверно/i.test(error));
+		
 		AnyBalance.trace(html);
-		// тут ошибка - не всегда ошибка, может попасться бред какой нибудь (хотя я не смог добиться такой ситуации)
-		// но другого способа показать хоть что-то не нашел)
-		throw new AnyBalance.Error("Не удалось авторизоваться. Возможная ошибка: " + err);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
-
-	// вроде прошли все круги ада
-
+	
 	var result = { success: true };
 
-	getParam(html, result, 'balance', /баланс:(?:[^>]*>){4}([\s\S]*?)</gi, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'loan', /займ:(?:[^>]*>){4}([\s\S]*?)</gi, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'deposit', /Залог:(?:[^>]*>){4}([\s\S]*?)</gi, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'available', /Доступно(?:[^>]*>){4}([\s\S]*?)</gi, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'premium', /Ажио:\s*(?:[^>]*>){4}([\s\S]*?)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'currency', /Валюта счета:\s*(?:[^>]*>){4}([\s\S]*?)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'contract', /Контракт:\s*(?:[^>]*>){4}([\s\S]*?)</i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'loan_rate', /Ставка по займу:\s*(?:[^>]*>){4}([\s\S]*?)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'status', /Статус счета:\s*(?:[^>]*>){7}([\s\S]*?)</i, replaceTagsAndSpaces);
-	getParam(html, result, 'profit', /Прибыль за всё время:\s*(?:[^>]*>){4}([\s\S]*?)</gi, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'name', /Владелец счета:\s*(?:[^>]*>){4}([\s\S]*?)</i, replaceTagsAndSpaces);
+	getParam(html, result, 'balance', /Баланс:([\s\S]*?)<\/tr>/, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['currency', 'balance'], /Баланс:([\s\S]*?)<\/tr>/, replaceTagsAndSpaces, parseCurrency);
 
+	getParam(html, result, 'loan', /Займ:([\s\S]*?)<\/tr>/, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'deposit', /Залог:([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'available', /Доступно:([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'premium', /Ажио:([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'contract', /Контракт:([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'loan_rate', /Ставка по займу:([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'status', /Статус счета:([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+	getParam(html, result, 'profit', /Прибыль за всё время:([\s\S]*?)<\/tr>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'name', /Владелец счета:(?:[\s\S]*?<\/td>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
 
 	AnyBalance.setResult(result);
 }

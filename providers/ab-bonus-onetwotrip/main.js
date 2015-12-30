c8f76ceff404a01255d10a4dedaa829f977f5d32
Xenia <adri.espanol@gmail.com>
2015-12-30 13:12:07 +0300

    Переписан провайдер OneTwoTrip.com

--- providers/ab-bonus-onetwotrip/main.js
+++ providers/ab-bonus-onetwotrip/main.js
@@ -1,14 +1,52 @@
-function main(){
-var prefs = AnyBalance.getPreferences();
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
 
-var str = AnyBalance.requestPost('https://secure.onetwotrip.com/_api/visitormanager/auth/', {
-    email: prefs.login,
-    pwd: prefs.password,
-    rememberMe: false
-  });
-var obj = jQuery.parseJSON(str);  
-if(obj.auth)
- AnyBalance.setResult({success: true, "ott-bonus": obj.bonus.total,"ott-curr": obj.bonus.currency}); 
-else
- AnyBalance.setResult({error: true, message: "Не удалось получить данные с сайта"}); 
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://www.onetwotrip.com/';
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl + 'ru/', g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+
+	html = AnyBalance.requestPost(baseurl + '_api/visitormanager/auth/ ', {
+        email: prefs.login,
+        pwd: prefs.password
+	}, addHeaders({
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'X-Requested-With': 'XMLHttpRequest',
+        Referer: baseurl + 'ru/'
+    }));
+
+    var json = getJson(html);
+	if (!json.auth) {
+		var error = json.error;
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	getParam(json.bonus.total, result, 'balance');
+	getParam(json.bonus.currency, result, 'currency');
+	
+	AnyBalance.setResult(result);
 }

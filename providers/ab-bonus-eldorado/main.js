cd64d1fdde6692f0018e490a52f27abc3d555ddc
Edward Anisonyan <stark@starksoft.ru>
2016-01-13 15:57:14 +0300

    Клуб Эльдорадо (ab-bonus-eldorado): - Исправление ошибок.

--- providers/ab-bonus-eldorado/main.js
+++ providers/ab-bonus-eldorado/main.js
@@ -1,13 +1,5 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Данные по бонусной карте Eldorado
-
-Сайт оператора: http://eldorado.ru/
-Личный кабинет: http://www.club.eldorado.ru/enter.php
-
-!!!Обязательное требование: первый раз требуется зайти в личный кабинет вручную, т.к однократно запрашивается привязка email или предлагает создать учетную запись к Вашей карте.
-Если требование не выполнено, получите ошибку провайдера: Не удаётся войти в личный кабинет. Сайт изменен?
 */
 
 var g_headers = {
@@ -17,12 +9,12 @@ var g_headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11',
 };
 
-function main(){
+function main() {
     var prefs = AnyBalance.getPreferences();
-
-    var cardNumber = getParam(prefs.login, null, null, /^\d+$/, [/^(\d{4})(\d{4})(\d{4})(\d{4})$/, '$1 $2 $3 $4']),
-	    login = cardNumber || prefs.login,
-        passMsg = cardNumber ? 'PIN-код' : 'пароль';
+	
+	var cardNumber = getParam(prefs.login, null, null, /^\d+$/, [/^(\d{4})(\d{4})(\d{4})(\d{4})$/, '$1 $2 $3 $4']);
+	var login = cardNumber || prefs.login;
+	var passMsg = cardNumber ? 'PIN-код' : 'пароль';
 
     checkEmpty(login, 'Введите номер карты или логин!');
     checkEmpty(prefs.password, 'Введите ' + passMsg + ' для входа в личный кабинет!');
@@ -31,63 +23,52 @@ function main(){
 	
 	var html = AnyBalance.requestGet(baseurl + 'personal/club/offers/index.php', g_headers);
 
-    var params = createFormParams(html, function(params, str, name, value) {
-        switch (name.toLowerCase()) {
-            case 'user_login':
-                return login;
-            case 'user_password':
-                return prefs.password;
-            default:
-                return value;
-        }
-    });
-
-    if(!params) {
-        AnyBalance.trace(html);
-        throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
-    }
+    html = AnyBalance.requestPost(baseurl + '_ajax/userCardAuth.php', [
+		['AUTH_FORM', 'Y'],
+		['action', 'AUTH'],
+		['auth_popup', '1'],
+		['backurl', '/personal/club/offers/index.php?login=yes'],
+		['USER_LOGIN', login],
+		['USER_PASSWORD', prefs.password],
+		['USER_PASSWORD', prefs.password],
+		['captcha_sid', ''],
+		['captcha_word', '']
+	], addHeaders({Referer: baseurl + 'personal/index.php', 'X-Requested-With': 'XMLHttpRequest'}));
 	
-    html = AnyBalance.requestPost(
-        baseurl + '_ajax/userCardAuth.php',
-        params,
-        addHeaders({Referer: baseurl + 'personal/index.php', 'X-Requested-With': 'XMLHttpRequest'})
-    );
-
     var json = getJson(html);
     if(!json.data && !json.success) {
         throw new AnyBalance.Error(json.message || 'Не удаётся войти в личный кабинет. Сайт изменен?');
     }
-
+	
     html = AnyBalance.requestGet(baseurl + '_ajax/getUserCardBonus.php', addHeaders({Referer: baseurl + 'personal/?loyalty', 'X-Requested-With': 'XMLHttpRequest'}));
     json = getJson(html);
-
+	
     if(!json.result) {
+		AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удаётся найти бонусы. Сайт изменен?');
     }
-
+	
     var result = {success: true};
-
+	
     getParam('' + json.result.total, result, 'balance', null, null, parseBalance);
     getParam('' + json.result.reserved, result, 'reserv', null, null, parseBalance);
     getParam('' + json.result.inactive, result, 'inactive', null, null, parseBalance);
-
-
-    if(AnyBalance.isAvailable('cardnum', 'phone', 'userName')){
+	
+    if(AnyBalance.isAvailable('cardnum', 'phone', 'userName')) {
         html = AnyBalance.requestGet(baseurl + 'personal/club/form/', g_headers);
-
-        getParam(html, result, 'cardnum', /Карта №([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
-    
-        var aggragate_join_space = create_aggregate_join(' ');
-
-        sumParam(html, result, 'userName', /<input[^>]+name="CLUB_USER_FORM_NAME_LAST"[^>]*value="([^"]*)/i, null, html_entity_decode, aggragate_join_space);
-        sumParam(html, result, 'userName', /<input[^>]+name="CLUB_USER_FORM_NAME_FIRST"[^>]*value="([^"]*)/i, null, html_entity_decode, aggragate_join_space);
-        sumParam(html, result, 'userName', /<input[^>]+name="CLUB_USER_FORM_NAMEMIDDLE"[^>]*value="([^"]*)/i, null, html_entity_decode, aggragate_join_space);
-
-        sumParam(html, result, 'phone', /<span[^>]+class=['"]phone_country['"][^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode, aggragate_join_space);
-        sumParam(html, result, 'phone', /<input[^>]+name="CLUB_USER_FORM_MOBPHONE_CODE"[^>]*value="([^"]*)/i, null, html_entity_decode, aggragate_join_space);
-        sumParam(html, result, 'phone', /<input[^>]+name="CLUB_USER_FORM_MOBPHONE_NUMBER"[^>]*value="([^"]*)/i, null, html_entity_decode, aggragate_join_space);
+		
+		getParam(html, result, 'cardnum', /Карта №([^<]+)/i, replaceTagsAndSpaces);
+		
+		var aggragate_join_space = create_aggregate_join(' ');
+		
+		sumParam(html, result, 'userName', /<input[^>]+name="CLUB_USER_FORM_NAME_LAST"[^>]*value="([^"]*)/i, replaceTagsAndSpaces, null, aggragate_join_space);
+        sumParam(html, result, 'userName', /<input[^>]+name="CLUB_USER_FORM_NAME_FIRST"[^>]*value="([^"]*)/i, replaceTagsAndSpaces, null, aggragate_join_space);
+        sumParam(html, result, 'userName', /<input[^>]+name="CLUB_USER_FORM_NAMEMIDDLE"[^>]*value="([^"]*)/i, replaceTagsAndSpaces, null, aggragate_join_space);
+		
+		sumParam(html, result, 'phone', /<span[^>]+class=['"]phone_country['"][^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, null, aggragate_join_space);
+        sumParam(html, result, 'phone', /<input[^>]+name="CLUB_USER_FORM_MOBPHONE_CODE"[^>]*value="([^"]*)/i, replaceTagsAndSpaces, null, aggragate_join_space);
+        sumParam(html, result, 'phone', /<input[^>]+name="CLUB_USER_FORM_MOBPHONE_NUMBER"[^>]*value="([^"]*)/i, replaceTagsAndSpaces, null, aggragate_join_space);
     }
 
     AnyBalance.setResult(result);
 }
-

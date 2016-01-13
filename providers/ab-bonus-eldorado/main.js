402e80607a3bbbae4c762391941c88ab654b8003
Edward Anisonyan <stark@starksoft.ru>
2016-01-13 17:17:55 +0300

    Клуб Эльдорадо (ab-bonus-eldorado): - Исправлена возможность заходить в кабинет с помощью логина и пароля.

--- providers/ab-bonus-eldorado/main.js
+++ providers/ab-bonus-eldorado/main.js
@@ -4,7 +4,6 @@
 
 var g_headers = {
     'Accept':'application/json, text/javascript, */*; q=0.01',
-    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
     'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11',
 };
@@ -12,9 +11,9 @@ var g_headers = {
 function main() {
     var prefs = AnyBalance.getPreferences();
 	
-	var cardNumber = getParam(prefs.login, null, null, /^\d+$/, [/^(\d{4})(\d{4})(\d{4})(\d{4})$/, '$1 $2 $3 $4']);
-	var login = cardNumber || prefs.login;
-	var passMsg = cardNumber ? 'PIN-код' : 'пароль';
+	var formattedLogin = getParam(prefs.login, null, null, /^\d+$/, [/^(\d{4})(\d{4})(\d{4})(\d{4})$/, '$1 $2 $3 $4']);
+	var login = formattedLogin || prefs.login;
+	var passMsg = formattedLogin ? 'PIN-код' : 'пароль';
 
     checkEmpty(login, 'Введите номер карты или логин!');
     checkEmpty(prefs.password, 'Введите ' + passMsg + ' для входа в личный кабинет!');
@@ -23,17 +22,27 @@ function main() {
 	
 	var html = AnyBalance.requestGet(baseurl + 'personal/club/offers/index.php', g_headers);
 
-    html = AnyBalance.requestPost(baseurl + '_ajax/userCardAuth.php', [
-		['AUTH_FORM', 'Y'],
-		['action', 'AUTH'],
-		['auth_popup', '1'],
-		['backurl', '/personal/club/offers/index.php?login=yes'],
-		['USER_LOGIN', login],
-		['USER_PASSWORD', prefs.password],
-		['USER_PASSWORD', prefs.password],
-		['captcha_sid', ''],
-		['captcha_word', '']
-	], addHeaders({Referer: baseurl + 'personal/index.php', 'X-Requested-With': 'XMLHttpRequest'}));
+	// Не трогать! Этот хак здесь живет не просто так.
+	// var captchaa;
+	// if(AnyBalance.getLevel() >= 7){
+		// AnyBalance.trace('Пытаемся ввести капчу');
+		var captcha = AnyBalance.requestGet(baseurl+ 'bitrix/tools/captcha.php?captcha_sid=123');
+		// captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		// AnyBalance.trace('Капча получена: ' + captchaa);
+	// }else{
+		// throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
+	// }
+	
+    html = AnyBalance.requestPost(baseurl + '_ajax/userCardAuth.php', {
+		'AUTH_FORM':'Y',
+		'action':'AUTH',
+		'auth_popup':'1',
+		'backurl':'/personal/orders/index.php?login=yes',
+		'USER_LOGIN':login,
+		'USER_PASSWORD':prefs.password,
+		captcha_sid:'123',
+		captcha_word:'5xx57',
+    }, addHeaders({Referer: baseurl + 'personal/orders/index.php', 'X-Requested-With': 'XMLHttpRequest'}));
 	
     var json = getJson(html);
     if(!json.data && !json.success) {

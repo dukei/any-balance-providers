921e855730bdeb76e610e0b26cdc767774ad29fc
Dmitry Kochin <kochin.du@krawlly.com>
2016-01-22 12:55:00 +0300

    Исправлены шаблоны под новый отладчик и зависимости Библиотека и nadapter переехали в зависимости

--- extra/development/ab-test-library/header.js
+++
@@ -1,3 +0,0 @@
-/** @preserve AnyBalance Library (https://github.com/dukei/any-balance-providers/)
-* The uncompressed full source code of this library is here: https://github.com/dukei/any-balance-providers/blob/master/extra/development/ab-test-library/library.js
-*/

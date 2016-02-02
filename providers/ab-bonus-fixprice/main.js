1b50280c7347ca26d956990c0ad3f3832f635c73
Roman Il'yasov <749625@gmail.com>
2016-02-02 17:06:51 +0300

    v1

--- providers/ab-bonus-fixprice/main.js
+++ providers/ab-bonus-fixprice/main.js
@@ -1,25 +1,28 @@
-﻿/**
+﻿
+/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
+
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'http://bonus.fix-price.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
-	
+
 	AB.checkEmpty(prefs.login, 'Введите логин!');
 	AB.checkEmpty(prefs.password, 'Введите пароль!');
-	
-	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
-	
-	if(!html || AnyBalance.getLastStatusCode() > 400){
+
+	var html = AnyBalance.requestGet(baseurl + '', g_headers);
+
+	if (!html || AnyBalance.getLastStatusCode() > 400) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
@@ -28,27 +31,31 @@ function main() {
 		login: prefs.login,
 		password: prefs.password,
 	}, AB.addHeaders({
-		Referer: baseurl + 'login',
-		'X-Requested-With': 'XMLHttpRequest'
+		'X-Requested-With': 'XMLHttpRequest',
+		Referer: baseurl + 'login'
+
 	}));
 
 	var json = AB.getJson(html);
 
-	if (!json.success) {
+	if (!json.success === true) {
 		var error = json.message;
-		if (error)
-			throw new AnyBalance.Error(error, null, /Пароль введен не верно/i.test(error));
-		
+		if (error) {
+			throw new AnyBalance.Error(error, null, /найден|пароль/i.test(error));
+		}
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 
-	html = AnyBalance.requestGet(baseurl+'profile', g_headers);
-	var result = {success: true};
-	
-	AB.getParam(html, result, 'balance',/<div[^>]+class="bonus-info"[^>]*>(?:[^>]*>){6}([\s\S]*?)<\/b>/i, AB.replaceTagsAndSpaces, AB.parseBalance);
-	AB.getParam(html, result, 'fio', /<div[^>]+class="user"[^>]*>(?:[^>]*>){6}([\s\S]*?)<\/a>/i, AB.replaceTagsAndSpaces);
-	AB.getParam(html, result, 'card', /на вашей карте №([\s\S]*?)<\/p>/i, AB.replaceTagsAndSpaces);
+	html = AnyBalance.requestGet(baseurl + 'profile', g_headers);
+	var result = {
+		success: true
+	};
+
+	AB.getParam(html, result, 'balance', /<div[^>]*class="[^"]*bonus-info[^"]*"[\s\S]*?(<h2[\s\S]*?<\/h2>)/i, AB.replaceTagsAndSpaces,
+		AB.parseBalance);
+	AB.getParam(html, result, 'fio', /<div[^>]*class="[^"]*user[^"]*"[^>]*>[\s\S]*?(<p[\s\S]*?)<\/a>/i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'card', /на\s+вашей\s+карте\s+№([\s\S]*?)<\/p>/i, AB.replaceTagsAndSpaces);
 
 	AnyBalance.setResult(result);
 }

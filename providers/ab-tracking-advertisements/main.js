8360cbb16eeb0e6806f39c36511863d95b086d2d
Roman Il'yasov <749625@gmail.com>
2016-02-04 19:54:51 +0300

    v1

--- providers/ab-tracking-advertisements/main.js
+++ providers/ab-tracking-advertisements/main.js
@@ -10,83 +10,99 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
-function main(){
+function main() {
 	var prefs = AnyBalance.getPreferences();
 	AnyBalance.setDefaultCharset('utf-8');
 
 	// Если указан логин и пароль - заходим по нему и ищем наши объявления
-	if(prefs.login && prefs.password) {
+	if (prefs.login && prefs.password) {
 		AnyBalance.trace('Есть логин и пароль, заходим...');
 		var baseurl = 'https://www.avito.ru/';
-	
+
 		var html = AnyBalance.requestGet(baseurl + 'profile', g_headers);
-		
+
 		html = requestPostMultipart(baseurl + 'profile', {
 			'next': '/profile',
 			login: prefs.login,
 			password: prefs.password,
 			quick_expire: 'on'
-		}, addHeaders({Referer: baseurl + 'profile'}));	
-		
+		}, addHeaders({
+			Referer: baseurl + 'profile'
+		}));
+
 		if (!/logout|profile\/exit/i.test(html)) {
-			var error = getParam(html, null, null, /class="error-description"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+			var error = getParam(html, null, null, /class="error-description"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces,
+				html_entity_decode);
 			if (error && /Неправильная пара электронная почта/i.test(error)) throw new AnyBalance.Error(error, null, true);
 			if (error) throw new AnyBalance.Error(error);
 			throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 		}
 		var req = prefs.pattern ? prefs.pattern : '';
 		//                   (<div\s+class="[^"]*title"(?:[^>]*>){2}[^>]*Перейти на страницу объявления[^>]*>[^>]*АКПП tiptronik(?:[\s\S]*?<\/div){4})
-		var re = new RegExp('(<div\\s+class=\\"[^\\"]*description\\"(?:[^>]*>){2}[^>]*Перейти на страницу объявления[^>]*>[^>]*'+ req +'(?:[\\s\\S]*?</div){4})', 'i');
+		var re = new RegExp(
+			'(<div\\s+class=\\"[^\\"]*description\\"(?:[^>]*>){2}[^>]*Перейти на страницу объявления[^>]*>[^>]*' + req +
+			'(?:[\\s\\S]*?</div){4})', 'i');
 		var div = getParam(html, null, null, re);
-		if(!div)
-			throw new AnyBalance.Error('Не удаётся найти ' + (prefs.pattern ? 'объявления с именем ' + prefs.pattern : 'ни одного объявления'));
-		
-		var result = {success: true};
-	
-		getParam(div, result, 'views', /Перейти на страницу объявления[^>]*просмотров:\s*(\d+)/i, replaceTagsAndSpaces, parseBalance);
-		getParam(div, result, 'views_today', /Перейти на страницу объявления[^>]*просмотров:[^>]*сегодня\s+(\d+)/i, replaceTagsAndSpaces, parseBalance);
+		if (!div)
+			throw new AnyBalance.Error('Не удаётся найти ' + (prefs.pattern ? 'объявления с именем ' + prefs.pattern :
+				'ни одного объявления'));
+
+		var result = {
+			success: true
+		};
+
+		getParam(div, result, 'views', /Перейти на страницу объявления[^>]*просмотров:\s*(\d+)/i, replaceTagsAndSpaces,
+			parseBalance);
+		getParam(div, result, 'views_today', /Перейти на страницу объявления[^>]*просмотров:[^>]*сегодня\s+(\d+)/i,
+			replaceTagsAndSpaces, parseBalance);
 		//getParam(div, result, '__tariff', /Перейти на страницу объявления[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-		getParam(div, result, 'adv_title', /Перейти на страницу объявления[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(div, result, 'adv_title', /Перейти на страницу объявления[^>]*>([^<]*)/i, replaceTagsAndSpaces,
+			html_entity_decode);
 		getParam(div, result, 'price', /<p\s*class="[^"]*price">([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 		getParam(div, result, ['currency', 'price'], /<p\s*class="[^"]*price">([^<]*)/i, replaceTagsAndSpaces, parseCurrency);
-		getParam(div, result, 'days_left', /До истечения срока размещения осталось\s*(\d+)\s*д/i, replaceTagsAndSpaces, parseBalance);
-		
+		getParam(div, result, 'days_left', /До истечения срока размещения осталось\s*(\d+)\s*д/i, replaceTagsAndSpaces,
+			parseBalance);
+
 		AnyBalance.setResult(result);
-	// Если нет ни логина ни пароля - просто ищем объявление
+		// Если нет ни логина ни пароля - просто ищем объявление
 	} else {
 		var region = prefs.region;
 		var pattern = prefs.pattern;
-		
-		var pattern1 = pattern.replace(/ /g,"+");
-		
-		var baseurl = 'https://m.avito.ru/'+region+'?q='+pattern1;
-		
+
+		var pattern1 = pattern.replace(/ /g, "+");
+
+		var baseurl = 'https://m.avito.ru/' + region + '?q=' + pattern1;
+
 		AnyBalance.trace('Starting search: ' + baseurl);
-		var info = AnyBalance.requestGet(baseurl);	
-		
+		var info = AnyBalance.requestGet(baseurl);
+
 		/*var error = $('#errHolder', info).text();
 		if(error){
 			throw new AnyBalance.Error(error);
-		}*/		
-		var result = {success: true};
+		}*/
+		var result = {
+			success: true
+		};
 		result.__tariff = prefs.pattern;
-		
-		if(matches = info.match(/не найдено/i)){
+
+		if (matches = info.match(/не найдено/i)) {
 			result.found = 0;
 			AnyBalance.setResult(result);
 			return;
 		}
-		
-		getParam(info, result, 'found', /<div class="nav-helper-content nav-helper-text">([\s\S]+?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-		
-    	if (!result.found) {
-    	    throw new AnyBalance.Error("Ошибка при получении данных с сайта.");
-    	}
-    	if ((matches = info.match(/<article[^>]*class="b-item "[^>]*>[^]*?<\/article>/ig))) {
+
+		getParam(info, result, 'found', /<div[^>]*class="[^"]*nav-helper-text[^"]*"[^>]*>([\s\S]*?)<\/div>/i,
+			replaceTagsAndSpaces, parseBalance);
+
+		if (!result.found) {
+			throw new AnyBalance.Error("Ошибка при получении данных с сайта.");
+		}
+		if ((matches = info.match(/<article[^>]*class="[^"]*b-item[^"]*"[^>]*>[\s\S]*?<\/article>/ig))) {
 			info = matches[0];
-			getParam(info, result, 'date', /<div class="info-date info-text">([^]*?),?&nbsp;/i, replaceTagsAndSpaces, html_entity_decode);
-			getParam(info, result, 'time', /<div class="info-date info-text">[^]*?,?&nbsp;([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
-				
+			getParam(info, result, 'date', /<div[^>]*class="[^"]*info-date[^"]*"[^>]*>([\s\S]*?),/i, replaceTagsAndSpaces);
+			getParam(info, result, 'time', /<div[^>]*class="[^"]*info-date[^"]*"[^>]*>[\s\S]*?,([\s\S]*?)<\/div>/i,
+				replaceTagsAndSpaces);
+
 			var datetime;
 			if (isset(result.date) && isset(result.time)) {
 				datetime = result.date + ' ' + result.time;
@@ -95,16 +111,19 @@ function main(){
 			} else if (isset(result.time)) {
 				datetime = result.time;
 			}
-			if(datetime)
+			if (datetime)
 				getParam(datetime, result, 'datetime');
 
-			getParam(info, result, 'last', /<h3 class="item-header">([\s\S]*?)<\/h3>/i, replaceTagsAndSpaces, html_entity_decode);
-			getParam(info, result, 'price', /<div class="item-price ">([\s\S]+?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-			getParam(info, result, 'currency', /<div class="item-price ">.*?\&nbsp;([^\d].*?)\s+<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    	} else {
-    	    throw new AnyBalance.Error("Ошибка при разборе ответа с сайта.");
-    	}
-		
+			getParam(info, result, 'last', /<h3[^>]*class="[^"]*item-header[^"]*"[^>]*>([\s\S]*?)<\/h3>/i, replaceTagsAndSpaces);
+			getParam(info, result, 'price', /<div[^>]*class="[^"]*item-price[^"]*"[^>]*>([\s\S]*?)<\/div>/i,
+				replaceTagsAndSpaces, parseBalance);
+			getParam(info, result, ['currency', 'balance'], /<div[^>]*class="[^"]*item-price[^"]*"[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces,
+				AB.parseCurrency);
+
+		} else {
+			throw new AnyBalance.Error("Ошибка при разборе ответа с сайта.");
+		}
+
 		AnyBalance.setResult(result);
 	}
 }

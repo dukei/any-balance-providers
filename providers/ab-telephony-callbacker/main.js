e686f9befd2f3ffc3b6f0104b8435300b9fde455
max.rasky <maxgabbana@gmail.com>
2016-02-04 16:00:15 +0300

    Add 'calls' counter; adjust parsing according to web site changes

--- providers/ab-telephony-callbacker/main.js
+++ providers/ab-telephony-callbacker/main.js
@@ -15,25 +15,38 @@ function main(){
 
     AnyBalance.setCookie('customer.callbacker.com', 'ui_language', 'english');
 
-    var html = AnyBalance.requestPost(baseurl + 'userinfo.php', {
-        done:'submit_log',
-        pr_login:prefs.login,
-        pr_password:prefs.password,
-        submit:'Войти'
+    var html = AnyBalance.requestGet(baseurl);
+
+    if (!html || AnyBalance.getLastStatusCode() > 400) {
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+    }
+
+    html = AnyBalance.requestPost(baseurl + 'userinfo.php', {
+        done: 'submit_log',
+        pr_login: prefs.login,
+        pr_password: prefs.password,
+        submit: 'Login'
     });
 
-    if(!/logout.php\?logout=true/i.test(html)){
-        var error = getParam(html, null, null, /<font[^>]+color="red"[^>]*>([\s\S]*?)<\/font>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
+    if (hasLoginLink(html)) {
+        var error = getParam(html, null, null, /<div[^>]*class="[^"]*alert-danger[^"]*">(blocked account[\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        if(error) {
+            throw new AnyBalance.Error(error, null, true);
+        }
+
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неправильный логин-пароль?');
     }
 
     var result = {success: true};
 
-    getParam(html, result, 'balance', /Balance:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'phone', /Phone:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /Account number:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'balance', /money[\s\S]*?<span\s*class="counter">([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'phone', /phone number[\s\S]*?<dd>([\s\S]*?)<\/dd>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'calls', /<span[^>]*>calls<\/span>([\s\S]*?)<\/nobr>/i, replaceTagsAndSpaces, parseBalance);
 
     AnyBalance.setResult(result);
+}
+
+function hasLoginLink(html) {
+    return /<button[^>]*value="login"[^>]*>login<\/button>/i.test(html);
 }

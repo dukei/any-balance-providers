58b0ee059d3c203f854a834916c259e3aa393e3f
alexandroppolus <sqlcpp@gmail.com>
2016-02-08 21:12:41 +0300

    Media Markt (ab-bonus-mediamarkt): - капча - fatal для неправильного логина/пароля

--- providers/ab-bonus-mediamarkt/main.js
+++ providers/ab-bonus-mediamarkt/main.js
@@ -4,59 +4,93 @@
 
 var g_headers = {
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	//'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36'
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://www.mediamarkt.ru/';
+	var baseurl = 'https://www.mediamarkt.ru';
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
+    
+    var html = AnyBalance.requestGet(baseurl, g_headers);
 
-	var html = AnyBalance.requestGet(baseurl + 'user/cards', g_headers);
-
-	var csrf = getParam(html, null, null, /<meta[^>]+name="csrf-token"[^>]*content="([^"]*)/i, null, html_entity_decode);
-	checkEmpty(csrf, 'Не удалось найти токен авторизации!', true);
-	
-	if(!html || AnyBalance.getLastStatusCode() > 400){
+	if(!html || AnyBalance.getLastStatusCode() >= 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
-	
-	html = AnyBalance.requestPost(baseurl + 'user/authenticate', {
-		login: prefs.login,
-		password: prefs.password,
-		remember_me: false,
-		_token: csrf
-	}, addHeaders({Referer: baseurl}));
-
-	var json = getJson(html);
-
-	if (!json.success) {
-		var errors = [];
-		for(var k in json.errors)
-			errors.push(json.errors[k]);
-		
-		var error = errors.join(', ');
-		if (errors.length > 0)
-			throw new AnyBalance.Error(error.replace(/Not Found/, 'Неверное имя пользователя или пароль'), null, /not found/i.test(error));
-
-		AnyBalance.trace(html);
+    
+    var loginForm = AB.getElement(html, /<form[^>]+?form-user-enter/i);
+    
+    if (!loginForm) {
+        AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
+    
+    var captchaCode = '';
+    var captchaUrl = getParam(loginForm, null, null, /<img[^>]*?src=["']([^"']+)/i);
+    if (captchaUrl) {
+        if (!/^https?:/.test(captchaUrl)) {
+            captchaUrl = captchaUrl.replace(/^\/?/, baseurl + '/');
+        }
+        var image = AnyBalance.requestGet(captchaUrl, addHeaders({Referer: baseurl}));
+        image = AnyBalance.requestGet(captchaUrl, addHeaders({Referer: baseurl}));
+        captchaCode = AnyBalance.retrieveCode('Введите проверочный код', image);
+    }
+    
+    var params = AB.createFormParams(html, function (params, str, name, value) {
+        if (name == 'captcha') {
+            return captchaCode;
+        }
+        if (name == 'login') {
+            return prefs.login;
+        }
+        if (name == 'password') {
+            return prefs.password;
+        }
+        return value;
+    });
+    params.remember_me = 'false';
+    delete params.q;
+    delete params.url;
+    
+    html = AnyBalance.requestPost(baseurl + '/user/authenticate', params, addHeaders({
+        Referer: baseurl,
+        Accept: 'application/json, text/javascript, */*; q=0.01',
+        Origin: baseurl,
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }));
+
+    var json = getJson(html);
+
+    if (!json.success) {
+        var errors = [];
+        for(var k in json.errors)
+            errors.push(json.errors[k]);
+
+        var error = errors.join(', ');
+        if (errors.length > 0) {
+            error = error.replace(/Not Found/i, 'Неверное имя пользователя или пароль');
+            throw new AnyBalance.Error(error, null, /пользовател|парол/i.test(error));
+        }
+
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
 
 	var result = {success: true};
 	
 	getParam((json.user.name || '') + ' ' + (json.user.surname || ''), result, 'fio', null, replaceTagsAndSpaces);
 	getParam(json.user.mobile_phone, result, 'phone', null, replaceTagsAndSpaces);
 	
-	html = AnyBalance.requestPost(baseurl + 'ajax/crm_get_user', {
-		_token: csrf
+	html = AnyBalance.requestPost(baseurl + '/ajax/crm_get_user', {
+		_token: params._token
 	}, addHeaders({Referer: baseurl}));
 	
 	json = getJson(html);

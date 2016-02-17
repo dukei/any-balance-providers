dc77ef0e972d89e04347f84db8446ca97625f8b6
alexandroppolus <sqlcpp@gmail.com>
2016-02-17 08:24:35 +0300

    Система Город (ab-municipal-system-gorod)

--- providers/ab-municipal-system-gorod/main.js
+++ providers/ab-municipal-system-gorod/main.js
@@ -7,36 +7,13 @@
 Личный кабинет: много
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
 
 var supported_cards = {
    '990005000': uralsib, //Система ГОРОД Башкортостан, Ижевск, Кемерово, Екатеринбург и др.
@@ -64,37 +41,78 @@ function main(){
 function uralsib(prefix){
     var prefs = AnyBalance.getPreferences();
     var login = prefs.login.substr(prefix.length);
-
+    
     var baseurl = "https://oplata.uralsibbank.ru/";
-    var html = AnyBalance.requestPost(baseurl + "gorod_login.asp", {
+
+    AnyBalance.setCookie('oplata.uralsibbank.ru', 'TS01db9e9f_28', '01403d4e06e826537cf8c80504ddcf44915dbf3b1bd97b8b2b6a1f9e4452726e8c9699f8046546440e57a9397486c29f669072f021');
+    AnyBalance.setCookie('oplata.uralsibbank.ru', 'TS01db9e9f', '0118f3d0af0d378be4bc2bcfa4f89939b3fef03015c8d1d1993ffdde052ac300893af8547cd62d73686b3ee36bc96c40b3d8a647ef8b41015a6d1846bcf867ca5ac1330ba5e04cd8e302139565e26ebec104cfaa149789b2cfd2a17a51ca439efeb2b335bce23cd21cb8e73bd5730a21e10becbdc6');
+    var csrt = '633068871708536467';
+    
+    var html = AnyBalance.requestGet(baseurl + "gorod_login.asp");
+    
+    html = AnyBalance.requestPost(baseurl + 'gorod_login.asp?csrt=' + csrt, {
         CARDNUMBER:login,
         PSW:prefs.password,
         CustomerLogin:'real'
-    });
+    }, addHeaders({Referer: baseurl + "gorod_login.asp" }));
+    
+    var table = AB.getElement(html, /<table[^>]*?name="acclist"/i);
 
-    if(!getParam(html, null, null, /(ИНФОРМАЦИЯ ПО КАРТЕ)/i)){
-      var error = getParam(html, null, null, /<p[^>]*style=['"]color:\s*#cc0000;['"][^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
-      if(error)
-          throw new AnyBalance.Error(error);
-      throw new AnyBalance.Error("Не удалось войти в личный кабинет по неизвестной причине");
+    if(!table){
+        var error = getParam(html, null, null, /<\/noscript\s*>\s*<p[^>]+?class="errorb"[^>]*>\s*(?:<b>ВНИМАНИЕ:?<\/b>)?\s*([^<]+)/i, replaceTagsAndSpaces);
+        if(error) {
+            throw new AnyBalance.Error(error, false, /Карта\s+не\s+зарегистрирована|Неверный\s+ПИН/i.test(error));
+        }
+        AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 
     var result = {success: true};
-
-    var retr = /<tr[^>]*>(?:(?:[\s\S](?!<tr))*?<td[^>]*>){5}(?:[\s\S](?!<tr))*?<\/tr>/ig, matches, tr;
-    while(matches = retr.exec(html)){
-        var _tr = matches[0];
-        var name = getParam(_tr, null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-        var accnum = getParam(_tr, null, null, /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-        if(prefs.accnum && accnum.toUpperCase().indexOf(prefs.accnum.toUpperCase()) < 0)
-            continue;
-        if(prefs.accname && name.toUpperCase().indexOf(prefs.accname.toUpperCase()) < 0)
-            continue;
-        tr = _tr;
-        break;
+    
+    var prefsAccnum = prefs.accnum && prefs.accnum.toUpperCase();
+    var prefsAccname = prefs.accname && prefs.accname.toUpperCase();
+    
+    function getAccnum(cells) {
+        return getParam(cells[2], null, null, null, AB.replaceTagsAndSpaces);
     }
-
-    if(!tr){
+    function getAccname(cells) {
+        return AB.getElement(cells[1], /<b\b/i, AB.replaceTagsAndSpaces);
+    }
+    
+    var trs = AB.getElements(table, /<tr[^>]*>(?=\s*<td)/ig);
+    var tr = null;
+    
+    for (var i = 0; i < trs.length; ++i) {
+        var cells = AB.getElements(trs[i], /<td/ig);
+        if (prefsAccnum) {
+            var accnum = getAccnum(cells);
+            if (accnum && accnum.toUpperCase().indexOf(prefsAccnum) >= 0) {
+                tr = cells;
+                break;
+            }
+        } else {
+            if (prefsAccname) {
+                var accname = getAccname(cells);
+                if (accname && accname.toUpperCase().indexOf(prefsAccname) >= 0) {
+                    tr = cells;
+                    break;
+                }
+            } else {
+                tr = cells;
+                break;
+            }
+        }
+    }
+    
+    if (tr) {
+        getParam(tr[0], result, 'fio', /<b\b[^>]*>([^<]+)/i, replaceTagsAndSpaces);
+        getParam(tr[0], result, 'address', /<br[^>]*>([^<]+)/i, replaceTagsAndSpaces);
+        getParam(getAccname(tr), result, 'service');
+        getParam(getAccname(tr), result, '__tariff');
+        getParam(tr[1], result, 'provider', /<br[^>]*>([^<]+)/i, AB.replaceTagsAndSpaces);
+        getParam(getAccnum(tr), result, 'accnum');
+        getParam(tr[4], result, 'balance', /<font[^>]*>([^<]+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    } else {
         var causes = [];
         if(prefs.accnum)
             causes[causes.length] = "номер которого содержит \"" + prefs.accnum + '"';
@@ -103,43 +121,7 @@ function uralsib(prefix){
         throw new AnyBalance.Error(causes.length==0 ? "Не найдено ни одного лицевого счета!" : "Не найдено лицевого счета, " + causes.join(' и ')); 
     }
 
-    getParam(tr, result, 'balance', /(?:[\s\S]*?<\/td>\s*<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'balance_total', /Итого задолженностей:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(tr, result, 'fio', /<td[^>]*>([\s\S]*?)<br/i, replaceTagsAndSpaces);
-    getParam(tr, result, 'address', /<td[^>]*>[\s\S]*?<br[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-    getParam(tr, result, 'service', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<br/i, replaceTagsAndSpaces);
-    getParam(tr, result, '__tariff', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<br/i, replaceTagsAndSpaces);
-    getParam(tr, result, 'provider', /(?:[\s\S]*?<td[^>]*>){2}[\s\S]*?<br[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-    getParam(tr, result, 'accnum', /(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<br[^>]*>/i, replaceTagsAndSpaces);
-
-    if(AnyBalance.isAvailable('strah', 'strah_ok', 'cold', 'hot', 'gas', 'gas_heat', 'heat', 'electr', 'electr_day', 'electr_night')){
-        var ref = getParam(tr, null, null, /"(gorod_acc_info[^"]*)/i);
-        if(!ref)
-            throw new AnyBalance.Error("Не удаётся найти ссылку на подробную информацию о счете!");
-
-        html = AnyBalance.requestGet(baseurl + ref);
-        
-        getParam(html, result, 'strah', /Сумма страхового взноса:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'strah_ok', /Согласие на страхование:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-        getParam(html, result, 'cold', /Холодная вода[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'hot', /Горячая вода[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'gas', /Газ.Нач.показание:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'gas_heat', /Газ\+Газ.отопление.Нач.показание:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'heat', /Газовое отопление.Нач.показание:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'electr', /Эл\/энергия.Нач.показания:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'electr_day', /Эл\/энергия\s*\(ДЕНЬ\).Нач.показания:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'electr_night', /Эл\/энергия\s*\(НОЧЬ\).Нач.показания:[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    }
-
     AnyBalance.setResult(result);
-}
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
 }
 
 function redirect(prefix, city){

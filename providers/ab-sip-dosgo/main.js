c490d775f4638387872ee953ebae18c68598c109
max.rasky <maxgabbana@gmail.com>
2016-02-24 13:34:52 +0300

    Initial commit

---
+++ providers/ab-sip-dosgo/main.js
@@ -0,0 +1,63 @@
+﻿
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://www.dosgo.ru/';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	AB.checkEmpty(prefs.login, 'Введите логин!');
+	AB.checkEmpty(prefs.password, 'Введите пароль!');
+
+	var html = AnyBalance.requestGet(baseurl + 'user', g_headers);
+
+	if (!html || AnyBalance.getLastStatusCode() > 400) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+	}
+
+	var params = {
+    name: prefs.login,
+    pass: prefs.password,
+    form_build_id: AB.getParam(html, null, null, /form_build_id[\s\S]*?id="([^"]+)/i),
+    form_id: 'user_login',
+    op: 'Войти'
+  };
+
+	html = AnyBalance.requestPost(
+    baseurl + 'user',
+    params,
+    AB.addHeaders({
+		  Referer: baseurl + 'user'
+	  })
+  );
+
+	if (!/logout/i.test(html)) {
+		var error = AB.getParam(html, null, null, /"messages error">((?:[^>]+>){3})/i, AB.replaceTagsAndSpaces);
+		if (error) {
+			throw new AnyBalance.Error(error, null, /логин или пароль/i.test(error));
+		}
+
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+
+	var result = {
+		success: true
+	};
+
+	AB.getParam(html, result, 'balance', /Ваш баланс:([^<]+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(html, result, ['currency', 'balance'], /Ваш баланс:([^<]+)/i, AB.replaceTagsAndSpaces, AB.parseCurrency);
+
+	AnyBalance.setResult(result);
+}

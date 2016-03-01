ac9bda09869947132bf7ed13a6cc0357e7734ddb
i2m <ildr.islmv@gmail.com>
2016-03-01 19:12:57 +0300

    Моя посылка (ab-tracking-moyaposylka)
    Добавлен код ошибки

--- providers/ab-tracking-moyaposylka/main.js
+++ providers/ab-tracking-moyaposylka/main.js
@@ -24,7 +24,7 @@ function apiCall(params) {
 
 	if(!json.success) {
 		AnyBalance.trace(JSON.stringify(json));
-		throw new AnyBalance.Error('Не удалось получить данные из-за ошибке на сервере, попробуйте обновить данные позже.');
+		throw new AnyBalance.Error('Не удалось получить данные из-за ошибке на сервере, попробуйте обновить данные позже. Код ошибки: ' + json.code.error);
 	}
 	return json;
 }

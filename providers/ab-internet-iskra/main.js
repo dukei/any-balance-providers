f86504d76865d9f27201c5b552855e88112f0dad
Edward Anisonyan <stark@starksoft.ru>
2016-03-02 15:04:32 +0300

    Seven Sky (Москва) (ab-internet-iskra): - Добавлен выбор кабинета в настройках. - Новый счетчик: ФИО.

--- providers/ab-internet-iskra/main.js
+++ providers/ab-internet-iskra/main.js
@@ -12,25 +12,38 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
+	
+	var cabinet = prefs.cabinet || '7sky';
+	
+	AnyBalance.trace('Выбран кабинет: ' + cabinet);
+	
+	if(cabinet == '7sky') {
+		doSevenSky(prefs);
+	} else if(cabinet == 'gorkom') {
+		doGorkom(prefs);
+	}
+}
+
+function doSevenSky(prefs){
     AnyBalance.setDefaultCharset('utf-8');
 	
     var baseurl = 'https://lk.seven-sky.net/';
-
-    var html = AnyBalance.requestGet(baseurl, g_headers);
-    
-    if(!html || AnyBalance.getLastStatusCode() > 400){
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 400){
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
     }
-
-    AnyBalance.sleep(2000);
-    
-    html = AnyBalance.requestPost(baseurl + 'login.jsp', {
+	
+	AnyBalance.sleep(2000);
+	
+	html = AnyBalance.requestPost(baseurl + 'login.jsp', {
         p_logname: prefs.login,
         p_pwd: prefs.password
     }, addHeaders({ Referer: baseurl }));
 
-    if(!/exit.jsp|action=logout/.test(html)){
+    if(!/exit.jsp|action=logout/.test(html)) {
         var error = getParam(html, null, null, /<p[^>]+class="hi"[^>]*>([^<]+)/, replaceTagsAndSpaces);
         if(error)
             throw new AnyBalance.Error(error, null, /Проверьте логин и пароль/i.test(error));
@@ -43,14 +56,14 @@ function main(){
 
     getParam(html, result, 'balance', /<li>\s*Баланс:(?:<[^>]*>){2}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'days', /<li[^>]*>\s*Дней до блокировки:(?:<[^>]*>){1}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, '__tariff', /Ваш тарифный план[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'licschet', /Лицевой счет №\s*(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, '__tariff', /Ваш тарифный план[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'licschet', /Лицевой счет №\s*(\d+)/i, replaceTagsAndSpaces);
     /*
     if(/lk.seven-sky\.net/.test(url)){	
     } else if(/stat.seven-sky\.net/.test(url)){
     	//Может, этого уже и нет...
         getParam(html, result, 'balance', /Ваш баланс(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-        getParam(html, result, 'licschet', /счет N([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+        getParam(html, result, 'licschet', /счет N([^<]+)/i, replaceTagsAndSpaces);
     } else{
     	throw new AnyBalance.Error('Кабинет по адресу ' + url + ' не поддерживается. Сайт изменен?');
     }
@@ -58,3 +71,42 @@ function main(){
 
     AnyBalance.setResult(result);
 }
+
+function doGorkom(prefs) {
+    AnyBalance.setDefaultCharset('utf-8');
+	
+    var baseurl = 'https://stat.seven-sky.net/cgi-bin/';
+
+    var html = AnyBalance.requestGet(baseurl + 'clients/login', g_headers);
+    
+    if(!html || AnyBalance.getLastStatusCode() > 400) {
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+    }
+    
+	// Иначе выбрасывает ошибку
+	AnyBalance.sleep(2000);
+	
+    html = AnyBalance.requestPost(baseurl + 'clients/login', {
+        login: prefs.login,
+        password: prefs.password
+    }, addHeaders({Referer: baseurl}));
+	
+    if(!/action=logout/.test(html)) {
+        var error = getParam(html, null, null, /<span\s+style="color:#101010"[^>]*>([\s\S]*?)<\/span>/, [replaceTagsAndSpaces, /[\n\r\t]/g, ' ']);
+        if(error)
+            throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+    }
+	
+	var result = {success: true};
+	
+    getParam(html, result, 'balance', /Ваш баланс:([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'licschet', /Статистика пользователя:[\s\S]*?сч[ёе]т [N#№]\s*([^<]+)/i, replaceTagsAndSpaces);
+    getParam(html, result, 'fio', /Статистика пользователя:[\s\S]*?<b>([^<,]+)/i, replaceTagsAndSpaces);
+	
+    AnyBalance.setResult(result);
+}
+

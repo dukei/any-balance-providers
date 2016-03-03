aae73fd2410cc5c4ab6877fd191c057842177fc1
max.rasky <maxgabbana@gmail.com>
2016-03-03 16:49:55 +0300

    Always ask for captcha input; add missing AB namespace

--- providers/ab-telephony-zadarma/main.js
+++ providers/ab-telephony-zadarma/main.js
@@ -21,20 +21,11 @@ function main() {
   var html = AnyBalance.requestGet(baseurl + 'auth/', g_headers);
 
   if (!html || AnyBalance.getLastStatusCode() > 400) {
-    // AnyBalance.trace(html);
+    AnyBalance.trace(html);
     throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
   }
 
-  var captchaKey = '';
-
-
-  var authForm = AB.getElement(html, /<form[^>]*action="[^"]*auth\/login[^"]*"[^>]*>/i);
-  AnyBalance.trace(authForm);
-  // if(/<img[^>]*id="captcha_login"[^>]*>/i.test(authForm)) {
-  if (!/none[\s\S]*?id="captcha_login/i.test(authForm)) {
-    AnyBalance.trace('captcha discovered');
-    captchaKey = captchaAuth(baseurl, captchaUrl);
-  }
+  var captchaKey = captchaAuth(baseurl, captchaUrl);
 
   html = AnyBalance.requestPost(baseurl + "auth/login/", {
     redirect: '',
@@ -42,7 +33,7 @@ function main() {
     captcha: captchaKey || '',
     email: prefs.login,
     password: prefs.password
-  }, addHeaders({
+  }, AB.addHeaders({
     Referer: baseurl
   }));
 
@@ -51,10 +42,6 @@ function main() {
 
   if (json.success !== true) {
     var error = json.error;
-
-    // if (json.needCaptcha === true) {
-    //
-    // }
 
     if (error !== '') {
       throw new AnyBalance.Error(error, null, /найден/i.test(error));
@@ -70,27 +57,26 @@ function main() {
     success: true
   };
 
-  AB.getParam(html, result, 'balance', /<span class="balance">[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-  AB.getParam(html, result, ['currency', 'balance'], /<span class="balance">[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces,
-    parseCurrency);
+  AB.getParam(html, result, 'balance', /<span class="balance">[^>]*>([\s\S]*?)<\//i, AB.replaceTagsAndSpaces, AB.parseBalance);
+  AB.getParam(html, result, ['currency', 'balance'], /<span class="balance">[^>]*>([\s\S]*?)<\//i, AB.replaceTagsAndSpaces,
+      AB.parseCurrency);
   AB.getParam(html, result, '__tariff', [/<p><strong>(.*)<\/strong>( \((?:стоимость|вартість|cost) \d+\.\d+.*\))<\/p>/i,
     /<h2>Текущий тарифный план<\/h2>\s*<p>\s*<strong>(.*)<\/strong>/i
-  ], replaceTagsAndSpaces, html_entity_decode);
-  AB.getParam(html, result, 'min', /использовано:[^<]+\[(\d+ мин)/i, replaceTagsAndSpaces, parseMinutes);
+  ], AB.replaceTagsAndSpaces);
+  AB.getParam(html, result, 'min', /использовано:[^<]+\[(\d+ мин)/i, AB.replaceTagsAndSpaces, AB.parseMinutes);
 
   if (isAvailable(['phone0', 'phone0till', 'phone1', 'phone1till', 'phone2', 'phone2till'])) {
     html = AnyBalance.requestGet(baseurl + 'dirnum/', g_headers);
-    var numbers = sumParam(html, null, null,
+    var numbers = AB.sumParam(html, null, null,
       /<h2[^>]*>(?:Бесплатный|Безкоштовний|Free)(?:[^<](?!c донабором|з донабором|with dtmf))*<\/h2>(?:[^>]*>){20,25}(?:<h2|Номер действителен|Номер діє|The number works)/ig
     );
     for (var i = 0; i < Math.min(numbers.length, 3); ++i) {
       AB.getParam(numbers[i], result, 'phone' + i,
         /(?:Вам подключен прямой номер|Вам надано прямий номер|Your connected number|Вам підключено прямий номер)[^>]*>([\s\S]*?)<\//i,
-        replaceTagsAndSpaces, html_entity_decode);
+          AB.replaceTagsAndSpaces);
       AB.getParam(numbers[i], result, 'phone' + i + 'till', /(?:Действует до|Діє до|Valid till)([^<]*)\./i,
-        replaceTagsAndSpaces, parseDateISO);
-      AB.getParam(numbers[i], result, 'phone' + i + 'status', /(?:Статус)\s*:([^<]*)/i, replaceTagsAndSpaces,
-        html_entity_decode);
+          AB.replaceTagsAndSpaces, AB.parseDateISO);
+      AB.getParam(numbers[i], result, 'phone' + i + 'status', /(?:Статус)\s*:([^<]*)/i, AB.replaceTagsAndSpaces);
     }
   }
 
@@ -99,7 +85,7 @@ function main() {
 
     var numbers = AB.sumParam(html, null, null, /<li>\s*<a href="#\d+"[^>]*>([^<]*)/ig);
     for (var i = 0; i < Math.min(numbers.length, 5); ++i) {
-      AB.getParam(numbers[i], result, 'shortphone' + i, null, replaceTagsAndSpaces, html_entity_decode);
+      AB.getParam(numbers[i], result, 'shortphone' + i, null, AB.replaceTagsAndSpaces);
     }
   }
 
@@ -108,9 +94,8 @@ function main() {
 
 //help func
 function captchaAuth(baseurl, captchaUrl) {
-  var captcha, captchaSrc, captchaKey;
+  var captcha, captchaKey;
   if (AnyBalance.getLevel() >= 7) {
-    // var captcha, captchaSrc, captchaKey;
     AnyBalance.trace('Пытаемся ввести капчу');
     captcha = AnyBalance.requestGet(baseurl + captchaUrl + Math.random(), g_headers);
     captchaKey = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);

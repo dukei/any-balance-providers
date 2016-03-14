88fa60ea2669f037241b564cf42e729a67ad4223
Edward Anisonyan <stark@starksoft.ru>
2016-03-14 13:37:10 +0300

    Bloomberg (ab-exchange-bloomberg): - Minor changes, improved stability.

--- providers/ab-exchange-bloomberg/main.js
+++ providers/ab-exchange-bloomberg/main.js
@@ -3,32 +3,36 @@
 */
 
 var g_headers = {
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Cache-Control': 'max-age=0',
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+	'Accept-Language':'ru,en;q=0.8',
 	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'http://www.bloomberg.com/';
+	
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.quote, 'Please, enter quote!');
 
-	// Ищем котировку
 	var html = AnyBalance.requestGet(baseurl + 'apps/data?pid=symautocomplete&Query='+prefs.quote, g_headers);
 	
 	var href = getParam(html, null, null, /Symbols\s*<\s*\/\s*a>(?:[^>]*>){4}[^>]*href=(?:"|')\/([^("')]*)/i);
 	if(!href) {
+		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Please check entered quote. The quote ' + prefs.quote + ' has not been found!');
 	}
 	
 	html = AnyBalance.requestGet(baseurl + href, g_headers);
 	
 	var header = getElement(html, /<div[^>]*class="basic-quote"[^>]*>/i);
-	if(!header)
-		throw new AnyBalance.Error('Please check entered quote. The quote ' + prefs.quote + ' has not been found!');
+	if(!header) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Please check entered quote. The quote header ' + prefs.quote + ' has not been found!');
+	}
 	
 	var result = {success: true};
 	
@@ -37,8 +41,6 @@ function main() {
 	getParam(header, result, 'quote_show', /<h1[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(header, result, 'balance', /class\s*=\s*"price"[^>]*>([\s\d.,]+)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(header, result, ['currency', 'balance', 'trending'], /class\s*=\s*"currency"[^>]*>([^<]+)/i);
-
-
 	getParam(header, result, 'trending_pcts', /class\s*=\s*"\s*trending_[^>]*>[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);	
 	
 	var i = getParam(header, null, null, /class\s*=\s*"\s*trending_down[^>]*>([\s\S]*?)</i, replaceTagsAndSpaces, parseBalance);

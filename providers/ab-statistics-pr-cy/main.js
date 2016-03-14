0fcdaf6532acb6af6dda2c585536571baa961f6f
max.rasky <maxgabbana@gmail.com>
2016-03-14 15:12:30 +0300

    Adjust base url, add/replace counters

--- providers/ab-statistics-pr-cy/main.js
+++ providers/ab-statistics-pr-cy/main.js
@@ -16,25 +16,78 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences(),
-        siteUrl = prefs.login.replace("http://", ""),
-        baseurl = "http://pr-cy.ru/a/";
+        siteUrl = prefs.login.replace('http://', ''),
+        baseurl = 'https://a.pr-cy.ru/';
 
     AnyBalance.setDefaultCharset('utf-8'); 
 
     var html = AnyBalance.requestGet(baseurl + siteUrl, g_headers);
 
-    if(!/<!-- Показатели Яндекс -->/i.test(html)){
-        throw new AnyBalance.Error('Не удалось получить информацию. Возможно вы неверно ввели название домена. Или сайт изменен.');
-    }
-
     var result = {success: true};
-    getParam(html, result, 'yandexTic', /<!-- тИЦ -->(?:[\s\S]*?<a[^>]+>){1}([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'yandexCatalog', /<!-- Яндекс каталог -->(?:[\s\S]*?<a[^>]+>){1}([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'yandexIndex', /<!-- Индекс Яндекса -->(?:[\s\S]*?<a[^>]+>){1}([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, html_entity_decode);
+    AB.getParam(html, result, 'searchSystems_mainParams_yandexTic', /Яндекс ТИЦ([^>]+>){5}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'searchSystems_mainParams_yandexRank', /Яндекс Rank(?:[^>]+>){3}\D*(\d+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'searchSystems_mainParams_googleRank', /Google PageRank(?:[^>]+>){3}\D*(\d+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
 
-    getParam(html, result, 'googlePR', /<!-- PR -->(?:[\s\S]*?<div[^>]+>){1}([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'googleCatalog', /<!-- DMOZ -->(?:[\s\S]*?<a[^>]+>){1}([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'googleIndex', /<!-- Гугл Индекс -->(?:[\s\S]*?<a[^>]+>){1}([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, html_entity_decode);
+    AB.getParam(html, result, 'searchSystems_indexing_yandexIndex', /<h3>Индексация[\s\S]+?Яндекс([^>]+>){5}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'searchSystems_indexing_googleIndex', /<h3>Индексация[\s\S]+?info-test[^>]*>Google([^>]+>){5}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+
+    AB.getParam(html, result, 'searchSystems_catalog_yandexCatalog', /<h3>Каталоги[\s\S]+?Яндекс\.Каталог(?:[^>]+>){3}((?:[^>]+>){2})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'searchSystems_catalog_dmozCatalog', /<h3>Каталоги[\s\S]+?info-test[^>]*>DMOZ([^>]+>){4}/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'searchSystems_catalog_mailRuCatalog', /<h3>Каталоги[\s\S]+?info-test[^>]*>Mail\.ru([^>]+>){4}/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'searchSystems_catalog_ramblerCatalog', /<h3>Каталоги[\s\S]+?info-test[^>]*>Rambler(?:[^>]+>){3}((?:[^>]+>){2})/i, AB.replaceTagsAndSpaces);
+
+    AB.getParam(html, result, 'searchSystems_sanction_domainSplicing', /<h3>Санкции[\s\S]+?Склейка домена(?:[^>]+>){3}((?:[^>]+>){2})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'searchSystems_sanction_agsFilter', /<h3>Санкции[\s\S]+?Фильтр АГС(?:[^>]+>){3}((?:[^>]+>){2})/i, AB.replaceTagsAndSpaces);
+
+    AB.getParam(html, result, 'searchSystems_virusCheck_yandexVirusCheck', /<h3>Проверка на вирусы[\s\S]+?Вирусы от Yandex(?:[^>]+>){3}((?:[^>]+>){2})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'searchSystems_virusCheck_googleVirusCheck', /<h3>Проверка на вирусы[\s\S]+?Вирусы от Google(?:[^>]+>){3}((?:[^>]+>){2})/i, AB.replaceTagsAndSpaces);
+
+
+    AB.getParam(html, result, 'traffic_alexRating_alexWorldRating', /Рейтинг Alexa[\s\S]+?Место в мире([^>]+>)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'traffic_alexRating_alexCountryRating', /Рейтинг Alexa[\s\S]+?Место в стране([^>]+>){2}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+
+    AB.getParam(html, result, 'traffic_gender_male', /Гендер[\s\S]+?мужчин[\s\S]*?([\d,.]+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'traffic_gender_female', /Гендер[\s\S]+?женщин[\s\S]*?([\d,.]+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+
+
+    AB.getParam(html, result, 'links_linkPad_referPages', /LinkPad[\s\S]+?ссылается страниц([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_linkPad_referDomains', /LinkPad[\s\S]+?ссылаются доменов([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+
+    AB.getParam(html, result, 'links_majesticSeo_referPages', /MajesticSEO[\s\S]+?ссылается страниц([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_majesticSeo_referDomains', /MajesticSEO[\s\S]+?ссылаются доменов([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_majesticSeo_trust', /MajesticSEO[\s\S]+?Траст(?:[^>]+>){5}\D+(\d+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_majesticSeo_quoting', /MajesticSEO[\s\S]+?Цитирование(?:[^>]+>){5}\D+(\d+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+
+    AB.getParam(html, result, 'links_socialNetwork_commonSocialActivity', /общая социальная активность([^>]+>)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_socialNetwork_vkActivity', /Социальная активность[\s\S]*?Вконтакте([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_socialNetwork_facebookLikes', /facebook лайки([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_socialNetwork_facebookShared', /facebook шареды([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'links_socialNetwork_googlePlus', /Социальная активность[\s\S]*?google\+([^>]+>){4}/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+
+
+    AB.getParam(html, result, 'optimization_content_pageTitle', /заголовок страницы(?:[^>]+>){3}((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_content_pageDescription', /описание страницы(?:[^>]+>){3}((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+
+    AB.getParam(html, result, 'optimization_heading_pageHeaders', /info-test[^>]*>заголовки(?:[^>]+>){5}((?:[^>]+>){35})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_heading_wordsAmount', /info-test[^>]*>количество слов([^>]+>){4}/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_heading_textLength', /info-test[^>]*>длина текста([^>]+>){4}/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_heading_loathing', /info-test[^>]*>тошнота(?:[^>]+>){3}\D*([\d.,]+)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'optimization_heading_pageSize', /info-test[^>]*>размер HTML страницы([^>]+>){4}/i, AB.replaceTagsAndSpaces);
+
+    AB.getParam(html, result, 'optimization_serverInfo_ip', /info-test[^>]*>IP([^>]+>){4}/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_location', /info-test[^>]*>местоположение сервера((?:[^>]+>){5})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_dataCenter', /info-test[^>]*>датацентр((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_domainAge', /info-test[^>]*>Возраст домена((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_domainTermination', /info-test[^>]*>окончание домена((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_sslCertificate', /info-test[^>]*>SSL-сертификат((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_redirect', /info-test[^>]*>редирект[^>]+>((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_encoding', /info-test[^>]*>кодировка((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_robotsFile', /info-test[^>]*>файл robots\.txt((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'optimization_serverInfo_sitemap', /info-test[^>]*>наличие sitemap((?:[^>]+>){8})/i, AB.replaceTagsAndSpaces);
+
+    AB.getParam(html, result, 'usability_main_favicon', /info-test[^>]*>favicon((?:[^>]+>){6})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'usability_main_responseCode404', /info-test[^>]*>код ответа страницы 404((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'usability_main_linkFrom404Page', /info-test[^>]*>ссылка со страницы 404((?:[^>]+>){4})/i, AB.replaceTagsAndSpaces);
 
     AnyBalance.setResult(result);
 }

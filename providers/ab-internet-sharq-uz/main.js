7d23acfd2edb658673040907a73f084b6c8720f4
Xenia <adri.espanol@gmail.com>
2016-03-24 17:45:43 +0300

    Исправлен провайдер SHARQ TELECOM (ab-internet-sharq-uz)

--- providers/ab-internet-sharq-uz/main.js
+++ providers/ab-internet-sharq-uz/main.js
@@ -8,9 +8,6 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
@@ -47,39 +44,45 @@ function main() {
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 
-	var result = {
-		success: true
-	};
+	var result = {success: true};
 
 	AB.getParam(html, result, '__tariff', /Тариф:([\s\S]*?)<table/i, AB.replaceTagsAndSpaces);
-	//состояние счёта
 
+	//состояние счёта
 	AB.getParam(html, result, 'c_p_b_balance',
 		/(?:Финансовая\s+история|Молиявий\s+тарих)[\s\S]*?(?:периода|ҳолати):([\s\S]*?)(?:Текущее|Шахсий)/i,
 		AB.replaceTagsAndSpaces, AB.parseBalance);
+
 	AB.getParam(html, result, ['currency', 'c_p_b_balance'],
 		/(?:Финансовая\s+история|Молиявий\s+тарих)[\s\S]*?(?:периода|ҳолати):([\s\S]*?)(?:Текущее|Шахсий)/i,
 		AB.replaceTagsAndSpaces, AB.parseCurrency);
+
 	AB.getParam(html, result, 'c_balance', /(?:Финансовая\s+история|Молиявий\s+тарих)[\s\S]*?(?:Текущее|Шахсий)([\s\S]*?)(?:Расход|Мавжуд)/i,
 		AB.replaceTagsAndSpaces, AB.parseBalance);
+
 	AB.getParam(html, result, 'expense', /(?:Финансовая\s+история|Молиявий\s+тарих)[\s\S]*?(?:Расход|Мавжуд)([\s\S]*?)<div/i, AB.replaceTagsAndSpaces,
 		AB.parseBalance);
+
 	//персональные данные
-	AB.getParam(html, result, 'id', /\(ID\):([\s\S]*?)</i, AB.replaceTagsAndSpaces);
-	AB.getParam(html, result, 'date', /(?:Дата\s+регистрации|Рўйхатдан\s+ўтилган\s+сана):\s*<\/td>([\s\S]*?<\/td>)/i,
-		AB.replaceTagsAndSpaces, AB.parseDate);
-	AB.getParam(html, result, 'login', /Логин:\s*<\/td>([\s\S]*?<\/td>)/i, AB.replaceTagsAndSpaces);
-	AB.getParam(html, result, 'phone', /(?:Мобильный\s+телефон|Уяли\s+телефон):([\s\S]*?)<div/i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'id',     /\(ID\):([\s\S]*?)</i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'date',   /(?:Дата\s+регистрации|Рўйхатдан\s+ўтилган\s+сана):\s*<\/td>([\s\S]*?<\/td>)/i,AB.replaceTagsAndSpaces, AB.parseDate);
+	AB.getParam(html, result, 'fio',    /(?:Ф.И.Ш|Ф.И.О):([^<]*)/i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'adress', /(?:Манзили|Адрес):([^<]*)/i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'email',  /<a[^>]+client_email[^>]*>([^<]*)/i, AB.replaceTagsAndSpaces);
+
+	AB.getParam(html, result, 'login',    /Логин:\s*<\/td>([\s\S]*?<\/td>)/i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'phone',    /(?:Мобильный\s+телефон|Уяли\s+телефон):([\s\S]*?)<div/i, AB.replaceTagsAndSpaces);
 	AB.getParam(html, result, 'exchange', /(<h4>Курс\s+валют[\s\S]*?<\/table>)/i, AB.replaceTagsAndSpaces);
+
 	//Трафик
-	var
-		table = AB.getElement(html, /<div[^>]*class="[^"]*traffic[^"]*"[^>]*>[\s\S]*?(<table[^>]*>)/i),
-		traffic = AB.sumParam(table, null, null, /<tr[^>]*>([\s\S]*?)<\/tr>/gi, AB.replaceTagsAndSpaces);
-
-	AB.getParam(traffic.join(', '), result, 'traffic');
-
-
-	// AB.getParam(html, result, 'status', /Статус:[\s\S]*?<b[^>]*>([\s\S]*?)<\/b>/i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'iTraffic', 	 /(?:ваш трафик|Сизнинг трафик)(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces, AB.parseTraffic);
+	AB.getParam(html, result, 'oTraffic', 	 /(?:ваш трафик|Сизнинг трафик)(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces, AB.parseTraffic);
+	AB.getParam(html, result, 'iTrafficTAS', /(?:входящий TAS-IX|TAS-IX учун кириш):(?:[^>]*>){2}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseTraffic);
+	AB.getParam(html, result, 'oTrafficTAS', /(?:Исходящий TAS-IX|TAS-IX учун чиқиш):(?:[^>]*>){2}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseTraffic);
+	AB.getParam(html, result, 'iTrafficST',  /(?:Входящий|Кириш)\s*ST:(?:[^>]*>){2}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseTraffic);
+	AB.getParam(html, result, 'oTrafficST',  /(?:Исходящий|Чиқиш)\s*ST:(?:[^>]*>){2}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseTraffic);
+	AB.getParam(html, result, 'traffic',     /(?:Лимит трафика|Трафик ҳажми)(?:[^>]*>){3}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseTraffic);
+	AB.getParam(html, result, 'status', 	 /(?:состояние услуги|Хизмат ҳолати):(?:[^>]*>){3}([^<]*)/i);
 
 	AnyBalance.setResult(result);
 }

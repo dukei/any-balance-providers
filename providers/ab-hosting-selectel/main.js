246431719a67d87a3ce173a71064d4c5966d6e7b
Edward Anisonyan <stark@starksoft.ru>
2016-03-29 18:52:25 +0300

    Selectel (Хостинг) (ab-hosting-selectel): - Поддержка изменений на сайте.

--- providers/ab-hosting-selectel/main.js
+++ providers/ab-hosting-selectel/main.js
@@ -1,59 +1,65 @@
 ﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Информация о балансе у хостинга Selectel.
-
-Сайт оператора: http://selectel.ru/
-Личный кабинет: https://support.selectel.ru/
 */
 
-function main(){
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
+function main() {
 	var prefs = AnyBalance.getPreferences();
-
-	AnyBalance.setDefaultCharset('utf-8');
-
 	var baseurl = 'https://support.selectel.ru/';
-
-	var html = AnyBalance.requestPost(baseurl + 'api/login', {
-		login:prefs.login,
-		password:prefs.password,
-	});
-
-	var status = AnyBalance.getLastStatusCode();
-
-	if( status != 200){
-		throw new AnyBalance.Error('Ошибка авторизации');
+	AnyBalance.setDefaultCharset('utf-8');
+	
+	AB.checkEmpty(prefs.login, 'Введите логин!');
+	AB.checkEmpty(prefs.password, 'Введите пароль!');
+	
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 402){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
-
-	html = AnyBalance.requestGet(baseurl);
-
-	status = AnyBalance.getLastStatusCode();
-
-	if( status != 200){
-		throw new AnyBalance.Error('Ошибка получения данных');
+	
+	html = AnyBalance.requestPost(baseurl + 'login/', {
+		login: prefs.login,
+		password: prefs.password,
+	}, AB.addHeaders({Referer: baseurl}));
+	
+	if (!/logout/i.test(html)) {
+		var error = AB.getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)/i, AB.replaceTagsAndSpaces);
+		if (error)
+			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
-
+	
 	var result = {success: true};
-
-	getParam(html, result, 'cloudBalance', /"cloud":{"balance":\d+/i, replaceTagsAndSpaces, parseBalance);
-	result['cloudBalance']=result['cloudBalance']/100;
-
-	getParam(html, result, 'storageBalance', /"storage":{"balance":\d+/i, replaceTagsAndSpaces, parseBalance);
-	result['storageBalance']=result['storageBalance']/100;
-
-	getParam(html, result, 'Balance', /,"balance":"\d+/i, replaceTagsAndSpaces, parseBalance);
-	result['Balance']=result['Balance']/100;
-
-	getParam(html, result, 'vkBalance', /","vk_balance":"\d+/i, replaceTagsAndSpaces, parseBalance);
-
-	getParam(html, result, 'id', /{"id":\d+/i, replaceTagsAndSpaces, parseBalance);
-
-	if(AnyBalance.isAvailable('username')) {
-		getParam(html, result, 'username', /,"username":"[^"]*/i);
-		a=result['username'].split("\":\"");
-		result['username'] = a[a.length-1]
+	
+	var info = getJsonObject(html, /var\s+client_info\s*=/i);
+	if(!info) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось получить информацию по пользователю. Сайт изменен?');
 	}
-
+	
+	getParam(getJsonObjSafe(info, ['cloud', 'balance']), result, 'cloudBalance', null, AB.replaceTagsAndSpaces, parseBalanceDivision);
+	getParam(getJsonObjSafe(info, ['storage', 'balance']), result, 'storageBalance', null, AB.replaceTagsAndSpaces, parseBalanceDivision);
+	getParam(getJsonObjSafe(info, 'balance'), result, 'Balance', null, AB.replaceTagsAndSpaces, parseBalanceDivision);
+	getParam(getJsonObjSafe(info, 'vk_balance'), result, 'vkBalance', null, AB.replaceTagsAndSpaces, AB.parseBalance);
+	getParam(getJsonObjSafe(info, 'id'), result, 'id', null, AB.replaceTagsAndSpaces);
+	getParam(getJsonObjSafe(info, 'username'), result, 'username', null, AB.replaceTagsAndSpaces);
+	
 	AnyBalance.setResult(result);
 }
 
+function parseBalanceDivision(str) {
+	var val = AB.parseBalance(str + '');
+	
+	if(isset(val))
+		return val/100;
+}

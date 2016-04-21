21dd7e9392ec70f699de7881725d356d0aa060a2
Dmitry Kochin <kochin.du@krawlly.com>
2016-04-21 12:19:14 +0300

    ГІОЦ КМДА 2016 (ab-municipal-gioc-kiev): Добавлено детальное отображение статей расхода

--- providers/ab-municipal-gioc-kiev/main.js
+++ providers/ab-municipal-gioc-kiev/main.js
@@ -8,7 +8,7 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
 };
 
 function main() {
@@ -48,10 +48,33 @@ function main() {
 		success: true
 	};
 
-	AB.getParam(html, result, 'debt', /<div class="values">(?:[^>]*>){7}([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces, AB.parseBalance);
-	AB.getParam(html, result, 'payed', /<div class="values">(?:[^>]*>){22}([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces, AB.parseBalance);
-	AB.getParam(html, result, 'address', /houses_line(?:[^>]*>){7}((?:[^>]*>){5})/i, [AB.replaceTagsAndSpaces, /\n/g, ' ']);
-	AB.getParam(html, result, 'pay_period', /houses_line[\s\S]*?bydate[^>]*>([^<]+)/i, AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'debt', /Сума до сплати[\s\S]*?<div[^>]+class="value"[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(html, result, 'payed', /Сплачено за[\s\S]*?<div[^>]+class="value"[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(getElement(html, /<div[^>]+class="title"[^>]*>/i), result, 'address', null, [AB.replaceTagsAndSpaces, /\n/g, ' ']);
+	AB.getParam(html, result, 'pay_period', /<div[^>]+class="bydate"[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces);
+
+	var countersToCheck = [];
+	for(var i=0; i<8; ++i){
+		countersToCheck.push('type' + i);
+		countersToCheck.push('value' + i);
+	}
+
+	if(isAvailable(countersToCheck)){
+		var url = getParam(html, null, null, /<a[^>]+href="([^"]*\/cabinet\/objects\/\d+[^"]*)/i, replaceHtmlEntities);
+		AnyBalance.trace('Ссылка на счет: ' + url);
+		if(url){
+			html = AnyBalance.requestGet(url, g_headers);
+			var bill = getElement(html, /<table[^>]+d[ae]tailbill-table[^>]*>/i);
+			var rows = getElements(bill, /<tr[^>]+item-row[^>]*>/ig);
+			AnyBalance.trace('Найдено ' + rows.length + ' строк в счете');
+	    
+			for(i=0; i<rows.length; ++i){
+				var row = rows[i];
+				getParam(row, result, 'type' + i, /<label[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+				getParam(row, result, 'value' + i, /<input[^>]+bill-summ-input[^>]*value="([^"]*)/i, replaceHtmlEntities, parseBalance);
+			}
+		}
+	}
 
 	AnyBalance.setResult(result);
 }

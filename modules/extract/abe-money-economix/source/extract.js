c8bf926395ebb74e1b6a75089359d83ca5018b53
Xenia P <adri.espanol@gmail.com>
2016-05-14 08:47:47 +0300

    Поддержка изменений на сайте (ab-money-economix) (#820)

--- modules/extract/abe-money-economix/source/extract.js
+++ modules/extract/abe-money-economix/source/extract.js
@@ -3,10 +3,10 @@
 */
 
 var g_headers = {
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept':         'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36'
+	'Connection':     'keep-alive',
+	'User-Agent':     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36'
 };
 
 var baseurl = 'https://ibank.economiks.ru:2443';
@@ -51,6 +51,7 @@ function login(prefs) {
 				return AnyBalance.retrieveCode('Пожалуйста, введите код с картинки', img, {time: 180000});
 			}
 		});
+
 		html = AnyBalance.requestPost(baseurl+'/web_banking/j_security_check', params, addHeaders({
 			Referer: baseurl + '/web_banking/login.jsf'
 		}));
@@ -80,77 +81,54 @@ function processAccounts(html, result) {
     if(!AnyBalance.isAvailable('accounts'))
         return;
 
-	//На одной странице находятся и карты и счета.
-	//Ищем все таблицы.
-	var accountsTable = getElements(html, /<table[^>]+class="tbllist"[^>]*>/ig, null, html_entity_decode);
-	if(!accountsTable) {
-		AnyBalance.trace(html);
-		AnyBalance.trace('Не удалось найти таблицу со счетами.');
-		return;
-	}
+  html = AnyBalance.requestGet(baseurl + '/web_banking/protected/accounts/index', g_headers);
 
-	//Ищем таблицу со счетами.
-	for(var i=0; i < accountsTable.length; i++) {
-		if(/счет/i.test(accountsTable[i])) {
-			var rows = getElements(accountsTable[i], /<tr[^>]*>/ig);
-		}
-	}
-	if(!rows) {
+	var accounts = getElements(html, /<div[^>]+productShort[^>]*>/ig, null, html_entity_decode);
+	if(!accounts.length) {
 		AnyBalance.trace(html);
-		AnyBalance.trace("Не нашли ни одного счёта.");
-		result.accounts = [];
+		AnyBalance.trace('Не удалось найти счета.');
 		return;
 	}
 
 
-	AnyBalance.trace('Найдено счетов: ' + (rows.length - 1)); //т.к. захватывает первую строчку с описанием колонок
+	AnyBalance.trace('Найдено счетов: ' + accounts.length);
 	result.accounts = [];
 	
-	for(var i=1; i < rows.length; ++i){
-        var acc = rows[i];
-		var id = getParam(acc, null, null, /<a[^>]*>(\d+)/i); //Там нет никаких отличительных особенностей. ТОлько номер
-		var num = getParam(acc, null, null, /<a[^>]*>(\d+)/i);
-		var title = getParam(acc, null, null, /<a[^>]*>([\s\S]*?)<\/a>/i);
+	for(var i = 0; i < accounts.length; ++i){
+    var acc   = accounts[i],
+        id    = getParam(acc, null, null, /<td[^>]+lh20 w100Pc paddR10[^>]*>([^<]*)/i, [replaceTagsAndSpaces, /,\s*/g, '']),
+        num   = getParam(acc, null, null, /<td[^>]+lh20 w100Pc paddR10[^>]*>([^<]*)/i, [replaceTagsAndSpaces, /,\s*/g, '']),
+        title = getParam(acc, null, null, /<span[^>]+aliasText[^>]*>([^<]*)/i);
 		
 		var c = {__id: id, __name: title, num: num};
 		
 		if(__shouldProcess('accounts', c)) {
-			processAccount(html, acc, c);
+			processAccount(acc, c);
 		}
 		
 		result.accounts.push(c);
 	}
 }
 
-function processAccount(html, account, result){
+function processAccount(acc, result){
     AnyBalance.trace('Обработка счета ' + result.__name);
 
-	//Нужно взять параметры, для отправки пост запроса по выбранной карте
-	var postTable = getElement(html, /<div[^>]+class="d-body-content"[^>]*>/i, null, html_entity_decode);
-	var params = createFormParams(postTable, function(params, str, name, value) {
-		return value;
-	});
+  var href = getParam(acc, null, null, /<a[^>]+href="([^"]*)/i);
+  if(!href) {
+    AnyBalance.trace(acc);
+    AnyBalance.trace("Не удалось найти ссылку на подробную информацию о счёте.")
+  }
 
-	var paramsLabel = getParam(account, null, null, /submitForm\('([\s\S]*?)',/i, [ /(.+)/i, '$1'+':_idcl']);
-	params[paramsLabel] = getParam(account, null, null, /submitForm\([^,]+,\s*'([\s\S]*?)'/i);
-	params.account_id = getParam(account, null, null, /'account_id',\s*'([\s\S]*?)'/i);
-	if(!params[paramsLabel] || !params.account_id || !paramsLabel) {
-		AnyBalance.trace("Не смогли найти параметр запроса данных по карте " + result.__name);
-		return;
-	}
+  var html = AnyBalance.requestGet(baseurl + href, g_headers);
 
-	html = AnyBalance.requestPost(baseurl+'/web_banking/protected/welcome.jsf', params, addHeaders({
-		Referer:baseurl+'/web_banking/protected/welcome.jsf'
-	}));
-
-	getParam(account, result, 'accounts.status', /<span[^>]+class="lowercase"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
-	getParam(html, result, 'accounts.agreement', /id="info"(?:[\s\S]*?<div[^>]*>){3}[^:]*:([\s\S]*?)<\//i, replaceTagsAndSpaces);
-	getParam(html, result, 'accounts.date_start', /id="info"(?:[\s\S]*?<div[^>]*>){4}[^:]*:([\s\S]*?)<\//i, replaceTagsAndSpaces, parseDate);
-	getParam(html, result, 'accounts.balance', /<span[^>]+class="amountBox\s*"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, ['accounts.currency' , 'accounts.balance', 'accounts.incomingLefts'], /<span[^>]+class="amountBox\s*"[^>]*>([\s\S]*?)<\//i, replaceTagsAndSpaces, parseCurrency);
+	getParam(acc,  result, 'accounts.status',                          /<span[^>]+class="lowercase"[^>]*>([\s\S]*?)<\/span>/i,                replaceTagsAndSpaces);
+	getParam(html, result, 'accounts.agreement',                       /decorate-block(?:[\s\S]*?<span[^>]+infoLine[^>]*>){2}[^:]*:([^<]*)/i, replaceTagsAndSpaces);
+	getParam(html, result, 'accounts.date_start',                      /amountbox[\s\S]*?<span[^>]+infoLine[^>]*>([^<]*)/i,                   replaceTagsAndSpaces, parseDate);
+	getParam(html, result, 'accounts.balance',                         /<span[^>]+amountBox[^>]*>([\s\S]*?)<\//i,                             replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, ['accounts.currency' , 'accounts.balance'], /<span[^>]+amountBox[^>]*>([\s\S]*?)<\//i,                             replaceTagsAndSpaces, parseCurrency);
 
 	if(AnyBalance.isAvailable('accounts.transactions')) {
-		processAccountTransactions(html, result);
+		processAccountTransactions(html, href, result);
 	}
 }
 
@@ -163,17 +141,17 @@ function processInfo(html, result){
 		AnyBalance.trace("Не смогли найти таблицу с пользовательскими данными.");
 		return;
 	}
-	getParam(infoTable, info, 'info.name_last', /Фамилия(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.name', /Имя(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.name_patronymic', /Отчество(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.email', /e-mail(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.mphone', /мобильный телефон(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.hphone', /Домашний телефон(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.wphone', /Рабочий телефон(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.inn', /инн(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.passport', /номер(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.raddress', /адрес регистрации(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
-	getParam(infoTable, info, 'info.faddress', /адрес фактического(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.name_last',       /Фамилия(?:[^>]*>){2}([\s\S]*?)<\/td>/i,            replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.name',            /Имя(?:[^>]*>){2}([\s\S]*?)<\/td>/i,                replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.name_patronymic', /Отчество(?:[^>]*>){2}([\s\S]*?)<\/td>/i,           replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.email',           /e-mail(?:[^>]*>){2}([\s\S]*?)<\/td>/i,             replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.mphone',          /мобильный телефон(?:[^>]*>){2}([\s\S]*?)<\/td>/i,  replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.hphone',          /Домашний телефон(?:[^>]*>){2}([\s\S]*?)<\/td>/i,   replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.wphone',          /Рабочий телефон(?:[^>]*>){2}([\s\S]*?)<\/td>/i,    replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.inn',             /инн(?:[^>]*>){2}([\s\S]*?)<\/td>/i,                replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.passport',        /номер(?:[^>]*>){2}([\s\S]*?)<\/td>/i,              replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.raddress',        /адрес регистрации(?:[^>]*>){2}([\s\S]*?)<\/td>/i,  replaceTagsAndSpaces);
+	getParam(infoTable, info, 'info.faddress',        /адрес фактического(?:[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Обработка депозитов
@@ -194,3 +172,11 @@ function processCards(html, result) {
 	throw new AnyBalance.Error('Обработка карт пока не поддерживается. Пожалуйста, обратитесь к разработчикам.');
 }
 
+function getParamByName(html, names) {
+  names = isArray(names) ? names : [names];
+  var params = {};
+  for(var i = 0; i < names.length; i++) {
+    params[names[i]] = getParam(html, null, null, new RegExp('name=["\']' + names[i] + '["\'][^>]*value=["\']([^"\']+)"', 'i')) || '';
+  }
+  return params;
+}

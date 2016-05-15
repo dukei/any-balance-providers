46986c00991f521ecf1a957f530f5a5f4914f444
Dmitry Kochin <dco@mail.ru>
2016-05-15 10:36:16 +0300

    Домашний Интернет и Телевидение МТС (ab-internet-mts): Исправлен барнаул

--- providers/ab-internet-mts/main.js
+++ providers/ab-internet-mts/main.js
@@ -881,49 +881,8 @@ function getKurgan() {
 }
 
 function getBarnaul() {
-  var prefs = AnyBalance.getPreferences();
-  AnyBalance.setDefaultCharset('utf-8');
-
   var baseurl = 'https://kabinet.barnaul.mts.ru/';
-  //TODO: Лучше перевести на typicalApiInetTv!!!
-
-  var html = AnyBalance.requestPost(baseurl + 'res/modules/AjaxRequest.php?Method=Login', {
-    BasicAuth: true,
-    'Client': 'mts',
-    'Data[Login]': prefs.login,
-    'Data[Passwd]': prefs.password,
-    'Service': 'API.User.Service'
-  });
-
-  var json = getJson(html);
-
-  if (json.Error == true) {
-    throw new AnyBalance.Error("Не удалось войти в личный кабинет. Неправильный логин-пароль?");
-  }
-  var token = json.Result.Result.Token[0];
-  html = AnyBalance.requestPost(baseurl + 'res/modules/AjaxRequest.php?Method=GetContainerByPath', {
-    'AccessToken': token,
-    'Client': 'mts',
-    'Service': 'API.Interface.Service'
-  });
-  json = getJson(html);
-  html = JSON.stringify(json);
-
-  AnyBalance.trace(html);
-
-  var result = {
-    success: true
-  };
-  //Вначале попытаемся найти активный тариф
-  getParam(html, result, '__tariff', /Name[\s\S]{1,20}Тариф\s*'([\s\S]*?)'/i, replaceTagsAndSpaces, html_entity_decode);
-  getParam(html, result, 'balance', /([\d.\-,]{1,10})(?:\&nbsp;|\s)руб/i, replaceTagsAndSpaces, parseBalance2);
-
-  /*  getParam(html, result, 'abon', /Абонентская плата:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
-      getParam(html, result, 'internet_cur', /Израсходовано:([^<]*)/i, replaceTagsAndSpaces, parseBalance2);
-      getParam(html, result, 'agreement', /Номер договора:[^<]*<[^>]*>([^<]*)/i, replaceTagsAndSpaces);
-      getParam(html, result, 'username', /Мои аккаунты[\s\S]{1,150}<strong>([\s\S]*?)<\/strong>/i, null);
-  */
-  AnyBalance.setResult(result);
+  typicalApiInetTv(baseurl);
 }
 
 function getNovokuz() {

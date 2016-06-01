db4a0e8260edf8ad2bb7263d2799bfee21930f91
borislip <borislip@users.noreply.github.com>
2016-06-01 22:29:50 +0300

    ab-money-bitcoin-wallet-stats - По просьбе пользователя добавил конвертирование в выбранную в настройках валюту.

--- providers/ab-money-bitcoin-wallet-stats/btcutil.js
+++ providers/ab-money-bitcoin-wallet-stats/btcutil.js
@@ -10,3 +10,26 @@ function validateBtcWallet(wallet)
 	if ((wallet[0]!='1') && (wallet[0]!='3'))
 		throw new AnyBalance.Error("Invalid BTC wallet, must begin with either '1' or '3'!");
 }
+
+// BTC exchange rate by currency code, from coindesk.com
+function getExchangeRate(currency)
+{
+    currency = currency.toUpperCase();
+    if (currency.match(/^[A-Z][A-Z][A-Z]$/)==null)
+        throw new AnyBalance.Error("currency code must be 3 characters long, English letters only");
+    var json = AnyBalance.requestGet('http://api.coindesk.com/v1/bpi/currentprice/' + currency + '.json', g_headers);
+    try 
+    {
+        json = JSON.parse(json);
+    } 
+    catch(error)
+    {
+        // errors are returned by coindesk as plain text string, so json parsing would fail
+        throw new AnyBalance.Error("coindesk.com: "+json);
+    }
+    
+    if ((typeof json.bpi=="undefined") || (typeof json.bpi[currency]=="undefined"))
+        throw new AnyBalance.Error("unexpected coindesk.com repsonse");
+
+    return(json.bpi[currency]);
+}

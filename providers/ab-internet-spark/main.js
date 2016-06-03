fb0469d1d414da0af35162a9aeab5db6c47b41ba
Dmitry Kochin <kochin.du@krawlly.com>
2016-06-03 14:27:52 +0300

    ТТК (СПАРК) (ab-internet-spark): - Поддержка изменений на сайте.

--- providers/ab-internet-spark/main.js
+++ providers/ab-internet-spark/main.js
@@ -7,37 +7,10 @@
 Личный кабинет: https://stat.spark-com.ru
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
-
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-function getTrafficGb(str){
-  return parseFloat((parseFloat(str)/1000).toFixed(2));
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
 
     var baseurl = "https://stat.spark-com.ru/";
-
 
     AnyBalance.setDefaultCharset('utf-8');
 
@@ -46,33 +19,30 @@ function main(){
         pass: prefs.password
     });
 
-    var error = getParam(html, null, null, /<ol class="error[^>]*>([\s\S]*?)<\/ol>/i, [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/, '']);
-    if(error)
-        throw new AnyBalance.Error(error);
+    if(!/FormLoginOut/i.test(html)){
+        var error = getElement(html, /<ol[^>]*class="error[^>]*>/i, replaceTagsAndSpaces);
+        if(error)
+            throw new AnyBalance.Error(error, null, /парол/i.test(error));
+
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    }
 
     var result = {success: true};
     
-    getParam(html, result, 'userName', /ФИО:[\s\S]*?>([^<]*)</i);
-    getParam(html, result, 'licschet', /Лицевой счет:[\s\S]*?>([^<]*)</i);
-    getParam(html, result, 'balance', /Текущий баланс:[\s\S]*?>([\-\d,\.\s]*)/i, [/\s+/g, '', /,/g, '.'], parseFloat);
-    getParam(html, result, 'abonentka', /Текущая абонентская плата за все услуги:[\s\S]*?>([\-\d,\.\s]*)/i, [/\s+/g, '', /,/g, '.'], parseFloat);
-    getParam(html, result, 'traffic', /Расход трафика в текущем месяце:[\s\S]*?>([\-\d,\.\s]*)/i, [/\s+/g, '', /,/g, '.'], parseFloat);
-    getParam(html, result, 'trafficGb', /Расход трафика в текущем месяце:[\s\S]*?>([\-\d,\.\s]*)/i, [/\s+/g, '', /,/g, '.'], getTrafficGb);
-    getParam(html, result, 'status', /Статус подключения:[\s\S]*?>([^<]*)/i);
-    getParam(html, result, '__tariff', /Текущий тарифный план:[\s\S]*?>([^<]*)/i, null, html_entity_decode);
+    getParam(html, result, 'userName', /ФИО:([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'licschet', /Лицевой счет:([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'balance', /Текущий баланс:([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'abonentka', /Текущая абонентская плата за все услуги:([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'traffic', /Расход трафика в текущем месяце:([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'trafficGb', /Расход трафика в текущем месяце:([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces, parseTrafficGb);
+    getParam(html, result, 'status', /Статус подключения:([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces);
+    getParam(html, result, '__tariff', /Текущий тарифный план:[\s\S]*?>([\s\S]*?)<\/(?:li|div)>/i, replaceTagsAndSpaces);
 
     if(AnyBalance.isAvailable('bonus')){
-	var html = AnyBalance.requestGet(baseurl + "account_bonuses/");
-    	getParam(html, result, 'bonus', /Итого<\/td><td[^>]*>[^<]*<\/td><td[^>]*>([\-\d,\.\s]*)/i, [/\s+/g, '', /,/g, '.'], parseFloat);
+		var html = AnyBalance.requestGet(baseurl + "account_bonuses/");
+    	getParam(html, result, 'bonus', /Итого<\/td><td[^>]*>[^<]*<\/td><td[^>]*>([\-\d,\.\s]*)/i, replaceTagsAndSpaces, parseBalance);
     }
     
     AnyBalance.setResult(result);
-}
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
 }

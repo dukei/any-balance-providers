d8436a57d65061996438d180e5dca15c5742bd60
Dmitry Kochin <dco@mail.ru>
2016-06-18 14:52:44 +0300

    Красное&amp;Белое (Урал) (ab-bonus-krasnoeibeloe): - Поддержка изменений на сайте.

--- providers/ab-bonus-krasnoeibeloe/main.js
+++ providers/ab-bonus-krasnoeibeloe/main.js
@@ -12,31 +12,31 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    checkEmpty(prefs.login, 'Введите номер карты!');
+    checkEmpty(/^\d{13}$/.test(prefs.login), 'Введите 13 цифр номера карты без пробелов и разделителей!');
     
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "http://www.krasnoeibeloe.ru/";
+    var baseurl = "http://krasnoeibeloe.ru/";
 	
     var incapsule = Cloudflare(baseurl + 'discount/?old=Y');
 	var html = AnyBalance.requestGet(baseurl + 'discount/?old=Y', g_headers);
     if(incapsule.isCloudflared(html))
         html = incapsule.executeScript(html);
 	
-	var form = getParam(html, null, null, /<form action="[^"]*" method="POST"(?:[^>]*>)[\s\S]*?<\/form>/i);
-    var bxajaxid = getParam(form, null, null, /name="bxajaxid"[\s\S]*?id="([^"]+)/i);
+	var form = getElements(html, [/<form[^>]*card_reg_form[^>]*>/ig, /<input[^>]+sessid/i])[0];
     var sessid = getParam(form, null, null, /name="sessid"[\s\S]*?value="([^"]+)/i);
-	
+    	
     html = AnyBalance.requestPost(baseurl + 'discount/?old=Y', {
-        'bxajaxid': bxajaxid,
-        'AJAX_CALL': 'Y',
         'sessid': sessid,
-        'card_number': prefs.login,
+        'card_number1': '2',
+        'card_number2': prefs.login.substr(1, 6),
+        'card_number3': prefs.login.substr(7, 6),
+        'card_number': '',
         'card_submit': 'Узнать'
     }, addHeaders({Referer: baseurl + 'discount/?old=Y'}));
 	
-	if (!/Скидка по вашей карте(?:[^>]*>)[1-9][0-9]*%/i.test(html)) {
-		var error = getParam(html, null, null, /"white-ramka"([^>]*>){2}/i, replaceTagsAndSpaces, html_entity_decode);
+	if (!/По данным на/i.test(getElement(html, /<div[^>]+bl_result_nomer_text[^>]*>/i))) {
+		var error = getParam(html, null, null, /<div[^>]+bl_result_nomer_text[^>]*>([\s\S]*?)(?:<br|<\/div>)/i, replaceTagsAndSpaces);
 		if (error)
 			throw new AnyBalance.Error(error, null, /Данных по карте не найдено/i.test(error));
 		
@@ -46,9 +46,10 @@ function main(){
 
     var result = {success: true};
 	
-	getParam(html, result, 'discount', /Скидка по вашей карте(?:[^>]*>)([0-9]+%)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'sumleft', /Вам необходимо накопить еще([\s\d,.]+)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'nextdis', /До скидки([\s\d,.%]+)Вам необходимо накопить еще/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'discount', /<div[^>]+bl_result_nomer_procent[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'sumleft', /накопить еще([\s\S]*?)<\/b>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'nextdis', /До скидки([\s\d,.%]+)Вам необходимо/i, replaceTagsAndSpaces, parseBalance);
+	getParam(prefs.login, result, '__tariff', null, [/(\d)(\d{6})(\d{6})/, '$1-$2-$3']);
 
     AnyBalance.setResult(result);
 }

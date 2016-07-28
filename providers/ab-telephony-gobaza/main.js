c67b73911c333368d1c2a656ffd4c893bc9cc0b7
Ashkaari <adri.espanol@gmail.com>
2016-07-28 12:33:42 +0300

    Поддержка изменений на сайте (новый адрес) (ab-telephony-gobaza)

--- providers/ab-telephony-gobaza/main.js
+++ providers/ab-telephony-gobaza/main.js
@@ -1,73 +1,85 @@
- /**
-Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-GoBaza - виртуальная атс
-Сайт оператора: http://www.gobaza.ru
-Личный кабинет: https://go2baza.cnt.ru/cnt/
-
-*/
 
 var g_headers = {
-    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-    'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-    'Origin':'https://go2baza.cnt.ru',
-    'Referer':'https://go2baza.cnt.ru/cnt/login.jsp',
-    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172 Safari/537.22'
-}
+    'Accept':           'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Charset':   'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language':  'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'User-Agent':       'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36'
+};
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');    
 
-    var baseurl = "https://go2baza.cnt.ru/cnt/";
-    
-    var html = AnyBalance.requestPost(baseurl + 'login', {
-        message:'',
-	Name:prefs.login,
-	Password:prefs.password,
-        submit: 'Войти!'
-    }, g_headers);
+    var baseurl = "http://lk.gobaza.ru/",
+        html    = AnyBalance.requestGet(baseurl, g_headers);
 
-    var gt = getParam(html, null, null, /<form[^>]+id='goto'[^>]*action='([^']*)/i);
-    if(gt){
-        html = AnyBalance.requestPost(baseurl + gt, {
-            j_username: 'user',
-            j_password: ''
-        }, g_headers);
+    if (!html || AnyBalance.getLastStatusCode() > 400) {
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Сайт провайдера временно недоступен! Попробуйте обновить данные позже.');
     }
 
-    if(!/report\.jsp/i.test(html)){
-        var error = getParam(html, null, null, /<input[^>]+name="message"[^>]*value="([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
+    var URL = AnyBalance.getLastUrl(),
+        idt = AB.getParam(html, null, null, /<input[^>]+IDENTIFICATION[^>]+value="([^"]*)/i),
+        fnm = AB.getParam(html, null, null, /<input[^>]+FORMNAME[^>]+value="([^"]*)/i);
+
+    if(!idt || !fnm) {
+        throw new AnyBalance.Error("Не удалось найти параметры для авторизации. Сайт изменён?");
+    }
+
+    html = AnyBalance.requestPost(URL, {
+        IDENTIFICATION: idt,
+        USERNAME:       prefs.login,
+        PASSWORD:       prefs.password,
+        FORMNAME:       fnm
+    }, g_headers);
+
+
+    if(!/replace/i.test(html)){
+        var error = getParam(html, null, null, /alert\("([^"]*)/i, AB.replaceTagsAndSpaces);
+        if(error) {
+            throw new AnyBalance.Error(error, null, /Неверное имя/i.test(error));
+
+        }
+
+        AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
 
-    html = AnyBalance.requestGet(baseurl + 'report.jsp', g_headers);
-
-    var dt = new Date();
-    var today = dt.getDate() + '.' + (dt.getMonth()+1) + '.' + dt.getFullYear();
-    html = AnyBalance.requestPost(baseurl + 'report', {
-        begin_date:today,
-        qty_on_page:30,
-        offset:'',
-        end_date:today,
-        page_number:1,
-        filter_id:1,
-        make_report:'Сформировать'
-    }, g_headers);
+    var menu_url = AB.getParam(html, null, null, /<frame[^>]+name="menu"[^>]+src="([^"]*)/i),
+        data_url = AB.getParam(html, null, null, /<frame[^>]+name="data"[^>]+src="([^"]*)/i);
     
-    var result = {
-        success: true
-    };
+    var result = {success: true};
 
-    getParam(html, result, 'balance', /Остаток на счете:([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'mins_total', /Общий счетчик[\s\S]*?<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'mins_total', /В абонентскую плату включено \(минут\):([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'mins_left', /Общий счетчик(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'mins_left', /из них не использовано \(минут\):([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, '__tariff', /Здравствуйте,([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-		
+    html = AnyBalance.requestGet(baseurl + 'pls/gbaza/!w3_p_main.showform' + data_url, g_headers);
+
+    AB.getParam(html, result, 'balance',     /<td[^>]*>Текущий баланс[^>]*>([^<]*)/i,           AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'st_balance',  /<td[^>]*>Баланс на начало[^>]*>([^<]*)/i,         AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'sum_bills',   /<td[^>]*>Сумма счетов[^>]*>([^<]*)/i,             AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'sum_current', /<td[^>]*>Сумма текущих начислений[^>]*>([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'sum_paid',    /<td[^>]*>Сумма платежей[^>]*>([^<]*)/i,           AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'rec_sum',     /<td[^>]*>Рекомендуемая сумма[^>]*>([^<]*)/i,      AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'to_limit',    /<td[^>]*>Порог отключения[^>]*>([^<]*)/i,         AB.replaceTagsAndSpaces, AB.parseBalance);
+
+    AB.getParam(html, result, 'date_start', /Дата подключения[^>]*>([^<]*)/i,                            AB.replaceTagsAndSpaces, AB.parseDate);
+    AB.getParam(html, result, 'next_date',  /Начало следующего расчётного периода(?:[^>]*>){2}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseDate);
+    AB.getParam(html, result, 'address',    /Адрес(?:[^>]*>){2}([^<]*)[^>]*>([^<]*)/i,                   AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'fio',        /Клиент(?:[^>]*>){2}([^<]*)/i,                               AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'account',     /Лицевой счёт(?:[^>]*>){2}([^<]*)/i,                        AB.replaceTagsAndSpaces);
+
+    if(isAvailable(['mins_month', 'mins_left', 'mins_total', 'mins_used'])) {
+        html = AnyBalance.requestGet(baseurl + 'pls/gbaza/!w3_p_main.showform' + menu_url);
+
+        var mins_url = AB.getParam(html, null, null, /<a[^>]+click(?:[^']*'){3}([^']*)[^>]*>Остаток минут/i);
+        if(!mins_url) {
+            AnyBalance.trace("Не удалось найти ссылку на страницу с остатками минут");
+        }
+
+        html = AnyBalance.requestGet(baseurl + 'pls/gbaza/!w3_p_main.showform' + mins_url);
+        AB.getParam(html, result, 'mins_total', /Количество включённых минут по тарифу(?:[\s\S]*?<td[^>]*>){2}([^<]*)/i,        AB.replaceTagsAndSpaces, AB.parseBalance);
+        AB.getParam(html, result, 'mins_month',  /Количество включённых минут в текущем месяце(?:[\s\S]*?<td[^>]*>){3}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+        AB.getParam(html, result, 'mins_used',   /Израсходованное количество минут(?:[\s\S]*?<td[^>]*>){4}([^<]*)/i,             AB.replaceTagsAndSpaces, AB.parseBalance);
+        AB.getParam(html, result, 'mins_left',   /Остаток минут до конца месяца(?:[\s\S]*?<td[^>]*>){5}([^<]*)/i,                AB.replaceTagsAndSpaces, AB.parseBalance);
+    }
+
     AnyBalance.setResult(result);
 }

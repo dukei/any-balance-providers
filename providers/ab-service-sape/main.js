8c3009328a4564a763ae280a000b19fbab9044da
Dmitry Kochin <kochin.du@krawlly.com>
2016-08-05 13:46:40 +0300

    Sape (продвижение сайтов) (ab-service-sape): - Поддержка изменений на сайте.

--- providers/ab-service-sape/main.js
+++ providers/ab-service-sape/main.js
@@ -7,19 +7,29 @@
 Личный кабинет: https://auth.sape.ru
 */
 
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 
     var url = 'https://auth.sape.ru/login/';
 
+    html = AnyBalance.requestGet(url, g_headers);
+
     var html = AnyBalance.requestPost(url, {
-    	act: 'login',
-    	r: 'http://www.sape.ru',
+    	r: '',
         username:prefs.login,
         password:prefs.password,
+        bindip: 0,
         submit: 'Войти'
-    });
+    }, addHeaders({Referer: url}));
 
    	var form = getElement(html, /<form[^>]+id="reg"[^>]*>/i);
     if(form && /Пустая капча/i.test(form)){
@@ -48,14 +58,15 @@ function main(){
         throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
     }
 
-    html = AnyBalance.requestGet('http://widget.sape.ru/balance/?alt=json&tpl=balance_main&container_id=balance_widget_src&charset=windows-1251');
+    html = AnyBalance.requestGet('https://widget.sape.ru/widget-info/?alt=json&charset=utf-8&subSysId=2', addHeaders({Referer: 'https://www.sape.ru/'}));
 
     var json = getJson(html);
 
     var result = {success: true};
 
-    getParam(json.balanceTotal, result, 'balance', /(.*)/, replaceTagsAndSpaces, parseBalance);
-    getParam(json.balanceAvailable, result, 'available', /(.*)/, replaceTagsAndSpaces, parseBalance);
+    getParam(json.balance.total, result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+    getParam(json.balance.available, result, 'available', null, replaceTagsAndSpaces, parseBalance);
+    getParam(prefs.login, result, '__tariff');
     
     AnyBalance.setResult(result);
 }

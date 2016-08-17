fe085e290ae646921044f8489265714f54b32e91
Ashkaari <adri.espanol@gmail.com>
2016-08-17 14:24:37 +0300

    Добавлены/исправлены счётчики (ab-municipal-portal-tver-itgkh)

--- providers/ab-municipal-portal-tver-itgkh/main.js
+++ providers/ab-municipal-portal-tver-itgkh/main.js
@@ -5,14 +5,11 @@
  */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	// Mobile
-	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
-	// Desktop
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Accept': 			'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 	'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 	'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 		'keep-alive',
+	'User-Agent': 		'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
 function main() {
@@ -68,17 +65,38 @@ function main() {
 
 	var dataForm = AB.getElement(html, /\<form[^>]+class=\"\s*form-horizontal\s*\"[^>]*\>/i);
 
-	AB.getParam(dataForm, result, 'debt', /Задолженность\s+по\s+сч[ёе]ту[\s\S]*?<input[^>]*value=\"([^"]*)\"/i, AB.replaceHtmlEntities, AB.parseBalance);
-	AB.getParam(dataForm, result, 'balance', /Остаток\s+на\s+сч[ёе]те[\s\S]*?<input[^>]*value=\"([^"]*)\"/i, AB.replaceHtmlEntities, AB.parseBalance);
+	AB.getParam(dataForm, result, 'debt',    /Задолженность\s+по\s+сч[ёе]ту[\s\S]*?<input[^>]*value=\"([^"]*)\"/i, AB.replaceHtmlEntities, AB.parseBalance);
+	AB.getParam(dataForm, result, 'balance', /Остаток\s+на\s+сч[ёе]те[\s\S]*?<input[^>]*value=\"([^"]*)\"/i,	   AB.replaceHtmlEntities, AB.parseBalance);
 
-	html = AnyBalance.requestGet(baseurl + 'account/accrual', g_headers);
+	if(isAvailable(['freshPeriod', 'inSaldo', 'accruedMoney', 'paidMoney', 'out_saldo'])) {
+		html = AnyBalance.requestGet(baseurl + 'account/accrual', g_headers);
 
-	var tableData = AB.getElement(html, /<table\s+class="[^"]*data_table[^"]*"[^>]*>/i);
+		var tableData = AB.getElement(html, /<table\s+class="[^"]*data_table[^"]*"[^>]*>/i);
 
-	AB.getParam(tableData, result, 'freshPeriod', /<td[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces, AB.parseDateWord);
-	AB.getParam(tableData, result, 'inSaldo', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces, AB.parseBalance);
-	AB.getParam(tableData, result, 'accruedMoney', /<a[^>]*href="[^"]*credit[^"]*">([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
-	AB.getParam(tableData, result, 'paidMoney', /<a[^>]*href="[^"]*paid[^"]*">([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
+		var items = parseTable(tableData);
+		result['freshPeriod'] = AB.parseDateWord(items[0]);
+		result['inSaldo'] = parseBalance(items[1]);
+		result['accruedMoney'] = parseBalance(items[2]);
+		result['paidMoney'] = parseBalance(items[4]);
+		result['out_saldo'] = parseBalance(items[5]);
+	}
 
 	AnyBalance.setResult(result);
+}
+
+function parseTable(html)
+{
+	var items = [],
+		match = 1,
+		i = 0,
+		regexp = /<td>(.*)<\/td>/g;
+
+	while (match != null) {
+		i++;
+		match = regexp.exec(html);
+		if (match) items.push(replaceAll(match[1], replaceTagsAndSpaces));
+		if (i > 5) break;
+	}
+
+	return items;
 }

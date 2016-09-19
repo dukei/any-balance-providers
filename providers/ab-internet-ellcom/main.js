15b28a3ab7026ae88521b7988881666b71365917
Ashkaari <adri.espanol@gmail.com>
2016-09-19 11:25:38 +0300

    Поддержка изменений на сайте (ab-internet-ellcom)

--- providers/ab-internet-ellcom/main.js
+++ providers/ab-internet-ellcom/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://bill.ellcom.ru/';
+	var baseurl = 'https://bill.ellco.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');

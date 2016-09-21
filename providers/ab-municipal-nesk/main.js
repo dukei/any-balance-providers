e8abc6e021c12f610336543d9f0d9b8b620c08db
Ashkaari <adri.espanol@gmail.com>
2016-09-21 15:31:02 +0300

    Поддержка изменений на сайте (ab-municipal-nesk)

--- providers/ab-municipal-nesk/main.js
+++ providers/ab-municipal-nesk/main.js
@@ -3,51 +3,56 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept':          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Charset':  'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Connection': 	   'keep-alive',
+	'User-Agent': 	   'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://sis.nesk.ru/';
+	var baseurl = 'https://sis.nesk.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
-	checkEmpty(prefs.login, 'Введите логин!');
-	checkEmpty(prefs.password, 'Введите пароль!');
+	AB.checkEmpty(prefs.login, 'Введите логин!');
+	AB.checkEmpty(prefs.password, 'Введите пароль!');
 	
 	var html = AnyBalance.requestGet(baseurl, g_headers);
 	
-	if(!html || AnyBalance.getLastStatusCode() > 400)
+	if(!html || AnyBalance.getLastStatusCode() > 400) {
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
-	
+	}
+
 	html = AnyBalance.requestPost(baseurl, {
-		login: prefs.login,
-		pass: prefs.password
+		login: 	prefs.login,
+		pass: 	prefs.password
 	}, addHeaders({Referer: baseurl}));
 	
 	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="error_msg"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
+		var error = AB.getParam(html, null, null, /<div[^>]+class="error_msg"[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces);
+		if (error) {
 			throw new AnyBalance.Error(error, null, /Неверный логин и\/или пароль/i.test(error));
+		}
 		
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 	
 	var result = {success: true};
-	
-	getParam(html, result, 'balance', /Задолженность(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'account', /Номер лицевого счёта(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'counter_number', /Счётчик(?:[\s\S]*?)Номер(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'counter_install', /Дата установки(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseDate);
-	getParam(html, result, '__tariff', /Наименование(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'last_counter_date', /Последние показания принятые к расчёту(?:[\s\S]*?)Дата(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseDate);
-	getParam(html, result, 'last_counter_day', /Последние показания принятые к расчёту(?:[\s\S]*?)день(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'last_counter_night', /Последние показания принятые к расчёту(?:[\s\S]*?)ночь(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'unaccounted_payment', /Неразнесённые платежи(?:[\s\S]*?)Сумма оплаты(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);    
+
+	AB.getParam(html, result, 'balance', 		  /(?:Задолженность|Переплата)(?:[^>]*>){2}([\s\S]*?)<\//i, AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(html, result, 'peny', 	  		  /Пени(?:[^>]*>){2}([^<]*)/i, 								AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(html, result, 'account', 		  /Номер лицевого счёта(?:[^>]*>){2}([\s\S]*?)<\//i, 		AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'counter_number',   /Счётчик(?:[\s\S]*?)Номер(?:[^>]*>){2}([\s\S]*?)<\//i, 	AB.replaceTagsAndSpaces);
+	AB.getParam(html, result, 'counter_install',  /Дата установки(?:[^>]*>){2}([\s\S]*?)<\//i, 				AB.replaceTagsAndSpaces, AB.parseDate);
+	AB.getParam(html, result, '__tariff', 		  /Наименование(?:[^>]*>){2}([\s\S]*?)<\//i, 				AB.replaceTagsAndSpaces);
+
+	AB.getParam(html, result, 'last_counter_date',   /Последние показания принятые к расчёту(?:[\s\S]*?<td[^>]*>){2}([^<]*)/i, 		      AB.replaceTagsAndSpaces, AB.parseDate);
+	AB.getParam(html, result, 'last_counter_value',  /Последние показания принятые к расчёту[\s\S]*?Показания(?:[^>]*>){2}([^<]*)/i, 	  AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(html, result, 'last_counter_day',    /Последние показания принятые к расчёту(?:[\s\S]*?)день(?:[^>]*>){2}([\s\S]*?)<\//i, AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(html, result, 'last_counter_night',  /Последние показания принятые к расчёту(?:[\s\S]*?)ночь(?:[^>]*>){2}([\s\S]*?)<\//i, AB.replaceTagsAndSpaces, AB.parseBalance);
+	AB.getParam(html, result, 'unaccounted_payment', /Неразнесённые платежи(?:[\s\S]*?)Сумма оплаты(?:[^>]*>){2}([\s\S]*?)<\//i, 		  AB.replaceTagsAndSpaces, AB.parseBalance);
 	
 	AnyBalance.setResult(result);
 }

249d183c5a1a6a1f0878324d881a6dff18832f19
Ashkaari <adri.espanol@gmail.com>
2016-09-23 12:16:46 +0300

    Поддержка изменений на сайте (ab-municipal-vkcomfort)

--- providers/ab-municipal-vkcomfort/main.js
+++ providers/ab-municipal-vkcomfort/main.js
@@ -3,37 +3,45 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Accept':           'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset':   'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language':  'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection':       'keep-alive',
+	'User-Agent':       'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://room.vkcomfort.ru/';
+	var baseurl = 'https://room.vkcomfort.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	var html = AnyBalance.requestGet('http://vkcomfort.ru/%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D0%B9-%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/', g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'login/', g_headers);
 	
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 	    
-    try {
-        html = AnyBalance.requestPost(baseurl, { 
-            accountLogin: prefs.login,
-            accountPassword: prefs.password
-        }, addHeaders({Referer: 'http://vkcomfort.ru/%D0%BB%D0%B8%D1%87%D0%BD%D1%8B%D0%B9-%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82/'}));
-	} catch (e) {
+    html = AnyBalance.requestPost(baseurl + 'login/', {
+        account: prefs.login,
+        password: prefs.password
+    }, addHeaders({
+        Referer: baseurl + 'login/'
+    }));
+
+    if (!/logout/i.test(html)) {
+        var error = AB.getParam(html, null, null, /<div[^>]+alert-warning[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces);
+        if (error) {
+            throw new AnyBalance.Error(error, null, /(Ваш аккаунт в Новом Личном Кабинете не найден|Введенный логин или пароль не верны)/i.test(error));
+        }
+
+        AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }    
-	
+    }
+
 	var result = {success: true};
 	
 	var balan = getParam(html, null, null, /class="(?:positive|negative)"[^>]*>[\s\S]*?<\//i, null, null);

e1074888af6f2ac2e9795f085e8a2c05352933a2
Dmitry Kochin <kochin.du@krawlly.com>
2016-10-04 18:49:42 +0300

    ЭНЛАЙН (Москва) (ab-internet-nline): Исправлена ошибка JS

--- providers/ab-internet-nline/main.js
+++ providers/ab-internet-nline/main.js
@@ -7,11 +7,6 @@
 Личный кабинет: https://www.nline.ru/cabineti
 */
 
-function parseTrafficGb(str){
-	var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-	return parseFloat((val/1024).toFixed(2));
-}
-
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
@@ -24,7 +19,7 @@ function main(){
         password:prefs.password
     });
 
-    var number = getParam(html, null, null, /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+    var number = getParam(html, null, null, /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
     if(!number){
         //AnyBalance.trace(html);
         var error = getParam(html, null, null, /alert\s*\(\s*'([^']*)/, replaceTagsAndSpaces);
@@ -33,7 +28,7 @@ function main(){
         
         html = AnyBalance.requestGet(baseurl + 'index.cgi');
 
-        var number = getParam(html, null, null, /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+        var number = getParam(html, null, null, /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
         if(!number)
             throw new AnyBalance.Error('Не удалось войти в личный кабинет. Пустой логин-пароль, проблемы на сайте или сайт изменен.');
     }
@@ -41,10 +36,10 @@ function main(){
     var result = {success: true};
 
     getParam(html, result, 'balance', /Остаток средств на счете:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'licschet', /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'agreement', /Номер договора:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /Текущий тариф:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'traffic', /Общий трафик:[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceFloat, parseTrafficGb);
+    getParam(html, result, 'licschet', /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'agreement', /Номер договора:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, '__tariff', /Текущий тариф:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'traffic', /Общий трафик:[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
     
     AnyBalance.setResult(result);
 }

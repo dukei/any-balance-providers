a02d6d69408ca0733ff4d90616221846cab0497b
Xenia P <adri.espanol@gmail.com>
2016-10-07 18:38:56 +0300

    Добавлен новый счётчик, поддержка изменений на сайте (ab-bonus-sportmaster) (#902)

--- providers/ab-bonus-sportmaster/main.js
+++ providers/ab-bonus-sportmaster/main.js
@@ -21,20 +21,20 @@ function main(){
     var prefs = AnyBalance.getPreferences();
     var baseurl = 'https://www.sportmaster.ru/';
     AnyBalance.setDefaultCharset('utf-8');
-    
-    checkEmpty(prefs.login, 'Введите логин!');
-    
+
+    AB.checkEmpty(prefs.login, 'Введите логин!');
+
     var html = AnyBalance.requestGet(baseurl + 'user/session/login.do', g_headers);
-    
+
     if(!html || AnyBalance.getLastStatusCode() > 400){
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
     }
-    
+
     var params = { password: prefs.password };
-    
+
     if (/^[a-z0-9_\.-]+@[a-z0-9_\.-]+$/i.test(prefs.login)) {
-        checkEmpty(prefs.password, 'Введите пароль!');
+        AB.checkEmpty(prefs.password, 'Введите пароль!');
         params.option = 'email';
         params.email = prefs.login;
     } else {
@@ -53,16 +53,16 @@ function main(){
     		throw new AnyBalance.Error('В качестве логина надо ввести либо е-мейл, либо номер карты, содержащий только цифры. Убедитесь, что в логине нет лишних символов, например, пробелов.', null, true);
         }
     }
-    
+
     var html = AnyBalance.requestPost(baseurl + 'user/session/login.do?continue=%2Fcatalog%2Fproduct%2Fwelcome.do&', params, addHeaders({
         Referer: baseurl + 'user/session/login.do'
     }));
 
     //if (!/userId/i.test(html) || /<input\s[^>]*type="password"/i.test(html)) {
     if (!/<a\s[^>]*?data-bind="[^"]*?username"[^>]*>([^<]+)/i.test(html)) {
-        var error = getParam(html, null, null, /<div[^>]+class="sm-form__errors-block"[^>]*>[\s\S]*?<ul[^>]*>(.*?)<\/ul>/i, replaceTagsAndSpaces);
+        var error = AB.getParam(html, null, null, /<div[^>]+sm-form__errors-block[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces);
         if (!error)
-        	error = getParam(html, null, null, /content:\s*'<div[^>]+clr-red[^>]*>([\s\S]*?)'/i, replaceTagsAndSpaces);
+        	error = AB.getParam(html, null, null, /content:\s*'<div[^>]+clr-red[^>]*>([\s\S]*?)'/i, AB.replaceTagsAndSpaces);
         if (error) {
             var fatal = /Неверный логин или пароль|пароль устарел/i.test(error);
             if (fatal) {
@@ -78,27 +78,27 @@ function main(){
                 throw new AnyBalance.Error(error);
             }
         }
-        
+
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
 
     html = AnyBalance.requestGet(baseurl + 'user/profile/bonus.do', g_headers);
-    
+
     var result = {success: true};
-	
-	getParam(html, result, '__tariff', /smProfile__level[^>]*>([^<]*)/i, replaceTagsAndSpaces);
-	getParam(html, result, 'cardnum', /<h3>\s*Номер карты\s*<\/h3>\s*<div>\s*([^<]+)/i, replaceTagsAndSpaces);
-    getParam(html, result, 'balance', /smProfile__total-amount[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'nextlevel', /Совершите покупки еще на([^<]*?)руб/i, replaceTagsAndSpaces, parseBalance);
+
+    AB.getParam(html, result, '__tariff',  /smProfile__level[^>]*>([^<]*)/i,                 AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'cardnum',   /<h3>\s*Номер карты\s*<\/h3>\s*<div>\s*([^<]+)/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'balance',   /smProfile__total-amount[^>]*>([^<]*)/i,          AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'nextlevel', /Совершите покупки еще на([^<]*?)руб/i,           AB.replaceTagsAndSpaces, AB.parseBalance);
 	
 	if(isAvailable('all')) {
-		var table = getParam(html, null, null, /<table[^>]*sm-profile__bonustable[^>]*>(?:[\s\S](?!<\/table>))[\s\S]*?<\/table>/i);
+		var table = AB.getParam(html, null, null, /<table[^>]*sm-profile__bonustable[^>]*>(?:[\s\S](?!<\/table>))[\s\S]*?<\/table>/i);
 		if(table) {
 			var string = '';
-			var array = sumParam(table, null, null, /<tr>\s*<td[^>]*>\s*[\s\S]*?<\/tr>/ig, replaceTagsAndSpaces);
+			var array = AB.sumParam(table, null, null, /<tr>\s*<td[^>]*>\s*[\s\S]*?<\/tr>/ig, replaceTagsAndSpaces);
 			for(var i = 0; i < array.length; i++) {
-				var current = getParam(array[i], null, null, null, [/(\d{4})$/i, '$1\n', /(\d{2})-(\d{2})-(\d{4})/, '$1/$2/$3']);
+				var current = AB.getParam(array[i], null, null, null, [/(\d{4})$/i, '$1\n', /(\d{2})-(\d{2})-(\d{4})/, '$1/$2/$3']);
 				string += current;
 			}
 			getParam(string, result, 'all');
@@ -109,11 +109,11 @@ function main(){
 }
 
 function getDataByCardNum(baseurl, prefs) {
-    var html = AnyBalance.requestGet(baseurl + 'user/clubpro/state.do', g_headers);
+    var html    = AnyBalance.requestGet(baseurl + 'user/clubpro/state.do', g_headers);
     var capdata = AnyBalance.requestGet(baseurl + 'newCaptcha.do', addHeaders({'X-Requested-With': 'XMLHttpRequest'}));
     var capJson = AB.getJson(capdata);
-    var image = AnyBalance.requestGet(baseurl + 'captcha.do?captchaKey=' + capJson.captchaKey, g_headers);
-    var code = AnyBalance.retrieveCode('Введите проверочный код', image);
+    var image   = AnyBalance.requestGet(baseurl + 'captcha.do?captchaKey=' + capJson.captchaKey, g_headers);
+    var code    = AnyBalance.retrieveCode('Введите проверочный код', image);
     
     var url = baseurl + 'user/clubpro/event_check_by_cardnumber.do?cardType=' + 
                 g_cardTypes[prefs.cardType].check +
@@ -138,7 +138,8 @@ function getDataByCardNum(baseurl, prefs) {
     
     var result = {success: true};
     AB.getParam(prefs.login, result, 'cardnum');
-    AB.getParam(html, result, 'balance', /бонусов[^<]*<span[^>]*>([^<]+)/, AB.replaceTagsAndSpaces, AB.parseBalance);
-    AB.getParam(html, result, '__tariff', /<span[^>]*?bonusdetails-level[^>]*>([^<]+)/i, AB.replaceTagsAndSpaces);
+    AB.getParam(html, result, 'balance',  /бонусов[^<]*<span[^>]*>([^<]+)/,                AB.replaceTagsAndSpaces, AB.parseBalance);
+    AB.getParam(html, result, 'till',     /Дата сгорания(?:[\s\S]*?<td[^>]*>){3}([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseDate);
+    AB.getParam(html, result, '__tariff', /<span[^>]*?bonusdetails-level[^>]*>([^<]+)/i,   AB.replaceTagsAndSpaces);
     AnyBalance.setResult(result);
 }

2f6230ab6285cfe15a1c5893463f0e93b21ddc5c
Dmitry Kochin <kochin.du@krawlly.com>
2016-10-07 19:48:42 +0300

    Perfect Money (ab-money-perfectmoney): Поддержка кириллицы в пароле

--- providers/ab-money-perfectmoney/main.js
+++ providers/ab-money-perfectmoney/main.js
@@ -7,6 +7,7 @@ mailto:wtiger@mail.ru
 */
 
 function main(){
+	AnyBalance.setDefaultCharset('utf-8');
 	var prefs = AnyBalance.getPreferences();
 
 	AnyBalance.trace('Authorizing and trying to get info...');
@@ -18,20 +19,19 @@ function main(){
 		"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64)"
 	});
 
-	var error = $('#errHolder', info).text();
+	var error = getElementById(info, 'errHolder', replaceTagsAndSpaces);
 	if(error){
 		throw new AnyBalance.Error(error);
 	}
 
-
 	if(matches = info.match(/API is disabled on this account/i)){
-		throw new AnyBalance.Error("API is disabled. Try to enable API in security section of your account on www.perfectmoney.is and set IP mask to *.*.*.*. PLEASE NOTE THAT ENABLING API IS SERIOUS SECURITY RISK FOR YOUR ACCOUNT.");}
+		throw new AnyBalance.Error("API is disabled. Try to enable API in security section of your account on www.perfectmoney.is and set IP mask to *.*.*.*");}
 
 	if(matches = info.match(/API is disabled for this IP/i)){
-		throw new AnyBalance.Error("API is not accepted for your IP-address. Open the security section of your account on www.perfectmoney.is and set IP mask to *.*.*.*. PLEASE NOTE THAT ENABLING API IS SERIOUS SECURITY RISK FOR YOUR ACCOUNT.");}
+		throw new AnyBalance.Error("API is not accepted for your IP-address. Open the security section of your account on www.perfectmoney.is and set IP mask to *.*.*.*");}
 
-	if(matches = info.match(/<input name='ERROR' type='hidden' value='(.*?)'>/i)){
-		throw new AnyBalance.Error(matches[1]);}
+	if(error = getParam(info, null, null, /<input[^>]+name='ERROR'[^>]*value='([^']*)/i, replaceHtmlEntities)){
+		throw new AnyBalance.Error(error);}
 
 	if(!(matches = info.match(/Account No\./i))){
 		throw new AnyBalance.Error("Error getting data.");}
@@ -41,12 +41,12 @@ function main(){
 	AnyBalance.trace('Parsing... ');
 
 	result.__tariff=prefs.AccountID;
-	getParam(info, result, 'u_account', /<input name='U(\d+)'/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'u_balance', /<input name='U\d+' type='hidden' value='(.*?)'>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'e_account', /<input name='E(\d+)'/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'e_balance', /<input name='E\d+' type='hidden' value='(.*?)'>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'g_account', /<input name='G(\d+)'/i, replaceTagsAndSpaces, parseBalance);
-	getParam(info, result, 'g_balance', /<input name='G\d+' type='hidden' value='(.*?)'>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(info, result, 'u_account', /<input[^>]+name='U(\d+)/i, replaceHtmlEntities, parseBalance);
+	getParam(info, result, 'u_balance', /<input[^>]+name='U\d+[^>]*value='([^']*)/i, replaceHtmlEntities, parseBalance);
+	getParam(info, result, 'e_account', /<input[^>]+name='E(\d+)/i, replaceHtmlEntities, parseBalance);
+	getParam(info, result, 'e_balance', /<input[^>]+name='E\d+[^>]*value='([^']*)/i, replaceHtmlEntities, parseBalance);
+	getParam(info, result, 'g_account', /<input[^>]+name='G(\d+)/i, replaceHtmlEntities, parseBalance);
+	getParam(info, result, 'g_balance', /<input[^>]+name='G\d+[^>]*value='([^']*)/i, replaceHtmlEntities, parseBalance);
 
 	AnyBalance.setResult(result);
 };

70d5a3a85a7514a0ba1b2c6e17042b1c9609a099
Dmitry Kochin <kochin.du@krawlly.com>
2016-11-03 12:06:08 +0300

    Яндекс.Электрички (ab-transport-ya-electrichki): - Поддержка изменений на сайте.

--- providers/ab-transport-ya-electrichki/main.js
+++ providers/ab-transport-ya-electrichki/main.js
@@ -23,76 +23,102 @@ function main () {
 	
 	var html = AnyBalance.requestGet(g_baseurl, g_headers);
 	
-	var href = g_baseurl + 'search/suburban/' + "?fromName=" + 
+	var href = g_baseurl + 'search/' + "?fromName=" + 
 	encodeURIComponent(prefs.station_from) + "&toName=" + 
 	encodeURIComponent(prefs.station_to);
 	
-	if(/\d{3,}/.test(prefs.station_from))
+	if(/^\w+\d+$/.test(prefs.station_from))
 		href += '&fromId=' + prefs.station_from;
 	
-	if(/\d{3,}/.test(prefs.station_to))
+	if(/^\w+\d+$/.test(prefs.station_to))
 		href += '&toId=' + prefs.station_to;
 	
 	html = AnyBalance.requestGet(href, addHeaders({'Referer': g_baseurl}));
-	
-	// Иногда требуется уточнить место отправления или прибытия, при совпадении названий станций, например Царицино - Весенняя
-	// Оставлено для совместимости, с теми, у кого введен region в настройках
-	if(/Пожалуйста, уточните(?:[^>]*>){7}\s*<a class="b-link"/.test(html)) {
-		var precise_from = getParam(html, null, null, /<div class="l-precise__inner"(?:[^>]*>){2}Пожалуйста, уточните(?:[^>]*>){2}место отправления(?:[\s\S]*?<div class="b-precise-list__item[\s\S]*?<\/div){1,2}/i);
-		if(precise_from) {
-			AnyBalance.trace('Требуется уточнить станцию отправления...');
-			if(prefs.region)
-				html = performPrecision(precise_from, prefs);
-			else
-				throw new AnyBalance.Error('Требуется уточнить станцию отправления! Введите ID станции в настройки вместо имени!');
+	var json = getJsonObject(html, /window.INITIAL_STATE\s*=\s*/);
+	var dests = {
+		to: 'Станция назначения',
+		from: 'Станция отправления',
+		point_not_found: 'Станция не найдена. Проверьте правильность написания или выберите другой город.'
+	};
+
+	if(!json.searchForm.from.key || !json.searchForm.to.key){
+		if(json.searchForm.ambiguous && (json.searchForm.ambiguous.to || json.searchForm.ambiguous.from)){
+			var ambs = {};
+			for(var st in json.searchForm.ambiguous){
+				var arr = [];
+				for(var i=0; i<json.searchForm.ambiguous[st].length; ++i){
+					var amb = json.searchForm.ambiguous[st][i];
+					for(var j=0; j<amb.ambiguousTitle.title.length; ++j){
+						arr.push('(' + amb.ambiguousTitle.title[j].key + ') ' + amb.ambiguousTitle.title[j].name + ', ' + amb.ambiguousTitle.additionalTitle);
+					}
+				}
+				AnyBalance.trace('Для ' + dests[st] + ' найдены варианты:\n' + arr.join('\n'));
+			}
+			throw new AnyBalance.Error('Не удалось однозначно определить станцию для ' + json.searchForm[st].title + '. Укажите в настройках код станции вместо названия:\n' + arr.join('\n'), null, true);
 		}
-		var precise_to = getParam(html, null, null, /<div class="l-precise__inner"(?:[^>]*>){2}Пожалуйста, уточните(?:[^>]*>){2}место прибытия(?:[\s\S]*?<div class="b-precise-list__item[\s\S]*?<\/div){1,2}/i);
-		if(precise_to) {
-			AnyBalance.trace('Требуется уточнить станцию прибытия...');
-			if (prefs.region)
-				html = performPrecision(precise_to, prefs);
-			else
-				throw new AnyBalance.Error('Требуется уточнить станцию прибытия! Введите ID станции в настройки вместо имени!');
+		if(json.searchForm.errors && json.searchForm.errors.length){
+			AnyBalance.trace(JSON.stringify(json.searchForm.errors));
+			for(var i=0; i<json.searchForm.errors.length; ++i){
+				var error = json.searchForm.errors[i];
+				var srcs = [];
+				for(var j=0; j<error.fields.length; ++j){
+					srcs.push(dests[error.fields[j]]);
+				}
+				var strText = 'Ошибка для ' + srcs.join(', ') + ': ' + (dests[error.type] || error.type);
+				throw new AnyBalance.Error(strText, null, true);
+			}
 		}
+		if(!json.searchForm.from.key)
+			throw new AnyBalance.Error('Пункт отправления ' + json.searchForm.from.title + ' не найден. Проверьте правильность написания или выберите другой город.');
+		if(!json.searchForm.to.key)
+			throw new AnyBalance.Error('Пункт отправления ' + json.searchForm.to.title + ' не найден. Проверьте правильность написания или выберите другой город.');
+	}
+
+	if(!prefs.trains && !prefs.buses && !prefs.suburbans)
+		prefs.suburbans = true; //По-умолчанию электрички включены
+
+	var types = {
+		bus: prefs.buses,
+		suburban: prefs.suburbans,
+		train: prefs.trains
+	}
+
+	var result = {success: true};
+
+	var idx = 0;
+	for(var i=0; i<json.search.segments.length; ++i){
+		var segment = json.search.segments[i];
+		var departure = new Date(parseDateISO(segment.departure));
+		var time = n2(departure.getHours()) + ':' + n2(departure.getMinutes());
+		var name = segment.transport.title + ' ' + segment.title + ' ' + time;
+
+		if(!types[segment.transport.code]){
+			AnyBalance.trace(name + ' не подходит под фильтр');
+			continue;
+		}
+		if(segment.isGone){
+			AnyBalance.trace(name + ' уже ушёл');
+			continue;
+		}
+		
+		AnyBalance.trace(name + ' подходит (' + (idx+1) + ')');
+
+		var ftime = time;
+		if(segment.transport.code == 'bus')
+			ftime += ' (А)'; 
+		if(segment.transport.code == 'train')
+			ftime += ' (П)'; 
+
+		getParam(ftime, result, 'train' + idx++);
+
+		if(idx >= MAX_TRAINS_COUNTERS)
+			break;
 	}
 	
-    // var trainRows = getTrainTableRows(html);
-	var trainRows = getElements(html, /<div[^>]+\bSearchSegment\b[^>]*>/ig);
-    
-    if (trainRows.length < 1)
-		throw new AnyBalance.Error("Не найдена информация о поездах!");
-	
-	var numResults = Math.min(trainRows.length, MAX_TRAINS_COUNTERS);
-	
-    var result = {success: true};
-	
-    for (var t = 0; t < numResults; t++) {
-		getParam(getElement(trainRows[t], /<div[^>]+SearchSegment__time\b[^>]*>/i), result, 'train' + t, /<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
-	}
-	
-	getParam(html, result, '__tariff', /Расписание электричек ([^<]+)/i, [replaceTagsAndSpaces, /–\s*Яндекс.Расписания/i, ''], capitalFirstLetters);
-	getParam(html, result, 'start', /<input[^>]+id="from"[^>]*value="([^"]+)/i, null, capitalFirstLetters);
-	getParam(html, result, 'finish', /input[^>]+id="to"[^>]*value="([^"]+)/i, null, capitalFirstLetters);
+	getParam(json.searchForm.from.title + ' - ' + json.searchForm.to.title + ', ' + json.searchForm.when.formatted, result, '__tariff');
+	getParam(json.searchForm.from.title, result, 'start');
+	getParam(json.searchForm.to.title, result, 'finish');
 	
     AnyBalance.setResult(result);
 }
 
-function performPrecision(html, prefs) {
-	var accurate = getParam(html, null, null, new RegExp('<a class="b-link"[^>]*href="([^"]+)(?:[^>]*>){1,4}[^<]*?' + prefs.region, 'i'), replaceTagsAndSpaces);
-	checkEmpty(accurate, 'Не удалось найти таблицу с уточнением местоположений!', true);
-	return AnyBalance.requestGet(g_baseurl + accurate);
-}
-
-function isExpress(inputText) {
-	return inputText.indexOf("b-timetable__express") >= 0;
-}
-
-function getPlatformInfo(inputText) {
-	var re = /<div class="b-timetable__platform.+?<\/div>/;
-	var cell = re.exec(inputText);
-	if (cell === null) {
-		return "";
-	} else {
-		return " (" + cell[0].substring(cell[0].indexOf(">") + 1, cell[0].indexOf("</")) + ")";
-	}
-}

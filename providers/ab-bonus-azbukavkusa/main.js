dd6a0968c85d15abc36a4d554fdd76ccd5cca39f
Dmitry Kochin <kochin.du@krawlly.com>
2016-11-28 14:30:08 +0300

    Азбука Вкуса (Бонусы) (ab-bonus-azbukavkusa): - Поддержка изменений на сайте.

--- providers/ab-bonus-azbukavkusa/main.js
+++ providers/ab-bonus-azbukavkusa/main.js
@@ -10,46 +10,66 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
-var g_cards = ['АВ-OLD', 'АВ', 'АВ9', 'ВК', 'СТ', 'ЭБВГ', 'АВЭ', 'M2M', 'ДС', 'КВ', 'ЛО'];
-
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://av.ru/';
+	var baseurl = 'https://av.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите номер карты!');
 	
-	var html = AnyBalance.requestGet(baseurl + 'personal/discount_cards/', g_headers);
-	
-	var found = false;
-	for(var i = 0; i < g_cards.length; i++) {
-		html = AnyBalance.requestPost(baseurl + 'udata/avemarket/request_value_proc/', {
-			'type_card': g_cards[i],
-			'number_card': prefs.login,
-		}, addHeaders({
-			Referer: baseurl + 'personal/discount_cards/',
-			'X-Requested-With': 'XMLHttpRequest'
-		}));
-		
-		if(found = !/Карта не найдена/i.test(html))
-			break;
+	var html = AnyBalance.requestGet(baseurl + 'personal/bonus_cards/', g_headers);
+
+	if (!html || AnyBalance.getLastStatusCode() > 400) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Сайт провайдера временно недоступен! Попробуйте обновить данные позже.');
 	}
+
+	var form = AB.getElement(html, /<form[^>]+window_authorization_form[^>]*>/i);
+	if(!form){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удаётся найти форму входа! Сайт изменен?');
+	}
+
+	var params = AB.createFormParams(form, function(params, str, name, value) {
+		if (/username/i.test(name)) {
+			return prefs.login;
+		} else if (/password/i.test(name)) {
+			return prefs.password;
+		}
+
+		return value;
+	});
+
 	
-	if (!found)
-		throw new AnyBalance.Error('Не удалось получить данные по карте, сайт изменен?');
+	html = AnyBalance.requestPost(baseurl + 'azstorefront/j_spring_security_check', params, addHeaders({
+		'X-Ajax-call': 'true',
+		Referer: baseurl + 'personal/bonus_cards/',
+		'X-Requested-With': 'XMLHttpRequest'
+	}));
+
+	var json = getJson(html);
 	
 	var result = {success: true};
 	
-	getParam(prefs.login, result, '__tariff');
-	getParam(html, result, 'discount', /<udata[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-	
-	// if(isset(result.discount)) {
-		// var next = getParam(html, null, null, /для увеличения скидки на\s*(\d+)\s*%/i, replaceTagsAndSpaces, parseBalance);
-		// getParam(next + result.discount, result, 'next_discount');
-	// }
-	
-	// getParam(html, result, 'to_reach_next_discount', /необходимо совершить покупок([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	// getParam(html, result, 'last_purch_date', /Дата последней покупки:([^<]*)/i, replaceTagsAndSpaces, parseDate);
+	if (!json.success || !json.targetUrl) {
+		var error = json.error;
+		if (error) {
+			throw new AnyBalance.Error(error, null, /парол|credentials/i.test(error));
+		}
+
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+
+	html = AnyBalance.requestGet(json.targetUrl, addHeaders({Referer: baseurl + 'personal/bonus_cards/'}));
+
+	getParam(html, result, 'num', /№ карты[\s\S]*?<span[^>]*b-card-list__value[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+	getParam(html, result, '__tariff', /№ карты[\s\S]*?<span[^>]*b-card-list__value[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+
+	getParam(html, result, 'balance', /<div[^>]+b-card-list__total[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance_card', /Ваши бонусы[\s\S]*?<span[^>]*b-card-list__amount[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'phone', /Мобильный телефон[\s\S]*?<span[^>]*b-card-list__value[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+	getParam(html, result, 'status', /<span[^>]+b-card-list__status[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
 	
 	AnyBalance.setResult(result);
 }

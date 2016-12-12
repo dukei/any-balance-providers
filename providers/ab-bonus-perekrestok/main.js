026d61ee823fd449c1c8d0e971f32a38f1ce8981
Dmitry Kochin <kochin.du@krawlly.com>
2016-12-12 22:03:28 +0300

    Перекресток (ab-bonus-perekrestok): - Поддержка изменений на сайте.

--- providers/ab-bonus-perekrestok/main.js
+++ providers/ab-bonus-perekrestok/main.js
@@ -7,6 +7,14 @@
 Личный кабинет: https://prcab.x5club.ru/cwa/
 */
 
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
 var g_baseurls = {
     ordinary: 'https://prcab.x5club.ru/cwa/',
     vip: 'https://prcab.x5club.ru/green/'
@@ -17,27 +25,39 @@ function main () {
     var baseurl = g_baseurls[prefs.type] || g_baseurls.ordinary;;
 
 	if(!prefs.login)
-		throw new AnyBalance.Error ('Введите номер карты.');
+		throw new AnyBalance.Error ('Введите номер карты или номер телефона.');
 	if(!prefs.password)
 		throw new AnyBalance.Error ('Введите пароль.');
 		
     AnyBalance.trace('Входим в кабинет ' + baseurl);
-    //submitLogin (prefs);
 	
     var html = AnyBalance.requestGet (baseurl + 'anonymousLogin.do');
-	
-    html = AnyBalance.requestPost (baseurl + 'login.do', {
-        'job':        'LOGIN',
-        'parameter':  formatDate (new Date ()),
-        'pricePlan':  '',
-        'login':      prefs.login,
-        'password':   prefs.password
-    });
+
+    var card = /^\d{16}$/.test(prefs.login);
+    var form = getElement(html, /<form[^>]+LoginForm/i);
+	var params = AB.createFormParams(form, function(params, str, name, value) {
+		if (name == 'job') {
+			return 'LOGIN';
+		} else if (name == 'password') {
+			return prefs.password;
+		} else if (name == 'captchaAnswer') {
+			var image = getParam(form, /<img[^>]+captcha[^>]+src="([^"]*)/i, replaceHtmlEntities);
+			image = AnyBalance.requestGet(joinUrl(baseurl, image), addHeaders({Referer: baseurl}));
+			return AnyBalance.retrieveCode('Введите символы с изображения', image);
+		}
+
+		return value;
+	});
+
+	params.loginMethod = card ? 'card' : 'mobile';
+	params[card ? 'login' : 'mobile'] = prefs.login;
+
+    html = AnyBalance.requestPost (baseurl + 'login.do', params, addHeaders({Referer: baseurl}));
 	
     if(!/logout\.do\?menuId=TML/i.test(html)){
         var error = getParam(html, null, null, /class="errorBoldText"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
         if(error)
-            throw new AnyBalance.Error(error);
+            throw new AnyBalance.Error(error, null, /Некоррект/i.test(error));
         throw new AnyBalance.Error('Неизвестная ошибка. Пожалуйста, свяжитесь с автором провайдера.');
     }
 	
@@ -60,21 +80,6 @@ function main () {
 		}
     }
     AnyBalance.setResult (result);
-}
-
-// Не совсем понятно, для чего это, можно же просто логин передавать, не?
-function submitLogin (prefs) {
-    var login
-    login = "";
-    for (var i = 0; i < prefs.login.length; i++) {
-        var c = prefs.login.charCodeAt (i);
-        if (c <= 57 && c >= 48) {
-            login += prefs.login.charAt (i);
-        } 
-    }
-    prefs.login = login;
-	// Вроде они теперь передают пароль напрямую, оставим на всякий
-    //prefs.password = calcSHA1 (login + prefs.password);
 }
 
 function formatDate (date) {

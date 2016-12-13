aa200e7334c2248beb639cada2a257d181f0505a
Dmitry Kochin <kochin.du@krawlly.com>
2016-12-13 13:45:48 +0300

    Перекресток (ab-bonus-perekrestok): Получение данных из кабинета без капчи

--- providers/ab-bonus-perekrestok/main.js
+++ providers/ab-bonus-perekrestok/main.js
@@ -15,89 +15,52 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
-var g_baseurls = {
-    ordinary: 'https://prcab.x5club.ru/cwa/',
-    vip: 'https://prcab.x5club.ru/green/'
-}
-
 function main () {
     var prefs = AnyBalance.getPreferences ();
-    var baseurl = g_baseurls[prefs.type] || g_baseurls.ordinary;;
+    var baseurl = "https://my.perekrestok.ru/"
 
-	if(!prefs.login)
-		throw new AnyBalance.Error ('Введите номер карты или номер телефона.');
-	if(!prefs.password)
-		throw new AnyBalance.Error ('Введите пароль.');
+	checkEmpty(prefs.login, 'Введите номер карты или номер телефона.');
+	checkEmpty(prefs.password, 'Введите пароль.');
 		
     AnyBalance.trace('Входим в кабинет ' + baseurl);
-	
-    var html = AnyBalance.requestGet (baseurl + 'anonymousLogin.do');
 
     var card = /^\d{16}$/.test(prefs.login);
-    var form = getElement(html, /<form[^>]+LoginForm/i);
-	var params = AB.createFormParams(form, function(params, str, name, value) {
-		if (name == 'job') {
-			return 'LOGIN';
-		} else if (name == 'password') {
-			return prefs.password;
-		} else if (name == 'captchaAnswer') {
-			var image = getParam(form, /<img[^>]+captcha[^>]+src="([^"]*)/i, replaceHtmlEntities);
-			image = AnyBalance.requestGet(joinUrl(baseurl, image), addHeaders({Referer: baseurl}));
-			return AnyBalance.retrieveCode('Введите символы с изображения', image);
-		}
 
-		return value;
-	});
+    var html = AnyBalance.requestPost(baseurl + '/api/v1/sessions/card/establish', JSON.stringify({
+    	card_no: card ? prefs.login : undefined,
+    	phone_no: card ? undefined : prefs.login,
+    	password: prefs.password
+    }), addHeaders({
+    	'Content-Type': 'application/json;charset=UTF-8'
+    }));
 
-	params.loginMethod = card ? 'card' : 'mobile';
-	params[card ? 'login' : 'mobile'] = prefs.login;
-
-    html = AnyBalance.requestPost (baseurl + 'login.do', params, addHeaders({Referer: baseurl}));
-	
-    if(!/logout\.do\?menuId=TML/i.test(html)){
-        var error = getParam(html, null, null, /class="errorBoldText"[^>]*>([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error, null, /Некоррект/i.test(error));
-        throw new AnyBalance.Error('Неизвестная ошибка. Пожалуйста, свяжитесь с автором провайдера.');
+    var json = getJson(html);
+    if(!json.data || !json.data.totp_secret_key){
+    	var error = json.error && json.error.description;
+    	if(error)
+    		throw new AnyBalance.Error(error, null, json.error.code == 'invalid_grant');
+    	AnyBalance.trace(html);
+    	throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
-	
+    
     var result = {success: true};
-	getParam(html, result, '__tariff', /Номер карты:[^\d]*(\d+)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'balance', /Баланс:[^\d]*(\d*)/i, replaceTagsAndSpaces, parseBalance);
-	
-	if (AnyBalance.isAvailable('customer')) {
-        html = AnyBalance.requestGet (baseurl + 'accountDetails.do');
-        getParam (html, result, 'customer', /(Имя[\s\S]*?<td>([^<]*)[\s\S]*Фамилия[\s\S]*?<td>([^<]*))/i, [/Имя[\s\S]*?<td>(.)[^<]*[\s\S]*Фамилия[\s\S]*?<td>([^<]+)/, '$2 $1.']);
+
+    if(isAvailable(['customer', '__tariff'])){
+    	html = AnyBalance.requestGet(baseurl + 'api/v1/users/self', g_headers);
+    	json = getJson(html);
+
+    	getParam(json.data.user.name + ' ' + json.data.user.surname, result, 'customer');
+    	getParam(json.data.user.card_no, result, '__tariff');
     }
-    if (AnyBalance.isAvailable('burnInThisMonth')) {
-        html = AnyBalance.requestGet (baseurl + 'balanceStructure.do');
-		var table = getParam(html, null, null, /<table[^>]*id=[^>]*class="results"[\s\S]*?<\/table>/i);
-		if(table){
-			getParam(html, result, 'burnDate', /Дата списания баллов(?:[\s\S]*?<td[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, parseDate);
-			getParam(html, result, 'burnInThisMonth', /Кол-во баллов,\s*подлежащих списанию(?:[\s\S]*?<td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-		}else{
-			AnyBalance.trace ('Не найдено аннулированных баллов');
-		}
+
+    if(isAvailable(['balance', 'burnInThisMonth', 'burnDate'])){
+    	html = AnyBalance.requestGet(baseurl + 'api/v1/balances', g_headers);
+    	json = getJson(html);
+
+    	getParam(json.data.balance_list[0].balance_points, result, 'balance');
+    	getParam(json.data.expiration_info.value, result, 'burnInThisMonth');
+    	getParam(json.data.expiration_info.date*1000, result, 'burnDate');
     }
+
     AnyBalance.setResult (result);
-}
-
-function formatDate (date) {
-    var day = date.getDate();
-    if (day < 10)
-        day = '0' + day;
-
-    var month = date.getMonth () + 1;
-    if (month < 10)
-        month = '0' + month;
-
-    var hours = date.getHours ();
-    if (hours < 10)
-        hours = '0' + hours;
-
-    var minutes = date.getMinutes ();
-    if (minutes < 10)
-        minutes = '0' + minutes;
-
-    return date.getFullYear () + '-' + month + '-' + day + ' ' + hours + ':' + minutes;
 }

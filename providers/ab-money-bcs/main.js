68b822fb8969e1274f03e710eeca160ae91bd05a
Dmitry Kochin <kochin.du@krawlly.com>
2016-12-27 19:06:35 +0300

    БКС ПРЕМЬЕР (ab-money-bcs): - Поддержка изменений на сайте.

--- providers/ab-money-bcs/main.js
+++ providers/ab-money-bcs/main.js
@@ -18,25 +18,21 @@ function main() {
 	AB.checkEmpty(prefs.login, 'Введите логин!');
 	AB.checkEmpty(prefs.password, 'Введите пароль!');
 	
-	var html = AnyBalance.requestGet(baseurl + 'bank/web/guest/home', g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'cy/web/guest/home', g_headers);
 	
 	if(AnyBalance.getLastStatusCode() > 400) {
 		throw new AnyBalance.Error('Ошибка! Сервер не отвечает! Попробуйте обновить баланс позже.');
 	}
 	
 	var captchaa = '';;
-	if(/Перегрузить картинку/i.test(html)) {
-		if(AnyBalance.getLevel() >= 7) {
-			AnyBalance.trace('Пытаемся ввести капчу');
-			var captcha = AnyBalance.requestGet(baseurl+ 'bank/web/guest/home?p_p_id=LoginPortlet_WAR_bcsinternetserverportalapp&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=captcha&p_p_cacheability=cacheLevelPage&force=' + new Date().getTime());
-			captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
-			AnyBalance.trace('Капча получена: ' + captchaa);
-		} else {
-			throw new AnyBalance.Error('Провайдер требует AnyBalance API v7, пожалуйста, обновите AnyBalance!');
-		}
+	if(!/captcha-hidden/i.test(html)) {
+		AnyBalance.trace('Пытаемся ввести капчу');
+		var captcha = AnyBalance.requestGet(baseurl+ 'cy/web/guest/home?p_p_id=LoginPortlet_WAR_bcsinternetserverportalapp&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=captcha&p_p_cacheability=cacheLevelPage&force=' + new Date().getTime());
+		captchaa = AnyBalance.retrieveCode("Пожалуйста, введите код с картинки", captcha);
+		AnyBalance.trace('Капча получена: ' + captchaa);
 	}
 
-	html = AnyBalance.requestPost(baseurl + 'bank/web/guest/home?p_p_id=LoginPortlet_WAR_bcsinternetserverportalapp&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=login&p_p_cacheability=cacheLevelPage', {
+	html = AnyBalance.requestPost(baseurl + 'cy/web/guest/home?p_p_id=LoginPortlet_WAR_bcsinternetserverportalapp&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=login&p_p_cacheability=cacheLevelPage', {
 		'login':prefs.login,
 		'password':prefs.password,
 		'_LoginPortlet_WAR_bcsinternetserverportalapp_captchaText': captchaa
@@ -68,9 +64,9 @@ function main() {
 
 function processCard(baseurl, prefs, plid, result) {
 	var html = AnyBalance.requestPost(
-        baseurl + 'bank/c/portal/render_portlet?p_l_id=' + plid + '&p_p_id=AccountListCompactForm_WAR_bcsinternetserverportalapp',
+        baseurl + 'cy/c/portal/render_portlet?p_l_id=' + plid + '&p_p_id=AccountListCompactForm_WAR_bcsinternetserverportalapp',
         {},
-        AB.addHeaders({Referer: baseurl + 'bank/web/guest/home', 'X-Requested-With':'XMLHttpRequest'})
+        AB.addHeaders({Referer: baseurl + 'cy/web/guest/home', 'X-Requested-With':'XMLHttpRequest'})
     );
 
     var cardRe = new RegExp(
@@ -93,9 +89,9 @@ function processCard(baseurl, prefs, plid, result) {
 
 function processBrokerAcc(baseurl, prefs, plid, result) {
 	var html = AnyBalance.requestPost(
-        baseurl + 'bank/c/portal/render_portlet?p_l_id=' + plid + '&p_p_id=GeneralAgreementListCompactForm_WAR_bcsinternetserverportalapp',
+        baseurl + 'cy/c/portal/render_portlet?p_l_id=' + plid + '&p_p_id=GeneralAgreementListCompactForm_WAR_bcsinternetserverportalapp',
         {},
-        AB.addHeaders({Referer: baseurl + 'bank/web/guest/home', 'X-Requested-With':'XMLHttpRequest'})
+        AB.addHeaders({Referer: baseurl + 'cy/web/guest/home', 'X-Requested-With':'XMLHttpRequest'})
     );
 	
 	var acc = AB.getParam(html, null, null, new RegExp('<tr>[\\s\\S]*?(PrimeFaces.ab[^<]*' + (prefs.cardnum || '') + '(?:[^>]*>){12})','i'));

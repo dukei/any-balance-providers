502db82b2ea3b51856cefac5e77476f1ed707f7c
Dmitry Kochin <kochin.du@krawlly.com>
2016-12-29 20:21:42 +0300

    PROSTO (Украина) (ab-internet-prosto): - Поддержка изменений на сайте.

--- providers/ab-internet-prosto/main.js
+++ providers/ab-internet-prosto/main.js
@@ -11,7 +11,8 @@ var g_headers = {
 };
 
 var errors = {
-	wrongpass: 'Неверный логин или пароль. Проверьте введенные данные.'
+	wrongpass: 'Неверный логин или пароль. Проверьте введенные данные.',
+	changepass: 'Вам необходимо сменить пароль. Войдите в личный кабинет через браузер, затем введите новый пароль в настройки провайдера'
 }
 
 function main() {
@@ -28,6 +29,7 @@ function main() {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
+	var htmlInitial = html;
 	
 	html = AnyBalance.requestPost(baseurl + 'server.pl', {
 		action: 'login-check',
@@ -39,7 +41,7 @@ function main() {
 		var json = getJson(html);
 	} catch (e) {}
 	
-	if (!json || json.result != 'ok') {
+	if (!json || (json.result != 'ok' && json.result != 'redirect')) {
 		var error = errors[json.result];
 		if (error)
 			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
@@ -47,16 +49,74 @@ function main() {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
-	
-	html = AnyBalance.requestGet(baseurl + json.url, g_headers);
+
+	if(json.result == 'redirect'){
+		return fetchHome(htmlInitial);
+	}else{
+		html = AnyBalance.requestGet(baseurl + json.url, g_headers);
+	}
 	
 	var result = {success: true};
 	
 	getParam(html, result, 'balance', /На Вашем счету(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'acc_num', /Лицевой счет(?:[\s\S]*?<td>)([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, '__tariff', /Тарифный план(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'status', /Доступ(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'state', /Состояние аккаунта(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'acc_num', /Лицевой счет(?:[\s\S]*?<td>)([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+	getParam(html, result, '__tariff', /Тарифный план(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+	getParam(html, result, 'status', /Доступ(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+	getParam(html, result, 'state', /Состояние аккаунта(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
 	
 	AnyBalance.setResult(result);
+}
+
+function fetchHome(html){
+	AnyBalance.trace('Получаем данные из home');
+
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'http://home.prosto.net/';
+
+	var form = getElement(html, /<form[^>]+redirect/i);
+	var params = AB.createFormParams(form, function(params, str, name, value) {
+		if (name == 'login') {
+			return prefs.login;
+		} else if (name == 'password') {
+			return prefs.password;
+		}
+		return value;
+	});
+	var url = getParam(form, /<form[^>]+action="([^"]*)/i, replaceHtmlEntities);
+	html = AnyBalance.requestPost(url, params, g_headers);
+
+	if(!/выход/i.test(html)){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Неправильный логин или пароль', null, true);
+	}
+
+
+	html = AnyBalance.requestPost(baseurl + 'server.php', {
+		module:	'app',
+		action:	'app-get-usr',
+		id:	0,
+		hashcode: -1021167119
+	}, addHeaders({
+		Referer: baseurl,
+		'X-Requested-With': 'XMLHttpRequest'
+	}));
+
+	var json = getJson(html);
+
+	var result = {success: true};
+
+	for(var i in json.objects){
+		var data = json.objects[i];
+
+		getParam(data.balance+'', result, 'balance', null, null, parseBalance);
+		getParam(data.account_id, result, 'acc_num');
+		getParam(data.tariff_name_uk, result, '__tariff');
+		getParam(data.status, result, 'status');
+//		getParam(html, result, 'state', /Состояние аккаунта(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+		break;
+	}
+	
+	
+	AnyBalance.setResult(result);
+
 }

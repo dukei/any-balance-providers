5df238be77e88098cf7d3b75fb23a36de6c480e8
Dmitry Kochin <kochin.du@krawlly.com>
2017-01-12 12:48:49 +0300

    Квартплата Info (ab-municipal-kvartplata-info): - Поддержка изменений на сайте.

--- providers/ab-municipal-kvartplata-info/main.js
+++ providers/ab-municipal-kvartplata-info/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://gcjs.kvartplata.info/';
+	var baseurl = 'https://gcjs.kvartplata.info/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');
@@ -29,18 +29,18 @@ function main() {
 	}, addHeaders({Referer: baseurl + 'lich_kab/Home/Login'}));
 	
 	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="error-box"[^>]*>[\s\S]*?([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+		var error = getParam(html, /<div[^>]+class="error-box"[^>]*>[\s\S]*?([\s\S]*?)<\//i, replaceTagsAndSpaces);
 		if (error)
-			throw new AnyBalance.Error(error, null, /Пользователь с таким адресом электронной почты и паролем не найден|Адрес электронной почты введен некорректно/i.test(error));
+			throw new AnyBalance.Error(error, null, /парол|Адрес электронной почты/i.test(error));
 		
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
 	
-	var form = getParam(html, null, null, /<form[^>]+action="[^"]*AccountInfo"[^>]*>([\s\S]*?)<\/form>/i);
+	var form = getParam(html, /<form[^>]+action="[^"]*AccountInfo"[^>]*>([\s\S]*?)<\/form>/i);
 
 	if(prefs.digits) {
-		var accs = sumParam(html, null, null, /<option value="(\d+)/ig);
+		var accs = sumParam(html, /<option value="(\d+)/ig);
 		if(!accs || accs.length < 1) {
 			AnyBalance.trace(html);
 			AnyBalance.trace('Не удалось найти счета в кабинете. Похоже на кабинет с одним счетом');
@@ -102,7 +102,7 @@ function main() {
 	
 	getParam(html, result, 'balance', /<b>Начислено:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalanceRK);
 	getParam(html, result, 'debt', /Вы не заплатили([^<]*)/i, replaceTagsAndSpaces, parseBalanceRK);
-	getParam(html, result, '__tariff', /<b>Период:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, '__tariff', /<b>Период:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces);
 	getParam(html, result, 'fine', /<b>Пеня:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalanceRK);
 	getParam(html, result, 'payment', /<b>К оплате:(?:[^>]*>){1}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalanceRK);
 	getParam(html, result, 'prev_payment', /Оплачено ранее([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalanceRK);
@@ -112,11 +112,13 @@ function main() {
 	AnyBalance.setResult(result);
 }
 
-function parseBalanceRK(_text){
-    var text = _text.replace(/\s+/g, '');
-    var rub = getParam(text, null, null, /(-?\d[\d\.,]*)\s*р/i, replaceFloat, parseFloat) || 0;
-    var kop = getParam(text, null, null, /(-?\d[\d\.,]*)\s*к/i, replaceFloat, parseFloat) || 0;
-    var val = rub+kop/100;
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + _text);
-    return val;
+function parseBalanceRK(_text) {
+  var text = _text.replace(/\s+/g, '');
+  var rub = getParam(text, /(-?\d[\d\.,]*)р/i, replaceTagsAndSpaces, parseBalance) || 0;
+  var _sign = rub < 0 || /-\d[\d\.,]*р/i.test(text) ? -1 : 1;
+  var kop = getParam(text, /(-?\d[\d\.,]*)к/i, replaceTagsAndSpaces, parseBalance) || 0;
+  var val = _sign*(Math.abs(rub) + kop / 100);
+  AnyBalance.trace('Parsing balance (' + val + ') from: ' + _text);
+  return val;
 }
+

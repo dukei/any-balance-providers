4c7cda229708b5c9eab4071caba25d811f34515d
Dmitry Kochin <kochin.du@krawlly.com>
2017-01-30 14:17:06 +0300

    Volgaspot (ab-internet-volgaspot): - Поддержка изменений на сайте.

--- providers/ab-internet-volgaspot/main.js
+++ providers/ab-internet-volgaspot/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://volgaspot.ru/site/';
+	var baseurl = 'http://volgaspot.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');

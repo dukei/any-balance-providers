8c4f38357c465c8c31025699d3ed12c508c39ebe
lex <lexxsol@yandex.ru>
2017-03-02 20:18:48 +0400

    fix provider as per updates in LK

--- providers/ab-telenet-samara/main.js
+++ providers/ab-telenet-samara/main.js
@@ -1,23 +1,24 @@
 ﻿function main(){
-  var base_url = "http://billing.telenettv.ru/utm5/";
+  var base_url = "http://lk.telenettv.ru/webexecuter?action=GetBalance&mid=0&module=contract";
   var prefs = AnyBalance.getPreferences();
   var orderNo = prefs.orderNo;
   var password = prefs.password;
   
   var content = AnyBalance.requestPost(base_url, {
-         login:orderNo,
-         password:password
+         user:orderNo,
+         pswd:password
   });
   
   var success = false, matches, balance;
-  if(!content.match("Войти в личный кабинет")){
-    if(matches = content.match(/<table class='utm-table'[\s\S]*?<\/table>/ig)){
+  if(!content.match("ОШИБКА: Договор не найден")){
+
+    if(matches = content.match(/<table class="table tableWidth tableFont balanceList"[\s\S]*?<\/table>/ig)){
       content = matches[0];
       if(matches = content.match(/<td[\s\S]*?<\/td>/gi)){
         var balanceIndex = -1;
         for(var i = 0; i < matches.length;i++){
           var td = matches[i];
-          if(td.indexOf('>Баланс<') >=0){
+          if(td.indexOf('>Исходящий остаток на конец месяца<') >=0){
             balanceIndex = i;
             break;
           }

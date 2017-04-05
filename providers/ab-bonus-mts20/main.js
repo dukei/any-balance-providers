7c2ac2f1b3f93a7d37d6f577672f44aef26bb7f6
Dmitry Kochin <kochin.du@krawlly.com>
2017-04-05 18:39:41 +0300

    МТС -20% (Бонусы) (ab-bonus-mts20): Поддержка входа через мобильный интернет

--- providers/ab-bonus-mts20/main.js
+++ providers/ab-bonus-mts20/main.js
@@ -20,15 +20,8 @@ function main() {
 	checkEmpty(/^.{10}$/.test(prefs.login), 'Введите номер телефона (10 цифр)!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 
-	var html = AnyBalance.requestGet(baseurl, g_headers);
-
-	if(!html || AnyBalance.getLastStatusCode() > 401){
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
-	}
-
-	html = enterMTS({
-		baseurl: g_baseurl,
+	var html = enterMtsLK({
+		baseurl: g_baseurl + 'login?goto='+encodeURIComponent(g_baseurl+'#!/login'),
 		url: baseurl+'amserver/UI/Login?goto='+encodeURIComponent(g_baseurl+'#!/login'),
 		login: prefs.login,
 		password: prefs.password,
@@ -41,11 +34,12 @@ function main() {
 
 	getParam(json.fullName || '', result, 'fio');
 	getParam(json.balance ? json.balance.toFixed(2) : undefined, result, 'balance', null, null, parseBalance);
+	getParam(json.status, result, 'status');
 
 	if(isAvailable(['lastCharge', 'allAccrued', 'date'])) {
 		//Если статус аккаунта "closed", то регистрация в программе МТС "20%" невозможна.
 		//=> у user'a нет информации о бонусах и мы упадём с ошибкой при получении json'a. А это ата-та, т.к. баланс мы получили
-		if(json.status != 'closed') {
+		if(!/closed|unregistered/i.test(json.status)) {
 			html = AnyBalance.requestGet(g_baseurl+'api/user/history', g_headers);
 			json = getJson(html);
 
@@ -59,7 +53,7 @@ function main() {
 			}
 			result.allAccured = sumAllAccured;
 		} else {
-			AnyBalance.trace('Ваш номер не может быть зарегистрирован в программе бонусов "20% возвращаются"');
+			AnyBalance.trace('Ваш номер не зарегистрирован в программе бонусов "20% возвращаются". Войдите в личный кабинет https://20.ssl.mts.ru через браузер и зарегистрируйтесь в программе.');
 		}
 
 	}

ca30db0c7c9839ab9d208f0c08a25aeef9094c12
Dmitry Kochin <kochin.du@krawlly.com>
2017-04-14 15:33:38 +0300

    SMSPilot (ab-service-smspilot): - Поддержка изменений на сайте.

--- providers/ab-service-smspilot/main.js
+++ providers/ab-service-smspilot/main.js
@@ -12,22 +12,24 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://www.smspilot.ru/';
+	var baseurl = 'https://smspilot.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	var html = AnyBalance.requestPost(baseurl + 'my.php', {
-		login_email:prefs.login,
-		login_password:prefs.password
-	}, addHeaders({Referer: baseurl + 'my.php'}));
-
+	var html = AnyBalance.requestGet(baseurl + 'my.php', g_headers);
+	
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 	
+	html = AnyBalance.requestPost(baseurl + 'my.php', {
+		login_email:prefs.login,
+		login_password:prefs.password
+	}, addHeaders({Referer: baseurl + 'my.php'}));
+
 	if (!/logout/i.test(html)) {
 		var error = getParam(html, null, null, /<div[^>]+class="error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
 		if (error)
@@ -40,7 +42,7 @@ function main() {
 	var result = {success: true};
 	
 	getParam(html, result, 'balance', /<a[^>]+user_balance_str[^>]*>([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'agreement', /Договор (?:\s|<[^>]*>)+№\s*<a[^>]*>([\s\S]*?)<\/a>/i, replaceTagsAndSpaces);
+	getParam(html, result, 'agreement', /Договор №\s*<a[^>]*>([\s\S]*?)<\/a>/i, replaceTagsAndSpaces);
 
 	html = AnyBalance.requestGet(baseurl + 'my-order.php', g_headers);
 	getParam(html, result, '__tariff', /Текущий тариф\s*:([^<]*)/i, [replaceTagsAndSpaces, /\s*\|\s*$/, '']);

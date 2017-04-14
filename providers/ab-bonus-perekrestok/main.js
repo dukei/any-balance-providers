cc47451385f8a75129550bcb85b5013774fbcfc4
Dmitry Kochin <kochin.du@krawlly.com>
2017-04-14 18:46:08 +0300

    Перекресток (ab-bonus-perekrestok): - Поддержка изменений в процедуре входа

--- providers/ab-bonus-perekrestok/main.js
+++ providers/ab-bonus-perekrestok/main.js
@@ -8,10 +8,10 @@
 */
 
 var g_headers = {
-	'Accept': 'application/json, text/plain, */*',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	'Accept-Language': 'ru,en-US;q=0.8,en;q=0.6',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
 	Origin: "https://my.perekrestok.ru"
 };
 
@@ -36,38 +36,139 @@ function callApi(verb, getParams, postParams){
 		throw new AnyBalance.Error(json.error.description, null, /парол/i.test(json.error.description));
 	}
 
+	if(json.non_field_errors){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Ошибка ' + verb + ': ' + json.non_field_errors.join(', '));
+	}
+
 	return json;
 }
 
-function handshakeAndEstablish(){
-    var prefs = AnyBalance.getPreferences ();
+function passCaptcha(handshake){
+    var captcha = handshake.server.features['security/captcha'];
+    if(captcha && captcha.enabled_for_resources && /establish/i.test(JSON.stringify(captcha.enabled_for_resources))){
+    	AnyBalance.trace('Требуется рекапча, надо показывать');
+    	var requestId = String(Math.floor(Math.random() * (9999999999 - 1000000000 + 1)) + 1000000000);
+    	html = AnyBalance.requestGet(baseurl + 'api/v3/security/captcha?' + 
+    		'request_id=' + requestId + '&' +
+    		'resource_id=' + baseurl + 'api/v3/sessions/card/establish' + '&' +
+    		'X-Authorization=' + encodeURIComponent(apiHeaders['X-Authorization']
+    	), addHeaders({
+    		Referer: baseurl + 'login'
+    	}));
 
-    var card = /^\d{16}$/.test(prefs.login);
-    var json = callApi('v3/startup/handshake', null, {
+    	var sitekey = getParam(html, /data-sitekey="([^"]*)/i, replaceHtmlEntities);
+    	var csrf = getParam(html, /<input[^>]+csrfmiddlewaretoken[^>]*/i);
+    	if(!sitekey || !csrf){
+    		AnyBalance.trace(html);
+    		throw new AnyBalance.Error('Не удалось получить идентификатор капчи. Сайт изменен?');
+    	}
+    	var url = AnyBalance.getLastUrl();
+
+    	var response = solveRecaptcha('Пожалуйста, докажите, что вы не робот', url, sitekey);
+
+    	html = AnyBalance.requestPost(url, {
+    		csrfmiddlewaretoken: getParam(csrf, /value=['"]([^'"]*)/i, replaceHtmlEntities),
+    		'g-recaptcha-response': response
+    	}, addHeaders({
+    		Referer: url
+    	}));
+
+    	if(!/success/i.test(AnyBalance.getLastUrl())){
+    		AnyBalance.trace('Captcha redirected to ' + AnyBalance.getLastUrl() + ' with content:\n' + html);
+    		throw new AnyBalance.Error('Не удалось проверить капчу. Сайт изменен?');
+    	}
+
+    	var captcha_token = getParam(AnyBalance.getLastUrl(), /token=([^&]*)/i, decodeURIComponent);
+    	AnyBalance.trace('Капчу преодолели (token: ' + captcha_token + '), идем дальше');
+    	return {request_id: requestId, token: captcha_token};
+    }
+
+    return {request_id: '', token: ''};
+}
+
+function pass2f(handshake){
+    var tfa = handshake.server.features['security/2fa'];
+    if(tfa && tfa.is_enabled){
+    	var factor = tfa.factors[0];
+    	var channel = factor.channels[0];
+    	AnyBalance.trace('Требуется подтверждение входа по ' + channel.id);
+    	if(!channel || channel.id != 'sms'){
+    		AnyBalance.trace(JSON.stringify(tfa));
+    		throw new AnyBalance.Error('Требуется двухфакторная авторизация по неизвестному каналу. Сайт изменен?');
+    	}
+
+    	json = callApi('v3/users/self/phones');
+    	if(!json.primary || !json.primary.number){
+    		AnyBalance.trace(JSON.stringify(json));
+    		throw new AnyBalance.Error('Нет номера телефона для подтверждения! Сайт изменен?');
+    	}
+
+    	json = callApi('v3/security/2fa/requests', null, {
+    		channel_id: channel.id,
+    		factor_id: factor.id,
+    		phone_number_raw: json.primary.number
+    	});
+
+    	if(AnyBalance.getLastStatusCode() != 201){
+    		AnyBalance.trace(JSON.stringify(json));
+    		throw new AnyBalance.Error('Не удалось послать смс. Сайт изменен?');
+    	}
+
+    	var code = AnyBalance.retrieveCode('Пожалуйста, введите код из СМС, посланной на номер ' + json.phone_number_raw, null, {inputType: 'number', time: 120000});
+    	json = callApi('v3/security/2fa/validations', null, {
+    		channel_id: channel.id,
+    		code: code,
+    		factor_id: factor.id,
+    		phone_number: json.phone_number_raw
+    	});
+
+    	if(json.need3f){
+    		AnyBalance.trace(JSON.stringify(json));
+    		throw new AnyBalance.Error('Прошли проверку смс, но перекрестку мало. Надо ещё что-то проходить. Попробуйте ещё раз, или обратитесь к разработчикам. Сайт изменен?');
+    	}
+
+    	AnyBalance.trace('Проверка 2f пройдена');
+    }
+}
+
+function handshakeAndEstablish(){
+    var prefs = AnyBalance.getPreferences (), html, json;
+
+    var handshake = callApi('v3/startup/handshake', null, {
     	app: {
     		platform: 'web',
     		user_agent: g_headers['User-Agent'],
-    		version: 1
+    		version: 2
     	},
     	version: 1
     });
 
-    apiHeaders['X-Authorization'] = 'Bearer ' + json.server.features['security/session'].token.value;
-    AnyBalance.setCookie('my.perekrestok.ru', 'token', 'Bearer%20' + json.server.features['security/session'].token.value);
+    apiHeaders['X-Authorization'] = 'Bearer ' + handshake.server.features['security/session'].token.value;
+    AnyBalance.setCookie('my.perekrestok.ru', 'token', 'Bearer%20' + handshake.server.features['security/session'].token.value);
     AnyBalance.setCookie('my.perekrestok.ru', 'header_name', 'X-Authorization');
 
-    json = callApi('v3/sessions/card/establish', null, {
-    	card_no: card ? prefs.login : undefined,
-    	phone_no: card ? undefined : prefs.login,
-    	password: prefs.password,
-    	request_id: '',
-    	token: ''
-    });
+    var params1 = passCaptcha(handshake);
+
+    var card = /^\d{16}$/.test(prefs.login);
+    if(card){
+        json = callApi('v3/sessions/card/establish', null, joinObjects({
+        	card_no: prefs.login,
+        	password: prefs.password,
+        }, params1));
+    }else{
+        json = callApi('v3/sessions/phone/establish', null, joinObjects({
+        	phone_no: prefs.login,
+        	password: prefs.password,
+        }, params1));
+    }
 
     if(!json.data || !json.data.totp_secret_key){
     	AnyBalance.trace(JSON.stringify(json));
-    	throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+    	throw new AnyBalance.Error('Не удалось авторизоваться. Сайт изменен?');
     }
+
+    pass2f(handshake);
 }
 
 function callApiReestablish(verb){
@@ -86,6 +187,7 @@ function callApiReestablish(verb){
 
 function main () {
     var prefs = AnyBalance.getPreferences ();
+    AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Введите номер карты или номер телефона.');
 	checkEmpty(prefs.password, 'Введите пароль.');

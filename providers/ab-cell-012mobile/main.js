88df85a3b777803e489afed4dfb113b32e765db1
borislip <borislip@users.noreply.github.com>
2017-05-26 00:07:27 +0300

    I am using 012 again, the provider is long abandoned (i was likely the only person using it), but resurrecting it, making it work again, shouldn't do any harm ;-)

--- providers/ab-cell-012mobile/main.js
+++ providers/ab-cell-012mobile/main.js
@@ -12,7 +12,7 @@ var g_headers =
 // ask for the GUID, this will only work on mobile connection, on the 012 network
 function getGuid()
 {
-	var guidUrl = 'http://mystg.orange.co.il/GeneralSrv/General.svc/Authentication/GetGuid';
+	var guidUrl = 'http://192.118.8.173/GeneralSrv/General.svc/Authentication/GetGuid';
 	var guidRequest = '{"brand":"012Mobile","platform":"WEB"}';
 	var json = getJson(AnyBalance.requestPost(guidUrl,guidRequest,addHeaders(g_headers)));
 	if (typeof json.GetGuidResult=="undefined")
@@ -57,7 +57,7 @@ function getSavedOrNetworkGuid()
 // get the key, required to get the bill data, needs the GUID above
 function getKey(guid)
 {
-	var keyUrl = 'https://mystg.orange.co.il/GeneralSrv/General.svc/AuthenticationSSL/GetKey';
+	var keyUrl = 'https://192.118.8.173:8443/GeneralSrv/General.svc/AuthenticationSSL/GetKey';
 	var keyRequest = '{"guid":"' + guid + '","brand":"012Mobile","platform":"WEB"}';
 	var json = getJson(AnyBalance.requestPost(keyUrl,keyRequest,addHeaders(g_headers)));
 	if ((typeof json.GetKeyResult=="undefined") || (json.GetKeyResult==null))
@@ -72,7 +72,7 @@ function getKey(guid)
 // get bill data JSON
 function getBillData(key)
 {
-	var billUrl = 'http://my.orange.co.il/Mobile012Srv/Mobile012.svc/myAccount/GetContractBillingData';
+	var billUrl = 'http://my.partner.co.il/Mobile012Srv/Mobile012.svc/myAccount/GetContractBillingData';
 	var billRequest = '{"brand":"012Mobile","platform":"WEB","key" : "' + key + '"}';
     var json = getJson(AnyBalance.requestPost(billUrl,billRequest,addHeaders(g_headers)));
 	if (typeof json.GetContractBillingDataResult=="undefined")
@@ -87,9 +87,15 @@ function getBillData(key)
 // get data plan JSON
 function getDataPlan(key)
 {
-	var url = 'http://my.orange.co.il/ProductsSrv/SurfingPackages.svc/ContractData/GetContractCustomerDetails';
+	var url = 'http://my.partner.co.il/ProductsSrvAddOns/SurfingPackages.svc/ContractData/GetContractSurfingDetails';
 	var request = '{"brand":"012Mobile","key" : "' + key + '"}';
-	return(getJson(AnyBalance.requestPost(url,request,addHeaders(g_headers)))); 
+    var json = getJson(AnyBalance.requestPost(url,request,addHeaders(g_headers)));
+    if (typeof json.GetContractSurfingDetailsResult=="undefined")
+	{
+		AnyBalance.trace(JSON.stringify(json));
+		throw new AnyBalance.Error("Unexpected server response");
+	}
+	return(json.GetContractSurfingDetailsResult); 
 }
 
 
@@ -113,33 +119,46 @@ function main()
 	// get bill data and contract info (contract info contains the roaming plans)
 	var bill = getBillData(key);
 	AnyBalance.trace('Bill: ' + JSON.stringify(bill));
-	
+
     // get data plan details
     var dataPlan = getDataPlan(key);
     AnyBalance.trace('Data Plan: ' + JSON.stringify(dataPlan));
-    if (dataPlan.balance!=null)
+
+    // data plan addon (used first)
+    if ((dataPlan.addons!=null) && (dataPlan.addons[0]!=null))
     {
-        result['dataprcnt'] = dataPlan.balance.UsedPercent;
-        result['datausage'] = dataPlan.balance.CreditAmmountMB*dataPlan.balance.UsedPercent/100.0/1024.0;
-        result['dataplans'] = dataPlan.surfing.VasProductName;
-    } else AnyBalance.trace('Warning, no data plan reported, not updating data balance...');
+        result['dataprcnt'] = dataPlan.addons[0].balance.UsedPercent;
+        result['datausage'] = dataPlan.addons[0].balance.CreditAmmountMB*dataPlan.addons[0].balance.UsedPercent/100.0/1024.0;
+        result['dataplans'] = dataPlan.addons[0].Name;
+    } 
+
+    // regular data plan
+    if ((dataPlan.surfing!=null) && ((result['dataprcnt']==null) || (result['dataprcnt']>99.4)))
+    {
+        result['dataprcnt'] = dataPlan.surfing.balance.UsedPercent;
+        result['datausage'] = dataPlan.surfing.balance.CreditAmmountMB*dataPlan.surfing.balance.UsedPercent/100.0/1024.0;
+        result['dataplans'] = dataPlan.surfing.Name;
+    }
 
     // get roaming plans
-	for (var i=0,n=0;i<bill.International.Entity.length;i++) 
+    if (bill.International.Entity!=null)
 	{
-		var plan = bill.International.Entity[i];
-        var key = 'roamingcredit'+(++n);
-        if (plan.REMAIN_AMOUNT!=null)
-            getParam(plan.REMAIN_AMOUNT.toString(), result, key, null, null, parseBalance); 
-        else
-            result[key] = 0;
-        result[key] *= 100;
-	} 
-	
+        for (var i=0,n=0;i<bill.International.Entity.length;i++) 
+        {
+            var plan = bill.International.Entity[i];
+            var key = 'roamingcredit'+(++n);
+            if (plan.REMAIN_AMOUNT!=null)
+                getParam(plan.REMAIN_AMOUNT.toString(), result, key, null, null, parseBalance); 
+            else
+                result[key] = 0;
+            result[key] *= 100;
+        } 
+    }
+    
 	// set the rest of the results
 	getParam(bill.ContractTotalFinalBalance, result, 'price', null, null, parseBalance); 
 	getParam(bill.FullName, result, 'fullname', null, null, html_entity_decode); 
-	getParam(bill.msisdn, result, 'number', null, null, html_entity_decode); 
+	getParam(bill.Msisdn, result, 'number', null, null, html_entity_decode); 
 	getParam(bill.NextMonthCharge, result, 'nextbill', null, null, parseDate); 
 	getParam(bill.DiscountExpiration, result, 'discountend', /\d\d\.\d\d\.\d\d\d\d/, null, parseDate); 
 	getParam(bill.PlanName, result, 'plan', null, null, html_entity_decode); 

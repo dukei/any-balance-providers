9c70d1cb05945f4d4d7957fdab3adf6bcf6ddc77
Kozak Ivan <kozak-iv@yandex.ru>
2017-07-12 10:22:08 +0300

    KiteNet (РуСат) (ab-internet-kitenet)

--- providers/ab-internet-kitenet/main.js
+++ providers/ab-internet-kitenet/main.js
@@ -12,22 +12,22 @@ var g_headers = {
 
 function main() {
     var prefs = AnyBalance.getPreferences();
-    
+
     checkEmpty(prefs.login, 'Введите логин!');
     checkEmpty(prefs.password, 'Введите пароль!');
-    
-    var baseurl = "http://my.rusat.com/cgi-bin/clients/";
+
+    var baseurl = "https://my.rusat.com/cgi-bin/clients/";
     AnyBalance.setDefaultCharset('utf-8');
-    
+
     var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
-    
+
     if(!html || AnyBalance.getLastStatusCode() > 400){
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
     }
-    
+
     AnyBalance.sleep(3000);
-    
+
     html = AnyBalance.requestPost(baseurl + 'login', {
         login: prefs.login,
         password: prefs.password,
@@ -39,16 +39,29 @@ function main() {
         var error = getParam(html, null, null, /Ошибка!([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
         if (error)
             throw new AnyBalance.Error(error, null, /Неправильный логин или пароль/i.test(error));
-        
+
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
-    
+
     var result = {success: true};
-    
-    getParam(html, result, 'id', /лицевой счет([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+    var session_id = getParam(html, null, null, /session_id=([0-9a-f]{32,})/i);
+
+    getParam(html, result, 'id', /счет N ([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, 'balance', /Ваш баланс:([\s\S]*?)руб/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'payment', /Рекомендуемый платеж:([\s\S]*?)<\/tr/i, replaceTagsAndSpaces, parseBalance);
-    
+
+    if(AnyBalance.isAvailable('traffic', 'tariff', 'status')) {
+        html = AnyBalance.requestGet(baseurl + 'deal_account?session_id=' + session_id + '&action=menu_services', addHeaders({Referer:baseurl + 'login'}));
+
+        getParam(html, result, 'tariff', /<a href=".*tree_item_id.*>(.*)\(/i, replaceTagsAndSpaces, html_entity_decode)
+
+        var params = getParam(html, null, null, /<li><a href=".*(service_id.*){1}">/i, replaceTagsAndSpaces, html_entity_decode);
+
+        html = AnyBalance.requestGet(baseurl + 'deal_service?session_id=' + session_id + '&' + params, addHeaders({Referer:baseurl + 'login'}));
+
+        getParam(html, result, 'traffic', /<nobr>.*МБ/g, replaceTagsAndSpaces, parseTraffic);
+        getParam(html, result, 'status', /<td.*>(.*){1}\(автоматическое.*/i, replaceTagsAndSpaces, html_entity_decode);
+    }
+
     AnyBalance.setResult(result);
 }

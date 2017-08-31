ceb76e3588e22e6ee63398a4e2c6de5bd0f19b1a
augus-it <30490042+augus-it@users.noreply.github.com>
2017-08-31 22:19:19 +0800

    Update main.js (#944)
    Обновление нерабочей версии

--- providers/ab-service-sms-ru/main.js
+++ providers/ab-service-sms-ru/main.js
@@ -7,50 +7,31 @@
 Личный кабинет: https://sms.ru/
 */
 
-function main(){
+function main() {
 	var prefs = AnyBalance.getPreferences();
 
-	var baseurl = 'http://sms.ru/';
+	var baseurl = 'https://sms.ru/';
 
-	var strToken = AnyBalance.requestGet(baseurl + 'auth/get_token');
-
-	var strBalance = AnyBalance.requestPost(baseurl + 'my/balance', {
-		login:prefs.login,
-		password:prefs.password,
-		token:strToken,
+	var strPage = AnyBalance.requestPost(baseurl + '?panel=login&action=login', {
+		user_phone:prefs.login,
+		user_password:prefs.password,
 	});
 
-	var status = strBalance.substr(0,3);
+	var strPage = AnyBalance.requestGet(baseurl + '?panel=my');
 
-	if( status > 100){
-		throw new AnyBalance.Error('Ошибка. Что-то пошло не так');
-	}
-
-	var strLimit = AnyBalance.requestPost(baseurl + 'my/limit', {
-		login:prefs.login,
-		password:prefs.password,
-		token:strToken,
-	});
-
-	var status = strLimit.substr(0,3);
-
-	if( status > 100){
+	if( strPage.indexOf('Добро пожаловать') < 0) {
 		throw new AnyBalance.Error('Ошибка. Что-то пошло не так');
 	}
 
 	var result = {success: true};
 
-	strBalance = strBalance.split("\n");
-	result['balance'] = strBalance[strBalance.length-1]
+	result['balance'] = /<div[\s\S]+>([\d\.]+)\sруб.<\/div>/i.exec(strPage)[1];
 
-	var strOctatok = strLimit.split("\n");
-	result['octatok'] = strOctatok[strOctatok.length-2]-strOctatok[strOctatok.length-1]
+	var re = /<b>Расход лимита:<\/b><br>([\d]+)\sиз\s([\d]+)<div/i;
 
-	var strSent = strLimit.split("\n");
-	result['sent'] = strLimit[strLimit.length-1]
-
-	strLimit = strLimit.split("\n");
-	result['limit'] = strLimit[strLimit.length-2]
+	result['sent'] = re.exec(strPage)[1];
+	result['limit'] = re.exec(strPage)[2];
+	result['remain'] = result['limit'] - result['sent'];
 
 	AnyBalance.setResult(result);
 }

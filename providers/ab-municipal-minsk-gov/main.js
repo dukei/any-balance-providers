c7778831476db4c7ececcbb1b19f88bb9956d476
Xenia <adri.espanol@gmail.com>
2017-12-25 16:55:44 +0300

    Поддержка изменений на сайте (ab-municipal-minsk-gov)

--- providers/ab-municipal-minsk-gov/main.js
+++ providers/ab-municipal-minsk-gov/main.js
@@ -1,14 +1,13 @@
 ﻿
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Encoding': 'gzip, deflate',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36'
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
 };
 
 function main() {
-	var baseurl = 'http://minsk.gov.by/ru/och';
+	var baseurl = 'https://minsk.gov.by/ru/och';
 
 	var prefs = AnyBalance.getPreferences();
 

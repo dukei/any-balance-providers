1b81339c61f2c2e6617c70b22d0b1b7bf0642732
Xenia P <adri.espanol@gmail.com>
2018-01-12 20:44:17 +0300

    Поддержка изменений на сайте (ab-internet-nline) (#977)

--- providers/ab-internet-nline/main.js
+++ providers/ab-internet-nline/main.js
@@ -7,39 +7,43 @@
 Личный кабинет: https://www.nline.ru/cabineti
 */
 
+
+var g_headers = {
+    'Accept':           'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'User-Agent':       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    'Accept-Language':  'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
+};
+
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "http://nline.ru/cabineti/";
+    var baseurl = "https://billing.nline.ru/";
 
-    var html = AnyBalance.requestPost(baseurl + 'index.cgi?login', {
-        prefix:'005',
-        login:prefs.login,
-        password:prefs.password
+    var html = AnyBalance.requestPost(baseurl + 'index.cgi', {
+        user:   '005' + prefs.login,
+        passwd: prefs.password
     });
 
-    var number = getParam(html, null, null, /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
-    if(!number){
-        //AnyBalance.trace(html);
-        var error = getParam(html, null, null, /alert\s*\(\s*'([^']*)/, replaceTagsAndSpaces);
-        if(error)
+    if(!/logout/i.test(html)){
+        var error = getParam(html, null, null, /<div[^>]*?role='alert'[^>]*>(?:[\s\S]*?)<\/h4>([^<]*)</, replaceTagsAndSpaces);
+        if(error) {
             throw new AnyBalance.Error(error, null, /парол/i.test(error));
-        
-        html = AnyBalance.requestGet(baseurl + 'index.cgi');
+        }
 
-        var number = getParam(html, null, null, /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
-        if(!number)
-            throw new AnyBalance.Error('Не удалось войти в личный кабинет. Пустой логин-пароль, проблемы на сайте или сайт изменен.');
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
     }
 
     var result = {success: true};
 
-    getParam(html, result, 'balance', /Остаток средств на счете:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'licschet', /005 номер:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
-    getParam(html, result, 'agreement', /Номер договора:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
-    getParam(html, result, '__tariff', /Текущий тариф:[\S\s]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces);
-    getParam(html, result, 'traffic', /Общий трафик:[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);
+    getParam(html, result, 'balance',   /депозит(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+    getParam(html, result, 'licschet',  prefs.login);
+    getParam(html, result, 'agreement', /Договор(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+    getParam(html, result, 'fio',       /ФИО(?:[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+    getParam(html, result, '__tariff',  /<div[^>]*>Тарифный план(?:[\s\S]*?)<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+
+    /*getParam(html, result, 'traffic', /Общий трафик:[\s\S]*?<td[^>]*>([\S\s]*?)<\/td>/i, replaceTagsAndSpaces, parseTrafficGb);*/
     
     AnyBalance.setResult(result);
 }

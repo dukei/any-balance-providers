7df0d0673c415e258a642f5216403827f487e0b6
Dmitry Kochin <dco@mail.ru>
2018-01-13 13:49:56 +0300

    Cryptocurrency Exchange Rates (ab-money-crypto-exchange): Extended provider output - added price in btc, cap, rank, symbol

--- providers/ab-money-crypto-exchange/main.js
+++ providers/ab-money-crypto-exchange/main.js
@@ -31,8 +31,8 @@ function currencyId(symbol)
     }
 
     // don't have it? shit! fetch the huge list
-    AnyBalance.trace("Fidning out cryptocurrency ID..."); 
-    var url = "https://api.coinmarketcap.com/v1/ticker/";
+    AnyBalance.trace("Finding out cryptocurrency ID..."); 
+    var url = "https://api.coinmarketcap.com/v1/ticker/?limit=0";
     var html = AnyBalance.requestGet(url, g_headers);
     var json = getJson(html);
     
@@ -75,6 +75,7 @@ function main()
     AnyBalance.trace(html, "server reply"); 
     var json = getJson(html);
     var rate = json[0]["price_" + target.toLowerCase()];
+    var cap = json[0]["market_cap_" + target.toLowerCase()];
     
     // the server will still return the usual JSON, nor an error, in case of bad 
     // target currency, but the price is going to be missing
@@ -82,6 +83,14 @@ function main()
         throw new AnyBalance.Error("Unexpected target currency code.");
     
 	// return the result
-    AnyBalance.trace(rate, "exchange rate"); 
-	AnyBalance.setResult({success: true, exchangerate: rate});
+    AnyBalance.trace(rate, "exchange rate");
+    var result = {success: true};
+
+    getParam(+rate, result, 'exchangerate');
+    getParam(json[0].price_btc*1000, result, 'price_btc');
+    getParam(json[0].symbol, result, ['symbol', 'exchangerate', 'rank']);
+    getParam(+json[0].rank, result, 'rank');
+    getParam(cap && +cap, result, 'cap');
+
+	AnyBalance.setResult(result);
 }

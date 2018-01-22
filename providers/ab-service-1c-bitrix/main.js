03247abbd4f44fb3976b73755f3f129078516cd4
+ViktorSinitsyn <mousesport007@ya.ru>
2018-01-22 15:00:09 +0300

    удален coinmarketcap + правки других провайдеров (#989)
    * правки
    
    * мелкие исправления
    
    * исправлен ab-bonus-oldi
    
    * add coinmarketcap
    
    * edit group coinmarketcap
    
    * add ab-finance-coincap
    
    * удален coinmarketcap - т.к уже имеется
    добавлена новая версия 1c-bitrix и nanopool
    
    * новый провайдер pikabu.ru

--- providers/ab-service-1c-bitrix/main.js
+++ providers/ab-service-1c-bitrix/main.js
@@ -14,45 +14,45 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'http://' + prefs.site;
-	AnyBalance.trace('Проверяю наличие формы...');
-	var html = AnyBalance.requestPost(baseurl + '/bitrix/admin/', addHeaders({Referer: baseurl})); 
-	var form = getParam(html, null, null, /<div[^>]+id="otp"[^>]*>([\s\S]*?)<\/div>/i);    
-		if(!form)
-			throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
-	
-    AnyBalance.trace('Форма найдена, пытаюсь авторизироваться...');
-    html = AnyBalance.requestPost(baseurl + '/bitrix/admin/?login=yes', {
-	'AUTH_FORM':'Y',
-	'TYPE':'AUTH',
-    'USER_LOGIN': prefs.login,
-    'USER_PASSWORD': prefs.password
-    });
-	
-  if(!/bitrix\/admin\/?logout=yes/i.test(html)) {
-        var error = getParam(html, null, null, /:'([\s\S]*?)<br>'/i); 
-        if(error)
-            throw new AnyBalance.Error('Проверьте логин или пароль');
-    }
-	
-	AnyBalance.trace('Авторизация выполнена, начинаю парсить'); 
-    var result = {success: true};
-    html = AnyBalance.requestGet(baseurl + "/bitrix/admin/update_system.php?lang=ru", g_headers);
-	if (/bitrix\/admin\/update_system\.php/i.test(html)) {
-	getParam(html, result, 'podpiskaStart', /<td>с ([\s\S]*?)по/i, replaceTagsAndSpaces, parseDate); 
-	getParam(html, result, 'podpiskaEnd', /по ([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
-	} else {
-	throw new AnyBalance.Error('Не удалось найти данную страницу. Сайт изменен?');
-	}
-	
-	html = AnyBalance.requestGet(baseurl + "/bitrix/admin/bitrixcloud_monitoring_admin.php?lang=ru", g_headers);
-    if (/bitrix\/admin\/bitrixcloud_monitoring_admin\.php/i.test(html)) {
-	getParam(html, result, 'srokDomena', /<td[^>]*>Реги[^>](?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)дн/i, replaceTagsAndSpaces, parseBalance); 
-	getParam(html, result, 'dneyPodpiski', /<td[^>]*>Лице[^>](?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)дн/i, replaceTagsAndSpaces, parseBalance);
+	checkEmpty(prefs.protocol, 'Выберите протокол!');
+	checkEmpty(prefs.site, 'Введите домен!');
+		var baseurl = prefs.protocol + prefs.site;
+		AnyBalance.trace('Проверяю наличие формы...');
+		var html = AnyBalance.requestPost(baseurl + '/bitrix/admin/', addHeaders({Referer: baseurl})); 
+		var form = getParam(html, null, null, /<div[^>]+id="otp"[^>]*>([\s\S]*?)<\/div>/i);    
+			if(!form)
+				throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+		
+		AnyBalance.trace('Форма найдена, пытаюсь авторизироваться...');
+		html = AnyBalance.requestPost(baseurl + '/bitrix/admin/?login=yes', {
+		'AUTH_FORM':'Y',
+		'TYPE':'AUTH',
+		'USER_LOGIN': prefs.login,
+		'USER_PASSWORD': prefs.password
+		});
+		
+	  if(!/bitrix\/admin\/?logout=yes/i.test(html)) {
+			var error = getParam(html, null, null, /:'([\s\S]*?)<br>'/i); 
+			if(error)
+				throw new AnyBalance.Error('Проверьте логин или пароль');
+		}
+		
+		AnyBalance.trace('Авторизация выполнена, начинаю парсить'); 
+		var result = {success: true};
+		html = AnyBalance.requestGet(baseurl + "/bitrix/admin/update_system.php?lang=ru", g_headers);
+		if (/bitrix\/admin\/update_system\.php/i.test(html)) {
+		getParam(html, result, 'podpiskaStart', /<td>с ([\s\S]*?)по/i, replaceTagsAndSpaces, parseDate); 
+		getParam(html, result, 'podpiskaEnd', /по ([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+		} else {
+		throw new AnyBalance.Error('Не удалось найти данную страницу. Сайт изменен?');
+		}
+		
+		html = AnyBalance.requestGet(baseurl + "/bitrix/admin/bitrixcloud_monitoring_admin.php?lang=ru", g_headers);
+		if (/bitrix\/admin\/bitrixcloud_monitoring_admin\.php/i.test(html)) {
+		getParam(html, result, 'srokDomena', /<td[^>]*>Реги[^>](?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)дн/i, replaceTagsAndSpaces, parseBalance); 
+		getParam(html, result, 'dneyPodpiski', /<td[^>]*>Лице[^>](?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)дн/i, replaceTagsAndSpaces, parseBalance);
 
-	AnyBalance.trace('Сматываюсь');
-	html = AnyBalance.requestGet(baseurl + "/bitrix/admin/?logout=yes", g_headers);
-	}
+		}
 	
     AnyBalance.setResult(result);
 }

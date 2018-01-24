5e6c0881f98f57c0170809405e2c138c104a50c0
Dmitry Kochin <dco@mail.ru>
2018-01-24 16:59:40 +0300

    Сільпо, мережа супермаркетів (Україна) (ab-bonus-silpo): - Поддержка изменений на сайте.

--- providers/ab-bonus-silpo/main.js
+++ providers/ab-bonus-silpo/main.js
@@ -6,107 +6,155 @@
 Персональная страничка: https://my.silpo.ua/
 */
 
-function parseDateMoment(str){
-    var mom = moment(str.replace(/i/ig, 'і'), ['DD MMM YYYY', 'HH:mm-D MMM YYYY']);
-    if(!mom.isValid()){
-        AnyBalance.trace('Failed to parse date from ' + str);
-    }else{
-        var val = mom.toDate();
-        AnyBalance.trace('Parsed date ' + val + ' from ' + str);
-        return val.getTime();
+var origin = 'https://my.silpo.ua';
+var baseurl = origin + '/';
+var g_headers = {
+	Connection: "keep-alive",
+	accept: "*/*",
+	Origin: origin,
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+	Referer: baseurl,
+	"Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+}
+
+var g_verbsinfo = {
+	"loginwithcapture": `mutation loginwithcapture($barcode: String!, $password: String!, $ckey: String!) {
+  loginwithcapture(barcode: $barcode, password: $password, ckey: $ckey) {
+    accessToken {
+      ...AccessTokenFragment
+      __typename
     }
+    user {
+      ...UserFragment
+      __typename
+    }
+    __typename
+  }
 }
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
+fragment AccessTokenFragment on AccessTokenType {
+  accessToken
+  expiresAt
+  __typename
+}
 
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
+fragment UserFragment on User {
+  id
+  firstName
+  lastName
+  middleName
+  email
+  emailConfirmed
+  notification
+  barcode
+  bonusAmount
+  vouchersAmount
+  spawnNextCouponDate
+  __typename
+}`,
+	"vouchers": `query vouchers($current: Boolean) {
+  vouchers(current: $current) {
+    ...VoucherFragment
+    __typename
+  }
+}
 
-    if(param)
-      result[param] = value;
-    else
-      return value
+fragment VoucherFragment on Voucher {
+  id
+  value
+  useType
+  voucherType
+  voucherText
+  typeDescription
+  status
+  modifiedAt
+  startedAt
+  endAt
+  items {
+    name
+    type
+    value
+    __typename
+  }
+  isChangeable
+  __typename
+}`,
+}
+
+function callApi(verb, data){
+	
+	var html = requestPostMultipart('https://silpo.ua/graphql', {
+		query: g_verbsinfo[verb],
+		variables: JSON.stringify(data),
+		debugName: '""',
+		operationName: '"' + verb + '"'
+	}, addHeaders({
+		Referer: baseurl
+	}));
+
+	var json = getJson(html);
+	if(json.errors){
+		var error = json.errors.map(function(e) { return e.message }).join(';\n');
+		throw new AnyBalance.Error(error, null, /парол/i.test(error));
 	}
+	return json.data[verb];
 }
-
-var replaceTagsAndSpaces = [/<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, '', /^"+|"+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
 
 function main(){
-	var baseurl = 'https://my.silpo.ua/';
 	var prefs = AnyBalance.getPreferences();
 	AnyBalance.setDefaultCharset('utf-8');
-	moment.lang('uk');
-	var authPwd = prefs.pass;
-	var authBarcode = prefs.login;
-	if (!prefs.login || prefs.login == '')
-		throw new AnyBalance.Error ('Введите № карты');
-	if (!prefs.pass || prefs.pass == '')
-		throw new AnyBalance.Error ('Введите пароль');
+
+	checkEmpty(prefs.login, 'Введите № карты');
+	checkEmpty(prefs.pass, 'Введите пароль');
+
+	var response = solveRecaptcha('Пожалуйста, докажите, что вы не робот', baseurl, '6Ld6QjMUAAAAAO8rI-MlhpdE2s0sF-m_qNY5gMYm');
 
 	AnyBalance.trace('Logging in...');
-	var html = AnyBalance.requestPost(baseurl + 'login', {
-			authBarcode: prefs.login,
-			authPwd: prefs.pass,
-			authRememberMe: "0"
-		}
-		, 
-		{"X-Requested-With":"XMLHttpRequest"}
-	);
-	AnyBalance.trace('Got answer from authorization: "' + html + '"');
-        html = html.replace(/^\s+|\s+$/g, '');
-	if (html == "1"){ 
-		html = AnyBalance.requestGet(baseurl + 'account');
-		var result = {success: true};
-		//ФИО
-		if (matches=/<div class="user_info">\s*<strong>\s*(.*?)\s*?<br\/>\s*?(.*?)&nbsp;(.*?)\s*?<\/strong><br\/><br\/>/.exec(html)){
-		result.__tariff=matches[1]+' '+matches[2]+' '+matches[3];
-		}
-		//Накапливаемый бонус по программе Мій «Власний Рахунок»
-		if (AnyBalance.isAvailable('bonus')) {
-			var matches = html.match(/<div class=[^>]*>\s*Загальна кількість\s*<div>(\d+?)<\/div>/i);
-			if (matches) {
-				result.bonus = parseFloat(matches[1]);
-			}
-		}
-		//Мої Спеціальні пропозиції
-		if (AnyBalance.isAvailable('baly')) {
-			var matches = html.match(/Мої (?:С|c)пеціальні пропозиції п*о*н*а*д*\s*<span[^>]*>(\d+?)\s*балів[^<]*</i);
-			if (matches) {
-				result.baly = parseFloat(matches[1]);
-			}
-		}
-		//Начисленные бонусы переведённые в грн.
-		if (AnyBalance.isAvailable('skidka')) {
-			var matches = html.match(/Всього надано (?:Б|б)онусів<[^>]*><[^>]*>\s*<[^>]*><[^>]*>(\d+?.\d+?) грн.<[^>]*>/i);
-			if (matches) {
-				result.skidka = parseFloat(matches[1]);
-			}
-		}
-		//Промежуточные балы появляющиеся при переводе бонусов со счета bonus на счет skidka
-		if (AnyBalance.isAvailable('bonus_perevod')) {
-			var matches = html.match(/Увага! Ваші (?:<strong>|<b>)(\d+?) (?:Б|б)алів(?:<\/strong>|<\/b>),/i);
-			if (matches) {
-				result.bonus_perevod = parseFloat(matches[1]);
-			}
-		}
-		//Дата перерасчета бонусов
-		getParam(html, result, 'bonus_conversion', /<th[^>]*>Наступне(?:&nbsp;|\s)+перерахування(?:&nbsp;|\s)+(?:Б|б)алів(?:&nbsp;|\s)+(?:у|в)(?:&nbsp;|\s)+(?:Б|б)онус<\/th>\s*<th[^>]*>Наступна(?:&nbsp;|\s)+доставка(?:&nbsp;|\s)+(?:С|с)ертифікатів<\/th>\s*<\/tr>\s*<tr>\s*<td[^>]*>([^<]*)<\/td>/i, replaceTagsAndSpaces, parseDateMoment);
+	var json = callApi('loginwithcapture', {
+		"barcode": prefs.login,
+		"password":prefs.pass,
+		"ckey":response
+	});
 
-		AnyBalance.setResult(result);
-	} else { 
-		var error = getParam(html, null, null, /<\/script>([\s\S]*?)<br[^>]*>/i, replaceTagsAndSpaces);
-		if(!error) error = 'Не удалось получить данные, неизвестная ошибка.';
-		throw new AnyBalance.Error(error);
+	if(!json.accessToken){
+		AnyBalance.trace(JSON.stringify(json));
+		throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
 	}
+	g_headers["access-token"] = json.accessToken.accessToken;
+
+	AnyBalance.trace('Got answer from authorization: "' + JSON.stringify(json) + '"');
+	
+	var result = {success: true};
+
+	getParam(json.user.lastName + ' ' + json.user.firstName + ' ' + json.user.middleName, result, '__tariff');
+	getParam(json.user.barcode, result, 'num');
+	getParam(json.user.bonusAmount, result, 'bonus');
+
+	json = callApi('vouchers', {"current":null});
+	AnyBalance.trace('Найдено ' + json.length + ' ваучеров');
+	json = json[0];
+	
+	getParam(json.value, result, 'skidka');
+	getParam(json.modifiedAt, result, 'bonus_conversion', null, null, parseDateISO);
+
+	for(var i=0; i<json.items.length; ++i){
+		var item = json.items[i];
+		if(item.type == 'BALANCE'){
+			getParam(item.value, result, 'baly');
+		}else if(item.type == 'PURCHASES'){
+			getParam(item.value, result, 'purchases');
+		}else if(item.type == 'COUPON'){
+			getParam(item.value, result, 'coupon');
+		}else if(item.type == 'RINGOO'){
+			getParam(item.value, result, 'ringoo');
+		}else if(item.type == 'BILA_ROMASHKA'){
+			getParam(item.value, result, 'romashka');
+		}else if(item.type == 'MONEYBOX'){
+			getParam(item.value, result, 'moneybox');
+		}else{
+			AnyBalance.trace('Неизвестный ваучер: ' + JSON.stringify(item));
+		}
+	}
+
+	AnyBalance.setResult(result);
 }

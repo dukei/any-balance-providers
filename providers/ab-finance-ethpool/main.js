5f7d38599089e47857b7881d933fca3dab4d1cf6
+ViktorSinitsyn <mousesport007@ya.ru>
2018-03-10 17:38:50 +0300

    flypool - улучшен поиск воркера добавлен провайдер ethermine добавлен провайдер ethpool

--- providers/ab-finance-ethpool/main.js
+++ providers/ab-finance-ethpool/main.js
@@ -7,41 +7,41 @@
 function main() {
 	var prefs = AnyBalance.getPreferences();
 	AnyBalance.setDefaultCharset('utf-8');
-	validateZECWallet(prefs.wallet);
-	
-	var baseurl = 'https://api-zcash.flypool.org/';
-	
+	validateWallet(prefs.wallet);
+
+	var baseurl = 'https://api.ethpool.org/';
+	AnyBalance.trace('Иду на: ' + baseurl);
 	var html = AnyBalance.requestGet(baseurl + 'miner/' + prefs.wallet + '/workers', g_headers);
 	if (!html || AnyBalance.getLastStatusCode() > 400) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Возможно сайт изменился.');
 	}
-	AnyBalance.trace(html);
+	
 	var json = getJson(html);
 		if (json.status != 'OK') {
 		var error = json.error;
 		if (error)
 			throw new AnyBalance.Error(error, null, /Invalid address/i.test(error));
 		
+		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось найти данный адрес кошелька');
 	}
 	
 	var fw = json.data.find(function(el){
-			return el.worker === prefs.worker;
+		return el.worker === prefs.worker;
 	});
 	
 	var hr = fw ? fw['currentHashrate'] : null;
 	if (hr == undefined || hr == null){
-		AnyBalance.trace('Не могу найти воркера: ' + prefs.worker + ' - хешрейта для воркера не будет');
+			AnyBalance.trace('Не могу найти воркера: ' + prefs.worker + ' - хешрейта для воркера не будет');
 	}
 	var rhr = fw ? fw['reportedHashrate'] : null;
+	
 	html = AnyBalance.requestGet(baseurl + 'miner/' + prefs.wallet + '/currentStats', g_headers);
 	AnyBalance.trace(html);
 	var json = getJson(html);
 	if (json.data !=null && json.data !== 'NO DATA'){
 		var aw = json.data.activeWorkers;	
-		var up = json.data.unpaid;
-		var uc = json.data.unconfirmed;
 		var hrg = json.data.currentHashrate;
 		var rhrg = json.data.reportedHashrate;
 	}
@@ -54,31 +54,22 @@ function main() {
 	getParam(rhrg, result, 'rephashrateGeneral', null, null, parseBalance2);
 	getParam(rhr, result, 'rephashrateWorker', null, null, parseBalance2);
 	getParam(aw, result, 'activeworker');
-	getParam(up, result, 'up', null, null, parseUPandUC);
-	getParam(uc, result, 'uc', null, null, parseUPandUC);
-	
 	getParam(prefs.worker, result, '__tariff');
     AnyBalance.setResult(result);
 }
 function parseBalance2(str) {
   var val = parseBalance(str);
   if (isset(val))
-    val = Math.round(val) / 1000;
+    val = Math.round(val/100000)  / 10;
   return val;
 }
-function parseUPandUC(str) {
-  var val = parseBalance(str);
-  if (isset(val))
-    val = Math.round(val / 1000) / (10*10000);
-  return val;
-}
-function validateZECWallet(wallet) {
+function validateWallet(wallet) {
 	if (typeof(wallet)=="undefined")
-		throw new AnyBalance.Error("Введите ZEC wallet!");
-	if ((wallet.length<35) || (wallet.length>95))
-		throw new AnyBalance.Error("Неверный ZEC wallet, адрес должен быть из 35 или 95 символов!");
+		throw new AnyBalance.Error("Введите wallet!");
+	if ((wallet.length<42) || (wallet.length>42))
+		throw new AnyBalance.Error("Неверный wallet, адрес должен быть из 42 символов!");
 	if (/[^a-zA-Z0-9]/.test(wallet))
-		throw new AnyBalance.Error("Неверный ZEC wallet, адрес не должен содержать спец. символов!");
-	if ((wallet[0]!='t') && (wallet[0]!='z'))
-		throw new AnyBalance.Error("Неверный ZEC wallet, адрес должен начинаться с 't' или 'z'!");
+		throw new AnyBalance.Error("Неверный wallet, адрес не должен содержать спец. символов!");
+	if (wallet[0]!='0')
+		throw new AnyBalance.Error("Неверный wallet, адрес должен начинаться с '0x' !");
 }

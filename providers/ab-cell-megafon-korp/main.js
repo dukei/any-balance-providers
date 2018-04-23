df780d92f2e4c68f847c104d34315ec8c2a05dc2
augus-it <30490042+augus-it@users.noreply.github.com>
2018-04-23 11:43:47 +0800

    Update main.js
    Исправлена ошибка с получением номера контракта вместо номера л/с, добавлен "Условный баланс".

--- providers/ab-cell-megafon-korp/main.js
+++ providers/ab-cell-megafon-korp/main.js
@@ -258,27 +258,28 @@ function getAccount(baseurl, accnum, result){
         	AnyBalance.trace(html);
         	throw new AnyBalance.Error('Не удалось найти лицевые счета после успешного входа');
         }
-        
+
         for(var i=0; i<json.account.length; ++i){
         	acc = json.account[i];
-        	AnyBalance.trace('Найден лицевой счет ' + acc.contract);
-        	if(!accnum || acc.contract.endsWith(accnum)){
+        	AnyBalance.trace('Найден лицевой счет ' + acc.number);
+        	if(!accnum || acc.number.endsWith(accnum)){
         		getParam(acc.balance.value, result, 'balance');
+        		getParam(acc.conditionBalance.value, result, 'cond_balance');
 			    getParam(acc.subsCount, result, 'abonCount');
 			    break;
         	}
         }
-        
+
         if(i >= json.account.length)
         	throw new AnyBalance.Error('Не удалось найти лицевой счет с последними цифрами ' + accnum);
 
-        getParam(acc.contract, result, 'licschet');
+        getParam(acc.number, result, 'licschet');
     }else{
     	getParam(html, result, 'balance', /<dt[^>]*>\s*Текущий баланс[\s\S]*?class="money[^>]*>([\s\S]*?)<span/i, replaceTagsAndSpaces, parseBalance);
 	    getParam(html, result, 'abonCount', /<dt[^>]*>\s*Абонентов[\s\S]*?class="span76[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	    getParam(html, result, 'licschet', /Лицевой счет\s*(\d+)/i, replaceTagsAndSpaces);
     }
-    
+
     if (isAvailable('unpaids')) {
     	if(acc){
 	        html = AnyBalance.requestGet(baseurl + 'b2b/account/accountInfo/' + acc.id, addHeaders({

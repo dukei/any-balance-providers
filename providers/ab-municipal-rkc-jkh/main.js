fac127778c7c4e1cbede2f88805fb42cb36559b7
s7eepz <41879566+s7eepz@users.noreply.github.com>
2018-08-01 00:05:19 +0900

    Update main.js (#1026)
    Помогите с regex, не получается. Логин пароль на почте info@anybalance.ru

--- providers/ab-municipal-rkc-jkh/main.js
+++ providers/ab-municipal-rkc-jkh/main.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
@@ -12,23 +12,23 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://rkc-jkh.ru/';
+	var baseurl = 'https://rkc-jkh.ru/';
 	AnyBalance.setDefaultCharset('windows-1251');
 	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	var html = AnyBalance.requestGet(baseurl + 'login.asp', g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'lk/login.php', g_headers);
 	
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 	
-	html = AnyBalance.requestPost(baseurl + 'access.asp', {
+	html = AnyBalance.requestPost(baseurl + 'lk/dolg.php', {
 		'Search': prefs.login,
 		'secondname': prefs.password,
-	}, addHeaders({Referer: baseurl + 'login.asp'}));
+	}, addHeaders({Referer: baseurl + 'lk/login.php'}));
 	
 	if (!/Добро пожаловать/i.test(html)) {
 		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
@@ -42,17 +42,17 @@ function main() {
 	var result = {success: true};
 	
 	// Задолженность на начало месяца
-	getParam(html, result, 'balance', /Задолженность на конец месяца(?:[^>]*>){9}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-	// Плановое начисление
-	getParam(html, result, 'plan', /Задолженность на конец месяца(?:[^>]*>){11}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'balance', /Задолженность на начало месяца(?:[^>]*>){9}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	// Предварительное начисление
+	getParam(html, result, 'plan', /Предварительное начисление(?:[^>]*>){11}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	// Перерасчеты
-	getParam(html, result, 'pere', /Задолженность на конец месяца(?:[^>]*>){13}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
-	// Фактическое начисление
-	getParam(html, result, 'fakt', /Задолженность на конец месяца(?:[^>]*>){17}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'pere', /Перерасчеты(?:[^>]*>){13}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	// Начислено на тек. дату
+	getParam(html, result, 'fakt', /Начислено на тек. дату(?:[^>]*>){17}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	// Поступления
-	getParam(html, result, 'postup', /Задолженность на конец месяца(?:[^>]*>){19}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'postup', /Поступления(?:[^>]*>){19}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	// Задолженность на конец месяца без учета пеней
-	getParam(html, result, 'debt_end', /Задолженность на конец месяца(?:[^>]*>){21}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'debt_end', /Задолженность на конец месяца без учета пеней(?:[^>]*>){21}([^<]+)/i, replaceTagsAndSpaces, parseBalance);
 	
 	AnyBalance.setResult(result);
 }

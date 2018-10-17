ce34949532583b78a438ef0a39549b47eea449c2
Dmitry Kochin <dco@mail.ru>
2018-10-17 15:57:48 +0300

    МГТС (ab-telephony-mgts): Восстановлено получение информации по номеру телефона и квартиры

--- providers/ab-telephony-mgts/main.js
+++ providers/ab-telephony-mgts/main.js
@@ -16,75 +16,35 @@ function main() {
 	
 	var result = {success: true};
 	
-	if(prefs.kvart && !prefs.password) {
-		AnyBalance.setOptions({
-			SSL_ENABLED_PROTOCOLS: ['TLSv1.1', 'TLSv1.2'] //Уралсиб, типа, безопасный
-		});     
+	if(!prefs.password) {
+		var html = AnyBalance.requestGet('https://1.elecsnet.ru/NotebookFront/services/0mhp/default.aspx?merchantId=92', g_headers);
 
-		AnyBalance.trace('Получаем информацию по номеру телефона и номеру квартиры...');
-		if(!/^\d{10}$/i.test(prefs.login)) {
-			throw new AnyBalance.Error('Необходим ввести номер телефона в десятизначном формате, например 4951234567');
-		}
-
-		//Зачем мешать полезным роботам информацию получать? Зачем такие сложности? Хватит, а?
-		AnyBalance.setCookie('oplata.uralsib.ru', 'PUBLIC_WWV_CUSTOM-F_147019715977769365_30', '1519690862229901');
-		AnyBalance.setCookie('oplata.uralsib.ru', 'REMEMBER_WWV', 'PCEsFm3jv59CdRtsUG%2BpQU5l26WdRHLMTL0ZQIyqCXY%3D_510271256');
-		AnyBalance.setCookie('oplata.uralsib.ru', 'WWV_CUSTOM-F_147019715977769365_30', '2942459402C78EB6389C26BFA8CDF2FE');
-		AnyBalance.setCookie('oplata.uralsib.ru', 'TS019fd881_30', '01403d4e06305cbad863e9a01c436ddb1deafaae94737cbbb4085fa6eb9e4e74233ca61eae3c5777ec5d03e538107c27179d74175e');
-		AnyBalance.setCookie('oplata.uralsib.ru', 'TS019fd881_1', '01403d4e065396f9cd674cfdf8de018aa59731aec79c654a965916554743cf96076517a76a15771b515163cd793025eb14acaaa038');
-		AnyBalance.setCookie('oplata.uralsib.ru', 'TS019fd881', '0118f3d0af8e6671c5048e9a90c55f43615fb2490b388c1949ca9c89f56bedbc289d8112bcc33212b483a9cdbe99b8dcd7e3b38ec6c128af371739d267732a51b3c00ceae58534334c4921c5dcf22eb62af9ebfedf63dff4a2abaceebc9163037a28ede07b');
-
-		var url = 'https://oplata.uralsib.ru/f?p=30:50:1519690862229901::NO::F30_SERVICE_ID,F30_RESET_SERVICE:442,Y';
-		var html = AnyBalance.requestGet(url, g_headers);
-
-		var rsa_E = getParam(html, /window\.rsa_E\s*=\s*"([^"]*)/);
-		var rsa_N = getParam(html, /window\.rsa_N\s*=\s*"([^"]*)/);
-		var form = getElement(html, /<form[^>]+wwvFlowForm/i);
-		if(!form || !rsa_E || !rsa_N){
-			AnyBalance.trace(html);
-			throw new AnyBalance.Error('Не удалось получить форму ввода данных');
-		}
-
-		var params = createFormParams(form);
-
-		var rsa = new RSAKey();
-		rsa.setPublic(rsa_E, rsa_N);
-
-		params.PAYER_FLAT1 = prefs.kvart;
-		params.PHONE_NUMBER = prefs.login;
-
-		delete params.P60_CVV;
-		params.P60_PAN = rsa.encrypt('4111 1111 1111 1111');
-		params.P60_EXPIRE = rsa.encrypt('10/21');
-		params.P60_CVC = rsa.encrypt('123');
-
-		html = AnyBalance.requestPost('https://oplata.uralsib.ru/wwv_flow.show', {
-			p_request: 'APPLICATION_PROCESS=SAVEPAYMENT',
-			p_flow_id: params.p_flow_id,
-			p_flow_step_id: params.p_flow_step_id,
-			p_instance: params.p_instance,
-			x01: createUrlEncodedParams(params),
-			x02: '' 
-		}, addHeaders({Referer: AnyBalance.getLastUrl() }));
+		html = AnyBalance.requestPost('https://1.elecsnet.ru/NotebookFront/services/0mhp/GetMerchantInfo', {
+		    merchantId:	'92',
+			paymentTool: '9',
+			'merchantFields[1]': prefs.login,
+			'merchantFields[2]': prefs.kvart || 13,
+		}, addHeaders({Referer: AnyBalance.getLastUrl(), 'X-Requested-With': 'XMLHttpRequest' }));
 		
-		var value = getParam(html, null, null, /value="([^"]+)"[^>]*id="EXTERNAL_RESPONSE"/i, replaceHtmlEntities);
-		if(!value){
-			var error = getElement(html, /<error/i, replaceTagsAndSpaces);
+		var json = getJson(html);
+		if(!json.isSuccess){
+			var error = json.message;
+			if(/телефонному номеру невозможно/i.test(error))
+				throw new AnyBalance.Error('Телефонный номер не существует', null, true);
 			if(error)
-				throw new AnyBalance.Error(error, null, /Номер не существует/i.test(error));
+				throw new AnyBalance.Error(error);
 			AnyBalance.trace(html);
 			throw new AnyBalance.Error('Не удалось получить баланс. Cайт изменен?');
 		}
 
 		//Формат ответа: лицевой счет - Название оператора - № транзакции в системе поставщика - Баланс ЛС
-		AnyBalance.trace('Ответ от внешней системы: ' + value);
+		AnyBalance.trace('Ответ от внешней системы: ' + json.message);
 
-		if(/Баланс_не_может_быть_получен/i.test(value))
+		if(/Баланс не может быть получен/i.test(json.message))
 			throw new AnyBalance.Error('Баланс временно недоступен. Попробуйте позднее или воспользуйтесь получением баланса по логину и паролю.');
 
-		getParam(value, result, 'balance', /МГТС-\d+-(.*)$/i, [/=/, '-'], parseBalance);
+		getParam(json.message, result, 'balance', /Баланс:([^<]*)/i, null, parseBalance);
 		getParam(prefs.login, result, 'phone');
-		getParam(value, result, 'licschet', /^(\d+)/i);
 
 	} else {
 		AnyBalance.trace('Входим по логину и паролю...');
@@ -125,7 +85,7 @@ function main() {
 		var json = getJsonObject(html, /mgts.data.widgets\s*=\s*/);
 		for(var i=0; json && i<json.length; ++i){
 			if(json[i].value)
-				sumParam(json[i].value, result, '__tariff', null, null, null, aggregate_join);
+				sumParam(json[i].value, result, '__tariff', null, replaceTagsAndSpaces, null, aggregate_join);
 		}
 
 		var services = getParam(html, null, null, /mgts.data.widget\s*=\s*(\[[\s\S]*?\]);/, null, getJson);
@@ -133,7 +93,7 @@ function main() {
 			//Сложим в tariffs все опции, у которых есть value
 			var tariffs = services.reduce(function(arr, cur){
 				if(cur.value)
-					arr.push(cur.value);
+					arr.push(replaceAll(cur.value, replaceTagsAndSpaces));
 				return arr;
 			}, []);
 			getParam(tariffs.join(', '), result, '__tariff');

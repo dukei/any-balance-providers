f182a25c7549af8925b992b2324b7c47f531df87
Dmitry Kochin <dco@mail.ru>
2019-06-20 14:18:33 +0300

    МТБанк (Беларусь) (ab-money-mybank): Поддержка старых версий Android

--- providers/ab-money-mybank/main.js
+++ providers/ab-money-mybank/main.js
@@ -135,3 +135,38 @@ function main() {
 	
 	AnyBalance.setResult(result);
 }
+
+if (!Array.prototype.filter) {
+  Array.prototype.filter = function(fun/*, thisArg*/) {
+    'use strict';
+
+    if (this === void 0 || this === null) {
+      throw new TypeError();
+    }
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i];
+
+        // ПРИМЕЧАНИЕ: Технически, здесь должен быть Object.defineProperty на
+        //             следующий индекс, поскольку push может зависеть от
+        //             свойств на Object.prototype и Array.prototype.
+        //             Но этот метод новый и коллизии должны быть редкими,
+        //             так что используем более совместимую альтернативу.
+        if (fun.call(thisArg, val, i, t)) {
+          res.push(val);
+        }
+      }
+    }
+
+    return res;
+  };
+}

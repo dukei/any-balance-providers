00b8509951065923ac49ba55eaf2c50a0f242a58
Dmitry Kochin <dco@mail.ru>
2019-07-02 12:32:24 +0300

    Семейная команда (Бонусы) (ab-bonus-rosneft): - Поддержка изменений на сайте.

--- providers/ab-bonus-rosneft/main.js
+++ providers/ab-bonus-rosneft/main.js
@@ -5,14 +5,13 @@
 var baseurl = 'https://komandacard.ru/';
 
 var g_headers = {
-	'Accept': 'application/json; charset=utf-8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept': '*/*',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
 	'X-Requested-With': 'XMLHttpRequest',
 	'Origin': baseurl.replace(/\/+$/, ''),
 	'Referer': baseurl,
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
 };
 
 function main() {
@@ -23,29 +22,53 @@ function main() {
 	checkEmpty(/^\d{10}$/.test(prefs.login), 'Введите 10 цифр номера телефона в формате 9261234567 без пробелов и разделителей!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 
-	var html = AnyBalance.requestGet(baseurl + 'account/login', g_headers);
+	var html = AnyBalance.requestGet(baseurl + 'location/getcity', g_headers);
 	var json = getJson(html);
 
-	var rvt = getParam(json.html, /<input[^>]+__RequestVerificationToken[^>]*value="([^"]*)/i, replaceHtmlEntities);
+	AnyBalance.setCookie('komandacard.ru', 'regionId', json.regionId);
+
+	var html = AnyBalance.requestGet(baseurl + 'account/login', g_headers);
+	var rvt = getParam(html, /<input[^>]+__RequestVerificationToken[^>]*value="([^"]*)/i, replaceHtmlEntities);
 
 	var captcha = solveRecaptcha('Пожалуйста, докажите, что вы не робот', baseurl + 'login', "6LejgioUAAAAAK6ZVEm_o8YhLHpnBil1J-hrPQnB");
 
 	html = AnyBalance.requestPost(baseurl + 'account/login', JSON.stringify({
-    	"__RequestVerificationToken": rvt,
-    	"Captcha": captcha,
-    	"Password": prefs.password,
-    	"Phone": '+7 ' + prefs.login,
+		Data: [
+			{
+				name: 'Phone',
+				value: '+7 ' + prefs.login,
+			},
+			{
+				name: 'Password',
+				value: prefs.password,
+			},
+			{
+				name: 'g-recaptcha-response',
+				value: captcha,
+			},
+			{
+				name: 'Captcha',
+				value: captcha,
+			},
+		],
+		GoForward: true
 	}), addHeaders({
 		RequestVerificationToken: rvt,
 		'Content-Type': 'application/json'
 	}));
 
-	json = getJson(html);
+	json = {error: 'Не удалось войти в личный кабинет. Неверный логин и пароль или сайт изменен'};
+	try{
+		json = getJson(html);
+	}catch(e){
+		AnyBalance.trace(html);
+		AnyBalance.trace(e.message);
+	}
 	
-	if(!json.params || json.params.result != 0){
-		var error = getElement(json.html, /<div[^>]+validation-summary-errors/i, replaceTagsAndSpaces);
+	if(!json.isFinished){
+		var error = json.error;
 		if(error)
-			throw new AnyBalance.Error(error, null, /парол/i.test(error));
+			throw new AnyBalance.Error(error);//, null, /парол/i.test(error));
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
 	}
@@ -58,6 +81,10 @@ function main() {
 	getParam(html, result, 'available', /Баллов доступно:[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
 
 	if(AnyBalance.isAvailable('last_sum', 'last_date', 'last_bonus', 'last_place', 'last_status')){
+		var dt = new Date();
+		html = AnyBalance.requestGet(baseurl + 'report/01.01.1900/' + n2(dt.getDate()) + '.' + n2(dt.getMonth()+1) + '.' + dt.getFullYear(),
+			addHeaders({Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}));
+
 		var row = getElement(html, /<div[^>]+block-statement__content/i);
 		row = row && getElement(row, /<div[^>]+block-table__body/i);
 		row = row && getElement(row, /<div[^>]+"block-table__row/i);
@@ -69,4 +96,11 @@ function main() {
 	}   
 	
 	AnyBalance.setResult(result);
+}
+
+function n2(str){
+	str = '' + str;
+	if(str.length < 2)
+		str = '0' + str;
+	return str;
 }

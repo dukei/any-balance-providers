d9971fa13eb78b9ca574e5315c2a535dbab8d104
Alexander Listratenkov <a.listratenkov@gmail.com>
2019-09-26 21:42:16 +0300

    Update main.js
    change url request

--- providers/ab-weather-montemp/main.js
+++ providers/ab-weather-montemp/main.js
@@ -1,8 +1,8 @@
-﻿/**
+/**
 
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 
-Получает текущую температуру воздуха с сайта https://www.flags8192.ru/. Для измерения температуры используется температурный 
+Получает текущую температуру воздуха с сайта https://listratenkov.com/. Для измерения температуры используется температурный 
 датчик LM75AD под управлением UsbTenkiMux (http://www.schmut.com/other-stuff/usbtenki-mux), расположенный в 
 п. Монастырщина Смоленской области и защищенный от воздействия внешних факторов, что позволяет довольно точно 
 определять температуру воздуха. 
@@ -13,8 +13,8 @@ function main(){
 	AnyBalance.setDefaultCharset('UTF-8');
 	
 	var result = {success: true};
-	AnyBalance.trace("Loading https://www.flags8192.ru/temps/temp22");
-	var html = AnyBalance.requestGet('https://www.flags8192.ru/temps/temp22');
+	AnyBalance.trace("Loading https://listratenkov.com/temps/temp22");
+	var html = AnyBalance.requestGet('https://listratenkov.com/temps/temp22');
 	
 	AnyBalance.trace("Parsing current temperature");
 	var regexp = /\S+/;

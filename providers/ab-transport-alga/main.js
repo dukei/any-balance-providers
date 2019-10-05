6d992d63ead486a8675a086e300c8608436ea762
Ainur <anurique@gmail.com>
2019-10-05 18:13:42 +0500

    парсинг параметров url, вместо парсинга кода страницы

--- providers/ab-transport-alga/main.js
+++ providers/ab-transport-alga/main.js
@@ -1,17 +1,33 @@
 function main(){
 	AnyBalance.trace('main started', null);
-	var n = '9643 10020 33118 64240';
-	var data ={cardnumber: n};
-	var info = AnyBalance.requestPost('https://pay.brsc.ru/Alga.pay/GoldenCrownSite.php', data);
-	var balance = -1;
-	if (matches = info.match(/<p>(.*?)руб.<\/p>/)){
-		AnyBalance.trace('found balance');
-		AnyBalance.trace(matches[1]);
-		balance = Number(matches[1]);
+	var prefs = AnyBalance.getPreferences();
+	var cardnumber = prefs.cardnumber;
+	AnyBalance.trace(cardnumber, null);
+	
+	var info = AnyBalance.requestPost('https://pay.brsc.ru/Alga.pay/GoldenCrownSite.php', {cardnumber: cardnumber});
+	
+	var result = {success:true};
+
+	lu = AnyBalance.getLastUrl(); //нужная инфа в параметрах url после перенаправления
+	
+	if (matches = lu.match(/allow=(.*)&/)){
+		var allow = matches[1];
+		if (allow === 'yes'){
+			if (bmatches = lu.match(/sum=(.*)/)){
+				result.balance = Number(bmatches[1]);
+			}
+		}else if (allow === 'no'){
+			result = {error: true, fatal: true};
+			if (mmatches = lu.match(/message=(.*)/)){
+				result.message = decodeURI(mmatches[1]).replace(/\+/g, ' ');
+			}
+		}else{
+			result = {error: true, message: 'unknown error'};
+		}
+		
 	}else{
-		AnyBalance.trace('nothing found');
-		AnyBalance.trace(matches);
+		result = {error: true, message: 'unknown error'};
 	}
-	AnyBalance.trace(info, null);
-	AnyBalance.setResult({success: true, balance: balance});
+
+	AnyBalance.setResult(result);
 }

7fcb948b1fd7b3bff8afaf30f5028a983df1bcb9
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2020-12-05 22:54:42 +0200

    Поддержка изменений на сайте

--- providers/ab-exchange-nbu/main.js
+++ providers/ab-exchange-nbu/main.js
@@ -3,7 +3,7 @@
 */
 
 function getRateDate(result, info) {
-	var matches, regexp = /<td[^>]+class="date"[^>]*>([\s\S]*?)<\/td>/i;
+	var matches, regexp = /span id=\"exchangeDate\"\>([\d\.]*?)<\/span>/i;
 	if (matches = info.match(regexp)) {
 		if (AnyBalance.isAvailable('date')) {
 			result.date = matches[1];
@@ -15,8 +15,8 @@ function getRateDate(result, info) {
 function getRate(result, info, namein) {
 	if (AnyBalance.isAvailable(namein)) {
 		var prefs = AnyBalance.getPreferences();
-		var matches, regexpValue = new RegExp('<tr[^>]*>\\s*<td[^>]*>[^<]*</td>\\s*<td[^>]*>' + namein + '(?:[\\s\\S]*?<td[^>]*>){3}([^<]*)', 'i'),
-			regexpMul = new RegExp('<tr[^>]*>\\s*<td[^>]*>[^<]*</td>\\s*<td[^>]*>' + namein + '(?:[\\s\\S]*?<td[^>]*>){1}([^<]*)', 'i');
+		var matches, regexpValue = new RegExp('Код літерний[\\s\\S]*?' + namein + '[\\s\\S]*?Офіційний курс\\"\\>([\\d\\.\\,]*)<\\/td>', 'i'),
+			regexpMul = new RegExp('Код літерний[\\s\\S]*?' + namein + '[\\s\\S]*?Кількість одиниць валюти\\"\\>([\\d]*)<\\/td>', 'i');
 		if (matches = info.match(regexpValue)) {
 			var val = parseFloat(matches[1].replace(',', '.'));
 			if (prefs.normalize) {
@@ -55,37 +55,45 @@ var shorts = {
 function main() {
 	if (AnyBalance.getLevel() < 5) return "Для этого провайдера необходимо AnyBalance API v.5+";
 	AnyBalance.trace('Connecting to nbu...');
-	var info = AnyBalance.requestGet('http://bank.gov.ua/control/uk/curmetal/detail/currency?period=daily');
+	var info = AnyBalance.requestGet('https://bank.gov.ua/ua/markets/exchangerates?period=daily');
 	
 	var result = {success: true};
 	
-	getRate(result, info, 'AUD');
-	getRate(result, info, 'AZM');
-	getRate(result, info, 'GBP');
-	getRate(result, info, 'BYR');
-	getRate(result, info, 'DKK');
 	getRate(result, info, 'USD');
 	getRate(result, info, 'EUR');
-	getRate(result, info, 'ISK');
-	getRate(result, info, 'KZT');
-	getRate(result, info, 'CAD');
-	getRate(result, info, 'LVL');
-	getRate(result, info, 'LTL');
-	getRate(result, info, 'MDL');
-	getRate(result, info, 'NOK');
-	getRate(result, info, 'PLN');
+	getRate(result, info, 'GBP');
 	getRate(result, info, 'RUB');
-	getRate(result, info, 'SGD');
-	getRate(result, info, 'XDR');
-	getRate(result, info, 'TRL');
-	getRate(result, info, 'TMM');
-	getRate(result, info, 'HUF');
-	getRate(result, info, 'UZS');
-	getRate(result, info, 'CZK');
-	getRate(result, info, 'SEK');
+	getRate(result, info, 'BYN');
+	getRate(result, info, 'KZT');
 	getRate(result, info, 'CHF');
 	getRate(result, info, 'CNY');
 	getRate(result, info, 'JPY');
+	getRate(result, info, 'AUD');
+	getRate(result, info, 'AZN');
+	getRate(result, info, 'DKK');
+	getRate(result, info, 'CAD');
+	getRate(result, info, 'MDL');
+	getRate(result, info, 'NOK');
+	getRate(result, info, 'PLN');
+	getRate(result, info, 'SGD');
+	getRate(result, info, 'XDR');
+	getRate(result, info, 'HUF');
+	getRate(result, info, 'CZK');
+	getRate(result, info, 'SEK');
+	getRate(result, info, 'BGN');
+	getRate(result, info, 'KRW');
+	getRate(result, info, 'HKD');
+	getRate(result, info, 'EGP');
+	getRate(result, info, 'INR');
+	getRate(result, info, 'HRK');
+	getRate(result, info, 'MXN');
+	getRate(result, info, 'ILS');
+	getRate(result, info, 'NZD');
+	getRate(result, info, 'ZAR');
+	getRate(result, info, 'RON');
+	getRate(result, info, 'IDR');
+	getRate(result, info, 'SAR');
+	getRate(result, info, 'TRY');
 	getRateDate(result, info);
 	
 	AnyBalance.setResult(result);

f271dfb40230c78b91dbf58fad6974683fb511f8
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2020-12-11 15:29:19 +0200

    Баланс по картам Приватбанка (альтернативный)
    Есть смысл добавлять только в том случае, если не удастся починить ab-money-privatbank
    Провайдер работает через прокси. Сейчас прокси (php страница) крутится на бесплатном хостинге 000webhost.com, если уж добавлять провайдер в базу, то возможно имеет смысл эту страницу разместить на anybalance.ru.
    Необходимость использовать прокси связана с тем что приватбанк при настроке мерчанта требует обязательно указать IP, с которого будут идти запросы, и в дальнейшем отвечает только этому IP. А смартфоны работают из разных точек доступа Wi-Fi + мобильный интернет....

---
+++ providers/ab-money-privatbank-merchant/main.js
@@ -0,0 +1,54 @@
+﻿/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var cards = {
+corp: "Корпоративная",
+pay: "Карта для выплат",
+unior: "Карта юниора",
+kluch: "Ключ к счету",
+univers: "Универсальная",
+USD: "Карта USD",
+EUR: "Карта EUR",
+ZP: "Зарплатная",
+PENS: "Пенсионная",
+SOTS: "Соц.выплаты",
+virt: "Виртуальная"
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	result={success: true};
+	var goodBalances=0;
+	var errorText='';
+	var curErr='';
+	for (var c in prefs){
+        	var responce='';
+		if (prefs[c].length>55 && AnyBalance.isAvailable(c)) {
+                var resp=AnyBalance.requestGet('https://anybalance.000webhostapp.com/?'+(prefs[c].replace(/[^0-9A-Z]+/ig,"&")));
+                    var curErr=getParam(resp, null, null, /id="error">([\s\S]*?)<\/div/i);
+                    if (!curErr || curErr==''){
+			getParam(resp, result, c, /id="balance">([\d\.]*)<\/div/i, null, parseBalance);
+			if (prefs.needLimit){
+				var fin_limit=getParam(resp, null, null, /id="fin_limit">([\d\.]*)<\/div/i, null, parseBalance);
+				if (fin_limit)  result['suf_'+c]='+'+fin_limit;
+			}
+			var currency=getParam(resp, null, null, /id="currency">([A-Z]*?)<\/div/i);
+			if (currency=='UAH'){
+                           currency='₴';
+                           if (!result.balance) result.balance=0;
+                           result.balance+=result[c];
+			}
+                        if (currency && prefs.needValut) result['suf_'+c]=(result['suf_'+c]?result['suf_'+c]+' '+currency:' '+currency);
+                    goodBalances+=1;
+                    }else{
+                    	AnyBalance.trace("Ошибка при чтении баланса карты " + cards[c]);
+                    	AnyBalance.trace(curErr);
+                    	errorText=cards[c]+': '+curErr;
+                    }
+		}
+	}
+        result.__tariff=getFormattedDate('DD.MM.YYYY HH:NN');
+        if (errorText && !goodBalances) throw new AnyBalance.Error(errorText);
+        AnyBalance.setResult(result);
+}

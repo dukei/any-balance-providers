85d5f0e30f35983abde8441bf9a3694574974804
Dmitry Kochin <dco@mail.ru>
2021-01-12 19:50:29 +0300

    Исправления пулреквестов

--- providers/ab-internet-orionnet/main.js
+++ providers/ab-internet-orionnet/main.js
@@ -1,12 +1,4 @@
-﻿/**
-Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
-
-Получает баланс и информацию о тарифе для интернет провайдера Орион телеком 
-
-Operator site: http://orionnet.ru/
-Личный кабинет: https://stat.orionnet.ru/
-*/
-
+﻿
 var g_headers = {
 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
@@ -17,30 +9,32 @@ var g_headers = {
 
 function main(){
     var prefs = AnyBalance.getPreferences();
-
-    var baseurl = "https://stat.orionnet.ru/";
-
+    var baseurl = "https://orionnet.ru";
     AnyBalance.setDefaultCharset('utf-8'); 
 
-    var html = requestPostMultipart(baseurl, {
+	var html = AnyBalance.requestPost(baseurl+'/auth.php', {
         login:prefs.login,
-        pass:prefs.password
+        password:prefs.password
     }, addHeaders());
-
-    if(!/\/\?go=exit/i.test(html)){
-        var error = getParam(html, null, null, /javascript:alert\(\'([\s\S]*?)\'/i, replaceTagsAndSpaces, html_entity_decode);
-        if(error)
-            throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-
-    var result = {success: true};
-    getParam(html, result, 'fio', /Полное имя:<\/span>(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, '__tariff', /Текущий тариф:<\/td>(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'balance', /Баланс:<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'credit', /Кредит:<\/td>\s*<td[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'pay', /Абон. плата:<\/span>(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'block', /Блокировка:<\/span>(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
-
-    AnyBalance.setResult(result);
+	
+	html = JSON.parse(html);
+    	
+	if (!html.status) {
+        if(html.message)
+            throw new AnyBalance.Error(html.message);
+        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');		
+	}
+	else {
+		var result = {success: true};
+		html = AnyBalance.requestGet(baseurl+'/my.php', addHeaders());
+		getParam(html, result, 'fio', /<div class="user_name">([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'adress', /<div class="address docs-popover purple-tooltip" data-toggle="tooltip">\s*([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'account', /Лиц.счет:<\/strong>\s*([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'balance', /Баланс:<\/strong>\s*([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, '__tariff', /<span class="tariff">\s*([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'credit', /Обещанный платеж:<\/strong>\s*([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, parseBalance);
+		getParam(html, result, 'block', /Состояние блокировки Вашего подключения">\s*([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(html, result, 'pay', /data-tariff-user-tcost="\s*([\s\S]*?)"/i, replaceTagsAndSpaces, parseBalance);
+	}
+    AnyBalance.setResult(result);	
 }

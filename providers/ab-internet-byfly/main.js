4470987e214cb0797f487c9fd4659ed798784ee0
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2021-01-19 11:53:19 +0200

    ab-internet-byfly Возврат к старому ЛК

--- providers/ab-internet-byfly/main.js
+++ providers/ab-internet-byfly/main.js
@@ -4,97 +4,59 @@
 
 var g_headers = {
 	Connection: 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-	Origin: 'https://my.beltelecom.by',
-	Referer: 'https://my.beltelecom.by/check-balance',
-	'Accept-Language': 'en-GB,en;q=0.9,ru-RU;q=0.8,ru;q=0.7,en-US;q=0.6',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
+	'Accept-Language': 'ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
+	accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+	referer: 'https://issaold.beltelecom.by/main.html',
+        dnt: 1,
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'upgrade-insecure-requests': 1
 };
 
-function encodeInt(e) {
-    var t = "", r = 64;
-    var a = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_".split("")
-    do {
-        t = a[e % r] + t,
-        e = Math.floor(e / r)
-    } while (e > 0);
-    return t;
-}
-
 function main(){
-    var prefs = AnyBalance.getPreferences();
-
-    var html = AnyBalance.requestPost('https://myapi.beltelecom.by/api/v1/contracts/check/balance', JSON.stringify({
-  		"login": prefs.login,
-  		"password": prefs.password,
-	}), addHeaders({
-		Accept: 'application/json',
-		'content-type': 'application/json',
-		hl: 'ru',
-		'X-Client': 'web',
-	}));
-
-	var json = getJson(html);
-	if(!json.channel){
-		var error = '';
-		for(var n in json){
-			error += n + ': ' + json[n].join('\n') + '\n';
+	var baseurl='https://issaold.beltelecom.by/';
+	try{
+         AnyBalance.restoreCookies();
+        }catch(e){
+	 clearAllCookies();
+	 AnyBalance.saveCookies();
+	 AnyBalance.saveData();
+	}
+	var html=AnyBalance.requestGet(baseurl+'main.html',g_headers);
+        var prefs = AnyBalance.getPreferences();
+	if (!/logout/i.test(html)){
+		if (/oper_user/i.test(html)){
+			var img=AnyBalance.requestGet(baseurl+'data/capcha/secpic.php',g_headers);
+		   var code= AnyBalance.retrieveCode('Пожалуйста, введите код с картинки', img, {
+			inputType: 'number',
+			minLength: 4,
+			maxLength: 4,
+			time: 300000
+		   });
+        	   var html=AnyBalance.requestPost(baseurl+'main.html',{
+        		redirect: '/main.html',
+                	oper_user: prefs.login,
+                	passwd: prefs.password,
+                	cap_field: code
+        		},g_headers);
+        	var html=AnyBalance.requestGet(baseurl+'main.html',g_headers);
 		}
-		if(error)
-			throw new AnyBalance.Error(error, null, /логин|парол/i.test(error));
+	}
+	if (!/logout/i.test(html)) {
 		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось войти, сайт изменен?');
-	}
-
-	AnyBalance.trace('Channel id: ' + json.channel);
-
-	html = AnyBalance.requestGet('https://myws.beltelecom.by/socket.io/?EIO=3&transport=polling&t=' + encodeInt(+new Date()), addHeaders({
-	    Accept: '*/*',
-	}));
-
-	var sess = getJsonObject(html);
-	AnyBalance.trace('sid: ' + sess.sid);
-
-	html = AnyBalance.requestPost('https://myws.beltelecom.by/socket.io/?EIO=3&transport=polling&sid=' + sess.sid + '&t=' + encodeInt(+new Date()),
-		'150:42' + JSON.stringify(
-			["subscribe",{"channel":json.channel,"auth":{"headers":{"Authorization":"Bearer undefined"}}}]
-		),
-		addHeaders({
-	    	Accept: '*/*',
-	    	'Content-type': 'text/plain;charset=UTF-8'
-		}));
-
-	if(html !== 'ok'){
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Ошибка подписки на баланс. Сайт изменен?');
-	}
-
-	json = [];
-	for(var i=0; i<20; ++i){
-		html = AnyBalance.requestGet('https://myws.beltelecom.by/socket.io/?EIO=3&transport=polling&sid=' + sess.sid + '&t=' + encodeInt(+new Date()),
-			addHeaders({
-	    		Accept: '*/*',
-			}));
-
-	    AnyBalance.trace('Попытка ' + (i+1) + '/20: ' + html);
-		
-		json = getJsonObject(html);
-		if(json && json[2])
-			break;
-	}
-
-	if(!json[2]){
-		throw AnyBalance.Error('Не удалось получить баланс. Сайт изменен?');
-	}
-
-	if(json[2].status !== 'success'){
-		var error = json[2].message;
-		throw AnyBalance.Error(error, null, /парол/i.test(error));
-	}
-
+		throw new AnyBalance.Error('Не удалось войти. Сайт изменен?');
+		}
 	var result = {success: true};
     
-    getParam(json[2].balance, result, 'balance');
-    getParam(json[2].tariff, result, '__tariff');
-    getParam(json[2].message, result, 'agreement');
+    getParam(html, result, 'balance',/баланс:([\s\S]*?)р/i,replaceTagsAndSpaces,parseBalance);
+    getParam(html, result, 'agreement',/Договор ([\s\S]*?)</i,replaceTagsAndSpaces,parseBalance);
+    getParam(html, result, 'username',/ФИО \/ Компания: ([\s\S]*?)</i,replaceTagsAndSpaces,null);
+    getParam(html, result, 'status',/Статус блокировки([\s\S]*?)<a href/i,replaceTagsAndSpaces,null);
+    getParam(html, result, 'login',/Логин ([\s\S]*?)</i,replaceTagsAndSpaces,parseBalance);
+    getParam(html, result, '__tariff',/Тарифный план на услуги([\s\S]*?)<\/tr/i,replaceTagsAndSpaces,null);
+    AnyBalance.saveCookies();
+    AnyBalance.saveData();
     AnyBalance.setResult(result);
+
 }

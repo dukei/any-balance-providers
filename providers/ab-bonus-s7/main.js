1629323bf91d02b73df30e496e96704e418855ce
Dmitry Kochin <dco@mail.ru>
2021-02-12 19:26:17 +0300

    S7 Приоритет (ab-bonus-s7): Обход ошибки соединения

--- providers/ab-bonus-s7/main.js
+++ providers/ab-bonus-s7/main.js
@@ -3,16 +3,13 @@
  */
 
 var g_headers = {
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-	'Accept-Language': 'ru',
-	'Origin': 'https://www.s7.ru',
-    'Referer': 'https://www.s7.ru/',
-	'Upgrade-Insecure-Requests': '1',
-	'Sec-Fetch-Mode': 'navigate',
-	'Sec-Fetch-User': '?1',
-	'Sec-Fetch-Site': 'none'
+	Connection: "keep-alive",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+	"Sec-Fetch-Site": "none",
+	"Accept-Encoding": "gzip, deflate, br",
+	"Accept-Language": "en-US,en;q=0.9",
 
 };
 
@@ -33,15 +30,40 @@ function main() {
     checkEmpty(prefs.login, 'Введите логин!');
     checkEmpty(prefs.password, 'Введите пароль!');
 
+   
+    var domain = 'www.s7.ru';
+    var cookies = {
+    	ssdkl: 'e048a38e0c734259a52cdac7f1e42b06',
+		lang: 'en',
+		isAuth: '0',
+		airport: 'DME',
+		country: 'RU',
+		_psc: '1',
+		rerf: 'AAAAAGAmqFI8HMC1AwcCAg==',
+		SES_ID2: 'M2EyZmRmOTEtY2Q0Ny00YjFjLThhMjUtMmYyMDM4NTdmOGUy'
+	}
+
+	for(var n in cookies){
+		AnyBalance.setCookie(domain, n, cookies[n]);
+	}
+
     var baseurl = 'https://www.s7.ru/';
 
-    var html = AnyBalance.requestGet(baseurl, g_headers);
+    var maxtries = 10;
+    for(var i=0; i<maxtries; ++i){
+    	AnyBalance.trace("connecting to s7, try " + (i+1) + ' of ' + maxtries);
+    	var html = AnyBalance.requestGet(baseurl, g_headers);
 
-    if (!html || (AnyBalance.getLastStatusCode() >= 400)) {
-        AnyBalance.trace(html);
-        throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+        if (!html || (AnyBalance.getLastStatusCode() >= 400)) {
+            AnyBalance.trace(baseurl + ' -> ' + AnyBalance.getLastStatusCode());
+            if(i == maxtries-1)
+            	throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+            else
+            	continue;
+        }
+        break;
     }
-
+     
     html = AnyBalance.requestGet(baseurl + 'servlets/formprocess/formsecurity?action=checkStatus', g_headers);
     var formStatus = AnyBalance.getLastResponseHeader('X-Form-Status');
     if(!formStatus){
@@ -49,21 +71,25 @@ function main() {
     	throw new AnyBalance.Error('Не удалось получить токен авторизации. Сайт изменен?');
     }
 
+    /*
     html = AnyBalance.requestGet(baseurl + 'servlets/formprocess/formsecurity?action=generateUiTicketUniqName', g_headers);
     var json = getJson(html);
     if(!json.c || !json.c.name){
     	AnyBalance.trace(html);
     	throw new AnyBalance.Error('Не удалось получить имя токена авторизации. Сайт изменен?');
     }
-
+      */
     var loginJson = AnyBalance.requestPost(baseurl + 'dotCMS/priority/ajaxLogin', {
         dispatch: 'login',
         username: prefs.login,
         password: prefs.password,
-        ticketName: json.c.name,
-		uiTicket: json.c.value,
         xFormCode: getFormCodeFromStatus(formStatus)
-    }, g_headers);
+    }, addHeaders({
+    	Accept: '*/*',
+		'X-Requested-With': 'XMLHttpRequest',
+		ADRUM: 'isAjax:true',
+		Referer: baseurl + 'ru/about/oneworld/index.dot'
+	}));
     
     var login = AB.getJson(loginJson);
     if (login.status != 'success') {

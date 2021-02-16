d3f0dc52bec79c44c68285830d7a6f04cbbc9937
Dmitry Kochin <dco@mail.ru>
2021-02-16 16:33:08 +0300

    Исправление название папки для петруни

--- providers/ab-municipal-domofon-e/main.js
+++ providers/ab-municipal-domofon-e/main.js
@@ -12,40 +12,44 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://domofon-e.ru/';
+	var baseurl = 'https://domofon-e.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 	
 	checkEmpty(prefs.login, 'Введите номер договора!');
 	checkEmpty(prefs.password, 'Введите пароль!');
 	
-	var html = AnyBalance.requestGet(baseurl + 'lk/state/', g_headers);
+//	var html = AnyBalance.requestGet(baseurl + 'lk/state/', g_headers);
 	
-	if(!html || AnyBalance.getLastStatusCode() > 400)
-		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+//	if(!html || AnyBalance.getLastStatusCode() > 400)
+//		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	
 	html = AnyBalance.requestPost(baseurl + 'templates/petrunya/ajax/getData/', {
 		dg: prefs.login,
 		ps: prefs.password,
 		persData: "1",
-		'get': 'checkUser'
+		'get': 'checkUser',
+		sn: 'domofon-e.ru'
 	}, addHeaders({Referer: baseurl + 'lk/state/', 'X-Requested-With': 'XMLHttpRequest'}));
-    
-	if (!/status="1"/i.test(html)) {
-		var error = getParam(html, null, null, [/<div[^>]+id="lk_error"[^>]*>([\s\S]*?)<\//i, /comment="([^"]+)/i], replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error, null, /Не правильно указан логин или пароль/i.test(error));
+        if (html) {
+        	var json=getJson(html);
+		if (json.status!=1) {
+			var error = json.comment;
+			if (error)
+				throw new AnyBalance.Error(error, null, /Не правильно указан логин или пароль/i.test(error));
 		
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+			AnyBalance.trace(html);
+			throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+		}
+	}else{
+		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
-	
     html = AnyBalance.requestGet(baseurl + 'lk/state/', g_headers);
     
 	var result = {success: true};
 	
 	getParam(html, result, 'balance', /Текущий баланс:(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'fio', /Владелец договора:(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'abon_payment', /Абонентская плата:(?:[^>]*>){4}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'abon_payment', /Абонентская плата:(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'account', /лицевого счета №([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 	getParam(html, result, 'status', /Состояние трубки(?:[^>]*>){2}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
 	

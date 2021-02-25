fe213ea4c9ad38710cdb3df2892791d2e195c8d3
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2021-02-25 17:19:27 +0200

    Личный кабинет перехал и полностью изменился

--- providers/ab-municipal-enera/main.js
+++ providers/ab-municipal-enera/main.js
@@ -2,66 +2,73 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
-var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
-};
-
-var g_headers1 = {'Accept': 'application/json, text/javascript, */*; q=0.01',
-	'Referer': 'https://www.enera.lg.ua/uk/customer',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-	'X-Requested-With': 'XMLHttpRequest'};
-
-
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://www.enera.lg.ua/uk/customer/';
+	var baseurl = 'https://lg.e-svitlo.com.ua/';
 	AnyBalance.setDefaultCharset('utf-8');
-	var html = AnyBalance.requestGet(baseurl+'get-billing-info?_=1562964221303', g_headers1);
-	var html = AnyBalance.requestGet(baseurl+'login', g_headers);
-
-        
-
-	var params = createFormParams(html, function(params, str, name, value){
-		if(name == 'login')
-			return prefs.login;
-		else if(name == 'passwd')
-			return prefs.password;		
-		return value;
-	});
-	
-	html = AnyBalance.requestPost(baseurl+'login', params, g_headers); 
-
-	
+	var html = AnyBalance.requestPost(baseurl+'registr_all_user/login_all_user',{email:prefs.login, password:prefs.password});
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
-	
-	if (!/\/uk\/customer\/logout/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
-		
+	var err=getParam(html,null,null,/navbar navbar-inverse">([\s\S]*?)<div class="navbar-inner"/,replaceTagsAndSpaces,html_entity_decode)
+	if (err&&err!='Ласкаво просимо'){
+		AnyBalance.trace(html);
+		AnyBalance.trace(err);
+		throw new AnyBalance.Error(err,false,/парол|заблок|e-?mail|телеф|пользоват|користува/.test(err));
+	}
+        var html = AnyBalance.requestGet(baseurl+'account_household');
+	if (!/logout/i.test(html)) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
 	}
-	var result = {success: true};
-	var responce = getJson(AnyBalance.requestGet(baseurl+'get-billing-info?_=1562964221303', g_headers1));
-	html=responce.data.billingDataHtml;
-	getParam(html, result, 'balance', /за електроенергію[\s\S]*?<\/tr>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'day', /(?:Останні показники \(день\))([\s\S]*?)\<\/tr\>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'night', /(?:Останні показники \(ніч\))([\s\S]*?)\<\/tr\>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'all', /(?:Значення останнього показання)([\s\S]*?)\<\/tr\>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'lastdate', /(?:Дата знімання показань лічильника)([\s\S]*?)\<\/tr\>/i, replaceTagsAndSpaces, parseDate);
-	var preresult= {success: true};
-	getParam(html, preresult, 'ls', /(?:особового рахунку)([\s\S]*?)\<\/tr\>/i, replaceTagsAndSpaces);
-	getParam(html, preresult, 'dogdate', /(?:Дата укладення договору)([\s\S]*?)\<\/tr\>/i, replaceTagsAndSpaces);
-	getParam(html, preresult, 'EIC', /(?:EIC)([\s\S]*?)\<\/tr\>/i, replaceTagsAndSpaces);
-        result.__tariff='ЛС:'+preresult.ls+' EIC:'+preresult.EIC+' Договор от '+preresult.dogdate;
+	var households=[];
+	var household={};
+	var table = getElement(html, /<table class="styled-up-custom-table">/i);
+	var rows = getElements(table, /<tr/ig);
+	for(var i=1; i<rows.length; i++){
+		var row = rows[i];
+		household.id = getParam(row, /insert_calc_value\?a=(\d*)/i, replaceTagsAndSpaces);
+		household.ls = getParam(row, /<td>([^<]*)/i, replaceTagsAndSpaces);
+		household.fio = getParam(row, /(?:[\s\S]*?<td[^>]*>){2}([^<]*)/i, replaceTagsAndSpaces);
+		household.adr = getParam(row, /(?:[\s\S]*?<td[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces);
+                if (household.id) 
+                	households.push({id:household.id,ls:household.ls,fio:household.fio,adr:household.adr});
+                AnyBalance.trace('Найден лицевой счет:\n'+household.ls+' ('+household.fio+', '+household.adr+')');
+	}
+	if (!households[0].id)
+		throw new AnyBalance.Error('Не найдено ни одного лицевого счета.');
+	if (prefs.ls) 
+		households=households.filter(h => h.ls.includes(prefs.ls)||h.fio.includes(prefs.ls)||h.adr.includes(prefs.ls));
+        if (households.length==0)
+        	throw new AnyBalance.Error('Не найден лицевой счет содержащий в лицевом счете, фамилии или адресе "'+prefs.ls+'"');
+        var result=households[0];
+        result.success=true;
+
+        AnyBalance.trace('Выбран лицевой счет:\n'+result.ls+' ('+result.fio+', '+result.adr+')');
+        var html = AnyBalance.requestGet(baseurl+'account_household/view_main_details?a='+result.id+'&highlight=account_household&osr=1');
+        getParam(html, result, 'eic', /EIC код[\s\S]*?<div[^>]*?>([^<]*)/);
+        getParam(html, result, 'lastvaluedate', /Останні розрахункові покази лічильника[\s\S]*?<div[^>]*?>([^<]*)/,null, parseDate);
+
+        var tmp=getElementsByClassName(html,'borg-text',replaceTagsAndSpaces, null);
+	getParam(tmp[1], result, 'balance', null , null, parseBalance);        
+        if (tmp[0]=='Сума до сплати') result.balance=-result.balance;
+        getParam(tmp[2], result, 'pay', null , null, parseBalance);        
+        getParam(tmp[3], result, 'lastpaydate', null , null, parseDate);        
+
+        tmp=getParam(html,/Останні розрахункові покази лічильника[\s\S]*?<\/div>([\s\S]*?)<div class="first-column">/)
+        tmp=getElements(tmp,/<h4>/g,replaceTagsAndSpaces,parseBalance)
+        getParam(tmp[0], result, 'value1');        
+        getParam(tmp[1], result, 'value2');        
+        getParam(tmp[2], result, 'value3');        
+
+        tmp=getParam(html,/Споживання за останній розрахунковий місяць[\s\S]*?<\/div>([\s\S]*?)<\/div>\s*<\/div>/)
+        tmp=getElements(tmp,/<h4>/g,replaceTagsAndSpaces,parseBalance)
+        getParam(tmp[0], result, 'use1');        
+        getParam(tmp[1], result, 'use2');        
+        getParam(tmp[2], result, 'use3');        
+
+        result.__tariff=['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Окябрь','Ноябрь','Декабрь'][getFormattedDate({offsetMonth:1,format:'M'},new Date(result.lastvaluedate))-1]+getFormattedDate({offsetMonth:1,format:' YYYY г.'},new Date(result.lastvaluedate));
 
         AnyBalance.setResult(result);
 }

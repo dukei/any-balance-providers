42bb6325591070368f3f3529545adf9c978709e3
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2021-04-29 15:16:47 +0300

    ab-bonus-perekrestok

--- providers/ab-bonus-perekrestok/main.js
+++ providers/ab-bonus-perekrestok/main.js
@@ -7,7 +7,7 @@
 Личный кабинет: https://prcab.x5club.ru/cwa/
 */
 
-var domain = "my.perekrestok.ru"
+var baseurl = "https://api.perekrestok.ru/api/customer/1.4.0.0/"
 
 var g_headers = {
 	'Accept': 'application/json, text/plain, */*',
@@ -15,38 +15,23 @@ var g_headers = {
 	'Connection': 'keep-alive',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
 	'If-Modified-Since': '0',
-	'Content-Type': 'application/json',
-	Origin: "https://" + domain
+	'Content-Type': 'application/json;charset=UTF-8',
 };
-
-var baseurl = "https://" + domain + "/"
-var apiHeaders = {
-	Referer: baseurl + 'login'
-};
-
-function callApi(verb, getParams, postParams){
-	var method = 'GET';
-	var h = apiHeaders;
-	if(isset(postParams)){
-		method = 'POST';
-		h = addHeaders({'Content-Type': 'application/json;charset=UTF-8'}, apiHeaders);
-	}
-	
-	var html = AnyBalance.requestPost(baseurl + 'api/' + verb, postParams && JSON.stringify(postParams), addHeaders(h), {HTTP_METHOD: method});
-
-	var json = html ? getJson(html) : {};
-
+function callApi(verb,postParams){
+AnyBalance.trace('============================================================');
+AnyBalance.trace('verb='+verb);
+if (postParams) AnyBalance.trace('postParams='+JSON.stringify(postParams));
+var method=postParams?'POST':'GET';
+var html = AnyBalance.requestPost(baseurl + verb, JSON.stringify(postParams), g_headers, {HTTP_METHOD: method});
+AnyBalance.trace('html='+html);
+AnyBalance.trace('============================================================');
 	if(AnyBalance.getLastStatusCode() >= 500 && !html)
 		throw new AnyBalance.Error('Ошибка сервера');
 
-	if(AnyBalance.getLastStatusCode() >= 400 && json.code){
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error(json.title + '\n' + json.message, null, /не привязан|некорректный/i.test(json.message));
-	}
-
+	var json = html ? getJson(html) : {};
 	if(json.error){
 		AnyBalance.trace(html);
-		throw new AnyBalance.Error(json.error.description, null, /парол/i.test(json.error.description));
+		throw new AnyBalance.Error(json.error.message, null, /парол/i.test(json.error.description));
 	}
 
 
@@ -55,148 +40,91 @@ function callApi(verb, getParams, postParams){
 		throw new AnyBalance.Error('Ошибка ' + verb + ': ' + json.non_field_errors.join(', '));
 	}
 
-	return json;
-}
-
-function setTokensCookies(tokens){
-    AnyBalance.setCookie(domain, 'perekrestok.accessToken', "Bearer%20" + tokens.accessToken);
-    AnyBalance.setCookie(domain, 'perekrestok.refreshToken', tokens.refreshToken);
-    g_headers['X-Authorization'] = "Bearer " + tokens.accessToken;
-}
-
-function passCaptcha(info){
-    var response = solveRecaptcha('Пожалуйста, докажите, что вы не робот', baseurl, info.captcha_key);
-
-    var json = callApi('..' + info.captcha_url, null, {
-    	'g_recaptcha_response': response
-    });
-
-    if(!json.is_valid){
-    	throw new AnyBalance.Error('Не удалось проверить капчу. Сайт изменен?');
-    }
+	return json.content;
 }
 
 function main () {
     var prefs = AnyBalance.getPreferences ();
     AnyBalance.setDefaultCharset('utf-8');
 
-	checkEmpty(prefs.login, 'Введите номер карты или номер телефона.');
-		
-    AnyBalance.trace('Входим в кабинет ' + baseurl);
+    checkEmpty(prefs.login, 'Введите номер телефона.');
+    prefs.login='7'+prefs.login.replace(/[^\d]*/g,'').substr(-10);
 
-    var html = AnyBalance.requestGet(baseurl + '', g_headers);
-    if(AnyBalance.getLastStatusCode() >= 400){
-    	AnyBalance.trace(html);
-    	throw new AnyBalance.Error('Личный кабинет ' + baseurl + ' временно недоступен. Пожалуйста, попробуйте позже');
-    }
+    AnyBalance.trace('Входим в кабинет https://my.perekrestok.ru/');
+    var data={};
+    data.token=AnyBalance.getData('token'+prefs.login);
+    data.refreshToken=AnyBalance.getData('refreshToken'+prefs.login);
+    data.uuid=AnyBalance.getData('uuid'+prefs.login);
 
-    var tokens = AnyBalance.getData('tokens');
-    var json;
 
-    var instanceId = hex_md5(prefs.login);
-    AnyBalance.setCookie(domain, 'perekrestok.instanceId', instanceId);
+    AnyBalance.trace('token:'+data.token);
+    AnyBalance.trace('refreshToken:'+data.refreshToken);
+    AnyBalance.trace('uuid:'+data.uuid);
 
-    if(!tokens){
-        tokens = callApi('v5/auth/signin', null, {
-        	"instance_id": instanceId,
-        	"device":{"platform":"WEB","version":"2.0"},
-        	"grant_type":"DEVICE_TOKEN"
-        });
-    }
-    
-    setTokensCookies(tokens);
-    json = retryTimes(function(){ return callApi('v5/users/me') }, 5);
-
-    if(AnyBalance.getLastStatusCode() == 401){
-    	try{
-    		AnyBalance.trace('Unauthorized, trying to refresh');
-            tokens = callApi('v5/auth/refresh', null, {
-            	"instance_id": instanceId,
-            	"refreshToken":tokens.refreshToken
-            });
-            setTokensCookies(tokens);
-            
-    		json = retryTimes(function(){ return callApi('v5/users/me') }, 5);
-    	}catch(e){
-    		AnyBalance.trace("Unable to refresh: " + e.message + ". Need to reauth");
-    		AnyBalance.setData("tokens", null);
-    		AnyBalance.saveData();
-    		throw new AnyBalance.Error(e.message, true);
-    	}
-    }
-
-    if(AnyBalance.getLastStatusCode() == 401){
-    	AnyBalance.trace('Still unauthorized, trying to login');
-    	AnyBalance.setCookie(domain, 'perekrestok.authorized', 'false');
-
-    	var num = prefs.login.replace(/\D/g, '');
-    	var phone = num.length < 16;
-    	if(phone)
-    		num = '+' + (num.length <= 10 ? '7' : '') + num;
-
-    	do{
-        	json = callApi(phone ? 'v5/2fa/phone/requests' : 'v5/2fa/loyalty/requests', null, {
-        		"number": num,
-        	});
-        	
-        	if(json.captcha_key){
-        		AnyBalance.trace('Потребовалась капча');
-        		passCaptcha(json);
-        	}else{
-        		break;
-        	}
-        	
-        }while(true);
-
-        var code = AnyBalance.retrieveCode('Пожалуйста, введите код из СМС для входа в ЛК Перекресток', null, {inputType: 'number', time: 300000});
-
-        tokens = callApi('v5/auth/signin', null, {
-        	"instance_id": instanceId,
-        	"grant_type":"IDENTITY_TOKEN",
-        	"two_factor": {
-        		"code": code,
-        		"token": json.token
-        	}
-        });
-        setTokensCookies(tokens);
-
-    	json = retryTimes(function(){ return callApi('v5/users/me') }, 5);
-    }
-
-    if(AnyBalance.getLastStatusCode() >= 400){
-    	throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
-    }
-
-    AnyBalance.setData('tokens', tokens);
-    AnyBalance.saveData();
-
-    var result = {success: true};
-
-    getParam(json.personalData.firstName + ' ' + json.personalData.lastName, result, 'customer');
-    getParam(json.loyaltyNo, result, '__tariff');
-    getParam(json.balances.points, result, 'balance');
-    getParam(json.balances.stickers, result, 'stickers');
-
-    if(isAvailable(['burnInThisMonth', 'burnDate'])){
-    	json = callApi('v3/balances');
-
-    	if(json.data.expiration_info){
-	    	getParam(json.data.expiration_info.value, result, 'burnInThisMonth');
-    		getParam(json.data.expiration_info.date*1000, result, 'burnDate');
-    	}
-    }
-
-    AnyBalance.setResult (result);
-}
-
-function retryTimes(func, times){
-	for(var i=0; i<times; ++i){
-		try{
-			return func();
-		}catch(e){
-			AnyBalance.trace('Попытка ' + (i+1) + '/' + times + ': ' + e.message);
-			if(i === times-1)
-				throw e;
+    if (data.token){
+    	AnyBalance.trace('Найден старый токен. Проверяем')
+        g_headers['Authorization'] = "Bearer " + data.token;
+        try{
+        	var json =callApi('user/profile');
+                AnyBalance.trace('Токен в порядке. Используем.')
+        }catch(e){
+        	AnyBalance.trace(e.message)
+    		AnyBalance.trace('Токен испорчен.')
+    		delete g_headers.Authorization;
+                data.token='';
         }
     }
+    if (!data.token&&data.refreshToken){
+    	AnyBalance.trace('Пробуем обновить токен');
+    	try{
+    		var json = callApi('token/refresh',{refreshToken:data.refreshToken,device:{name:'Chrome 77',os:'Windows 10',uuid:data.uuid}});
+    		data.uuid =json.device.uuid;
+    		data.token=json.accessToken;
+    		data.refreshToken=json.refreshToken;
+    		g_headers['Authorization'] = "Bearer " + json.accessToken;
+    		var json =callApi('user/profile');
+                AnyBalance.trace('токен успешно обновлен.');
+    	}catch(e){
+    		AnyBalance.trace(e.message)
+    		delete g_headers.Authorization;
+                data.token='';
+    		AnyBalance.trace('Не удалось обновить токен.');
+    	}
+    }
+    if (!data.token){
+    	AnyBalance.trace('Авторизация через смс');
+    	var json = callApi('token/anonymous',{device:{name:'Chrome 77',os:'Windows 10'}});
+    	g_headers['Authorization'] = "Bearer " + json.accessToken;
+    	var json = callApi('token/anonymous',{device:{name:'Chrome 77',os:'Windows 10'}});
+    	g_headers['Authorization'] = "Bearer " + json.accessToken;
+    	var json = callApi('user/sign-in',{phone: prefs.login,isAdvertAgreed:true});
+    	data.uuid=json.uuid;
+    	var code = AnyBalance.retrieveCode('Пожалуйста, введите код из СМС для входа в ЛК Перекресток. Код отправлен на телефон '+json.phone, null, {inputType: 'number', time: 300000});
+    	var json = callApi('user/sign-in/confirm',{code: code,isAdvertAgreed: true,uuid: data.uuid});    
+        data.uuid =json.device.uuid;
+    	data.token=json.accessToken;
+    	data.refreshToken=json.refreshToken;
+    	g_headers['Authorization'] = "Bearer " + json.accessToken;
+        var json =callApi('user/profile');
+    }
+
+    var result = {success: true};
+    getParam(json.basicInformation.firstName + ' ' + json.basicInformation.lastName, result, 'customer');
+
+    var json =callApi('user/loyalty/card');
+    getParam(json.accountId, result, '__tariff');
+    getParam(json.points, result, 'balance');
+    getParam(json.rubles/100, result, 'balance_rub');
+    getParam(json.stickers, result, 'stickers');
+    if (json.nearestExpiration){
+    getParam(json.nearestExpiration.points, result, 'burnInThisMonth')
+    getParam(json.nearestExpiration.rubles/100, result, 'burnInThisMonth_rub')
+    getParam(json.nearestExpiration.date, result, 'burnDate', null, [/(\d{4})-(\d{2})-(\d{2})/,'$3.$2.$1'], parseDate)
+    }
+    AnyBalance.setData('token'+prefs.login, data.token);
+    AnyBalance.setData('refreshToken'+prefs.login, data.refreshToken);
+    AnyBalance.setData('uuid'+prefs.login, data.uuid);
+    AnyBalance.saveData();
+
+    AnyBalance.setResult (result);
 }

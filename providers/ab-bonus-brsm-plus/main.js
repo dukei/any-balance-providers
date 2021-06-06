7257e00afacb83aea4db2664478dd572d0ee252b
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2021-06-06 22:44:51 +0300

    ab-bonus-brsm-plus

--- providers/ab-bonus-brsm-plus/main.js
+++ providers/ab-bonus-brsm-plus/main.js
@@ -3,52 +3,70 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+'Content-Type':'application/json;charset=utf-8',
+'X-Requested-With':'XMLHttpRequest',
+'User-Agent':'Dalvik/2.1.0 (Linux; U; Android 7.1.1; ONEPLUS A5000 Build/NMF26X)',
+'Connection':'Keep-Alive',
+'Accept-Encoding':'gzip'
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://brsm-plus.com/';
 	AnyBalance.setDefaultCharset('utf-8');
-	
-	checkEmpty(prefs.login, 'Введите логин!');
-	checkEmpty(prefs.password, 'Введите пароль!');
-	html = AnyBalance.requestPost(baseurl + 'ua/login', {
-		num_cart: prefs.login,
-		password: prefs.password
-	});
-	if (html != '{"success":true}') {
-		var error = getJson(html).error;
-		if (error)
-			throw new AnyBalance.Error(error, null, /Стoрiнка тимчасoвo недoступна/i.test(error));
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-	}
+	checkEmpty(prefs.phone, 'Введите телефон!');
+        prefs.phone='+380'+prefs.phone.replace(/[^\d]*/g,'').substr(-9);
+	var token=AnyBalance.getData('token'+prefs.phone);
+	if (!token) {
+		AnyBalance.trace('Первая авторизация');
+		callAPI('Mobile/resend_sms',{str:prefs.phone});
+		var code=AnyBalance.retrieveCode('Введите код из SMS', null, {inputType: 'number',minLength:4,maxLength:4, time: 60000});
+                var json=callAPI('Token/PhoneSMSLogin',{Username:prefs.phone,Password:code});
+                if (!json.AccessToken) throw new AnyBalance.Error('Не удалось войти в приложение',null,true)
+                var token=json.AccessToken;
+                AnyBalance.setData('token'+prefs.phone,json.AccessToken)
+                AnyBalance.setData('RefreshToken'+prefs.phone,json.RefreshToken)
+                AnyBalance.saveData();
 
-//	var html = AnyBalance.requestGet(baseurl + 'ua/api2/mobile/get_economy_data', g_headers);
-	var html = AnyBalance.requestGet(baseurl + 'ua/api2/mobile/get_user_data', g_headers);
-	var json=getJson(html);
+        }
+        g_headers = {
+                'Authorization':'Bearer '+token,
+                'Connection':'Keep-Alive',
+                'Accept-Encoding':'gzip',
+                'User-Agent':'okhttp/3.12.0'
+        }
+        var json=callAPI('Mobile/GetUserProfile','  ');
 	var result = {success: true};
-	if  (json.Result=='SUCCESS'){
-		getParam(json.Data.curr_points, result, 'balance');
-		getParam(json.Data.prev_points, result, 'prev_points');
-	}else{
-         	if (json.Data.message) {throw new AnyBalance.Error(json.Data.message)} else {throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?')};
-	}
+        result.balance=json.curr_points;
+        result.prev_points=json.prev_points;
+        if (json.card1)  {
+        	result.card1=json.card1;
+                result.card1link='Ссылка: <a href="https://barcode.tec-it.com/barcode.ashx?data='+json.card1+'&code=&multiplebarcodes=false&translate-esc=true&unit=Fit&dpi=96&imagetype=Gif&rotation=1&color=%23000000&bgcolor=%23ffffff&codepage=Default&qunit=Mm&quiet=0&hidehrt=False">https://barcode.tec-it.com/barcode.ashx?data='+json.card1+'&code=&multiplebarcodes=false&translate-esc=true&unit=Fit&dpi=96&imagetype=Gif&rotation=1&color=%23000000&bgcolor=%23ffffff&codepage=Default&qunit=Mm&quiet=0&hidehrt=False</a>'
+                }
+        if (json.card2)  {
+        	result.card2=json.card2;
+                result.card2link='https://barcode.tec-it.com/barcode.ashx?data='+json.card2+'&code=&multiplebarcodes=false&translate-esc=true&unit=Fit&dpi=96&imagetype=Gif&rotation=1&color=%23000000&bgcolor=%23ffffff&codepage=Default&qunit=Mm&quiet=0&hidehrt=False'
+                }
 
+        result.__tariff=json.last_name+' '+json.first_name+' '+json.middle_name
 
-	var html = AnyBalance.requestGet(baseurl + 'ua/api2/mobile/get_economy_data', g_headers);
-	var json=getJson(html);
-	if  (json.Result=='SUCCESS'){
-                for (var c in json.Data)  result[c]=json.Data[c];
-	}
+	var json = callAPI('Mobile/get_economy_data');
+	for (var c in json)  result[c]=json[c];
 
-	//var discNext = getElement(html, /<div class="disc_next">/i);
-	
-	
 	AnyBalance.setResult(result);
+}
+	
+function callAPI(verb,params){
+	AnyBalance.trace('Запрос '+verb);
+	if (params)
+		var answer=AnyBalance.requestPost('https://td4.brsm-nafta.com/api/v2/'+verb,JSON.stringify(params),g_headers);
+	else
+		var answer=AnyBalance.requestGet('https://td4.brsm-nafta.com/api/v2/'+verb,g_headers);
+	AnyBalance.trace('Ответ:\n '+answer);
+	try{
+		var json=getJson(answer);
+	}catch(e){
+		throw new AnyBalance.Error(answer);
+	}
+	if (json.errorMsg) throw new AnyBalance.Error(json.errorMsg);
+	return json;
 }

f87ccd3bfa8d2f1881c3ee30a3e60ebb34060ef9
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2021-06-16 18:42:35 +0300

    ab-bonus-rivegauche

--- providers/ab-bonus-rivegauche/main.js
+++ providers/ab-bonus-rivegauche/main.js
@@ -3,76 +3,114 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'Origin'	:'http://www.rivegauche.ru',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+'Sec-Fetch-Mode':'cors',
+'Origin':'https://rivegauche.ru',
+'Client-Version':'1.0.2',
+'Content-Type':'application/json',
+'Accept':'application/json, text/plain, */*',
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
+'Sec-Fetch-Site':'same-site',
+'Referer':'https://rivegauche.ru/',
+'Accept-Language':'ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6'
 };
+	var baseurl = 'https://api.rivegauche.ru/rg/v1/newRG/customers/';
 
 function main(){
 	AnyBalance.setDefaultCharset('utf-8');
 	var prefs = AnyBalance.getPreferences();
-	
-	checkEmpty(prefs.cardnum, 'Введите номер карты!');
+	var token=AnyBalance.getData('token'+prefs.login);
+        AnyBalance.restoreCookies();
+	if (token) {
+	   AnyBalance.trace('Найден старый токен:\n'+token);
+	   g_headers.Authorization='Bearer '+token;
 
-	var baseurl = 'http://www.rivegauche.ru/discount/savings';
-	
-	var html = AnyBalance.requestGet(baseurl, g_headers);
-	// Ищем токен
-	var form_build_id = AB.getParam(html, null, null, /form_build_id"[^>]*id="([^"]*)/i);
-	if(!form_build_id)
-		throw new AnyBalance.Error('Не удалось найти форму для запроса. Сайт изменен?');
-	
-	var html = AnyBalance.requestPost(baseurl, {
-		'cardtype':prefs.cardtype || 0,
-		'cardnum':prefs.cardnum,
-		'op':'',
-		'form_build_id':form_build_id,
-		'form_id':"savings_check_form"
-	}, addHeaders({
-		Referer: baseurl
-	}));
-	
-	if (!/статус карты/i.test(html)) {
-		var error = AB.getParam(html, null, null, [/"carderrors"[^>]*>([\s\S]*?)<\/ul/i, /<div[^>]+class="text"[^>]*>([^<]*)/i], AB.replaceTagsAndSpaces);
-		if (error) {
-			throw new AnyBalance.Error(error, null, /Неверный номер карты/i.test(error));
-		}
-
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+           var html=AnyBalance.requestGet(baseurl+'current/statistic',g_headers);
+           AnyBalance.trace('===Результат current/statistic:\n'+html);
+           if (/errors/i.test(html)){
+              token='';
+              AnyBalance.trace(html);
+              if (/token/i.test(html)) {
+		AnyBalance.trace('Токен устарел.');
+		var refreshToken=AnyBalance.getData('refreshToken'+prefs.login);
+		if  (refreshToken) {
+			AnyBalance.trace('Пробуем освежить токен');
+			try{
+                        	token=refresh_token(prefs,refreshToken);
+                        }catch(e){
+                        	AnyBalance.trace(e.message);
+                        }
+                }
+              }
+	   }
 	}
-
+	if (!token) token=loginByPassword(prefs);
+	if (!token) {
+		AnyBalance.setData('token'+prefs.login,'');
+		AnyBalance.setData('refreshToken'+prefs.login,'');
+		clearAllCookies();
+		AnyBalance.saveCookies();
+		AnyBalance.saveData();
+		throw new AnyBalnce.Error('Авторизация не удалась. Возможно изменился API',false,true);
+	}
 	var result = {success: true};
 
-	AB.getParam(html, result, '__tariff', /На[^<]*карте\s*№([^<]*)/i, AB.replaceTagsAndSpaces);
-	AB.getParam(html, result, 'db_balance', /class="savingsAmmount">([^<]*)/i, AB.replaceTagsAndSpaces, AB.parseBalance);
-	AB.getParam(html, result, 'db_date', [/последняя операция по карте (.*)<br>/m, /дата последней[^<]*по карте([^<]*)/i], AB.replaceTagsAndSpaces, AB.parseDate);
-	AB.getParam(html, result, 'db_status', /"card-status"[^>]*>\s*статус карты([^<]*)/i, AB.replaceTagsAndSpaces);
-
-	AB.getParam(html, result, 'db_bonus', /бонус на(?:[^>]*>)?День Рождения(?:[^>]*>){4}\s*<div[^>]*savingsAmmount[^>]*>([^<]+)<\//i, AB.replaceTagsAndSpaces, AB.parseBalance);
-
-	//Обычная карта
-	/*if (prefs.cardtype == 0){	
-		getParam(html, result, '__tariff', /На[^<]*карте\s*№([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-		getParam(html, result, 'db_balance', /class="savingsAmmount">([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'db_date', /дата последней[^<]*по карте([^<]*)/i, replaceTagsAndSpaces, parseDate);
-		getParam(html, result, 'db_status', /"card-status"[^>]*>\s*статус карты([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-	//Золотая карта
-	} else if (prefs.cardtype==1) {
-		getParam(html, result, 'db_balance', /class="savingsAmmount">([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-		getParam(html, result, 'db_date', [/последняя операция по карте (.*)<br>/m, /дата последней[^<]*по карте([^<]*)/i], replaceTagsAndSpaces, parseDate);
-
+	var html=AnyBalance.requestGet(baseurl+'current/discount-cards',g_headers);
+	AnyBalance.trace('===Результат current/discount-cards:\n'+html);
+	json=getJson(html).defaultCard;
+	if (json){
 		
-		regexp=/сумма бонуса к Вашему дню рождения:<\/div>\n<div class=\"savingsAmmount\">\n(.*)\n<\/div>/m;
-		if (res=regexp.exec(html)){
-			result.bonus=res[1];
-		}
+		result.__tariff=json.type.name;
+		result.balance=parseBalance(JSON.stringify(result.loyPointBalances)||0);
+		result.fullCardNumber=json.fullCardNumber.replace(/(\d{4})(\d{3})(\d{2})(\d*)/,'$1 $2-$3-$4');
+	}
 
-		getParam(html, result, 'db_status', /"card-status"[^>]*>\s*статус карты([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-	}*/
-
+	var html=AnyBalance.requestGet(baseurl+'current/statistic',g_headers);
+	AnyBalance.trace('===Результат current/statistic:\n'+html);
+	json=getJson(html);
+	result.orders=json.processingOrdersCount+'/'+json.ordersTotalCount;
+	result.favoritesProductsCount=json.favoritesProductsCount;
+	result.awaitingProductsCount=json.awaitingProductsCount;
+        AnyBalance.saveData();
 	AnyBalance.setResult(result);
+}
+
+function loginByPassword(prefs){
+	AnyBalance.trace('Нужно логиниться');
+	checkEmpty(prefs.login, 'Введите номер карты или email!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	var solve=solveRecaptcha("Пожалуйста, докажите, что Вы не робот",'https://rivegauche.ru/my-account', '6Lc1JKUZAAAAAMk0jBpSvm_WFs9hi9bgsisdcYBE')
+	var html = AnyBalance.requestPost(baseurl+'oauth2/acquire-token',JSON.stringify({
+		"username": prefs.login,
+		"password": prefs.password,
+		"rememberMe": true,
+		"reCaptchaResponse": solve	
+		}), g_headers);
+	AnyBalance.trace('===Результат oauth2/acquire-token:\n'+html);
+	var json=getJson(html);
+	if (json.errors) throw new AnyBalance.Error(json.errors[0].message,false,/password|block/i.test(JSON.stringify(json)));
+	if (!json.tokenPair) throw new AnyBalance.Error('Ошибка авторизации. Пара токенов не найдена');
+	var token=json.tokenPair.accessToken.value;
+	var refreshToken=json.tokenPair.refreshToken.value;
+	AnyBalance.setData('token'+prefs.login,token);
+        AnyBalance.setData('refreshToken'+prefs.login,refreshToken);
+        AnyBalance.saveCookies();
+        AnyBalance.saveData();
+        g_headers.Authorization='Bearer '+token
+        return token;
+}
+function refresh_token(prefs,refreshToken){
+	g_headers.Authorization='';
+	var html = AnyBalance.requestPost(baseurl+'oauth2/exchange-token',JSON.stringify({refreshToken: refreshToken}), g_headers);
+        AnyBalance.trace('===Результат oauth2/exchange-token:\n'+html);
+	var json=getJson(html);
+	if (json.errors) throw new AnyBalance.Error(json.errors[0].message,false,/password|block/i.test(JSON.stringify(json)));
+	if (!json.tokenPair) throw new AnyBalance.Error('Ошибка авторизации. Пара токенов не найдена');
+	var token=json.tokenPair.accessToken.value;
+	var refreshToken=json.tokenPair.refreshToken.value;
+	AnyBalance.setData('token'+prefs.login,token);
+        AnyBalance.setData('refreshToken'+prefs.login,refreshToken);
+        AnyBalance.saveCookies();
+        AnyBalance.saveData();
+        g_headers.Authorization='Bearer '+token
+        return token;
 }

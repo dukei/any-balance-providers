ac088d9fe6c00ebe3f2a452ef0f3fc21d266d356
Dmitry Kochin <dco@mail.ru>
2021-09-30 11:21:59 +0300

    Курсы валют СБ РФ (ab-exchange-sbrf): - Улучшения и украшения

--- providers/ab-exchange-sbrf/main.js
+++ providers/ab-exchange-sbrf/main.js
@@ -1,54 +1,68 @@
 /**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+
+Получает курсы валют с сайта Сбербанка
+
+Сайт: http://data.sberbank.ru/moscow/ru/quotes/metal
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+  'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+  'Connection': 'keep-alive',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
 };
 
-function main() {
-	var result = {success: true};
-	
-	var prefs = AnyBalance.getPreferences();
-	var region = findRegion(prefs.region).REGION_ID;
-	
-	var baseurl = 'https://www.sberbank.ru/portalserver/proxy/?pipe=shortCachePipe&url=';
-	var body = JSON.stringify({
-		"currencyData":[
-			{"currencyCode":"978","rangesAmountFrom":[0,1000]},
-			{"currencyCode":"840","rangesAmountFrom":[0,1000]}
-		],
-		"categoryCode":"base"
-	});
-	var addUrl = 'http://localhost/rates-web/rateService/rate?regionId=' + region + '&fromDate=' + getFormattedDate({offsetDay: 7}) + '&toDate=' + getFormattedDate() + '&hash=' + computeHash(body);
-	
-	var html = AnyBalance.requestPost(baseurl + encodeURIComponent(addUrl), body, addHeaders({
-		'Content-Type': 'application/json',
-		'Referer': 'https://www.sberbank.ru/ru/quotes/currencies',
-		'X-Requested-With': 'XMLHttpRequest'
-	}));
-	var json = getJson(html);
+var g_iso_to2letters = {
+	EUR: 'eur',
+	USD: 'usd'
+};
 
-	if(AnyBalance.isAvailable('date', 'usd_purch', 'usd_sell')){
-		var info = json['840'].rates[0];
-		AB.getParam(info.activeFrom, result, 'date');
-    	AB.getParam(info.buyValue, result, 'usd_purch');
-    	AB.getParam(info.sellValue, result, 'usd_sell');
-    	AB.getParam(getFormattedDate(null, new Date(info.activeFrom)), result, '__tariff');
-	}
-	
-	if(AnyBalance.isAvailable('date', 'eur_purch', 'eur_sell')){
-		var info = json['978'].rates[0];
-		AB.getParam(info.activeFrom, result, 'date');
-    	AB.getParam(info.buyValue, result, 'eur_purch');
-    	AB.getParam(info.sellValue, result, 'eur_sell');
-    	AB.getParam(getFormattedDate(null, new Date(info.activeFrom)), result, '__tariff');
-	}
-	
-	AnyBalance.setResult(result);
+function main(){
+  var prefs = AnyBalance.getPreferences();
+  AnyBalance.setDefaultCharset('utf-8');
+
+  var region = findRegion(prefs.region).TRBANK_CODE;
+
+  var html = AnyBalance.requestGet('https://www.sberbank.ru/proxy/services/rates/public/actual?rateType=ERNP-4&isoCodes[]=USD&isoCodes[]=EUR&regionId=' + region,
+	addHeaders({
+  		'X-Requested-With': 'XMLHttpRequest',
+  		'X-Request-ID': '7cfa7f739ebdf97418a3963183fbba05',
+  		Referer: 'https://www.sberbank.ru/ru/quotes/currencies/'
+  	}));
+
+  var json = getJson(html);
+
+  var result = {success: true};
+
+  for(var key in json){
+  	  var valut = json[key];
+  	  var name = g_iso_to2letters[key];
+	  AnyBalance.trace(name + ': ' + JSON.stringify(valut));
+      if(AnyBalance.isAvailable(name + '_purch'))
+        result[name + '_purch'] = valut.rateList[0].rateBuy;
+      if(AnyBalance.isAvailable(name + '_sell'))
+        result[name + '_sell'] = valut.rateList[0].rateSell;
+      if(AnyBalance.isAvailable(name + '_amount') && (amount = getAmount(name)))
+        result[name + '_amount'] = valut.rateList[0].rateBuy * amount;
+	  if(AnyBalance.isAvailable('date'))
+        sumParam(valut.startDateTime, result, 'date', null, null, null, aggregate_max);
+	  getParam(getFormattedDate(null, new Date(valut.startDateTime)), result, '__tariff');
+  }
+
+  AnyBalance.setResult(result);
 }
+
+function getAmount(valut){
+	var prefs = AnyBalance.getPreferences();
+	if(!prefs.amount)
+		return undefined;
+	if(/^[\d\s\.,]+$/.test(prefs.amount))
+		return parseBalance(prefs.amount);
+	var amount = getParam(prefs.amount, null, null, new RegExp(valut + '\s*:([^;a-z]*)', 'i'), null, parseBalance);
+	return amount;
+}
+
+    	//AB.getParam(getFormattedDate(null, new Date(info.activeFrom)), result, '__tariff');
 

5bba3b4706b24a4a957b5c9b70ab6a57f8df73ea
Dmitry Kochin <dco@mail.ru>
2021-11-30 14:43:15 +0300

    НО Фонд ЖКХ (Санкт-Петербург) (ab-municipal-fkr-spb): - Новая библиотека

--- providers/ab-municipal-fkr-spb/main.js
+++ providers/ab-municipal-fkr-spb/main.js
@@ -11,11 +11,15 @@ var g_headers = {
 
 function main() {
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'http://fkr-spb.ru';
+    var baseurl = 'https://fkr-spb.ru';
     AnyBalance.setDefaultCharset('utf-8');
 
     checkEmpty(prefs.login, 'Введите логин!');
     checkEmpty(prefs.password, 'Введите пароль!');
+	if (prefs.housenum && !prefs.flatnum)
+		checkEmpty(prefs.flatnum, 'Введите номер квартиры!');
+	if (prefs.flatnum && !prefs.housenum)
+		checkEmpty(prefs.housenum, 'Введите номер дома!');
 
     var html = AnyBalance.requestGet(baseurl + '/user', g_headers);
 
@@ -23,45 +27,68 @@ function main() {
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
     }
-    var formBuildID = getParam(html, null, null, /<input[^>]+name="form_build_id"[^>]+value="([\s\S]*?)"[^>]*>/i);
+	
+	var form = getElement(html, /<form[^>]+id="user-login"[^>]*>/i);
+        if(!form){
+        	AnyBalance.trace(form);
+        	throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+        }
+	
+    var formBuildID = getParam(form, null, null, /<input[^>]+name="form_build_id"[^>]+value="([\s\S]*?)"[^>]*>/i);
     html = AnyBalance.requestPost(baseurl + '/user', {
         name: prefs.login,
         pass: prefs.password,
         form_id: 'user_login',
         op: 'Войти',
         form_build_id: formBuildID,
-        'Remember': 'false'
+//      'Remember': 'false'
     }, addHeaders({Referer: baseurl}));
 
     if (!/\/user\/\d+\/edit/i.test(html)) {
-        var error = getParam(html, null, null, /<div[^>]+class="messages error"[^>]*>[\s\S]*?<h2[^>]*>[\s\S]*?<\/h2>([\s\S]*?)\(.*?<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+        var error = getParam(html, null, null, /<div[^>]+class="messages error"[^>]*>[\s\S]*?<h2[^>]*>[\s\S]*?<\/h2>([\s\S]*?)\(.*?<\/div>/i, replaceTagsAndSpaces);
         if (error)
-            throw new AnyBalance.Error(error, null, /Имя пользователя или пароль введены неверно/i.test(error));
+            throw new AnyBalance.Error(error, null, /Логин или пароль введены неверно/i.test(error));
 
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
     var result = {success: true};
 
-    var userID = getParam(html, null, null, /<a href="\/user\/(\d+)\/[^>]*>/i, null, html_entity_decode);
+    var userID = getParam(html, null, null, /<a href="\/user\/(\d+)\/[^>]*>/i);
 
-    html = AnyBalance.requestGet(baseurl + '/user/' + userID + '/edit');
-    getParam(html, result, 'fio', /id="edit-field-fio-und-0-value"[^>]+value="([\s\S]*?)"[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'fio', /<a href="\/user\/\d+\/edit"\stitle="([\s\S]*?)">/i, replaceTagsAndSpaces);
 
     var json = requestGetJson('/rest.php?method=flats', baseurl);
 
     if (!isArray(json) || json.length == 0)
-        throw new AnyBalance.Error('Не удалось найти информацию по квартире, сайт изменен?');
+        throw new AnyBalance.Error('Не удалось найти информацию по квартире. Сайт изменен?');
+	
+	var curFlat;
+	for(var i=0; i<json.length; ++i){
+		var curr = json[i];
+		AnyBalance.trace('Найдена квартира №' + curr.flatNum + ', дом №' + curr.house.houseNum);
+		if(!curFlat && (!prefs.flatnum  || endsWith(curr.house.houseNum + curr.flatNum, prefs.housenum + prefs.flatnum))){
+			AnyBalance.trace('Выбрана квартира №' + curr.flatNum + ', дом №' + curr.house.houseNum);
+			curFlat = curr.id;
+		}
+	}
 
-    getParam(json[0].fullAddress, result, 'adress', null, replaceTagsAndSpaces, html_entity_decode);
-    getParam(json[0].sqrFull + '', result, 'flatArea', null, null, parseBalance);
-    getParam(json[0].kadNum, result, 'flatKadNum', null, replaceTagsAndSpaces, html_entity_decode);
-    getParam(json[0].house.kadNum, result, 'houseKadNum', null, replaceTagsAndSpaces, html_entity_decode);
+	if(!curFlat)
+		throw new AnyBalance.Error('Не удалось найти квартиру №' + prefs.flatnum + ', дом №' + prefs.housenum);
 
-    json = requestGetJson('/rest.php?method=flat&id=' + json[0].id, baseurl);
+    json = requestGetJson('/rest.php?method=flat&id=' + curFlat, baseurl);
+	
+	getParam(json.saldo + '', result, 'balance', null, null, parseBalance);
+	getParam(json.penalty + '', result, 'penalty', null, null, parseBalance);
+	getParam(json.account, result, '__tariff', null, replaceTagsAndSpaces);
+	getParam(json.account, result, 'account', null, replaceTagsAndSpaces);
+	getParam(json.fullAddress, result, 'adress', null, replaceTagsAndSpaces);
+    getParam(json.sqrFull + '', result, 'flatArea', null, null, parseBalance);
+    getParam(json.kadNum, result, 'flatKadNum', null, replaceTagsAndSpaces);
+    getParam(json.house.kadNum, result, 'houseKadNum', null, replaceTagsAndSpaces);
 
-    getParam(json.house.houseTotals.collectPercent, result, 'contributions');
-    getParam(json.saldo + '', result, 'balance', null, null, parseBalance);
+    getParam(json.house.houseTotals.collectPercent + '', result, 'contribution', null, null, parseBalance);
+	getParam(json.house.houseTotals.collectPercentPeny + '', result, 'collectPercentPeny', null, null, parseBalance);
 
     AnyBalance.setResult(result);
 }

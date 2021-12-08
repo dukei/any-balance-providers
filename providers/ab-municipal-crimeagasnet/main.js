0e472178f2238f8540a4973dd01a08ab223f9470
Dmitry Kochin <dco@mail.ru>
2021-12-08 23:52:27 +0300

    Крым-газ: Поддержка изменений на сайте 	- Добавлена возможность выбора лицевого счета

--- providers/ab-municipal-crimeagasnet/main.js
+++ providers/ab-municipal-crimeagasnet/main.js
@@ -12,44 +12,43 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://crimeagasnet.ru:8106';
+	var baseurl = 'https://lkk.crimeagasnet.ru';
 	AnyBalance.setDefaultCharset('utf-8');
 
 //	return createList();
 	
-	checkEmpty(prefs.fam, 'Введите фамилию!');
-	checkEmpty(prefs.licschet, 'Введите лицевой счет!');
+	checkEmpty(prefs.login, 'Введите фамилию!');
+	checkEmpty(prefs.password, 'Введите лицевой счет!');
 	
-	var html = AnyBalance.requestGet(baseurl + '/custom_scripts/abonent.php', g_headers);
+	var html = AnyBalance.requestGet(baseurl + '/', g_headers);
 	
 	if(!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
 
-	var captchaUrl = getParam(html, null, null, /<img[^>]+id="captcha"[^>]*src="([^"]*)/i);
-	if(!captchaUrl){
-		var error = sumParam(html, null, null, /<h4[^>]*>([\s\S]*?)<\/h4>/ig, replaceTagsAndSpaces, null, create_aggregate_join(' '));
-		if(error)
-			throw new AnyBalance.Error(error);
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось получить ссылку на капчу. Сайт изменен?');
-	}
-	var img = AnyBalance.requestGet(baseurl + captchaUrl, g_headers);
-	var captcha = AnyBalance.retrieveCode('Введите код с картинки', img);
+//	var captchaUrl = getParam(html, null, null, /<img[^>]+id="captcha"[^>]*src="([^"]*)/i);
+//	if(!captchaUrl){
+//		var error = sumParam(html, null, null, /<h4[^>]*>([\s\S]*?)<\/h4>/ig, replaceTagsAndSpaces, null, create_aggregate_join(' '));
+//		if(error)
+//			throw new AnyBalance.Error(error);
+//		AnyBalance.trace(html);
+//		throw new AnyBalance.Error('Не удалось получить ссылку на капчу. Сайт изменен?');
+//	}
+//	var img = AnyBalance.requestGet(baseurl + captchaUrl, g_headers);
+//	var captcha = AnyBalance.retrieveCode('Введите код с картинки', img);
 	
-	html = AnyBalance.requestPost(baseurl + '/custom_scripts/abonent.php', {
-		fam: prefs.fam,
-		nl: prefs.licschet,
-		uegh: prefs.region,
-		captcha_code: captcha,
-		search:'Отправить'
-	}, addHeaders({Referer: baseurl + '/custom_scripts/abonent.php'}));
+	html = AnyBalance.requestPost(baseurl + '/', [
+		['mail',prefs.login],
+        ['password',prefs.password],
+        ['save_my','on'],
+        ['submit','']
+	], addHeaders({'Content-Type': 'application/x-www-form-urlencoded', 'Referer': baseurl + '/'}));
 	
-	if (!/<input[^>]+id="fam_res"/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="err"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+	if (!/sign-out/i.test(html)) {
+		var error = getParam(html, null, null, /<h3[^>]*>([\s\S]*?)\./i, replaceTagsAndSpaces);
 		if (error)
-			throw new AnyBalance.Error(error, null, /Пользователь не найден/i.test(error));
+			throw new AnyBalance.Error(error, null, /логин|парол/i.test(error));
 		
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
@@ -57,13 +56,35 @@ function main() {
 	
 	var result = {success: true};
 	
-	getParam(html, result, 'balance', /(?:Переплата|Долг) на (?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-	if(result.balance && /Долг на/i.test(html))
+	var accounts = getElements(html, /<tr[^>]([\s\S]*?)<\/tr>/ig);
+	AnyBalance.trace('Найдено лицевых счетов: ' + accounts.length);	
+
+	if(accounts.length < 1)
+		throw new AnyBalance.Error('У вас нет ни одного лицевого счета');
+	
+	var curAcc;
+	for(var i=0; i<accounts.length; i++){
+		var account = getParam(accounts[i], /ls_id[\s\S]*?(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces);
+	   	AnyBalance.trace('Найден лицевой счет ' + account);
+	   	if(!curAcc && (!prefs.num || endsWith(account, prefs.num))){
+	   		AnyBalance.trace('Выбран лицевой счет ' + account);
+	   		curAcc = accounts[i];
+	   	}
+	}
+
+	if(!curAcc)
+	   	throw new AnyBalance.Error('Не удалось найти лицевой счет ' + prefs.num);
+	
+	getParam(curAcc, result, 'balance', /ls_id[\s\S]*?(?:[^>]*>){9}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	if(result.balance && /\-/i.test(result.balance))
 		result.balance = -result.balance;
 
-	getParam(html, result, 'fio', /<input[^>]+id="fam_res"[^>]*value="([^"]*)/i, replaceHtmlEntities);
-	sumParam(html, result, '__tariff', /<input[^>]+id="fam_res"[^>]*value="([^"]*)/i, replaceHtmlEntities, null, aggregate_join);
-	sumParam(prefs.licschet, result, '__tariff', null, null, null, aggregate_join);
+	getParam(curAcc, result, '__tariff', /ls_id[\s\S]*?(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces);
+	getParam(curAcc, result, 'licschet', /ls_id[\s\S]*?(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces);
+	getParam(curAcc, result, 'department', /ls_id[\s\S]*?(?:[^>]*>){3}([^<]*)/i, replaceTagsAndSpaces);
+	getParam(curAcc, result, 'address', /ls_id[\s\S]*?(?:[^>]*>){7}([^<]*)/i, replaceTagsAndSpaces);
+	getParam(curAcc, result, 'fio', /ls_id[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces);
+//	sumParam(prefs.licschet, result, '__tariff', null, null, null, aggregate_join);
 	
 	AnyBalance.setResult(result);
 }

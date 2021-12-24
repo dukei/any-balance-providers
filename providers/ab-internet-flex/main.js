ee9c15e75fa54137f4c5ed61cdda5383492616b2
Dmitry Kochin <dco@mail.ru>
2021-12-24 10:32:31 +0300

    Flex

--- providers/ab-internet-flex/main.js
+++ providers/ab-internet-flex/main.js
@@ -7,81 +7,59 @@
 Личный кабинет: https://www.flex.ru/stats/
 */
 
-function getParam (html, result, param, regexp, replaces, parser) {
-	if (param && (param != '__tariff' && !AnyBalance.isAvailable (param)))
-		return;
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.6',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+};
 
-	var value = regexp.exec (html);
-	if (value) {
-		value = value[1];
-		if (replaces) {
-			for (var i = 0; i < replaces.length; i += 2) {
-				value = value.replace (replaces[i], replaces[i+1]);
-			}
-		}
-		if (parser)
-			value = parser (value);
-
-    if(param)
-      result[param] = value;
-    else
-      return value
-	}
-}
-
-var replaceTagsAndSpaces = [/&nbsp;/g, ' ', /<[^>]*>/g, ' ', /\s{2,}/g, ' ', /^\s+|\s+$/g, ''];
-var replaceFloat = [/\s+/g, '', /,/g, '.'];
-
-function parseBalance(text){
-    var val = getParam(text.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-    AnyBalance.trace('Parsing balance (' + val + ') from: ' + text);
-    return val;
-}
-
-function parseTrafficGb(str){
-  var val = getParam(str.replace(/\s+/g, ''), null, null, /(-?\d[\d\s.,]*)/, replaceFloat, parseFloat);
-  return parseFloat((val/1024).toFixed(2));
-}
+var baseurl = 'https://flex.ru/stats/';
 
 function main(){
     var prefs = AnyBalance.getPreferences();
+	
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
 
     AnyBalance.setDefaultCharset('utf-8');
 
-    var baseurl = "https://flex.ru/stats/";
-
-    var html = AnyBalance.requestPost(baseurl + '?method=auth', {
+    var html = AnyBalance.requestPost(baseurl + '?auth', {
         login:prefs.login,
         password:prefs.password,
         act:'login'
-    });
+    }, addHeaders({
+		'Content-Type': 'application/x-www-form-urlencoded',
+        'Referer': baseurl
+    }));
 
     //AnyBalance.trace(html);
-    if(!/\?method=logout/.test(html)){
+    if(!/\?logout/.test(html)){
         var error = getParam(html, null, null, /alert\('([^']*)/, replaceTagsAndSpaces, html_entity_decode);
         if(error)
             throw new AnyBalance.Error(error);
-        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Проблемы на сайте или сайт изменен.');
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
     }
 
-    html = AnyBalance.requestGet(baseurl + '?method=show&page=traffic');
+    html = AnyBalance.requestGet(baseurl + '?traffic', g_headers);
 
     var result = {success: true};
 
     getParam(html, result, 'balance', /Баланс лицевого счета\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, 'status', /Состояние учетной записи\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'traffic', /Зачтенный трафик с начала месяца\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, parseTrafficGb);
+	getParam(html, result, ['currency', 'balance'], /Баланс лицевого счета\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, parseCurrency);
+    getParam(html, result, 'discount', /Скидка\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
+    getParam(html, result, 'traffic', /Зачтенный трафик с начала месяца\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, parseTraffic);
+	getParam(html, result, 'outgoing', /Исходящий \(от абонента\)[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
+	getParam(html, result, 'incoming', /Входящий \(к абоненту\)[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
+	getParam(html, result, 'limit', /Лимит[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
+	getParam(html, result, 'exceeding', /Превышение[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
+	getParam(html, result, 'remains', /Остаток[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseTraffic);
     getParam(html, result, 'licschet', /Код пользователя\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
     getParam(html, result, '__tariff', /Действующий тарифный план\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'ip_address', /Привязка по IP адресу\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
+	getParam(html, result, 'username', /Имя пользователя\s*:([\S\s]*?)<br[^>]*>/i, replaceTagsAndSpaces, html_entity_decode);
 
     AnyBalance.setResult(result);
 }
-
-function html_entity_decode(str)
-{
-    //jd-tech.net
-    var tarea=document.createElement('textarea');
-    tarea.innerHTML = str;
-    return tarea.value;
-}
-

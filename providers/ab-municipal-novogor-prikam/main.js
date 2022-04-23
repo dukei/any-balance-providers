09f6c6811a46745282deb03404d931369130b585
Dmitry Kochin <dco@mail.ru>
2022-04-23 09:56:00 +0300

    Новогор-Прикамье (ab-municipal-novogor-prikam): Committing changes

--- providers/ab-municipal-novogor-prikam/main.js
+++ providers/ab-municipal-novogor-prikam/main.js
@@ -10,13 +10,21 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
 };
 
+var baseurl = 'https://lk.novogor.perm.ru';
+var g_savedData;
+
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://lk.novogor.perm.ru';
+	
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login1, 'Введите логин!');
 	checkEmpty(prefs.password1, 'Введите пароль!');
+	
+	if(!g_savedData)
+		g_savedData = new SavedData('novogor', prefs.login);
+
+	g_savedData.restoreCookies();
 
 	var html = AnyBalance.requestGet(baseurl + '/account', g_headers);
 	AnyBalance.trace('Status?');
@@ -26,26 +34,48 @@ function main() {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 	}
+	
+	if(/logout/i.test(html)){
+		AnyBalance.trace('Сессия сохранена. Входим автоматически.');
+	}else{
+		AnyBalance.trace('Сессия новая. Будем логиниться заново.');
+		clearAllCookies();
+	
+        var form = getElement(html, /<form[^>]+class="form ajax-form"[^>]*>/i);
+	    if(!form){
+	    	AnyBalance.trace(html);
+	    	throw new AnyBalance.Error('Не удалось найти форму входа! Сайт изменен?');
+	    }
 
+	    var key = getParam(form, /data-sitekey="?([^"]*)/i, replaceHtmlEntities);
+	    var captcha;
+	    if(key)
+	    	captcha = solveRecaptcha("Пожалуйста, докажите, что вы не робот", AnyBalance.getLastUrl(), key);
 
-	var params = createFormParams(html, function(params, str, name, value) {
-		if (name == 'login')
-			return prefs.login1;
-		else if (name == 'password')
-			return prefs.password1;
+	    var params = createFormParams(form, function(params, str, name, value) {
+	    	if (name == 'login')
+	    		return prefs.login1;
+	    	else if (name == 'password')
+	    		return prefs.password1;
 
-		return value;
-	});
+	    	return value;
+	    });
+	
+	    params['g-recaptcha-response'] = captcha;
 
-	html = AnyBalance.requestPost(baseurl + '/login', params, addHeaders({Referer: baseurl + '/login'}));
+	    html = AnyBalance.requestPost(baseurl + '/login', params, addHeaders({Referer: baseurl + '/login'}));
 
-	if (!/logout/i.test(html)) {
-		var error = getParam(html, null, null, /Ошибка/i, replaceTagsAndSpaces);
-		if (error)
-			throw new AnyBalance.Error(error, null, /имя пользователя или пароль неверны/i.test(error));
+	    if (!/logout/i.test(html)) {
+	    	var error = getParam(html, null, null, /<p[^>]+class="error-text">([\s\S]*?)<\/p>/i, replaceTagsAndSpaces);
+	    	if (error)
+	    		throw new AnyBalance.Error(error, null, /пользовател|парол/i.test(error));
 
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	    	AnyBalance.trace(html);
+	    	throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	    }
+		
+		g_savedData.setCookies();
+	    g_savedData.save();
 	}
 
 	// Получим список лицевых счетов

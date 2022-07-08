3d9c2e6b0bf8f4e7d88cfc39da7a4334952bed25
Dmitry Kochin <dco@mail.ru>
2022-07-08 19:07:29 +0300

    Климовская сеть (ab-internet-klimovsk): Committing changes

--- providers/ab-internet-klimovsk/main.js
+++ providers/ab-internet-klimovsk/main.js
@@ -14,9 +14,6 @@ function main() {
 	var prefs = AnyBalance.getPreferences();
 	var baseurl = 'https://lk.klimovsk.net/';
 	AnyBalance.setDefaultCharset('utf-8');
-
-	//Взято полностью из ab-internet-mts
-	var urlAjax = baseurl + 'client2/client/public/index.php?r=account/vgroups&agrmid=';
 	
 	checkEmpty(prefs.login, 'Введите логин!');
 	checkEmpty(prefs.password, 'Введите пароль!');
@@ -24,7 +21,7 @@ function main() {
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('utf-8');
     
-    var html = AnyBalance.requestGet(baseurl);
+    var html = AnyBalance.requestGet(baseurl + 'lbweb-client');
     
     var csrfToken = getParam(html, null, null, /<input[^>]+value="([^"]*)[^>]+id="YII_CSRF_TOKEN"/i, replaceHtmlEntities);
     if(csrfToken){
@@ -39,7 +36,7 @@ function main() {
       'yt0': 'Войти'
     });
     
-    if (!/r=site\/logout/i.test(html)) {
+    if (!/lbweb-client\/site\/logout/i.test(html)) {
         var error = getElement(html, /<div[^>]+alert/i, replaceTagsAndSpaces);
         if (error)
           throw new AnyBalance.Error(error, null, /парол/i.test(error));
@@ -50,95 +47,99 @@ function main() {
 
 	var result = {success: true};
 
-  var priority = {
-    active: 0,
-    inactive: 1
-  };
+    var priority = {
+        active: 0,
+        inactive: 1
+    };
 
   //Вначале попытаемся найти интернет лиц. счет
-  var accTv = [],
-    accInet = [];
+    var accTv = [],
+        accInet = [];
+	
+    var accs = getElements(html, /<option[^>]+value[^>]*>/ig);
+	
+	AnyBalance.trace('Найдено счетов: ' + accs.length);
+	
+	for (var i = 0; i < accs.length; ++i) {
+		var selected = getParam(accs[i], null, null, /selected="selected"[^>]*>([\s\S]*?)<\/option>/i);
+		if (selected !== '') {
+			var account = selected;
+			var accountID = getParam(accs[i], null, null, /<option[^>]+value="([^"]*?)"/i);
+		}else{
+			AnyBalance.trace('Договор не выбран?\n' + html); 
+		}
+	}
+	
+	var balance = getParam(html, null, null, /Баланс[\s\S]*?<a style[^>]*>([\s\S]*?)<\/a>/i, null, parseBalance);
 
-  var table = getParam(html, null, null, /Номер договора[\s\S]*?<\/table>/i);
-  var accs = sumParam(html, null, null, /<tr[^>]*agreements[^>]*row(?:[^>]*>){10,20}\s*<\/tr>/ig);
-  AnyBalance.trace('Найдено счетов: ' + accs.length);
-
-  for (var i = 0; i < accs.length; ++i) {
-    var account = getParam(accs[i], null, null, [/<strong>\s*(\d+)/i, /<td[^>]+class="first_col"[^>]*>([\s\S]*?)<\/td>/i]);
-    var accountID = getParam(accs[i], null, null, /<tr[^>]*agreements[^>]*row[^>]*?(\d+)/i);
-    var balance = getParam(accs[i], null, null, /(-?[\s\d.,]+руб)/i, null, parseBalance);
-
-    if (!isset(account) || !isset(accountID)) {
-      AnyBalance.trace('Не удалось найти данные, проблемы на сайте?');
-      continue;
-    }
-
-    var xhtml = AnyBalance.requestGet(urlAjax + accountID);
+    var xhtml = AnyBalance.requestPost('https://lk.klimovsk.net/lbweb-client/account/vgroups', {
+		'agrmid': accountID,
+        'YII_CSRF_TOKEN': csrfToken
+	});
     var json = getJson(xhtml);
 
     // Может быть несколько услуг по одному счету
     AnyBalance.trace('Услуг по счету ' + account + ': ' + json.body.length);
 
     for (var j = 0; j < json.body.length; j++) {
-      var tarifdescr = json.body[j].tarifdescr; //Цифровое ТВ
+        var tarifdescr = json.body[j].tarifdescr; //Цифровое ТВ
 
-      if (typeof tarifdescr == 'object') {
-        tarifdescr = tarifdescr.descr;
-      }
+        if (typeof tarifdescr == 'object') {
+            tarifdescr = tarifdescr.descr;
+        }
 
-      var state = json.body[j].state.state + ''; //Состояние: активен, Недостаточно средств, Выключен, Действует
-      var services = json.body[j].services[0] + ''; //Нет подключенных услуг
+        var state = json.body[j].state.state + ''; //Состояние: активен, Недостаточно средств, Выключен, Действует
+        var services = json.body[j].services[0] + ''; //Нет подключенных услуг
 
-      var response = {
-        bal: balance,
-        abon: json.body[j].rent,
-        acc: account,
-        accId: accountID,
-        'tarifdescr': tarifdescr,
-        'state': state,
-        'services': services
-      };
-      var act = /Состояние:\s+актив|Действует/i.test(state) ? 'active' : 'inactive';
-      var pri = priority[act];
+        var response = {
+            bal: balance,
+            abon: json.body[j].rent,
+            acc: account,
+            accId: accountID,
+            'tarifdescr': tarifdescr,
+            'state': state,
+            'services': services
+        };
+        var act = /Состояние:\s+актив|Действует/i.test(state) ? 'active' : 'inactive';
+        var pri = priority[act];
       // Это ТВ
-      if (/\BТВ\B|Телевидение/.test(tarifdescr) && !/ШПД/.test(tarifdescr)) {
-        if (!isset(accTv[pri]))
-          accTv[pri] = response;
+        if (/\BТВ\B|Телевидение/.test(tarifdescr) && !/ШПД/.test(tarifdescr)) {
+            if (!isset(accTv[pri]))
+                accTv[pri] = response;
         // Это интернет
-      } else {
-        if (!isset(accInet[pri]))
-          accInet[pri] = response;
-      }
+        } else {
+            if (!isset(accInet[pri]))
+                accInet[pri] = response;
+        }
     }
-  }
 
-  var usedAccs = {}; //аккаунты только уникальные собираем
+    var usedAccs = {}; //аккаунты только уникальные собираем
 
-  function readAcc(json, isInet) {
-    if (json) {
-      getParam(json.bal, result, isInet ? 'balance' : 'balance_tv');
-      if (!usedAccs['acc_' + json.acc]) { //аккаунты только уникальные собираем
-        sumParam(json.acc, result, 'acc_num', null, replaceTagsAndSpaces, null, aggregate_join);
-        usedAccs['acc_' + json.acc] = true;
-      }
+    function readAcc(json, isInet) {
+        if (json) {
+            getParam(json.bal, result, isInet ? 'balance' : 'balance_tv');
+            if (!usedAccs['acc_' + json.acc]) { //аккаунты только уникальные собираем
+                sumParam(json.acc, result, 'acc_num', null, replaceTagsAndSpaces, null, aggregate_join);
+                usedAccs['acc_' + json.acc] = true;
+            }
 
-      if (!/Выключен/i.test(json.state) && !/не\s*доступно/i.test(json.services)) {
-        sumParam(json.abon, result, 'abon', null, null, parseBalance, aggregate_sum);
-        sumParam(json.tarifdescr, result, '__tariff', null, null, null, aggregate_join);
-      }
+            if (!/Выключен/i.test(json.state) && !/не\s*доступно/i.test(json.services)) {
+                sumParam(json.abon, result, 'abon', null, null, parseBalance, aggregate_sum);
+                sumParam(json.tarifdescr, result, '__tariff', null, replaceTagsAndSpaces, null, aggregate_join);
+            }
+        }
     }
-  }
 
-  function readAccByPriority(arr, isInet) {
-    for (var i = 0; i < arr.length; ++i)
-      if (arr[i])
-        return readAcc(arr[i], isInet);
-  }
+    function readAccByPriority(arr, isInet) {
+        for (var i = 0; i < arr.length; ++i)
+        if (arr[i])
+            return readAcc(arr[i], isInet);
+    }
 
-  readAccByPriority(accInet, true);
-  readAccByPriority(accTv);
+    readAccByPriority(accInet, true);
+    readAccByPriority(accTv);
 
-  getParam(html, result, 'fio', /<span[^>]+class="content-aside-name"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+    getParam(html, result, 'fio', /<div[^>]+class="name-person person"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
 
-  AnyBalance.setResult(result);
+    AnyBalance.setResult(result);
 }

e8fc535b6719ef3bd2201c1f6942ee4eb4cd5fb6
Dmitry Kochin <dco@mail.ru>
2022-07-08 19:08:37 +0300

    Lovit)) интернет провайдер (ab-internet-lovit): Committing changes

--- providers/ab-internet-lovit/main.js
+++ providers/ab-internet-lovit/main.js
@@ -1,78 +1,212 @@
-﻿
-/**
+﻿/**
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
-};
+    'Authorization': 'Bearer',
+    'Connection': 'Keep-Alive',
+    'Accept-Encoding': 'gzip',
+    'User-Agent': 'okhttp/3.12.0'
+}
+
+function saveTokens(json){
+	AnyBalance.setData('accessToken', json.access_token);
+//	AnyBalance.setData('refreshToken', json.refresh_token);
+    AnyBalance.setData('expiresIn', json.expires_in);
+	AnyBalance.setData('tokenType', json.token_type);
+	AnyBalance.saveData();
+}
+
+function callApi(verb, params){
+	var accessToken = AnyBalance.getData('accessToken');
+	var method = 'GET', headers = g_headers;
+	if(params){
+		method = 'POST';
+		if (!accessToken){
+			headers = addHeaders({'Authorization': 'Bearer', 'Content-Type': 'application/x-www-form-urlencoded'});
+		}else{
+		    headers = addHeaders({'Authorization': 'Bearer ' + accessToken, 'Content-Type': 'application/x-www-form-urlencoded'});
+		}
+	}else{
+		if (/preauth/i.test(verb)){
+			headers = addHeaders({'Authorization': 'Bearer'});
+		}else{
+		    headers = addHeaders({'Authorization': 'Bearer ' + accessToken});
+		}
+	}
+	
+	AnyBalance.trace('Запрос: ' + verb);
+	var html = AnyBalance.requestPost('https://mobile-api-v2.tut.net/api/v6/' + verb, params, headers, {HTTP_METHOD: method});
+	var json = getJson(html);
+	AnyBalance.trace('Ответ: ' + JSON.stringify(json));
+	if(json.error){
+		var error = json.message || json.error;
+		if(error && error === 'Unauthorised')
+			throw new AnyBalance.Error('Неверный логин или пароль!');
+		if(error)
+			throw new AnyBalance.Error(error, null, /логин|парол/i.test(error));
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+	}
+
+	return json;
+}
+
+function loginPure(action, params, method){
+	var prefs = AnyBalance.getPreferences(), json;
+	
+	var login = prefs.login.replace(/\D/g, '');
+	
+	if (/^\d{10}$/.test(login)){
+	    var json = callApi('preauth?username=7' + login + '&pass=' + prefs.password);
+	}else{
+		var json = callApi('preauth?username=' + login + '&pass=' + prefs.password);
+	}
+
+    if(!json || !json.ac){
+    	AnyBalance.trace(JSON.stringify(json));
+    	throw new AnyBalance.Error('Не удалось получить идентификатор пользователя. Сайт изменен?');
+    }
+
+	var userId = json.ac[0].id;
+	AnyBalance.trace('Идентификатор получен: ' + userId);
+	
+    var json = callApi('auth', {username: userId, password: prefs.password, grant_type: 'password'}, 'POST');
+
+    if(!json || !json.access_token){
+    	AnyBalance.trace(JSON.stringify(json));
+    	throw new AnyBalance.Error('Не удалось получить токен авторизации. Сайт изменен?');
+    }
+
+	var accessToken = json.access_token;
+	AnyBalance.trace('Токен получен: ' + accessToken);
+	
+	AnyBalance.setData('login', prefs.login);
+	AnyBalance.setData('pass', prefs.password);
+	AnyBalance.setData('userId', userId);
+	AnyBalance.saveData();
+	saveTokens(json);
+}
+
+function loginAccessToken(){
+	var prefs = AnyBalance.getPreferences();
+	var accessToken = AnyBalance.getData('accessToken');
+	var userId = AnyBalance.getData('userId');
+	try{
+	    AnyBalance.trace('Токен сохранен. Пробуем войти...');
+		var json = callApi('auth', {username: userId, password: prefs.password, grant_type: 'password'}, 'POST');
+		AnyBalance.trace('Успешно вошли по accessToken');
+		return true;
+	}catch(e){
+		AnyBalance.trace('Не удалось войти по accessToken: ' + e.message);
+		return false;
+	}
+}
+
+//function loginRefreshToken(){
+//	var prefs = AnyBalance.getPreferences();
+//	var refreshToken = AnyBalance.getData('refreshToken');
+//	try{
+//		AnyBalance.trace('Токен устарел. Пробуем обновить...');
+//		var json = callApi('connect/token', {grant_type: 'refresh_token', username: prefs.login, refresh_token: refreshToken, client_id: 'mobile-app.client'}, 'POST');
+//		AnyBalance.trace('Успешно вошли по refreshToken');
+//		saveTokens(json);
+//		return true;
+//	}catch(e){
+//		AnyBalance.trace('Не удалось войти по refreshToken: ' + e.message);
+//		saveTokens({});
+//		return false;
+//	}
+//}
+
+function loginToken(){
+	var prefs = AnyBalance.getPreferences();
+	
+	if(!AnyBalance.getData('accessToken')){
+		AnyBalance.trace('Токен не сохранен. Будем логиниться');
+		return false;
+	}
+	
+	if(AnyBalance.getData('accessToken') && (AnyBalance.getData('login') !== prefs.login)){
+		AnyBalance.trace('Токен соответствует другому логину');
+		return false;
+	}
+	
+	if(AnyBalance.getData('accessToken') && (AnyBalance.getData('pass') !== prefs.password)){
+		AnyBalance.trace('Токен соответствует другому паролю');
+		return false;
+	}
+
+	if(loginAccessToken()){
+		return true;
+	}else{
+		AnyBalance.trace('Токен устарел. Будем логиниться заново');
+		return loginPure();
+	}
+}
+
+function login(){
+	if(!loginToken()){
+		loginPure();
+	}
+}
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://lk.lovit.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 
-	AB.checkEmpty(prefs.login, 'Введите логин!');
-	AB.checkEmpty(prefs.password, 'Введите пароль!');
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
 
-	var html = AnyBalance.requestGet(baseurl + 'login', g_headers);
+	login();
 
-	if (!html || AnyBalance.getLastStatusCode() > 400) {
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Сайт провайдера временно недоступен! Попробуйте обновить данные позже.');
-	}
-
-	var form = AB.getElement(html, /<form[^>]+auth_form/i);
-	if(!form){
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удаётся найти форму входа! Сайт изменен?');
-	}
-
-	var params = AB.createFormParams(form, function(params, str, name, value) {
-		if (name == 'login') {
-			return prefs.login;
-		} else if (name == 'password') {
-			return prefs.password;
-		}
-
-		return value;
-	});
-
-	html = AnyBalance.requestPost(baseurl + 'login', params, AB.addHeaders({
-		Referer: baseurl + 'login'
-	}));
-
-	if (!/react-root/i.test(html)) {
-		AnyBalance.trace(html);
-		if(!/pass-field/i.test(html))
-			throw new AnyBalance.Error('Не удалось войти в личный кабинет. Неправильный логин или пароль?');
-
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-	}
-
-	var result = {
-		success: true
+	var result = {success: true};
+	
+	var json = callApi('data/clientinfo?password=' + prefs.password);
+	
+	var state = {
+		false: 'Активный',
+		true: 'Приостановлен'
 	};
-
-	html = AnyBalance.requestGet(baseurl + 'api/user/info', addHeaders({Referer: baseurl}));
-	var json = getJson(html);
-	if(json.status !== 'ok'){
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось получить информацию о балансе');
+	
+	if (json.data.balance) {
+		getParam(parseFloat(json.data.balance.toFixed(2)), result, 'balance', null, null, parseBalance);
 	}
-		
+	getParam(json.data.paymentInMonth, result, 'abon', null, null, parseBalance);
+	getParam(json.data.tarif, result, '__tariff');
+	getParam(state[json.data.freeze]||json.data.freeze, result, 'status');
+	getParam(json.data.ac_id, result, 'agreement');
+	getParam(json.data.address, result, 'address');
+	getParam(json.data.contact_phone.replace(/(.*)(\d{3})(\d{3})(\d\d)(\d\d)$/, '+7 $2 $3-$4-$5'), result, 'phone');
+	getParam(json.data.fio, result, 'fio');
+	
+	var date = getParam(json.data.datePayment, null, null, null, null, parseDate);
+	if (date) {
+		result.date_to_off = date;
+		var dt = new Date(date).getTime();
+		var days = Math.ceil((dt - (new Date().getTime())) / 86400 / 1000);
+		if (days >= 0) {
+			result.days_to_off = days;
+		} else {
+			AnyBalance.trace('Дата отключения уже наступила');
+			result.days_to_off = 0;
+		}
+	} else {
+ 		AnyBalance.trace('Не удалось получить дату отключения');
+ 	}
+	
+	var services = json.data.services;
+	if(services){
+		AnyBalance.trace('Найдено подключенных услуг: ' + services.length);
+		for(var i = 0; i<services.length; i++){
+			var sname = (i >= 1 ? 'servicename' + (i + 1) : 'servicename');
+	    	var sdesc = (i >= 1 ? 'servicedesc' + (i + 1) : 'servicedesc');
+		   	getParam(services[i].title, result, sname);
+			getParam(services[i].desc, result, sdesc);
+		}
+	}else{
+ 		AnyBalance.trace('Не удалось получить данные по услугам');
+ 	}
 
-	AB.getParam(json.data.balance, result, 'balance');
-	AB.getParam(+json.data.days_to_off, result, 'days_to_off');
-	AB.getParam(json.data.forecast_payment, result, 'abon');
-	AB.getParam(json.data.tariff_name, result, '__tariff');
-	AB.getParam(json.data.acc_status ? 'Активный' : 'Приостановлен', result, 'status');
-	AB.getParam(prefs.login, result, 'agreement');
-
-	AnyBalance.setResult(result);
+    AnyBalance.setResult(result);
 }

b3f618a8d5a048aeb4d51b672181bf562906898a
Dmitry Kochin <dco@mail.ru>
2022-10-29 10:27:36 +0300

    Яндекс.Директ (ab-adv-yandex-direct): Committing changes

--- modules/login/yandex/source/login.js
+++ modules/login/yandex/source/login.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
 Процедура входа в Яндекс. Унифицирована и выделена в отдельный файл для удобства встраивания
 */
 
@@ -38,7 +38,7 @@ function loginYandex(login, password, html, retpath, origin) {
 			Referer: referer
 		}));
 		const json = getJson(html);
-		if(json.status !== 'ok'){
+		if(json.status !== 'ok' && json.errors[0] !== 'captcha.required'){
 			AnyBalance.trace(html);
 			const error = json.errors.join('; ');
 			throw new AnyBalance.Error(message + ': ' + error, null, reFatal && reFatal.test(error));
@@ -61,75 +61,161 @@ function loginYandex(login, password, html, retpath, origin) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Этот логин не может быть авторизован (неверный логин)', null, true);
 	}
+	
 	const track_id = json.track_id;
+	
+	function checkCaptcha(){
+		AnyBalance.trace('Потребовался ввод текстовой капчи');
 
-	html = apiPost('registration-validations/auth/multi_step/commit_password', {
+		json = apiPost('registration-validations/textcaptcha', {
+			csrf_token: csrf,
+			track_id: track_id,
+			language: 'ru',
+			scale_factor: 3,
+		}, 'Запрос на отправку капчи не принят');
+
+		var captchaImg = AnyBalance.requestGet(json.image_url, g_headers);
+		var key = json.key;		
+		var captcha = AnyBalance.retrieveCode('В некоторых случаях Яндекс использует защиту от роботов в виде текстовой капчи.\nПожалуйста, введите символы с картинки', captchaImg, {/*inputType: 'number', */time: 180000});
+        
+		json = apiPost('registration-validations/checkHuman', {
+			csrf_token: csrf,
+			track_id: track_id,
+			answer: captcha,
+	    	key: key,
+	    }, 'Запрос на проверку ответа не принят');
+		
+	}
+
+	json = apiPost('registration-validations/auth/multi_step/commit_password', {
 		csrf_token: csrf,
 		track_id: track_id,
 		password: password,
 		retpath: retpath
 	}, 'Пароль не принят', /not_matched/);
+	
+	if(json.errors && json.errors[0] === 'captcha.required'){
+	    checkCaptcha();
+		
+		json = apiPost('registration-validations/auth/multi_step/commit_password', {
+	    	csrf_token: csrf,
+	    	track_id: track_id,
+	    	password: password,
+	    	retpath: retpath
+	    }, 'Пароль не принят', /not_matched/);
+		
+	}
+	
+	if(json.state === 'change_password'){
+		AnyBalance.trace('Потребовалась смена пароля');
+		throw new AnyBalance.Error('Яндекс требует сменить пароль. Пожалуйста, перейдите на страницу авторизации https://passport.yandex.ru/auth/welcome через браузер, смените пароль и введите новый пароль в настройки провайдера', null, true);
+	}
 
 	if(json.state === 'auth_challenge'){
 		AnyBalance.trace('Потребовалась дополнительная проверка входа');
 
-		html = apiPost('registration-validations/auth/challenge/submit', {
+		json = apiPost('registration-validations/auth/challenge/submit', {
 			csrf_token: csrf,
 			track_id: track_id,
-		}, 'Запрос проверки: ' + error);
+		}, 'Запрос на проверку входа не принят');
 
 		const challengeType = json.challenge.challengeType;
 
 		if(challengeType === 'phone_confirmation'){
 			AnyBalance.trace('Требуется подтверждение по телефону');
+			
 			const phoneId = json.challenge.phoneId;
 			const hint = json.challenge.hint;
 
-			html = apiPost('registration-validations/auth/validate_phone_by_id', {
+			json = apiPost('registration-validations/auth/validate_phone_by_id', {
 				csrf_token: csrf,
 				track_id: track_id,
 				phoneId: phoneId
 			}, 'Запрос на валидацию не принят');
 
-			if(!json.valid_for_flash_call){
+			if(json.valid_for_flash_call === false && json.valid_for_call === false){
+				AnyBalance.trace('Требуется подтверждение с помощью кода из SMS');
+				var confirm_method = 'by_sms';
+			}else if(json.valid_for_flash_call === true && json.valid_for_call === true){
+				AnyBalance.trace('Требуется подтверждение с помощью входящего звонка');
+				var confirm_method = 'by_flash_call';
+			}else if(!json.valid_for_flash_call && !json.valid_for_call){
 				AnyBalance.trace(html);
 				throw new AnyBalance.Error('Требуемое подтверждение не поддерживается');
 			}
 
-
-			html = apiPost('registration-validations/phone-confirm-code-submit', {
+			json = apiPost('registration-validations/phone-confirm-code-submit', {
 				csrf_token: csrf,
 				track_id: track_id,
 				phone_id: phoneId,
-				confirm_method: 'by_flash_call',
+				confirm_method: confirm_method,
 				isCodeWithFormat: true
-			}, 'Запрос на посылку кода не принят');
+			}, 'Запрос на отправку кода не принят');
 
-			const code = AnyBalance.retrieveCode('Пожалуйста, введите последние ' + json.code_length + ' цифры номер, который вам сейчас позвонит на ваш номер ' + json.number.masked_international);
+			if (json.global_sms_id){
+			    var code = AnyBalance.retrieveCode('Пожалуйста, введите код из ' + json.code_length + ' цифр из SMS, отправленного на ваш номер ' + json.number.masked_international, null, {inputType: 'number', time: 180000});
+			}else{
+				var code = AnyBalance.retrieveCode('Пожалуйста, введите последние ' + json.code_length + ' цифры номера телефона из звонка, поступившего на ваш номер ' + json.number.masked_international, null, {inputType: 'number', time: 180000});
+			}
 
-			html = apiPost('registration-validations/phone-confirm-code', {
+			json = apiPost('registration-validations/phone-confirm-code', {
 				csrf_token: csrf,
 				track_id: track_id,
-				code: code,
-			}, 'Подтверждение кода не удалось');
+				code: code
+			}, 'Запрос на подтверждение кода не принят');
 			
+			json = apiPost('registration-validations/auth/challenge/commit', {
+		    	csrf_token: csrf,
+		    	track_id: track_id,
+		    	challenge: challengeType,
+		    }, 'Ошибка подтверждения входа');
+			
+		}else if(challengeType === 'push_2fa'){
+			AnyBalance.trace('Требуется подтверждение через мобильный Яндекс');
+			
+            json = apiPost('registration-validations/auth/challenge/send_push', {
+				csrf_token: csrf,
+				track_id: track_id
+			}, 'Запрос на отправку уведомления не принят');
 
+			var answer = AnyBalance.retrieveCode('Пожалуйста, введите код из уведомления, отправленного в мобильное приложение Яндекс на вашем устройстве', null, {inputType: 'number', time: 180000});
+			
+			json = apiPost('registration-validations/auth/challenge/commit', {
+			    csrf_token: csrf,
+			    track_id: track_id,
+			    challenge: challengeType,
+				answer: answer
+		    }, 'Ошибка подтверждения входа');
+			
 		}else{
 			AnyBalance.trace(html);
-			throw new AnyBalance.Error('Challenge не поддерживется: ' + challengeType);
+			throw new AnyBalance.Error('Затребованный тип проверки не поддерживется: ' + challengeType);
 		}
 
-		html = AnyBalance.requestPost(baseurl + '/registration-validations/auth/challenge/commit', {
-			csrf_token: csrf,
-			track_id: track_id,
-			challenge: challengeType,
-		}, 'Ошибка подтверждения');
 	}
 
 	html = AnyBalance.requestGet(baseurl + '/auth/finish/?' + createUrlEncodedParams({
 		track_id: track_id,
 		retpath: retpath
 	}), addHeaders({Referer: referer}));
+	
+	if(/host/i.test(html)&&/goal/i.test(html)){	
+        var varIt = getJsonObject(html, /var\s*it\s*=\s*/);
+		varIt = JSON.stringify(varIt);
+			
+		var host = getParam(varIt, /"host":"([^"]*)/i, replaceHtmlEntities, decodeURIComponent);
+	    var goal = getParam(varIt, /"goal":"([^"]*)/i, replaceHtmlEntities, decodeURIComponent);
+    	var container = getParam(html, /element2\.value\s?=\s?'([^']*)/i, replaceHtmlEntities, decodeURIComponent);
+    		
+    	html = AnyBalance.requestPost(host, {'goal': goal, 'container': container}, addHeaders({
+	        'Content-Type': 'application/x-www-form-urlencoded',
+    		'Origin': 'https://sso.passport.yandex.ru',
+            'Referer': 'https://sso.passport.yandex.ru/'
+	    }));
+    }else{
+    	AnyBalance.trace(html);
+    	throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+    }
 
 	return html;
 }
--- providers/ab-adv-yandex-direct/main.js
+++ providers/ab-adv-yandex-direct/main.js
@@ -3,14 +3,33 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Origin': "https://passport.yandex.ru",
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.6,en;q=0.4',
 	'Cache-Control': 'max-age=0',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
 };
+
+var g_currency = {
+	RUB: '₽',
+	KZT: '₸',
+	BYN: 'Br',
+	USD: '$',
+	ILS: '₪',
+	AMD: '֏',
+	GEL: '₾',
+	MDL: 'лей',
+	AZN: '₼',
+	TJS: 'смн.',
+	KGS: 'сом',
+	CAD: 'C$',
+	undefined: ''
+};
+
+var baseurl = "https://direct.yandex.ru/";
+var g_savedData;
 
 function getIdKey(html){
     return getParam(html, null, null, /<input[^>]*name="idkey"[^>]*value="([^"]*)/i);
@@ -27,20 +46,59 @@ function main(){
 	if(prefs.cid && !/\d+/.test(prefs.cid))
         throw new AnyBalance.Error("Введите ID рекламной кампании, по которой вы хотите получить информацию. Он должен состоять только из цифр!");
 	
-	var baseurl = "https://passport.yandex.ru/passport?mode=auth";
-	var html = loginYandex(prefs.login, prefs.password);
+	if(!g_savedData)
+		g_savedData = new SavedData('direct', prefs.login);
+
+	g_savedData.restoreCookies();
 	
-    var yandexuid = getParam(html, null, null, /mode=logout&[^<]*\byu=(\d+)/);
+	var uLogin = g_savedData.get('uLogin');
+	var html = AnyBalance.requestGet(baseurl + 'wizard/overview/?ulogin=' + uLogin, g_headers);
+	
+	if (!html || AnyBalance.getLastStatusCode() > 400) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
+	}
+	
+	if(!/=logout/i.test(html)){
+		AnyBalance.trace('Сессия новая. Будем логиниться заново...');
+        clearAllCookies();
+    	var html = '';
+    	html = loginYandex(prefs.login, prefs.password, html, baseurl, 'direct');
+			
+		html = AnyBalance.requestGet(baseurl, g_headers);
+			
+        if(!/=logout/i.test(html)){
+    		AnyBalance.trace(html);
+    	    throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+    	}
+
+		var lastUrl = AnyBalance.getLastUrl();
+		
+		if(!/ulogin=/i.test(lastUrl)){
+			var uLogin = AnyBalance.getCookie('yandex_login');
+		    html = AnyBalance.requestGet(baseurl + 'wizard/overview/?ulogin=' + uLogin, g_headers);
+		}else{
+			var uLogin = getParam(lastUrl, null, null, /ulogin=([\s\S]*?)$/i, replaceTagsAndSpaces);
+		}
+			
+		g_savedData.set('uLogin', uLogin);
+		g_savedData.setCookies();
+	    g_savedData.save();
+	}else{
+		AnyBalance.trace('Сессия сохранена. Входим автоматически...');
+	}
+	
+    var yandexuid = getParam(html, null, null, /=logout&[^<]*\byu=(\d+)/);
     if(!yandexuid)
-        throw new AnyBalance.Error("Не удалось зайти. Проверьте логин и пароль.");
+        throw new AnyBalance.Error('Не удалось найти идентификатор сессии. Сайт изменен?');
 	
-    var result = {success: true};
+	var result = {success: true};
 	
     var jsonInfoStr = AnyBalance.requestGet('https://direct.yandex.ru/widget/export?yandexuid=' + yandexuid + '&cid=' + (prefs.cid || ''), g_headers);
 	if(/Сервис временно недоступен/.test(jsonInfoStr))
 		throw new AnyBalance.Error("Яндекс сообщает: Сервис временно недоступен");
 	
-	AnyBalance.trace('Got from yandex: ' + jsonInfoStr);
+	AnyBalance.trace('Обзор: ' + jsonInfoStr);
 	var jsonInfo = JSON.parse(jsonInfoStr);
 	
 	if(jsonInfo.no_campaigns)
@@ -50,6 +108,7 @@ function main(){
         throw new AnyBalance.Error('Нет активной рекламной кампании ID:' + prefs.cid);
 	
     var sum_rest = jsonInfo.sum_rest;
+	var currency_code = jsonInfo.currency_code;
     var active_camps_num = jsonInfo.active_camps_num;
     var active_camps_list = jsonInfo.camps_list || [];
     var camps_info = jsonInfo.camps_info || [];
@@ -59,8 +118,13 @@ function main(){
     var overdraft_debt = (overdraft && 1*overdraft.debt) || 0;
     var overdraft_pay_date = (overdraft && overdraft.pay_date) || '';
 	
-    if(AnyBalance.isAvailable('balance'))
+    result.currency = (g_currency[currency_code]) || currency_code;
+	
+	if(AnyBalance.isAvailable('balance'))
         result.balance = sum_rest;
+	
+	if(AnyBalance.isAvailable('currency_full'))
+        result.currency_full = currency_code;
 	
     if(has_overdraft){    
         if(AnyBalance.isAvailable('o_rest'))
@@ -98,6 +162,29 @@ function main(){
         if(AnyBalance.isAvailable('c_clicks'))
             result.c_clicks = camps_info.clicks_today;
     }
+	
+	var jsonInfoStr = AnyBalance.requestGet('https://api.passport.yandex.ru/all_accounts', g_headers);
+	
+	AnyBalance.trace('Профиль: ' + jsonInfoStr);
+	var jsonInfo = JSON.parse(jsonInfoStr);
+	
+	var email = jsonInfo.accounts[0].defaultEmail;
+	var login = jsonInfo.accounts[0].login;
+	var firstname = jsonInfo.accounts[0].displayName.firstname;
+	var lastname = jsonInfo.accounts[0].displayName.lastname;
+	var fio = firstname;
+	if(lastname)
+		fio += ' ' + lastname;
+	result.__tariff = fio;
+	
+	if(AnyBalance.isAvailable('fio'))
+	    result.fio = fio;
+	
+	if(AnyBalance.isAvailable('email'))
+	    result.email = email;
+	
+	if(AnyBalance.isAvailable('login'))
+	    result.login = login;
 	
     AnyBalance.setResult(result);
 }

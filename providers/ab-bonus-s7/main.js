7f71c85b2859237aa9282f7de4ba43620abceb03
Dmitry Kochin <dco@mail.ru>
2022-10-29 10:29:54 +0300

    S7 Приоритет (ab-bonus-s7): Committing changes

--- providers/ab-bonus-s7/main.js
+++ providers/ab-bonus-s7/main.js
@@ -1,41 +1,21 @@
 ﻿/**
  Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
- */
+*/ 
 
 var g_headers = {
-	accept: 'application/json',
-	authorization: 'Basic YW5kcm9pZDpUUFh3aFg0RnRYZUxTZXRkVjhURWE4NnBXOEE5aFZGdA==',
-	'x-language': 'en',
-	'x-platform': 'android',
-	'x-application-version': '4.3.4',
-	'x-device': '',
-	Connection: 'Keep-Alive',
-	'Accept-Encoding': 'gzip',
-	'User-Agent': 'okhttp/4.9.0',
-	ADRUM_1: 'isMobile:true',
-	ADRUM: 'isAjax:true'
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+	'Cache-Control': 'max-age=0',
+	'Connection': 'Keep-Alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36 OPR/85.0.4341.75'
 };
 
-function callApi(verb, params){
-	var method = 'GET', params_str = '', headers = g_headers;
-	if(params){
-		params_str = JSON.stringify(params);
-		method = 'POST';
-		headers = addHeaders({'Content-Type': 'application/json; charset=UTF-8'});
-	}
-	
-	var html = AnyBalance.requestPost('https://ps3.api.s7.ru/' + verb, params_str, headers, {HTTP_METHOD: method});
-	var json = getJson(html);
-	if(json.error){
-		var error = json.error.message || json.error;
-		if(error)
-			throw new AnyBalance.Error(error,null, /crede/i.test(error));
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-	}
-
-	return json;
-}
+var baseurl = 'https://www.s7.ru';
+var token;
+var resid;
+var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'];
+var replaceCard = [replaceTagsAndSpaces, /\D/g, '', /(.*)(\d\d\d)(\d\d\d)$/, '$1 $2 $3'];
 
 function generateUUID() {
 	function s4() {
@@ -44,78 +24,67 @@ function generateUUID() {
   	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
+function generateUUID1() {
+	function s4() {
+  		return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+	}
+  	return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+}
+
 function getDeviceId(){
 	var prefs = AnyBalance.getPreferences();
 	var id = hex_md5(prefs.login);
 	return id;
 }
 
-function loginPure(){
+function login(){
 	var prefs = AnyBalance.getPreferences(), json;
-	
-	var type = 'CARD';
-	if(/@/.test(prefs.login))
-		type = 'EMAIL';
-	else if(/^\+\d+$/.test(prefs.login))
-		type = 'PHONE';
 
-	var json = callApi('auth/api/profiles/tickets', {
-        "device": getDeviceId(),
+	html = AnyBalance.requestPost('https://myprofile.s7.ru/auth/auth/api/profiles/tickets', JSON.stringify({
         "id": prefs.login,
         "secret": prefs.password,
-//        "temporaryResource": "a52cdf6b-aba9-4a94-8e6b-8b01f4081f64",
-        "type": type
-    });
+        "temporaryResource": generateUUID(),
+        "device": getDeviceId()
+    }), addHeaders({
+		'accept': 'application/json, text/plain, */*',
+		'adrum': 'isAjax:true',
+		'content-type': 'application/json',
+        'origin': baseurl,
+        'referer': baseurl + '/',
+        'x-language': 'ru'
+	}));
+	
+	var json = getJson(html);
+		
+	if (json.error) {
+        if (json.error.code == 'invalid.credentials') {
+	    	var error = json.error.message;
+        	if (error) {
+	    		AnyBalance.trace(html);
+        		throw new AnyBalance.Error(error);	
+        	}
 
-  	g_headers['x-token'] = json.ticket.token;
-
-    AnyBalance.setData('login', prefs.login);
-    AnyBalance.setData('token', json.ticket.token);
-    AnyBalance.setData('resid', json.ticket.resourceId);
-    AnyBalance.saveData();
+        	AnyBalance.trace(html);
+        	throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+        }
+    }
+	
+	token = json.ticket.token;
+	resid = json.ticket.resourceId;
 }
 
-function setAuthHeader(at){
-	if(at)
-		g_headers.Authorization = (at.token_type || at.tokenType) + ' ' + (at.access_token || at.accessToken);
-	else
-		delete g_headers.Authorization;
-}
-
-function loginAccessToken(){
-	var token = AnyBalance.getData('token');
-	try{
-	    g_headers['x-token'] = token;
-		callApi('profiles/api/profiles/' + AnyBalance.getData('resid'));
-		AnyBalance.trace('Удалось войти по token');
-		return true;
-	}catch(e){
-		AnyBalance.trace('Не удалось войти по token: ' + e.message);
-	    delete g_headers['x-token'];
-		return false;
-	}
-}
-
-function loginToken(){
+function setCoockies() {
 	var prefs = AnyBalance.getPreferences();
-
-	if(!AnyBalance.getData('token') || !AnyBalance.getData('resid')){
-		AnyBalance.trace("Токен не сохранен");
-		return false;
-	}
-
-	if(prefs.login != AnyBalance.getData('login')){
-		AnyBalance.trace("Токен соответствует другому логину");
-		return false;
-	}
-
-	return loginAccessToken();
-}
-
-function login(){
-	if(!loginToken()){
-		loginPure();
-	}
+	if (/@/.test(prefs.login)) {
+		var ssdge = getDeviceId();
+	} else {
+	    var ssdge = generateUUID1();
+    }
+	
+    AnyBalance.setCookie('.s7.ru', 'ssdcp', token);
+	AnyBalance.setCookie('.s7.ru', 'userId', resid);
+	AnyBalance.setCookie('.s7.ru', 'ssdge', ssdge);
+	AnyBalance.setCookie('.s7.ru', 'profileId', resid);
 }
 
 function main() {
@@ -124,14 +93,30 @@ function main() {
 
     checkEmpty(prefs.login, 'Введите логин!');
     checkEmpty(prefs.password, 'Введите пароль!');
+	
+	AnyBalance.trace('Пробуем войти в личный кабинет...');
+	
+    var tries = 0;
+	do {
+		login();
+		setCoockies();
+		var html = AnyBalance.requestGet(baseurl + '/api/v1/profile/profileAccount', addHeaders({
+	    	'accept': 'application/json, text/plain, */*',
+	    	'adrum': 'isAjax:true',
+	    	'origin': baseurl,
+            'referer': baseurl + '/',
+	    	'upgrade-insecure-requests': '1'
+	    }));
+	
+	    var json = getJson(html);
+		json = json.c;
+	    AnyBalance.trace('Попытка ' + tries + ': ' + JSON.stringify(json));
+	} while (!json.memberId && ++ tries < 5);
 
-    g_headers['x-device'] = getDeviceId();
-
-    login();
-
-    var resid = AnyBalance.getData('resid');
-
-    var jsonLoyalty = callApi('loyalty/api/loyalties/' + resid);
+	if (!json.memberId) {
+		AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось получить данные профиля. Сайт изменен?');
+	}
 
     var cardLevels = {
         CLASSIC: 'Классическая',
@@ -140,25 +125,27 @@ function main() {
         PLATINUM: 'Платиновая'
     };
 
-    var result = {
-        success: true
-    };
+    var result = {success: true};
 
-    for(var i=0; i<jsonLoyalty.profile.balancesContainer.length; ++i){
-    	var b = jsonLoyalty.profile.balancesContainer[i];
-    	if(b.type === 'REDEMPTION')
-    		AB.getParam(b.value, result, 'balance');
-    	if(b.type === 'QUALIFYING')
-    		AB.getParam(b.value, result, 'qmiles');
-    	if(b.type === 'FLIGHTS')
-    		AB.getParam(b.value, result, 'flights');
-    }
-                      
-    AB.getParam(jsonLoyalty.profile.memberId, result, 'cardnum');
+//    for(var i=0; i<jsonLoyalty.profile.balancesContainer.length; ++i){
+//    	var b = jsonLoyalty.profile.balancesContainer[i];
+//    	if(b.type === 'REDEMPTION')
+//    		AB.getParam(jsonLoyalty.milesBalance, result, 'balance');
+//    	if(b.type === 'QUALIFYING')
+//    		AB.getParam(b.value, result, 'qmiles');
+//    	if(b.type === 'FLIGHTS')
+//    		AB.getParam(b.value, result, 'flights');
+//    }
+    AB.getParam(json.milesBalance, result, 'balance', null, null, parseBalance); 
+	AB.getParam(json.burningMiles, result, 'burning', null, null, parseBalance);
+    var cardNum = AB.getParam(json.memberId, result, 'cardnum', null, replaceCard);
+	AB.getParam(json.statusExpireDate, result, 'expDate', null, null, parseDate);
+	AB.getParam(json.phone.phoneString, result, 'phone', null, replaceNumber);
 
-    AB.getParam(jsonLoyalty.profile.names[0].firstName + ' ' + jsonLoyalty.profile.names[0].lastName, result, 'userName');
-    AB.getParam(cardLevels[jsonLoyalty.profile.eliteTier.level] || jsonLoyalty.profile.eliteTier.level, result, 'type');
-    AB.getParam(cardLevels[jsonLoyalty.profile.eliteTier.level] || jsonLoyalty.profile.eliteTier.level, result, '__tariff');
+    AB.getParam(json.firstName + ' ' + json.lastName, result, 'userName');
+    AB.getParam(cardLevels[json.cardLevel] || json.cardLevel, result, 'type');
+	AB.getParam(json.travelLevelStatus, result, 'status');
+    AB.getParam(cardNum + ' | ' + json.travelLevelStatus, result, '__tariff');
                         
     AnyBalance.setResult(result);
 }

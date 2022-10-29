36fadba2cccbd0cdf6f2581aac6444a747bfe6ca
Dmitry Kochin <dco@mail.ru>
2022-10-29 10:30:36 +0300

    Уральские авиалинии (ab-bonus-uralair): Committing changes

--- providers/ab-bonus-uralair/main.js
+++ providers/ab-bonus-uralair/main.js
@@ -3,48 +3,117 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.6,en;q=0.4',
+	'Cache-Control': 'no-cache',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
 };
+
+var baseurl = "https://www.uralairlines.ru";
+var g_savedData;
+var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'];
 
 function main(){
     var prefs = AnyBalance.getPreferences();
     AnyBalance.setDefaultCharset('windows-1251');
-	checkEmpty(prefs.login, 'Введите логин!');
-	checkEmpty(prefs.password, 'Введите пароль!');
 	
-    var baseurl = "https://loyalty.uralairlines.ru/";
-	
-	var html = AnyBalance.requestGet(baseurl + 'member/login', g_headers);
-	
-	var params = createFormParams(html, function(params, str, name, value) {
-		if (name == 'Username') 
-			return prefs.login;
-		else if (name == 'Password')
-			return prefs.password;
+    if(!g_savedData)
+		g_savedData = new SavedData('uralairlines', prefs.login);
 
-		return value;
-	});
+	g_savedData.restoreCookies();
+
+    AnyBalance.trace ('Пробуем войти в личный кабинет...');
 	
-    html = AnyBalance.requestPost(baseurl + 'member/login', params, addHeaders({Referer: baseurl + 'member/login'}));
+	var html = AnyBalance.requestGet(baseurl + '/', g_headers);
 	
-    if(!/>Выйти</.test(html)) {
-		var error = getParam(html, null, null, /id="ContentPlaceHolder\d+_error\d+"(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	if(AnyBalance.getLastStatusCode() >= 400){
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте ещё раз позже');
+    }
+	
+	if(/authorized=\"1\"/i.test(html)){
+		AnyBalance.trace('Сессия сохранена. Входим автоматически...');
+	}else{
+		AnyBalance.trace('Сессия новая. Будем логиниться заново...');
+		clearAllCookies();
+    	loginSite(prefs);
     }
 
     var result = {success: true};
-
-    getParam(html, result, 'balance', /<div[^>]+class="b-program_balance-name"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-    getParam(html, result, '__tariff', /<div[^>]+class="b-program_level-name"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'cardnum', /Номер карты:([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'fio', /<div[^>]+class="b-user_name"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-    getParam(html, result, 'city', /<div[^>]+class="b-user_city"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	var html = AnyBalance.requestGet(baseurl + '/ajax.php?component=auth_form&template=header&lang=ru&action=getInfo', addHeaders({
+		'Accept': 'application/json, text/plain, */*',
+		'Origin': baseurl,
+		'Referer': baseurl + '/'
+	}));
+    
+	var json = getJson(html);
+	AnyBalance.trace('Кабинет: ' + JSON.stringify(json));
+	
+	getParam(json.info.balance, result, 'balance', null, null, parseBalance);
+    getParam(json.info.card, result, 'cardnum');
+	getParam(json.info.program + ' | ' + json.info.level, result, '__tariff');
+	getParam(json.info.level, result, 'curr_level');
+	getParam(json.info.progress.level_points, result, 'on_level', null, null, parseBalance);
+	getParam(json.info.next_level, result, 'next_level');
+	getParam((json.info.progress.flight_level_up - json.info.progress.level_points), result, 'to_next_level', null, null, parseBalance);
+	getParam(json.info.firstname + ' ' + json.info.lastname, result, 'fio');
+	
+	var dt = new Date();
+	var html = AnyBalance.requestGet(baseurl + '/cabinet/profile/?ajax=profile&action=default&_=' + dt.getTime(), addHeaders({
+		'Accept': 'application/json, text/plain, */*',
+		'Referer': baseurl + '/cabinet/profile/'
+	}));
+    
+	var json = getJson(html);
+	AnyBalance.trace('Профиль: ' + JSON.stringify(json));
+	
+	getParam(json.info.address.City, result, 'city');
+	getParam(json.info.phone, result, 'phone', null, replaceNumber);
 
     AnyBalance.setResult(result);
+}
+
+function loginSite(prefs){
+	var prefs = AnyBalance.getPreferences();
+	
+	AnyBalance.setDefaultCharset('windows-1251');
+	checkEmpty(prefs.login, 'Введите логин!');
+	checkEmpty(prefs.password, 'Введите пароль!');
+	
+	html = AnyBalance.requestGet(baseurl + '/', g_headers);
+	
+    var captchaKey = getParam(html, null, null, /captcha-key="([^"]*)/i);
+	var captcha = solveRecaptcha('Пожалуйста, подтвердите, что вы не робот', AnyBalance.getLastUrl(), captchaKey, {USERAGENT: g_headers['User-Agent']});
+	
+	var html = AnyBalance.requestPost(baseurl + '/ajax.php?component=auth_form&template=header&lang=ru&action=auth', {
+		'username': prefs.login,
+        'password': prefs.password,
+		'my_computer': '0',
+		'g-recaptcha-response': captcha
+	}, addHeaders({
+		'Accept': 'application/json, text/plain, */*',
+		'Origin': baseurl,
+		'Referer': baseurl + '/',
+	}));
+    
+	var json = getJson(html);
+	AnyBalance.trace(JSON.stringify(json));
+	
+    if (json.status != 'success'){
+		var error = json.global_form.text;
+   	    if (error) {
+			AnyBalance.trace(html);
+       		throw new AnyBalance.Error(error);	
+       	}
+
+       	AnyBalance.trace(html);
+       	throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+    }
+	
+	g_savedData.setCookies();
+	g_savedData.save();
 }

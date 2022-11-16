9b5100ec202ad522951f4d9d386fcc75c3050d11
Dmitry Kochin <dco@mail.ru>
2022-11-16 10:28:14 +0300

    ТК Энергия (ab-transport-nrg-tk): Committing changes

--- providers/ab-transport-nrg-tk/main.js
+++ providers/ab-transport-nrg-tk/main.js
@@ -3,9 +3,12 @@
 */
 
 var g_headers = {
-	'Accept':'*/*',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36',
-	'Accept-Language': 'ru,en;q=0.8'
+	'Accept': 'application/json, text/javascript, */*; q=0.01',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.7,en;q=0.4',
+	'Connection': 'keep-alive',
+	'Origin': 'https://nrg-tk.ru',
+    'Referer': 'https://nrg-tk.ru/',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
 };
 
 function main() {
@@ -15,49 +18,40 @@ function main() {
 	checkEmpty(prefs.number, 'Введите номер накладной!');
 	
     var baseurl = 'https://nrg-tk.ru/client/tracking/';
-    var ajaxurl = 'https://clients.nrg-tk.ru/tracking';
-
-	AnyBalance.setCookie('nrg-tk.ru', 'DDOSEXPERT_COM_V3', 'c175d59221c53d61968148f9e5c2e939');
 
     var html = AnyBalance.requestGet(baseurl, g_headers);
-    var sitekey = getParam(html, /data-sitekey="([^"]*)/i, replaceHtmlEntities);
 
-    if(!sitekey){
-    	AnyBalance.trace(html);
-    	throw new AnyBalance.Error('Не удалось найти форму запроса. Сайт изменен?');
-    }
-
-    var response = solveRecaptcha('Пожалуйста, докажите, что вы не робот.', baseurl, sitekey);
-
-    html = AnyBalance.requestPost(ajaxurl, {
-    	docNum: prefs.number,
-    	'g-recaptcha-response': response,
-    	submit: ''
-    }, addHeaders({
-    	'X-Requested-With': 'XMLHttpRequest',
-    	Referer: baseurl
-
-    }));
+    html = AnyBalance.requestGet('https://api2.nrg-tk.ru/v2/sending/state/?docNum=' + prefs.number, g_headers);
 
     var json = getJson(html);
+	AnyBalance.trace('Info: ' + JSON.stringify(json));
 	
 	if(!json || json.code){
-		var error = (json && json.code);
+		var error = (json && json.message);
 		if (error)
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
+			throw new AnyBalance.Error(error, null, /not found/i.test(error));
 		
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Не удалось найти информацию. Сайт изменен?');
 	}
 	
     var result = {success: true};
+	
+	result.__tariff = prefs.number;
 
-    getParam(json.states[0].title + getState(json.states[0]), result, 'now');
+    getParam(json.states[0].movingDateFormatted, result, 'date', null, replaceTagsAndSpaces, parseDate);
+	getParam(json.states[0].movingDateFormatted, result, 'time', null, replaceTagsAndSpaces, parseDate);
+	getParam(json.states[0].subStateTitle, result, 'operation');
+	getParam(json.states[0].title, result, 'location');
+	getParam(json.states[0].title + getState(json.states[0]), result, 'now');
     getParam(json.cityTo.name, result, 'tocity');
     getParam(json.cityFrom.name, result, 'from');
-    getParam(json.places, result, 'sits');
-    getParam(json.weight, 'weight');
-    getParam(json.volume, result, 'volume');
+	getParam(json.priceTripType.title, result, 'triptype');
+    getParam(json.places, result, 'sits', null, replaceTagsAndSpaces, parseBalance);
+    getParam(json.weight, result, 'weight', null, replaceTagsAndSpaces, parseBalance);
+    getParam(json.volume, result, 'volume', null, replaceTagsAndSpaces, parseBalance);
+	var delivery = 'С ' + json.deliveryDateFromFormatted + ' по ' + json.deliveryDateToFormatted;
+	getParam(delivery + '', result, 'delivery');
 	
     AnyBalance.setResult(result);
 }
@@ -69,11 +63,12 @@ function getState(state){
             info = ' создана';
             break
         case 1:
-            info = ': ' + state.stateInfo.warehouse.title + ', адрес ' + state.stateInfo.warehouse.address + ', телефон ' + state.stateInfo.warehouse.phone;
+            info = ': ' + state.stateInfo.warehouse.title + ',<br>адрес: ' + state.stateInfo.warehouse.address + ',<br>телефон: ' + state.stateInfo.warehouse.phone;
             break
         case 2:
             var d = new Date(state.stateInfo.trip.date * 1000);
-            info = ': от ' + $.date(d) + ' ' + state.stateInfo.trip.cityFrom.name + '-' + state.stateInfo.trip.cityTo.name;
+			var dt = n2(d.getDate()) + '.' + n2(d.getMonth()+1) + '.' + d.getFullYear();
+            info = ': от ' + dt + ' ' + state.stateInfo.trip.cityFrom.name + ' -> ' + state.stateInfo.trip.cityTo.name;
             break
         case 3:
             info = '';
@@ -83,7 +78,8 @@ function getState(state){
             break
         case 20:
             var d = new Date(state.stateInfo.issued.issueDate * 1000);
-            info = ': ' + (state.stateInfo.issued.issueDateFormatted).slice(0,-9) + ', на складе "' + state.stateInfo.issued.warehouse.title + '"';
+			var dt = n2(d.getDate()) + '.' + n2(d.getMonth()+1) + '.' + d.getFullYear();
+            info = ': ' + (state.stateInfo.issued.issueDateFormatted).slice(0,-9) + ',<br>на складе "' + state.stateInfo.issued.warehouse.title + '"';
             break
         default:
             info = ': информация отсутствует. За информацией обратитесь к операторам по номеру 8-800-700-7000';

2ce0669eb9c0dfaab84ef1e5f31a2ab70cf7a17d
Dmitry Kochin <dco@mail.ru>
2022-11-16 10:24:04 +0300

    WebMoney (ab-money-wm): Committing changes

--- providers/ab-money-wm/main.js
+++ providers/ab-money-wm/main.js
@@ -18,7 +18,7 @@ var g_headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
 };
 function delCookies(){
-	AnyBalance.trace('Удалены данные предыдущей сесии.');
+	AnyBalance.trace('Удалены данные предыдущей сесии');
 	AB.clearAllCookies();
 	AnyBalance.clearData();
 	AnyBalance.saveData();
@@ -31,7 +31,7 @@ function isLoged(){
 		Origin: 'https://mini.webmoney.ru',
 		Referer: ref
 	}));
-	info = getJson(info);	
+	info = getJson(info);
 	AnyBalance.trace(info.loggedOn?'Авторизованы для WMID '+info.wmId:'Не авторизованы');
 	return info.loggedOn;
 }
@@ -41,13 +41,12 @@ function handleRedirect(html){
 		var prefs = AnyBalance.getPreferences();
 		AnyBalance.trace('Доп. форма переадресации перед продолжением...');
 		var ref = AnyBalance.getLastUrl();
-
 		var action = getParam(form, /<form[^>]+action="([^"]*)/i, replaceHtmlEntities);
 		var params = createFormParams(form);
 		params.fid = hex_md5(prefs.login);
 		var delay = getParam(form, /<form[^>]+data-submit-delay="([^"]*)/i, replaceHtmlEntities, parseBalance) || 0;
 		if(delay > 0){
-			AnyBalance.trace('Необходимо подождать ' + delay + ' милисек');
+			AnyBalance.trace('Необходимо подождать ' + delay + ' мсек');
 			AnyBalance.sleep(delay);
 		}
 		html = AnyBalance.requestPost(joinUrl(ref, action), params, addHeaders({Referer: ref}));
@@ -60,6 +59,17 @@ function handleRedirect(html){
 		html = AnyBalance.requestPost(joinUrl(ref, action), params, addHeaders({Referer: ref}));
 		ref = AnyBalance.getLastUrl();
 	}
+	if(/\/authorize/i.test(html)){
+		AnyBalance.trace('Требуется дологиниться');
+		ref = AnyBalance.getLastUrl();
+		form = getElement(html, /<form[^>]+form/i);
+		if(form){
+		    params = createFormParams(form);
+			action = getParam(form, null, null, /<form[^>]+action="([^"]*)/i, replaceHtmlEntities);
+			html = AnyBalance.requestPost(joinUrl(ref, action), params, addHeaders({Referer: ref}));
+			ref = AnyBalance.getLastUrl();
+		}
+	}
 	return html;
 }
 
@@ -71,7 +81,7 @@ function patchRequests(){
 	function req(func, url, args){
 		args[0] = url.replace(/\s/g, '%20');
 		if(url != args[0]){
-			AnyBalance.trace('Invalid url detected, patched: ' + url);
+			AnyBalance.trace('Неверная ссылка, исправляем: ' + url);
 		}
 		return func.apply(AnyBalance, args);
 	}
@@ -103,10 +113,10 @@ function main(){
 		var html = AnyBalance.requestGet(baseurl+'finances', g_headers);
 		token=getParam(html , /__RequestVerificationToken[\s\S]*?value="([\s\S]*?)"/);
 	}catch(e){
-		delCookies
+		delCookies;
 		if (e.message.indexOf('ProtocolException: Invalid redirect URI:')>-1) throw new AnyBalance.Error('Необходима повторная авторизация',true);
-                if (e.message.indexOf('NetworkError: Failed to execute')>-1) throw new AnyBalance.Error(baseurl+' временно недоступен! Попробуйте обновить данные позже.');
-                AnyBalance.Error('Ошибка при обращении к '+baseurl);
+            if (e.message.indexOf('NetworkError: Failed to execute')>-1) throw new AnyBalance.Error(baseurl+' временно недоступен! Попробуйте обновить данные позже.');
+            AnyBalance.Error('Ошибка при обращении к '+baseurl);
 		AnyBalance.trace(e.message);
 
 	}
@@ -118,45 +128,51 @@ function main(){
 
 	var elements;
         if(isLoged() && token){
-		var fns = AnyBalance.requestGet(baseurl + 'srv/finance/purses/', addHeaders({
+		var fns = AnyBalance.requestGet(baseurl + 'api/finance/purses/', addHeaders({
 			Accept: 'application/json, text/plain, */*',
 			Referer: baseurl + 'finances',
-                        'X-XSRF-Token': token
+           'X-XSRF-Token': token
 		}));
 		try{
 			elements = getJson(fns);
 			if(/denied/i.test(elements.Message)) throw new AnyBalance.Error(elements.Message);
 			AnyBalance.trace('Удалось войти в предыдущей сессии');
 		}catch(e){
-			AnyBalance.trace('test of login failed, should relogin: ' + e.message);
-                        elements=false;
+			AnyBalance.trace('Не удалось проверить авторизацию, надо перелогиниваться: ' + e.message);
+            elements=false;
 			html = AnyBalance.requestGet(baseurl, g_headers);
 		}
 	}
 	
-	if(/logOnUrl/i.test(html)){
+	if(/isLogon/i.test(html)||!/__RequestVerificationToken/i.test(html)){
 		AnyBalance.trace('Мгновенно не зашли');
-                //AnyBalance.trace(html);
-		var logOnUrl = getParam(html, /logOnUrl="([\s\S]*?)"/i, replaceSlashes);
-		html=AnyBalance.requestGet(logOnUrl, g_headers);
+        //AnyBalance.trace(html);
+		html = AnyBalance.requestGet('https://www.webmoney.ru/', g_headers);
+		ref = getParam(html, null, null, /<a[^>]+class="button sign-in success" target="_self" href="([\s\S]*?)">/i, replaceHtmlEntities);
+		AnyBalance.trace('Ссылка на вход: ' + ref);
+		html = AnyBalance.requestGet(joinUrl(baseurlLogin, ref), addHeaders({Referer: baseurl + 'welcome.aspx?ReturnUrl=%2f'}));
+		//var logOnUrl = getParam(html, /logOnUrl="([\s\S]*?)"/i, replaceSlashes);
+		//html=AnyBalance.requestGet(logOnUrl, g_headers);
 		html = handleRedirect(html);
 		if (isLoged()){
-		     	token=getParam(html , /__RequestVerificationToken[\s\S]*?value="([\s\S]*?)"/);
-                        if (token) AnyBalance.trace('Авторизация восстановлена');
-                }else{
-		        AnyBalance.trace('Автовход не удался, пробуем всё заново авторизовывать');
-			ref = getParam(html, null, null, /action="([\s\S]*?)"/i, replaceHtmlEntities);
+		    token=getParam(html , /__RequestVerificationToken[\s\S]*?value="([\s\S]*?)"/);
+            if (token)
+				AnyBalance.trace('Авторизация восстановлена');
+        }else{
+		    AnyBalance.trace('Автовход не удался, пробуем всё заново авторизовывать');
+			html = AnyBalance.requestGet('https://www.webmoney.ru/', g_headers);
+			ref = getParam(html, null, null, /<a[^>]+class="button sign-in success" target="_self" href="([\s\S]*?)">/i, replaceHtmlEntities);
 			AnyBalance.trace('Ссылка на вход: ' + ref);
-	                if (ref){
-			html = AnyBalance.requestGet(joinUrl(baseurlLogin, ref), addHeaders({Referer: baseurl + 'welcome.aspx?ReturnUrl=%2f'}));
-			html = handleRedirect(html);
-			ref = AnyBalance.getLastUrl();
-	                }
+	        if (ref){
+			    html = AnyBalance.requestGet(joinUrl(baseurlLogin, ref), addHeaders({Referer: baseurl + 'welcome.aspx?ReturnUrl=%2f'}));
+			    html = handleRedirect(html);
+			    ref = AnyBalance.getLastUrl();
+	        }
 			var form = AB.getElement(html, /<form[^>]+password[^>]*>/i);
 			if(!form){
 				AnyBalance.trace(html);
 				delCookies;
-				throw new AnyBalance.Error('Не удаётся найти форму входа! Сайт изменен?');
+				throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
 			}
 	        
 			var params = AB.createFormParams(form, function(params, str, name, value) {
@@ -171,8 +187,8 @@ function main(){
 	        
 					if(!imgUrl){
 						AnyBalance.trace(html);
-                                                delCookies;
-						throw new AnyBalance.Error('Не удаётся найти капчу. Сайт изменен?');
+                        delCookies;
+						throw new AnyBalance.Error('Не удалось найти капчу. Сайт изменен?');
 					}
 					var img = AnyBalance.requestGet(joinUrl(baseurlLogin, imgUrl), addHeaders({Referer: ref}));
 					return AnyBalance.retrieveCode("Пожалуйста, введите число с картинки", img, {
@@ -186,7 +202,7 @@ function main(){
 				return value;
 			});
 			var action = getParam(form, null, null, /<form[\s\S]*?action=\"([\s\S]*?)\"/i, replaceHtmlEntities);
-                        //params.fid = hex_md5(prefs.login); //Теперь требуется фингерпринт передавать
+            //params.fid = hex_md5(prefs.login); //Теперь требуется фингерпринт передавать
 			html = AnyBalance.requestPost(joinUrl(baseurlLogin, action), params, AB.addHeaders({
 				Referer: ref
 			}));
@@ -197,7 +213,7 @@ function main(){
 				action = getParam(html, null, null, /<form[^>]+action="([^"]*)/i, replaceHtmlEntities);
 	        
 				var auth_options = getElements(html, /<li[^>]+auth-option[\s"]/ig);
-				AnyBalance.trace('Найдено ' + auth_options.length + ' вариантов подтверждения');
+				AnyBalance.trace('Найдено вариантов подтверждения: ' + auth_options.length);
 				for(var i=0; i<auth_options.length; ++i){
 					var o = auth_options[i];
 					var name = getElement(o, /<b/i, replaceTagsAndSpaces);
@@ -205,7 +221,8 @@ function main(){
 	        
 					AnyBalance.trace('Опция ' + name + (available ? ' доступна' : ' недоступна'));
 	        
-					if(name == 'SMS' && available){
+					//if(name == 'SMS' && available){
+					if(name == 'Подтверждение по номеру телефона' && available){
 						html = AnyBalance.requestPost(joinUrl(ref, action), {Command: 'Sms'}, addHeaders({Referer: ref}));
 						ref = AnyBalance.getLastUrl();
 					   	break;
@@ -217,18 +234,18 @@ function main(){
 						ref = AnyBalance.getLastUrl();
 					   	break;
 					}
-
 				}
 	        
 				if(i >= auth_options.length){
 					AnyBalance.trace(html);
-                                        delCookies;
-					throw new AnyBalance.Error('Не удалось найти поддерживаемой опции для подтверждения входа. Сайт изменен?');
+                    delCookies;
+					throw new AnyBalance.Error('Не удалось найти доступной опции для подтверждения входа. Сайт изменен?');
 				}
 			}
 
 			if(/\bSms\b/i.test(ref)){
-				AnyBalance.trace('Требуется SMS подтверждение на вход');
+				//AnyBalance.trace('Требуется SMS подтверждение на вход');
+				AnyBalance.trace('Требуется подтверждение по номеру телефона на вход');
 
 				action = getParam(html, null, null, /<form[^>]+action="([^"]*)/i, replaceHtmlEntities);
 				params = AB.createFormParams(html);
@@ -236,14 +253,17 @@ function main(){
 				if(!params.Challenge){
 					var error = getElement(html, /<[^>]+login-global-error/i, replaceTagsAndSpaces);
 					if(error)
-						 throw new AnyBalance.Error(error);
+						throw new AnyBalance.Error(error);
 					AnyBalance.trace(html);
-                                        delCookies;
-					throw new AnyBalance.Error('Не удалось перейти к подтверждению входа по SMS. Сайт изменен?');
+                    delCookies;
+					//throw new AnyBalance.Error('Не удалось перейти к подтверждению входа по SMS. Сайт изменен?');
+					throw new AnyBalance.Error('Не удалось перейти к подтверждению входа по номеру телефона. Сайт изменен?');
 				}
 				
-				params.Answer = AnyBalance.retrieveCode('Для входа в кошелек, пожалуйста, введите код из SMS, посланной на номер ' + 
-					params.PhoneNumber + ' (сессия ' + params.Challenge + ')', null, {inputType: 'number', minLength: 5, maxLength: 5, time: 180000});
+				//params.Answer = AnyBalance.retrieveCode('Для входа в кошелек, пожалуйста, введите код из SMS, посланной на номер ' + 
+				//params.PhoneNumber + ' (сессия ' + params.Challenge + ')', null, {inputType: 'number', minLength: 5, maxLength: 5, time: 180000});
+                params.Answer = AnyBalance.retrieveCode('Пожалуйста, введите последние 4 цифры номера телефона из звонка, поступившего на номер ' + 
+				params.PhoneNumber + ' (сессия ' + params.Challenge + ')', null, {inputType: 'number', minLength: 4, maxLength: 4, time: 180000});
 	        
 				html = AnyBalance.requestPost(joinUrl(ref, action), params, addHeaders({Referer: ref}));
 			}else if(/\bEnum\b/i.test(ref)){
@@ -256,33 +276,33 @@ function main(){
 				if(!params.Challenge){
 					var error = getElement(html, /<[^>]+login-global-error/i, replaceTagsAndSpaces);
 					if(error)
-						 throw new AnyBalance.Error(error);
+						throw new AnyBalance.Error(error);
 					AnyBalance.trace(html);
-                                        delCookies;
-					throw new AnyBalance.Error('Не удалось перейти к подтверждению входа по ENUM. Сайт изменен?');
+                    delCookies;
+					throw new AnyBalance.Error('Не удалось перейти к подтверждению входа по E-NUM. Сайт изменен?');
 				}
 				
-				params.Answer = AnyBalance.retrieveCode('Для входа в кошелек, пожалуйста, введите число-ответ из приложения E-NUM с логином ' + 
-					params.EnumId + '. Число-вопрос для ввода в приложение Е-NUM: ' + params.Challenge, null, {inputType: 'number', minLength: 7, maxLength: 7, time: 180000});
+				params.Answer = AnyBalance.retrieveCode('Пожалуйста, введите число-ответ из приложения E-NUM с логином ' + 
+				params.EnumId + '. Число-вопрос для ввода в приложение Е-NUM: ' + params.Challenge, null, {inputType: 'number', minLength: 7, maxLength: 7, time: 180000});
 	        
 				html = AnyBalance.requestPost(joinUrl(ref, action), params, addHeaders({Referer: ref}));
+			}else if(/\bCompleted\b/i.test(ref)){
+				AnyBalance.trace('Подтверждение на вход не требуется');
 			}else{
-				AnyBalance.trace('Не поддерживаемый способ подтверждения: ' + ref);
-		   
-		       }
+				AnyBalance.trace('Неподдерживаемый способ подтверждения: ' + ref);
+			}
 		}
 	    
 		ref = AnyBalance.getLastUrl();
 		if(!/Completed|init=true/i.test(ref)&&!token){
 			delCookies;
 			var error = getElement(html, /<span[^>]+field-validation-error/i, replaceTagsAndSpaces);
-			if (!error)  error = getElement(html, /<[^>]+login-global-error/i, replaceTagsAndSpaces);
-			if(error)
+			if (!error)
+				error = getElement(html, /<[^>]+login-global-error/i, replaceTagsAndSpaces);
+			if (error)
 				throw new AnyBalance.Error(error, null, /парол|Пользовател/i.test(error));
 			AnyBalance.trace(ref + '\n' + html);
 			throw new AnyBalance.Error('Не удалось войти в кошелек. Сайт изменен?');
-
-
 		}
 
 		AnyBalance.trace('Успешно авторизовались');
@@ -303,11 +323,21 @@ function main(){
 			html = AnyBalance.requestPost(joinUrl(ref, action), params, addHeaders({Referer: ref}));
 			ref = AnyBalance.getLastUrl();
 		}
-	
-		if (/logOnUrl/i.test(html)) {
+		if(/\/authorize/i.test(html)){
+		    AnyBalance.trace('Требуется дологиниться');
+		    form = getElement(html, /<form[^>]+form/i);
+		    if(form){
+		    	params = createFormParams(form);
+		    	action = getParam(form, null, null, /<form[^>]+action="([^"]*)/i, replaceHtmlEntities);
+		    	html = AnyBalance.requestPost(joinUrl(ref, action), params, addHeaders({Referer: ref}));
+		    	ref = AnyBalance.getLastUrl();
+		    }
+		}
+		
+		if (/logOnUrl/i.test(html)&&!/__RequestVerificationToken/i.test(html)) {
 			AnyBalance.trace(html);
-                        delCookies;
-			throw new AnyBalance.Error('Не удалось зайти в кошелек после успешной авторизации. Сайт изменен?');
+            delCookies;
+			throw new AnyBalance.Error('Не удалось войти в кошелек после успешной авторизации. Сайт изменен?');
 		}
 	    
 		AnyBalance.trace('Успешно вошли');
@@ -316,15 +346,13 @@ function main(){
 		__setLoginSuccessful();
 	}
 
-	var result = {
-		success: true
-	};
+	var result = {success: true};
 
 	if(!elements){
-		var fns = AnyBalance.requestGet(baseurl + 'srv/finance/purses/', addHeaders({
+		var fns = AnyBalance.requestGet(baseurl + 'api/finance/purses/', addHeaders({
 			Accept: 'application/json, text/plain, */*',
 			Referer: baseurl + 'finances',
-                        'X-XSRF-Token': token
+            'X-XSRF-Token': token
 		}));
 		try{
 			elements = getJson(fns);
@@ -333,14 +361,14 @@ function main(){
 		}catch(e){
 			AnyBalance.trace('Вход не удался: ' + e.message);
 			delCookies;
-                        throw new AnyBalance.Error(elements.Message)
+            throw new AnyBalance.Error(elements.Message)
 		}
 	}
 
 		AnyBalance.saveCookies();
 		AnyBalance.saveData();
 
-	AnyBalance.trace('Найдено ' + elements.length + ' кошельков');
+	AnyBalance.trace('Найдено кошельков: ' + elements.length);
 	for(var i=0; i<elements.length; ++i){
 		var e = elements[i];
 		var num = e.number;
@@ -352,15 +380,38 @@ function main(){
 		sumParam(num, result, curr.toLowerCase() + '_num', null, null, null, aggregate_join);
 	}
 
-	html = AnyBalance.requestGet(baseurl + 'srv/profile/info', addHeaders({
+	html = AnyBalance.requestGet(baseurl + 'api/profile', addHeaders({
 		Accept: 'application/json, text/plain, */*',
 		Referer: baseurl + 'finances',
-               'X-XSRF-Token': token
+        'X-XSRF-Token': token
 	}));
 
 	var json = getJson(html);
     getParam(json.wmid, result, '__tariff');
+	getParam(json.wmid, result, 'wmid');
+	getParam(json.passport.desc, result, 'status');
+	getParam(json.nick, result, 'nickname');
+	getParam(json.email, result, 'email');
+	getParam(json.phoneNumberMasked, result, 'phone');
     getParam(json.fullName, result, 'fio');
+	
+	if (AnyBalance.isAvailable('chats')) {
+	    html = AnyBalance.requestGet(baseurl + 'api/chats/', addHeaders({
+	    	Accept: 'application/json, text/plain, */*',
+	    	Referer: baseurl + 'finances',
+            'X-XSRF-Token': token
+	    }));
+
+	    var json = getJson(html);
+		if (json.length && json.length>0){
+		    for (var i=0; i<json.length; i++) {
+            var chat = json[i];
+            sumParam(chat.unreadCount, result, 'chats', null, null, null, aggregate_sum);
+            }
+	    }else{
+			AnyBalance.trace('Не удалось найти информацию о сообщениях');
+		}
+	}
 
     AnyBalance.setResult(result);
 }

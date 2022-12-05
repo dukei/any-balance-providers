4a053a3898d18550b9cfff5dd6bf2ce575b0ce59
Dmitry Kochin <dco@mail.ru>
2022-12-05 12:00:02 +0300

    Привет, Мир! (ab-bonus-privetmir): Committing changes

--- providers/ab-bonus-privetmir/main.js
+++ providers/ab-bonus-privetmir/main.js
@@ -5,16 +5,15 @@
 var g_headers = {
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Encoding': 'gzip, deflate, br',
 	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.6',
 	'Cache-Control': 'max-age=0',
 	'Connection': 'keep-alive',
+	'Upgrade-Insecure-Requests': '1',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'
 };
 
 var baseurl = "https://privetmir.ru";
 var baseurlApi = "https://api-user.privetmir.ru/api";
-var g_token;
 var g_savedData;
 var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'];
 
@@ -37,25 +36,59 @@ function main(){
         throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте ещё раз позже');
     }
 	
-	if(/\/?exit=yes/i.test(html)){
-		AnyBalance.trace('Сессия сохранена. Входим автоматически...');
+	if(!/\/?exit=yes/i.test(html)){
+		var refreshToken = g_savedData.get('refreshToken');
+		if(refreshToken){
+			AnyBalance.trace('Сессия устарела. Пробуем обновить...');
+			
+			AnyBalance.setCookie('.privetmir.ru', 'BITRIX_SM_IS_PREMIUM', 'N');
+	        AnyBalance.setCookie('.privetmir.ru', 'refreshToken', refreshToken + '');
+	
+	        html = AnyBalance.requestGet(baseurl + '/personal/cashback/', g_headers);
+			
+			if(!/\/?exit=yes/i.test(html)){
+				AnyBalance.trace('Не удалось обновить сессию. Будем логиниться заново...');
+			    clearAllCookies();
+    	        loginSite(prefs);
+			}else{
+				AnyBalance.trace('Сессия успешно обновлена');
+	            g_savedData.set('hashQuery', AnyBalance.getCookie('hashQuery'));
+	            g_savedData.set('accessToken', AnyBalance.getCookie('accessToken'));
+	            g_savedData.set('refreshToken', AnyBalance.getCookie('refreshToken'));
+	            g_savedData.setCookies();
+	            g_savedData.save();
+			}
+		}else{
+		    AnyBalance.trace('Сессия новая. Будем логиниться заново...');
+		    clearAllCookies();
+    	    loginSite(prefs);
+		}
 	}else{
-		AnyBalance.trace('Сессия новая. Будем логиниться заново...');
-		clearAllCookies();
-    	loginSite(prefs);
+		AnyBalance.trace('Сессия сохранена. Входим автоматически...');
+		g_savedData.set('accessToken', AnyBalance.getCookie('accessToken'));
+	    g_savedData.setCookies();
+	    g_savedData.save();
     }
 
     var result = {success: true};
 	
-	var html = AnyBalance.requestGet(baseurl + '/personal/cashback/', g_headers);
+	var accessToken = g_savedData.get('accessToken');
+	
+	html = AnyBalance.requestGet(baseurl + '/personal/cashback/', g_headers);
 	
 	var dataUrl = getParam(html, /<div[^>]+id="lk"[^>]+data-url="([^"]+)"><\/div>/i, replaceHtmlEntities);
 	
+	if (!dataUrl){
+       	AnyBalance.trace(html);
+       	throw new AnyBalance.Error('Не удалось найти ссылку для перехода. Сайт изменён?');
+    }
+	
 	html = AnyBalance.requestGet(dataUrl, addHeaders({
 		'Accept': 'application/json, text/plain, */*',
+		'Authorization': 'Bearer ' + accessToken,
 		'Origin': baseurl,
 		'Referer': baseurl + '/'
-	    }));
+	}));
 	
 	var json = getJson(html);
 
@@ -137,7 +170,7 @@ function loginSite(prefs){
 		var captcha = solveRecaptcha('Пожалуйста, докажите, что вы не робот', AnyBalance.getLastUrl(), JSON.stringify({SITEKEY: recaptcha.key, TYPE: 'V3', ACTION: 'login', USERAGENT: g_headers['User-Agent']}));
 	}
 	
-	var html = AnyBalance.requestPost(baseurlApi + '/auth', {
+	html = AnyBalance.requestPost(baseurlApi + '/auth', {
 		login: formattedLogin,
         pass: prefs.password,
 		rememberMe: '',
@@ -153,18 +186,20 @@ function loginSite(prefs){
 	AnyBalance.trace(JSON.stringify(json));
 
     if (json.success != true){
-		var error = json.data.errorMessage;
+		var error = json.data.errorMessage || json.data.errorCaptcha;
     	if (error) {
 			AnyBalance.trace(html);
-       		throw new AnyBalance.Error(error);	
+       		throw new AnyBalance.Error(error, null, /телефон|парол/i.test(error));	
        	}
 
        	AnyBalance.trace(html);
        	throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменён?');
     }
+    
+	g_savedData.set('hashQuery', json.data.hash);
+	g_savedData.set('accessToken', json.data.accessToken);
+	g_savedData.set('refreshToken', json.data.refreshToken);
 	
-	var fingerprint = json.data.fingerprint;
-	var g_token = json.data.token;
 	g_savedData.setCookies();
 	g_savedData.save();
 }

7c60c521be8620c089d76418411c99b8b42ea0dc
Dmitry Kochin <dco@mail.ru>
2023-02-13 11:56:01 +0300

    Avon (ab-affiliate-avon): Committing changes

--- providers/ab-affiliate-avon/main.js
+++ providers/ab-affiliate-avon/main.js
@@ -8,11 +8,13 @@ Operator site: http://www.avon.ru
 */
 
 var g_headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+	'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
+	'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 };
 
 var g_currency = {
@@ -22,11 +24,12 @@ var g_currency = {
 	'EUR': '€'
 };
 
+var baseurl = "https://www.avon.ru/";
 var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'];
 
 function main(){
-    var prefs   = AnyBalance.getPreferences(),
-        baseurl = "https://www.avon.ru/";
+    var prefs = AnyBalance.getPreferences();
+    
     AnyBalance.setDefaultCharset('utf-8');
 
     checkEmpty(prefs.login, 'Введите логин!');
@@ -35,10 +38,16 @@ function main(){
     var html = AnyBalance.requestGet(baseurl + 'REPSuite/loginMain.page', g_headers);
     if (!html || AnyBalance.getLastStatusCode() > 400) {
         AnyBalance.trace(html);
-        throw new AnyBalance.Error('Сайт провайдера временно недоступен! Попробуйте обновить данные позже.');
+        throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
     }
+	
+	var form = getElement(html, /<form[^>]+name="loginForm"[^>]*>/i);
+	if(!form){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти форму входа! Сайт изменен?');
+	}
 
-    var params = createFormParams(html, function(params, str, name, value) {
+    var params = createFormParams(form, function(params, str, name, value) {
         if (name == 'userIdDisplay') {
             return prefs.login;
         } else if (name == 'password') {
@@ -49,21 +58,21 @@ function main(){
     });
 
     html = AnyBalance.requestPost(baseurl + 'REPSuite/login.page', params, addHeaders({
-        Referer: baseurl + 'manager/representative',
+        Referer: baseurl + 'REPSuite/loginMain.page',
     }));
 	
-	if(/Изменить пароль/i.test(html)){
+	if(/change_password|passwordmessage/i.test(html)){
 		AnyBalance.trace('Сайт требует изменить пароль в целях безопасности. Пропускаем...');
         html = AnyBalance.requestGet(baseurl + 'REPSuite/home.page', g_headers);
     }
 
     if(!/logoutMain/i.test(html)){
-        var error = getParam(html, null, null, /<div[^>]+logindivnojs[^>]*>(?:[\s\S]*?errortd){2}[^\(]*\(([^\)]*)/i);
+        var error = getParam(html, null, null, /<div[^>]+class="Error-message text[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
         if(error)
             throw new AnyBalance.Error(error, null, /неверный компьютерный номер/i.test(error));
 
         AnyBalance.trace(html);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+        throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
     }
 
     var result = {success: true};
@@ -75,25 +84,27 @@ function main(){
 	getParam(html, result, 'limit', /Доступный лимит:[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'penalty', /Пени:[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 	getParam(html, result, 'quartordersum', /Сумма заказов:([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'tonextlevel', /Для перехода на следующий уровень[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'garantdiscount', /Гарантированная[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces);
-	getParam(html, result, 'monthordersum', /Сумма заказов в этом месяце[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'rubdiscount', /Скидка в рублях[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'currdiscount', /Достигнутый уровень скидки[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces);
-	getParam(html, result, 'tonextdiscount', /До следующего уровня скидки[\s\S]*?(?:[^>]*>){4}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'tonextlevel', /Сумма продаж[\s\S]*?для достижения уровня[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'garantdiscount', /Гарантированная[\s\S]*?(?:[^>]*>){6}([^<]*)/i, replaceTagsAndSpaces);
+	getParam(html, result, 'monthordersum', /Сумма заказов в этом месяце[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'rubdiscount', /Скидка в рублях[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
+	getParam(html, result, 'thismonthdiscount', /Скидка в этом месяце[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces);
+	getParam(html, result, 'tonextdiscount', /Сумма продаж[\s\S]*?для достижения уровня[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'bonus',   /Доступные баллы[\s\S]*?<span[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'p_bonus', /Потенциальные баллы[\s\S]*?<span[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
     getParam(html, result, 'h_bonus', /Горящие баллы[\s\S]*?<span[^>]*>([^<]*)/i, replaceTagsAndSpaces, parseBalance);
 
+	var perc = {'Бронза': '15%', 'Серебро': '20%', 'Золото': '25%', 'Платина': '30%', 'VIP Платина': '35%'};
 	var stat = getElements(html, /<div[^>]+club-labels[^>]*>/ig);
-		if(stat){
-		    for(var i = 0; i<stat.length; i++){
-		    	if (/bold/i.test(stat))
-		    	    getParam(stat[i], result, '__tariff', /<div[^>]+font-weight-bold[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
-		    }
-		}else{
- 			AnyBalance.trace('Не удалось получить данные по привилегиям');
- 		}
+	if(stat){
+	    for(var i = 0; i<stat.length; i++){
+	    	if (/bold/i.test(stat))
+	    	    var disc = getParam(stat[i], result, '__tariff', /<div[^>]+font-weight-bold[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+				getParam(perc[disc], result, 'currdiscount');	
+	    }
+	}else{
+ 		AnyBalance.trace('Не удалось получить данные по привилегиям');
+ 	}
 	
 	getParam(html, result, 'expdate', /headerMenuCampaignDaysLeft[\s\S]*?(?:[^"]*>){5}([^"]*)/i, replaceTagsAndSpaces, parseDate);
     getParam(html, result, 'daysleft', /headerMenuCampaignDaysLeft[\s\S]*?(?:[^>]*>){1}([^<]*)/i, replaceTagsAndSpaces, parseBalance);
@@ -139,7 +150,12 @@ function main(){
 	    var firstName = getParam(html, null, null, /Имя:[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces);
 	    var secondName = getParam(html, null, null, /Отчество:[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces);
 	    var lastName = getParam(html, null, null, /Фамилия:[\s\S]*?(?:[^>]*>){5}([^<]*)/i, replaceTagsAndSpaces);
-	    getParam(firstName + ' ' + secondName + ' ' + lastName, result, 'fio', null, replaceTagsAndSpaces);
+	    var fio = firstName; // Если пользователь не указал в профиле фамилию, значение свойства "name" имеет вид "Имя null", поэтому делаем в виде сводки
+	    if (secondName)
+	        fio += ' ' + secondName;
+	    if (lastName)
+	        fio += ' ' + lastName;
+	    getParam(fio, result, 'fio', null, null);
 	}
 
     //Возвращаем результат

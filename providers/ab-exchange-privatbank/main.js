b1a6b929dcc6186b5f9e0adcbff3be4a3546d190
DimaZUA <60350224+DimaZUA@users.noreply.github.com>
2023-02-28 00:00:50 +0200

    Add files via upload

--- providers/ab-exchange-privatbank/main.js
+++ providers/ab-exchange-privatbank/main.js
@@ -2,47 +2,29 @@
 Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
 */
 
-function getRate(result, html, curr){
-    getParam(html, result, curr + "_buy", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*buy="([^"]*)', 'i'), null, parseBalance);
-    getParam(html, result, curr + "_sell", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*sale="([^"]*)', 'i'), null, parseBalance);
+function getRate(result, html, curr,suf){
+    getParam(html.find(valut=>valut.ccy==curr).buy, result, curr + "_buy" + (suf||''), null, null, parseBalance);
+    getParam(html.find(valut=>valut.ccy==curr).sale, result, curr + "_sell" + (suf||''), null, null, parseBalance);
 }
-function getCardExchangeRate(result, html, curr){
-    getParam(html, result, curr + "_buy_card", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*buy="([^"]*)', 'i'), null, parseBalance);
-    getParam(html, result, curr + "_sell_card", new RegExp('<exchangerate[^>]+\\bccy="' + curr + '"[^>]*sale="([^"]*)', 'i'), null, parseBalance);
-}
-function getBittrex(result, curr){
-    AnyBalance.trace('Connecting to bitrex.com for get '+curr);
-    var html = AnyBalance.requestGet('https://bittrex.com/api/v1.1/public/getmarketsummary?market=usd-'+curr);
-    result[curr + '_bittrex']=getJson(html).result[0].Bid;
-}
-
 
 function main() {
 	AnyBalance.trace('Connecting to privatbank for get exchange');
-	var info = AnyBalance.requestGet('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5');
-    
+	var info = getJson(AnyBalance.requestGet('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5'));
 	var result = {success: true};
         
 	getRate(result, info, 'USD');
 	getRate(result, info, 'EUR');
-	getRate(result, info, 'RUR');
+//	getRate(result, info, 'RUR');
 
         if(AnyBalance.isAvailable('USD_buy_card','USD_sell_card','EUR_buy_card','EUR_sell_card','RUR_buy_card','RUR_sell_card')){
 	  AnyBalance.trace('Connecting to privatbank for get cardExchange');
-	  var info = AnyBalance.requestGet('https://api.privatbank.ua/p24api/pubinfo?cardExchange');
+	  var info = getJson(AnyBalance.requestGet('https://api.privatbank.ua/p24api/pubinfo?cardExchange'));
 
-	  getCardExchangeRate(result, info, 'USD');
-	  getCardExchangeRate(result, info, 'EUR');
-	  getCardExchangeRate(result, info, 'RUR');
+	  getRate(result, info, 'USD', '_card');
+	  getRate(result, info, 'EUR', '_card');
+//	  getRate(result, info, 'RUR');
 	}
-	if(AnyBalance.isAvailable('BTC_bittrex')) getBittrex(result, 'BTC');
-       	if(AnyBalance.isAvailable('ETH_bittrex')) getBittrex(result, 'ETH');
 
-        if(AnyBalance.isAvailable('Brent')){ 
-       	  AnyBalance.trace('Connecting to investing.com for get Brent price');
-	  var info = AnyBalance.requestGet('https://ru.widgets.investing.com/live-commodities?hideTitle=true&cols=last&pairs=8833');
-          getParam(info, result, "Brent", new RegExp('last js-col-last pid.*>((\d|\,|\.)+)<', 'i'), null, parseBalance);
-        }
 	result.__tariff=getFormattedDate('DD.MM.YYYY HH:NN')
 	AnyBalance.setResult(result);
 }

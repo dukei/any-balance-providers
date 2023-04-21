7a15e001fe35e30e4809aea1884d1f38a5c6d350
Dmitry Kochin <dco@mail.ru>
2023-04-21 13:27:22 +0300

    Курсы валют Альфа-Банка (ab-exchange-alfabank): v.2:  	- Поддержка изменений на сайте 	- Добавлен выбор источника курсов в настройках (Отделения банка и Альфа-Онлайн) 	- Добавлены все валюты из списка, предоставляемого Альфа-банком (Китайский юань)

--- providers/ab-exchange-alfabank/main.js
+++ providers/ab-exchange-alfabank/main.js
@@ -4,54 +4,69 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept': 'application/json, text/plain, */*',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Referer': 'https://alfabank.ru/currency/',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
 };
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://alfabank.ru/_/rss/_currency.html';
+	
 	AnyBalance.setDefaultCharset('utf-8');
+	
+	if(/office/.test(prefs.type)){
+        var rType = 'rateCass';
+		var sourceType = 'Отделения банка';
+	}else{
+        var rType = 'makeCash';
+		var sourceType = 'Альфа-Онлайн';
+    }
+	
+	var date = new Date(+new Date() + 3*60*60*1000).toISOString();
+	var dateISO = date.replace(/(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d)(.*)/i, '$1' + '+03:00');
+    
+	var html = AnyBalance.requestGet('https://alfabank.ru/api/v1/scrooge/currencies/alfa-rates?currencyCode.in=USD,EUR,CNY,GBP,CHF,CZK,TRY&rateType.in=rateCass,makeCash&lastActualForDate.eq=true&clientType.eq=standardCC&date.lte=' + dateISO, g_headers);
 
-
-
-	var html = AnyBalance.requestGet(baseurl + '', g_headers);
-
-	if (!html || AnyBalance.getLastStatusCode() > 400) {
+	if (!html || AnyBalance.getLastStatusCode() >= 500) {
 		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+		throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
 	}
+	
+	AnyBalance.trace(html);
 
-
-	var result = {
-		success: true
-	};
-
-	AB.getParam(html, result, 'date', /<title>([^<]*на[\s\S]*?)<\/title>/i, AB.replaceTagsAndSpaces, AB.parseDate);
-
-	AB.getParam(html, result, 'buyUSD', /<td[^>]*id="[^"]*США[^"]*Buy[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-	AB.getParam(html, result, 'sellUSD', /<td[^>]*id="[^"]*США[^"]*Sell[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-
-	AB.getParam(html, result, 'buyEUR', /<td[^>]*id="[^"]*Евро[^"]*Buy[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-	AB.getParam(html, result, 'sellEUR', /<td[^>]*id="[^"]*Евро[^"]*Sell[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-
-	AB.getParam(html, result, 'buyGBP', /<td[^>]*id="[^"]*Английский[^"]*Buy[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-	AB.getParam(html, result, 'sellGBP', /<td[^>]*id="[^"]*Английский[^"]*Sell[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-
-	AB.getParam(html, result, 'buyCHF', /<td[^>]*id="[^"]*Швейцарский[^"]*Buy[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-	AB.getParam(html, result, 'sellCHF', /<td[^>]*id="[^"]*Швейцарский[^"]*Sell[^"]*"[^>]*>([\s\S]*?)<\/td>/i, AB.replaceTagsAndSpaces,
-		AB.parseBalance);
-
-
+    var json = getJson(html);
+	
+	if(!json.data){
+		var error = json.error || json.message || json.title;
+		if(error)
+			throw new AnyBalance.Error(error, null, true);
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось получить информацию по курсам валют. Сайт изменен?');
+	}
+    
+	var result = {success: true};
+	
+	for(var i = 0; i<json.data.length; i++){
+		var valut = json.data[i];
+		var t = valut.rateByClientType[0].ratesByType;
+		var rates = t.filter(function (b) { return b.rateType == rType })[0];
+		var key = valut.currencyCode;
+	    AnyBalance.trace(key + ': ' + JSON.stringify(valut));
+        if(AnyBalance.isAvailable('buy' + key) && rates)
+            result['buy' + key] = rates.lastActualRate.buy.originalValue;
+        if(AnyBalance.isAvailable('sell' + key) && rates)
+            result['sell' + key] = rates.lastActualRate.sell.originalValue;
+        if(AnyBalance.isAvailable('date') && rates)
+            sumParam(getFormattedDate(null, new Date(rates.lastActualRate.date)), result, 'date', null, null, parseDateSilent, aggregate_max);
+	    if(AnyBalance.isAvailable('source'))
+            getParam(sourceType, result, 'source');
+	    if(rates)
+		    getParam(getFormattedDate(null, new Date(rates.lastActualRate.date)), result, '__tariff');
+	}
+    
 	AnyBalance.setResult(result);
 }

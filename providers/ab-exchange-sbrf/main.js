4b8f5f6291901bcf2d704f82e4eaddabc1f28783
Dmitry Kochin <dco@mail.ru>
2023-04-21 13:30:24 +0300

    Курсы валют СБ РФ (ab-exchange-sbrf): v.16:  	- Поддержка изменений на сайте. 	- Добавлены новые валюты (Австралийский доллар, Гонконгский доллар, Дирхам ОАЭ, Казахстанский тенге).

--- providers/ab-exchange-sbrf/main.js
+++ providers/ab-exchange-sbrf/main.js
@@ -7,11 +7,13 @@
 */
 
 var g_headers = {
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-  'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-  'Connection': 'keep-alive',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Connection': 'keep-alive',
+    'cache-control': 'max-age=0',
+    'upgrade-insecure-requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
 };
 
 var g_iso_to2letters = {
@@ -25,54 +27,60 @@ var g_iso_to2letters = {
 	SEK: 'sek',
 	CHF: 'chf',
 	GBP: 'gbp',
-	CNY: 'cny'
+	CNY: 'cny',
+	HKD: 'hkd',
+	AUD: 'aud',
+	AED: 'aed',
+	KZT: 'kzt',
 };
 
 function main(){
-  var prefs = AnyBalance.getPreferences();
-  AnyBalance.setDefaultCharset('utf-8');
+    var prefs = AnyBalance.getPreferences();
+    AnyBalance.setDefaultCharset('utf-8');
 
     var region = findRegion(prefs.region).TRBANK_CODE;
   
     if (/office/.test(prefs.type)) {
         var rateType = 'ERNP-4';
-        var requestID = '7cfa7f739ebdf97418a3963183fbba05';
+        var enterType = '?tab=vsp';
 		var sourceType = 'Офисы банка';
 	} else {
         var rateType = 'ERNP-2';
-        var requestID = '8e4f8491fbf9f2ada11a32133ef30b80';
+        var enterType = '?tab=sbol';
 		var sourceType = 'СберБанк Онлайн';
     }	
-
-  var html = AnyBalance.requestGet('https://www.sberbank.ru/proxy/services/rates/public/actual?rateType=' + rateType + '&isoCodes[]=USD&isoCodes[]=EUR&isoCodes[]=CAD&isoCodes[]=DKK&isoCodes[]=JPY&isoCodes[]=NOK&isoCodes[]=SGD&isoCodes[]=SEK&isoCodes[]=CHF&isoCodes[]=GBP&isoCodes[]=CNY&regionId=' + region,
+	
+	var html = AnyBalance.requestGet('https://www.sberbank.ru/ru/quotes/currencies', g_headers);
+  
+    html = AnyBalance.requestGet('https://www.sberbank.ru/proxy/services/rates/public/actual?rateType=' + rateType + '&isoCodes[]=USD&isoCodes[]=EUR&isoCodes[]=CNY&isoCodes[]=GBP&isoCodes[]=KZT&isoCodes[]=AED&isoCodes[]=SGD&isoCodes[]=SEK&isoCodes[]=NOK&isoCodes[]=CAD&isoCodes[]=CHF&isoCodes[]=DKK&isoCodes[]=JPY&isoCodes[]=HKD&isoCodes[]=AUD&regionId=' + region,
 	addHeaders({
-  		'X-Requested-With': 'XMLHttpRequest',
-  		'X-Request-ID': requestID,
-  		Referer: 'https://www.sberbank.ru/ru/quotes/currencies/'
+		accept: '*/*',
+  		Referer: 'https://www.sberbank.ru/ru/quotes/currencies' + enterType
   	}));
+	AnyBalance.trace(html);
 
-  var json = getJson(html);
+    var json = getJson(html);
 
-  var result = {success: true};
+    var result = {success: true};
 
-  for(var key in json){
-  	  var valut = json[key];
-  	  var name = g_iso_to2letters[key];
-	  AnyBalance.trace(name + ': ' + JSON.stringify(valut));
-      if(AnyBalance.isAvailable(name + '_purch'))
-        result[name + '_purch'] = valut.rateList[0].rateBuy;
-      if(AnyBalance.isAvailable(name + '_sell'))
-        result[name + '_sell'] = valut.rateList[0].rateSell;
-      if(AnyBalance.isAvailable(name + '_amount') && (amount = getAmount(name)))
-        result[name + '_amount'] = valut.rateList[0].rateBuy * amount;
-	  if(AnyBalance.isAvailable('date'))
-        sumParam(valut.startDateTime, result, 'date', null, null, null, aggregate_max);
-	  if(AnyBalance.isAvailable('source'))
-        getParam(sourceType, result, 'source');
-	  getParam(getFormattedDate(null, new Date(valut.startDateTime)), result, '__tariff');
-  }
+    for(var key in json){
+    	var valut = json[key];
+  	    var name = g_iso_to2letters[key];
+	    AnyBalance.trace(name + ': ' + JSON.stringify(valut));
+        if(AnyBalance.isAvailable(name + '_purch'))
+            result[name + '_purch'] = valut.rateList[0].rateBuy;
+        if(AnyBalance.isAvailable(name + '_sell'))
+            result[name + '_sell'] = valut.rateList[0].rateSell;
+        if(AnyBalance.isAvailable(name + '_amount') && (amount = getAmount(name)))
+            result[name + '_amount'] = valut.rateList[0].rateBuy * amount;
+	    if(AnyBalance.isAvailable('date'))
+            sumParam(valut.startDateTime, result, 'date', null, null, null, aggregate_max);
+	    if(AnyBalance.isAvailable('source'))
+            getParam(sourceType, result, 'source');
+	    getParam(getFormattedDate(null, new Date(valut.startDateTime)), result, '__tariff');
+    }
 
-  AnyBalance.setResult(result);
+    AnyBalance.setResult(result);
 }
 
 function getAmount(valut){

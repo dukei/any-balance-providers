9a1324fa1d54387842267dfd6360b82a96209c7b
Dmitry Kochin <dco@mail.ru>
2023-04-21 13:32:11 +0300

    Умные сети (ab-internet-umnyeseti): v.2:  	- Поддержка изменений на сайте.

--- providers/ab-internet-umnyeseti/main.js
+++ providers/ab-internet-umnyeseti/main.js
@@ -12,7 +12,7 @@ var g_headers = {
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'https://stat.umnyeseti.ru:8080/';
+	var baseurl = 'https://stat.umnyeseti.ru/';
 	AnyBalance.setDefaultCharset('utf-8');
 
 	checkEmpty(prefs.login, 'Введите логин!');

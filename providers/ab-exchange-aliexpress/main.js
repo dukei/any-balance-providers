412bba1682541c73a2679afe0f896a5bdf677589
Dmitry Kochin <dco@mail.ru>
2023-04-21 13:28:03 +0300

    Курс AliExpress (ab-exchange-aliexpress): v.6:  	- Поддержка альтернативного источника информации. 	- Добавлены счетчики для сравнительного анализа.

--- providers/ab-exchange-aliexpress/main.js
+++ providers/ab-exchange-aliexpress/main.js
@@ -4,60 +4,116 @@
 */
 
 var g_headers = {
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept': 'application/json, text/javascript, */*; q=0.01',
   'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-  'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-  'Connection': 'keep-alive',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+  'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+  'X-Requested-With': 'XMLHttpRequest',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
 };
 
-function getPrice(cur){
-    var url = 'https://gpsfront.aliexpress.com/queryGpsProductAjax.do?&callback=jQuery18308581276012533401_1490101903659&widget_id=5041187&platform=pc&limit=12&offset=0&locale=ru_RU&phase=1&_=';
-    AnyBalance.setCookie('.aliexpress.com', 'aep_usuc_f', 'site=rus&region=RU&b_locale=ru_RU&c_tp=' + cur);
-    var html = AnyBalance.requestGet(url, g_headers);
-    var json = getParam(html, /jQuery18308581276012533401_1490101903659\s*\(([\s\S]*)\)/, null, getJson);
-
-    var products = {};
-    for(var i=0; i<json.gpsProductDetails.length; ++i){
-    	var p = json.gpsProductDetails[i];
-    	products[p.productId] = p;
-    }
-
-    AnyBalance.trace('Найдено ' + i + ' товаров для валюты ' + cur);
-
-    return products;
-}
-
 function main() {
-
   	AnyBalance.setDefaultCharset('utf-8');
-  
-    var productsUSD = getPrice('USD');
-    var productsRUB = getPrice('RUB');
-
-    var prub, pusd;
-    for(var pid in productsUSD){
-    	prub = productsRUB[pid];
-    	if(!prub)
-    		continue;
-    	pusd = productsUSD[pid];
-    	break;
-    }
-
-    if(!pusd)
-    	throw new AnyBalance.Error('Не удалось найти одинаковые продукты для разных валют', true);
-
-    AnyBalance.trace('Товар "' + pusd.productTitle + '" стоит ' + pusd.minPrice);
-    AnyBalance.trace('Товар "' + prub.productTitle + '" стоит ' + prub.minPrice);
-
-    var result = {
-        success: true,
-        rate: parseFloat((parseBalance(prub.minPrice)/parseBalance(pusd.minPrice)).toFixed(4))
-    };
-    
-    if(AnyBalance.isAvailable('date')) {
-        result.date = +new Date();
-    }
+	
+	var html = AnyBalance.requestGet('https://moneyfromnothing.ru/blog/aliexpress-kurs-dollara/rates_data.json?_=' + new Date().getTime(), g_headers);
+	
+	if (!html || AnyBalance.getLastStatusCode() > 400){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
+	}
+	
+	var json = getJson(html);
+	AnyBalance.trace(JSON.stringify(json));
+	
+	var result = {success: true};
+	
+	getParam(json.updateTime, result, 'date', null, null, parseDateISO);
+	getParam(json.currentRateAliExpress, result, 'rate', null, null, parseBalanceRound);
+	getParam(json.currentRateAliExpressRu, result, 'rateru', null, null, parseBalanceRound);
+	getParam(json.currentRateCbr, result, 'ratecbrf', null, null, parseBalanceRound);
+	getParam(json.updateTime && json.updateTime.replace(/(\d{4})-(\d{2})-(\d{2})(.*)/,'$3.$2.$1'), result, '__tariff');
+	
+	if(AnyBalance.isAvailable('date'))
+	    getParam(json.updateTime && json.updateTime.replace(/(\d{4})-(\d{2})-(\d{2})(.*)/,'$3.$2.$1'), result, 'date', null, null, parseDate);
+	
+	if (AnyBalance.isAvailable('trend', 'averrate')) {
+	    if (json.ratesAliExpress && json.ratesAliExpress.length>0){
+			var __rate;
+	    	for(var i=0; i<json.ratesAliExpress.length; ++i){
+	    		var rate = json.ratesAliExpress[i].rate;
+	    		sumParam(rate, result, '__rate', null, null, parseBalanceSilent, aggregate_sum);
+	    	}
+		
+	    	var weekAgoRate = json.ratesAliExpress[0].rate;
+	    	var period = json.ratesAliExpress.length;
+	    	var averrageRate = result.__rate/period;
+			delete result.__rate;
+			getParam(averrageRate, result, 'averrate', null, null, parseBalanceRound);
+	        	
+	    	if(result.rate > weekAgoRate){
+	    		result.trend = 'AG$ ↑';
+	    	}else if(result.rate < weekAgoRate){
+	    		result.trend = 'AG$ ↓';
+	    	}else{
+	    		result.trend = 'AG$ -';
+	    	}
+	    }
+	}
+	
+	if (AnyBalance.isAvailable('trendru', 'averrateru')) {
+	    if (json.ratesAliExpressRu && json.ratesAliExpressRu.length>0){
+			var __rateru;
+	    	for(var i=0; i<json.ratesAliExpressRu.length; ++i){
+	    		var rate = json.ratesAliExpressRu[i];
+	    		sumParam(rate.rate, result, '__rateru', null, null, parseBalanceSilent, aggregate_sum);
+	    	}
+	        	
+	    	var weekAgoRate = json.ratesAliExpressRu[0].rate;
+	    	var period = json.ratesAliExpressRu.length;
+	    	var averrageRate = result.__rateru/period;
+			delete result.__rateru;
+			getParam(averrageRate, result, 'averrateru', null, null, parseBalanceRound);
+	        	
+	    	if(result.rateru > weekAgoRate){
+	    		result.trendru = 'AR$ ↑';
+	    	}else if(result.rateru < weekAgoRate){
+	    		result.trendru = 'AR$ ↓';
+	    	}else{
+	    		result.trendru = 'AR$ -';
+	    	}
+	    }
+	}
+	
+	if (AnyBalance.isAvailable('trendcbrf', 'averratecbrf')) {
+	    if (json.ratesCbr && json.ratesCbr.length>0){
+			var __ratecbrf;
+	    	for(var i=0; i<json.ratesCbr.length; ++i){
+	    		var rate = json.ratesCbr[i];
+	    		sumParam(rate.rate, result, '__ratecbrf', null, null, parseBalanceSilent, aggregate_sum);
+	    	}
+	        	
+	    	var weekAgoRate = json.ratesCbr[0].rate;
+	    	var period = json.ratesCbr.length;
+	    	var averrageRate = result.__ratecbrf/period;
+			delete result.__ratecbrf;
+			getParam(averrageRate, result, 'averratecbrf', null, null, parseBalanceRound);
+	        	
+	    	if(result.ratecbrf > weekAgoRate){
+	    		result.trendcbrf = 'CB$ ↑';
+	    	}else if(result.ratecbrf < weekAgoRate){
+	    		result.trendcbrf = 'CB$ ↓';
+	    	}else{
+	    		result.trendcbrf = 'CB$ -';
+	    	}
+	    }
+	}
     
     AnyBalance.setResult(result);
+}
+
+function parseBalanceRound(val) {
+	var balance = parseBalance(val + '');
+	if(!isset(balance))
+		return null;
+	
+	return Math.round(balance*100)/100;
 }

04a37df0c22d543155d3ccbb7c71a65271a0507d
Dmitry Kochin <dco@mail.ru>
2023-07-25 12:06:26 +0300

    Аэрофлот Бонус (ab-bonus-aeroflot): v.17:  	- Поддержка изменений в процедуре входа. 	- Добавлены дополнительные счетчики.

--- providers/ab-bonus-aeroflot/main.js
+++ providers/ab-bonus-aeroflot/main.js
@@ -3,18 +3,19 @@
 */
 
 var g_headers = {
-	'Accept': 'application/json;charset=utf-8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 	'Accept-Language': 'ru,ru-RU;q=0.9,en-US;q=0.8,en;q=0.7,uk;q=0.6',
 	'Cache-Control': 'max-age=0',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
-	'content-type':'application/json',
-	'origin':'https://www.aeroflot.ru',
-	'referer':'https://www.aeroflot.ru/',
-	'Accept-Encoding':'gzip, deflate, br'
+	'Content-Type':'application/json',
+	'Origin':'https://www.aeroflot.ru',
+	'Referer':'https://www.aeroflot.ru/',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 };
 
-var baseurl = "https://gw.aeroflot.ru/api/pr/";
+var baseurl = 'https://gw.aeroflot.ru/api/pr/';
+var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'];
 
 var tier_level = {
 	BASIC: 'Базовый',
@@ -25,181 +26,173 @@ var tier_level = {
 
 function main() {
     var prefs = AnyBalance.getPreferences();
-    var token=AnyBalance.getData('token'+prefs.login);
+	
+	var baseurl = "https://gw.aeroflot.ru/api/pr/";
 	AnyBalance.setDefaultCharset('utf-8');
-    	//1. найти frontend/static/js/aac.release.... в get https://www.aeroflot.ru/personal/login
+	
+	var token = AnyBalance.getData('token' + prefs.login);
+	
+    //1. найти frontend/static/js/aac.release.... в get https://www.aeroflot.ru/personal/login
 	//2. скачать и найти в нем CLIENT_ID и CLIENT_SECRET
-	var CLIENT_ID='52965ca1-f60e-46e3-834d-604e023600f2';
-	var CLIENT_SECRET='rU0gE3yP1wV0dY6nJ8kY8pD6pI5dF7xP5nH5nR4cH3sC0rK2rR';
-	g_headers['X-IBM-Client-Id']=CLIENT_ID;
-	g_headers['X-IBM-Client-Secret']=CLIENT_SECRET;
+	
+	var CLIENT_ID = '52965ca1-f60e-46e3-834d-604e023600f2';
+	var CLIENT_SECRET = 'rU0gE3yP1wV0dY6nJ8kY8pD6pI5dF7xP5nH5nR4cH3sC0rK2rR';
+	
+	g_headers['X-IBM-Client-Id'] = CLIENT_ID;
+	g_headers['X-IBM-Client-Secret'] = CLIENT_SECRET;
 
-if (token){
-        g_headers.authorization=token;
-        AnyBalance.trace('Найден старый токен. Проверяем.');
-try{
-    var json=callAPI('LKAB/Profile/v3/get',{lang:"ru",data:{}});
-}catch(e){
-	AnyBalance.trace('Токен устарел. Нужна авторзация');
-	var token='';
-        AnyBalance.setData('token'+prefs.login,'');
-        AnyBalance.saveData('token'+prefs.login,'');
-	}
-}
-if (!token){
-    checkEmpty(prefs.login, 'Введите логин');
-    checkEmpty(prefs.login, 'Введите пароль');
-	AnyBalance.trace('Начинаем авторизацию');
-
-//var html=AnyBalance.requestGet('https://www.aeroflot.ru/', g_headers);
-//var url=getParam(html,/  src="(\/[^"]*)"/);
-//AnyBalance.trace('Ссылка на вход '+(url?'':'не')+' найдена на ' + AnyBalance.getLastUrl());
-//AnyBalance.trace('url='+url);
-//var html=AnyBalance.requestGet('https://www.aeroflot.ru/aac/app/ru-ru/login', g_headers);
-//var url1=getParam(html,/  src="(\/[^"]*)"/);
-//AnyBalance.trace('Ссылка на вход '+(url1?'':'не')+' найдена на ' + AnyBalance.getLastUrl());
-//AnyBalance.trace('url='+url);
-//if (!url&&url1) url=url1;
-//var html=AnyBalance.requestGet('https://www.aeroflot.ru/aac/app/ru-ru/login?client_id='+CLIENT_ID+'&nonce='+Math.random().toString(),+'&redirect_uri=https%3A%2F%2Fwww.aeroflot.ru%2Fru-ru&response_type=code%20id_token%20token&scope=openid%20user-loyalty-profile%20personal-cabinet%20feedback', g_headers);
-//var url1=getParam(html,/  src="(\/[^"]*)"/);
-//AnyBalance.trace('Ссылка на вход '+(url1?'':'не')+' найдена на ' + AnyBalance.getLastUrl());
-//AnyBalance.trace('url='+url);
-//if (!url&&url1) url='https://gw.aeroflot.ru/api/pr/AAC/Authorization/v1/get';
-
-//if (!url) {
-//	AnyBalance.trace(html);
-//        throw new AnyBalance.Error('Ссылка на вход не найдена');
-//	}
-//var html=AnyBalance.requestPost('https://www.aeroflot.ru'+url, JSON.stringify({
-//	"sensor_data": "7a74G7m23Vrp0o5c9131841.68-1,2,-94,-100,Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36,uaend,12147,20030107,ru,Gecko,3,0,0,0,397692,5720974,1920,1040,1920,1080,1920,969,1920,,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1,8328,0.386659864193,808162860486.5,0,loc:-1,2,-94,-101,do_en,dm_en,t_en-1,2,-94,-105,-1,2,-94,-102,-1,2,-94,-108,-1,2,-94,-110,0,1,405,615,2;-1,2,-94,-117,-1,2,-94,-111,0,82,-1,-1,-1;-1,2,-94,-109,0,82,-1,-1,-1,-1,-1,-1,-1,-1,-1;-1,2,-94,-114,-1,2,-94,-103,-1,2,-94,-112,https://www.aeroflot.ru/aac/app/ru-ru/login-1,2,-94,-115,1,1055,32,82,82,0,1187,709,0,1616325720973,6,17290,0,1,2881,0,0,711,569,0,E0B5B3AF18458708FD28F9FC547A4D59~-1~YAAQRndlXxEmikR4AQAAczmFVAVzLUkC5c+nxkIObY1HPH1hbS4e8GVEBhn2cMp/hhB4Cy8m6dGfsjeqE1H6dcKttO2HjH6V9E26He8p5cN5cc+aGcXe/cxdzn96Gs54vKmKuSxciVc87117bIFmPVUCQp466QwjFsBLidluZy/ZXYm3hnwUklrtPZChbzxCE2z8Leszeh7+w84lsbhRmvvQ/jhz4Q4qX+ZzN7B5+ppnZ+4/9qSJE7HbbqJSM0lDsTQefMMe29ptErqX4DuVR5OzNf37gEiN8zUsqeBYAeDfwOhDxAmZfUQckx9hCI3Pr6KKjsWRlU8yEdvO54EcTGuILlEbMdDPDrZ2CmSW/lRCgVxipP/r2JZavsJ61pKpeoluLYXXXoC+0wGm6OpG6f0Sb15u3My5mY3wFT+3yASWo96bxqY=~-1~-1~-1,40046,995,462783532,30261693,PiZtE,54076,49-1,2,-94,-106,9,1-1,2,-94,-119,85,91,230,135,119,120,103,289,89,85,79,4301,3334,767,-1,2,-94,-122,0,0,0,0,1,0,0-1,2,-94,-123,-1,2,-94,-124,-1,2,-94,-126,-1,2,-94,-127,11321144241322243122-1,2,-94,-70,-1752250632;-915298718;dis;,7,8;true;true;true;-120;true;24;24;true;false;1-1,2,-94,-80,5579-1,2,-94,-116,85814607-1,2,-94,-118,94413-1,2,-94,-129,ddf68f2157d3c0edf2744f0b57b5e4c63840c2c94b8cc853141f293336e1e305,1,c3d9e5035928d1bb3c02f20ce039bd9dc56ac709bc815ec610eb17ba312244b9,Google Inc.,Google SwiftShader,997243818a5c455507656f0044857e58f77ce7ed2b259a614bcd5a5312f3b5f5,24-1,2,-94,-121,;26;5;0"
-//}),g_headers)
-
-
-var params={
-    "lang": "ru",
-    "data": {
-        "oidc": {
-            "clientId": CLIENT_ID,
-            "scopes": [
-                "openid",
-                "user-loyalty-profile",
-                "personal-cabinet",
-                "feedback"
-            ],
-            "responseTypes": [
-                "code",
-                "id_token",
-                "token"
-            ],
-            "nonce": Math.random().toString(),
-            "redirectUri": "https://www.aeroflot.ru/auth/app"
-        },
-        "auth": {
-            "login": prefs.login,
-            "password": prefs.password
-        },
-        "tfa": {
-            "fingerprint": "65f937e9e6822cf3249efa338ea4edcd",
-            "fingerprintRaw": "{\"excludes\":{\"userAgent\":true,\"language\":true,\"timezoneOffset\":true,\"timezone\":true,\"sessionStorage\":true,\"localStorage\":true,\"indexedDb\":true,\"openDatabase\":true,\"addBehavior\":true,\"plugins\":true,\"fonts\":true,\"fontsFlash\":true,\"audio\":true,\"hasLiedLanguages\":true,\"adBlock\":true,\"enumerateDevices\":true,\"wasRedirect\":false}}"
-        },
-        "relogin": true
+    if(token){
+        g_headers.authorization = token;
+        AnyBalance.trace('Токен авторизации сохранен. Пробуем войти...');
+        try{
+            var json = callAPI('LKAB/Profile/v3/get', {lang: "ru", data: {}});
+			AnyBalance.trace('Успешно вошли по accessToken');
+        }catch(e){
+			AnyBalance.trace('Не удалось войти по accessToken');
+	        var token = '';
+            AnyBalance.setData('token' + prefs.login, '');
+            AnyBalance.saveData('token' + prefs.login, '');
+	    }
     }
-//	"lang": "ru",
-//	"data": {
-//		"oidc": {
-//			"clientId": CLIENT_ID,
-//			"scopes": ["openid", "user-loyalty-profile", "personal-cabinet", "feedback"],
-//			"responseTypes": ["code", "id_token", "token"],
-//			"nonce": Math.random().toString(),
-//			"redirectUri": "https://www.aeroflot.ru/auth/app"
-//		},
-//		"auth": {
-//			"login": prefs.login,
-//			"password": prefs.password
-//		},
-//		"tfa": {
-//			"fingerprint": "6951aa9432766fe1c9808b5d430a3689",
-//			"fingerprintRaw": "{\"excludes\":{\"userAgent\":true,\"language\":true,\"timezoneOffset\":true,\"timezone\":true,\"sessionStorage\":true,\"localStorage\":true,\"indexedDb\":true,\"openDatabase\":true,\"addBehavior\":true,\"plugins\":true,\"fonts\":true,\"fontsFlash\":true,\"audio\":true,\"hasLiedLanguages\":true,\"adBlock\":true,\"enumerateDevices\":true}}"
-//		},
-//		"relogin": true
-//	}
-}
-var json=callAPI('AAC/Authorization/v1/get',params)
-params.data.relogin=false;
-var json=callAPI('AAC/Authorization/v1/get',params)
-if (!json.data.tokens){
-	AnyBalance.trace(html);
-	throw new AnyBalance.Error('Не удалось получить токен авторизации. Возможно изменения в API.');
-	}
-g_headers.authorization=json.data.tokens.tokenType+' '+json.data.tokens.accessToken;
-//AnyBalance.setData('accessToken'+prefs.login,json.data.tokens.accessToken);
-//AnyBalance.setData('tokenType'+prefs.login,json.data.tokens.tokenType);
-//AnyBalance.setData('code'+prefs.login,json.data.tokens.code);
-//AnyBalance.setData('idToken'+prefs.login,json.data.tokens.idToken);
-var json=callAPI('LKAB/Profile/v3/get',{lang:"ru",data:{}});
-AnyBalance.setData('token'+prefs.login,g_headers.authorization);
-AnyBalance.saveData();
-}
+	
+    if(!token){
+        checkEmpty(prefs.login, 'Введите логин');
+        checkEmpty(prefs.login, 'Введите пароль');
+	
+	    AnyBalance.trace('Сессия новая. Будем логиниться заново...');
+	
+	    var html = AnyBalance.requestGet('https://www.aeroflot.ru/personal/login', g_headers);
+
+        var params = {
+            "lang": "ru",
+            "data": {
+                "oidc": {
+                    "clientId": CLIENT_ID,
+                    "scopes": [
+                        "openid",
+                        "user-loyalty-profile",
+                        "personal-cabinet",
+                        "feedback"
+                    ],
+                    "responseTypes": [
+                        "code",
+                        "id_token",
+                        "token"
+                    ],
+                    "nonce": Math.random().toString(),
+                    "redirectUri": "https://www.aeroflot.ru/auth/app"
+                },
+                "auth": {
+                    "login": prefs.login,
+                    "password": prefs.password
+                },
+                "tfa": {
+                    "fingerprint": "a2d6c37b8f2363c821b8f7a0575a9dcd",
+                    "fingerprintRaw": "{\"excludes\":{\"userAgent\":true,\"language\":true,\"timezoneOffset\":true,\"timezone\":true,\"sessionStorage\":true,\"localStorage\":true,\"indexedDb\":true,\"openDatabase\":true,\"addBehavior\":true,\"plugins\":true,\"fonts\":true,\"fontsFlash\":true,\"audio\":true,\"hasLiedLanguages\":true,\"adBlock\":true,\"enumerateDevices\":true,\"wasRedirect\":false}}"
+                },
+                "relogin": true
+            }
+        }
+
+        AnyBalance.setCookie('aeroflot.ru', 'AF_preferredLanguage', 'ru');
+        AnyBalance.setCookie('aeroflot.ru', 'AF_preferredLocale', 'ru');
+
+        var json = callAPI('AAC/Authorization/v1/get', params);
+
+        if(!json.data.tokens){
+	        AnyBalance.trace(html);
+	        throw new AnyBalance.Error('Не удалось получить токен авторизации. Сайт изменен?');
+	    }
+	
+        g_headers.authorization = json.data.tokens.tokenType + ' ' + json.data.tokens.accessToken;
+
+        var json = callAPI('LKAB/Profile/v3/get', {lang: "ru", data: {}});
+        AnyBalance.setData('token' + prefs.login, g_headers.authorization);
+        AnyBalance.saveData();
+    }
 
     var result = {success: true};
 	
-	getParam(json.data.loyaltyInfo.miles.balance + '', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
-	getParam(json.data.loyaltyInfo.currentYearStatistics.qualifyingMiles   + '', result, 'qmiles', null, replaceTagsAndSpaces, parseBalance);
-	getParam(json.data.loyaltyInfo.currentYearStatistics.segments   + '', result, 'segments', null, replaceTagsAndSpaces, parseBalance);
-	getParam(json.data.loyaltyInfo.currentYearStatistics.businessSegments  + '', result, 'segmentsBusiness', null, replaceTagsAndSpaces, parseBalance);
-	getParam(json.data.loyaltyInfo.miles.activityDate , result, 'milesActivityDate', null, replaceTagsAndSpaces, parseDateISO);
-	getParam(json.data.loyaltyInfo.miles.expirationDate , result, 'milesExpirationDate', null, replaceTagsAndSpaces, parseDateISO);
-	getParam(json.data.loyaltyInfo.tierLevelExpirationDate , result, 'levelExpirationDate', null, replaceTagsAndSpaces, parseDateISO);
-	getParam(tier_level[json.data.loyaltyInfo.tierLevel]||json.data.loyaltyInfo.tierLevel, result, 'level');
-	getParam(json.data.loyaltyInfo.loyaltyId , result, '__tariff');
-	getParam(json.data.loyaltyInfo.loyaltyId, result, 'partnum');
+	var loyaltyInfo = json.data && json.data.loyaltyInfo;
+	var contactInfo = json.data && json.data.contact;
+	
+	getParam(loyaltyInfo.miles.balance + '', result, 'balance', null, replaceTagsAndSpaces, parseBalance);
+	getParam(loyaltyInfo.currentYearStatistics.qualifyingMiles   + '', result, 'qmiles', null, replaceTagsAndSpaces, parseBalance);
+	getParam(loyaltyInfo.currentYearStatistics.segments   + '', result, 'segments', null, replaceTagsAndSpaces, parseBalance);
+	getParam(loyaltyInfo.currentYearStatistics.businessSegments  + '', result, 'segmentsBusiness', null, replaceTagsAndSpaces, parseBalance);
+	getParam(loyaltyInfo.miles.activityDate , result, 'milesActivityDate', null, replaceTagsAndSpaces, parseDateISO);
+	getParam(loyaltyInfo.miles.expirationDate , result, 'milesExpirationDate', null, replaceTagsAndSpaces, parseDateISO);
+	getParam(loyaltyInfo.tierLevelExpirationDate , result, 'levelExpirationDate', null, replaceTagsAndSpaces, parseDateISO);
+	getParam(loyaltyInfo.regDate , result, 'regDate', null, replaceTagsAndSpaces, parseDateISO);
+	getParam(tier_level[loyaltyInfo.tierLevel]||loyaltyInfo.tierLevel, result, 'level');
+	getParam(loyaltyInfo.loyaltyId , result, '__tariff');
+	getParam(loyaltyInfo.loyaltyId, result, 'partnum');
+	
+	var regName = {};
+	sumParam(contactInfo.firstName, regName, '__rn', null, null, null, create_aggregate_join(' '));
+	sumParam(contactInfo.middleName, regName, '__rn', null, null, null, create_aggregate_join(' '));
+	sumParam(contactInfo.lastName, regName, '__rn', null, null, null, create_aggregate_join(' '));
+	getParam(regName.__rn, result, 'regname');
+	
+	getParam(contactInfo.forumNickName, result, 'nickname');
+	getParam(contactInfo.email, result, 'email');
+	getParam(contactInfo.phones && contactInfo.phones[0] && contactInfo.phones[0].phoneNumber, result, 'phone', null, replaceNumber);
+	
+	var person = {};
+	sumParam(contactInfo.firstNameRu, person, '__pn', null, null, null, create_aggregate_join(' '));
+	sumParam(contactInfo.middleNameRu, person, '__pn', null, null, null, create_aggregate_join(' '));
+	sumParam(contactInfo.lastNameRu, person, '__pn', null, null, null, create_aggregate_join(' '));
+	getParam(person.__pn, result, 'fio', null, null, capitalFirstLetters);
 	
     AnyBalance.setResult(result);
-
-
-	
-	//mainAppAPI(prefs);
-	//mainSite(prefs);
 }
+
 function callAPI(location, params) {
-	AnyBalance.trace('Запрос к '+location);
-	var html=AnyBalance.requestPost(baseurl+location,JSON.stringify(params),g_headers);
-        AnyBalance.trace('Ответ:'+html);
-	if (!html || AnyBalance.getLastStatusCode() > 401) {
+	AnyBalance.trace('Запрос: ' + location);
+	
+	var html = AnyBalance.requestPost(baseurl + location, JSON.stringify(params), g_headers);
+	
+    AnyBalance.trace('Ответ:' + html);
+	
+	if(!html || AnyBalance.getLastStatusCode() > 401) {
 		AnyBalance.trace(html);
-		throw new AnyBalance.Error(baseurl+' временно недоступен! Попробуйте обновить данные позже.');
+		throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
 	}
-	var json=getJson(html);
-	if (json.errors && json.errors.length>0){
-		var error=json.errors[0].userMessage;
-		if (/робот/i.test(error)){
+	
+	var json = getJson(html);
+	
+	if(json.errors && json.errors.length > 0){
+		var error = json.errors[0].userMessage;
+		if(/робот/i.test(error)){
 			AnyBalance.trace('Аэрофлот требует решить капчу');
-			params.data.auth.captchaText=solveRecaptcha(error, 'https://www.aeroflot.ru/aac/app/ru-ru/login', '6LfPVBETAAAAAFL_KotHis1PSaeI3UV11RplpwTo');
+			params.data.auth.captchaText = solveRecaptcha(error, 'https://www.aeroflot.ru/aac/app/ru-ru/login', '6LfPVBETAAAAAFL_KotHis1PSaeI3UV11RplpwTo');
 			return callAPI(location, params);
 		}else{
-			throw new AnyBalance.Error(error,false,/парол/i.test());	
+			throw new AnyBalance.Error(error, false, /парол/i.test());	
 		}
 	}
-	if (json.data.consents && json.data.consents.isNeeded) 
+	
+	if(json.data.consents && json.data.consents.isNeeded) 
 		throw new AnyBalance.Error('Требуется согласие с пользовательским соглашением. Предоставьте его через сайт или мобильное приложение Аэрофлот',false,true);
-	if (json.data.tfa && json.data.tfa.isNeeded){
-	   if (json.data.tfa.action=='enter_pin'){
-	   	var code=json.data.tfa.code;
-		var tfa=callAPI('AAC/SmsInfo/Limits/v1/get',{lang:"ru",confirmationToken:code})
-//		var pin=AnyBalance.retrieveCode('На Ваш номер +'+json.data.tfa.smsinfo.phone.fullNumber+' направлен код подтверждения. Попыток: '+(tfa.data.attemptLimit.total-tfa.data.attemptLimit.current)+' из '+tfa.data.attemptLimit.total+'Введите полученный код', null, {
-		var pin=AnyBalance.retrieveCode('На Ваш номер направлен код подтверждения. Введите полученный код', null, {
-						inputType: 'number',
-						minLength: 4,
-						maxLength: 4,
-						time: 180000//(tfa.data.timeSecondsLimit.total-tfa.data.timeSecondsLimit.current)
-					});
-		var tfa=callAPI('AAC/2FA/Confirm/v1/get',{lang:"ru",data:{code:code,pin:pin.toString()}})
-	   }else{
-		AnyBalance.trace(JSON.stringify(json));
-		throw new AnyBalance.Error('Не поддерживаемый тип подтверждения авторизации:'+json.data.tfa.action)
-	   }
-
+	
+	if(json.data.tfa && json.data.tfa.isNeeded){
+	    if (json.data.tfa.action == 'enter_pin'){
+	   	    var code = json.data.tfa.code;
+		    var tfa = callAPI('AAC/SmsInfo/Limits/v1/get',{lang:"ru",confirmationToken:code})
+//		    var pin = AnyBalance.retrieveCode('На Ваш номер +'+json.data.tfa.smsinfo.phone.fullNumber+' направлен код подтверждения. Попыток: '+(tfa.data.attemptLimit.total-tfa.data.attemptLimit.current)+' из '+tfa.data.attemptLimit.total+'Введите полученный код', null, {
+		    var pin = AnyBalance.retrieveCode('На Ваш номер направлен код подтверждения. Введите полученный код', null, {
+				inputType: 'number',
+				minLength: 4,
+				maxLength: 4,
+				time: 180000//(tfa.data.timeSecondsLimit.total-tfa.data.timeSecondsLimit.current)
+			});
+		    var tfa = callAPI('AAC/2FA/Confirm/v1/get', {lang: "ru", data: {code: code, pin: pin.toString()}})
+	    }else{
+		    AnyBalance.trace(JSON.stringify(json));
+		    throw new AnyBalance.Error('Не поддерживаемый тип подтверждения авторизации: ' + json.data.tfa.action);
+	    }
 	}
+	
 	return json;
 }
 
@@ -261,7 +254,6 @@ function mainAppAPI(prefs) {
     AnyBalance.setResult(result);
 }
 
-	
 function cnonceGenerator() {
 	return getRandomString(16, 32);
 }

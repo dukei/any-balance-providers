97174c8a5a3d51ff54e463e3efcfb1e8df8d73f0
Dmitry Kochin <dco@mail.ru>
2023-07-25 12:08:15 +0300

    Рошаль-Онлайн (ab-internet-roshal-online): v.3:  	- Поддержка нового личного кабинета

--- providers/ab-internet-roshal-online/main.js
+++ providers/ab-internet-roshal-online/main.js
@@ -3,96 +3,127 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+	'Cache-Control': 'max-age=0',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+};
+
+var baseurl = 'http://netstat.atnr.ru/';
+
+var acc_status = {
+	true: 'Активный',
+	false: 'Неактивный',
+	undefined: 'Не определен'
 };
 
 function main() {
     var prefs = AnyBalance.getPreferences();
-    var baseurl = 'http://netstat.atnr.ru/';
+    
     AnyBalance.setDefaultCharset('utf-8');
 
     checkEmpty(prefs.login, 'Введите логин!');
     checkEmpty(prefs.password, 'Введите пароль!');
 
-    var html = AnyBalance.requestGet(baseurl, g_headers);
+    var html = AnyBalance.requestGet(baseurl + 'cabinet/', addHeaders({'Referer': baseurl}));
 
     if(!html || AnyBalance.getLastStatusCode() > 400){
             AnyBalance.trace(html);
-            throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+            throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
     }
-
-    var params = createFormParams(html, function(params, str, name, value) {
-            if (name == 'login') 
-                    return prefs.login;
-            else if (name == 'password')
-                    return prefs.password;
-            return value;
-    });
-
-    html = AnyBalance.requestPost(baseurl, params, addHeaders({Referer: baseurl}));
-
-    if (!/logout/i.test(html)) {
-            var error = getParam(html, null, null, /<p[^>]*>([\s\S]*?)<\/p>/i, replaceTagsAndSpaces, html_entity_decode);
-            if (error)
-                    throw new AnyBalance.Error(error, null, /Неверно указаны логин или пароль/i.test(error));
-            AnyBalance.trace(html);
-            throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-
-    var result = {success: true};
-    
-    function RX(title) {
-        return RegExp('<tr[^>]*>\\s*<td[^>]*?utm-cell[^>]*>\s*' +
-            title.replace(/\s+/g, '\\s+') +
-            ':?\s*</td\\s*>\\s*<td[^>]*>\\s*([^<]+)', 'i');
-    }
-    function RXCell(n, g) {
-        return RegExp('<tr[^>]*>(?:[\\s\\S]*?<td[^>]*?utm-cell[^>]*>){' + n + '}([^<]+)', g ? 'ig': 'i');
-    }
-    
-    var table = AB.getElement(html, /<table[^>]*?utm-table/i);
-    
-    if (!table) {
-        AnyBalance.trace(html);
-        throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
-    }
-    
-    getParam(table, result, 'status', RX('состояние интернета'), replaceTagsAndSpaces);
-    getParam(table, result, 'balance', RX('Баланс'), replaceTagsAndSpaces, parseBalance);
-    getParam(table, result, 'fio', RX('ФИО'), replaceTagsAndSpaces);
-    getParam(table, result, 'credit', RX('Кредит'), replaceTagsAndSpaces, parseBalance);
-    getParam(table, result, 'accountID', RX('Основной лицевой счет'), replaceTagsAndSpaces);
-    
-    if (AnyBalance.isAvailable('adress')) {
-        html=AnyBalance.requestGet(baseurl+'?module=user_details', g_headers);
-        table = AB.getElement(html, /<table[^>]*?utm-table/i);
-        getParam(table, result, 'adress', RX('Фактический адрес'), replaceTagsAndSpaces);
-    }
-
-    if (AnyBalance.isAvailable(['currentTariff', 'deadline', 'cost', 'writtenOff'])) {
-        html=AnyBalance.requestGet(baseurl+'?module=41_services', g_headers);
-        table = AB.getElement(html, /<table[^>]*?utm-table/i);
+	
+	html = AnyBalance.requestPost(baseurl + 'customer_api/login', JSON.stringify({
+        'login': prefs.login,
+        'password': prefs.password,
+        'mobile_telephone': ''
+    }), addHeaders({'Content-Type': 'application/json;charset=UTF-8', 'Referer': baseurl + 'cabinet/'}));
+	
+	var json = getJson(html);
+	
+	if(!json.sid_customer || json.error){
+		var error = json.error;
+		if(error)
+			throw new AnyBalance.Error(error, null, /user|логин|password|парол/i.test(html));
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+	}
+	
+	var result = {success: true};
+	
+	html = AnyBalance.requestGet(baseurl + 'customer_api/auth/profile', g_headers);
+	
+	var json = getJson(html);
+	
+	AnyBalance.trace('Profile: ' + JSON.stringify(json));
+	
+	for(var i=0; i<json.tariffs.length; ++i){
+		sumParam(json.tariffs[i].name, result, '__tariff', null, null, null, create_aggregate_join(', '));
+		sumParam(json.tariffs[i].name, result, 'currentTariff', null, null, null, create_aggregate_join(',\n '));
+	}
+	
+	getParam(json.balance + '', result, 'balance', null, null, parseBalance);
+	getParam(json.credit + '', result, 'credit', null, null, parseBalance);
+	getParam(json.payment_in_month + '', result, 'cost', null, null, parseBalance);
+	getParam(json.basic_account, result, 'accountID');
+	if(json.actual_address)
+	    getParam(json.actual_address, result, 'adress');
+	if(json.email)
+	    getParam(json.email, result, 'email');
+	if(json.mobile_telephone)
+	    getParam(json.mobile_telephone, result, 'phone', null, [replaceTagsAndSpaces, /.*(\d{3})-(\d{3})-(\d{2})-(\d{2})$/, '+7 $1 $2-$3-$4']);
+	getParam(json.full_name, result, 'fio');
+	
+	if(json.accounts && json.accounts.length > 0){
+		var ag = [];
+		var s = {};
+	    for(var i=0; i<json.accounts.length; ++i){
+		    var servicesGroups = json.accounts[i].services;
+		    for(var j=0; j<servicesGroups.length; ++j){
+				var cost = getParam(servicesGroups[j].cost + '', null, null, null, null, parseBalance);
+				
+				sumParam(servicesGroups[j].name + ': ' + cost + ' ₽', s, '__s', null, null, null, create_aggregate_join(',\n '));
+		    }
+			ag.push(s.__s);
+	    }
+		result['services'] = ag.join('.\n ');
+	}else{
+		AnyBalance.trace('Не удалось получить информацию по услугам');
+	}
+	
+	html = AnyBalance.requestGet(baseurl + 'customer_api/auth/statistics', g_headers);
+	
+	var json = getJson(html);
+	
+	AnyBalance.trace('Statistics: ' + JSON.stringify(json));
+	
+	if(json && json.length > 0){
+	    var dtc = new Date();
+	    var dtcPeriod = n2(dtc.getMonth()+1) + '.' + dtc.getFullYear();
         
-        sumParam(table, result, 'cost', RXCell(6, true), replaceTagsAndSpaces, parseBalance, AB.aggregate_sum);
-        sumParam(table, result, 'writtenOff', RXCell(7, true), replaceTagsAndSpaces, parseBalance, AB.aggregate_sum);
-        getParam(table, result, 'currentTariff', RXCell(3), replaceTagsAndSpaces);
-        getParam(table, result, 'deadline', RXCell(5), replaceTagsAndSpaces, parseDate);
-    }
-
-
-
-
-    /*
-        
-		<counter id="currentTariff" name="Текущий тариф" type="text"/>
-		<counter id="cost" name="Абонентская плата" units=" p"/>
-		<counter id="writtenOff" name="Списано абонентской платы" units=" p"/>
-		<counter id="deadline" name="Конец расчётного периода" type="time" format="dd/mm/yyyy"/>
-        */
-
-	AnyBalance.setResult(result);
+		var dte = new Date(dtc.getFullYear(), dtc.getMonth()+1, 1);
+		var dtEndPeriod = n2(dte.getDate()) + '.' + n2(dte.getMonth()+1) + '.' + dte.getFullYear();
+		
+		getParam(dtEndPeriod, result, 'deadline', null, null, parseDate);
+	
+	    var monthes = {0: 'Январь', 1: 'Февраль', 2: 'Март', 3: 'Апрель', 4: 'Май', 5: 'Июнь', 6: 'Июль', 7: 'Август', 8: 'Сентябрь', 9: 'Октябрь', 10: 'Ноябрь', 11: 'Декабрь'};
+	
+	    getParam(monthes[dtc.getMonth()] + ' ' + dtc.getFullYear(), result, 'period');
+	
+	    for(var i=0; i<json.length; ++i){
+		    var dt = new Date(json[i].actual_date*1000);
+			var dtPeriod = n2(dt.getMonth()+1) + '.' + dt.getFullYear();
+		
+		    if(dtPeriod == dtcPeriod && json[i].payment_incurrency < 0){
+		        sumParam(json[i].payment_incurrency, result, 'writtenOff', null, null, parseBalance, aggregate_sum);
+		    }
+	    }
+	}else{
+		AnyBalance.trace('Не удалось получить информацию по статистике');
+	}
+	
+    AnyBalance.setResult(result);
 }

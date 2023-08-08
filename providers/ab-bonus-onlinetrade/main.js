f52f4358c493bdd0ac5c304375e2194133d34692
Dmitry Kochin <dco@mail.ru>
2023-08-08 12:15:29 +0300

    ОнЛайн Трейд (ab-bonus-onlinetrade): v.10:  	- Поддержка API мобильного приложения.

--- providers/ab-bonus-onlinetrade/main.js
+++ providers/ab-bonus-onlinetrade/main.js
@@ -3,14 +3,15 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; AUM-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Mobile Safari/537.36',
 };
 
-var baseurl = 'https://www.onlinetrade.ru/';
+var baseurl = 'https://m.onlinetrade.ru/';
 var g_savedData;
 var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'];
 
@@ -31,23 +32,15 @@ function main() {
 	
 	if (!/\?log_out=1/i.test(html)) {
 		AnyBalance.trace('Сессия новая. Будем логиниться заново...');
-		clearAllCookies();
-	
-	    var html = AnyBalance.requestGet(baseurl + 'member/login.html', g_headers);
+		clearAllCookiesExceptProtection();
+		
+		var html = AnyBalance.requestGet(baseurl, addHeaders({"Referer": "android-app://ru.onlinetrade.app/"}));
+		
+		html = AnyBalance.requestGet(baseurl + 'member/login.html', g_headers);
 		
 		if(!html || AnyBalance.getLastStatusCode() > 400){
 	    	AnyBalance.trace(html);
 	    	throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
-	    }
-		
-	    var cE = getParam(html, /var\s+cE\s*=\s*['"]([^"']*)/);
-	    if(cE){
-	    	AnyBalance.trace('Fooling stormwall');
-	    	var cK = getParam(html, /var\s+cK\s*=\s*(\d+)/, null, parseBalance);
-	    	var cookie = generateCookieValue(cK, cE);
-	    	AnyBalance.trace('swp_token: ' + cookie);
-	    	AnyBalance.setCookie('www.onlinetrade.ru', 'swp_token', cookie);
-			html = AnyBalance.requestGet(baseurl + 'member/login.html', g_headers);
 	    }
 
 	    var form = getElements(html, [/<form/ig, /password/i])[0];
@@ -71,8 +64,8 @@ function main() {
 	    });
 	
 	    html = AnyBalance.requestPost(baseurl + 'member/login.html', params, addHeaders({Referer: baseurl + 'member/login.html'}));
-	
-	    if (!/\?log_out=1/i.test(html)) {
+		
+		if (!/\?log_out=1/i.test(html)) {
 	    	var error = getElement(html, /<[^>]+coloredMessage__red/i, replaceTagsAndSpaces);
 	    	if (error)
 	    		throw new AnyBalance.Error(error, null, /логин|парол/i.test(error));
@@ -108,17 +101,14 @@ function main() {
 	if (AnyBalance.isAvailable('last_oper_sum', 'last_oper_date', 'last_oper_name', 'last_oper_type')) {
  		html = AnyBalance.requestGet(baseurl + 'member/bonuses.html', g_headers);
 			
-	    var table = getElement(html, /<table[^>]+id="tableONBonus__ID[^>]*>/i);
-		if (table) {
-			var tbody = getElement(table, /<tbody[^>]*>/i);
-			var hist = getElements(tbody, /<tr[^>]*>/ig)[0];
-	    }
+	    var table = getElement(html, /<div[^>]+id="tableONBonus__ID[^>]*>/i);
+	    var item = getElements(table, /<div[^>]+class="editableList__item[^>]*>/ig)[0];
 		
-		if (hist) {
-			getParam(hist, result, 'last_oper_sum', /<td>(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
-		    getParam(hist, result, 'last_oper_date', /<td>(?:[\s\S]*?<span[^>]*>){1}([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseDate);
-			getParam(hist, result, 'last_oper_name', /<td>(?:[\s\S]*?<a[^>]*>){1}([\s\S]*?)<\/a>/i, replaceTagsAndSpaces, capitalFirstLetters);
-		    getParam(hist, result, 'last_oper_type', /<td>(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, capitalFirstLetters);
+		if (item) {
+			getParam(item, result, 'last_oper_sum', /<span[^>]+class="(?:green|red)">[\s\S]*?<\/span>([\s\S]*?)<span/i, replaceTagsAndSpaces, parseBalance);
+		    getParam(item, result, 'last_oper_date', /<div[^>]*>(?:[\s\S]*?<span[^>]*>){1}([\s\S]*?)<\/span/i, replaceTagsAndSpaces, parseDate);
+			getParam(item, result, 'last_oper_name', /<div[^>]*>(?:[\s\S]*?<a[^>]*>){1}([\s\S]*?)<\/a/i, replaceTagsAndSpaces);
+		    getParam(item, result, 'last_oper_type', /<span[^>]+class="(?:green|red)">([\s\S]*?)[.,:;-]*?<\/span/i, replaceTagsAndSpaces, capitalFirstLetters);
 		} else {
 			AnyBalance.trace('Не удалось получить данные по последней операции');
 		}
@@ -133,4 +123,8 @@ function main() {
 	}
 	
 	AnyBalance.setResult(result);
+}
+
+function clearAllCookiesExceptProtection(){
+	clearAllCookies(function(c){return!/spid|spsc/i.test(c.name)})
 }

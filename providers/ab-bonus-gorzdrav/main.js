b79fa50a0c3500cb60a2a60e35ba2feb7bf508a3
Dmitry Kochin <dco@mail.ru>
2023-08-08 12:17:52 +0300

    Горздрав (Бонусы) (ab-bonus-gorzdrav): v.3:  	- Поддержка изменений на сайте

--- providers/ab-bonus-gorzdrav/main.js
+++ providers/ab-bonus-gorzdrav/main.js
@@ -5,18 +5,18 @@
 
 var g_headers = {
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-	'Accept-Encoding': 'gzip, deflate, br',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.6,en;q=0.4',
     'Cache-Control': 'max-age=0',
 	'Connection': 'keep-alive',
-	'Host': 'gorzdrav.org',
     'Referer': 'https://gorzdrav.org/',
+	'Upgrade-Insecure-Requests': '1',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
 };
 
+var g_status = {Active: 'Активна', Inactive: 'Не активна', Blocked: 'Заблокирована'};
+
 var baseurl = 'https://gorzdrav.org';
-var g_csrf;
 var g_savedData;
 var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'];
 
@@ -32,7 +32,7 @@ function main() {
 	
 	var html = AnyBalance.requestGet(baseurl + '/', addHeaders({'Upgrade-Insecure-Requests': 1}));
 	
-	if (!html || AnyBalance.getLastStatusCode() > 500) {
+	if (!html || AnyBalance.getLastStatusCode() > 400) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Сайт провайдера временно недоступен! Попробуйте обновить данные позже');
 	}
@@ -47,24 +47,39 @@ function main() {
 	
 	var result = {success: true};
 	
-	html = AnyBalance.requestGet(baseurl + '/my-account/my-profile', addHeaders({'Upgrade-Insecure-Requests': 1}));
-		
-	getParam(html, result, 'balance', /Активные бонусы[\s\S]*?form-label[^>]*>\(([\s\S]*?)\)<\/span>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'bonuses_active', /Активные бонусы[\s\S]*?card__value[^>]*>([\s\S]*?)<span/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'bonuses_inactive', /Неактивные бонусы[\s\S]*?card__value[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'bonuses_burn', /card__balance b-icn--fire">([\s\S]*?)сгорит/i, replaceTagsAndSpaces, parseBalance);
-	getParam(html, result, 'bonuses_till', /card__balance b-icn--fire">[\s\S]*?сгорит<br>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, parseDate);
+	html = AnyBalance.requestGet(baseurl + '/my-account/my-profile', g_headers);
+	
+	getParam(html, result, 'email', /name="email"[\s\S]*?value="([\s\S]*?)"/i, replaceTagsAndSpaces);
+	getParam(html, result, 'phone', /name="phone"[\s\S]*?value="([\s\S]*?)"/i, replaceNumber);
 	
 	var firstName = getParam(html, null, null, /name="firstName"[\s\S]*?value="([\s\S]*?)"/i, replaceTagsAndSpaces);
 	var middleName = getParam(html, null, null, /name="middleName"[\s\S]*?value="([\s\S]*?)"/i, replaceTagsAndSpaces);
+	var lastName = getParam(html, null, null, /name="lastName"[\s\S]*?value="([\s\S]*?)"/i, replaceTagsAndSpaces);
 	var fio = firstName;
 	if (middleName)
 		fio += ' ' + middleName;
+	if (lastName)
+		fio += ' ' + lastName;
 	getParam(fio, result, 'fio', null, replaceTagsAndSpaces);
-	getParam(html, result, '__tariff', /Номер карты[\s\S]*?card__number[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
-	getParam(html, result, 'cardnum', /Номер карты[\s\S]*?card__number[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
-	getParam(html, result, 'cardstate', /Статус карты[\s\S]*?card__value[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, capitalFirstLetters);
-	getParam(html, result, 'phone', /name="phone"[\s\S]*?value="([\s\S]*?)"/i, replaceNumber);
+	
+	var csrfToken = getParam(html, /name="CSRFToken"[^>]*value="([^"]*)/i, replaceHtmlEntities);
+	
+	html = AnyBalance.requestPost(baseurl + '/loyaltyCard', {'CSRFToken': csrfToken}, addHeaders({
+		'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+		'Referer': baseurl + '/profile'
+	}));
+	
+	var json = getJson(html);
+	
+	getParam(json.active, result, 'balance', null, null, parseBalance);
+	getParam(json.active, result, 'bonuses_active', null, null, parseBalance);
+	getParam(json.inactive, result, 'bonuses_inactive', null, null, parseBalance);
+	getParam(json.amounts[0].amount, result, 'bonuses_burn', null, null, parseBalance);
+	getParam(json.amounts[0].expirationDate, result, 'bonuses_till', null, null, parseDateISO);
+	
+	getParam(json.barcode, result, '__tariff');
+	getParam(json.barcode, result, 'cardnum');
+	getParam(g_status[json.status]||json.status, result, 'cardstate');
 
 	AnyBalance.setResult(result);
 }
@@ -84,16 +99,21 @@ function loginSite(prefs) {
 	
 	var html = AnyBalance.requestGet(baseurl + '/', addHeaders({'Upgrade-Insecure-Requests': 1}));
 	
-	if (!html || AnyBalance.getLastStatusCode() > 500) {
+	if (!html || AnyBalance.getLastStatusCode() > 400) {
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Сайт провайдера временно недоступен! Попробуйте обновить данные позже');
 	}
 	
-	g_csrf = getParam(html, /name="CSRFToken"[^>]*value="([^"]*)/i, replaceHtmlEntities);
+	if (/cloudflare/.test(html)){
+       	AnyBalance.trace(html);
+	    throw new AnyBalance.Error('Обнаружена защита от роботов. Попробуйте обновить провайдер не ранее, чем через 24 часа');
+    }
 	
-	if(!g_csrf){
+	var csrfToken = getParam(html, /name="CSRFToken"[^>]*value="([^"]*)/i, replaceHtmlEntities);
+	
+	if(!csrfToken){
 		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось найти токен для авторизации. Сайт изменен?');
+		throw new AnyBalance.Error('Не удалось найти токен авторизации. Сайт изменен?');
 	}
 	
 	var captcha = solveRecaptcha('Пожалуйста, подтвердите, что вы не робот', AnyBalance.getLastUrl(), '6Le2gr8UAAAAAF46W1xVYXBYnFAsUxz73HU4CBSP', {USERAGENT: g_headers['User-Agent']});
@@ -103,28 +123,37 @@ function loginSite(prefs) {
 		'authorizationFormType': 'PHONE',
         'g-recaptcha-response': captcha,
 		'isVisibleCaptcha': false,
-        'CSRFToken': g_csrf
+        'CSRFToken': csrfToken
     }, addHeaders({
 		'Accept': '*/*',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-		'Host': 'gorzdrav.org',
 		'Origin': baseurl,
 		'Referer': baseurl + '/',
 		'X-Requested-With': 'XMLHttpRequest'
 	}));
 	
-	var code = AnyBalance.retrieveCode('Пожалуйста, введите код подтверждения, высланный на номер ' + prefs.login, null, {inputType: 'number', time: 180000});
+	if (AnyBalance.getLastStatusCode() >= 400) {
+		if(json.global_error){
+			var error = json.global_error;
+		    AnyBalance.trace(html);
+	    	throw new AnyBalance.Error(error, null, null, /ошибка|смс/i.test(error));
+		}
+		
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось отправить SMS. Сайт изменен?');
+	}
+	
+	var code = AnyBalance.retrieveCode('Пожалуйста, введите код подтверждения, высланный на номер +7' + prefs.login, null, {inputType: 'number', time: 180000});
 	
 	html = AnyBalance.requestPost(baseurl + '/j_spring_security_check', {
         'j_username': formattedLogin,
 		'authorizationFormType': 'PHONE',
 		'isVisibleCaptcha': false,
         'j_password': code,
-        'CSRFToken': g_csrf
+        'CSRFToken': csrfToken
     }, addHeaders({
 		'Accept': '*/*',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-		'Host': 'gorzdrav.org',
 		'Origin': baseurl,
 		'Referer': baseurl + '/',
 		'X-Requested-With': 'XMLHttpRequest'

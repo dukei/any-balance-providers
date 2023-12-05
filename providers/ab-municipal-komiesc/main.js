94cb168ab0353ac9b6b17f2bd0c894828339528c
Dmitry Kochin <dco@mail.ru>
2023-12-05 14:40:29 +0300

    Коми энергосбытовая компания (ab-municipal-komiesc): v.5:  	- Улучшена обработка ошибок

--- providers/ab-municipal-komiesc/main.js
+++ providers/ab-municipal-komiesc/main.js
@@ -3,11 +3,12 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	'Accept-Language': 'en-GB,en;q=0.9,ru-RU;q=0.9,ru;q=0.8,en-US;q=0.7',
+	'Cache-Control': 'max-age=0',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
 };
 
 function main() {
@@ -20,42 +21,142 @@ function main() {
 	
 	var html = AnyBalance.requestGet(baseurl + 'cabinet', g_headers);
 	
-	if(!html || AnyBalance.getLastStatusCode() > 400){
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
-	}
+	if(!html || AnyBalance.getLastStatusCode() >= 400){
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
+    }
 
-	var form = getParam(html, null, null, /user-login-form[^>]*>([^]*?)<\/form>/i);
-	if(!form)
-		throw new AnyBalance.Error('Не найдена форма входа. Сайт изменен.');
+	var form = getElement(html, /<form[^>]+user-login-form[^>]*>/i);
+	if(!form){
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Не удалось найти форму входа. Сайт изменен?');
+    }
 	
 	var params = createFormParams(form, function(params, str, name, value) {
-		if (name == 'name') 
-			return prefs.login;
-		else if (name == 'pass')
-			return prefs.password;
+		if(name == 'name'){
+	   		return prefs.login;
+    	}else if(name == 'pass'){
+	    	return prefs.password;
+	    }
+		
 		return value;
 	});
 	
-	html = AnyBalance.requestPost(baseurl + 'cabinet?destination=cabinet', params, addHeaders({Referer: baseurl + 'login'}));
+	var action = getParam(form, null, null, /<form[\s\S]*?action="([^"]*)/i, replaceHtmlEntities);
 	
-	// Ошибка может быть в ЛК
-	var error = getParam(html, null, null, /<div[^>]+class="messages error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
-	if (error)
-		throw new AnyBalance.Error(error, null, /Введенные Вами имя пользователя или пароль неверны/i.test(error));
-
-	if (!/logout/i.test(html)) {
+	html = AnyBalance.requestPost(joinUrl(baseurl, action), params, addHeaders({Referer: baseurl + 'login'}));
+	
+    if(!/logout/i.test(html)){
+		var error = getParam(html, null, null, /<div[^>]+class="messages error"[^>]*>([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	    if(error)
+		    throw new AnyBalance.Error(error, null, /пользовател|парол/i.test(error));
+		
 		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+		throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
 	}
-
-	html = AnyBalance.requestGet(baseurl + 'cabinet', g_headers);
 	
 	var result = {success: true};
 	
-	getParam(html, result, 'balance', /Состояние расчетов:[^>]*>\s*<div[^>]*>([^<]+)/i, [replaceTagsAndSpaces, 'долг', '-'], parseBalance);
-	getParam(html, result, 'account', /Номер договора:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, '__tariff', /Адрес:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	if(/info_messages/i.test(AnyBalance.getLastUrl())){
+		AnyBalance.trace('Сайт вывел информационное сообщение. Переходим принудительно');
+		
+		html = AnyBalance.requestGet(baseurl, g_headers);
+	}
+	
+	if(/maindata_u/i.test(AnyBalance.getLastUrl())){
+		AnyBalance.trace('Мы в кабинете для юридических лиц. Получаем основные данные');
+		var token = getParam(html, null, null, /<div[^>]+dummy-block-token="([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
+		
+		html = AnyBalance.requestGet('https://cabinet.komiesc.ru:8080/load2_content/data_request?token=' + token, g_headers);
+		
+		var info = JSON.parse(html).content;
+		
+		if(!info){
+		    var tries = 0, maxTries = 10;
+		    do{
+			    AnyBalance.trace('Не удалось получить основные данные. Повторная попытка: ' + (tries + 1) + ' из ' + maxTries);
+			    html = AnyBalance.requestGet(baseurl + 'maindata_u', g_headers);
+			
+			    token = getParam(html, null, null, /<div[^>]+dummy-block-token="([^"]*)/i, replaceTagsAndSpaces, html_entity_decode);
+			
+		        html = AnyBalance.requestGet('https://cabinet.komiesc.ru:8080/load2_content/data_request?token=' + token, g_headers);
+		
+		        var info = JSON.parse(html).content;
+			    AnyBalance.trace('Сервер вернул: ' + html);
+		    }while(!info && (++ tries < maxTries));
+		}
+		
+		if(info && info == null){
+		    AnyBalance.trace(html);
+		    throw new AnyBalance.Error('Не удалось получить основные данные. Попробуйте еще раз позже');
+	    }
+		
+		if(info && !/content_table/i.test(info)){
+		    AnyBalance.trace(html);
+		    throw new AnyBalance.Error('Данных для отображения не найдено. Попробуйте еще раз позже');
+	    }
+		
+		if(!info){
+		    AnyBalance.trace(html);
+		    throw new AnyBalance.Error('Не удалось получить основные данные. Сайт изменен?');
+	    }
+		
+        result.account = prefs.login;
+		result.__tariff = prefs.login;
+        
+	    getParam(info, result, 'fio', /Покупатель[\s\S]*?tcell[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(info, result, 'servicecontract', /Номер договора[\s\S]*?tcell[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+		getParam(info, result, 'servicebalance', /Состояние расчетов[\s\S]*?tcell[^>]*>([\s\S]*?)<\/td>/i, [replaceTagsAndSpaces, 'долг', '-'], parseBalance);
+	    getParam(info, result, 'serviceprovider', /Продавец[\s\S]*?tcell[^>]*>([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, html_entity_decode);
+		    
+        getParam(info, result, 'balance', /Состояние расчетов[\s\S]*?tcell[^>]*>([\s\S]*?)<\/td>/i, [replaceTagsAndSpaces, 'долг', '-'], parseBalance);
+	}else{
+		AnyBalance.trace('Мы в кабинете для физиических лиц. Получаем данные по договорам');
+		
+		html = AnyBalance.requestGet(baseurl + 'cabinet', g_headers);
+	
+	    getParam(html, result, 'account', /<div[^>]+id="client_no">[\s\S]*?Номер клиента:\s*([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	    getParam(html, result, '__tariff', /<div[^>]+id="client_no">[\s\S]*?Номер клиента:\s*([\s\S]*?)<\/div>/i, replaceTagsAndSpaces, html_entity_decode);
+	    getParam(html, result, 'lodgers', /Кол-во проживающих:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, parseBalance);
+	    getParam(html, result, 'address', /Адрес:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	    getParam(html, result, 'fio', /Покупатель:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+	
+	    // Получаем услуги
+	    var info = getElement(html, /<div[^>]+class="info_column_block"[^>]*>/i);
+		
+		if(!info){
+		    AnyBalance.trace(html);
+		    throw new AnyBalance.Error('Не удалось получить информацию. Сайт изменен?');
+	    }
+		
+	    var services = getElements(info, /<div[^>]+class="info_column_secondary"[^>]*>/ig);
+	    
+	    if(services && services.length > 0){
+		    AnyBalance.trace('Найдено договоров: ' + services.length + '. Получаем информацию по каждому договору');
+	        for(var i=0; i<services.length; ++i){
+	            var service = services[i];
+		        var scontract = (i >= 1 ? 'servicecontract' + (i + 1) : 'servicecontract');
+		        var sbalance = (i >= 1 ? 'servicebalance' + (i + 1) : 'servicebalance');
+	            var sprovider = (i >= 1 ? 'serviceprovider' + (i + 1) : 'serviceprovider');
+                
+                getParam(service, result, scontract, /Номер договора:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		        getParam(service, result, sbalance, /Состояние расчетов:[^>]*>\s*<div[^>]*>([^<]+)/i, [replaceTagsAndSpaces, 'долг', '-'], parseBalance);
+	            getParam(service, result, sprovider, /Поставщик:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		        
+                sumParam(service, result, 'balance', /Состояние расчетов:[^>]*>\s*<div[^>]*>([^<]+)/i, [replaceTagsAndSpaces, 'долг', '-'], parseBalanceSilent, aggregate_sum);
+		        
+		        if (i>=4)
+		            break;
+	        }
+	    }else{
+		    AnyBalance.trace('Других договоров не найдено. Получаем информацию по основному договору');
+		    getParam(info, result, 'servicecontract', /Номер договора:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		    getParam(info, result, 'servicebalance', /Состояние расчетов:[^>]*>\s*<div[^>]*>([^<]+)/i, [replaceTagsAndSpaces, 'долг', '-'], parseBalance);
+	        getParam(info, result, 'serviceprovider', /Поставщик:[^>]*>\s*<div[^>]*>([^<]+)/i, replaceTagsAndSpaces, html_entity_decode);
+		       
+            getParam(info, result, 'balance', /Состояние расчетов:[^>]*>\s*<div[^>]*>([^<]+)/i, [replaceTagsAndSpaces, 'долг', '-'], parseBalance);
+	    }
+    }
 	
 	AnyBalance.setResult(result);
 }

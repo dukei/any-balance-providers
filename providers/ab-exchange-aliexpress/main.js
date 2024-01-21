80097525a10cf2b2511a40f2e75be67d2f069796
Dmitry Kochin <dco@mail.ru>
2024-01-21 13:48:30 +0300

    Курс AliExpress (ab-exchange-aliexpress): v.8:  	- Исправлено получение информации в некоторых случаях.

--- providers/ab-exchange-aliexpress/main.js
+++ providers/ab-exchange-aliexpress/main.js
@@ -4,51 +4,78 @@
 */
 
 var g_headers = {
-  'Accept': 'application/json, text/javascript, */*; q=0.01',
-  'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-  'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-  'X-Requested-With': 'XMLHttpRequest',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
 };
 
 function main() {
   	AnyBalance.setDefaultCharset('utf-8');
 	
-	var html = AnyBalance.requestGet('https://moneyfromnothing.ru/blog/aliexpress-kurs-dollara/rates_data.json?_=' + new Date().getTime(), g_headers);
+	var html = AnyBalance.requestGet('https://helpix.ru/currency/', g_headers);
 	
 	if (!html || AnyBalance.getLastStatusCode() > 400){
 		AnyBalance.trace(html);
 		throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
 	}
 	
-	var json = getJson(html);
-	AnyBalance.trace(JSON.stringify(json));
-	
 	var result = {success: true};
 	
-	getParam(json.updateTime, result, 'date', null, null, parseDateISO);
-	getParam(json.currentRateAliExpress, result, 'rate', null, null, parseBalanceRound);
-	getParam(json.currentRateAliExpressRu, result, 'rateru', null, null, parseBalanceRound);
-	getParam(json.currentRateCbr, result, 'ratecbrf', null, null, parseBalanceRound);
-	getParam(json.updateTime && json.updateTime.replace(/(\d{4})-(\d{2})-(\d{2})(.*)/,'$3.$2.$1'), result, '__tariff');
+	var table = getElement(html, /<table[^>]+class="b-tabcurr">/i);
+	var rows = getElements(table, [/<tr[^>]+class="b-tabcurr__tr"[^>]*>/ig, /b-currency/i]);
 	
-	if(AnyBalance.isAvailable('date'))
-	    getParam(json.updateTime && json.updateTime.replace(/(\d{4})-(\d{2})-(\d{2})(.*)/,'$3.$2.$1'), result, 'date', null, null, parseDate);
+	if (!table || !rows){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось найти таблицу курсов. Сайт изменен?');
+	}
+	
+	if(rows.length && rows.length > 0){
+	    AnyBalance.trace('Найдено строк с курсами: ' + rows.length);
+		getParam(rows[0], result, 'date', /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, [replaceTagsAndSpaces, /(\d{4})-(\d{2})-(\d{2})(.*)/, '$3.$2.$1'], parseDate);
+		getParam(rows[0], result, '__tariff', /(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, [replaceTagsAndSpaces, /(\d{4})-(\d{2})-(\d{2})(.*)/, '$3.$2.$1']);
+		for(var i=0; i<rows.length; ++i){
+	    	var row = rows[i];
+			
+			var rate = getParam(row, null, null, /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalanceSilent);
+			var rateru = getParam(row, null, null, /(?:[\s\S]*?<td[^>]*>){3}<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalanceSilent);
+			var ratecbrf = getParam(row, null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalanceSilent);
+			
+			if(rate && !result.rate)
+				getParam(rate, result, 'rate', null, null, parseBalanceRound);
+			
+			if(rateru && !result.rateru)
+				getParam(rateru, result, 'rateru', null, null, parseBalanceRound);
+			
+			if(ratecbrf && !result.ratecbrf)
+				getParam(ratecbrf, result, 'ratecbrf', null, null, parseBalanceRound);
+			
+			if(i>=9 || (result.rate && result.rateru && result.ratecbrf)) break;
+	    }
+	}else{
+		AnyBalance.trace('Не удалось получить информацию по курсам');
+	}
 	
 	if (AnyBalance.isAvailable('trend', 'averrate')) {
-	    if (json.ratesAliExpress && json.ratesAliExpress.length>0){
+	    if (rows && rows.length>0){
 			var __rate;
-	    	for(var i=0; i<json.ratesAliExpress.length; ++i){
-	    		var rate = json.ratesAliExpress[i].rate;
-	    		sumParam(rate, result, '__rate', null, null, parseBalanceSilent, aggregate_sum);
-	    	}
-		
-	    	var weekAgoRate = json.ratesAliExpress[0].rate;
-	    	var period = json.ratesAliExpress.length;
+			var period = 0;
+	    	for(var i=0; i<rows.length; ++i){
+	    		sumParam(rows[i], result, '__rate', /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalanceSilent, aggregate_sum);
+	    	    
+			    if(result.__rate) period += 1;
+				
+				if(i>=9) break;
+			}
+		    
+	    	var weekAgoRate = getParam(rows[period], null, null, /(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalanceSilent);
 	    	var averrageRate = result.__rate/period;
 			delete result.__rate;
 			getParam(averrageRate, result, 'averrate', null, null, parseBalanceRound);
-	        	
+	        
 	    	if(result.rate > weekAgoRate){
 	    		result.trend = 'AG$ ↑';
 	    	}else if(result.rate < weekAgoRate){
@@ -60,15 +87,18 @@ function main() {
 	}
 	
 	if (AnyBalance.isAvailable('trendru', 'averrateru')) {
-	    if (json.ratesAliExpressRu && json.ratesAliExpressRu.length>0){
+	    if (rows && rows.length>0){
 			var __rateru;
-	    	for(var i=0; i<json.ratesAliExpressRu.length; ++i){
-	    		var rate = json.ratesAliExpressRu[i];
-	    		sumParam(rate.rate, result, '__rateru', null, null, parseBalanceSilent, aggregate_sum);
-	    	}
-	        	
-	    	var weekAgoRate = json.ratesAliExpressRu[0].rate;
-	    	var period = json.ratesAliExpressRu.length;
+			var period = 0;
+	    	for(var i=0; i<rows.length; ++i){
+				sumParam(rows[i], result, '__rateru', /(?:[\s\S]*?<td[^>]*>){3}<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalanceSilent, aggregate_sum);
+	    	    
+				if(result.__rateru) period += 1;
+				
+				if(i>=9) break;
+			}
+	        
+			var weekAgoRate = getParam(rows[period], null, null, /(?:[\s\S]*?<td[^>]*>){3}<span[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces, parseBalanceSilent);
 	    	var averrageRate = result.__rateru/period;
 			delete result.__rateru;
 			getParam(averrageRate, result, 'averrateru', null, null, parseBalanceRound);
@@ -84,15 +114,18 @@ function main() {
 	}
 	
 	if (AnyBalance.isAvailable('trendcbrf', 'averratecbrf')) {
-	    if (json.ratesCbr && json.ratesCbr.length>0){
+	    if (rows && rows.length>0){
 			var __ratecbrf;
-	    	for(var i=0; i<json.ratesCbr.length; ++i){
-	    		var rate = json.ratesCbr[i];
-	    		sumParam(rate.rate, result, '__ratecbrf', null, null, parseBalanceSilent, aggregate_sum);
+			var period = 0;
+	    	for(var i=0; i<rows.length; ++i){
+				sumParam(rows[i], result, '__ratecbrf', /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalanceSilent, aggregate_sum);
+				
+				if(result.__ratecbrf) period += 1;
+				
+				if(i>=9) break;
 	    	}
-	        	
-	    	var weekAgoRate = json.ratesCbr[0].rate;
-	    	var period = json.ratesCbr.length;
+	        
+			var weekAgoRate = getParam(rows[period], null, null, /(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalanceSilent);
 	    	var averrageRate = result.__ratecbrf/period;
 			delete result.__ratecbrf;
 			getParam(averrageRate, result, 'averratecbrf', null, null, parseBalanceRound);
@@ -106,6 +139,8 @@ function main() {
 	    	}
 	    }
 	}
+	
+	setCountersToNull(result);
     
     AnyBalance.setResult(result);
 }

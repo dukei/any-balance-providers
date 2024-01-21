4b0c6a2bd0448636dc8e185d74b568f92fcf037d
Dmitry Kochin <dco@mail.ru>
2024-01-21 13:59:39 +0300

    BoxBerry (ab-tracking-boxberry): v.3:  	- Поддержка изменений на сайте.

--- providers/ab-tracking-boxberry/main.js
+++ providers/ab-tracking-boxberry/main.js
@@ -3,77 +3,95 @@
 */
 
 var g_headers = {
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 };
+
+var g_baseurl = 'https://boxberry.ru/';
+
+function callApi(verb){
+	AnyBalance.trace('Запрос: ' + g_baseurl + verb);
+	html = AnyBalance.requestGet(g_baseurl + verb, addHeaders({
+		Accept: 'application/json, text/plain, */*',
+		Referer: g_baseurl
+	}));
+    if(!html || AnyBalance.getLastStatusCode() >= 500){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
+	}
+	var json = getJson(html);
+    AnyBalance.trace('Ответ: ' + JSON.stringify(json));
+	if(json.errorMessage){
+		var error =  json.errorMessage;
+		if(error)
+			throw new AnyBalance.Error(error, null, /не найден/i.test(error));
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось войти в личный кабинет. Сайт изменен?');
+	}
+	return json;
+}
 
 function main() {
 	var prefs = AnyBalance.getPreferences();
-	var baseurl = 'http://boxberry.ru/';
+	
 	AnyBalance.setDefaultCharset('utf-8');
 	
-	checkEmpty(prefs.login, 'Введите трек-номер!');
-	
-	try {
-		var html = AnyBalance.requestGet(baseurl, g_headers);
-	} catch(e){}
-	
-	if(AnyBalance.getLastStatusCode() > 400 || !html) {
-		throw new AnyBalance.Error('Ошибка! Сервер не отвечает! Попробуйте обновить данные позже.');
-	}
-	
-	var params = createFormParams(html, function(params, str, name, value) {
-		if (name == 'login') 
-			return prefs.login;
-		else if (name == 'password')
-			return prefs.password;
-
-		return value;
-	});
-	
-	html = AnyBalance.requestPost(baseurl + 'departure_track/', {
-		id: prefs.login,
-	}, addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));
-	
-	if (!/№ заказа/i.test(html)) {
-		var error = getParam(html, null, null, /<div[^>]+class="t-error"[^>]*>[\s\S]*?<ul[^>]*>([\s\S]*?)<\/ul>/i, replaceTagsAndSpaces, html_entity_decode);
-		if (error)
-			throw new AnyBalance.Error(error, null, /Неверный логин или пароль/i.test(error));
-		
-		AnyBalance.trace(html);
-		throw new AnyBalance.Error('Не удалось найти информацию по трек-номеру ' + prefs.login + '. Сайт изменен?');
-	}
+	checkEmpty(prefs.login, 'Введите трек-номер отправления!');
 	
 	var result = {success: true};
+
+	var json = callApi('api/v1/tracking/order/get?searchId=' + prefs.login);
 	
-	getParam(html, result, 'orderNum', /<b>\s*№ заказа интернет-магазина(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, '__tariff', /<b>\s*№ заказа интернет-магазина(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'storeName', /<b>\s*Название интернет-магазина(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'dest', /<b>\s*Город назначения(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'weight', /<b>\s*Вес товара(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, 'point', /<b>\s*Пункт выдачи(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
-	getParam(html, result, ['orderNumBox', 'all'], /<b>\s*Номер заказа Boxberry(?:[^>]*>){3}([\s\S]*?)<\//i, replaceTagsAndSpaces, html_entity_decode);
+	if(json.length == 0)
+		throw new AnyBalance.Error('Неизвестный тип почтового отправления, проверьте правильность введенных данных');
+	
+	var info = json[0];
+	
+	var infoOrderNum = (info && info.imCode) || '',
+	    infoOrderNumBox = (info && info.order_id) || '',
+		infoStoreName = (info && info.NameIM) || '???',
+		infoWeight = (info && info.Weight) || 0,
+		infoToPay = (info && info.sum) || 0,
+		infoPointCity = (info && info.point_city) || '???',
+		infoAddressCD = (info && info.addressCD) || '???',
+		infoTrackNum = (info && info.ProgramNumber) || '',
+		infoTrackId = (info && info.track_id) || '';
+
+	var json = callApi('api/v1/tracking/status/get?trackId=' + infoTrackId);
+	
+	var status = json.Statuses[json.Statuses.length-1]; // Статус ищем с конца
+	
+	var statusDate = (status && status.date_time) || '',
+//		statusPlace = (status && status.location) || '',
+		statusName = (status && status.name) || '???';
+	
+	getParam(infoOrderNum, result, 'orderNum');
+	getParam(infoOrderNumBox, result, 'orderNumBox');
+	getParam(infoTrackNum, result, '__tariff');
+    getParam(infoStoreName, result, 'storeName');
+    getParam(statusDate, result, 'date', null, [/\(|\)/g, ''], parseDate);
+	getParam(infoPointCity, result, 'dest');
+	getParam(infoToPay, result, 'toPay');
+    getParam(infoAddressCD, result, 'point');
+    getParam(statusName, result, 'status');
+	getParam(infoWeight, result, 'weight');
 	
 	if(isAvailable('all')) {
-		html = AnyBalance.requestPost(baseurl + 'track2.php', {
-			'type': 'trackone',
-			'type_track': 'z',
-			'id': result.orderNumBox
-		}, addHeaders({Referer: baseurl, 'X-Requested-With':'XMLHttpRequest'}));		
-		
-		var items = sumParam(html, null, null, /\d{2}\.\d{2}.\d{2} \(?\d{2}:\d{2}\)?(?:[^>]*>){3}[\s\S]*?<\//ig);
-		if(items) {
-			var text = '';
+		var items = json.Statuses;
+		if(items && items.length > 0) {
 			for(var i = 0; i < items.length; i++) {
-				var match = /(\d{2}\.\d{2}.\d{2} \(?\d{2}:\d{2}\)?)(?:[^>]*>){3}([\s\S]*?)<\//i.exec(items[i]);
-				
-				text += match[1] + ': ' + match[2] + '\n';
+				var item = items[i];
+                
+				sumParam(item.date_time.replace(/\(|\)/g, '') + ': ' + item.name, result, 'all', null, null, null, create_aggregate_join('.\n '));
 			}
-			result.all = text;
-		}
+		}else{
+		    AnyBalance.trace('Не удалось получить информацию по статусам отправления');
+	    }
 	}
+	
 	AnyBalance.setResult(result);
 }

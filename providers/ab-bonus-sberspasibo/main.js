c883ede86307c053f1b7a3805b5fd1f46df4364f
Dmitry Kochin <dco@mail.ru>
2024-02-28 17:15:55 +0300

    СберСпасибо (ab-bonus-sberspasibo): v.3:  	- Исправлены незначительные ошибки 	- Добавлены дополнительные счетчики

--- providers/ab-bonus-sberspasibo/main.js
+++ providers/ab-bonus-sberspasibo/main.js
@@ -31,10 +31,15 @@ function loginPure(){
 		Referer: baseurl + "/login"
 	}));
 	
+	if(AnyBalance.getLastStatusCode() >= 500){
+        AnyBalance.trace(html);
+        throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
+    }
+	
 	var json = getJson(html);
 	AnyBalance.trace(JSON.stringify(json));
 	
-	if (json.data.isSended === true){
+	if(json.data.isSended === true){
 	    var code = AnyBalance.retrieveCode('Пожалуйста, введите код подтверждения из SMS, высланного на номер +7' + prefs.login, null, {inputType: 'number', time: 180000});
     }else{
 		throw new AnyBalance.Error('Не удалось отправить SMS с кодом подтверждения. Сайт изменен?');
@@ -79,8 +84,12 @@ function loginAccessToken(){
 		var html = AnyBalance.requestGet(baseurl + '/api/online/personal/me', addHeaders({
 			Referer: baseurl + "/login"
 		}));
+		if(AnyBalance.getLastStatusCode() >= 500){
+            AnyBalance.trace(html);
+            throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
+        }
 	    var json = getJson(html);
-		if (json.error){
+		if(json.error){
 			AnyBalance.trace(json.error.messages[0]);
 		    return false;
 		}else{
@@ -105,8 +114,12 @@ function loginRefreshToken(){
 			"Content-Type": "application/json;charset=UTF-8",
 			Referer: baseurl + "/login"
 		}));
+		if(AnyBalance.getLastStatusCode() >= 500){
+            AnyBalance.trace(html);
+            throw new AnyBalance.Error('Сайт провайдера временно недоступен. Попробуйте еще раз позже');
+        }
 	    var json = getJson(html);
-		if (json.error){
+		if(json.error){
 			AnyBalance.trace(json.error.messages[0]);
 		    return false;
 		}else{
@@ -161,59 +174,99 @@ function main() {
 	
 	var html = AnyBalance.requestGet(baseurl + '/api/online/personal/me', addHeaders({Referer: baseurl + "/login"}));
 	var json = getJson(html);
+	AnyBalance.trace('Профиль: ' + JSON.stringify(json));
 	
 	getParam(json.data.loyaltySystem.balance, result, 'balance', null, null, parseBalance);
-	getParam(json.data.loyaltySystem.levlelName, result, 'level');
-	getParam(json.data.loyaltySystem.levlelName, result, '__tariff');
-	if (json.data.personal.phone){
+	getParam(json.data.loyaltySystem.miles, result, 'miles', null, null, parseBalance);
+	getParam(json.data.loyaltySystem.levelName, result, 'level');
+	getParam(json.data.loyaltySystem.levelName, result, '__tariff');
+	getParam(json.data.email, result, 'email');
+	if(json.data.personal.phone){
 	    getParam(json.data.personal.phone.replace(/.*(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 $1 $2-$3-$4'), result, 'phone');
 	}
-	if (json.data.loyaltySystem.bon_to_annulment){
+	if(json.data.loyaltySystem.bon_to_annulment && json.data.loyaltySystem.bon_to_annulment.length > 0){
 	    getParam(json.data.loyaltySystem.bon_to_annulment.value, result, 'annul', null, null, parseBalance);
 	    getParam(json.data.loyaltySystem.bon_to_annulment.date, result, 'annuldate', null, null, parseDate);
 	}
 	
-	var html = AnyBalance.requestGet(baseurl + '/api/online/personal/loyalitySystem/level', addHeaders({Referer: baseurl + "/lk_levels"}));
-	var json = getJson(html);
-	
-	if (json.data && json.data.length){
-		AnyBalance.trace('Найдено уровней: ' + json.data.length);
-		for(var i=0; i<json.data.length; ++i){
-	    	var lvl = json.data[i];
-			var cur = json.data[i].isCurrent;
-			
-			if (cur === true){
-				getParam(lvl.name, result, 'level');
-				getParam(lvl.name, result, '__tariff');
-				if (lvl.expDate){
-				    getParam(lvl.expDate.replace(/(\d\d\d\d)-(\d\d)-(\d\d).*/, '$3.$2.$1'), result, 'leveltill', null, null, parseDate);
+	if(AnyBalance.isAvailable(['level', '__tariff', 'leveltill', 'categories'])){
+	    var html = AnyBalance.requestGet(baseurl + '/api/online/personal/loyalitySystem/level', addHeaders({Referer: baseurl + "/lk_levels"}));
+	    var json = getJson(html);
+	    AnyBalance.trace('Текущий уровень: ' + JSON.stringify(json));
+	    
+	    if(json.data && json.data.length){
+		    AnyBalance.trace('Найдено уровней: ' + json.data.length);
+		    for(var i=0; i<json.data.length; ++i){
+	    	    var lvl = json.data[i];
+			    
+			    if(lvl.isCurrent === true){
+				    getParam(lvl.name, result, 'level');
+				    getParam(lvl.name, result, '__tariff');
+				    if(lvl.expDate){
+				        getParam(lvl.expDate.replace(/(\d\d\d\d)-(\d\d)-(\d\d).*/, '$3.$2.$1'), result, 'leveltill', null, null, parseDate);
+			        }
+				    
+				    if(AnyBalance.isAvailable('categories')){
+					    if(lvl.categories && lvl.categories.level && lvl.categories.level.length > 0){
+		                    AnyBalance.trace('Найдено категорий: ' + lvl.categories.level.length);
+		                    for(var j=0; j<lvl.categories.level.length; ++j){
+	                            var category = lvl.categories.level[j];
+                                
+			                    if(category.active === true)
+			                        sumParam(category.name + ': ' + category.accrual + '%', result, 'categories', null, null, null, create_aggregate_join(',<br> '));
+		                    }
+							
+							if(!result.categories)
+								result.categories = 'Нет данных';
+	                    }else{
+		                    AnyBalance.trace('Не удалось найти информацию по категориям');
+							result.categories = 'Нет данных';
+	                    }
+				    }
+				    
+				    break;
 			    }
-			}
+	        }
+	    }else{
+		    AnyBalance.trace('Не удалось получить информацию об уровнях');
 	    }
-	}else{
-		AnyBalance.trace('Не удалось получить информацию об уровнях');
 	}
 	
-	var html = AnyBalance.requestGet(baseurl + '/api/online/personal/loyalitySystem/nextLevel', addHeaders({Referer: baseurl + "/lk_levels"}));
-	var json = getJson(html);
-	
-	getParam(json.data.data.nextLevelName, result, 'levelnext');
-	
-	var html = AnyBalance.requestGet(baseurl + '/api/online/personal/loyalitySystem/transactions?page=1&cnt=100', addHeaders({Referer: baseurl + "/lk_history"}));
-	var json = getJson(html);
-	
-	if (json.data.transactions && json.data.transactions.length){
-		AnyBalance.trace('Найдено операций: ' + json.data.transactions.length);
-		for(var i=json.data.transactions.length-1; i>=0; i--){
-	    	var transaction = json.data.transactions[i];
-
-			getParam(transaction.operationDate, result, 'lastoperdate', null, null, parseDate);
-	        getParam(transaction.bonusBalanceChange, result, 'lastopersum', null, null, parseBalance);
-	        getParam(transaction.partnerName, result, 'lastoperdesc');
-			break;
+	if(AnyBalance.isAvailable('levelnext')){
+	    var html = AnyBalance.requestGet(baseurl + '/api/online/personal/loyalitySystem/nextLevel', addHeaders({Referer: baseurl + "/lk_levels"}));
+	    var json = getJson(html);
+	    AnyBalance.trace('Следующий уровень: ' + JSON.stringify(json));
+	    
+	    getParam(json.data.data.nextLevelName, result, 'levelnext');
+	}
+		
+	if(AnyBalance.isAvailable(['lastopertype', 'lastoperdate', 'lastoperrubsum', 'lastopersum', 'lastopercard', 'lastoperdesc'])){
+	    var html = AnyBalance.requestGet(baseurl + '/api/online/personal/loyalitySystem/transactions?page=1&cnt=100', addHeaders({Referer: baseurl + "/lk_history"}));
+	    var json = getJson(html);
+	    AnyBalance.trace('История операций: ' + JSON.stringify(json));
+	    
+	    if(json.data.transactions && json.data.transactions.length){
+		    AnyBalance.trace('Найдено операций: ' + json.data.transactions.length);
+		    for(var i=json.data.transactions.length-1; i>=0; i--){
+	    	    var transaction = json.data.transactions[i];
+			    
+			    for(var i=0; i<transaction.promotions.length; ++i){
+	                var promotion = transaction.promotions[i];
+					
+			        sumParam(promotion.name, result, 'lastopertype', null, null, null, create_aggregate_join(',\n '));
+		        }
+                
+			    getParam(transaction.operationDate, result, 'lastoperdate', null, null, parseDate);
+			    getParam(-(transaction.amount), result, 'lastoperrubsum', null, null, parseBalance);
+	            getParam(transaction.bonusBalanceChange, result, 'lastopersum', null, null, parseBalance);
+			    getParam(transaction.cardName + ' (' + transaction.cardLastDigits + ')', result, 'lastopercard');
+	            getParam(transaction.partnerName, result, 'lastoperdesc');
+			    
+			    break;
+	        }
+	    }else{
+		    AnyBalance.trace('Не удалось получить информацию о последней операции');
 	    }
-	}else{
-		AnyBalance.trace('Не удалось получить информацию о последней операции');
 	}
 
 	AnyBalance.setResult(result);

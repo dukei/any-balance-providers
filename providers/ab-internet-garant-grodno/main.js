86f18aefc854815eca7d97afecc40c8607b3c29f
Dmitry Kochin <dco@mail.ru>
2025-12-11 20:58:47 +0300

    ГАРАНТ (garant.by) (ab-internet-garant-grodno): v.5:  - Исправление ошибок

--- providers/ab-internet-garant-grodno/main.js
+++ providers/ab-internet-garant-grodno/main.js
@@ -7,7 +7,8 @@ var g_headers = {
 	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
 	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
 	'Connection': 'keep-alive',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31'
+    'Access-Control-Allow-Origin': '*',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 YaBrowser/25.10.0.0 Safari/537.36'
 };
 function main() {
 	var prefs = AnyBalance.getPreferences();
@@ -24,7 +25,7 @@ function main() {
 	
 	}
 	var token_aut = getParam(html, null, null, /authenticity_token" value="(.*)" autocomplete="off/i, replaceTagsAndSpaces, html_entity_decode);
-AnyBalance.trace(token_aut);
+AnyBalance.trace("Аuthenticity_token : "+token_aut);
 	html = AnyBalance.requestPost(baseurl+'/login', {
 			utf8: "✓",
 			authenticity_token: token_aut,
@@ -32,11 +33,12 @@ AnyBalance.trace(token_aut);
 			'user[password]': prefs.password,
 			commit: 'Войти'
 		}, addHeaders({
-				Referer: baseurl
+				Referer: baseurl,
+              
 			}));
 AnyBalance.trace(html);
-	if (!/>Личный кабинет</i.test(html)) {
-		var error = getParam(html, null, null, /Личный кабинет/i, replaceTagsAndSpaces, html_entity_decode);
+	if (/>Неверный логин или пароль</i.test(html)) {
+		var error = getParam(html, null, null, /Неверный логин или пароль/i, replaceTagsAndSpaces, html_entity_decode);
 		if (error)
 			throw new AnyBalance.Error(error);
 		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
@@ -44,30 +46,221 @@ AnyBalance.trace(html);
 	var result = {
 		success: true
 	};
+	
+	var time=new Date().getTime()
+	
+	var XCsrfToken=getParam(html, null, null, /<meta name=\"csrf-token\" content=\"(.*)\"/i,null , html_entity_decode);//replaceTagsAndSpaces
+	AnyBalance.trace("XCsrfToken : " + XCsrfToken);
+	var g_headers2 = {
+		'Accept':'application/json, text/javascript, */*; q=0.01',
+        'Access-Control-Allow-Origin': '*',
+		'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+		'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+		'X-Requested-With':'XMLHttpRequest',
+		'X-csrf-token':XCsrfToken,
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 YaBrowser/25.10.0.0 Safari/537.36'
+	};
 	var data = getParam(html, null, null, /new HupoApp\((.*)\.data/i,null , html_entity_decode);//replaceTagsAndSpaces
 	//AnyBalance.trace(data);
+	
 	var res_data = getJsonEval(data);
 //	AnyBalance.trace(res_data);
 	 AnyBalance.trace("ФИО : " + res_data.data.person.vc_name);
 	getParam(res_data.data.person.vc_name, result, 'fio');
 	AnyBalance.trace("Лицевой счет : " + res_data.data.personal_accounts[0].vc_subj_code);
 	getParam(res_data.data.personal_accounts[0].vc_subj_code, result, 'account');
-	AnyBalance.trace("Тариф интернета : " + res_data.data.servs[0].vc_name);
-	getParam(res_data.data.servs[0].vc_name, result, '__tariff');
-	AnyBalance.trace("Баланс : " + res_data.data.personal_accounts[0].n_sum_bal + ' BYN');
-	getParam(res_data.data.personal_accounts[0].n_sum_bal, result, 'balance', res_data.data.personal_accounts[0].n_sum_bal, replaceTagsAndSpaces, parseBalance);
 
-	for (var i = 0; i < res_data.data.personal_accounts.length; i++) {
-		if (res_data.data.personal_accounts[i].vc_currency_code == "BYN") {
-			if (/-3/i.test(res_data.data.personal_accounts[i].vc_account)) {
-				if (res_data.data.personal_accounts[i].n_temporal_overdraft) {
-					AnyBalance.trace("Кредит: " + res_data.data.personal_accounts[i].n_temporal_overdraft + ' BYN,' + " До: " + res_data.data.personal_accounts[i].d_overdraft_end);
-					getParam("Кредит: " + res_data.data.personal_accounts[i].n_temporal_overdraft + ' BYN,' + " До: " + res_data.data.personal_accounts[i].d_overdraft_end, result, 'balanceCredit');
-				}
-			}
+    if(res_data.data.equipment_addresses[0].vc_visual_code.indexOf('Беларусь')>=0){
+        AnyBalance.trace("Адрес : " + res_data.data.equipment_addresses[0].vc_visual_code);
+        getParam(res_data.data.equipment_addresses[0].vc_visual_code, result, '__tariff');
+    }else{
+        AnyBalance.trace("Адрес : " + res_data.data.equipment_addresses[1].vc_visual_code);
+	getParam(res_data.data.equipment_addresses[1].vc_visual_code, result, '__tariff');
+    }
+	
+
+	if(res_data.data.personal_accounts[0].vc_code.indexOf('TV')>=0){ //Если есть тариф Телевидения
+	
+	/*
+	TV
+	*/
+		json_TV = AnyBalance.requestGet(baseurl + "/accounts/"+res_data.data.personal_accounts[0].vc_code+'?='+time,g_headers2);
+		if (!json_TV || AnyBalance.getLastStatusCode() > 400) {
+			throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
 		}
-	} 
+		AnyBalance.trace("json_TV : " +json_TV);
+		var dataJsonTv = getJsonEval(json_TV);
+		AnyBalance.trace("Баланс Tv: " + dataJsonTv.data.personal_account.n_sum_bal + ' BYN');
+		getParam(dataJsonTv.data.personal_account.n_sum_bal, result, 'balanceTv', dataJsonTv.data.personal_account.n_sum_bal, replaceTagsAndSpaces, parseBalance);
+		if(dataJsonTv.data.servs[0]){
+		AnyBalance.trace("Тариф Tv: " + dataJsonTv.data.servs[0].vc_name + '');
+		getParam(dataJsonTv.data.servs[0].vc_name, result, 'tariаfTv', dataJsonTv.data.servs[0].vc_name, null, null);
+		
+	
+
+				if(dataJsonTv.data.user_additional_servs[0]){ //Если есть Доп услуга телевидения
+					var additionalServs='';
+					let sum=0.0 ;
+					for (var i = 0; i < dataJsonTv.data.user_additional_servs.length; i++) {
+					dRes=dataJsonTv.data.user_additional_servs[i];
+					AnyBalance.trace("Доп услуга: " + dRes.vc_name);
+					AnyBalance.trace("Цена в месяц: " + dRes.n_good_sum+' '+dRes.vc_currency_code);
+					additionalServs += dRes.vc_name + '-' + dRes.n_good_sum + ' ' + dRes.vc_currency_code +`\n`;
+					sum=parseFloat(dRes.n_good_sum)+parseFloat(sum);
+					}
+					AnyBalance.trace("Доп услуга: " + additionalServs);
+					AnyBalance.trace(" sumTV: " +  sum);
+				 let	fullSumTv=parseFloat(dataJsonTv.data.servs[0].n_good_sum)+parseFloat(sum);
+					getParam(additionalServs, result, 'additionalServicesTv', additionalServs, null, null);
+					AnyBalance.trace("Полный тариф TV: " +  fullSumTv);
+					getParam( ""+fullSumTv, result, 'tarifIntAdditionalTv',""+fullSumTv, replaceTagsAndSpaces, parseBalance);
+					
+
+				}
+				
+				
+		AnyBalance.trace("Сумма тарифа Телевидения в месяц: " +  dataJsonTv.data.servs[0].n_good_sum );
+		getParam( dataJsonTv.data.servs[0].n_good_sum, result, 'sumTariаfTv',  dataJsonTv.data.servs[0].n_good_sum, replaceTagsAndSpaces, parseBalance);
+		
+        
+		AnyBalance.trace("Последний платеж Телевидения: " +  dataJsonTv.data.personal_account.n_last_payment_sum);
+        let lastActivitiestv =''+dataJsonTv.data.personal_account.n_last_payment_sum+' '+dataJsonTv.data.personal_account.vc_last_payment_type+' '+dataJsonTv.data.personal_account.vc_last_payment_bank;
+     getParam( lastActivitiestv, result, 'lastActivitiesTv',  lastActivitiestv,null, null);
+     
+			}
+	/*
+	INT
+	*/
+		json_Int = AnyBalance.requestGet(baseurl + "/accounts/"+res_data.data.personal_accounts[1].vc_code+'?='+time,g_headers2);
+		if (!json_Int || AnyBalance.getLastStatusCode() > 400) {
+			throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+		}
+		AnyBalance.trace("json_Int: " + json_Int);
+		var dataJsonInt = getJsonEval(json_Int);
+		
+		AnyBalance.trace("Баланс интернета : " + dataJsonInt.data.personal_account.n_sum_bal);
+		getParam(dataJsonInt.data.personal_account.n_sum_bal, result, 'balanceInt', dataJsonInt.data.personal_account.n_sum_bal, replaceTagsAndSpaces, parseBalance);
+		AnyBalance.trace("Тариф интернета: " +  dataJsonInt.data.servs[0].vc_name );
+		getParam( dataJsonInt.data.servs[0].vc_name, result, 'tarifInt', dataJsonInt.data.servs[0].vc_name,null, null);
+		
+
+		if(dataJsonInt.data.user_additional_servs[0]){ // Доп услуга Интернета
+			var additionalServs='';
+			let sumInt=0.0;
+			for (var i = 0; i < dataJsonInt.data.user_additional_servs.length; i++) {
+			dIRes=dataJsonInt.data.user_additional_servs[i];
+			AnyBalance.trace("Доп услуга: " + dIRes.vc_name);
+			AnyBalance.trace("Цена в месяц: " + dIRes.n_good_sum+' '+dIRes.vc_currency_code);
+			additionalServs += dIRes.vc_name + '-' + dIRes.n_good_sum + ' ' + dIRes.vc_currency_code +`\n`;
+			sumInt=parseFloat(sumInt)+parseFloat(dIRes.n_good_sum);
+			}
+			AnyBalance.trace("Доп услуга: " + additionalServs);
+			getParam(additionalServs, result, 'additionalServicesInt', additionalServs, null, null);
+			
+			
+		 let	fullSumInt
+		 fullSumInt=parseFloat(dataJsonInt.data.servs[0].n_good_sum)+parseFloat(sumInt);
+		 AnyBalance.trace(" sumInt: " + sumInt);
+		 AnyBalance.trace("Полный тариф Int: " +  fullSumInt);
+		 getParam( ""+fullSumInt, result, 'tarifIntAdditionaliInt',""+fullSumInt, replaceTagsAndSpaces, parseBalance);
+	 
+		
+ 
+			
+		}				
+
+		AnyBalance.trace("Сумма тарифа Интернета в месяц: " +  dataJsonInt.data.servs[0].n_good_sum );
+		getParam( dataJsonInt.data.servs[0].n_good_sum, result, 'sumTariаfIn',  dataJsonInt.data.servs[0].n_good_sum, replaceTagsAndSpaces, parseBalance);
+	
+		AnyBalance.trace("Последний платеж Интернета: " +  dataJsonInt.data.personal_account.n_last_payment_sum);
+       let lastActivitiesint =''+dataJsonInt.data.personal_account.n_last_payment_sum+' '+dataJsonInt.data.personal_account.vc_last_payment_type+' '+dataJsonInt.data.personal_account.vc_last_payment_bank;
+	getParam( lastActivitiesint, result, 'lastActivitiesInt',  lastActivitiesint,null, null);
+	
+	if(dataJsonInt.data.servs[0].n_good_state_id==11114){
+		can_disconnect=`Услуга Интернет  оказывается`;
+		AnyBalance.trace("Состояние Услуги: "+can_disconnect);
+		getParam(can_disconnect, result, 'canDisconnect', can_disconnect, null, null);
+				
+	}else{
+		can_disconnect=`Услуга Интернет не оказывается : Пополните счет`;
+		AnyBalance.trace("Состояние Услуги: "+can_disconnect);
+		getParam(can_disconnect, result, 'canDisconnect', can_disconnect, null, null);
+		
+	}
+		
+
+
+	}else{
+	/*
+	INT
+	*/
+		json_Int = AnyBalance.requestGet(baseurl + "/accounts/"+res_data.data.personal_accounts[0].vc_code+'?='+time,g_headers2);
+
+		if (!json_Int || AnyBalance.getLastStatusCode() > 400) {
+			throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+		}
+
+		AnyBalance.trace("json_Int: " + json_Int);
+		var dataJsonInt = getJsonEval(json_Int);
+		
+		AnyBalance.trace("Баланс интернета : " + dataJsonInt.data.personal_account.n_sum_bal);
+		getParam(dataJsonInt.data.personal_account.n_sum_bal, result, 'balanceInt', dataJsonInt.data.personal_account.n_sum_bal, replaceTagsAndSpaces, parseBalance);
+		AnyBalance.trace("Тариф интернета: " +  dataJsonInt.data.servs[0].vc_name );
+		getParam( dataJsonInt.data.servs[0].vc_name, result, 'tarifInt', dataJsonInt.data.servs[0].vc_name,null, null);
+		
+
+		if(dataJsonInt.data.user_additional_servs[0]){ // Доп услуга Интернета
+			var additionalServs='';
+			let sumInt=0.0;
+			for (var i = 0; i < dataJsonInt.data.user_additional_servs.length; i++) {
+			dIRes=dataJsonInt.data.user_additional_servs[i];
+			AnyBalance.trace("Доп услуга: " + dIRes.vc_name);
+			AnyBalance.trace("Цена в месяц: " + dIRes.n_good_sum+' '+dIRes.vc_currency_code);
+			additionalServs += dIRes.vc_name + '-' + dIRes.n_good_sum + ' ' + dIRes.vc_currency_code +`\n`;
+			sumInt=parseFloat(sumInt)+parseFloat(dIRes.n_good_sum);
+			}
+			AnyBalance.trace("Доп услуга: " + additionalServs);
+			getParam(additionalServs, result, 'additionalServicesInt', additionalServs, null, null);
+			
+			
+		 let	fullSumInt
+		 fullSumInt=parseFloat(dataJsonInt.data.servs[0].n_good_sum)+parseFloat(sumInt);
+		 AnyBalance.trace(" sumInt: " + sumInt);
+		 AnyBalance.trace("Полный тариф Int: " +  fullSumInt);
+		 getParam( ""+fullSumInt, result, 'tarifIntAdditionaliInt',""+fullSumInt, replaceTagsAndSpaces, parseBalance);
+	 
+		
+			
+		}		
+		AnyBalance.trace("Сумма тарифа Интернета в месяц: " +  dataJsonInt.data.servs[0].n_good_sum );
+		getParam( dataJsonInt.data.servs[0].n_good_sum, result, 'sumTariаfIn',  dataJsonInt.data.servs[0].n_good_sum, replaceTagsAndSpaces, parseBalance);
+	
+		AnyBalance.trace("Последний платеж Интернета: " +  dataJsonInt.data.personal_account.n_last_payment_sum);
+       let lastActivitiesint =''+dataJsonInt.data.personal_account.n_last_payment_sum+' '+dataJsonInt.data.personal_account.vc_last_payment_type+' '+dataJsonInt.data.personal_account.vc_last_payment_bank;
+	getParam( lastActivitiesint, result, 'lastActivitiesInt',  lastActivitiesint,null, null);
+	
+
+	if(dataJsonInt.data.servs[0].n_good_state_id==11114){
+		can_disconnect=`Услуга Интернет  оказывается`;
+		AnyBalance.trace("Состояние Услуги: "+can_disconnect);
+		getParam(can_disconnect, result, 'canDisconnect', can_disconnect, null, null);
+				
+	}else{
+		can_disconnect=`Услуга Интернет не оказывается : Пополните счет`;
+		AnyBalance.trace("Состояние Услуги: "+can_disconnect);
+		getParam(can_disconnect, result, 'canDisconnect', can_disconnect, null, null);
+		
+	}
+	
+	}
+	
+	
+
+			if (dataJsonInt.data.personal_account.n_temporal_overdraft) {
+					AnyBalance.trace("Кредит: " + dataJsonInt.data.personal_account.n_temporal_overdraft + ' BYN,' + " До: " + dataJsonInt.data.personal_account.d_overdraft_end.replace('.000+03:00', ' '));
+					getParam("Кредит: " + dataJsonInt.data.personal_account.n_temporal_overdraft + ' BYN,' + " До: " + dataJsonInt.data.personal_account.d_overdraft_end.replace('.000+03:00', ' '), result, 'balanceCredit');
+				}
+
 	AnyBalance.requestGet(baseurl+"/logout");
 	AnyBalance.setResult(result);
 }
-

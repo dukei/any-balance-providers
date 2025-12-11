93d05a92a3a9b0419efd5b2a5cc0d2861df56e06
Dmitry Kochin <dco@mail.ru>
2025-12-11 21:05:15 +0300

    Пермэнергосбыт (Пермь) (ab-municipal-permenergosbyt): v.9:  	- Поддержка изменений на сайте 	- Исправлено получение баланса 	- Добавлены дополнительные счетчики 	- Улучшена обработка ошибок

--- providers/ab-municipal-permenergosbyt/main.js
+++ providers/ab-municipal-permenergosbyt/main.js
@@ -8,12 +8,15 @@ Operator site: http://permenergosbyt.ru/
 */
 
 var g_headers = {
-	'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Charset':'windows-1251,utf-8;q=0.7,*;q=0.3',
-	'Accept-Language':'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-	'Connection':'keep-alive',
-	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+	'Connection': 'keep-alive',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36'
 };
+
+var replaceNumber = [replaceTagsAndSpaces, /\D/g, '', /.*(\d)(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+$1 $2 $3-$4-$5'];
 
 function redirectIfNeeded(html){
     if(/document.forms\[0\].submit/i.test(html)){
@@ -52,9 +55,9 @@ function main(){
     var reAccount = /Лицевой счёт\: (\d+)/i;
 
     if (form || !/action="logout"/i.test(html), !reAccount.test(html)) {
-        var error = getElement(html, /<div[^>]+(?:alert)/i, replaceTagsAndSpaces);
+        var error = getElement(html, /<div[^>]+(?:alert-warning)[^>]*>/i, replaceTagsAndSpaces);
         if (error)
-            throw new AnyBalance.Error('Не удалось войти в личный кабинет. ' + error.replace('×', ''));
+			throw new AnyBalance.Error(error.replace('×', ''), null, /неверн|номер/i.test(error));
         AnyBalance.trace(html);
         throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
     }
@@ -124,18 +127,107 @@ function main(){
     // Не учитывает положительный баланс (переплату)
     // result.balance = -getParam(html, null, null, /<tr[^>]+class="tr-pay-total"+>.*?<\/tr>/i, replaceTagsAndSpaces, parseBalance);
     // Возьмём сумму из модального окна "Детальная информация"
-    var table = getElement(html, /<div[^>]+id="modal-help-balance"[^>]+>.*(<table.*<\/table>)/i);
+    var table = getElement(html, /<div[^>]+id="modal-help-balance"[^>]*>[\s\S]*?<table[^>]+class="table table-xs-line"[^>]*>/i);
     var trs = getElements(table, /<tr>(.*)<\/tr>/ig);
     result.balance = 0;
 	for (var i = 0; i < trs.length; i++) {
         result.balance += getParam(trs[i], null, null, '', [replaceTagsAndSpaces, /задолженность/i, '-', /переплата/i, ''], parseBalance);
 	}
+	// Информация по приборам учета
+	var table = getElement(html, /<table[^>]+id="single-header-measures-\d+"[^>]*>/i);
+    var trs = getElements(table, /<tr[^>]+class="info"[^>]*>/ig);
+	
+	if(trs && trs.length && trs.length > 0){
+	    AnyBalance.trace('Найдено счетчиков: ' + trs.length);
+	    for (var i = 0; i < trs.length; i++) {
+		    var counter = trs[i];
+		    var ctype = (i >= 1 ? 'countertype' + (i + 1) : 'countertype');
+	        var ctariff = (i >= 1 ? 'countertariff' + (i + 1) : 'countertariff');
+	        var crate = (i >= 1 ? 'counterrate' + (i + 1) : 'counterrate');
+	        var cdateset = (i >= 1 ? 'counterdateset' + (i + 1) : 'counterdateset');
+	        var cdateterm = (i >= 1 ? 'counterdateterm' + (i + 1) : 'counterdateterm');
+		    var cvalency = (i >= 1 ? 'countervalency' + (i + 1) : 'countervalency');
+		    var cnum = (i >= 1 ? 'counternum' + (i + 1) : 'counternum');
+		    
+            getParam(counter, result, ctype, /<tr(?:[\s\S]*?<td[^>]*>){1}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, capitalizeFirstLetter);
+	        getParam(counter, result, ctariff, /<tr(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, capitalizeFirstLetter);
+	        getParam(counter, result, crate, /<tr(?:[\s\S]*?<td[^>]*>){3}([\s\S]*?)<\/td>/i, [replaceTagsAndSpaces, /\s+/ig, '']);
+	        getParam(counter, result, cdateset, /<tr(?:[\s\S]*?<td[^>]*>){4}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+	        getParam(counter, result, cdateterm, /<tr(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseDate);
+		    getParam(counter, result, cvalency, /<tr(?:[\s\S]*?<td[^>]*>){6}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+		    getParam(counter, result, cnum, /<tr(?:[\s\S]*?<td[^>]*>){7}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+	    }
+	}else{
+		AnyBalance.trace('Не удалось получить информацию по счетчикам');
+	}
+	
     result.fio = current_account.fio;
     result.address = current_account.address;
-
-    getParam(html, result, 'phone', /<p><strong>Телефон\: <\/strong>(.*?)<\/p>/i, replaceTagsAndSpaces);
-    getParam(html, result, 'date', /alert.*?\:\s+(.+?)<\/strong>/i, replaceTagsAndSpaces, parseDate);
+	
+	// Резервные способы получения счетчиков
+//	getParam(html, result, 'account', /<div[^>]+class="account-num-text"[^>]*>[\s\S]*?Лицевой сч[её]т:[\s\S]*?([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+//	getParam(html, result, 'date', /alert.*?\:\s+(.+?)<\/strong>/i, replaceTagsAndSpaces, parseDate);
+//	getParam(html, result, 'address', /<div[^>]+class="client-adress"[^>]*>[\s\S]*?Адрес:[\s\S]*?([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+//	getParam(html, result, 'fio', /<div[^>]+class="client-name"[^>]*>[\s\S]*?ФИО:[\s\S]*?([\s\S]*?)<\/div>/i, replaceTagsAndSpaces);
+	
+	getParam(html, result, 'email', /<span[^>]+class="ic-email-val"[^>]*>([\s\S]*?)<\/span>/i, replaceTagsAndSpaces);
+	if(prefs.phone){
+	    getParam(prefs.phone, result, 'phone', null, replaceNumber);
+	}else{
+		getParam(html, result, 'phone', /<span[^>]+class="ic-phone-val"[^>]*>([\s\S]*?)<\/span>/i, replaceNumber);
+	}
+	
+	if(AnyBalance.isAvailable(['countervalue', 'countervalue1', 'countervalue2'])){
+	    html = AnyBalance.requestGet(baseurl + 'personal/show?action=measures', g_headers);
+	    
+	    // Показания приборов учета
+	    var table = getElement(html, /<table[^>]+id="single-header-example-\d+"[^>]*>/i);
+        var trs = getElements(table, /<tr[^>]+class="info measure_tbl"[^>]*>/ig);
+	    
+		if(trs && trs.length && trs.length > 0){
+		    AnyBalance.trace('Найдено показаний счетчиков: ' + trs.length);
+            for (var i = 0; i < trs.length; i++) {
+		        var counter = trs[i];
+	            var cvalue = (i >= 1 ? 'countervalue' + (i + 1) : 'countervalue');
+		        
+		        getParam(counter, result, cvalue, /<tr(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+	        }
+		}else{
+			AnyBalance.trace('Не удалось получить показания счетчиков');
+		}
+	}
+	
+	// Получаем историю платежей
+	if(AnyBalance.isAvailable(['lastopertype', 'lastoperdate', 'lastopersum'])){
+	    var dt = new Date();
+        var dtPrev = new Date(dt.getFullYear(), dt.getMonth()-3, '01');
+        var dateFrom = n2(dtPrev.getDate()) + '.' + n2(dtPrev.getMonth()+1) + '.' + dtPrev.getFullYear();
+		var dateTo = n2(dt.getDate()) + '.' + n2(dt.getMonth()+1) + '.' + dt.getFullYear();
+		
+		html = AnyBalance.requestGet(baseurl + 'personal/show?action=charges&active_from=' + dateFrom + '&active_to=' + dateTo, g_headers);
+	    var table = getElement(html, /<table[^>]+id="single-header-payment-\d+"[^>]*>/i);
+        var trs = getElements(table, /<tr[^>]+class="footable-partdisabled[^>]*>/ig);
+	
+	    if(trs && trs.length && trs.length > 0){
+			AnyBalance.trace('Найдено операций: ' + trs.length);
+		    for(var i=0; i<trs.length; ++i){
+	    	    var operation = trs[i];
+                
+                getParam(operation, result, 'lastopertype', /<tr(?:[\s\S]*?<td[^>]*>){2}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces);
+	    	    getParam(operation, result, 'lastoperdate', /<tr(?:[\s\S]*?<th[^>]*>){1}([\s\S]*?)<\/th>/i, replaceTagsAndSpaces, parseDate);
+	    	    getParam(operation, result, 'lastopersum', /<tr(?:[\s\S]*?<td[^>]*>){5}([\s\S]*?)<\/td>/i, replaceTagsAndSpaces, parseBalance);
+                
+                break;
+	        }
+		}else{
+			AnyBalance.trace('Не удалось получить историю операций');
+		}
+	}
 
     //Возвращаем результат
     AnyBalance.setResult(result);
+}
+
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }

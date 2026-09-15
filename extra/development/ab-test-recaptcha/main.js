494c04c2de3e9878deca2181a13aebe3883e88aa
Dmitry Kochin <dco@mail.ru>
2026-09-15 18:26:09 +0300

    Тест рекапчи

---
+++ extra/development/ab-test-recaptcha/main.js
@@ -0,0 +1,69 @@
+﻿
+/**
+Провайдер AnyBalance (http://any-balance-providers.googlecode.com)
+*/
+
+var g_headers = {
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+	'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+	'Connection': 'keep-alive',
+	// Mobile
+	//'User-Agent':'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+',
+	// Desktop
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36',
+};
+
+function main() {
+	var prefs = AnyBalance.getPreferences();
+	var baseurl = 'https://www.google.com/recaptcha/api2/demo';
+	AnyBalance.setDefaultCharset('utf-8');
+
+	var html = AnyBalance.requestGet(baseurl, g_headers);
+
+	if (!html || AnyBalance.getLastStatusCode() >= 400) {
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Сайт провайдера временно недоступен! Попробуйте обновить данные позже.');
+	}
+
+	var form = AB.getElement(html, /<form[^>]+recaptcha-demo-form[^>]*>/i);
+	if(!form){
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удаётся найти форму входа! Сайт изменен?');
+	}
+
+	var params = AB.createFormParams(form, function(params, str, name, value) {
+		if (name == 'name') {
+			return prefs.login;
+                }
+		return value;
+	});
+
+        const sitekey = AB.getParam(form, /data-sitekey="([^"]*)/);
+        const token = AB.solveRecaptcha("Пожалуйста, подтвердите, что вы не робот", baseurl, {
+		SITEKEY: sitekey,
+		USERAGENT: g_headers["User-Agent"]
+        });
+        params["g-recaptcha-response"] = token;
+
+	html = AnyBalance.requestPost(baseurl, params, AB.addHeaders({
+		Referer: baseurl
+	}));
+
+	if (!/Проверка прошла успешно/i.test(html)) {
+		//TODO: Необходимо обработать ошибку входа!
+		var error = AB.getParam(html, null, null, /<div[^>]+class="recaptcha-error-message"[^>]*>([\s\S]*?)<\/div>/i, AB.replaceTagsAndSpaces);
+		if (error) {
+			throw new AnyBalance.Error(error);
+		}
+
+		AnyBalance.trace(html);
+		throw new AnyBalance.Error('Не удалось зайти в личный кабинет. Сайт изменен?');
+	}
+
+	var result = {
+		success: true
+	};
+
+	AnyBalance.setResult(result);
+}
